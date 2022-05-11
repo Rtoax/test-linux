3d2a7bb5777ea395dd9f4635633ec032ce78d8cb
Rong Tao <2386499836@qq.com>
2022-05-11 22:36:12 +0800

    将 /proc/PIC/maps [vdso] vma 输出到文件

--- vdso/scripts/bpftrace/getauxval.c
+++ vdso/scripts/bpftrace/getauxval.c
@@ -13,16 +13,26 @@
 #include <unistd.h>
 #include <sys/auxv.h>
 
+#define VDSO_FILE_NAME "vDSO-VMA.out"
+
 int main(int argc, char *argv[])
 {
 	char cmd[256];
 	uintptr_t vdso;
+	FILE *fp;
+	int i;
 
 	vdso = (uintptr_t) getauxval(AT_SYSINFO_EHDR);
 	printf("AT_SYSINFO_EHDR \033[1;31m0x%lx\033[m\n", vdso);
 
+	/* Print /proc/PID/maps [vdso] item */
 	snprintf(cmd, sizeof(cmd), "cat /proc/%d/maps | grep vdso]$", getpid());
 	system(cmd);
+
+	/* Output vDSO VMA to File */
+	fp = fopen(VDSO_FILE_NAME, "w");
+	fwrite((void*)vdso, 4096, 1, fp);
+	fclose(fp);
 
 	return 0;
 }
