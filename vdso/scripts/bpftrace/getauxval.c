d3afb5d60fb68e96d9af21a5d00071614ce2f1c6
Rong Tao <2386499836@qq.com>
2022-05-12 09:58:37 +0800

    /proc/PID/auxv

--- vdso/scripts/bpftrace/getauxval.c
+++ vdso/scripts/bpftrace/getauxval.c
@@ -6,6 +6,12 @@
  * Get vDSO aux vector.
  *
  * You can check 'create_elf_tables.bt' scription.
+ *
+ * History:
+ * 2022.05.11 Rong Tao
+ *	Output [vdso] to file.
+ * 2022.05.12 Rong Tao
+ *	Print /proc/PID/auxv AT_SYSINFO_EHDR item.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,17 +23,35 @@
 
 int main(int argc, char *argv[])
 {
-	char cmd[256];
+	char buffer[256];
+	char *cmd, *auxv;
 	uintptr_t vdso;
 	FILE *fp;
 	int i;
 
 	vdso = (uintptr_t) getauxval(AT_SYSINFO_EHDR);
-	printf("AT_SYSINFO_EHDR \033[1;31m0x%lx\033[m\n", vdso);
+	printf("getauxval: \033[1;31m0x%016lx\033[m\n", vdso);
 
 	/* Print /proc/PID/maps [vdso] item */
-	snprintf(cmd, sizeof(cmd), "cat /proc/%d/maps | grep vdso]$", getpid());
+	cmd = buffer;
+	snprintf(cmd, sizeof(buffer), "cat /proc/%d/maps | grep vdso]$", getpid());
 	system(cmd);
+
+	/* Read /proc/PID/auxv */
+	auxv = buffer;
+	snprintf(auxv, sizeof(buffer), "/proc/%d/auxv", getpid());
+	fp = fopen(auxv, "r");
+	while (!feof(fp)) {
+		unsigned long type, value;
+		fread(&type, sizeof(unsigned long), 1, fp);
+		fread(&value, sizeof(unsigned long), 1, fp);
+		//printf("%016lx %016lx\n", type, value);
+		if (type == AT_SYSINFO_EHDR) {
+			printf("%s: \033[1;31m0x%016lx\033[m\n", auxv, value);
+			break;
+		}
+	}
+	fclose(fp);
 
 	/* Output vDSO VMA to File */
 	fp = fopen(VDSO_FILE_NAME, "w");
