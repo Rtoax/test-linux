c59700444eef967a46274fcdf5c388bb7e2e3590
Rong Tao <rongtao@cestc.cn>
2022-05-24 14:57:45 +0800

    Contain commands to a single function.

--- sched/load/scripts/loadavg.sh
+++ sched/load/scripts/loadavg.sh
@@ -43,10 +43,15 @@ format_output()
 	awk '{print $1"\t\t"$2"\t\t"$3"\t\t"$4"\t\t"$5"\t\t"$6"\t\t"$7"\t\t"$8}'
 }
 
-format_output "DATE NTask DTask RTask STask LV1 LV5 LV15"
-for ((i=0;;i++))
-do
-	echo "Loop $i" 1>&2
-	format_output "`date_` `nr_tasks` `nr_D_tasks` `nr_R_tasks` `nr_S_tasks` `load_avg`"
-	sleep 60
-done
+statistic_loadavg()
+{
+	format_output "DATE NTask DTask RTask STask LV1 LV5 LV15"
+	for ((i=0;;i++))
+	do
+		echo "Loop $i" 1>&2
+		format_output "`date_` `nr_tasks` `nr_D_tasks` `nr_R_tasks` `nr_S_tasks` `load_avg`"
+		sleep 60
+	done
+}
+
+statistic_loadavg
