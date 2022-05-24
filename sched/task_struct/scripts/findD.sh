9cfb2a9d455f5bd27089617e8a12b98bc9a02ae7
Rong Tao <rongtao@cestc.cn>
2022-05-24 09:44:48 +0800

    Find task state with ps script.

--- sched/task_struct/scripts/findD.sh
+++ sched/task_struct/scripts/findD.sh
@@ -1,3 +1,35 @@
-#!/bin/bash 
-# D - TASK_UNINTERRUPTIBLE
-ps -e -w -o stat,pid,lstart,comm | grep ^D
+#!/bin/bash
+# Rong Tao <rongtao@cestc.cn>
+
+# R - Running
+# S - Sleeping
+# D - Uninterruptible sleeping(Usually I/O)
+# Z - Zombie
+# T - Stop or Traced
+# W - Memory Swap
+# X - Dead
+# < - Higher Priority
+# N - Lower Priority
+# L - Some page locked in memory
+# s - Leader(has child tasks)
+# l - Multi-thread, cloned-thread(CLONE_THREAD, NPTL pthreads)
+# + - Background Tasks Group
+
+find_state()
+{
+	ps -e -w -o stat,pid,lstart,comm | grep $1
+}
+
+find_D()
+{
+	find_state ^D
+}
+
+find_Sleep()
+{
+	find_state ^S
+}
+
+# check D task by default
+test -z $1 && find_D
+test ! -z $1 && find_state ^$1
