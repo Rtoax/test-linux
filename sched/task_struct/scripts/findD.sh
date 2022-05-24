35c54a7107f1fc279e03185b60b065adfe97d2ce
Rong Tao <rongtao@cestc.cn>
2022-05-24 09:54:10 +0800

    Print ps state string by default.

--- sched/task_struct/scripts/findD.sh
+++ sched/task_struct/scripts/findD.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Rong Tao <rongtao@cestc.cn>
 
+# man ps | grep -A 21 "^PROCESS STATE"
 # R - Running
 # S - Sleeping
 # D - Uninterruptible sleeping(Usually I/O)
 # Z - Zombie
+# I - Idle kernel thread
 # T - Stop or Traced
 # W - Memory Swap
 # X - Dead
@@ -14,6 +16,13 @@
 # s - Leader(has child tasks)
 # l - Multi-thread, cloned-thread(CLONE_THREAD, NPTL pthreads)
 # + - Background Tasks Group
+
+check_state()
+{
+	echo -e "\033[1;31musage: findD.sh [State]\033[m"
+	echo    " [State] as follows:"
+	man ps | grep -A 21 "^PROCESS STATE"
+}
 
 find_state()
 {
@@ -31,5 +40,5 @@ find_Sleep()
 }
 
 # check D task by default
-test -z $1 && find_D
+test -z $1 && check_state
 test ! -z $1 && find_state ^$1
