6a1476dab427e21613480a6e268addcb97ac7578
Rong Tao <rongtao@cestc.cn>
2022-05-26 21:18:48 +0800

    Add more test for Guest OS.

--- tools/perf/kvm/guest-stress.sh
+++ tools/perf/kvm/guest-stress.sh
@@ -1,32 +1,89 @@
 #!/bin/bash
 # Guest OS stress test
+# Rong Tao <rongtao@cestc.cn>
 
 OUTPUT_FILE=data.out
 
+###############################################################################
 # TEST dd
-dd_()
+dd_output_()
 {
+	dd if=/dev/zero of=$OUTPUT_FILE bs=1M count=100
+}
+dd_input_()
+{
+	test -f $OUTPUT_FILE && dd if=$OUTPUT_FILE of=/dev/null bs=1M count=100
+}
+dd_loop_()
+{
+	echo "Test dd, hit ctrl-c to end."
 	while true
 	do
-		dd if=/dev/zero of=$OUTPUT_FILE bs=1M count=100
+		dd_output_ 2>/dev/null
+		dd_input_ 2>/dev/null
 	done
 }
 
-# TEST negative dentry dcache
+###############################################################################
+mkdir_cd_rmdir_()
+{
+	dir=`mktemp -u /tmp/test____dirXXXXX`
+	mkdir -p $dir
+	cd $dir
+	cd - >/dev/null
+	rmdir $dir
+}
 neg_dentry_()
 {
-	echo "Generate negative dentry, hit ctrl-c to end."
 	# access no exist directory
-	while :; do cd `mktemp -u /tmp/rongtaoXXXXX` 2>/dev/null; done
+	cd `mktemp -u /tmp/test____dirXXXXX` 2>/dev/null
+}
+dentry_loop_()
+{
+	echo "Test dentry, hit ctrl-c to end."
+	while :
+	do
+		mkdir_cd_rmdir_
+		neg_dentry_
+	done
 }
 
+##
+###############################################################################
+# Set all test here.
+#
+test_list=(dd_input_ dd_output_ neg_dentry_ mkdir_cd_rmdir_)
+test_num=${#test_list[@]}
+
+###############################################################################
+# TEST random
+get_random_()
+{
+	echo `expr $RANDOM % $test_num`
+}
+random_loop_()
+{
+	echo "Test all, hit ctrl-c to end."
+	for ((i=0;i<$test_num;i++))
+	do
+		echo ${test_list[$i]}
+	done
+	while :
+	do
+		${test_list[`get_random_`]} 2>/dev/null
+	done
+}
+
+###############################################################################
 signal_handler()
 {
 	echo "Catch SIG"
 	rm -f $OUTPUT_FILE
+	rm -rf /tmp/test____dir*
 	exit 0
 }
 
+###############################################################################
 # __usage__
 usage()
 {
@@ -35,8 +92,9 @@ cat<<'EOF'
 usage guest-stress [OPT]
 
 [OPT]
-          dd	- Write a file to disk.
-  neg_dentry	- Generate negative dentry.
+          dd	- Write/Read a file to/from disk.
+      dentry	- Generate/Access/Remove dentry.
+         ALL	- Random test all above.
 
  All test is RUNNING FOREVER, hit ctrl-C to end.
 EOF
@@ -46,15 +104,19 @@ EOF
 trap "echo Goodbye..." EXIT
 trap "signal_handler" SIGINT
 
+###############################################################################
 # __main__
 case $1 in
 dd)
-	dd_
+	dd_loop_
 	;;
-neg_dentry)
-	neg_dentry_
+dentry)
+	dentry_loop_
 	;;
-*)
+ALL)
+	random_loop_
+	;;
+-h|--help|*)
 	usage
 	;;
 esac
