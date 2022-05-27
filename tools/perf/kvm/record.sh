8098e2151716459c37309d34d5669b2e1f910163
Rong Tao <rongtao@cestc.cn>
2022-05-27 15:09:29 +0800

    perf stat.
    perf stat -e '...' -p PID record -o perf.data
    perf stat report -i perf.data

--- tools/perf/kvm/record.sh
+++ tools/perf/kvm/record.sh
@@ -10,7 +10,8 @@ kvm_record_host()
 stat_cache_misses()
 {
 	pid=$1
-	sudo perf stat -e L1-dcache-load-misses,L1-icache-load-misses,LLC-load-misses,LLC-store-misses,cache-misses,branch-misses,branch-load-misses,dTLB-load-misses,dTLB-store-misses,iTLB-load-misses,node-load-misses,node-store-misses -p $pid
+	sudo perf stat -e L1-dcache-load-misses,L1-icache-load-misses,LLC-load-misses,LLC-store-misses,cache-misses,branch-misses,branch-load-misses,dTLB-load-misses,dTLB-store-misses,iTLB-load-misses,node-load-misses,node-store-misses -p $pid record -o perf.data
+	sudo perf stat report -i perf.data
 }
 
 stat_cache_misses $@
