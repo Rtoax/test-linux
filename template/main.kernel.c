c5b734ac5b38aa04373e1f4ca58a565a120db9e4
Rong Tao <rongtao@cestc.cn>
2022-05-29 05:23:54 -0400

    Add CMake/Make/C templates.

---
+++ template/main.kernel.c
@@ -0,0 +1,31 @@
+/**
+ * File:
+ * Author:
+ * Date:
+ *
+ * Description:
+ * Ref:
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 14, 0)
+#else
+#endif
+
+static int __init kernel_init(void)
+{
+	return 0;
+}
+
+static void __exit kernel_exit(void)
+{
+}
+
+module_init(kernel_init);
+module_exit(kernel_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("");
+MODULE_DESCRIPTION("xxx");
