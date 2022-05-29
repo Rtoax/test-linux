b92d636ee5f056bb216397714af5ead4a6d248e5
Rong Tao <rongtao@cestc.cn>
2022-05-29 15:42:44 +0800

    BCC for usdt tracing.

--- bpf/bcc/samples/usdt/example.py
+++ bpf/bcc/samples/usdt/example.py
@@ -1,14 +1,16 @@
+#!/bin/env python3
 from bcc import BPF, USDT
 
 bpf_source = """
 #include <uapi/linux/ptrace.h>
-int trace_binary_exec(struct pt_regs *ctx) {
-  u64 pid = bpf_get_current_pid_tgid();
-  bpf_trace_printk("New hello_usdt process running with PID: %d\\n", pid);
+int trace_binary_exec(struct pt_regs *ctx)
+{
+    u64 pid = bpf_get_current_pid_tgid();
+    bpf_trace_printk("New hello_usdt process running with PID: %d\\n", pid);
 }
 """
 
-usdt = USDT(path = "./hello_usdt")
+usdt = USDT(path = "./hello_usdt.out")
 usdt.enable_probe(probe = "probe-main", fn_name = "trace_binary_exec")
 bpf = BPF(text = bpf_source, usdt_contexts = [usdt])
 bpf.trace_print()
--- bpf/bcc/samples/usdt/hello_usdt.c
+++ bpf/bcc/samples/usdt/hello_usdt.c
@@ -1,7 +1,8 @@
 #include <sys/sdt.h>
 
-int main(int argc, char const *argv[]) {
-    DTRACE_PROBE(hello-usdt, probe-main);
-    return 0;
+int main(int argc, char const *argv[])
+{
+	DTRACE_PROBE(hello-usdt, probe-main);
+	return 0;
 }
 
