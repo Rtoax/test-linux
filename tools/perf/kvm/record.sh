ddc1245f80549735a426915b1f62b99f5f1dc521
Rong Tao <rongtao@cestc.cn>
2022-05-30 14:54:36 +0800

    perf stat -e cache-misses -p PID
     $ sudo perf stat -e cache-misses -p 2373960
     ^C
     Performance counter stats for process id '2373960':
    
                12,642      cache-misses
    
           2.680810193 seconds time elapsed

--- tools/perf/kvm/record.sh
+++ tools/perf/kvm/record.sh
@@ -7,11 +7,17 @@ kvm_record_host()
 	sudo perf kvm -i perf.data stat report
 }
 
-stat_cache_misses()
+stat_cache_misses_0()
+{
+	sudo perf stat -e cache-misses -p 2373960
+}
+
+stat_cache_misses_1()
 {
 	pid=$1
 	sudo perf stat -e L1-dcache-load-misses,L1-icache-load-misses,LLC-load-misses,LLC-store-misses,cache-misses,branch-misses,branch-load-misses,dTLB-load-misses,dTLB-store-misses,iTLB-load-misses,node-load-misses,node-store-misses -p $pid record -o perf.data
 	sudo perf stat report -i perf.data
+
 }
 
 stat_cache_misses $@
