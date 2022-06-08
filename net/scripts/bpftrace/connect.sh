6936554dbd0bb8352a7973689a3d6c9c296be107
Rong Tao <rongtao@cestc.cn>
2022-06-08 17:07:33 +0800

    Add tracepoint:syscalls:sys_enter_connect/sys_exit_connect.

---
+++ net/scripts/bpftrace/connect.sh
@@ -0,0 +1,15 @@
+#!/bin/env bpftrace
+/**
+ * File: connect.bt
+ * Author: Rong Tao <rongtao@cestc.cn>
+ * Date: 2022.06.08
+ *
+ * Make sure connect(2) enter/exit even.
+ */
+
+tracepoint:syscalls:sys_enter_connect,
+tracepoint:syscalls:sys_exit_connect
+{
+	time("%H:%M:%S ");
+	printf("%-16s %-8s %-8d\n", probe, comm, pid);
+}
