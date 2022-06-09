d6e69a0e63280d8af80e131ded61db2adf92ea96
Rong Tao <rongtao@cestc.cn>
2022-06-09 18:21:08 +0800

    Move version/modules/ to module/version

--- module/version/linux_banner.c
+++ module/version/linux_banner.c
@@ -11,17 +11,17 @@
 
 static int kernel_init(void)
 {
-    printk(KERN_INFO "my init.\n");
+	printk(KERN_INFO "my init.\n");
 	/**
 	 *	linux_banner undefined
 	 */
 	//printk(KERN_INFO "linux_banner = %s\n", linux_banner);
-    return 0;
+	return 0;
 }
 
 static void kernel_exit(void)
 {
-    printk(KERN_INFO "my exit.\n");
+	printk(KERN_INFO "my exit.\n");
 }
 
 module_init(kernel_init);
