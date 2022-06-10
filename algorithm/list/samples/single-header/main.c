baf14c5a86059c49d0f57c1cd27f6842349ddca7
Rong Tao <rongtao@cestc.cn>
2022-06-10 13:54:26 +0800

    Add Makefile and Do not specify a special kernel version

--- algorithm/list/samples/single-header/main.c
+++ algorithm/list/samples/single-header/main.c
@@ -17,92 +17,92 @@ struct list_test {
 
 int demo_list_1_add_del_swap_for_each()
 {
-    struct list_head lhead;
-    
-    struct list_test lt1 = LIST_TEST_INITIALIZER(1);
-    struct list_test lt2 = LIST_TEST_INITIALIZER(2);
-    struct list_test lt3 = LIST_TEST_INITIALIZER(3);
-    struct list_test lt4 = LIST_TEST_INITIALIZER(4);
-    struct list_test lt5 = LIST_TEST_INITIALIZER(5);
-    
-    struct list_test *iter;
-    
-    INIT_LIST_HEAD(&lhead);
-    
-    list_add_tail(&lt1.list, &lhead);
-    list_add_tail(&lt2.list, &lhead);
-    list_add_tail(&lt3.list, &lhead);
-    list_add_tail(&lt4.list, &lhead);
-    list_add_tail(&lt5.list, &lhead);
+	struct list_head lhead;
+	
+	struct list_test lt1 = LIST_TEST_INITIALIZER(1);
+	struct list_test lt2 = LIST_TEST_INITIALIZER(2);
+	struct list_test lt3 = LIST_TEST_INITIALIZER(3);
+	struct list_test lt4 = LIST_TEST_INITIALIZER(4);
+	struct list_test lt5 = LIST_TEST_INITIALIZER(5);
+	
+	struct list_test *iter;
+	
+	INIT_LIST_HEAD(&lhead);
+	
+	list_add_tail(&lt1.list, &lhead);
+	list_add_tail(&lt2.list, &lhead);
+	list_add_tail(&lt3.list, &lhead);
+	list_add_tail(&lt4.list, &lhead);
+	list_add_tail(&lt5.list, &lhead);
 
-    list_del_init(&lt3.list);
+	list_del_init(&lt3.list);
 	list_for_each_entry(iter, &lhead, list) {
 		printf("%d - ", iter->a);
 	}printf("\n");
-    
+	
 
-    return 0;
+	return 0;
 }
 
 
 
 int demo_list_2_move_and_move_tail()
 {
-    struct list_head lhead1, lhead2;
-    
-    struct list_test lt1 = LIST_TEST_INITIALIZER(1);
-    struct list_test lt2 = LIST_TEST_INITIALIZER(2);
-    struct list_test lt3 = LIST_TEST_INITIALIZER(3);
-    struct list_test lt4 = LIST_TEST_INITIALIZER(4);
-    struct list_test lt5 = LIST_TEST_INITIALIZER(5);
-    
-    struct list_test *iter;
-    
-    INIT_LIST_HEAD(&lhead1);
-    INIT_LIST_HEAD(&lhead2);
-    
-    list_add_tail(&lt1.list, &lhead1);
-    list_add_tail(&lt2.list, &lhead1);
-    list_add_tail(&lt3.list, &lhead1);
-    list_add_tail(&lt4.list, &lhead2);
-    list_add_tail(&lt5.list, &lhead2);
+	struct list_head lhead1, lhead2;
+	
+	struct list_test lt1 = LIST_TEST_INITIALIZER(1);
+	struct list_test lt2 = LIST_TEST_INITIALIZER(2);
+	struct list_test lt3 = LIST_TEST_INITIALIZER(3);
+	struct list_test lt4 = LIST_TEST_INITIALIZER(4);
+	struct list_test lt5 = LIST_TEST_INITIALIZER(5);
+	
+	struct list_test *iter;
+	
+	INIT_LIST_HEAD(&lhead1);
+	INIT_LIST_HEAD(&lhead2);
+	
+	list_add_tail(&lt1.list, &lhead1);
+	list_add_tail(&lt2.list, &lhead1);
+	list_add_tail(&lt3.list, &lhead1);
+	list_add_tail(&lt4.list, &lhead2);
+	list_add_tail(&lt5.list, &lhead2);
 
-    list_for_each_entry(iter, &lhead1, list) {
+	list_for_each_entry(iter, &lhead1, list) {
 		printf("%d - ", iter->a);
 	}printf("\n");
-    list_for_each_entry(iter, &lhead2, list) {
-		printf("%d - ", iter->a);
-	}printf("\n");
-
-    list_move(&lt1.list, &lhead2);
-
-    list_for_each_entry(iter, &lhead1, list) {
-		printf("%d - ", iter->a);
-	}printf("\n");
-    list_for_each_entry(iter, &lhead2, list) {
+	list_for_each_entry(iter, &lhead2, list) {
 		printf("%d - ", iter->a);
 	}printf("\n");
 
-    list_move_tail(&lt2.list, &lhead2);
+	list_move(&lt1.list, &lhead2);
 
-    list_for_each_entry(iter, &lhead1, list) {
+	list_for_each_entry(iter, &lhead1, list) {
 		printf("%d - ", iter->a);
 	}printf("\n");
-    list_for_each_entry(iter, &lhead2, list) {
+	list_for_each_entry(iter, &lhead2, list) {
 		printf("%d - ", iter->a);
 	}printf("\n");
 
-    return 0;
+	list_move_tail(&lt2.list, &lhead2);
+
+	list_for_each_entry(iter, &lhead1, list) {
+		printf("%d - ", iter->a);
+	}printf("\n");
+	list_for_each_entry(iter, &lhead2, list) {
+		printf("%d - ", iter->a);
+	}printf("\n");
+
+	return 0;
 }
 
 
 int main()
 {
-    demo_list_1_add_del_swap_for_each();
-    demo_list_2_move_and_move_tail();
+	demo_list_1_add_del_swap_for_each();
+	demo_list_2_move_and_move_tail();
 
 
-    return 0;
+	return 0;
 }
 
 
