44f7d07357bf59ec8beed8105b7e4c3a856e9ca8
Rong Tao <rongtao@cestc.cn>
2022-10-20 11:22:19 +0800

    Add bcc python template
    Link: https://blog.csdn.net/qq_43581644/article/details/122641670
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ template/bcc_template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from bcc import ArgString, BPF
+from bcc.containers import filter_by_containers
+from bcc.utils import printb
+import argparse
+import os
+
+bpf_text = """
+#include <linux/sched.h>
+
+struct my_data {
+    unsigned long addr_task;
+    unsigned long addr_se;
+};
+
+BPF_PERF_OUTPUT(sched_fork_events);
+
+int trace_sched_fork_entry(struct pt_regs *ctx)
+{
+    int ret = PT_REGS_RC(ctx);
+
+    struct my_data data = {};
+    struct task_struct *task = (struct task_struct *)PT_REGS_PARM1(ctx);
+
+    data.addr_task = (unsigned long)task;
+    data.addr_se = (unsigned long)&task->se;
+
+    sched_fork_events.perf_submit(ctx, &data, sizeof(data));
+
+    return 0;
+}
+
+"""
+
+def print_sched_fork_event(cpu, data, size):
+    event = b["sched_fork_events"].event(data)
+    printb(b"sched_fork: %lx %lx" % (event.addr_task, event.addr_se))
+
+b = BPF(text=bpf_text)
+b.attach_kprobe(event="sched_fork", fn_name="trace_sched_fork_entry")
+
+print("Tracing sched_fork ... Hit Ctrl-C to end")
+b["sched_fork_events"].open_perf_buffer(print_sched_fork_event)
+
+while True:
+    try:
+        b.perf_buffer_poll()
+    except KeyboardInterrupt:
+        exit()
