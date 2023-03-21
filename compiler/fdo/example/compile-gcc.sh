cf9139c8fbe273e17115f501517f2b741d589a17
Rong Tao <rongtao@cestc.cn>
2023-03-21 19:35:43 +0800

    fdo/gcc: Add prog_name var
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/fdo/example/compile-gcc.sh
+++ compiler/fdo/example/compile-gcc.sh
@@ -2,23 +2,26 @@
 
 set -e
 
+prog_name=gcc-sort
+
 cflags=""
 
 # 普通编译
 gcc_ordinary()
 {
-	gcc ${cflags} sort.c -o sort.out
+	gcc ${cflags} sort.c -o ${prog_name}-orig.out
 }
 
 # 生成profile，并使用
 gcc_fdo()
 {
-	gcc ${cflags} sort.c -o sort_fdo.out -fprofile-generate
+	gcc ${cflags} sort.c -o ${prog_name}-fdo.out -fprofile-generate
 
-	./sort_fdo.out
+	./${prog_name}-fdo.out
 
-	gcc ${cflags} sort.c -o sort_fdo.out \
-		-fprofile-use=sort.gcda
+	# -fprofile-use will use default gcda profile
+	gcc ${cflags} sort.c -o ${prog_name}-fdo.out \
+		-fprofile-use
 }
 
 # 使用AutoFDO
@@ -26,12 +29,12 @@ gcc_autofdo()
 {
 	gcc_ordinary
 
-	perf record -b -e br_inst_retired.near_taken:pp -- ./sort.out
+	perf record -b -e br_inst_retired.near_taken:pp -- ./${prog_name}-orig.out
 
-	create_gcov --binary=./sort.out --profile=perf.data \
+	create_gcov --binary=./${prog_name}-orig.out --profile=perf.data \
 		    --gcov=sort.gcov -gcov_version=1 >/dev/null
 
-	gcc ${cflags} -fauto-profile=sort.gcov sort.c -o sort_autofdo.out
+	gcc ${cflags} -fauto-profile=sort.gcov sort.c -o ${prog_name}-autofdo.out
 }
 
 
