5c5ab859a8b635e54c084c9991715c74cac4337d
Rong Tao <rongtao@cestc.cn>
2023-04-11 14:09:35 +0800

    fdo: branch: Replace f_ prefix to branch_
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/fdo/example/branch.c
+++ compiler/fdo/example/branch.c
@@ -3,18 +3,18 @@
 
 #include "common.h"
 
-unsigned long f_A(unsigned long a);
-unsigned long f_B_7th(unsigned long a);
-unsigned long f_C_1st(unsigned long a);
-unsigned long f_D_6th(unsigned long a);
-unsigned long f_E_5th(unsigned long a);
-unsigned long f_F_4th(unsigned long a);
-unsigned long f_G_3rd(unsigned long a);
-unsigned long f_H_2nd(unsigned long a);
+unsigned long branch_A(unsigned long a);
+unsigned long branch_B_7th(unsigned long a);
+unsigned long branch_C_1st(unsigned long a);
+unsigned long branch_D_6th(unsigned long a);
+unsigned long branch_E_5th(unsigned long a);
+unsigned long branch_F_4th(unsigned long a);
+unsigned long branch_G_3rd(unsigned long a);
+unsigned long branch_H_2nd(unsigned long a);
 
 
 unsigned long __noinline
-f_A(unsigned long a)
+branch_A(unsigned long a)
 {
 	unsigned long i, ret = 0;
 
@@ -25,62 +25,62 @@ f_A(unsigned long a)
 	 */
 	for (i = 0; i < a; i++) {
 		if (i % 400 == 1)
-			ret += f_B_7th(i);
+			ret += branch_B_7th(i);
 		else if (i % 300 == 2)
-			ret += f_D_6th(i);
+			ret += branch_D_6th(i);
 		else if (i % 200 == 3)
-			ret += f_E_5th(i);
+			ret += branch_E_5th(i);
 		else if (i % 100 == 4)
-			ret += f_F_4th(i);
+			ret += branch_F_4th(i);
 		else if (i % 50 == 5)
-			ret += f_G_3rd(i);
+			ret += branch_G_3rd(i);
 		else if (i % 10 == 6)
-			ret += f_H_2nd(i);
+			ret += branch_H_2nd(i);
 		else
-			ret += f_C_1st(i);
+			ret += branch_C_1st(i);
 	}
 
 	return ret;
 }
 
 unsigned long __noinline
-f_B_7th(unsigned long a)
+branch_B_7th(unsigned long a)
 {
 	return a + 3;
 }
 
 unsigned long __noinline
-f_C_1st(unsigned long a)
+branch_C_1st(unsigned long a)
 {
 	return a + 4;
 }
 
 unsigned long __noinline
-f_D_6th(unsigned long a)
+branch_D_6th(unsigned long a)
 {
 	return a + 5;
 }
 
 unsigned long __noinline
-f_E_5th(unsigned long a)
+branch_E_5th(unsigned long a)
 {
 	return a + 8;
 }
 
 unsigned long __noinline
-f_F_4th(unsigned long a)
+branch_F_4th(unsigned long a)
 {
 	return a + 9;
 }
 
 unsigned long __noinline
-f_G_3rd(unsigned long a)
+branch_G_3rd(unsigned long a)
 {
 	return a + 11;
 }
 
 unsigned long __noinline
-f_H_2nd(unsigned long a)
+branch_H_2nd(unsigned long a)
 {
 	return a + 13;
 }
@@ -88,7 +88,7 @@ f_H_2nd(unsigned long a)
 unsigned long
 test_branch(void)
 {
-	return f_A(200000000);
+	return branch_A(200000000);
 }
 
 int main()
