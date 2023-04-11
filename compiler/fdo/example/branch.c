00b8221b08f8217abf739f37ee09758e3702fef0
Rong Tao <rongtao@cestc.cn>
2023-04-11 10:11:17 +0800

    fdo: branch: Ordering of branching statements.
    $ ./compile.sh -c clang -t branch
    
    $ objdump -d clang-test-branch-orig.out | grep '<f_A>:' -A 120 | grep callq
      401193:	e8 38 01 00 00       	callq  4012d0 <f_B_7th>
      401224:	e8 b7 00 00 00       	callq  4012e0 <f_D_6th>
      401247:	e8 a4 00 00 00       	callq  4012f0 <f_E_5th>
      401265:	e8 96 00 00 00       	callq  401300 <f_F_4th>
      401283:	e8 88 00 00 00       	callq  401310 <f_G_3rd>
      4012a4:	e8 77 00 00 00       	callq  401320 <f_H_2nd>
      4012ae:	e8 7d 00 00 00       	callq  401330 <f_C_1st>
    
    Ordering of branching statements.
    
    $ objdump -d clang-test-branch-fdo.out | grep '<f_A>:' -A 120 | grep callq
      4011a0:	e8 8b 01 00 00       	callq  401330 <f_C_1st>
      401287:	e8 94 00 00 00       	callq  401320 <f_H_2nd>
      401294:	e8 77 00 00 00       	callq  401310 <f_G_3rd>
      4012a1:	e8 5a 00 00 00       	callq  401300 <f_F_4th>
      4012ae:	e8 3d 00 00 00       	callq  4012f0 <f_E_5th>
      4012bb:	e8 70 01 00 00       	callq  401430 <f_D_6th>
      4012c8:	e8 93 fd ff ff       	callq  401060 <f_B_7th>
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/fdo/example/branch.c
+++ compiler/fdo/example/branch.c
@@ -4,11 +4,13 @@
 #include "common.h"
 
 unsigned long f_A(unsigned long a);
-unsigned long f_B(unsigned long a);
-unsigned long f_C(unsigned long a);
-unsigned long f_D(unsigned long a);
-unsigned long f_E(unsigned long a);
-unsigned long f_F(unsigned long a);
+unsigned long f_B_7th(unsigned long a);
+unsigned long f_C_1st(unsigned long a);
+unsigned long f_D_6th(unsigned long a);
+unsigned long f_E_5th(unsigned long a);
+unsigned long f_F_4th(unsigned long a);
+unsigned long f_G_3rd(unsigned long a);
+unsigned long f_H_2nd(unsigned long a);
 
 
 unsigned long __noinline
@@ -16,50 +18,71 @@ f_A(unsigned long a)
 {
 	unsigned long i, ret = 0;
 
+	/**
+	 * I deliberately put the number of function calls in reverse order
+	 * of branching statements, and FDO will optimize the ordering of
+	 * branching statements.
+	 */
 	for (i = 0; i < a; i++) {
 		if (i % 400 == 1)
-			ret += f_B(i);
+			ret += f_B_7th(i);
 		else if (i % 300 == 2)
-			ret += f_D(i);
+			ret += f_D_6th(i);
 		else if (i % 200 == 3)
-			ret += f_E(i);
+			ret += f_E_5th(i);
 		else if (i % 100 == 4)
-			ret += f_F(i);
-		else if (i % 2 == 0)
-			ret += f_C(i);
+			ret += f_F_4th(i);
+		else if (i % 50 == 5)
+			ret += f_G_3rd(i);
+		else if (i % 10 == 6)
+			ret += f_H_2nd(i);
+		else
+			ret += f_C_1st(i);
 	}
 
 	return ret;
 }
 
 unsigned long __noinline
-f_B(unsigned long a)
+f_B_7th(unsigned long a)
 {
 	return a + 3;
 }
 
 unsigned long __noinline
-f_C(unsigned long a)
+f_C_1st(unsigned long a)
 {
 	return a + 4;
 }
 
 unsigned long __noinline
-f_D(unsigned long a)
+f_D_6th(unsigned long a)
 {
 	return a + 5;
 }
 
 unsigned long __noinline
-f_E(unsigned long a)
+f_E_5th(unsigned long a)
 {
 	return a + 8;
 }
 
 unsigned long __noinline
-f_F(unsigned long a)
+f_F_4th(unsigned long a)
 {
 	return a + 9;
+}
+
+unsigned long __noinline
+f_G_3rd(unsigned long a)
+{
+	return a + 11;
+}
+
+unsigned long __noinline
+f_H_2nd(unsigned long a)
+{
+	return a + 13;
 }
 
 unsigned long
