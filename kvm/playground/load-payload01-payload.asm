e276a770171366d5ac248c8a02d9603a698f89a5
Rong Tao <rongtao@cestc.cn>
2023-04-26 19:43:15 +0800

    kvm: playground: Add load-payload01
    Link: https://gitlab.com/Rtoax/kvm-playground.git
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ kvm/playground/load-payload01-payload.asm
@@ -0,0 +1,7 @@
+	global start_of_code
+	global end_of_code
+section .data
+start_of_code:
+	incbin "load-payload01-program.bin"
+end_of_code:
+
---
+++ kvm/playground/load-payload01-program.asm
@@ -0,0 +1,23 @@
+	global start
+
+start:  mov dx, 0x3f8	
+	mov eax, "abcd"
+	call print_eax
+	hlt
+
+print_eax:
+	push edx
+	push ecx
+	mov ecx, 0x4
+	mov dx, 0x3f8
+.loop:	
+	out dx, al
+	shr eax, 0x8
+	sub ecx, 0x1
+	jnz .loop
+.end:
+	mov al, "x"
+	out dx, al
+	pop ecx
+	pop edx
+	ret	
