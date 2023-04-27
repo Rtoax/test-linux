df9567e98c1a6a64d34bfbb62898548a1ee39d6b
Rong Tao <rongtao@cestc.cn>
2023-04-27 14:27:23 +0800

    kvm: cpuid: Return -E2BIG when KVM_GET_SUPPORTED_CPUID hits max entries
    kernel commit("KVM: x86: Return -E2BIG when KVM_GET_SUPPORTED_CPUID
    hits max entries")
    
    Link: https://lwn.net/Articles/813719/
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- kvm/playground/cpuid.c
+++ kvm/playground/cpuid.c
@@ -1,3 +1,10 @@
+/**
+ * Get kvm CPUID
+ *
+ * refs:
+ * [0] https://www.kernel.org/doc/html/latest/virt/kvm/api.html
+ * [1] https://lwn.net/Articles/813719/
+ */
 #include <cpuid.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -30,14 +37,35 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
+	/**
+	 * Check KVM_GET_SUPPORTED_CPUID is support or not.
+	 * https://www.kernel.org/doc/html/latest/virt/kvm/api.html
+	 */
 	ret = ioctl(kvm, KVM_CHECK_EXTENSION, KVM_CAP_EXT_CPUID);
 	if(ret == -1) {
 		printf("KVM_CAP_EXT_CPUID not available. Error code: %d\n", ret);
 		return -1;
 	}
 
+	/**
+	 * Check KVM_GET_EMULATED_CPUID is support or not.
+	 * https://www.kernel.org/doc/html/latest/virt/kvm/api.html
+	 */
+	ret = ioctl(kvm, KVM_CHECK_EXTENSION, KVM_CAP_EXT_EMUL_CPUID);
+	if(ret == -1) {
+		printf("KVM_CAP_EXT_CPUID not available. Error code: %d\n", ret);
+		return -1;
+	}
+
 	struct kvm_cpuid2 *cpuid;
-	int nent = 40;
+	/**
+	 * See kernel commit("KVM: x86: Return -E2BIG when KVM_GET_SUPPORTED_CPUID
+	 * hits max entries")
+	 */
+#ifndef KVM_MAX_CPUID_ENTRIES
+#define KVM_MAX_CPUID_ENTRIES 256
+#endif
+	int nent = KVM_MAX_CPUID_ENTRIES;
 	unsigned long size = sizeof(*cpuid) + nent * sizeof(*cpuid->entries);
 
 	cpuid = (struct kvm_cpuid2*)malloc(size);
@@ -45,15 +73,27 @@ int main(int argc, char **argv)
 
 	cpuid->nent = nent;
 
+	/**
+	 * KVM_GET_SUPPORTED_CPUID
+	 * - This ioctl returns x86 cpuid features which are supported by both
+	 *   the hardware and kvm in its default configuration.
+	 *
+	 * KVM_GET_EMULATED_CPUID
+	 * - This ioctl returns x86 cpuid features which are emulated by kvm.
+	 *   Userspace can use the information returned by this ioctl to query
+	 *   which features are emulated by kvm instead of being present natively.
+	 */
 	ret = ioctl(kvm, KVM_GET_SUPPORTED_CPUID, cpuid);
 	printf("CPUID ret no: %d\n", ret);
 	if (ret == -1) {
-		printf("KVM_GET_SUPPORTED_CPUID could not read CPUID info. Error code: %d\n", ret);
+		printf("KVM_GET_SUPPORTED_CPUID could not read CPUID info. Error code: %d, %d\n",
+			ret, cpuid->nent);
 		return -1;
 	}
 
 	for (i = 0; i < cpuid->nent; i++) {
-		printf("F: 0x%08x, idx: 0x%08x, flags: 0x%08x, eax: 0x%08x, ebx: 0x%08x, ecx: 0x%08x, edx: 0x%08x\n",
+		printf("%-4d F: 0x%08x, idx: 0x%08x, flags: 0x%08x, eax: 0x%08x, ebx: 0x%08x, ecx: 0x%08x, edx: 0x%08x\n",
+			i,
 			cpuid->entries[i].function,
 			cpuid->entries[i].index,
 			cpuid->entries[i].flags,
