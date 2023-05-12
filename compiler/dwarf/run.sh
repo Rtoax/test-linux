dafc8f539d2a5cf416df378582cf233115097cfd
Rong Tao <rongtao@cestc.cn>
2023-05-12 16:44:22 +0800

    dwarf: run.sh: Hint dwarfdump command line
    $ ./run.sh
    .debug_info: dwarfdump --print-info (-i)
    .debug_abbrev: dwarfdump --print-abbrev (-b)
    .debug_frame: dwarfdump --print-frame (-f)
    .debug_line: dwarfdump --print-lines (-l)
    .debug_str: dwarfdump --print-strings (-s)
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/dwarf/run.sh
+++ compiler/dwarf/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 gcc hello.c -g -o hello
-dwarfdump hello | more
+
+cat <<-EOF
+.debug_info: dwarfdump --print-info (-i)
+.debug_abbrev: dwarfdump --print-abbrev (-b)
+.debug_frame: dwarfdump --print-frame (-f)
+.debug_line: dwarfdump --print-lines (-l)
+.debug_str: dwarfdump --print-strings (-s)
+EOF
