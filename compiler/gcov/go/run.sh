3141a3d31bb420d65c04ececdc912ca8bd30c1c0
Rong Tao <rongtao@cestc.cn>
2023-05-16 23:34:49 -0400

    gcov: dwarf: golang
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/gcov/go/run.sh
+++ compiler/gcov/go/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-export CGO_CFLAGS="-g -O2 -fprofile-generate -fprofile-arcs -ftest-converage -lgcov"
-export CGO_LDFLAGS="-g -O2 -fprofile-arcs -lgcov"
-go build hello.go
-./hello
+unset CGO_CFLAGS
+unset CGO_LDFLAGS
+go build -o hello hello.go
+
+CFLAGS="-g -O2 -fprofile-generate -fprofile-arcs -ftest-converage -lgcov"
+LDFLAGS="-g -O2 -fprofile-arcs -lgcov"
+export CGO_CFLAGS=${CFLAGS}
+export CGO_LDFLAGS=${LDFLAGS}
+go build -o hello1 hello.go
+
+#go build -ldflags="-s" -o hello2 hello.go
