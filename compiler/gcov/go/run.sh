48671ce95ea8bfabeab6db21a06db68e090d54ac
Rong Tao <rtoax@foxmail.com>
2023-05-17 13:57:51 +0800

    gcov: autofdo: golang
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- compiler/gcov/go/run.sh
+++ compiler/gcov/go/run.sh
@@ -11,3 +11,16 @@ export CGO_LDFLAGS=${LDFLAGS}
 go build -o hello1 hello.go
 
 #go build -ldflags="-s" -o hello2 hello.go
+
+autofdo()
+{
+	local exe=$1
+	local perf_data=$exe.perf.data
+
+	perf record -b -e br_inst_retired.near_taken:pp -o $perf_data -- ./$exe
+	create_gcov --binary=$exe --profile=$perf_data --gcov=$exe.gcov -gcov_version=1
+	dump_gcov $exe.gcov
+}
+
+autofdo hello
+autofdo hello1
