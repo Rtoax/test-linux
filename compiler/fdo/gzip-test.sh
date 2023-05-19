c7a533773feea5c1b17ac27d2b4e0c3398be6ee1
Rong Tao <rongtao@cestc.cn>
2023-05-19 14:06:09 +0800

    fdo: gzip: Auto compile
    $ ./gzip-test.sh compile-orig
    $ ./gzip-test.sh compile-fdo
    $ ./gzip-test.sh test
    100000+0 records in
    100000+0 records out
    409600000 bytes (410 MB, 391 MiB) copied, 0.874604 s, 468 MB/s
    
    	NAME	real	user
    gzip.orig	9.159	9.013
    gzip.fdo	8.411	8.250
    gzip.orig	9.276	9.141
    gzip.fdo	8.810	8.665
    gzip.orig	9.661	9.503
    gzip.fdo	8.591	8.447
    gzip.orig	9.302	9.162
    gzip.fdo	8.672	8.534
    gzip.orig	9.942	9.781
    gzip.fdo	8.496	8.341
    gzip.orig	8.987	8.860
    gzip.fdo	8.407	8.269
    gzip.orig	8.825	8.704
    gzip.fdo	8.816	8.676
    gzip.orig	9.078	8.943
    gzip.fdo	8.185	8.041
    gzip.orig	9.175	9.037
    gzip.fdo	8.294	8.157
    gzip.orig	8.932	8.793
    gzip.fdo	8.881	8.720
    
    NUM	Orig	FDO
    0	9.013	8.250
    1	9.141	8.665
    2	9.503	8.447
    3	9.162	8.534
    4	9.781	8.341
    5	8.860	8.269
    6	8.704	8.676
    7	8.943	8.041
    8	9.037	8.157
    9	8.793	8.720
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/fdo/gzip-test.sh
+++ compiler/fdo/gzip-test.sh
@@ -5,7 +5,12 @@
 # 2023-04-19	Rong Tao	Create this
 # 2023-05-12	Rong Tao	Support autofdo
 # 2023-05-15	Rong Tao	Support llvm-bolt
+# 2023-05-19	Rong Tao	Auto compile original/fdo
 #
+
+set -e
+
+verbose=
 
 test_data=test.dat
 declare -a orig_record fdo_record autofdo_record bolt_record
@@ -46,10 +51,12 @@ run_gzip()
 # Compile
 #
 # GCC Native:
-#  CFLAGS += -fprofile-generate
-#  gcc ... ${CFLAGS} -o gzip.orig
-#  CFLAGS += -fprofile-use
-#  gcc ... ${CFLAGS} -o gzip.fdo
+#  make clean
+#  make CFLAGS=-fprofile-generate
+#  ./gzip -k test.dat
+#  make clean
+#  make CFLAGS=-fprofile-use or
+#  make CFLAGS="-fprofile-use -Wno-missing-profile -Wno-suggest-attribute=cold"
 #
 # GCC AutoFDO
 #  gcc ... -o gzip.gcc.orig
@@ -75,22 +82,183 @@ run_gzip()
 #  CFLAGS += -fprofile-use=profile.profdata
 #  gcc ... ${CFLAGS} -o gzip.fdo
 
-dd if=/dev/random of=${test_data} bs=4096 count=100000
+gcov_dir=GCOVs
 
-num=10
-echo -e "\n\tNAME\treal\tuser"
-for ((i = 0; i < ${num}; i++))
+gen_gcov_dir()
+{
+	if [[ ! -e ${gcov_dir} ]]; then
+		mkdir ${gcov_dir}
+	fi
+}
+
+clean_gcov()
+{
+	rm -fr \
+		$(find -name '*.gcov') \
+		$(find -name '*.gcda')
+}
+
+gen_test_data()
+{
+	if [[ ! -e test.dat ]]; then
+		dd if=/dev/random of=${test_data} bs=4096 count=100000
+	fi
+}
+
+clean_tmp()
+{
+	rm -fr \
+		test.dat.gz
+}
+
+clean_exe()
+{
+	set -x
+	rm -fr \
+		gzip.orig \
+		gzip.fdo \
+		gzip.autofdo \
+		gzip.bolt
+	set +x
+}
+
+clean_git()
+{
+	git clean -dfx \
+		-e ${gcov_dir} \
+		-e test.sh \
+		-e gzip.orig \
+		-e gzip.fdo \
+		-e gzip.autofdo
+}
+
+configure_makefile()
+{
+	clean_git
+	make ${verbose:+V=1} clean || true
+
+	./bootstrap
+	./configure
+
+	sed -i 's/^CFLAGS =/CFLAGS = $(RT_CFLAGS) /g' Makefile
+	sed -i 's/^LDFLAGS =/LDFLAGS = $(RT_LDFLAGS) /g' Makefile
+}
+
+compile_orig()
+{
+	configure_makefile
+
+	make ${verbose:+V=1} clean
+	clean_tmp
+
+	make ${verbose:+V=1}
+
+	mv gzip gzip.orig
+}
+
+compile_fdo()
+{
+	gen_gcov_dir
+
+	configure_makefile
+	make ${verbose:+V=1} clean
+	clean_tmp
+	clean_gcov
+
+	make ${verbose:+V=1} RT_CFLAGS=-fprofile-generate=${gcov_dir}
+
+	gen_test_data
+
+	./gzip -k test.dat
+
+	configure_makefile
+	make ${verbose:+V=1} clean
+
+	make ${verbose:+V=1} RT_CFLAGS="-fprofile-use=${gcov_dir} -Wno-missing-profile -Wno-suggest-attribute=cold"
+
+	mv gzip gzip.fdo
+}
+
+fdo_test()
+{
+	gen_test_data
+
+	local num=10
+	echo -e "\n\tNAME\treal\tuser"
+	for ((i = 0; i < ${num}; i++))
+	do
+		run_gzip orig gzip.orig
+		run_gzip fdo gzip.fdo
+		[[ -e gzip.autofdo ]] && run_gzip autofdo gzip.autofdo
+		[[ -e gzip.bolt ]] && run_gzip bolt gzip.bolt
+	done
+
+	# Print results
+	echo
+	echo -e -n "NUM"
+	echo -e -n "\tOrig"
+	echo -e -n "\tFDO"
+	[[ -e gzip.autofdo ]] && echo -e -n "\tAutoFDO"
+	[[ -e gzip.bolt ]] && echo -e -n "\tBOLT"
+	echo
+	for ((i = 0; i < ${num}; i++))
+	do
+		echo -e -n "${i}"
+		echo -e -n "\t${orig_record[$i]}"
+		echo -e -n "\t${fdo_record[$i]}"
+		[[ -e gzip.autofdo ]] && echo -e -n "\t${autofdo_record[$i]}"
+		[[ -e gzip.bolt ]] && echo -e -n "\t${bolt_record[$i]}"
+		echo
+	done
+}
+
+while :;
 do
-	run_gzip orig gzip.orig
-	run_gzip fdo gzip.fdo
-	[[ -e gzip.autofdo ]] && run_gzip autofdo gzip.autofdo
-	[[ -e gzip.bolt ]] && run_gzip bolt gzip.bolt
+	case $1 in
+	-v|--verbose)
+		shift
+		export PS4='+${BASH_SOURCE}:${LINENO}:${FUNCNAME[0]}: '
+		set -x
+		verbose=YES
+		;;
+	*)
+		break
+		;;
+	esac
 done
 
-# Print results
-echo
-echo -e "NUM\tOrig\tFDO\tAutoFDO\tBOLT"
-for ((i = 0; i < ${num}; i++))
-do
-	echo -e "${i}\t${orig_record[$i]}\t${fdo_record[$i]}\t${autofdo_record[$i]}\t${bolt_record[$i]}"
-done
+case $1 in
+config)
+	configure_makefile
+	;;
+compile-orig)
+	compile_orig
+	;;
+compile-fdo)
+	compile_fdo
+	;;
+test)
+	fdo_test
+	;;
+clean)
+	clean_git
+	clean_gcov
+	;;
+*)
+	cat <<-EOF
+
+	gzip-test.sh [-v|--verbose] [option]
+
+	config
+
+	compile-orig
+	compile-fdo
+
+	test
+
+	clean
+
+	EOF
+	;;
+esac
+
