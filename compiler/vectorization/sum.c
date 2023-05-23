e70801405ca66042eab6961c96e71000ed06d564
Rong Tao <rongtao@cestc.cn>
2023-05-23 19:38:01 +0800

    compiler/vectorization: align struct
    Link: https://monoinfinito.wordpress.com/series/vectorization-in-gcc/
    Link: https://gcc.gnu.org/onlinedocs/gcc/Vector-Extensions.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/vectorization/sum.c
+++ compiler/vectorization/sum.c
@@ -1,4 +1,12 @@
+/**
+ * ref: https://monoinfinito.wordpress.com/series/vectorization-in-gcc/
+ */
+
 #define SIZE (5)
+
+struct value_align {
+	int v[SIZE];
+} __attribute__ ((aligned (__BIGGEST_ALIGNMENT__)));
 
 long sum(int *v)
 {
@@ -7,6 +15,17 @@ long sum(int *v)
 
 	for (i = 0; i < SIZE; i++)
 		s += v[i];
+
+	return s;
+}
+
+long sum_align(struct value_align *v)
+{
+	long s = 0;
+	unsigned int i;
+
+	for (i = 0; i < SIZE; i++)
+		s += v->v[i];
 
 	return s;
 }
