1e81f6c0429abe2ac4acaee3d2ffc4c638b4de76
Rong Tao <rtoax@foxmail.com>
2023-05-25 20:37:26 +0800

    vhost: Rename to vhost-user
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- vhost/vhost-user/config.sh
+++ vhost/vhost-user/config.sh
@@ -1,7 +1,7 @@
 QEMU=/usr/libexec/qemu-kvm
 IMG_QCOW2=$PWD/vm.qcow2
 IMG_ISO=/home/isos/AlmaLinux-9.0-x86_64-dvd.iso
-SOCK_VHOST_PATH=$PWD/vm.sock
+SOCK_VHOST_PATH=$PWD/vhost.sock
 
 if [[ ! -e ${IMG_QCOW2} ]]; then
 	qemu-img create -f qcow2 ${IMG_QCOW2} 10G
--- vhost/vhost-user/vhost-user.sh
+++ vhost/vhost-user/vhost-user.sh
@@ -4,7 +4,7 @@
 
 rm -f ${SOCK_VHOST_PATH}
 
-gcc vhost.c -o vhost
+gcc vhost-user.c -o vhost-user
 
 echo "Listen on ${SOCK_VHOST_PATH}"
-./vhost ${SOCK_VHOST_PATH}
+./vhost-user ${SOCK_VHOST_PATH}
