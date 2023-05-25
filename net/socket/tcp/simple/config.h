edab40deff3f6816a8884996a963401445bb748e
Rong Tao <rongtao@cestc.cn>
2023-05-25 16:21:53 +0800

    net: Add socket/tcp from test [0]
    [0] github.com/rtoax/test
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ net/socket/tcp/simple/config.h
@@ -0,0 +1,6 @@
+const int MAX_LINE = 2048;
+const int PORT = 6000;
+const int BACKLOG = 10;
+const int LISTENQ = 6666;
+const int MAX_CONNECT = 20;
+
---
+++ net/socket/tcp/simple/server.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#include "config.h"
+
+int main(int argc, char *argv[])
+{
+	struct sockaddr_in servaddr, cliaddr;
+	int listenfd, connfd;
+	pid_t childpid;
+	char buf[MAX_LINE];
+	socklen_t clilen;
+
+	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket error");
+		exit(1);
+	}
+
+	bzero(&servaddr, sizeof(servaddr));
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(PORT);
+
+	if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+		perror("bind error");
+		exit(1);
+	}
+
+	if (listen(listenfd, LISTENQ) < 0) {
+		perror("listen error");
+		exit(1);
+	}
+
+	for (;;) {
+		clilen = sizeof(cliaddr);
+		connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &clilen);
+		if (connfd < 0) {
+		    perror("accept error");
+		    exit(1);
+		}
+
+		if ((childpid = fork()) == 0) {
+	    	close(listenfd);
+	    	ssize_t n;
+	    	char buf[MAX_LINE];
+	    	while((n = read(connfd, buf, MAX_LINE)) > 0)
+				write(connfd, buf, n);
+	    	exit(0);
+		}
+		close(connfd);
+	}
+
+	close(listenfd);
+}
