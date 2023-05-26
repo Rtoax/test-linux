545ff06aededec2e89c820295428785acadb74a6
Rong Tao <rongtao@cestc.cn>
2023-05-26 16:55:04 +0800

    aes: Add a demo
    Link: https://www.cnblogs.com/kentle/p/15826075.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ instructions/examples/aes/test/sm4.h
@@ -0,0 +1,20 @@
+#ifndef SM4_H
+#define SM4_H
+
+#include <stdint.h>
+
+/**
+ * @brief SM4 密钥
+ */
+typedef struct _SM4_Key {
+    uint32_t rk[32];//32轮密钥
+} SM4_Key;
+
+/**
+ * @brief 初始化 SM4 轮密钥
+ * @param key 128bit长度密钥
+ * @param sm4_key SM4 密钥
+ */
+void SM4_KeyInit(uint8_t* key, SM4_Key* sm4_key);
+
+#endif // !SM4_H
