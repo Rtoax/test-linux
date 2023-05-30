f42a80dce395a83299c87412aa834c99dbcc0154
Rong Tao <rongtao@cestc.cn>
2023-05-30 09:11:26 +0800

    tracer: osnoise: Add tracing.sh
    $ sudo ./tracing.sh
    Tracing 2 seconds...
      <...>-8124 [000] .......  2563.793414: 1000000   17048  98.29520    7084    5      1   1310    230    358
      <...>-8125 [001] .......  2563.793428: 1000000     580  99.94200     247    5      1   1009    109     36
      <...>-8126 [002] .......  2563.793445: 1000000   11187  98.88130    5158    5      1   1058    160     99
      <...>-8127 [003] .......  2563.793457: 1000000     376  99.96240      89    5      0   1006    124      3
      <...>-8128 [004] .......  2563.793468: 1000000   25961  97.40390    9924    5      1   1081    166     81
      <...>-8129 [005] .......  2563.793478: 1000000    1700  99.83000     864    5      1   1015    136     30
      <...>-8130 [006] .......  2563.793494: 1000000     170  99.98300      31    5      0   1005    158     13
      <...>-8131 [007] .......  2563.793507: 1000000    1461  99.85390     649    5      0   1024    122     69
      <...>-8132 [008] .......  2563.793519: 1000000    6786  99.32140    1220    7      0   1347    152    933
      <...>-8133 [009] .......  2563.793531: 1000000     169  99.98310      32    5      0   1003    117      3
      <...>-8134 [010] .......  2563.795835: 1000000    1680  99.83200     689    6      1   1039    123     40
      <...>-8135 [011] .......  2563.795984: 1000000     232  99.97680      32    7      0   1007    132      6
      <...>-8124 [000] .......  2564.793418: 1000000     639  99.93610     164    0      0   1044    119     24
      <...>-8125 [001] .......  2564.793428: 1000000      40  99.99600      15    0      0   1001     73     29
      <...>-8126 [002] .......  2564.793446: 1000000     681  99.93190     655    0      0   1001     90     18
      <...>-8127 [003] .......  2564.793458: 1000000     107  99.98930      71    0      1   1002     85      3
      <...>-8128 [004] .......  2564.793472: 1000000      44  99.99560      31    0      0   1002    110     25
      <...>-8129 [005] .......  2564.793479: 1000000    1159  99.88410     797    0      0   1005     86     13
      <...>-8130 [006] .......  2564.793494: 1000000     115  99.98850     115    0      0   1000    107     12
      <...>-8131 [007] .......  2564.793508: 1000000     408  99.95920     103    0      0   1012     92     43
      <...>-8132 [008] .......  2564.793519: 1000000     216  99.97840      13    0      0   1017    108     53
      <...>-8133 [009] .......  2564.793532: 1000000       0 100.00000       0    0      0   1000     82      1
      <...>-8134 [010] .......  2564.795836: 1000000    3156  99.68440    1331    0      0   1046    119     95
      <...>-8135 [011] .......  2564.795984: 1000000      32  99.99680      13    2      1   1002     90      3
    
    Link: https://docs.kernel.org/trace/osnoise-tracer.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ tracer/osnoise/tracing.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# osnoise tracing
+#
+# Copyright (C) 2023 CESTC, Co.
+#
+# 2023-05-30	Rong Tao	Create this.
+#
+
+if [[ $(id -u) != 0 ]]; then
+	echo "ERROR: running with root(sudo)"
+	exit 1
+fi
+
+. /boot/config-$(uname -r)
+
+if [[ $CONFIG_OSNOISE_TRACER != y ]]; then
+	echo "ERROR: $(uname -r) is not support osnoise"
+	exit 1
+fi
+
+current_tracer=$(cat /sys/kernel/tracing/current_tracer)
+sec=2
+
+echo "Tracing $sec seconds..."
+echo osnoise > /sys/kernel/tracing/current_tracer
+
+sleep $sec
+cat /sys/kernel/tracing/trace
+
+echo ${current_tracer} > /sys/kernel/tracing/current_tracer
