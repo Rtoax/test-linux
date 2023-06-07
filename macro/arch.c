59eb955583f57c57fcf392598567e5537f1d2d42
Rong Tao <rongtao@cestc.cn>
2023-06-07 20:39:41 +0800

    macros/arch: Add uname(2)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- macro/arch.c
+++ macro/arch.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/utsname.h>
 
 const char *cpu_arch(void)
 {
 	const char *arch = "Unknown";
+
 #if defined(__x86_64__)
 	arch = "x86_64";
 #elif defined(__aarch64__)
 	arch = "aarch64";
+#else
+	static struct utsname name;
+	if (uname(&name) == -1) {
+		fprintf(stderr, "cannot get system name");
+	} else {
+		arch = name.machine;
+	}
 #endif
 	return arch;
 }
