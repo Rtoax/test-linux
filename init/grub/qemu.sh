13e5c0ad4bfb93ccb9d1dcb4c7c778a2f7bb3ca6
Rong Tao <rongtao@cest.cn>
2023-06-07 16:46:35 +0800

    grub/qemu.sh: Add more hint and add -h, -v arguments
    Signed-off-by: Rong Tao <rongtao@cest.cn>

--- init/grub/qemu.sh
+++ init/grub/qemu.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -ex
-
 qemu_emulator=/usr/libexec/qemu-kvm
 grub_img=$PWD/grub.img
 qemu_grub_img=$PWD/qemu-grub.img
@@ -15,6 +13,10 @@ test_grub_with_qemu()
 	local dev_loop=/dev/${dev_name}
 
 
+	# Do some clean first
+	sudo umount ${boot_dir}
+	sudo rm -f ${grub_img}
+
 	# 512MiB
 	dd if=/dev/zero of=${grub_img} bs=1024 count=524288
 
@@ -22,7 +24,20 @@ test_grub_with_qemu()
 	sudo losetup ${dev_loop} ${grub_img}
 
 	# Add DOS partition table
-	#  o   create a new empty DOS partition table
+	# Running:
+	#  Command (m for help): o
+	#  Created a new DOS disklabel with disk identifier 0x16488105.
+	#
+	#  Command (m for help): n
+	#  Partition type
+	#     p   primary (0 primary, 0 extended, 4 free)
+	#     e   extended (container for logical partitions)
+	#  Select (default p): p
+	#  Partition number (1-4, default 1):
+	#  First sector (2048-1048575, default 2048):
+	#  Last sector, +/-sectors or +/-size{K,M,G,T,P} (2048-1048575, default 1048575):
+	#  Created a new partition 1 of type 'Linux' and of size 511 MiB.
+	#  Command (m for help): w
 	sudo fdisk ${dev_loop}
 
 	# Flush partition info
@@ -45,7 +60,9 @@ test_grub_with_qemu()
 
 	sudo qemu-img convert -O qcow2 ${grub_img} ${qemu_grub_img}
 
+	# Running qemu...
 	sudo ${qemu_emulator} -hda ${qemu_grub_img}
+	# Copy kernel image to ${boot_dir}
 	# grub>
 	# grub>ls
 	# (hd0)(hd0,msdos1)(fd0)
@@ -61,6 +78,14 @@ while :; do
 		dev_minor=$1
 		shift
 		;;
+	-v | --verbose)
+		shift
+		set -x
+		;;
+	-e | --err)
+		shift
+		set -e
+		;;
 	-h | --help)
 		shift
 		cat <<-EOF
@@ -69,6 +94,8 @@ while :; do
 
 		--minor         device minor
 		-h, --help      show this information
+		-v, --verbose   show verbose, set -x
+		-e, --err       set -e
 
 		EOF
 		exit 0
