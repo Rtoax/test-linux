29fb1d111478d8c86dd6195b67f0ae5bbd63a8cf
Rong Tao <rongtao@cestc.cn>
2023-06-09 21:31:36 +0800

    pthread_create-deadline.c: Code indent
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/pthread/samples/pthread_create-deadline.c
+++ glibc/pthread/samples/pthread_create-deadline.c
@@ -36,83 +36,82 @@
 static volatile int done;
 
 struct sched_attr {
-   __u32 size;
+	__u32 size;
 
-   __u32 sched_policy;
-   __u64 sched_flags;
+	__u32 sched_policy;
+	__u64 sched_flags;
 
-   /* SCHED_NORMAL, SCHED_BATCH */
-   __s32 sched_nice;
+	/* SCHED_NORMAL, SCHED_BATCH */
+	__s32 sched_nice;
 
-   /* SCHED_FIFO, SCHED_RR */
-   __u32 sched_priority;
+	/* SCHED_FIFO, SCHED_RR */
+	__u32 sched_priority;
 
-   /* SCHED_DEADLINE (nsec) */
-   __u64 sched_runtime;
-   __u64 sched_deadline;
-   __u64 sched_period;
+	/* SCHED_DEADLINE (nsec) */
+	__u64 sched_runtime;
+	__u64 sched_deadline;
+	__u64 sched_period;
 };
 
 int sched_setattr(pid_t pid, const struct sched_attr *attr, unsigned int flags)
 {
-   return syscall(__NR_sched_setattr, pid, attr, flags);
+	return syscall(__NR_sched_setattr, pid, attr, flags);
 }
 
 int sched_getattr(pid_t pid, struct sched_attr *attr, unsigned int size,
 				  unsigned int flags)
 {
-   return syscall(__NR_sched_getattr, pid, attr, size, flags);
+	return syscall(__NR_sched_getattr, pid, attr, size, flags);
 }
 
 void *run_deadline(void *data)
 {
-   struct sched_attr attr;
-   int x = 0;
-   int ret;
-   unsigned int flags = 0;
+	struct sched_attr attr;
+	int x = 0;
+	int ret;
+	unsigned int flags = 0;
 
-   printf("deadline thread started [%ld]\n", gettid());
+	printf("deadline thread started [%ld]\n", gettid());
 
-   attr.size = sizeof(attr);
-   attr.sched_flags = 0;
-   attr.sched_nice = 0;
-   attr.sched_priority = 0;
+	attr.size = sizeof(attr);
+	attr.sched_flags = 0;
+	attr.sched_nice = 0;
+	attr.sched_priority = 0;
 
-   /* This creates a 10ms/30ms reservation */
-   attr.sched_policy = SCHED_DEADLINE;
-   attr.sched_runtime = 10 * 1000 * 1000;
-   attr.sched_period = attr.sched_deadline = 30 * 1000 * 1000;
+	/* This creates a 10ms/30ms reservation */
+	attr.sched_policy = SCHED_DEADLINE;
+	attr.sched_runtime = 10 * 1000 * 1000;
+	attr.sched_period = attr.sched_deadline = 30 * 1000 * 1000;
 
-   ret = sched_setattr(0, &attr, flags);
-   if (ret < 0) {
-       done = 0;
-       perror("sched_setattr");
-       exit(-1);
-   }
+	ret = sched_setattr(0, &attr, flags);
+	if (ret < 0) {
+		done = 0;
+		perror("sched_setattr");
+		exit(-1);
+	}
 
-   while (!done) {
-       x++;
-   }
+	while (!done) {
+		x++;
+	}
 
-   printf("deadline thread dies [%ld]\n", gettid());
-   return NULL;
+	printf("deadline thread dies [%ld]\n", gettid());
+	return NULL;
 }
 
 int main(int argc, char **argv)
 {
-   pthread_t thread;
+	pthread_t thread;
 
-   printf("main thread [%ld]\n", gettid());
+	printf("main thread [%ld]\n", gettid());
 
-   pthread_create(&thread, NULL, run_deadline, NULL);
+	pthread_create(&thread, NULL, run_deadline, NULL);
 
-   sleep(2);
+	sleep(2);
 
-   done = 1;
-   pthread_join(thread, NULL);
+	done = 1;
+	pthread_join(thread, NULL);
 
-   printf("main dies [%ld]\n", gettid());
-   return 0;
+	printf("main dies [%ld]\n", gettid());
+	return 0;
 }
-
 
