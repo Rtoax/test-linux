2d41969713d9ae376491927567fc50e1b27ae740
Rong Tao <rongtao@cestc.cn>
2023-06-09 15:57:41 +0800

    syscall/ioctl: Add TIOCSWINSZ
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/ioctl/TIOCSWINSZ.c
+++ syscall/samples/ioctl/TIOCSWINSZ.c
@@ -7,7 +7,7 @@ int main(void)
 
 	ioctl(fileno(stdin), TIOCGWINSZ, &sz);
 
-	printf("TIOCGWINSZ = %#08x\n", TIOCGWINSZ);
+	printf("TIOCSWINSZ = %#08x\n", TIOCGWINSZ);
 	printf("%i, %i, %i, %i\n",
 		sz.ws_col, sz.ws_row, sz.ws_xpixel, sz.ws_ypixel);
 
