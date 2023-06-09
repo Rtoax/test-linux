554981b786d2b91eab9fe91f11e6a79b04f26daa
Rong Tao <rongtao@cestc.cn>
2023-06-09 15:23:58 +0800

    syscall/ioctl: TIOCGWINSZ
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/ioctl/TIOCGWINSZ.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/ioctl.h>
+
+int main(void)
+{
+	struct winsize sz;
+
+	ioctl(fileno(stdin), TIOCGWINSZ, &sz);
+
+	printf("%i, %i, %i, %i\n",
+		sz.ws_col, sz.ws_row, sz.ws_xpixel, sz.ws_ypixel);
+
+	return 0;
+}
