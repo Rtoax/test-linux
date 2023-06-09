30a0c25a5a8619243cb651b35a550033ba37a966
Rong Tao <rongtao@cestc.cn>
2023-06-09 19:29:00 +0800

    pthread/samples: Add pthread_create.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/pthread/samples/pthread_create.c
@@ -0,0 +1,22 @@
+#include <pthread.h>
+#include <stdio.h>
+
+
+void* print_xs(void* unused)
+{
+	while (1)
+		fputc ('x', stderr);
+
+	return NULL;
+}
+
+int main(void)
+{
+	pthread_t thread_id;
+	pthread_create (&thread_id, NULL, &print_xs, NULL);
+
+	while (1)
+		fputc ('o', stderr);
+
+	return 0;
+}
