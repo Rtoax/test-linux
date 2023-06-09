78968165bb6a04ef0ec5c11305eb3e6de01f42d5
Rong Tao <rongtao@cestc.cn>
2023-06-09 22:42:52 +0800

    pthread/samples: Add pthread_setcancelstate,pthread_testcancel
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/pthread/samples/pthread_cancel.c
+++ glibc/pthread/samples/pthread_cancel.c
@@ -8,7 +8,22 @@ void* test_task_fn(void* unused)
 {
 	printf("child thread sleeping.\n");
 
+	/* Disable cancel */
+	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+
+	/* Do some important job, that can't be cancelled */
 	sleep(2);
+
+	/* Enable cancel */
+	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+
+	/* Check cancel request queue, and try exit */
+	pthread_testcancel();
+
+	printf("Nobody give me a cancel.\n");
+
+	/* Cancel immediate if cancel request received */
+	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
 
 	static int status = 12121;
 
@@ -28,8 +43,9 @@ int main (void)
 	time_t abst = time(&abst);
 	abstime.tv_sec = abst + 1;
 
-	printf("parent wait child thread exit\n");
+	printf("parent wait child thread to exit\n");
 	pthread_timedjoin_np(thread_id, (void**)&pstatus, &abstime);
+	printf("parent wait child thread to exit timeout\n");
 
 	printf("parent cancel child thread\n");
 	pthread_cancel(thread_id);
