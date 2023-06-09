fb1d5660601b5f03a2e2d8614c1f999a329e4cd6
Rong Tao <rongtao@cestc.cn>
2023-06-09 16:05:36 +0800

    syscall/ioctl: Fix TIOCGWINSZ.c TIOCSWINSZ.c for sw_64
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/ioctl/TIOCGWINSZ.c
+++ syscall/samples/ioctl/TIOCGWINSZ.c
@@ -7,7 +7,11 @@ int main(void)
 
 	ioctl(fileno(stdin), TIOCGWINSZ, &sz);
 
+#if defined(__sw_64__)
+	printf("TIOCGWINSZ = %#08lx\n", TIOCGWINSZ);
+#else
 	printf("TIOCGWINSZ = %#08x\n", TIOCGWINSZ);
+#endif
 	printf("%i, %i, %i, %i\n",
 		sz.ws_col, sz.ws_row, sz.ws_xpixel, sz.ws_ypixel);
 
--- syscall/samples/ioctl/TIOCSWINSZ.c
+++ syscall/samples/ioctl/TIOCSWINSZ.c
@@ -5,9 +5,13 @@ int main(void)
 {
 	struct winsize sz;
 
-	ioctl(fileno(stdin), TIOCGWINSZ, &sz);
+	ioctl(fileno(stdin), TIOCSWINSZ, &sz);
 
-	printf("TIOCSWINSZ = %#08x\n", TIOCGWINSZ);
+#if defined(__sw_64__)
+	printf("TIOCSWINSZ = %#08lx\n", TIOCSWINSZ);
+#else
+	printf("TIOCSWINSZ = %#08x\n", TIOCSWINSZ);
+#endif
 	printf("%i, %i, %i, %i\n",
 		sz.ws_col, sz.ws_row, sz.ws_xpixel, sz.ws_ypixel);
 
