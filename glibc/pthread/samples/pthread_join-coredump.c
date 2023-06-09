6b1c792ef0492516537aef369b3a901c1f5238fa
Rong Tao <rongtao@cestc.cn>
2023-06-09 21:47:09 +0800

    pthread/samples: Add pthread_join()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/pthread/samples/pthread_join-coredump.c
@@ -0,0 +1,44 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+pthread_t thread;
+
+void *thread_fn(void *arg)
+{
+	printf("Thread return.\n");
+	return NULL;
+}
+
+int main ()
+{
+	int r;
+	bool join = false;
+
+	r = pthread_create(&thread, NULL, thread_fn, NULL);
+	if (r == 0) {
+		printf("Create thread success.\n");
+		join = true;
+	}
+	pthread_setname_np(thread, "Thread");
+
+	/**
+	 * misoperation
+	 */
+#if defined(HAVE_MISOPERATION)
+	thread = 0;
+#else
+# pragma message "-DHAVE_MISOPERATION to misoperation pthread_t"
+#endif
+
+	if (join) {
+		printf("Wait thread exit.\n");
+		pthread_join(thread, NULL);
+	}
+
+	printf("Main return.\n");
+
+	return 0;
+}
---
+++ glibc/pthread/samples/pthread_join.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<string.h>
+
+void *fun1(void *arg)
+{
+	printf("I'm thread 1;\n");
+	return (void *)1;
+}
+void *fun2(void *arg)
+{
+	printf("I'm thread 2;\n");
+	pthread_exit((void *)2);
+}
+void *fun3(void *arg)
+{
+	while (1) {
+		printf("I'm thread 3;\n");
+		pthread_testcancel();
+	}
+
+	return ((void *)3);
+}
+
+int main()
+{
+	pthread_t tid;
+	void *retval = NULL;
+	int ret;
+	int err;
+
+	ret = pthread_create(&tid, NULL, fun1, NULL);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_create error:%s\n", strerror(ret));
+	}
+	err = pthread_join(tid, (void **)&retval);
+	if (err != 0) {
+		fprintf(stderr, "pthread_join error:%s\n", strerror(err));
+	}
+	printf("thread exit code :%ld\n", (intptr_t)retval);
+
+	ret = pthread_create(&tid, NULL, fun2, NULL);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_create error:%s\n", strerror(ret));
+	}
+	err = pthread_join(tid, (void **)&retval);
+	if (err != 0) {
+		fprintf(stderr, "pthread_join error:%s\n", strerror(err));
+	}
+	printf("thread exit code :%ld\n", (intptr_t)retval);
+
+	ret = pthread_create(&tid, NULL, fun3, NULL);
+	if (ret != 0) {
+		fprintf(stderr, "pthread_create error:%s\n", strerror(ret));
+	}
+
+	sleep(2);
+	pthread_cancel(tid);
+	err = pthread_join(tid, (void **)&retval);
+	if (err != 0) {
+		fprintf(stderr, "pthread_join error:%s\n", strerror(err));
+	}
+	printf("thread exit code :%ld\n", (intptr_t)retval);
+
+	return 0;
+}
