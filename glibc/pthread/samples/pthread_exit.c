36af98c73cd4293f9608851266e7ae409d5b2cd3
Rong Tao <rongtao@cestc.cn>
2023-06-09 21:22:22 +0800

    pthread/samples: Add pthread_exit.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/pthread/samples/pthread_exit.c
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <stdio.h>
+
+void* test_task_fn(void* unused)
+{
+	static int status = 12121;
+	printf("test_task_fn.\n");
+	pthread_exit(&status);
+	return NULL;
+}
+
+int main ()
+{
+	int *pstatus;
+	pthread_t thread_id;
+
+	pthread_create(&thread_id, NULL, test_task_fn, NULL);
+	pthread_join(thread_id, (void**)&pstatus);
+
+	printf("pstatus = %d\n", *pstatus);
+	return 0;
+}
+
