2bee43d9522cdf47a0101f9da42d26396f48e467
Rong Tao <rongtao@cestc.cn>
2023-06-09 13:16:11 +0800

    syscall: Add strace demos
    Link: https://techlog.cn/article/list/10183724
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/strace/strace.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Trace read(2),close(2) only
+sudo strace -e trace=open,read,close ls
+
+# Trace exec*
+sudo strace -q -e trace=process ls
+
+# Trace file system relate syscall
+sudo strace -q -e trace=file ls
+
+# Trace memory syscall
+sudo strace -q -e trace=memory ls
+
+# Trace network syscall
+sudo strace -q -e trace=network ls
+
+# Trace signal
+sudo strace -q -e trace=signal ls
+
+# Count each syscall
+sudo strace -c ls
+
+# Show called time
+sudo strace -t ls
+
+# Show spent time
+sudo strace -T ls
