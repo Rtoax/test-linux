a18144f1b8e79c430b4f3d601968b7a2789b0cbb
Rong Tao <rongtao@cestc.cn>
2023-06-11 14:46:41 +0800

    pthread_attr_init.c: code indent
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/pthread/samples/pthread_attr_init.c
+++ glibc/pthread/samples/pthread_attr_init.c
@@ -7,17 +7,17 @@
 
 void* test_task_fn(void* unused)
 {
-    pthread_attr_t attr;
+	pthread_attr_t attr;
 
-    memshow("before init > ", &attr, sizeof(pthread_attr_t));
+	memshow("before init > ", &attr, sizeof(pthread_attr_t));
 
-    pthread_attr_init(&attr);
-    
-    memshow("after  init > ", &attr, sizeof(pthread_attr_t));
-    
-    pthread_attr_destroy(&attr);
+	pthread_attr_init(&attr);
 
-    memshow("after destroy > ", &attr, sizeof(pthread_attr_t));
+	memshow("after  init > ", &attr, sizeof(pthread_attr_t));
+
+	pthread_attr_destroy(&attr);
+
+	memshow("after destroy > ", &attr, sizeof(pthread_attr_t));
 
 	return NULL;
 }
@@ -25,7 +25,7 @@ void* test_task_fn(void* unused)
 int main(void)
 {
 	pthread_t thread_id;
-    
+
 	pthread_create(&thread_id, NULL, test_task_fn, NULL);
 
 	pthread_join(thread_id, NULL);
