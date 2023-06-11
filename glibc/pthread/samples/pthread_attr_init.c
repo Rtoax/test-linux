31d5c8bfd85436adc9ed8dd51354d6cc5265b78f
Rong Tao <rongtao@cestc.cn>
2023-06-11 14:44:53 +0800

    Add pthread_attr_destroy, pthread_attr_init
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/pthread/samples/pthread_attr_init.c
@@ -0,0 +1,36 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#include <libs/memshow.h>
+
+
+void* test_task_fn(void* unused)
+{
+    pthread_attr_t attr;
+
+    memshow("before init > ", &attr, sizeof(pthread_attr_t));
+
+    pthread_attr_init(&attr);
+    
+    memshow("after  init > ", &attr, sizeof(pthread_attr_t));
+    
+    pthread_attr_destroy(&attr);
+
+    memshow("after destroy > ", &attr, sizeof(pthread_attr_t));
+
+	return NULL;
+}
+
+int main(void)
+{
+	pthread_t thread_id;
+    
+	pthread_create(&thread_id, NULL, test_task_fn, NULL);
+
+	pthread_join(thread_id, NULL);
+
+	return 0;
+}
+
+
