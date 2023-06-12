0edd5171b0b0b2e8ee704ea408094904f4648aec
Rong Tao <rongtao@cestc.cn>
2023-06-12 19:49:46 +0800

    syscall/samples: Update nr.sh
    Format:
    
        #if defined(NR_SYS)
        #if defined(__NR_io_setup)
        NR_SYS(__NR_io_setup)
        #endif
        #if defined(__NR_io_destroy)
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/nr.sh
+++ syscall/samples/nr.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-if [[ ! -e /usr/include/asm/unistd_64.h ]]; then
-	echo "ERROR: /usr/include/asm/unistd_64.h not exist."
+unistd=
+
+if [[ -e /usr/include/asm/unistd_64.h ]]; then
+	unistd=/usr/include/asm/unistd_64.h
+elif [[ -e /usr/include/asm-generic/unistd.h ]]; then
+	unistd=/usr/include/asm-generic/unistd.h
+fi
+
+if [[ -z ${unistd} ]]; then
+	echo "ERROR: ${unistd} not exist."
 	exit 1
 fi
 
 cat >nr.h<<-EOF
 #if defined(NR_SYS)
 EOF
-grep __NR_ /usr/include/asm/unistd_64.h | \
-	awk '{print "NR_SYS("$2")"}' \
-	>> nr.h
+for n in $(
+grep 'define __NR_' ${unistd} | \
+	awk '{print $2}' | \
+	grep -v '[{|)|(|,|;|:]')
+do
+	cat >>nr.h<<-EOF
+	#if defined($n)
+	NR_SYS($n)
+	#endif
+	EOF
+done >> nr.h
 cat >>nr.h<<-EOF
 #else
 # error "Undefined NR_SYS"
