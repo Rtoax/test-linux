f597d13fbc71a2ca960f1255d8351049295e746d
Rong Tao <rongtao@cestc.cn>
2023-06-14 15:56:32 +0800

    syscall/samples: Add sched_getaffinity()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/sched_getaffinity.c
@@ -0,0 +1,26 @@
+#define __USE_GNU
+#include <sched.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <libs/memshow.h>
+
+int main()
+{
+	int ret;
+	cpu_set_t cpuset;
+	pid_t pid;
+
+	CPU_ZERO(&cpuset);
+
+	pid = getpid();
+
+	ret = sched_getaffinity(pid, sizeof(cpu_set_t), &cpuset);
+
+	printf("ret = %d\n", ret);
+
+	memshow("cpuset > ", &cpuset, sizeof(cpu_set_t));
+
+	return 0;
+}
