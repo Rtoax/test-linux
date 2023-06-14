451069acbd5a7471d1bbde10a1ca489835ac448c
Rong Tao <rongtao@cestc.cn>
2023-06-14 16:59:14 +0800

    syscall/samples: epoll: level-trigger/edge-trigger
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/epoll/et.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/epoll.h>
+
+int main(void)
+{
+	int epfd, nfds;
+	struct epoll_event event, events[5];
+	char buf[256];
+
+	epfd = epoll_create(1);
+
+	event.data.fd = STDIN_FILENO;
+	event.events = EPOLLIN | EPOLLET;
+
+	epoll_ctl(epfd, EPOLL_CTL_ADD, STDIN_FILENO, &event);
+	while (1) {
+		nfds = epoll_wait(epfd, events, 5, -1);
+		int i;
+		for (i = 0; i < nfds; ++i) {
+			if (events[i].data.fd == STDIN_FILENO) {
+				read(STDIN_FILENO, buf, 1);
+				printf("hello world\n");
+
+				/* Modify */
+#if 0
+				event.data.fd = STDIN_FILENO;
+				event.events = EPOLLIN | EPOLLET;
+				epoll_ctl(epfd, EPOLL_CTL_MOD, STDIN_FILENO, &event);
+#endif
+			}
+		}
+	}
+}
---
+++ syscall/samples/epoll/lt.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/epoll.h>
+
+int main(void)
+{
+	int epfd, nfds;
+	char buf[256];
+	struct epoll_event event, events[5];
+	epfd = epoll_create(1);
+
+	event.data.fd = STDIN_FILENO;
+	event.events = EPOLLIN; /* default */
+
+	epoll_ctl(epfd, EPOLL_CTL_ADD, STDIN_FILENO, &event);
+	while (1) {
+		nfds = epoll_wait(epfd, events, 5, -1);
+		int i;
+		for (i = 0; i < nfds; ++i) {
+			if (events[i].data.fd == STDIN_FILENO) {
+				read(STDIN_FILENO, buf, 1);
+				printf("hello world\n");
+			}
+		}
+	}
+
+	return 0;
+}
