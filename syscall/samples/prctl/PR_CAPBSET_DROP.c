43dcb66e44b31751a536506bed029eb51b5f674e
Rong Tao <rongtao@cestc.cn>
2023-06-26 16:24:04 +0800

    prctl-PR_CAPBSET_DROP: perror more
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/prctl/PR_CAPBSET_DROP.c
+++ syscall/samples/prctl/PR_CAPBSET_DROP.c
@@ -15,7 +15,11 @@ int main(int argc, char *argv[])
 	for (cap = 0; cap < 2; cap++) {
 		ret = prctl(PR_CAPBSET_DROP, cap, 0, 0, 0);
 		if (ret < 0 && ! (errno == EINVAL)) {
-			perror("prctl drop bounding:");
+			perror("prctl drop bounding");
+		} else if (ret < 0) {
+			perror("prctl-PR_CAPBSET_DROP");
+		} else {
+			perror("prctl-PR_CAPBSET_DROP");
 		}
 	}
 
