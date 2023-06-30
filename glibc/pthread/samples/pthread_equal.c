bfb5127106b1000ccb9456977ed8f0292875d04b
Rong Tao <rongtao@cestc.cn>
2023-06-30 08:30:24 +0800

    pthread/samples: Add pthread_equal.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/pthread/samples/pthread_equal.c
@@ -0,0 +1,39 @@
+#include <pthread.h>
+#include <stdio.h>
+
+
+void* test_task_fn(void* unused)
+{
+	pthread_t threadid;
+	int ret;
+
+	threadid = pthread_self();
+
+	ret = pthread_equal(pthread_self(), threadid);
+	if (ret) {
+		printf("equal.\n");
+	}
+
+	pthread_exit(&threadid);
+	return NULL;
+}
+
+int main(void)
+{
+	int eret;
+	pthread_t thread_id;
+	pthread_t *threadid_child;
+
+	pthread_create(&thread_id, NULL, test_task_fn, NULL);
+
+	pthread_join(thread_id, (void**)&threadid_child);
+
+	eret = pthread_equal(pthread_self(), *threadid_child);
+	if (eret == 0) {
+		printf("non-equal.\n");
+	}
+
+	return 0;
+}
+
+
