32d8dbf1254271a15783ec5593e26c21b7f42f83
Rong Tao <rongtao@cestc.cn>
2023-07-03 16:43:10 +0800

    syscall/samples: Add getrlimit.c RLIMIT_CORE
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/getrlimit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+
+static void print_rlimit(const char *prefix, struct rlimit *rlimit)
+{
+	printf("%s : %ld %ld\n", prefix, rlimit->rlim_cur, rlimit->rlim_max);
+}
+
+int main(void)
+{
+	int __attribute__((unused)) ret;
+	struct rlimit rlimit;
+
+	ret = getrlimit(RLIMIT_CORE, &rlimit);
+	if (ret != 0) {
+		fprintf(stderr, "get RLIMIT_CORE failed.\n");
+		return 1;
+	}
+	/* unlimited(-1) */
+	print_rlimit("RLIMIT_CORE", &rlimit);
+
+	return 0;
+}
