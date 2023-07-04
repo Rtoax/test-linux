db1ef56c5ee87d8800367a9a6576901e78c26f93
Rong Tao <rongtao@cestc.cn>
2023-07-04 14:56:34 +0800

    syscall/samples: Fix readlink(2) error
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/readlink.c
+++ syscall/samples/readlink.c
@@ -5,12 +5,16 @@
 
 #define BUF_LEN	1024
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	int ret;
 	char buffer[BUF_LEN] = {0};
+	char *filename = argv[0];
 
-	ret = readlink("/etc/os-release", buffer, BUF_LEN);
+
+	symlink(filename, "readlink.out");
+
+	ret = readlink("readlink.out", buffer, BUF_LEN);
 	if (ret > 0)
 		printf("readlink: %s\n", buffer);
 	else
