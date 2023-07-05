1a1155a7018c13cddc689f39fa2fe883cf0a4cf6
Rong Tao <rongtao@cestc.cn>
2023-07-05 11:17:54 +0800

    glibc/stdio: Add vasprintf.c demo
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/stdio/vasprintf.c
@@ -0,0 +1,31 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdarg.h>
+#include <malloc.h>
+
+
+void test_vasprintf(char *fmt, ...)
+{
+	char *output = NULL;
+	int ret;
+	va_list va;
+
+	va_start(va, fmt);
+
+	ret = vasprintf(&output, fmt, va);
+	if (ret < 0) {
+		perror("vasprintf");
+	} else {
+		printf("output: %s", output);
+		free(output);
+	}
+
+	va_end(va);
+}
+
+int main(void)
+{
+	test_vasprintf("Hello, %s\n", "Rong Tao");
+
+	return 0;
+}
