30dff178403b7a3415bdd19de1a91e02bdced93c
Rong Tao <rongtao@cestc.cn>
2023-07-06 12:45:15 +0800

    macro: Add weak_alias() 2
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- macro/alias.c
+++ macro/alias.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+
 #include "alias.h"
+
 
 void __func1(void)
 {
 	printf("%s\n", __func__);
 }
+weak_alias(__func1, alias1_func1);
 
-void func1(void) weak_alias("__func1");
+void alias2_func1(void) f_weak_alias("__func1");
+
 
 int main(void)
 {
-	func1();
+	alias1_func1();
+	alias2_func1();
 
 	return 0;
 }
--- macro/alias.h
+++ macro/alias.h
@@ -1,4 +1,8 @@
 #pragma once
 
-#define weak_alias(name)	__attribute__((weak, alias(name)))
+#define f_weak_alias(name) \
+	__attribute__((weak, alias(name)))
+
+#define weak_alias(name, aliasname) \
+	extern typeof (name) aliasname __attribute__ ((weak, alias(#name)))
 
