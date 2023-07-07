c01c15ffe51249367384613150ab47a7cfd9b14b
Rong Tao <rongtao@cestc.cn>
2023-07-07 13:50:28 +0800

    syscall/samples: Add faccessat2.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/faccessat2.c
@@ -0,0 +1,26 @@
+#include <fcntl.h> /* Definition of AT_* constants */
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/syscall.h>
+
+
+/* No glibc swapper */
+int sys_faccessat2(int dirfd, const char *pathname, int mode, int flags)
+{
+	return syscall(SYS_faccessat2, dirfd, pathname, mode, flags);
+}
+
+int main(void)
+{
+	int ret;
+
+	ret = sys_faccessat2(AT_FDCWD, "/etc/os-release", F_OK, AT_EACCESS);
+	if (ret != 0) {
+		fprintf(stderr, "faccessat2 failed: %s\n", strerror(errno));
+	}
+
+	return 0;
+}
+
