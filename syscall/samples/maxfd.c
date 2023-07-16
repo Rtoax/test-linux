3665264653a7283aaa6bb7935beac2431e6e0ddb
Rong Tao <rongtao@cestc.cn>
2023-07-16 15:06:30 +0800

    syscall/samples: maxfd: Add resource limit test
        $ ./maxfd
        Number open files limit is 1024(524288)
        socket: Too many open files
        The max fd is 524287
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/maxfd.c
+++ syscall/samples/maxfd.c
@@ -3,13 +3,25 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/resource.h>
 
 
 int main(void)
 {
 	unsigned long i, cnt_success = 0;
 	const unsigned long nr_max = 1000000;
-	int *fds = malloc(sizeof(int) * nr_max);
+	int *fds = NULL;
+	struct rlimit rlimit;
+
+	getrlimit(RLIMIT_NOFILE, &rlimit);
+	printf("Number open files limit is %ld(%ld)\n",
+		rlimit.rlim_cur, rlimit.rlim_max);
+
+	/* Set max number of open files to rlim_max  */
+	rlimit.rlim_cur = rlimit.rlim_max;
+	setrlimit(RLIMIT_NOFILE, &rlimit);
+
+	fds = malloc(sizeof(int) * nr_max);
 	memset(fds, 0, sizeof(int) * nr_max);
 
 	for (i = 0; i < nr_max; i++) {
