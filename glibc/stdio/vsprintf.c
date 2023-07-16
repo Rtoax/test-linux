a6428d8bf8378f15e617d501fb170f0420508542
Rong Tao <rongtao@cestc.cn>
2023-07-16 13:38:16 +0800

    glibc/stdio: Add vsprintf.c demo
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/stdio/vsprintf.c
@@ -0,0 +1,31 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdarg.h>
+#include <malloc.h>
+
+
+void test_vsprintf(char *fmt, ...)
+{
+	int ret;
+	char *output = NULL;
+	va_list va;
+
+	va_start(va, fmt);
+
+	output = malloc(1024);
+	ret = vsprintf(output, fmt, va);
+	if (ret < 0) {
+		perror("vfprintf");
+	} else {
+		printf("output = %s", output);
+	}
+
+	va_end(va);
+}
+
+int main(void)
+{
+	test_vsprintf("Hello, %s\n", "Rong Tao");
+
+	return 0;
+}
