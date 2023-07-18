40ffe647afa9712bf5c94ece9990c1ada2a76989
Rong Tao <rongtao@cestc.cn>
2023-07-18 20:25:08 +0800

    syscall/samples: Add fdopen.c
        open
        fdopen
        fclose
        close <-- don't needed.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/fdopen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int main(void)
+{
+	int fd = open("a.txt", O_CREAT | O_CLOEXEC | O_RDWR, 0644);
+	assert(fd > 2);
+
+	FILE *fp = fdopen(fd, "a");
+
+	fprintf(fp, "Hello world.\n");
+
+	fclose(fp);
+	perror("fclose");
+
+	/* Double close: Bad file descriptor */
+	close(fd);
+	perror("close");
+}
--- syscall/samples/readlink2.cpp
+++ syscall/samples/readlink2.cpp
@@ -2,6 +2,7 @@
 #include <limits.h>
 #include <string>
 #include <iostream>
+#include <filesystem>
 
 
 std::string do_readlink(std::string const& path)
@@ -23,6 +24,11 @@ int main(void)
 	std::string link = do_readlink(path);
 
 	std::cout << path << ": " << link << std::endl;
+
+	std::string path2 = "/bin/ls";
+	std::filesystem::path absolutePath = std::filesystem::canonical(path2);
+
+	std::cout << path2 << ": " << absolutePath << std::endl;
 
 	return 0;
 }
