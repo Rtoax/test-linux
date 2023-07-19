b37a29b216ef0d66613a743c4c8d7d7de613869a
Rong Tao <rongtao@cestc.cn>
2023-07-19 14:52:52 +0800

    softlockup: Add loop.c module
    Link: https://blog.csdn.net/dog250/article/details/104997385
    Link: https://blog.csdn.net/21cnbao/article/details/108250786
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ sched/softlockup/loop.c
@@ -0,0 +1,33 @@
+#include <linux/module.h>
+#include <linux/kthread.h>
+
+
+struct task_struct *kt;
+
+static int loop_func(void *arg)
+{
+	int i = 0;
+	while (!kthread_should_stop()) {
+		i++;
+	}
+	return 0;
+}
+
+static int __init init_loop(void)
+{
+	kt = kthread_run(loop_func, NULL, "loop_thread");
+	if (IS_ERR(kt)) {
+		return -1;
+	}
+
+	return 0;
+}
+
+static void __exit exit_loop(void)
+{
+	kthread_stop(kt);
+}
+
+module_init(init_loop);
+module_exit(exit_loop);
+MODULE_LICENSE("GPL");
