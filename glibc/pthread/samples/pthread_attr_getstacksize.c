77d3a487de8bbeca47438bf454edd3c5c633c8f5
Rong Tao <rongtao@cestc.cn>
2023-07-21 15:43:39 +0800

    pthread: Add pthread_attr_getstacksize.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/pthread/samples/pthread_attr_getstacksize.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+
+
+void *thread(void *arg)
+{
+	size_t stacksize;
+	pthread_attr_t attr;
+
+	pthread_attr_init(&attr);
+	pthread_attr_getstacksize(&attr, &stacksize);
+	printf("stacksize: %ld MB\n", stacksize / 1024);
+
+	return ((void *)0);
+}
+
+int main(void)
+{
+	size_t stacksize;
+	pthread_t tid;
+	pthread_attr_t attr;
+
+	pthread_attr_init(&attr);
+	pthread_create(&tid, &attr, thread, NULL);
+
+	pthread_attr_getstacksize(&attr, &stacksize);
+	printf("stacksize: %ld MB\n", stacksize / 1024);
+
+	pthread_join(tid, NULL);
+
+	return 0;
+}
+
