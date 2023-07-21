df3098ea8cdab07bfb3519f926339bb663d5fdc9
Rong Tao <rongtao@cestc.cn>
2023-07-21 11:26:29 +0800

    syscall/samples: Add get_thread_area.c
        $ ./get_thread_area
        get_thread_area failed: Function not implemented
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/get_thread_area.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <string.h>
+#include <syscall.h>
+#include <limits.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/syscall.h>
+
+#include <linux/unistd.h>
+#if defined __i386__ || defined __x86_64__
+# include <asm/ldt.h>
+#endif
+
+
+int sys_get_thread_area(struct user_desc *u_info)
+{
+	int ret = syscall(__NR_get_thread_area, u_info);
+	return ret >= 0 ? ret : -errno;
+}
+
+int main(void)
+{
+	int ret;
+	struct user_desc u_info;
+
+	memset(&u_info, 0, sizeof(u_info));
+
+	u_info.entry_number = 1;
+
+	ret = sys_get_thread_area(&u_info);
+	if (ret != 0) {
+		fprintf(stderr, "get_thread_area failed: %s\n", strerror(errno));
+		exit(1);
+	}
+
+	printf("base_addr: %x\n", u_info.base_addr);
+	printf("limit: %x\n", u_info.limit);
+
+	return 0;
+}
