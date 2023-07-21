40fd1f501fe358882d6fe258398617cf94adb484
Rong Tao <rongtao@cestc.cn>
2023-07-21 15:54:41 +0800

    syscall/samples: pidfd_open (kernel >= 5.2.0)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/pidfd_open.c
+++ syscall/samples/pidfd_open.c
@@ -5,6 +5,12 @@
 #include <sys/syscall.h>
 #include <sys/types.h>
 
+#include <linux/version.h>
+
+/* pidfd_open: kernel >= 5.2.0 */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 2, 0)
+#define __NR_pidfd_open -1
+#endif
 
 int sys_pidfd_open(pid_t pid, int flags)
 {
