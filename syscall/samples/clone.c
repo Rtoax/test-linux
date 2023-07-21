f132bbb4335dd1168e61877fb9c5624fb858fc40
Rong Tao <rongtao@cestc.cn>
2023-07-21 16:56:53 +0800

    syscall/samples: Add clone.c (todo)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/clone.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sched.h>
+
+/**
+ * glibc wrapper:
+ * int clone(int (*fn)(void *), void *child_stack, int flags, void *arg, ...
+ *           pid_t *ptid, struct user_desc *tls, pid_t *ctid);
+ *
+ * syscall:
+ * long clone(unsigned long flags, void *child_stack, void *ptid, void *ctid,
+ *            struct pt_regs *regs);
+ */
+
+int main(void)
+{
+	// TODO
+
+	return 0;
+}
