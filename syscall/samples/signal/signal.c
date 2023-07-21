7c6909fe0c09d112e9160b9eb9ed1de20b7abcd5
Rong Tao <rongtao@cestc.cn>
2023-07-21 17:45:39 +0800

    syscall/samples: Add signal.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/signal/signal.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+volatile sig_atomic_t keep_going = 1;
+
+void sig_handler(int signum)
+{
+	psignal(signum, "Signal");
+
+	switch (signum) {
+	case SIGINT:
+		keep_going = 0;
+		break;
+	case SIGALRM:
+		printf("Get SIGALRM.\n");
+		break;
+	}
+}
+
+int main(void)
+{
+	sighandler_t sighandler;
+
+	signal(SIGINT, sig_handler);
+	sighandler = signal(SIGALRM, sig_handler);
+	if (sighandler == SIG_ERR) {
+		printf("signal SIGALRM failed.\n");
+	}
+
+	while (keep_going) {
+		sleep(1);
+		alarm(1);
+	}
+
+	return 0;
+}
