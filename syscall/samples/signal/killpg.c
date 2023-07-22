426b3274bcda99baed8113f299fe461ae5f050b1
Rong Tao <rtoax@foxmail.com>
2023-07-22 16:50:05 +0800

    syscall/samples/signal: Add killpg.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ syscall/samples/signal/killpg.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+
+void handler(int signum)
+{
+	printf("Get signal\n");
+	exit(1);
+}
+
+int main()
+{
+	signal(SIGINT, handler);
+
+	while (1) {
+		printf("main loop\n");
+		killpg(getpid(), SIGINT);
+	}
+
+	return 0;
+}
