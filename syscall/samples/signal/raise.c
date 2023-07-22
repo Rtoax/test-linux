b356d6e880c5921c1d78a7fe78153609be0baba4
Rong Tao <rongtao@cestc.cn>
2023-07-22 16:36:15 +0800

    syscall/samples/signal: Add raise.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/signal/raise.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+
+void handler(int signum)
+{
+	printf("RongTao\n");
+	exit(1);
+}
+
+int main(void)
+{
+	signal(SIGINT, handler);
+
+	while (1) {
+		printf("main loop\n");
+		raise(SIGINT);
+	}
+
+	return 0;
+}
