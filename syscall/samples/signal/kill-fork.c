5c5d8975119c0c02109d46222f608474da8e6e05
Rong Tao <rtoax@foxmail.com>
2023-07-22 16:59:43 +0800

    syscall/samples/signal: Add kill-fork.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ syscall/samples/signal/kill-fork.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+
+void handler(int signum)
+{
+	printf("Get signal, pid %d\n", getpid());
+	exit(1);
+}
+
+int main(void)
+{
+	int i = 0;
+	pid_t pid, ppid;
+
+	signal(SIGINT, handler);
+	ppid = getpid();
+	printf("Parent pid %d\n", ppid);
+
+	/* Fork 5 children */
+	for (i = 0; i < 5; i++) {
+		pid = fork();
+		signal(SIGINT, handler);
+
+		if (pid == 0)
+			break;
+	}
+
+	if (i < 5) {
+		printf("%dth child pid %d\n", i + 1, getpid());
+		/* Kill parent */
+		if (i == 4)
+			kill(ppid, SIGINT);
+	} else {
+		sleep(1);
+	}
+
+	return 0;
+}
