91060d7bee1bd14af557f7bb682b0eb07f1325f6
Rong Tao <rongtao@cestc.cn>
2023-07-22 16:44:04 +0800

    syscall/samples/signal: Add kill.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/signal/kill.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+
+int ret = -1;
+
+void handler(int signum)
+{
+	printf("Get signal, ret = %d\n\n", ret);
+	exit(1);
+}
+
+int main(void)
+{
+	signal(SIGINT, handler);
+
+	while (1) {
+		printf("123123\n");
+		ret = kill(getpid(), SIGINT);
+	}
+
+	return 0;
+}
