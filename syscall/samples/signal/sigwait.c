3a9c39bc354b5763e754e274e5c714e53c2eb242
Rong Tao <rongtao@cestc.cn>
2023-07-24 13:00:20 +0800

    syscall/samples/signal: Add sigwait.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/signal/sigwait.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void sig_handler(int signum)
+{
+	/* Click ctrl-c twice */
+	psignal(signum, "Get signal again");
+	exit(0);
+}
+
+int main(void)
+{
+	sighandler_t sighandler;
+	sigset_t sigset;
+	int signum;
+
+
+	sighandler = signal(SIGINT, sig_handler);
+	if (sighandler == SIG_ERR) {
+		printf("signal SIGALRM failed.\n");
+	}
+
+	sigemptyset(&sigset);
+	sigaddset(&sigset, SIGINT);
+
+	printf("Waiting signal...\n");
+	/* Click ctrl-c once */
+	sigwait(&sigset, &signum);
+	psignal(signum, "Get signal");
+
+	sleep(1);
+
+	return 0;
+}
+
