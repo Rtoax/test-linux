3a79fedd12fbd63ea4117fe49f07ffc8ea45edbb
Rong Tao <rongtao@cestc.cn>
2023-07-24 16:39:50 +0800

    sw_64/tst-getaddrinfo: -Wl,-mgprel-size=32
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- arch/sw_64/tst-getaddrinfo.sh
+++ arch/sw_64/tst-getaddrinfo.sh
@@ -8,6 +8,8 @@ gcc_args+=( -o tst-getaddrinfo )
 gcc_args+=( -O2 )
 gcc_args+=( -fpie )
 gcc_args+=( -fmerge-all-constants )
+# Try to fix compile error on sw_64
+gcc_args+=( -Wl,-mgprel-size=32 )
 
 gcc ${gcc_args[@]}
 
