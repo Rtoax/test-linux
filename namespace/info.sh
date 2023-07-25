da12e2ec08663c976966bd8f15e6ba5c5e584f38
Rong Tao <rtoax@foxmail.com>
2023-07-25 16:17:20 +0800

    namespace: Add info.sh
    $ ./info.sh
    
    ./info.sh [command]
    
    command:
    
    	ps
    	detail
    
    Link: https://unix.stackexchange.com/questions/105403/how-to-list-namespaces-in-linux
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ namespace/info.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+
+ps_ns_info()
+{
+	ps -e -o pidns,ipcns,mntns,netns,userns,utsns,cgroup,ppid,pid,cmd
+}
+
+process_detail()
+{
+	local lsns_ns_types=( mnt net ipc user pid uts cgroup time )
+	local pgrep_ns_type=( ipc mnt net pid user uts )
+
+	for lsns_type in ${lsns_ns_types[@]}
+	do
+		# sed: remove 1st line PID
+		local ns_pids=( $(lsns -t ${lsns_type} -o pid | sed '1d') )
+		for pid in ${ns_pids[@]}
+		do
+			for pgrep_type in ${pgrep_ns_type[@]}
+			do
+				echo -e "\033[1;31m## ${lsns_type}/${pid}/${pgrep_type}\033[m"
+				# --ns: Match processes that belong to the same namespaces
+				# --nslist: Match only the provided namespaces
+				pgrep --ns ${pid} --nslist ${pgrep_type} -a
+			done
+		done
+	done
+}
+
+case $1 in
+ps)
+	ps_ns_info
+	;;
+detail)
+	process_detail
+	;;
+*)
+	echo -e "
+$0 [command]
+
+command:
+
+	ps
+	detail
+	"
+	;;
+esac
+
