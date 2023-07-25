ff187d0d68a6705a61019f7afc46e70c9ef7a7cb
Rong Tao <rtoax@foxmail.com>
2023-07-25 18:40:35 +0800

    syscall/samples/ioctl: Add SIOCGIFCONF.c
        $ ./SIOCGIFCONF
        1. eno1              macaddr 1C:69:7A:6E:F4:D0
        2. virbr0            macaddr 2E:8A:4C:2E:75:3E
        3. vnet1             macaddr 52:54:00:28:F6:80
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ syscall/samples/ioctl/SIOCGIFCONF.c
@@ -0,0 +1,58 @@
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+	struct ifreq ifr;
+	struct ifconf ifc;
+	char buf[2048];
+	int count = 0;
+
+	int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+	if (sock == -1) {
+		printf("socket error\n");
+		return -1;
+	}
+
+	ifc.ifc_len = sizeof(buf);
+	ifc.ifc_buf = buf;
+	if (ioctl(sock, SIOCGIFCONF, &ifc) == -1) {
+		printf("ioctl error\n");
+		return -1;
+	}
+
+	struct ifreq *it = ifc.ifc_req;
+	const struct ifreq *const end = it + (ifc.ifc_len / sizeof(struct ifreq));
+	for (; it != end; ++it) {
+		strcpy(ifr.ifr_name, it->ifr_name);
+		if (ioctl(sock, SIOCGIFFLAGS, &ifr) != 0) {
+			printf("get mac info error\n");
+			continue;
+		}
+		if (ifr.ifr_flags & IFF_LOOPBACK)
+			continue;
+
+		if (ioctl(sock, SIOCGIFHWADDR, &ifr) == 0) {
+			unsigned char *ptr;
+			char str_macaddr[64];
+
+			count++;
+			ptr = (unsigned char *)&ifr.ifr_ifru.ifru_hwaddr.sa_data[0];
+
+			snprintf(str_macaddr, 64, "%02X:%02X:%02X:%02X:%02X:%02X",
+					*(ptr + 0),
+					*(ptr + 1),
+					*(ptr + 2),
+					*(ptr + 3),
+					*(ptr + 4),
+					*(ptr + 5));
+			printf("%d. %-16s  macaddr %-16s\n",
+				count, ifr.ifr_name, str_macaddr);
+		}
+	}
+}
