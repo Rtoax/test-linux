adeea21787571a32d8779ed99b17dcdf7b525300
Rong Tao <rtoax@foxmail.com>
2023-07-25 21:08:56 +0800

    glibc: Add wcstol.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ glibc/wctype/wcstol.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stddef.h> /* wchar_t */
+#include <wctype.h>
+#include <wchar.h>
+
+
+int main(void)
+{
+	wchar_t *wc = L"123abc";
+	wchar_t *ret;
+
+	unsigned long l = wcstol(wc, &ret, 10);
+
+	wprintf(L"%w %d\n", ret, l);
+
+	return 0;
+}
