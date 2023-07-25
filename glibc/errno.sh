355dcbc81a904ce94e08f42a81d95140b83367a2
Rong Tao <rtoax@foxmail.com>
2023-07-25 11:08:22 +0800

    glibc: Add errno.sh
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ glibc/errno.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+errnos_base=(
+	$(cat /usr/include/asm-generic/errno-base.h | tr '\t' ' ' | grep 'define E' | awk '{print $2}')
+)
+errnos=(
+	$(cat /usr/include/asm-generic/errno.h | tr '\t' ' ' | grep 'define E' | awk '{print $2}')
+)
+
+file=nr_errno.h
+
+cat >${file}<<-EOF
+#include <errno.h>
+#if defined(NR_ERRNO)
+EOF
+
+for n in ${errnos_base[@]} ${errnos[@]}
+do
+	cat >>${file}<<-EOF
+	NR_ERRNO($n)
+	EOF
+done
+cat >>${file}<<-EOF
+#else
+# error "Undefined NR_ERRNO"
+#endif
+EOF
+
