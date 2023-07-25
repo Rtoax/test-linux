b28cef121e5954fabc28c52adc928ed0802daca8
Rong Tao <rtoax@foxmail.com>
2023-07-25 20:53:24 +0800

    glibc: Add wctype/
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ glibc/wctype/iswctype.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stddef.h> /* wchar_t */
+#include <wctype.h>
+
+
+int main(void)
+{
+	wchar_t wc = L'2';
+
+	if (iswctype(wc, wctype("digit")))
+		printf("iswctype\n");
+
+	return 0;
+}
