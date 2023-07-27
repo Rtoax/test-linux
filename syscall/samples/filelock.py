3b138f4858ad6659671d54289424a52725826c86
Rong Tao <rtoax@foxmail.com>
2023-07-27 12:39:08 +0800

    syscall/samples: filelock.py: Use tid instead of pid
    16250    16250    flock-2          0        2208729890       0
    16250    16250    flock-2          0        2208729890       0
    16251    16251    flock-2          16250    2208729890       -11
    16250    16250    flock-2          0        2208729890       0
    16252    16253    flock-4-thread   0        2208729890       0
    16252    16253    flock-4-thread   16252    2208729890       0
    16252    16253    flock-4-thread   16252    2208729890       -11
    16252    16254    flock-4-thread   0        2208729890       0
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- syscall/samples/filelock.py
+++ syscall/samples/filelock.py
@@ -5,7 +5,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License")
 #
 # 2023-07-26    Rong Tao    Create this.
-# 2023-07-27    Rong Tao    Add tid.
+# 2023-07-27    Rong Tao    Use tid(tgid) instead of pid.
 
 from __future__ import print_function
 from bcc import ArgString, BPF
@@ -21,7 +21,7 @@ bpf_text = """
 struct data_t {
     u32 pid;
     u32 tid;
-    u32 owner_pid;
+    u32 owner_tid;
     char comm[TASK_COMM_LEN];
     u32 ino;
     int ret;
@@ -29,65 +29,65 @@ struct data_t {
 BPF_PERF_OUTPUT(filelock_events);
 
 struct ctx_info {
-    u32 owner_pid;
+    u32 owner_tid;
 };
-BPF_HASH(lock_pid_map, u32);
+BPF_HASH(lock_tid_map, u32);
 
 TRACEPOINT_PROBE(filelock, flock_lock_inode) {
     unsigned long i_ino = args->i_ino;
     int ret = args->ret;
 
     struct file_lock *lock = args->fl;
-    u32 lock_pid = lock->fl_pid;
-    u32 owner_pid = 0;
+    u32 lock_tid = lock->fl_pid;
+    u32 owner_tid = 0;
 
     u32 pid = bpf_get_current_pid_tgid() >> 32;
     u32 tid = bpf_get_current_pid_tgid() & 0x00000000ffffffff;
-    struct ctx_info *ctx_info = (void *)lock_pid_map.lookup(&pid);
+    struct ctx_info *ctx_info = (void *)lock_tid_map.lookup(&tid);
     if (ctx_info == 0) {
-        owner_pid = 0;
+        owner_tid = 0;
     } else {
-        owner_pid = ctx_info->owner_pid;
+        owner_tid = ctx_info->owner_tid;
     }
 
     struct data_t data = {};
 
     bpf_get_current_comm(&data.comm, sizeof(data.comm));
-    data.pid = lock_pid;
+    data.pid = lock_tid;
     data.tid = tid;
-    data.owner_pid = owner_pid;
+    data.owner_tid = owner_tid;
     data.ino = i_ino;
     data.ret = ret;
 
-    lock_pid_map.delete(&pid);
+    lock_tid_map.delete(&tid);
 
     filelock_events.perf_submit((void *)args, &data, sizeof(data));
 
     return 0;
 }
 
-static inline u32 try_get_owner_pid(u32 pid, struct list_head *head) {
-    u32 owner_pid = 0;
+static inline u32 try_get_owner_tid(u32 tid, struct list_head *head) {
+    u32 owner_tid = 0;
 
     if (!head || head == head->next) {
-        owner_pid = 0;
+        owner_tid = 0;
     } else {
         struct file_lock *fl = (void *)head->next - offsetof(struct file_lock, fl_list);
-        if (pid != fl->fl_pid) {
-            owner_pid = fl->fl_pid;
+        if (tid != fl->fl_pid) {
+            owner_tid = fl->fl_pid;
         }
     }
 
-    return owner_pid;
+    return owner_tid;
 }
 
 TRACEPOINT_PROBE(filelock, locks_get_lock_context) {
     struct file_lock_context *ctx = args->ctx;
 
-    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    u32 tid = bpf_get_current_pid_tgid() & 0x00000000ffffffff;
     struct ctx_info info = {};
 
-    info.owner_pid = 0;
+    info.owner_tid = 0;
 
     /**
      * flock_make_lock: fl->fl_pid = current->tgid;
@@ -95,9 +95,9 @@ TRACEPOINT_PROBE(filelock, locks_get_lock_context) {
      */
     struct list_head *head = &ctx->flc_flock;
 
-    info.owner_pid = try_get_owner_pid(pid, head);
+    info.owner_tid = try_get_owner_tid(tid, head);
 
-    lock_pid_map.update(&pid, (void *)&info);
+    lock_tid_map.update(&tid, (void *)&info);
 
     return 0;
 }
@@ -109,7 +109,7 @@ def print_filelock_event(cpu, data, size):
            % (event.pid,
               event.tid,
               event.comm,
-              event.owner_pid,
+              event.owner_tid,
               event.ino,
               event.ret))
 
@@ -117,7 +117,7 @@ b = BPF(text=bpf_text)
 
 print("Tracing filelock ... Hit Ctrl-C to end")
 print("%-8s %-8s %-16s %-8s %-16s %-8s" \
-      % ("PID", "TID", "COMM", "OWN_PID", "INODE", "RESULT"))
+      % ("PID", "TID", "COMM", "OWN_TID", "INODE", "RESULT"))
 
 b["filelock_events"].open_perf_buffer(print_filelock_event)
 
