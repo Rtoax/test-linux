a68f67afc8b1d4ed03471b109c5df75780d2cd64
Rong Tao <rtoax@foxmail.com>
2023-08-05 13:05:58 +0800

    glibc/stdlib: Add mkdtemp.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ glibc/stdlib/mkdtemp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(void)
+{
+	char path[] = "tmpd-XXXXXX";
+	char *tmpd = mkdtemp(path);
+	printf("tmpd = %s\n", tmpd ?: "NULL");
+
+	rmdir(tmpd);
+	return 0;
+}
