5e3218b57ce4427fed6f0799dcf7b4166a56d363
Rong Tao <rtoax@foxmail.com>
2023-08-10 21:14:15 +0800

    pthread/samples: Add pthread_rwlock_init.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ glibc/pthread/samples/pthread_rwlock_init.c
@@ -0,0 +1,34 @@
+
+#include <pthread.h>
+#include <stdio.h>
+
+
+pthread_rwlock_t rwlock1 = PTHREAD_RWLOCK_INITIALIZER;
+pthread_rwlock_t rwlock2;
+
+void* test_task_fn(void* unused)
+{
+	printf("test_task_fn.\n");
+    static int status = 12121;
+
+    pthread_exit(&status);
+	return NULL;
+}
+
+int main (void)
+{
+    int *pstatus;
+	pthread_t thread_id;
+
+    pthread_rwlock_init(&rwlock2, NULL);
+
+	pthread_create(&thread_id, NULL, test_task_fn, NULL);
+
+	pthread_join(thread_id, (void**)&pstatus);
+
+    printf("pstatus = %d\n", *pstatus);
+
+    pthread_rwlock_destroy(&rwlock2);
+
+	return 0;
+}
