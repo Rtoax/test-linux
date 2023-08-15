baad8707546ea4c13e9d4f4c97beee06f872ac47
Rong Tao <rongtao@cestc.cn>
2023-08-15 10:42:08 +0800

    softirqs.sh: Add print_softirqs()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- interrupt/softirq/softirqs.sh
+++ interrupt/softirq/softirqs.sh
@@ -68,27 +68,32 @@ update_softirqs()
 	# echo ${softirqs[@]}
 }
 
-update_softirqs
-
-echo
-echo "Printing /proc/softirqs, print with alignment"
-echo "Show softirqs: $softirq_type, number ${#softirqs[@]}"
-echo
-echo -e -n "\033[1m"
-for ((i = 0; i < $interval - 1; i++))
-do
-	printf "%-4s %-16s\t" CPU NUM
-done
-printf "%-4s %-16s\n" CPU NUM
-echo -e -n "\033[m"
-
-
-for ((i = 0; i < ${#softirqs[@]}; i+=$interval))
-do
-	for ((j = 0; j < $interval - 1; j++))
+print_softirqs()
+{
+	echo
+	echo "Printing /proc/softirqs, print with alignment"
+	echo "Show softirqs: $softirq_type, number ${#softirqs[@]}"
+	echo
+	echo -e -n "\033[1m"
+	for ((i = 0; i < $interval - 1; i++))
 	do
-		# +1: skip softirqs[0]
-		printf "%-4d %-16d\t" $( expr $i + $j ) ${softirqs[$i + $j + 1]}
+		printf "%-4s %-16s\t" CPU NUM
 	done
-	printf "%-4d %-16d\n" $( expr $i + $j ) ${softirqs[$i + $j + 1]}
-done
+	printf "%-4s %-16s\n" CPU NUM
+	echo -e -n "\033[m"
+
+
+	for ((i = 0; i < ${#softirqs[@]}; i+=$interval))
+	do
+		for ((j = 0; j < $interval - 1; j++))
+		do
+			# +1: skip softirqs[0]
+			printf "%-4d %-16d\t" $( expr $i + $j ) ${softirqs[$i + $j + 1]}
+		done
+		printf "%-4d %-16d\n" $( expr $i + $j ) ${softirqs[$i + $j + 1]}
+	done
+}
+
+
+update_softirqs
+print_softirqs
