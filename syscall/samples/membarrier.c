b7e9334548a25fccba03e9eb6a0b274be8b7705b
Rong Tao <rongtao@cestc.cn>
2023-08-15 19:19:56 +0800

    syscall/samples: Add membarrier.c (todo)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/membarrier.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/syscall.h>
+#include <linux/membarrier.h>
+
+
+int main(void)
+{
+	syscall(SYS_membarrier, MEMBARRIER_CMD_GLOBAL, MEMBARRIER_CMD_FLAG_CPU, 0);
+
+	return 0;
+}
+
+
