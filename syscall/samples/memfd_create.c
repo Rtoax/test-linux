555f9cbffd8b02b216c04892ce1832214a07d919
Rong Tao <rtoax@foxmail.com>
2023-08-15 08:43:28 +0800

    syscall/samples: Add memfd_create.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ syscall/samples/memfd_create.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <linux/memfd.h>
+#include <sys/syscall.h>
+
+
+int anonyexec(const char *path, char *argv[])
+{
+	int fd, fdm, filesize;
+	void *elfbuf;
+	char  cmdline[256];
+
+	/* Open elf file */
+	fd = open(path, O_RDONLY);
+	filesize = lseek(fd, SEEK_SET, SEEK_END);
+	lseek(fd, SEEK_SET, SEEK_SET);
+	elfbuf = malloc(filesize);
+	read(fd, elfbuf, filesize);
+	close(fd);
+
+	/* Create memfd and copy ELF into it. */
+	fdm = syscall(__NR_memfd_create, "elf", MFD_CLOEXEC);
+	ftruncate(fdm, filesize);
+	write(fdm, elfbuf, filesize);
+	free(elfbuf);
+
+	/* Exec ELF in memory */
+	sprintf(cmdline, "/proc/self/fd/%d", fdm);
+	argv[0] = cmdline;
+	execve(argv[0], argv, NULL);
+	free(elfbuf);
+	return -1;
+}
+
+int main(void)
+{
+	int ret;
+	char *argv[] = {"/bin/uname", "-a", NULL};
+
+	ret = anonyexec("/bin/uname", argv);
+
+	return ret;
+}
