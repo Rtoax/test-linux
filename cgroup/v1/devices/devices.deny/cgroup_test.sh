2b43c36cc65b260a8edecc50bd464ca26e4d71c5
Rong Tao <rongtao@cest.cn>
2023-08-16 19:09:45 +0800

    devices.deny: Check support cgroup-v1 by /sys/fs/cgroup/devices
    Signed-off-by: Rong Tao <rongtao@cest.cn>

--- cgroup/v1/devices/devices.deny/cgroup_test.sh
+++ cgroup/v1/devices/devices.deny/cgroup_test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # link: https://0xax.gitbooks.io/linux-insides/content/Cgroups/linux-cgroups-1.html
 
+if [[ ! -d /sys/fs/cgroup/devices ]]; then
+	echo "ERROR: not support cgroup-v1"
+	exit 1
+fi
+
 if [[ $(id -u | awk '{print $1}') != 0 ]]; then
 	echo "ERROR: run with root(sudo)"
+	echo "MAYBE: sudo grubby --update-kernel=ALL --args=systemd.unified_cgroup_hierarchy=0"
 	exit 1
 fi
 
