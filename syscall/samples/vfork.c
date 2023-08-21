a135d103b6aa332170ff6ce5f84cec0b6f2c93b9
Rong Tao <rtoax@foxmail.com>
2023-08-21 17:01:31 +0800

    syscall/samples: Move sched/fork/test to syscall/samples
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- syscall/samples/vfork.c
+++ syscall/samples/vfork.c
@@ -1,18 +1,14 @@
-/**
- * File: fork.c
- * Author: Rong Tao
- * Date: 2022.05.10
- */
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+
 int main(int argc, char *argv[])
 {
 	int pid;
 
-	if ((pid = fork()) == 0) {
+	if ((pid = vfork()) == 0) {
 		printf("Child %d.\n", getpid());
 	} else if (pid > 0) {
 		printf("Parent %d.\n", getpid());
