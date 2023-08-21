b7371dbebddc36794f9d1577eafac783e649a47c
Rong Tao <rtoax@foxmail.com>
2023-08-21 16:56:09 +0800

    task/hung: Add ext4 hung tasks example
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- sched/hung.sh
+++ sched/hung.sh
@@ -2,9 +2,52 @@
 
 set -e
 
-tmpfs=/path/to/mountpoint
+fs=rootfs.ext4
+mnt=rootfs.mnt
 
-sudo fsfreeze --freeze ${tmpfs}
-cat /etc/os-release > ${tmpfs}/os-release
-sudo fsfreeze --unfreeze ${tmpfs}
 
+freeze_mnt() {
+	sudo fsfreeze --freeze ${mnt}
+}
+
+unfreeze_mnt() {
+	sudo fsfreeze --unfreeze ${mnt}
+}
+
+create_mnt() {
+	dd if=/dev/zero of=${fs} bs=1M count=1
+	mkfs.ext4 ${fs}
+	mkdir ${mnt}
+	sudo mount ${fs} ${mnt}
+}
+
+destroy_mnt() {
+	sudo umount ${mnt}
+	sudo rm -rf ${mnt} ${fs}
+}
+
+# $1 - number of hung tasks
+create_hung_task() {
+	local nr=$1
+	[[ -z $nr ]] && nr=1
+	for ((i = 0; i < $nr; i++))
+	do
+		time cat /etc/os-release > ${mnt}/os-release &
+	done
+}
+
+clean () {
+	unfreeze_mnt || true
+	destroy_mnt || true
+}
+
+clean
+
+create_mnt
+freeze_mnt
+
+create_hung_task 10
+echo "cat /etc/os-release hunging for 5 seconds."
+sleep 5
+
+clean
