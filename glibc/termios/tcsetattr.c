b8743366abeb5c1736bab522431204b3661f6fcc
Rong Tao <rongtao@cestc.cn>
2023-08-23 14:44:05 +0800

    glibc/termios: Add tcsetattr.c tcgetattr.c
    Link: https://stackoverflow.com/questions/32809983/how-do-i-use-the-ioctl-system-call-to-disable-keypress-echo-and-buffering
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/termios/tcsetattr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+	struct termios old, new;
+
+
+	tcgetattr(STDIN_FILENO, &old);
+	new = old;
+	/* disable line buffering and feedback */
+	new.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &new);
+
+	printf("Reading 5 characters without local echo...\n");
+
+	for (i = 0; i < 5; i++) {
+		char x = getchar();
+		printf("[%c] ", x);
+	}
+	printf("\nRestoring terminal config\n");
+	tcsetattr(STDIN_FILENO, TCSANOW, &old);
+
+	return 0;
+}
