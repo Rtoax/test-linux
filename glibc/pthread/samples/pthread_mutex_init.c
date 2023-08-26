9924166b0c96ff1951f66935fcb1d3ddf6e7dbec
Rong Tao <rongtao@cestc.cn>
2023-08-26 21:18:06 +0800

    pthread/samples: Add pthread_mutex APIs
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/pthread/samples/pthread_mutex_init.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <pthread.h>
+
+
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex2;
+
+void* test_task_fn(void* unused)
+{
+	printf("test_task_fn.\n");
+
+	static int status = 12121;
+
+	pthread_exit(&status);
+	return NULL;
+}
+
+int main (void)
+{
+	int *pstatus;
+	pthread_t thread_id;
+
+	pthread_mutex_init(&mutex2, NULL);
+
+	pthread_create(&thread_id, NULL, test_task_fn, NULL);
+
+	pthread_join(thread_id, (void**)&pstatus);
+
+	printf("pstatus = %d\n", *pstatus);
+
+	pthread_mutex_destroy(&mutex2);
+
+	return 0;
+}
---
+++ glibc/pthread/samples/pthread_mutexattr_setrobust.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <pthread.h>
+
+
+pthread_mutex_t mutex;
+pthread_mutexattr_t mutexattr;
+
+void* test_task1_fn(void* unused)
+{
+	printf("test_task1_fn.\n");
+
+	pthread_mutex_lock(&mutex);
+
+	printf("task1: locked\n");
+	printf("task1: exit\n");
+
+	/* task2 still can lock mutex */
+	pthread_exit(NULL);
+
+	/* never called here */
+	pthread_mutex_unlock(&mutex);
+	pthread_exit(NULL);
+	return NULL;
+}
+
+void* test_task2_fn(void* unused)
+{
+	printf("test_task2_fn.\n");
+
+	pthread_mutex_lock(&mutex);
+
+	printf("task2: locked\n");
+
+	pthread_mutex_unlock(&mutex);
+
+	pthread_exit(NULL);
+	return NULL;
+}
+
+int main(void)
+{
+	int *pstatus;
+	pthread_t thread_id1, thread_id2;
+
+	pthread_mutexattr_init(&mutexattr);
+	pthread_mutexattr_setrobust(&mutexattr, PTHREAD_MUTEX_ROBUST);
+
+	pthread_mutex_init(&mutex, &mutexattr);
+
+	pthread_create(&thread_id1, NULL, test_task1_fn, NULL);
+	pthread_create(&thread_id2, NULL, test_task2_fn, NULL);
+
+	pthread_join(thread_id1, (void**)&pstatus);
+	pthread_join(thread_id1, (void**)&pstatus);
+
+	pthread_mutex_destroy(&mutex);
+
+	return 0;
+}
