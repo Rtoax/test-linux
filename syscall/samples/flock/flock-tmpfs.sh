8de25825222388f6a8543cf918c42c8f628bb1ac
Rong Tao <rongtao@cestc.cn>
2023-08-28 11:58:04 +0800

    syscall/samples/flock: flock.c support to getopt()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/flock/flock-tmpfs.sh
+++ syscall/samples/flock/flock-tmpfs.sh
@@ -16,11 +16,11 @@ sudo mount tmpfs -t tmpfs -o size=1G -o mode=0777 $PWD/tmpfs
 make flock
 
 # Holding the flock for seconds
-./flock $PWD/tmpfs/testfile 3 &
+./flock -f $PWD/tmpfs/testfile -s 3 &
 
 # Make sure flock above startup already
 sleep 0.5
 
 # Resource temporarily unavailable until above flock exit
-while ! ./flock $PWD/tmpfs/testfile; do :; done
+while ! ./flock -f $PWD/tmpfs/testfile; do :; done
 
--- syscall/samples/flock/flock.c
+++ syscall/samples/flock/flock.c
@@ -7,26 +7,76 @@
 #include <sys/types.h>
 #include <sys/file.h>
 #include <sys/wait.h>
+#include <getopt.h>
 
+
+enum {
+	ARG_LOCK_MAND = 101,
+};
+
+static const char *filename = "testfile";
+static int secs = 1;
+
+#define OPERATION_ALL	\
+	(LOCK_EX | LOCK_NB | LOCK_MAND | LOCK_READ | LOCK_WRITE)
+
+void usage(int err)
+{
+	printf(
+	"-h, --help      show this information\n" \
+	"-f, --file      specify file to lock, default: %s\n" \
+	"-s, --secs      hold lock for seconds, default: %d\n" \
+	"--lock-mand     operation LOCK_MAND\n" \
+	,
+	filename,
+	secs);
+
+	exit(err);
+}
 
 int main(int argc, char *argv[])
 {
 	int fd;
 	int ret;
-	const char *filename = "testfile";
-	int secs = 1;
+	int cmd, option_index;
+	int operation = 0;
 
-	if (argc > 1)
-		filename = argv[1];
+	static struct option options[] = {
+		{"help",        no_argument,       0, 'h'},
+		{"file",        required_argument, 0, 'f'},
+		{"secs",        required_argument, 0, 's'},
+		{"lock-mand",   no_argument,       0, ARG_LOCK_MAND},
+		{0, 0, 0, 0}
+	};
 
-	if (argc > 2)
-		secs = atoi(argv[2]);
+	while (1) {
+		cmd = getopt_long(argc, argv, "hf:s:", options, &option_index);
+		if (cmd == -1)
+			break;
+		switch (cmd) {
+		case 'h':
+			usage(0);
+			break;
+		case 'f':
+			filename = optarg;
+			break;
+		case 's':
+			secs = atoi(optarg);
+			break;
+		case ARG_LOCK_MAND:
+			operation |= LOCK_MAND;
+			break;
+		default:
+			abort();
+			break;
+		}
+	}
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
 
 	/* LOCK_RW = LOCK_READ | LOCK_WRITE */
-	//ret = flock(fd, LOCK_EX | LOCK_NB | LOCK_MAND | LOCK_READ | LOCK_WRITE);
-	ret = flock(fd, LOCK_EX | LOCK_NB);
+	operation |= LOCK_EX | LOCK_NB;
+	ret = flock(fd, operation);
 	if (ret != 0) {
 		fprintf(stderr, "flock: %s\n", strerror(errno));
 		exit(1);
@@ -39,6 +89,7 @@ int main(int argc, char *argv[])
 	write(fd, "hello\n", sizeof("hello\n"));
 
 	flock(fd, LOCK_UN);
+	printf("Unlock %s\n", filename);
 
 	close(fd);
 	return 0;
