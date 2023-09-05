3a9b6be17a2e85df5b5fc2aa08ece5f29c7a7143
Rong Tao <rongtao@cestc.cn>
2023-09-05 11:42:14 +0800

    init/u-boot/makeimg.sh: Add args and make ext4 partition-2
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- init/bootloader/u-boot/makeimg.sh
+++ init/bootloader/u-boot/makeimg.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-set -ex
+set -e
 
+pre_clean() {
+	sudo rm -f test.qcow2
+}
 create_disk() {
+	pre_clean
 	sudo modprobe nbd max_part=16
 	lsmod | grep nbd
-	sudo rm -f test.qcow2
 	sudo qemu-img create -f qcow2 test.qcow2 128G
 	sudo qemu-nbd --connect /dev/nbd0 test.qcow2 -f qcow2
 	sudo chown rongtao:rongtao test.qcow2
@@ -19,10 +22,15 @@ partition() {
 	1
 	2048
 	100M
+	n
+	p
+	2
+
+
 	w
 	EOF
 	sudo fdisk /dev/nbd0 < tmp.txt
-	rm tmp.txt
+	sudo fdisk -l /dev/nbd0
 }
 
 makefs() {
@@ -31,20 +39,74 @@ makefs() {
 	sudo mount /dev/nbd0p1 fat.out
 	pushd fat.out
 	# FIXME: Add more file
-	sudo sh -c 'echo "RT" > README'
+	sudo sh -c 'echo "RT is fat" > README'
 	popd
-	sudo umount fat.out
-	rmdir fat.out
+	sudo mkfs.ext4 /dev/nbd0p2
+	mkdir ext4.out
+	sudo mount /dev/nbd0p2 ext4.out
+	pushd ext4.out
+	# FIXME: Add more file
+	sudo sh -c 'echo "RT is ext4" > README'
+	popd
 }
 
 destroy() {
 	sudo umount fat.out || true
 	sudo rm -rf fat.out || true
+	sudo umount ext4.out || true
+	sudo rm -rf ext4.out || true
 	sudo qemu-nbd --disconnect /dev/nbd0 || true
 	sudo rmmod nbd || true
+	sudo rm tmp.txt || true
 }
-trap destroy EXIT
+
+usage()
+{
+	cat <<-EOF
+
+	--nofs
+	--nodestroy
+	-v, --verbose
+	-h, --help
+
+	EOF
+}
+
+fs_flag=YES
+destroy_flag=YES
+
+while true
+do
+case $1 in
+-v | --verbose)
+	shift
+	export PS4='+${BASH_SOURCE}:${LINENO}:${FUNCNAME[0]}: '
+	set -x
+	;;
+-h | --help)
+	shift
+	usage
+	exit 0
+	;;
+--nofs)
+	shift
+	fs_flag=
+	;;
+--nodestroy)
+	shift
+	destroy_flag=
+	;;
+*)
+	break
+	;;
+esac
+done
+
+[[ ! -z ${destroy_flag} ]] && trap destroy EXIT
 
 create_disk
-partition
-makefs
+
+if [[ ! -z ${fs_flag} ]]; then
+	partition
+	makefs
+fi
