1daab141457ed2cf204be533a8d62fb58e7ae8e8
Rong Tao <rongtao@cestc.cn>
2023-09-08 22:07:48 +0800

    mm/oom: Add oom_notifier.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ mm/oom/modules/oom_notifier.c
@@ -0,0 +1,37 @@
+/**
+ * oom_notifier.c - register OOM notifier
+ *
+ * 2023-09-08	Rong Tao	Create this
+ */
+#include <linux/module.h>
+#include <linux/oom.h>
+
+static int notifier_fn(struct notifier_block *nb,
+			unsigned long action, void *data)
+{
+	/* TODO */
+	return 0;
+}
+
+struct notifier_block nb = {
+	.notifier_call = notifier_fn,
+	.priority = 0,
+};
+
+static int __init kernel_init(void)
+{
+	int ret = register_oom_notifier(&nb);
+	return ret;
+}
+
+static void __exit kernel_exit(void)
+{
+	unregister_oom_notifier(&nb);
+}
+
+module_init(kernel_init);
+module_exit(kernel_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Rong Tao");
+MODULE_DESCRIPTION("Register OOM notifier chain");
