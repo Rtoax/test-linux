92c5f59482a561c986cbb70194016cfe783d12a2
Rong Tao <rtoax@foxmail.com>
2023-09-21 20:55:49 +0800

    pthread/samples: pthread_cleanup_push.c register dtor
    Link: https://blog.csdn.net/caianye/article/details/5912172
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- glibc/pthread/samples/pthread_cleanup_push.c
+++ glibc/pthread/samples/pthread_cleanup_push.c
@@ -1,3 +1,8 @@
+/**
+ * 从pthread_cleanup_push()的调用点到pthread_cleanup_pop()之间的程序段中的终止
+ * 动作（包括调用 pthread_exit()和取消点终止）都将执行pthread_cleanup_push()所
+ * 指定的清理函数。
+ */
 #include <stdio.h>
 #include <pthread.h>
 #include <malloc.h>
@@ -7,7 +12,7 @@
 void* allocate_buffer(size_t size)
 {
 	printf("allocate_buffer.\n");
-	return malloc (size);
+	return malloc(size);
 }
 /* Deallocate a temporary buffer. */
 void deallocate_buffer(void* buffer)
@@ -15,7 +20,7 @@ void deallocate_buffer(void* buffer)
 	printf("deallocate_buffer.\n");
 	free (buffer);
 }
-void do_some_work()
+void do_some_work(void)
 {
 	/* Allocate a temporary buffer. */
 	void* temp_buffer = allocate_buffer(1024);
@@ -24,7 +29,14 @@ void do_some_work()
 	 * case the thread exits or is cancelled. */
 	pthread_cleanup_push(deallocate_buffer, temp_buffer);
 
+	/* Exit */
+	int status = 12121;
+	pthread_exit(&status);
+
 	pthread_cleanup_pop(1);
+
+	/* Never called because thread already exit. */
+	deallocate_buffer(temp_buffer);
 }
 
 int main(void)
