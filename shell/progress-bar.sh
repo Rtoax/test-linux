ad08f79ff2a79e56e0b39baac0b07e422c91a164
Rong Tao <rongtao@cestc.cn>
2023-09-22 20:09:57 +0800

    shell/progress-bar.sh: Modify
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- shell/progress-bar.sh
+++ shell/progress-bar.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
+max_length=50
+
 progress_bar()
 {
-	i=$1
-	len=`expr ${#i} + 5 `
+	local i=$1
+	local len=`expr ${#i} + 5 `
 
 	echo -n "$i % ["
 	for ((j = 0; j < $i; j++))
 	do
 		printf "#"
 	done
-	for ((j = $i; j < 100; j++))
+	for ((j = $i; j < ${max_length}; j++))
 	do
 		printf " "
 	done
 	printf "]"
 	sleep 0.1
 
-	for ((j = 0; j < `expr 100 + $len`; j++))
+	for ((j = 0; j < `expr ${max_length} + $len`; j++))
 	do
 		printf "\b"
 	done
 }
 
-for ((i = 0; i < 100; i++))
+for ((i = 0; i < ${max_length}; i++))
 do
 	progress_bar $i
 done
