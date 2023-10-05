207eedfc7fb58856e1a1f5e4b1aeb7297ec7b708
Rong Tao <rtoax@foxmail.com>
2023-10-05 21:49:20 +0800

    pthread/out-of-order: Add barrier.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ glibc/pthread/out-of-order/barrier.c
@@ -0,0 +1,59 @@
+/**
+ * 证明CPU乱序执行，并使用内存屏障消除乱序执行的影响
+ *
+ * 2021-05-26	Rong Tao	Create this.
+ */
+#include <stdio.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+#define __unused __attribute__((unused))
+
+#if defined(HAVE_MBARRIER)
+static void __unused inline mrwbarrier() { asm volatile("mfence":::"memory"); }
+static void __unused inline mrbarrier()  { asm volatile("lfence":::"memory"); }
+static void __unused inline mwbarrier()  { asm volatile("sfence":::"memory"); }
+#else
+#define mrwbarrier()
+#define mrbarrier()
+#define mwbarrier()
+#endif
+
+volatile int x, y, a, b;
+
+void *task1(void *arg)
+{
+	a = 1;
+	mrwbarrier();
+	x = b;
+	pthread_exit(NULL);
+}
+
+void *task2(void *arg)
+{
+	b = 1;
+	mrwbarrier();
+	y = a;
+	pthread_exit(NULL);
+}
+
+int main(void)
+{
+	pthread_t tasks[2];
+
+	while (true) {
+		x = y = a = b = 0;
+
+		pthread_create(&tasks[0], NULL, task1, NULL);
+		pthread_create(&tasks[1], NULL, task2, NULL);
+
+		pthread_join(tasks[0], NULL);
+		pthread_join(tasks[1], NULL);
+
+		if (x == 0 && y == 0) {
+			printf("CPU Out of Order Exec Happend.\n");
+		}
+	}
+
+	return 0;
+}
