af2aee04e25e2d5e81c781a492f45b09aca283cc
Rong Tao <rtoax@foxmail.com>
2023-10-05 21:04:26 +0800

    insn: Add rdmsr.c
    	$ sudo ./rdmsr
    	MSR(rte = 8) - tsc_hz = 578721592771682304
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ instructions/examples/rdmsr.c
@@ -0,0 +1,36 @@
+/**
+ * Module Specific Register - MSR
+ */
+#include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/types.h>
+
+#define RTE_EXEC_ENV_LINUX
+
+static int32_t
+rdmsr(int msr, uint64_t *val)
+{
+#ifdef RTE_EXEC_ENV_LINUX
+	int fd, ret;
+
+	fd = open("/dev/cpu/0/msr", O_RDONLY);
+	if (fd < 0)
+		return fd;
+	ret = pread(fd, val, sizeof(uint64_t), msr);
+	close(fd);
+	return ret;
+#else
+	RTE_SET_USED(msr);
+	RTE_SET_USED(val);
+	return -1;
+#endif
+}
+
+int main(void)
+{
+	uint64_t tsc_hz = 0;
+	int32_t ret = rdmsr(0xCE, &tsc_hz);
+	printf("MSR(rte = %d) - tsc_hz = %ld\n", ret, tsc_hz);
+}
