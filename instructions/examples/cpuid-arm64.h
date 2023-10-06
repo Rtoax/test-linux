5d5257aea1aba94576a3bc3986718808e6bd65aa
Rong Tao <rongtao@cestc.cn>
2023-10-06 11:40:16 +0800

    insn/cpuid: Add get_cpuid() for arm64 with msr
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/cpuid-arm64.h
+++ instructions/examples/cpuid-arm64.h
@@ -1,1 +1,11 @@
+#pragma once
+
+int get_cpuid(unsigned long *flags)
+{
+	unsigned long cpuid;
+	asm("mrs %0, MIDR_EL1" : "=r" (cpuid));
+	*flags = cpuid;
+	return 0;
+}
+
 #include "cpuid-generic.h"
