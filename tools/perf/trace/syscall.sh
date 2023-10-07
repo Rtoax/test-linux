ef228d88c2a067ada5fe38ca0da25492b7153b72
Rong Tao <rongtao@cestc.cn>
2023-10-07 16:33:17 +0800

    perf/trace: Tracing --failure syscall
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tools/perf/trace/syscall.sh
+++ tools/perf/trace/syscall.sh
@@ -2,6 +2,9 @@
 
 # Tracing syscalls
 sudo perf trace
+sudo perf trace --pid=$pid
+#  Show only syscalls that failed, i.e. that returned < 0
+sudo perf trace --failure
 
 # Tracing open/openat for 4 events
 sudo perf trace -e open* --max-events 4
