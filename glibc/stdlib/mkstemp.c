090c39dac55af768e2b97adf828f4a6c67488f87
Rong Tao <rongtao@cestc.cn>
2023-10-10 12:00:37 +0800

    glibc/stdlib: Add mkstemp.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/stdlib/mkstemp.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+int write_temp_file(char* buffer, size_t length)
+{
+	char temp_filename[] = "tmp-XXXXXX";
+	int fd = mkstemp(temp_filename);
+	unlink(temp_filename);
+	write(fd, &length, sizeof(length));
+	write(fd, buffer, length);
+	return fd;
+}
+
+char* read_temp_file(int temp_file, size_t* length)
+{
+	char* buffer;
+	int fd = temp_file;
+	lseek(fd, 0, SEEK_SET);
+	read(fd, length, sizeof(*length));
+	buffer =(char*)malloc(*length);
+	read(fd, buffer, *length);
+	close(fd);
+	return buffer;
+}
+
+int main(void)
+{
+	size_t length;
+	char *buf;
+
+	int fd = write_temp_file("HELLO", 5);
+	buf = read_temp_file(fd, &length);
+
+	printf("%s\n", buf);
+	free(buf);
+	return 0;
+}
