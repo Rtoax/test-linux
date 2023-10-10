b50e03d2d3469fb5d62ed92759f88cd555dc4bf1
Rong Tao <rongtao@cestc.cn>
2023-10-10 18:39:25 +0800

    sched/stopped.sh: Add more method to get stopped process
    Link: https://unix.stackexchange.com/questions/321440/how-can-i-check-to-see-if-a-process-is-stopped-from-the-command-line
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- sched/stopped.sh
+++ sched/stopped.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+_check_process_stopped() {
+	local pid=$(pidof stopped)
+	local status=$(cat /proc/$pid/wchan)
+
+	if [ "$status" == "do_signal_stop" ] ; then
+		echo "$pid do stopped: $status"
+	else
+		echo "$pid does not stopped: $status"
+	fi
+
+	if [[ "$(ps -o state= -p $pid)" = T ]]; then
+		echo "$pid do stopped"
+	else
+		echo "$pid does not stopped"
+	fi
+}
+
 _check() {
 	ps -e -w -o stat,pid,lstart,comm | grep stopped
 }
@@ -19,6 +36,7 @@ _continue() {
 case $1 in
 check)
 	_check
+	_check_process_stopped
 	;;
 stop)
 	_stop
