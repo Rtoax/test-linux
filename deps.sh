61fca7de3a458614f1f3cbeb01311784c420d86a
Rong Tao <rtoax@foxmail.com>
2023-10-11 22:00:53 +0800

    deps.sh: Add gdb
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- deps.sh
+++ deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkgs=( make cmake gcc clang )
+pkgs=( make cmake gcc clang gdb )
 pkgs+=( systemtap-sdt-devel )  # sdt.h
 pkgs+=( libdwarf-tools )       # dwarfdump
 pkgs+=( bpftrace bcc )         # eBPF
