c31e1dff03a1425ca6f08c151a41f7baccfe0b94
Rong Tao <rongtao@cestc.cn>
2023-10-11 19:04:55 +0800

    pthread/samples: pthread_create.c: Add signal() and usleep()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/pthread/samples/pthread_create.c
+++ glibc/pthread/samples/pthread_create.c
@@ -1,11 +1,23 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <unistd.h>
 
+
+bool loop = true;
+void sig_handler(int signum)
+{
+	printf("Catch signal.\n");
+	loop = false;
+}
 
 void* print_xs(void* unused)
 {
-	while (1)
-		fputc ('x', stderr);
+	while (loop) {
+		fputc('x', stderr);
+		usleep(1000);
+	}
 
 	return NULL;
 }
@@ -13,10 +25,16 @@ void* print_xs(void* unused)
 int main(void)
 {
 	pthread_t thread_id;
+
+	signal(SIGINT, sig_handler);
 	pthread_create(&thread_id, NULL, &print_xs, NULL);
 
-	while (1)
-		fputc ('o', stderr);
+	while (loop) {
+		fputc('o', stderr);
+		usleep(1000);
+	}
+
+	pthread_join(thread_id, NULL);
 
 	return 0;
 }
