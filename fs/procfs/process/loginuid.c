1ad38b63e680bbdd42bf2f86dd18a595272cd95f
Rong Tao <rongtao@cestc.cn>
2023-10-13 19:39:04 +0800

    fs/procfs: Add process/loginuid.c (/proc/PID/loginuid)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ fs/procfs/process/loginuid.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+uid_t
+get_process_owner(pid_t pid)
+{
+	char procpath[128];
+	snprintf(procpath, sizeof(procpath), "/proc/%d/loginuid", pid);
+
+	struct stat info;
+	if (stat(procpath, &info)) {
+		return 0;
+	}
+
+	return info.st_uid;
+}
+
+int main(void)
+{
+	pid_t pid = getpid();
+	uid_t uid = get_process_owner(pid);
+	printf("Pid %d belongs to uid %d\n", pid, uid);
+	return 0;
+}
