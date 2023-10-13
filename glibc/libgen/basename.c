d63ae812086e4cbb8b8ab61a16ba129b81232fbc
Rong Tao <rongtao@cestc.cn>
2023-10-13 19:51:28 +0800

    glibc/libgen/basename: Add get_basename()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/libgen/basename.c
+++ glibc/libgen/basename.c
@@ -3,6 +3,15 @@
 #include <string.h>
 #include <libgen.h>
 
+
+const char *get_basename(const char *name)
+{
+	const char *base = name ? strrchr(name, '/') : name;
+	if (base)
+		return base + 1;
+	return name;
+}
+
 int main(int argc, char *argv[])
 {
 	char path[] = "/a/b/c/d/e/f";
@@ -10,6 +19,7 @@ int main(int argc, char *argv[])
 	printf("%s\n", basename(path));
 	printf("%s\n", basename(path));
 	printf("%s\n", basename(path));
+	printf("%s\n", get_basename(path));
 
 	return 0;
 }
