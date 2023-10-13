d96d6b2b77adf366b03dd8300e7870a208417827
Rong Tao <rongtao@cestc.cn>
2023-10-13 20:10:40 +0800

    bpf/bcc/samples/tracepoints/example.py: Run successfully
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/bcc/samples/tracepoints/example.py
+++ bpf/bcc/samples/tracepoints/example.py
@@ -1,7 +1,8 @@
+#!/bin/env python3
 from bcc import BPF
 
 bpf_source = """
-int trace_bpf_prog_load(void ctx) {
+int trace_bpf_prog_load(void *ctx) {
   char comm[16];
   bpf_get_current_comm(&comm, sizeof(comm));
 
@@ -10,6 +11,8 @@ int trace_bpf_prog_load(void ctx) {
 }
 """
 
+print("Tracing execve ... Hit Ctrl-C to end")
+
 bpf = BPF(text = bpf_source)
-bpf.attach_tracepoint(tp = "bpf:bpf_prog_load", fn_name = "trace_bpf_prog_load")
+bpf.attach_tracepoint(tp = "syscalls:sys_enter_execve", fn_name = "trace_bpf_prog_load")
 bpf.trace_print()
