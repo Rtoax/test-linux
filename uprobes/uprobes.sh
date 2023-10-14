972dca3a73c907159a4332a92cf28c5e0a1012d3
Rong Tao <rtoax@foxmail.com>
2023-10-14 16:33:10 +0800

    uprobes/uprobes.sh: Add more check
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- uprobes/uprobes.sh
+++ uprobes/uprobes.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 TRACEFS=/sys/kernel/debug/tracing/
 
@@ -11,24 +12,24 @@ if [[ $(id -u) != 0 ]]; then
 fi
 
 binary=$( realpath ${binary} )
-addr=$(
-	objdump -T ${binary} | grep -w ${func} | awk '{print $1}' || {
-		echo "ERROR: Not found ${func} in ${binary}"
-		exit 1
-	}
-)
+addr=$( objdump -T ${binary} | grep -w ${func} 2>/dev/null | awk '{print $1}' )
+
+[[ ! -e ${binary} ]] && echo "ERROR: ${binary} is not exist" && exit 1
+[[ -z ${addr} ]] && echo "ERROR: Not found ${func} in ${binary}" && exit 1
 
 echo "p:uprobes/${func} ${binary}:0x${addr} %ip %ax" > ${TRACEFS}/uprobe_events
 cat ${TRACEFS}/events/uprobes/${func}/format
+cat ${TRACEFS}/events/uprobes/${func}/enable
 
 echo 1 > ${TRACEFS}/events/uprobes/enable
 
 clean() {
+	local ret=$?
 	echo 0 > ${TRACEFS}/events/uprobes/enable
 	echo > ${TRACEFS}/uprobe_events
+	exit ${ret}
 }
 trap clean EXIT
 
 watch -n1 tail ${TRACEFS}/trace
-
 
