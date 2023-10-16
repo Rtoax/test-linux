d8223739b07887cc32ec5c4cd4c00b28d5513588
Rong Tao <rongtao@cestc.cn>
2023-10-16 15:35:02 +0800

    barrier/barrier-x86.h: Add smp_ suffix
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- synchronization/barrier/barrier-x86.h
+++ synchronization/barrier/barrier-x86.h
@@ -1,12 +1,11 @@
 #define barrier() __asm__ __volatile__("": : :"memory")
 
 #if defined(__i386__)
-#define mb()	asm volatile("lock; addl $0,0(%%esp)" ::: "memory")
-#define rmb()	asm volatile("lock; addl $0,0(%%esp)" ::: "memory")
-#define wmb()	asm volatile("lock; addl $0,0(%%esp)" ::: "memory")
+#define smp_mb()	asm volatile("lock; addl $0,0(%%esp)" ::: "memory")
+#define smp_rmb()	asm volatile("lock; addl $0,0(%%esp)" ::: "memory")
+#define smp_wmb()	asm volatile("lock; addl $0,0(%%esp)" ::: "memory")
 #elif defined(__x86_64__)
-#define mb() 	asm volatile("mfence":::"memory")
-#define rmb()	asm volatile("lfence":::"memory")
-#define wmb()	asm volatile("sfence" ::: "memory")
+#define smp_mb() 	asm volatile("mfence":::"memory")
+#define smp_rmb()	asm volatile("lfence":::"memory")
+#define smp_wmb()	asm volatile("sfence" ::: "memory")
 #endif
-
