329ad605f694545b2f9c72a61c76061d654557b9
Rong Tao <rongtao@cestc.cn>
2023-10-17 10:37:13 +0800

    synchronization/completion: Use kthread_should_stop() for each two threads
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- synchronization/completion/modules/completion.c
+++ synchronization/completion/modules/completion.c
@@ -7,35 +7,44 @@
 #include <linux/completion.h>
 
 
-#define NR_KTHREAD 1
-
-struct task_struct *tasks[NR_KTHREAD];
+struct task_struct *tasks[2];
 static DECLARE_COMPLETION(cpu_running);
 
 
 static int thread1(void *data)
 {
-	const int ms = 2000;
+	const int ms = 1000;
 	while (!kthread_should_stop()) {
-		printk(KERN_INFO "Sleep %d ms\n", ms);
+		printk(KERN_INFO "Thread1: Sleep %d ms\n", ms);
 		msleep(ms);
 		complete(&cpu_running);
 		schedule();
 	}
+	printk(KERN_INFO "Thread1: exit.\n");
+
+	return 0;
+}
+
+static int thread2(void *data)
+{
+	printk(KERN_INFO "Thread2: waiting thread1 to done...\n");
+	wait_for_completion(&cpu_running);
+	printk(KERN_INFO "Thread2: thread1 completion.\n");
+
+	while (!kthread_should_stop())
+		schedule();
+
+	printk(KERN_INFO "Thread2: exit.\n");
 	return 0;
 }
 
 static int kernel_init(void)
 {
-	int i;
-
 	init_completion(&cpu_running);
 
-	for (i = 0; i < NR_KTHREAD; i++) {
-		tasks[i] = kthread_run(&thread1, NULL, "rtoax-%d", i);
-	}
+	tasks[0] = kthread_run(&thread1, NULL, "rtoax-worker");
+	tasks[1] = kthread_run(&thread2, NULL, "rtoax-waiter");
 
-	wait_for_completion(&cpu_running);
 	return 0;
 }
 
@@ -45,7 +54,7 @@ static void kernel_exit(void)
 
 	complete_all(&cpu_running);
 
-	for (i = 0; i < NR_KTHREAD; i++) {
+	for (i = 0; i < 2; i++) {
 		kthread_stop(tasks[i]);
 	}
 }
