4873bdf16bec41b39dede615e4f6d3a81e505963
Rong Tao <rongtao@cestc.cn>
2023-10-17 15:56:30 +0800

    algorithm: Add pi.c
    Link: https://stackoverflow.com/questions/9912151/math-constant-pi-value-in-c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ algorithm/pi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "unused.h"
+
+double caculate_pi(int accuracy)
+{
+	int i;
+	double result = 1;
+	double a = 2;
+	double b = 1;
+
+	for (i = 0; i < accuracy; i++) {
+		result = a / b * result;
+		if (a < b) {
+			a = a + 2;
+		} else if(b < a) {
+			b = b + 2;
+		}
+	}
+	return result * 2;
+}
+
+int main(void)
+{
+	double __unused pi = caculate_pi(50000000);
+	printf("pi = %.30lf\n", pi);
+	return 0;
+}
