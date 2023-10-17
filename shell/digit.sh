e1886711660589089fd514343b3144cce220ce71
Rong Tao <rongtao@cestc.cn>
2023-10-17 14:26:10 +0800

    shell: Add digit.sh
    Link: https://blog.csdn.net/whatday/article/details/105532407
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ shell/digit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+is_digit() {
+	grep '^[[:digit:]]*$' <<< "$1" 2>/dev/null || {
+		echo "ERROR: $1 is not digit" >&2
+		return 1
+	}
+	return 0
+}
+
+is_digit abc
+is_digit 000
+is_digit 123
+is_digit a23
+
