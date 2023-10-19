94f9f3f98b0f025ea23752e21c10428ccfb2ea39
Rong Tao <rongtao@cestc.cn>
2023-10-19 10:32:40 +0800

    raid/info.sh: Add mdadm command example
    Like the anaconda (inst.debug) told me:
    
     INFO:program:Running [36] mdadm --examine -E /dev/sdb ...
     INFO:program:stdout[36]: /dev/sdb:
               Magic : de11de11
             Version : 01.00.00
     Controller GUID : 4C534920:20202020:534B3833:34373535:39390000:20300000
                       (LSI     SK83475599)
      Container GUID : 4C534920:20202020:1000005D:10009361:50D5237D:CBB0E1A1
                       (LSI      12/22/22 03:05:33)
                 Seq : 00000026
       Redundant hdr : yes
       Virtual Disks : 1
           VD GUID[0] : 4C534920:20202020:1000005D:10009361:50D5237D:CA55795C
                       (LSI      12/22/22 03:05:33)
              unit[0] : 0
             state[0] : Optimal, Consistent
        init state[0] : Not Initialised
            access[0] : Read/Write
              Name[0] :
      Raid Devices[0] : 2 (0@0K 1@0K)
        Chunk Size[0] : 512 sectors
        Raid Level[0] : RAID0
       Device Size[0] : 976224256
        Array Size[0] : 1952448512
      Physical Disks : 255
           Number    RefNo      Size       Device      Type/State
              0    b7b97e69  976224256K                 active/Online
              1    fcb56574  976224256K /dev/sdb        active/Online
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- storage/raid/info.sh
+++ storage/raid/info.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-echo "------------------------------ lspci ------------------------------------"
-sudo lspci | grep RAID | sed 's/^/LSPCI: /g'
-echo "------------------------------- lshw ------------------------------------"
-sudo lshw -class storage | grep '\-raid' -A11 | sed 's/^/LSHW: /g'
-echo "-------------------------------------------------------------------------"
+dev_raid_metadata() {
+	local dev=$1
+	# -E, --examine Print contents of the metadata stored on the named device(s)
+	sudo mdadm --examine ${dev}
+}
+
+find_raid() {
+	echo "------------------------------ lspci ------------------------------------"
+	sudo lspci | grep RAID | sed 's/^/LSPCI: /g'
+	echo "------------------------------- lshw ------------------------------------"
+	sudo lshw -class storage | grep '\-raid' -A11 | sed 's/^/LSHW: /g'
+	echo "-------------------------------------------------------------------------"
+}
+
+find_raid
+dev_raid_metadata /dev/sda
+
