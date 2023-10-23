aecd8a486f75f32a4ac3335531aa6efda7500744
Rong Tao <rongtao@cestc.cn>
2023-10-23 11:08:36 +0800

    synchronization/mutex: Add modules/ demo
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ synchronization/mutex/modules/mutex_lock.c
@@ -0,0 +1,65 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/delay.h>
+#include <linux/mutex.h>
+
+#define NR_KTHREAD 3
+
+struct task_struct *task[NR_KTHREAD];
+
+static unsigned long my_sum = 0;
+static DEFINE_MUTEX(my_mutexlock);
+
+int thread_function(void *data)
+{
+	int nr_inc = 10000;
+
+	while (!kthread_should_stop()) {
+		if (nr_inc <= 0) {
+			msleep_interruptible(1000);
+			printk("kthread sleep.\n");
+			continue;
+		}
+
+		mutex_lock(&my_mutexlock);
+		my_sum++;
+		nr_inc--;
+		if (!nr_inc) {
+			mutex_unlock(&my_mutexlock);
+			continue;
+		}
+		mutex_unlock(&my_mutexlock);
+
+		schedule();
+	}
+
+	printk(KERN_INFO "thread done, sum = %ld\n", my_sum);
+	return 0;
+}
+
+static int kernel_init(void)
+{
+	int itask;
+	printk(KERN_INFO "mykthread init.\n");
+
+	for (itask = 0; itask < NR_KTHREAD; itask++)
+		task[itask] = kthread_run(&thread_function, NULL, "rtoax-%d", itask);
+
+	return 0;
+}
+
+static void kernel_exit(void)
+{
+	int itask;
+	for (itask = 0; itask < NR_KTHREAD; itask++)
+		kthread_stop(task[itask]);
+	printk(KERN_INFO "Exit, SUM = %ld\n", my_sum);
+}
+
+module_init(kernel_init);
+module_exit(kernel_exit);
+MODULE_AUTHOR("Rong Tao");
+MODULE_LICENSE("GPL");
