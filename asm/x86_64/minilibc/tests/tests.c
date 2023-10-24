bec05cc80a292668dbc7eb11d2c1416a68023d7c
Rong Tao <rongtao@cestc.cn>
2023-10-24 20:46:24 +0800

    asm/x86_64: Add minilibc from github
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ asm/x86_64/minilibc/tests/tests.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "tests.h"
+
+static struct test *tests[100];
+static int nr_tests = 0;
+
+void add_test(struct test *t)
+{
+	tests[nr_tests++] = t;
+}
+
+int main(void)
+{
+	int i;
+	struct test *t;
+
+	for (i = 0; i < nr_tests; i++) {
+		t = tests[i];
+		printf("%s\n", t->name);
+		t->run();
+	}
+
+	return 0;
+}
