d0bbe92b8ff27634ec5658c6b7a49b2e42090b92
Rong Tao <rongtao@cestc.cn>
2023-10-24 19:55:51 +0800

    asm/x86_32: Add average and power
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- asm/x86_32/echo.asm
+++ asm/x86_32/echo.asm
@@ -5,11 +5,11 @@
 ; one per line.
 ; ----------------------------------------------------------------------------
 
-	global	_main
-	extern	_printf
+	global	main
+	extern	printf
 
 	section .text
-_main:
+main:
 	mov	ecx, [esp+4]	        ; argc
 	mov	edx, [esp+8]		; argv
 top:
@@ -18,7 +18,7 @@ top:
 
 	push	dword [edx]		; the argument string to display
 	push	format			; the format string
-	call	_printf
+	call	printf
 	add	esp, 8			; remove the two parameters
 
 	pop	edx			; restore registers printf used
@@ -27,6 +27,7 @@ top:
 	add	edx, 4			; point to next argument
 	dec	ecx			; count down
 	jnz	top			; if not done counting keep going
+	mov	eax, 0
 
 	ret
 format:
