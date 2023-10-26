b1852796c7eed892365b70f4abef6645e153d088
Rong Tao <rongtao@cestc.cn>
2023-10-26 15:05:10 +0800

    atomic: Test atomic_try_cmpxchg_acquire()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ synchronization/atomic/modules/atomic_try_cmpxchg_acquire.c
@@ -0,0 +1,34 @@
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/init.h>
+#include <linux/bug.h>
+#include <linux/kernel.h>
+#include <linux/atomic.h>
+#include <linux/module.h>
+
+
+static __init int test_atomics_init(void)
+{
+	int ret;
+	atomic_t a;
+	u32 val = 0;
+
+	atomic_set(&a, 0xff00);
+
+	ret = atomic_try_cmpxchg_acquire(&a, &val, 1 << 3);
+	pr_info("%d %x %x\n", ret, val, atomic_read(&a));
+
+	ret = atomic_try_cmpxchg_acquire(&a, &val, 1 << 3);
+	pr_info("%d %x %x\n", ret, val, atomic_read(&a));
+
+	/* insmod failed on purpose */
+	return -1;
+}
+
+static __exit void test_atomics_exit(void) {}
+
+module_init(test_atomics_init);
+module_exit(test_atomics_exit);
+
+MODULE_LICENSE("GPL");
+
