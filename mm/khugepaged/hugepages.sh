9dc586e324fe09be1d71801dc0198c2894eb45b0
Rong Tao <rongtao@cestc.cn>
2023-10-28 14:08:31 +0800

    hugepages.sh: Add mount hugetlbfs
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- mm/khugepaged/hugepages.sh
+++ mm/khugepaged/hugepages.sh
@@ -1,20 +1,48 @@
 #!/bin/bash
-
 set -e
 
-declare -a hugepage_dirs
-declare -a nr_hugepages
+huge_mnt_path="/mnt/huge"
 
-hugepage_dirs=( $(ls /sys/kernel/mm/hugepages/) )
+mount_hugetlbfs() {
 
-for p in ${hugepage_dirs[@]}
-do
-	nr_hugepages+=( $(cat /sys/kernel/mm/hugepages/$p/nr_hugepages) )
-done
+	# test if dir exist
+	if [ ! -x $huge_mnt_path ]
+	then
+		mkdir $huge_mnt_path
+	fi
+	mount -t hugetlbfs nodev $huge_mnt_path
+	echo 20 > /proc/sys/vm/nr_hugepages
+}
+umount_hugetlbfs() {
+	# TODO: can't umount hugetlbfs
+	umount $huge_mnt_path
+}
 
-echo -e "    SIZE \tNUM"
-for ((i = 0; i < ${#hugepage_dirs[@]}; i++))
-do
-	echo -e "    ${hugepage_dirs[$i]:10}\t${nr_hugepages[$i]}"
-done
+get_hugepage_info() {
+	declare -a hugepage_dirs
+	declare -a nr_hugepages
 
+	hugepage_dirs=( $(ls /sys/kernel/mm/hugepages/) )
+
+	for p in ${hugepage_dirs[@]}
+	do
+		nr_hugepages+=( $(cat /sys/kernel/mm/hugepages/$p/nr_hugepages) )
+	done
+
+	echo -e "    SIZE \tNUM"
+	for ((i = 0; i < ${#hugepage_dirs[@]}; i++))
+	do
+		echo -e "    ${hugepage_dirs[$i]:10}\t${nr_hugepages[$i]}"
+	done
+}
+
+if [[ $(id -u) != 0 ]]; then
+	echo "ERROR: Must run with root(sudo)"
+	exit 1
+fi
+
+get_hugepage_info
+mount_hugetlbfs
+get_hugepage_info
+umount_hugetlbfs
+get_hugepage_info
