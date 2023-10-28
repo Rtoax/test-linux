b36c24da70d4647c72520d3383b40e746c1faffa
Rong Tao <rtoax@foxmail.com>
2023-10-28 21:24:54 +0800

    mmap: Fix get_unmapped_area.c code style
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- mm/mmap/modules/get_unmapped_area.c
+++ mm/mmap/modules/get_unmapped_area.c
@@ -1,19 +1,21 @@
 /**
- *	File ./get_unmapped_area.c 
- *	Time 2021.11.05
- *	Author	Rong Tao <rongtao@cestc.cn>
+ * get_unmapped_area.c
+ *
+ * 2021-11-05	Rong Tao	Create this.
  */
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/mm.h>
 #include <linux/mm_types.h>
 
+
 static void test__get_unmapped_area(void)
 {
 	unsigned long addr;
 	addr = get_unmapped_area(NULL, 0, PAGE_SIZE, 0, 0);
-	if(!addr) {
+	if (!addr) {
 		printk(KERN_ERR "can't get unmapped area.\n");
+		return;
 	}
 	printk(KERN_INFO "get_unmapped_area = 0x%lx\n", addr);
 }
@@ -24,13 +26,12 @@ static int __init mymem_init(void)
 
 	test__get_unmapped_area();
 
-	return 0;
+	/* insmod failed in purpose */
+	return -1;
 }
 
 static void __exit mymem_exit(void)
-{
-	printk("mymem module is leaving..\n");
-}
+{}
 
 MODULE_LICENSE("GPL");
 module_init(mymem_init);
