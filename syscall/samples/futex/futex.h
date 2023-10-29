8b09cdec8a9150efbf6841f5798931a13a095ac4
Rong Tao <rtoax@foxmail.com>
2023-10-29 20:49:52 +0800

    syscall/samples/futex: Add some demos
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- syscall/samples/futex/futex.h
+++ syscall/samples/futex/futex.h
@@ -3,8 +3,12 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
+#define weak_alias(name, aliasname) \
+	extern typeof (name) aliasname __attribute__ ((weak, alias(#name)))
 
-static long sys_futex(void *addr1, int op, int val1, struct timespec *timeout, void *addr2, int val3)
+static long sys_futex(void *addr1, int op, int val1, struct timespec *timeout,
+		      void *addr2, int val3)
 {
 	return syscall(SYS_futex, addr1, op, val1, timeout, addr2, val3);
 }
+weak_alias(sys_futex, futex);
