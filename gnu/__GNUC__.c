fcbc39502d0bf7102b7de83c554ea47b71c0c52a
Rong Tao <rtoax@foxmail.com>
2023-10-29 17:07:08 +0800

    gnu: Add __GNUC__.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ gnu/__GNUC__.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#if defined (__GNUC__) && ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 5) || __GNUC__ >= 5)
+#endif
+
+int main(void)
+{
+	printf("__GNUC__ = %d.\n", __GNUC__);
+	printf("__GNUC_MINOR__ = %d.\n", __GNUC_MINOR__);
+	return 0;
+}
