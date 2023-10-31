af9d25c1793d80f041b978e352902e6334c3527c
Rong Tao <rongtao@cestc.cn>
2023-10-31 05:17:04 -0400

    c/auto.c: Fix warning: type defaults to ‘int’ in declaration of ‘i1’ [-Wimplicit-int]
        gcc -Werror -Wall -I../macro/ -DDEBUG -O0 -g -ggdb -Wno-error=implicit-int -o auto.o -c auto.c
        auto.c: In function ‘main’:
        auto.c:5:23: warning: type defaults to ‘int’ in declaration of ‘i1’ [-Wimplicit-int]
            5 |         auto __unused i1 = 10;
              |                       ^~
        gcc   auto.o   -o auto
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/auto.c
+++ c/auto.c
@@ -2,7 +2,10 @@
 
 int main(void)
 {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wimplicit-int"
 	auto __unused i1 = 10;
+#pragma GCC diagnostic pop
 	auto __unused int i2 = 10;
 	return 0;
 }
