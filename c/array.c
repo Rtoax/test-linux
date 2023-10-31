a01bad6ac29a214d712f72e8ab3af31aa7cf67fa
Rong Tao <rongtao@cestc.cn>
2023-10-31 04:57:12 -0400

    c/array.c: Fix error: array index 3 is past the end of the array
    Specify CC=clang in Makefile:
    
        $ clang -Werror -Wall -I../macro/ -DDEBUG -O0 -g -ggdb  -o array.o -c array.c
    
        array.c:10:18: error: array index 3 is past the end of the array
        (which contains 2 elements) [-Werror,-Warray-bounds]
                printf("%p\n", &array[3][0]);
                                ^     ~
        array.c:5:2: note: array 'array' declared here
                int array[2][3];
                ^
        array.c:12:17: error: array index 3 is past the end of the array
         (which contains 2 elements) [-Werror,-Warray-bounds]
                printf("%p\n", array[3]);
                               ^     ~
        array.c:5:2: note: array 'array' declared here
                int array[2][3];
                ^
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/array.c
+++ c/array.c
@@ -7,9 +7,12 @@ int main(void)
 	printf("%p\n", &array[0][0]);
 	printf("%p\n", &array[1][0]);
 	printf("%p\n", &array[0][1]);
-	printf("%p\n", &array[3][0]);
 	printf("%p\n", &array[0][3]);
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Warray-bounds"
+	printf("%p\n", &array[3][0]);
 	printf("%p\n", array[3]);
+#pragma GCC diagnostic pop
 
 	return 0;
 }
--- c/compat.c
+++ c/compat.c
@@ -13,7 +13,7 @@ int prog_load(int a, int b, int c, int d, int e, int f)
 	return printf("6: %d %d %d %d %d %d\n", a, b, c, d, e, f);
 }
 
-int main()
+int main(void)
 {
 	prog_load(1, 2, 3, 4, 5, 6);
 	prog_load(1, 2, 3, 4);
