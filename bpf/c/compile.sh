91d5b5a3d8cb39f4100dd9f0ff6fd1bccaa35be2
Rong Tao <rongtao@cestc.cn>
2023-11-01 03:05:58 -0400

    make.list: Add more to 'user' target
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/c/compile.sh
+++ bpf/c/compile.sh
@@ -5,9 +5,14 @@ __bcc() {
 		echo $0 [bpf-prog.c]
 		return 1
 	fi
-	prog=`basename $1`
+	local prog=$(realpath $1)
+	local arch
+
+	[[ $(uname -m) == aarch64 ]] && arch=arm64
+	[[ $(uname -m) == x86_64 ]] && arch=x86-64
+
 	clang -O2 -emit-llvm -c $prog -o - | \
-		llc -march=x86-64 -filetype=obj -o ${prog%.*}.o
+		llc -march=${arch} -filetype=obj -o ${prog%.*}.o
 }
 
 __bcc bpf.c
