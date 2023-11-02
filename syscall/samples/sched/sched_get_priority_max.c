f9381bd7f3f99cae08abfaed787dd44f1ccc6ab0
Rong Tao <rongtao@cestc.cn>
2023-11-02 11:35:11 +0800

    syscall/samples/sched: Add sched_get_priority_{max,min}.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/sched/sched_get_priority_max.c
@@ -0,0 +1,27 @@
+#include <sched.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sched.h>
+#include <linux/sched.h>
+
+#define RANGE(policy) \
+	printf("%-16s %-8d %-8d %-8d\n", \
+		#policy, policy, \
+		sched_get_priority_min(policy), \
+		sched_get_priority_max(policy));
+
+int main(void)
+{
+	printf("%-16s %-8s %-8s %-8s\n", "POLICY", "NUM", "MIN", "MAX");
+
+	RANGE(SCHED_OTHER);
+	RANGE(SCHED_NORMAL);
+	RANGE(SCHED_FIFO);
+	RANGE(SCHED_RR);
+	RANGE(SCHED_BATCH);
+	RANGE(SCHED_ISO);
+	RANGE(SCHED_IDLE);
+	RANGE(SCHED_DEADLINE);
+
+	return 0;
+}
