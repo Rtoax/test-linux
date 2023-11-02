8f5749c8af304fd27ef0f2072edeca54f84fbbd2
Rong Tao <rongtao@cestc.cn>
2023-11-02 11:12:24 +0800

    syscall/samples/sched: Add more demos
    Link: https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux_for_real_time/7/html/reference_guide/using_the_sched_setaffinity_system_call_to_set_processor_affinity
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/sched/CPU_ALLOC.c
@@ -0,0 +1,35 @@
+/**
+ * https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux_for_real_time/7/html/reference_guide/using_the_sched_setaffinity_system_call_to_set_processor_affinity
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sched.h>
+
+
+int main(int argc, char **argv)
+{
+	int i, online=0;
+	ulong ncores = sysconf(_SC_NPROCESSORS_CONF);
+	cpu_set_t *setp = CPU_ALLOC(ncores);
+	ulong setsz = CPU_ALLOC_SIZE(ncores);
+
+	CPU_ZERO_S(setsz, setp);
+
+	if (sched_getaffinity(getpid(), setsz, setp) == -1) {
+		perror("sched_getaffinity(2) failed");
+		exit(errno);
+	}
+
+	for (i=0; i < CPU_COUNT_S(setsz, setp); i++) {
+		if (CPU_ISSET_S(i, setsz, setp))
+			online++;
+	}
+
+	printf("%ld cores configured, %d cpus allowed in affinity mask\n",
+		ncores, online);
+	CPU_FREE(setp);
+
+	return 0;
+}
---
+++ syscall/samples/sched/CPU_SETSIZE.c
@@ -0,0 +1,9 @@
+#include <sched.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("CPU_SETSIZE = %d\n", CPU_SETSIZE);
+	return 0;
+}
