59f0b0f10c6d3a94aad4e8c4694552a283f0b96c
Rong Tao <rongtao@cestc.cn>
2023-11-07 19:59:52 +0800

    xdp/samples/bcc: Add -i,--interface argument
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/xdp/samples/bcc/loader.py
+++ bpf/xdp/samples/bcc/loader.py
@@ -3,8 +3,26 @@
 from bcc import BPF
 import time
 import sys
+import argparse
 
-device = "enp1s0:1"
+examples = """examples:
+    ./loader.py -i eno1       # track eno1 interface
+"""
+
+parser = argparse.ArgumentParser(
+    description="bcc XDP demo",
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    epilog=examples)
+parser.add_argument("-i", "--interface", default="-1",
+    help="specify ether interface to track, check with ifconfig, ip, etc.")
+
+args = parser.parse_args()
+device = args.interface
+
+if device == "-1":
+    print("Must specify interface with -i")
+    exit()
+
 b = BPF(src_file="program.c")
 fn = b.load_func("myprogram", BPF.XDP)
 b.attach_xdp(device, fn, 0)
