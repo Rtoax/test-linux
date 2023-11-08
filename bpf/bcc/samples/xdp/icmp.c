bf9e63619e9a517ab21517ade3427fd315dff525
Rong Tao <rongtao@cestc.cn>
2023-11-08 14:43:21 +0800

    bcc/samples/xdp:
    Terminal 10.56.52.5:
    
        $ sudo ./icmp.py -i eno1 -t tc
    
    Other Host:
    
        $ ping 10.56.52.5 -c1
        PING 10.56.52.5 (10.56.52.5) 56(84) bytes of data.
    
        --- 10.56.52.5 ping statistics ---
        1 packets transmitted, 0 received, 100% packet loss, time 0ms
    
    Terminal 10.56.52.5:
    
        $ sudo ./icmp.py -i eno1 -t tc
        Tracing ICMP, hit CTRL+C to stop
        b' <idle>-0 [004] ..s2. 21305.186245: bpf_trace_printk: [tc] ingress got packet'
        b' <idle>-0 [004] ..s21 21305.187346: bpf_trace_printk: [xdp] ICMP request for 534380a type 8'
        b' <idle>-0 [004] ..s2. 21305.187362: bpf_trace_printk: [tc] ingress got packet'
        b' <idle>-0 [004] ..s2. 21305.187365: bpf_trace_printk: [tc] ICMP request for 534380a type 8'
        ^C << exit
    
    Other Host:
    
        $ ping 10.56.52.5 -c1
        PING 10.56.52.5 (10.56.52.5) 56(84) bytes of data.
        64 bytes from 10.56.52.5: icmp_seq=1 ttl=64 time=0.300 ms
        --- 10.56.52.5 ping statistics ---
        1 packets transmitted, 1 received, 0% packet loss, time 0ms
        rtt min/avg/max/mdev = 0.300/0.300/0.300/0.000 ms
    
    Link: https://www.youtube.com/watch?v=0p987hCplbk
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/bcc/samples/xdp/icmp.c
+++ bpf/bcc/samples/xdp/icmp.c
@@ -3,6 +3,7 @@
 #include <linux/icmp.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
+#include <linux/pkt_cls.h>
 #include <uapi/linux/bpf.h>
 
 static __always_inline unsigned short
@@ -44,4 +45,20 @@ int xdp_icmp(struct xdp_md *ctx)
 	}
 
 	return XDP_PASS;
+}
+
+int tc(struct __sk_buff *skb)
+{
+	bpf_trace_printk("[tc] ingress got packet\n");
+
+	void *data = (void *)(long)skb->data;
+	void *data_end = (void *)(long)skb->data_end;
+
+	if (is_icmp_ping_request(data, data_end)) {
+		struct iphdr *iph = data + sizeof(struct ethhdr);
+		struct icmphdr *icmp = data + sizeof(struct ethhdr) + sizeof(struct iphdr);
+		bpf_trace_printk("[tc] ICMP request for %x type %x\n", iph->daddr, icmp->type);
+		return TC_ACT_SHOT;
+	}
+	return TC_ACT_OK;
 }
--- bpf/bcc/samples/xdp/icmp.py
+++ bpf/bcc/samples/xdp/icmp.py
@@ -4,6 +4,8 @@ from bcc import BPF
 import time
 import sys
 import argparse
+import socket
+from pyroute2 import IPRoute
 
 examples = """examples:
     ./icmp.py -i eno1       # track eno1 interface
@@ -15,9 +17,12 @@ parser = argparse.ArgumentParser(
     epilog=examples)
 parser.add_argument("-i", "--interface", default="-1",
     help="specify ether interface to track, check with ifconfig, ip, etc.")
+parser.add_argument("-t", "--tc", default="-1",
+    help="specify a traffix control.")
 
 args = parser.parse_args()
 device = args.interface
+tc = args.tc
 
 if device == "-1":
     print("Must specify interface with -i")
@@ -26,6 +31,24 @@ if device == "-1":
 b = BPF(src_file="icmp.c")
 fn = b.load_func("xdp_icmp", BPF.XDP)
 b.attach_xdp(device, fn, 0)
+
+if tc == "-1":
+    print("You can specify -t,--tc")
+elif tc == "tc":
+    global iproute
+    iproute = IPRoute()
+    # BPF_PROG_TYPE_SCHED_CLS
+    fi = b.load_func("tc", BPF.SCHED_CLS)
+    links = iproute.link_lookup(ifname=device)
+    idx = links[0]
+
+    try:
+        iproute.tc("add", "ingress", idx, "ffff:")
+    except:
+        print("qdisc ingress alread exists")
+
+    iproute.tc("add-filter", "bpf", idx, ":1", fd=fi.fd, name=fi.name,
+                parent="ffff:", action="drop", classid=1)
 
 print("Tracing ICMP, hit CTRL+C to stop")
 while 1:
@@ -36,3 +59,5 @@ while 1:
         break
 
 b.remove_xdp(device, 0)
+if tc == "tc":
+    iproute.tc("del", "ingress", idx, "ffff:")
