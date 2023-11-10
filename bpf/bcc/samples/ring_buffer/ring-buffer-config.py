5f52ec8f3f677f1ad192a91aee15b980ed7eace3
Rong Tao <rongtao@cestc.cn>
2023-11-10 08:58:58 +0800

    bcc/samples: Add ring_buffer and config buffer from [0]
    [0] https://github.com/lizrice/learning-ebpf.git chaper4/
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ bpf/bcc/samples/ring_buffer/ring-buffer-config.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from bcc import BPF
+import ctypes as ct
+
+program = r"""
+struct user_msg_t {
+    char message[16];
+};
+
+BPF_HASH(config, u32, struct user_msg_t);
+
+BPF_RINGBUF_OUTPUT(output, 1);
+
+struct data_t {
+    int pid;
+    int uid;
+    char command[16];
+    char message[16];
+};
+
+int hello(void *ctx) {
+    struct data_t data = {};
+    char message[16] = "Hello World";
+    struct user_msg_t *p;
+
+    data.pid = bpf_get_current_pid_tgid() >> 32;
+    data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+
+    bpf_get_current_comm(&data.command, sizeof(data.command));
+
+    p = config.lookup(&data.uid);
+    if (p != 0) {
+        bpf_probe_read_kernel(&data.message, sizeof(data.message), p->message);
+    } else {
+        bpf_probe_read_kernel(&data.message, sizeof(data.message), message);
+    }
+
+    output.ringbuf_output(&data, sizeof(data), 0);
+
+    return 0;
+}
+"""
+
+b = BPF(text=program)
+b["config"][ct.c_int(0)] = ct.create_string_buffer(b"Hey root!")
+b["config"][ct.c_int(1000)] = ct.create_string_buffer(b"Hey user 1000!")
+syscall = b.get_syscall_fnname("execve")
+b.attach_kprobe(event=syscall, fn_name="hello")
+
+def print_event(cpu, data, size):
+    data = b["output"].event(data)
+    print(f"{data.pid} {data.uid} {data.command.decode()} {data.message.decode()}")
+
+b["output"].open_ring_buffer(print_event)
+while True:
+    b.ring_buffer_poll()
---
+++ bpf/bcc/samples/ring_buffer/ring_buffer.py
@@ -0,0 +1,1 @@
+ring-buffer-config.py
