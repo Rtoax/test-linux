216b2c098ead202ee4416b245f0da1fe8624833a
Rong Tao <rtoax@foxmail.com>
2023-11-13 21:46:51 +0800

    syscall/samples/sched: Add clone/
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ syscall/samples/sched/clone/clone.c
@@ -0,0 +1,49 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mount.h>
+#include <stdio.h>
+#include <sched.h>
+#include <signal.h>
+#include <unistd.h>
+
+
+#define STACK_SIZE (1024 * 1024)
+
+static char stack[STACK_SIZE];
+
+int child(void* arg)
+{
+	int ret;
+
+	sethostname("myhost", 6);
+	/* TODO: switch root to rootfs/initramfs */
+	ret = chroot("/");
+	if (ret) {
+		perror("chroot");
+		return 0;
+	}
+	chdir("/");
+	mount("proc", "/proc", "proc", 0, NULL);
+	execlp("/bin/sh", "/bin/sh" , NULL);
+	printf("Ooops\n");
+	return 1;
+}
+
+int main(void)
+{
+	int flags;
+	pid_t pid;
+
+	flags = CLONE_NEWUTS | CLONE_NEWIPC | CLONE_NEWPID | CLONE_NEWNS | SIGCHLD;
+	pid = clone(child, stack + STACK_SIZE, flags, NULL);
+	if (pid == -1) {
+		perror("clone");
+	}
+
+	waitpid(pid, NULL, 0);
+	return 0;
+}
+
