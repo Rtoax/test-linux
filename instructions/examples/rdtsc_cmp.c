f8cf24f1f6505cba78a99e9f1e3f9a837e42cce0
Rong Tao <rongtao@cestc.cn>
2023-11-14 19:02:32 +0800

    instructions/examples: rdtsc_cmp.c: Add [nloop] arg
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/rdtsc_cmp.c
+++ instructions/examples/rdtsc_cmp.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "rdtsc.h"
 
 #include "utils.h"
 
-#define TEST_NLOOP	10000000
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	uint64_t i;
 	uint64_t tscp, tsc;
 	struct timeval start, end;
+	unsigned long test_nloop = 10000000;
+
+	fprintf(stderr, "%s [nloop], default: %ld\n", argv[0], test_nloop);
+
+	if (argc > 1) {
+		test_nloop = strtoul(argv[1], NULL, 10);
+	}
+
+	fprintf(stdout, "NLoop %ld\n", test_nloop);
 
 	/**
 	 * rdtsc is faster than rdtscp
@@ -19,28 +28,28 @@ int main(void)
 	 */
 	uint32_t rcx = 0;
 	gettimeofday(&start, NULL);
-	for (i = 0; i < TEST_NLOOP; i++) {
+	for (i = 0; i < test_nloop; i++) {
 		tscp = rdtscp(&rcx);
 	}
 	gettimeofday(&end, NULL);
 	print_diff_tv("           rdtscp ", &end, &start, true);
 
 	gettimeofday(&start, NULL);
-	for (i = 0; i < TEST_NLOOP; i++) {
+	for (i = 0; i < test_nloop; i++) {
 		tscp = rdtscp_ignore_rcx();
 	}
 	gettimeofday(&end, NULL);
 	print_diff_tv("rdtscp ignore rcx ", &end, &start, true);
 
 	gettimeofday(&start, NULL);
-	for (i = 0; i < TEST_NLOOP; i++) {
+	for (i = 0; i < test_nloop; i++) {
 		tsc = rdtsc();
 	}
 	gettimeofday(&end, NULL);
 	print_diff_tv("            rdtsc ", &end, &start, true);
 
 	gettimeofday(&start, NULL);
-	for (i = 0; i < TEST_NLOOP; i++) {
+	for (i = 0; i < test_nloop; i++) {
 		tsc = rdtsc_fence();
 	}
 	gettimeofday(&end, NULL);
