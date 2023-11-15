33cada736458ff64a1e3bfa2df0263042c30c655
Rong Tao <rtoax@foxmail.com>
2023-11-15 22:04:40 +0800

    syscall/samples/time: Add clock_gettime-CLOCK_BOOTTIME.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ syscall/samples/time/clock_gettime-CLOCK_BOOTTIME.c
@@ -0,0 +1,18 @@
+#include <time.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include "helpers.h"
+
+
+int main(int argc, char **argv)
+{
+	struct timespec elapsed_from_boot;
+
+	sys_clock_gettime(CLOCK_BOOTTIME, &elapsed_from_boot);
+	printf("%ld - seconds elapsed from boot\n", elapsed_from_boot.tv_sec);
+
+	clock_gettime(CLOCK_BOOTTIME, &elapsed_from_boot);
+	printf("%ld - seconds elapsed from boot\n", elapsed_from_boot.tv_sec);
+
+	return 0;
+}
