98d9c55cede04d61ad6d697983516bf7acd91d8e
Rong Tao <rongtao@cestc.cn>
2023-11-15 19:29:51 +0800

    gdb: Modify pthread-gdb.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- gdb/pthread-gdb.sh
+++ gdb/pthread-gdb.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+set -x
+
+make pthread-gdb
 pid=$(pidof pthread-gdb)
+if [[ -z "$pid" ]]; then
+	./pthread-gdb &
+	sleep 1
+	pid=$(pidof pthread-gdb)
+fi
 child_tid=$(ls /proc/$pid/task/ | tail -n1)
 
 gdb -p $child_tid < pthread-gdb.gdb
+
+sleep 1
+kill -9 ${pid}
