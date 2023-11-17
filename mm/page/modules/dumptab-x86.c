54a6a1bf0fe75cfba8b5f0ed8e60bdd5f2fe77f0
Rong Tao <rongtao@cestc.cn>
2023-11-17 20:07:39 +0800

    page: dumptab-x86.c: Code format
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- mm/page/modules/dumptab-x86.c
+++ mm/page/modules/dumptab-x86.c
@@ -48,32 +48,32 @@ char* string_page_size(enum pt_addr_bit bitpos)
 	size_t pagesize;
 	CORNY_ASSERT(bitpos == PDPT || bitpos == PD || bitpos == PT);
 	pagesize = 1 << bitpos;
-	switch(pagesize){
-		case 1024*1024*1024:
-			return "1GB";
-		case 2*1024*1024:
-			return "2MB";
-		case 4*1024:
-			return "4KB";
-		default:
-			return "<BUG PAGESIZE>";
+	switch (pagesize) {
+	case 1024*1024*1024:
+		return "1GB";
+	case 2*1024*1024:
+		return "2MB";
+	case 4*1024:
+		return "4KB";
+	default:
+		return "<BUG PAGESIZE>";
 	}
 }
 
 /* page table bits 51:M reserved, must be 0. Create global bitmask once.*/
 static u64 pte_reserved_flags;
 
-
-static int check_entry(u64 e){
+static int check_entry(u64 e)
+{
 	/* 51:M reserved, must be 0*/
-	if(e & pte_reserved_flags){
+	if (e & pte_reserved_flags) {
 		printk("invalid entry!\n");
 		return 0;
 	}
-	if(e & _PAGE_PSE){
-		//TODO references a page directly, probably sanity check address?
+	if (e & _PAGE_PSE) {
+		// TODO references a page directly, probably sanity check address?
 	}
-	if(!(e & _PAGE_PRESENT) && e){
+	if (!(e & _PAGE_PRESENT) && e) {
 		printk("strange entry!\n");
 		return 1;
 	}
@@ -113,7 +113,8 @@ static inline u64 entry_extract_flags(u64 entry)
 }
 
 
-/*The part of the virtual address defined by the page table entry at index for the page table level indicated by bitpos*/
+/* The part of the virtual address defined by the page table entry at index for
+ * the page table level indicated by bitpos*/
 static inline u64 pte_addr_part(int index, enum pt_addr_bit bitpos)
 {
 	CORNY_ASSERT(index < 512);
@@ -126,13 +127,13 @@ static inline u64 pte_addr_part(int index, enum pt_addr_bit bitpos)
  * returns:
  *  1: descend to deeper levels
  *  0: stop descending
- * */
+ */
 int dump_entry(struct seq_file *s, struct dumptbl_state *state, enum pt_addr_bit bitpos)
 {
 	char *str_level;
 	struct page_table *table; //page table
 	int i; //index into the table
-	u64 e; //page table entry 
+	u64 e; //page table entry
 
 	int ret = 1; //continue descending
 
@@ -147,48 +148,47 @@ int dump_entry(struct seq_file *s, struct dumptbl_state *state, enum pt_addr_bit
 	CORNY_ASSERT(state->pdpt_i < 512);
 	CORNY_ASSERT(state->pd_i < 512);
 
-	switch(bitpos){
-		case PML4: /*outer level*/
-			table = state->pml4;
-			i = state->pml4_i;
-			str_level = "pml4";
-			baddr = &state->pml4_baddr;
-			//sign extend the most significant bits to canonical address
-			outer_baddr = 0;
-			if(pte_addr_part(i, 39) & (1LL << 47) /*highest bit set*/){
-				outer_baddr = (0xffffLL << 48);
-			}
-			break;
-		case PDPT:
-			table = state->pdpt;
-			i = state->pdpt_i;
-			str_level = "  pdpt";
-			outer_baddr = state->pml4_baddr;
-			baddr = &state->pdpt_baddr;
-			break;
-		case PD:
-			table = state->pd;
-			i = state->pd_i;
-			str_level = "      pd";
-			CORNY_ASSERT((state->pml4_baddr | state->pdpt_baddr) == state->pdpt_baddr);
-			outer_baddr = state->pdpt_baddr;
-			baddr = &state->pd_baddr;
-			break;
-		case PT: /*final level*/
-			table = state->pt;
-			i = state->pt_i;
-			str_level = "        pt";
-			CORNY_ASSERT((state->pml4_baddr | state->pdpt_baddr | state->pd_baddr) == state->pd_baddr);
-			outer_baddr = state->pd_baddr;
-			baddr = &state->pt_baddr;
-			break;
+	switch (bitpos) {
+	case PML4: /*outer level*/
+		table = state->pml4;
+		i = state->pml4_i;
+		str_level = "pml4";
+		baddr = &state->pml4_baddr;
+		//sign extend the most significant bits to canonical address
+		outer_baddr = 0;
+		if (pte_addr_part(i, 39) & (1LL << 47) /*highest bit set*/) {
+			outer_baddr = (0xffffLL << 48);
+		}
+		break;
+	case PDPT:
+		table = state->pdpt;
+		i = state->pdpt_i;
+		str_level = "  pdpt";
+		outer_baddr = state->pml4_baddr;
+		baddr = &state->pdpt_baddr;
+		break;
+	case PD:
+		table = state->pd;
+		i = state->pd_i;
+		str_level = "      pd";
+		CORNY_ASSERT((state->pml4_baddr | state->pdpt_baddr) == state->pdpt_baddr);
+		outer_baddr = state->pdpt_baddr;
+		baddr = &state->pd_baddr;
+		break;
+	case PT: /*final level*/
+		table = state->pt;
+		i = state->pt_i;
+		str_level = "        pt";
+		CORNY_ASSERT((state->pml4_baddr | state->pdpt_baddr | state->pd_baddr) == state->pd_baddr);
+		outer_baddr = state->pd_baddr;
+		baddr = &state->pt_baddr;
+		break;
 	}
 
 	e = (u64)table->entry[i];
 
-
 	CORNY_ASSERT(check_entry(e));
-	if(!(e & _PAGE_PRESENT)){
+	if (!(e & _PAGE_PRESENT)) {
 		/*skip page which is marked not present. Do not emit any output.*/
 		return 0;
 	}
@@ -200,22 +200,23 @@ int dump_entry(struct seq_file *s, struct dumptbl_state *state, enum pt_addr_bit
 	CORNY_ASSERT(((state->pdpt_baddr & addr_max) == 0)); //no overlapping bits
 	addr_max |= *baddr;
 
-	if((e & _PAGE_PSE) || bitpos == PT){
+	if ((e & _PAGE_PSE) || bitpos == PT) {
 		// PSE for 2MB or 1GB direct mapping
 		// bitpos == PT, then the _PAGE_PSE bit is the PAT bit. But for 4k pages, we always have a direct mapping
 		_direct_mapping = 1;
 		ret = 0; // do not descend to any deeper page tables!
 	}
 
-	if((state->last_addr + 1 == *baddr) && (state->last_flags == entry_extract_flags(e))){
+	if ((state->last_addr + 1 == *baddr) &&
+		(state->last_flags == entry_extract_flags(e))) {
 		// don't print consecutive similar mappings
 		//seq_printf(s, ".");
 		++state->skipped;
-	}else{
-		if(state->skipped){
+	} else {
+		if (state->skipped)
 			seq_printf(s, "%s skipped %d following entries (til %p)\n",
 				   str_level, state->skipped, (void*)state->last_addr);
-		}
+
 		state->skipped = 0;
 		seq_printf(s, "%s v %p %p %s %s %s %s %s %s", str_level,
 			(void*)*baddr, (void*)addr_max,
@@ -226,9 +227,10 @@ int dump_entry(struct seq_file *s, struct dumptbl_state *state, enum pt_addr_bit
 			e & _PAGE_ACCESSED ? "A" : "",
 			e & _PAGE_NX ? "NX" : ""
 			);
-		if(_direct_mapping){
+
+		if (_direct_mapping)
 			seq_printf(s, " -> %s page", string_page_size(bitpos));
-		}
+
 		seq_printf(s, "\n");
 	}
 
@@ -249,7 +251,7 @@ void* next_page_table_vaddr(u64 pagetbl_entry)
 	CORNY_ASSERT((pagetbl_entry & pte_reserved_flags) == 0);
 
 	vaddr = phys_to_virt(paddr);
-	if(!virt_addr_valid(vaddr) || !IS_ALIGNED(paddr, 4096)){
+	if (!virt_addr_valid(vaddr) || !IS_ALIGNED(paddr, 4096)) {
 		printk("CRITICAL: invalid addr!\n");
 		return NULL; /*error*/
 	}
@@ -276,7 +278,7 @@ static int dump_pagetable(struct seq_file *s)
 
 	seq_printf(s, "(%s) x86_phys_bits %hu\n", boot_cpu_data.x86_vendor_id, boot_cpu_data.x86_phys_bits);
 
-	if(boot_cpu_data.x86_phys_bits != 36){
+	if (boot_cpu_data.x86_phys_bits != 36) {
 		seq_printf(s, "MAXPHYADDR should usually be 36 but is is %u\n", boot_cpu_data.x86_phys_bits);
 	}
 	state.maxphyaddr = boot_cpu_data.x86_phys_bits;
@@ -288,27 +290,26 @@ static int dump_pagetable(struct seq_file *s)
 	__asm__ __volatile__ ("mov %%cr3, %%rax \n mov %%rax,%0": "=m" (cr3) : /*InputOperands*/ : "rax");
 	__asm__ __volatile__ ("mov %%cr4, %%rax \n mov %%rax,%0": "=m" (cr4) : /*InputOperands*/ : "rax");
 
-	if(rdmsrl_safe(MSR_EFER, &ia32_efer)){
+	if (rdmsrl_safe(MSR_EFER, &ia32_efer)) {
 		seq_printf(s, "Error reading MSR\n");
 		return 1;
 	}
 
-	if(!(ia32_efer & EFER_NX)){
+	if (!(ia32_efer & EFER_NX)) {
 		seq_printf(s, "No IA32_EFER.NXE?????\n");
 	}
 
 
 	seq_printf(s, "cr3: 0x%llx\n", cr3);
-	if(cr3 & 0xfe7 /*ignored*/
-	   || (cr3 & (0xffffffffffffffffLL << state.maxphyaddr) /*reserved*/)){
+	if (cr3 & 0xfe7 /*ignored*/ || (cr3 & (0xffffffffffffffffLL << state.maxphyaddr) /*reserved*/))
 		seq_printf(s, "cr3 looks shady\n");
-	}
-	if(cr3 & X86_CR3_PWT || cr3 & X86_CR3_PCD){
+
+	if (cr3 & X86_CR3_PWT || cr3 & X86_CR3_PCD)
 		seq_printf(s, "unexpected options in cr3\n");
-	}
-	if(cr0 & X86_CR0_PG  &&  cr4 & X86_CR4_PAE  &&  ia32_efer & EFER_LME  &&  !(cr4 & X86_CR4_PCIDE)){
+
+	if (cr0 & X86_CR0_PG  &&  cr4 & X86_CR4_PAE  &&  ia32_efer & EFER_LME  &&  !(cr4 & X86_CR4_PCIDE))
 		seq_printf(s, "paging according to Tab 4-12 p. 2783 intel dev manual (March 2017)\n");
-	}else{
+	else {
 		/**
 		 *	Here
 		 *	2021.11.09 Rong Tao
@@ -316,34 +317,34 @@ static int dump_pagetable(struct seq_file *s)
 		seq_printf(s, "unknown paging setup\n");
 		return -EPERM; /*I'm afraid I can't do that*/
 	}
-	if(!(cr4 & X86_CR4_PKE)){
+	if (!(cr4 & X86_CR4_PKE))
 		seq_printf(s, "No protection keys enabled (this is normal)\n");
-	}
 
 	state.pml4 = phys_to_virt(cr3);
 	seq_printf(s, "page table in virtual memory at %p\n", state.pml4);
-	if(!virt_addr_valid(state.pml4) || !IS_ALIGNED(cr3, 4096)){
+	if (!virt_addr_valid(state.pml4) || !IS_ALIGNED(cr3, 4096)) {
 		seq_printf(s, "invalid addr!\n");
 		return 1; /*error*/
 	}
 
 	// 4 nested for loops to walk 4 levels of pages
 	// walk the outermost page table
-	for(state.pml4_i = 0; state.pml4_i < 512; ++state.pml4_i){
-		if(dump_entry(s, &state, PML4)){
+	for (state.pml4_i = 0; state.pml4_i < 512; ++state.pml4_i) {
+		if (dump_entry(s, &state, PML4)) {
 			// walk next level
 			state.pdpt = next_page_table_vaddr((u64)state.pml4->entry[state.pml4_i]);
-			for(state.pdpt_i = 0; state.pdpt_i < 512; ++state.pdpt_i){
-				if(dump_entry(s, &state, PDPT)){
+			for (state.pdpt_i = 0; state.pdpt_i < 512; ++state.pdpt_i) {
+				if (dump_entry(s, &state, PDPT)) {
 					continue; //README says we just dump two levels
 					// walk next level
 					state.pd = next_page_table_vaddr((u64)state.pdpt->entry[state.pdpt_i]);
-					for(state.pd_i = 0; state.pd_i < 512; ++state.pd_i){
-						if(dump_entry(s, &state, PD)){
+					for (state.pd_i = 0; state.pd_i < 512; ++state.pd_i) {
+						if (dump_entry(s, &state, PD)) {
 							//print final level here
 							state.pt = next_page_table_vaddr((u64)state.pd->entry[state.pd_i]);
-							for(state.pt_i = 0; state.pt_i < 512; ++state.pt_i){
-								CORNY_ASSERT(!dump_entry(s, &state, PT)); //we cannot go deeper!
+							for (state.pt_i = 0; state.pt_i < 512; ++state.pt_i) {
+								//we cannot go deeper!
+								CORNY_ASSERT(!dump_entry(s, &state, PT));
 							};
 							// reset pt entries in state for assertions
 							state.pt = NULL;
@@ -370,9 +371,8 @@ static int pagetbl_seq_show(struct seq_file *s, void *v)
 {
         seq_printf(s, "dumping all in one go. yolo.\n");
 	int ret = dump_pagetable(s);
-	if(ret){
+	if (ret)
 		seq_printf(s, "Something went wrong, errror %d\n", ret);
-	}
         return 0;
 }
 
@@ -408,7 +408,6 @@ fail:
 	debugfs_remove_recursive(pagetbl_debug_root);
 	return -ENOMEM;
 }
-module_init(test_module_init);
 
 static void __exit test_module_exit(void)
 {
@@ -416,7 +415,7 @@ static void __exit test_module_exit(void)
 	debugfs_remove_recursive(pagetbl_debug_root);
 }
 
+module_init(test_module_init);
 module_exit(test_module_exit);
-
 MODULE_AUTHOR("Cornelius Diekmann");
 MODULE_LICENSE("GPL");
