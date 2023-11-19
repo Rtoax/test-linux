fdee7894d4b40c417bb0e7210240c95e47283c98
Rong Tao <rtoax@foxmail.com>
2023-11-19 17:27:25 +0800

    syscall/sample/mm: Add brk() sbrk()
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ syscall/samples/mm/sbrk.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+int main(void)
+{
+	char *pmem;
+	int i;
+	void *sbrkret;
+
+	pmem = (char *)malloc(32);
+	if (pmem == NULL) {
+		perror("malloc");
+		exit (EXIT_FAILURE);
+	}
+
+	printf("pmem = %p\n", pmem);
+
+	sbrkret = sbrk(0);
+	if (sbrkret != (void *)-1)
+		printf("heap size on each load: %lu\n", (long)sbrkret - (long)pmem);
+
+	for (i = 0; i < 8; i++) {
+		printf("sbrkret = %p\n", sbrk(1));
+	}
+	free(pmem);
+
+	return 0;
+}
+
