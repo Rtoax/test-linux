d2d14113623a7365d1f2dfb04242a011fb8b383b
Rong Tao <rtoax@foxmail.com>
2023-11-20 20:16:52 +0800

    syscall/samples: Add acct.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ syscall/samples/acct.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+/* sa -  summarizes accounting information */
+
+int main(int argc, char *argv[])
+{
+	int ret = acct(argv[1]);
+	printf("acct: ret = %d, %s\n",ret, strerror(errno));
+	return 0;
+}
