bd448d181d076f48cc6ca5e363693cf47bfc2205
Rong Tao <rtoax@foxmail.com>
2023-11-20 21:09:32 +0800

    syscall/samples: Add fchdir, fchown
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ syscall/samples/fchdir.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+int main(void)
+{
+	int fd = open("/home/", O_RDONLY);
+	fchdir(fd);
+	printf("%s\n", (char *)getcwd(NULL, 100));
+	close(fd);
+	return 0;
+}
---
+++ syscall/samples/fchown.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h> /*S_IRUSR*/
+
+
+int main(int argc, char *argv[])
+{
+	int fd = open(argv[1], O_RDONLY);
+	fchown(fd, S_ISUID, S_ISGID);
+	close(fd);
+	return 0;
+}
