3f26cdbd1ee44e504b7f5f3df61602f2ccdb8e29
Rong Tao <rtoax@foxmail.com>
2023-11-20 20:30:46 +0800

    sysconf: Add confstr.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- glibc/unistd/sysconf/sysconf.c
+++ glibc/unistd/sysconf/sysconf.c
@@ -2,9 +2,22 @@
 #include <unistd.h>
 
 
+const char *confstr_unsafe(int conf)
+{
+	int ret;
+	size_t len = confstr(conf, NULL, 0);
+	static char buffer[256];
+
+	memset(buffer, 0, sizeof(buffer));
+	ret = confstr(conf, buffer, len + 1);
+	if (ret == 0)
+		return "Unknown";
+	return buffer;
+}
+
 int main(void)
 {
-#define SYSCONF(c) printf("%-48s %-8d %-8ld\n", #c, c, sysconf(c));
+#define SYSCONF(c) printf("%-48s %-8d %-8ld %s\n", #c, c, sysconf(c), confstr_unsafe(c));
 #include "confs_def.h"
 #undef SYSCONF
 	return 0;
