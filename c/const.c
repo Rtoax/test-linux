bd4b1e3610febd3d9ce7cc950275094d04d5cda0
Rong Tao <rongtao@cestc.cn>
2023-11-25 17:44:35 +0800

    c: Add const.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ c/const.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define __unused __attribute__((unused))
+
+
+int main(void)
+{
+	const char __unused *s1 = "rongtao";
+	const char __unused const *s2 = "rongtao";
+
+	s1 = "hello";
+	/* Forbidden */
+	//s2 = "hello";
+
+	return 0;
+}
