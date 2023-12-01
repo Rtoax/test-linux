c58c990e62c82c6b7f496c2f3528c805da5880d6
Rong Tao <rongtao@cestc.cn>
2023-12-01 19:14:14 +0800

    device: Add query.sh
        $ ./query.sh
        DEV                      PATH
        /dev/nvme0               /sys/devices/pci0000:00/0000:00:1d.0/0000:3a:00.0/nvme/nvme0
        /dev/nvme0n1             /sys/devices/pci0000:00/0000:00:1d.0/0000:3a:00.0/nvme/nvme0/nvme0n1
        /dev/nvme0n1p1           /sys/devices/pci0000:00/0000:00:1d.0/0000:3a:00.0/nvme/nvme0/nvme0n1/nvme0n1p1
        /dev/nvme0n1p2           /sys/devices/pci0000:00/0000:00:1d.0/0000:3a:00.0/nvme/nvme0/nvme0n1/nvme0n1p2
        /dev/nvme0n1p3           /sys/devices/pci0000:00/0000:00:1d.0/0000:3a:00.0/nvme/nvme0/nvme0n1/nvme0n1p3
        /dev/nvme0n1p4           /sys/devices/pci0000:00/0000:00:1d.0/0000:3a:00.0/nvme/nvme0/nvme0n1/nvme0n1p4
        /dev/nvme0n1p5           /sys/devices/pci0000:00/0000:00:1d.0/0000:3a:00.0/nvme/nvme0/nvme0n1/nvme0n1p5
        /dev/sda                 /sys/devices/pci0000:00/0000:00:17.0/ata3/host2/target2:0:0/2:0:0:0/block/sda
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ device/query.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+declare -a devices pathes
+
+devices=( $(ls /dev/ | grep -e sd[a-z]* -e vd[a-z]* -e nvme*) )
+
+for dev in ${devices[@]}
+do
+	pathes+=( /sys$(udevadm info --query path --name /dev/${dev}) )
+done
+
+printf "%-24s %s\n" DEV PATH
+for ((i = 0; i < ${#devices[@]}; i++))
+do
+	printf "%-24s %s\n" /dev/${devices[$i]} ${pathes[$i]}
+done
