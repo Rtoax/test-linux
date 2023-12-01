277ac8c05f21f45c12b3cb18dbcc3f2404a79166
Rong Tao <rongtao@cestc.cn>
2023-12-01 17:40:59 +0800

    lvm: list.sh: Add vgs command
    - see kernel.spec
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- storage/lvm/scripts/list.sh
+++ storage/lvm/scripts/list.sh
@@ -2,7 +2,10 @@
 
 echo -------- Volume Group ---------
 sudo vgs
+sudo vgs --options vg_uuid,pv_name --noheadings --separator :
+echo
 echo -------- Physical Volume ---------
 sudo pvs
+echo
 echo -------- Logical Volume  ---------
 sudo lvs
