4aad3a1fa4846aedb11e467bcad56ac28d46d8ef
Rong Tao <rtoax@foxmail.com>
2023-12-02 14:09:07 +0800

    compiler/reloc: Add reloc-types.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ compiler/reloc/reloc-types.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+ * x86_64: R_X86_64_64
+ */
+int gin;
+int gii = 1;
+static int gsin;
+static int gsii = 1;
+void *gp;
+
+int main(void)
+{
+	return 0;
+}
