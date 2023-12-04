9dd9164b3bad66ad3182766116e01235b252bd97
Rong Tao <rongtao@cestc.cn>
2023-12-04 09:41:54 +0800

    glibc/string: memset: Add tl_memset()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/string/memset.c
+++ glibc/string/memset.c
@@ -1,4 +1,16 @@
 #include <string.h>
+#include <stdint.h>
+#include <sys/types.h>
+
+
+void *tl_memset(void *s, int c, size_t n)
+{
+	size_t i;
+	uint8_t *si = s;
+	for (i = 0; i < n; i++)
+		si[i] = c;
+	return s;
+}
 
 int main(void)
 {
