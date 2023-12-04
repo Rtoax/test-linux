c66914f15df7ef6239697708f5bf4815f4f0d68e
Rong Tao <rtoax@foxmail.com>
2023-12-04 21:52:37 +0800

    modules/nothing/reloc.c: ksym is not RELA
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- module/nothing/reloc.c
+++ module/nothing/reloc.c
@@ -1,4 +1,5 @@
 #include <linux/module.h>
+#include <linux/kthread.h>
 
 int global_int1;
 int global_int2;
@@ -6,6 +7,9 @@ int global_int3 = 0xabcdffff;
 static int sglobal_int1;
 
 void func1(void) {}
-static void sfunc1(void) {}
+static void sfunc1(void) {
+	/* This is not RELA item. */
+	kthread_run(NULL, NULL, NULL);
+}
 
 MODULE_LICENSE("Dual BSD/GPL");
