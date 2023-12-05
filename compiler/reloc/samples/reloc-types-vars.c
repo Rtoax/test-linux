e5a20ff8ad488320daf0dfa9935fc632fe088fbe
Rong Tao <rongtao@cestc.cn>
2023-12-05 11:13:05 +0800

    relocs: aarch64: R_AARCH64_ADR_PRE + R_AARCH64_ADD_ABS
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/reloc-types-vars.c
+++ compiler/reloc/samples/reloc-types-vars.c
@@ -3,14 +3,17 @@ static int static_global_init_int = 1;
 /* .bss */
 static int static_global_uninit_int;
 static int static_global_uninit_int2;
+static int static_global_uninit_int3;
 
 static int func(void)
 {
 	/**
 	 * x86_64: R_X86_64_PC32
+	 * aarch64: R_AARCH64_ADR_PRE + R_AARCH64_ADD_ABS
 	 */
 	static_global_init_int = 2;
 	static_global_uninit_int = 3;
 	static_global_uninit_int2 = 3;
+	static_global_uninit_int3 = 3;
 	return 0;
 }
