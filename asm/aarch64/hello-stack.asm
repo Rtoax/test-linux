44cbf4dec438f539dc91d19b6058eba38bc2c43b
Rong Tao <rongtao@cestc.cn>
2023-12-05 20:45:32 +0800

    syscall/samples: Add write.c
    todo: asm/aarch64/hello-stack.asm
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ asm/aarch64/hello-stack.asm
@@ -0,0 +1,31 @@
+
+.text
+.globl _start
+_start:
+	stp	x29, x30, [sp, #-32]!
+	mov	x29, sp
+	str	xzr, [sp, #16]
+	mov	w0, #0x48                  	// #72
+	strb	w0, [sp, #16]
+	mov	w0, #0x65                  	// #101
+	strb	w0, [sp, #17]
+	mov	w0, #0x6c                  	// #108
+	strb	w0, [sp, #18]
+	mov	w0, #0x6c                  	// #108
+	strb	w0, [sp, #19]
+	mov	w0, #0x6f                  	// #111
+	strb	w0, [sp, #20]
+	mov	w0, #0xa                   	// #10
+	strb	w0, [sp, #21]
+	add	x0, sp, #0x10
+	str	x0, [sp, #24]
+
+	mov     x0, #1      /* fd := STDOUT_FILENO */
+	ldr     x1, [sp, #24]    /* buf := msg */
+	ldr     x2, #0x8    /* count := len */
+	mov     w8, #64     /* write is syscall #64 */
+	svc     #0          /* invoke syscall */
+
+	mov	w0, #0x0                   	// #0
+	ldp	x29, x30, [sp], #32
+	ret
---
+++ syscall/samples/write.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(void)
+{
+	union {
+		unsigned long l;
+		char c[8];
+	} hello = {
+		.c[0] = 'H',
+		.c[1] = 'e',
+		.c[2] = 'l',
+		.c[3] = 'l',
+		.c[4] = 'o',
+		.c[5] = '\n',
+	};
+
+	char *s = (void *)&hello.l;
+	write(1, s, 8);
+	return 0;
+}
