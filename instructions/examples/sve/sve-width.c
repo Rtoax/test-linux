e03bbd99732b6cf13267e416964279d75a763776
Rong Tao <rongtao@cestc.cn>
2023-12-06 13:35:47 +0800

    sve: Fix errors
    Link: https://github.com/ctyler/sve2-test
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/sve/sve-width.c
+++ instructions/examples/sve/sve-width.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <arm_sve.h>
 
 
 int main(void)
 {
-        uint64_t lanes;
+	uint64_t lanes;
 
-        __asm__ __volatile__("CNTB %[lanes]" : [lanes]"=r"(lanes) : : );
-        printf("SVE vector width is %d bytes or %d bits.\n", lanes, lanes * 8);
+	__asm__ __volatile__("CNTB %[lanes]" : [lanes]"=r"(lanes) : : );
+	printf("SVE vector width is %d bytes or %d bits.\n", lanes, lanes * 8);
 
-        lanes = svcntb();
-        printf("SVE vector width is %d bytes or %d bits.\n", lanes, lanes * 8);
+	lanes = svcntb();
+	printf("SVE vector width is %d bytes or %d bits.\n", lanes, lanes * 8);
 
-        return 0;
+	return 0;
 }
