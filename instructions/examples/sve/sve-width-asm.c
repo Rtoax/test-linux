857ca3577fdc64ec772d98460c5b22cc2bcfeba2
Rong Tao <2386499836@qq.com>
2023-12-06 04:55:12 +0000

    add instructions/examples/sve/sve-width-asm.c.
    Signed-off-by: Rong Tao <2386499836@qq.com>

---
+++ instructions/examples/sve/sve-width-asm.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void)
+{
+        uint64_t lanes;
+        __asm__ __volatile__("CNTB %[lanes]" : [lanes]"=r"(lanes) : : );
+        printf("SVE vector width is %d bytes or %d bits.\n", lanes, lanes * 8);
+        return 0;
+}
