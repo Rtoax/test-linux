e4af287cc3d131d857302db37105809aa97998ae
Rong Tao <rongtao@cestc.cn>
2023-12-12 15:35:56 +0800

    gdb/samples: Add daxpy.c
    - PDF <Vector Processing with Arm SVE>
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ gdb/samples/daxpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void daxpy(double *x, double *y, double a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		y[i] = a * x[i] + y[i];
+}
+
+int main(void)
+{
+	double x[] = {1, 2, 3, 4, 5};
+	double y[] = {1, 2, 3, 4, 5};
+	daxpy(x, y, 3, 5);
+	return 0;
+}
+
