95f0c842b6891b3b17a9ebd323598ab0f91ef193
Rong Tao <rongtao@cestc.cn>
2023-12-13 15:00:03 +0800

    neon: Add more test vaddq_s64.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/neon/vaddq_s64.c
+++ instructions/examples/neon/vaddq_s64.c
@@ -5,17 +5,20 @@
 int main(void)
 {
 	size_t i;
-	double Val1[2] = { 2.46574621, 0.46546221 };
-	double Val2[2] = { 2.63565654, 0.46574621 };
-	double Sum[2] = { 0.0, 0.0 };
-	double Sum_C[2] = { 0.0, 0.0 };
+	double Val1[4] = { 2.46574621, 0.46546221, 3.14, 1.23 };
+	double Val2[4] = { 2.63565654, 0.46574621, 3.14, 1.23 };
+	double Sum[4] = { 0.0, 0.0, 0.0, 0.0 };
+	double Sum_2[4] = { 0.0, 0.0, 0.0, 0.0 };
+	double Sum_C[4] = { 0.0, 0.0, 0.0, 0.0 };
 
-	float64x2_t f1 = vld1q_f64(&(Val1[0]));
-	float64x2_t f2 = vld1q_f64(&(Val2[0]));
-	float64x2_t sum = vaddq_f64(f1, f2);
-	vst1q_f64(Sum, sum);
+	for (i = 0; i < 4; i += 2) {
+		float64x2_t f1 = vld1q_f64(&(Val1[i]));
+		float64x2_t f2 = vld1q_f64(&(Val2[i]));
+		float64x2_t sum = vaddq_f64(f1, f2);
+		vst1q_f64(&Sum[i], sum);
+	}
 
-	for (i = 0; i < 2; i++) {
+	for (i = 0; i < 4; i++) {
 		Sum_C[i] = Val1[i] + Val2[i];
 		if (Sum_C[i] != Sum[i])
 			printf("[Error]   Sum %lf != %lf\n", Sum[i], Sum_C[i]);
--- instructions/examples/sve/neon-sve.c
+++ instructions/examples/sve/neon-sve.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <time.h>
 #include <sys/time.h>
+#include <arm_neon.h>
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
@@ -13,6 +14,7 @@ struct test {
 	test_fn_t fn;
 	unsigned long spent_us;
 	double *x, *y;
+	int cmp;
 };
 
 
@@ -23,15 +25,22 @@ static inline unsigned long usecs(void)
 	return tv.tv_sec * 1000000UL + tv.tv_usec;
 }
 
-void daxpy_c(double *x, double *y, double a, size_t n)
+void c_X_x_Y(double *x, double *y, double a, size_t n)
 {
 	size_t i;
 	for (i = 0; i < n; i++)
-		y[i] = a * x[i] + y[i];
+		y[i] = x[i] * y[i];
 }
 
-void daxpy_neon(double *x, double *y, double a, size_t n)
+void neon_X_x_Y(double *x, double *y, double a, size_t n)
 {
+	size_t i;
+	for (i = 0; i < n; i += 2) {
+		float64x2_t xi = vld1q_f64(&x[i]);
+		float64x2_t yi = vld1q_f64(&y[i]);
+		float64x2_t sum = vmulq_f64(xi, yi);
+		vst1q_f64(&y[i], sum);
+	}
 }
 
 void init_arr(double *arr, size_t n)
@@ -41,17 +50,35 @@ void init_arr(double *arr, size_t n)
 		arr[i] = i;
 }
 
+int cmp_arr(double *x, double *y, size_t n)
+{
+	size_t i;
+	for (i = 0; i < n; i++)
+		if (x[i] != y[i])
+			return 1;
+	return 0;
+}
+
 struct test tests[] = {
-	{ "C", daxpy_c, 0, },
-	{ "Neon", daxpy_neon, 0, },
+	{
+		.name = "   C: y[i] = x[i] * y[i]",
+		.fn = c_X_x_Y,
+		.spent_us = 0,
+	},
+	{
+		.name = "Neon: y[i] = x[i] * y[i]",
+		.fn = neon_X_x_Y,
+		.spent_us = 0,
+	},
 };
 
 int main(int argc, char *argv[])
 {
 	int i;
-	size_t n = 100000000;
+	size_t n = 10000000000;
 	double a = 1.1;
 
+	struct test *t_base = &tests[0];
 
 	for (i = 0; i < ARRAY_SIZE(tests); i++) {
 		unsigned long start;
@@ -66,6 +93,19 @@ int main(int argc, char *argv[])
 		start = usecs();
 		t->fn(t->x, t->y, a, n);
 		t->spent_us = usecs() - start;
+
+		if (i != 0)
+			t->cmp = cmp_arr(t_base->y, t->y, n);
+		else
+			t->cmp = 0;
+	}
+
+
+	printf("%-32s %-16s %-8s\n", "TEST_NAME", "SPENT(us)", "RSLT");
+
+	for (i = 0; i < ARRAY_SIZE(tests); i++) {
+		struct test *t = &tests[i];
+		printf("%-32s %-16ld %-8s\n", t->name, t->spent_us, t->cmp ? "Failed" : "Passed");
 	}
 
 	for (i = 0; i < ARRAY_SIZE(tests); i++) {
@@ -73,6 +113,5 @@ int main(int argc, char *argv[])
 		free(t->x);
 		free(t->y);
 	}
-
 	return 0;
 }
