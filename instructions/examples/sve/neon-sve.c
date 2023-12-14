c69f9101392a15171ccc5fa69a44d842d6e96bdc
Rong Tao <rongtao@cestc.cn>
2023-12-14 09:35:12 +0800

    neon-sve.c: Good struct test
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/sve/neon-sve.c
+++ instructions/examples/sve/neon-sve.c
@@ -10,18 +10,51 @@
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
-typedef void (*test_u8_fn_t)(uint8_t *x, uint8_t *y, size_t n);
-typedef void (*test_fn_t)(double *x, double *y, size_t n);
+#define DEFINE_CMP_FUNC(funcname, type) \
+int funcname(void *x, void *y, size_t n) {	\
+	int __ret = 0;	\
+	size_t ___i;	\
+	type *___x = x;	\
+	type *___y = y;	\
+	printf("call %s\n", #funcname);	\
+	for (___i = 0; ___i < n; ___i++)	\
+		if (___x[___i] != ___y[___i]) {	\
+			fprintf(stderr, "F:%ld\n", ___i);	\
+			__ret = 1;	\
+			break;	\
+		}	\
+	return __ret;	\
+}
+
+#define DEFINE_INIT_FUNC(funcname, type)	\
+void funcname(void *array, size_t n) {	\
+	size_t i;	\
+	type *arr = array;	\
+	printf("call %s\n", #funcname);	\
+	for (i = 0; i < n; i++)	\
+		arr[i] = i;	\
+}
+
+DEFINE_CMP_FUNC(cmp_arr_u8, uint8_t)
+DEFINE_CMP_FUNC(cmp_arr_f64, double)
+
+DEFINE_INIT_FUNC(init_arr_u8, uint8_t)
+DEFINE_INIT_FUNC(init_arr_f64, double)
+
+typedef void (*test_fn_t)(void *x, void *y, size_t n);
+typedef int (*cmp_fn_t)(void *x, void *y, size_t n);
+typedef void (*init_fn_t)(void *x, size_t n);
 
 struct test {
 	const char *name;
-	union {
-		test_u8_fn_t fn_u8;
-		test_fn_t fn_double;
-	};
+	test_fn_t fn;
+	cmp_fn_t cmp;
+	init_fn_t init;
+	ssize_t elem_size;
 	unsigned long spent_us;
 	void *x, *y;
-	int cmp;
+	struct test *cmp_with;
+	int cmp_rslt;
 };
 
 
@@ -32,23 +65,29 @@ static inline unsigned long usecs(void)
 	return tv.tv_sec * 1000000UL + tv.tv_usec;
 }
 
-void double_c_X_x_Y(double *x, double *y, size_t n)
+void double_c_X_x_Y(void *_x, void *_y, size_t n)
 {
 	size_t i;
+	double *x = _x;
+	double *y = _y;
 	for (i = 0; i < n; i++)
 		y[i] = x[i] * y[i];
 }
 
-void u8_c_X_x_Y(uint8_t *x, uint8_t *y, size_t n)
+void u8_c_X_x_Y(void *_x, void *_y, size_t n)
 {
 	size_t i;
+	uint8_t *x = _x;
+	uint8_t *y = _y;
 	for (i = 0; i < n; i++)
 		y[i] = x[i] * y[i];
 }
 
-void double_neon_X_x_Y(double *x, double *y, size_t n)
+void double_neon_X_x_Y(void *_x, void *_y, size_t n)
 {
 	size_t i;
+	double *x = _x;
+	double *y = _y;
 	for (i = 0; i < n; i += 2) {
 		float64x2_t xi = vld1q_f64(&x[i]);
 		float64x2_t yi = vld1q_f64(&y[i]);
@@ -57,9 +96,11 @@ void double_neon_X_x_Y(double *x, double *y, size_t n)
 	}
 }
 
-void u8_neon_X_x_Y(uint8_t *x, uint8_t *y, size_t n)
+void u8_neon_X_x_Y(void *_x, void *_y, size_t n)
 {
 	size_t i;
+	uint8_t *x = _x;
+	uint8_t *y = _y;
 	for (i = 0; i < n; i += 8) {
 		uint8x8_t xi = vld1_u8(x + i);
 		uint8x8_t yi = vld1_u8(y + i);
@@ -69,9 +110,11 @@ void u8_neon_X_x_Y(uint8_t *x, uint8_t *y, size_t n)
 }
 
 #if !defined(CONFIG_NO_SVE)
-void u8_sve_X_x_Y(uint8_t *x, uint8_t *y, size_t n)
+void u8_sve_X_x_Y(void *_x, void *_y, size_t n)
 {
 	size_t i;
+	uint8_t *x = _x;
+	uint8_t *y = _y;
 	size_t vl = svcntb();
 	printf("SVE lane %ld\n", vl);
 
@@ -84,9 +127,11 @@ void u8_sve_X_x_Y(uint8_t *x, uint8_t *y, size_t n)
 	}
 }
 
-void double_sve_X_x_Y(double *x, double *y, size_t n)
+void double_sve_X_x_Y(void *_x, void *_y, size_t n)
 {
 	size_t i;
+	double *x = _x;
+	double *y = _y;
 	size_t vl = svcntb();
 	printf("SVE lane %ld\n", vl);
 
@@ -101,63 +146,61 @@ void double_sve_X_x_Y(double *x, double *y, size_t n)
 }
 #endif
 
-#define init_arr(type, array, n) do {	\
-	size_t i;	\
-	type *arr = array;	\
-	for (i = 0; i < n; i++)	\
-		arr[i] = i;	\
-} while (0)
-
-#define cmp_arr(type, x, y, n) ({	\
-	int __ret = 0;	\
-	size_t ___i;	\
-	type *___x = x;	\
-	type *___y = y;	\
-	for (___i = 0; ___i < n; ___i++)	\
-		if (___x[___i] != ___y[___i]) {	\
-			fprintf(stderr, "F:%ld\n", ___i);	\
-			__ret = 1;	\
-			break;	\
-		}	\
-	__ret;	\
-})
-
-struct test tests_u8[] = {
-	{
+struct test tests[] = {
+	[0] = {
 		.name = "   C: y[i] = x[i] * y[i] (u8)",
-		.fn_u8 = u8_c_X_x_Y,
+		.fn = u8_c_X_x_Y,
+		.init = init_arr_u8,
+		.cmp = cmp_arr_u8,
+		.elem_size = sizeof(uint8_t),
 		.spent_us = 0,
+		.cmp_with = NULL,
+	},
+	[1] = {
+		.name = "   C: y[i] = x[i] * y[i] (f64)",
+		.fn = double_c_X_x_Y,
+		.init = init_arr_f64,
+		.cmp = cmp_arr_f64,
+		.elem_size = sizeof(double),
+		.spent_us = 0,
+		.cmp_with = NULL,
 	},
 	{
 		.name = "Neon: y[i] = x[i] * y[i] (u8)",
-		.fn_u8 = u8_neon_X_x_Y,
+		.fn = u8_neon_X_x_Y,
+		.init = init_arr_u8,
+		.cmp = cmp_arr_u8,
+		.elem_size = sizeof(uint8_t),
 		.spent_us = 0,
+		.cmp_with = &tests[0],
+	},
+	{
+		.name = "Neon: y[i] = x[i] * y[i] (f64)",
+		.fn = double_neon_X_x_Y,
+		.init = init_arr_f64,
+		.cmp = cmp_arr_f64,
+		.elem_size = sizeof(double),
+		.spent_us = 0,
+		.cmp_with = &tests[1],
 	},
 #if !defined(CONFIG_NO_SVE)
 	{
 		.name = " Sve: y[i] = x[i] * y[i] (u8)",
-		.fn_u8 = u8_sve_X_x_Y,
+		.fn = u8_sve_X_x_Y,
+		.init = init_arr_u8,
+		.cmp = cmp_arr_u8,
+		.elem_size = sizeof(uint8_t),
 		.spent_us = 0,
+		.cmp_with = &tests[0],
 	},
-#endif
-};
-
-struct test tests_double[] = {
-	{
-		.name = "   C: y[i] = x[i] * y[i] (f64)",
-		.fn_double = double_c_X_x_Y,
-		.spent_us = 0,
-	},
-	{
-		.name = "Neon: y[i] = x[i] * y[i] (f64)",
-		.fn_double = double_neon_X_x_Y,
-		.spent_us = 0,
-	},
-#if !defined(CONFIG_NO_SVE)
 	{
 		.name = " Sve: y[i] = x[i] * y[i] (f64)",
-		.fn_double = double_sve_X_x_Y,
+		.fn = double_sve_X_x_Y,
+		.init = init_arr_f64,
+		.cmp = cmp_arr_f64,
+		.elem_size = sizeof(double),
 		.spent_us = 0,
+		.cmp_with = &tests[1],
 	},
 #endif
 };
@@ -167,72 +210,39 @@ int main(int argc, char *argv[])
 	int i;
 	size_t n = 10000000;
 
-	struct test *t_double_base = &tests_double[0];
-	struct test *t_u8_base = &tests_u8[0];
-
-	for (i = 0; i < ARRAY_SIZE(tests_double); i++) {
+	for (i = 0; i < ARRAY_SIZE(tests); i++) {
 		unsigned long start;
-		struct test *t = &tests_double[i];
+		struct test *t = &tests[i];
+		struct test *t_cmp = t->cmp_with;
 
 		printf("Testing %s\n", t->name);
 
-		t->x = malloc(sizeof(double) * n);
-		t->y = malloc(sizeof(double) * n);
+		t->x = malloc(t->elem_size * n);
+		t->y = malloc(t->elem_size * n);
 
-		init_arr(double, t->x, n);
-		init_arr(double, t->y, n);
-
-		start = usecs();
-		t->fn_double(t->x, t->y, n);
-		t->spent_us = usecs() - start;
-
-		if (i != 0)
-			t->cmp = cmp_arr(double, t_double_base->y, t->y, n);
-		else
-			t->cmp = 0;
-	}
-	for (i = 0; i < ARRAY_SIZE(tests_u8); i++) {
-		unsigned long start;
-		struct test *t = &tests_u8[i];
-
-		printf("Testing %s\n", t->name);
-
-		t->x = malloc(sizeof(uint8_t) * n);
-		t->y = malloc(sizeof(uint8_t) * n);
-
-		init_arr(uint8_t, t->x, n);
-		init_arr(uint8_t, t->y, n);
+		t->init(t->x, n);
+		t->init(t->y, n);
 
 		start = usecs();
-		t->fn_u8(t->x, t->y, n);
+		t->fn(t->x, t->y, n);
 		t->spent_us = usecs() - start;
 
-		if (i != 0)
-			t->cmp = cmp_arr(uint8_t, t_u8_base->y, t->y, n);
+		if (t_cmp)
+			t->cmp_rslt = t->cmp(t_cmp->y, t->y, n);
 		else
-			t->cmp = 0;
+			t->cmp_rslt = 0;
 	}
-
 
 	printf("Length of array %ld\n", n);
 	printf("%-50s %-16s %-8s\n", "TEST_NAME", "SPENT(us)", "RSLT");
 
-	for (i = 0; i < ARRAY_SIZE(tests_double); i++) {
-		struct test *t = &tests_double[i];
-		printf("%-50s %-16ld %-8s\n", t->name, t->spent_us, t->cmp ? "Failed" : "Passed");
-	}
-	for (i = 0; i < ARRAY_SIZE(tests_u8); i++) {
-		struct test *t = &tests_u8[i];
-		printf("%-50s %-16ld %-8s\n", t->name, t->spent_us, t->cmp ? "Failed" : "Passed");
+	for (i = 0; i < ARRAY_SIZE(tests); i++) {
+		struct test *t = &tests[i];
+		printf("%-50s %-16ld %-8s\n", t->name, t->spent_us, t->cmp_rslt ? "Failed" : "Passed");
 	}
 
-	for (i = 0; i < ARRAY_SIZE(tests_double); i++) {
-		struct test *t = &tests_double[i];
-		free(t->x);
-		free(t->y);
-	}
-	for (i = 0; i < ARRAY_SIZE(tests_u8); i++) {
-		struct test *t = &tests_u8[i];
+	for (i = 0; i < ARRAY_SIZE(tests); i++) {
+		struct test *t = &tests[i];
 		free(t->x);
 		free(t->y);
 	}
