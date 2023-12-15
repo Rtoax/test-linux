db1f21469423f949b7b3168dfc8f962b7d371682
Rong Tao <rongtao@cestc.cn>
2023-12-14 21:56:58 -0500

    asm: sve: SEGV
    gdb layout asm:
    
    │B+ 0x4006c4 <daxpy>        ldrsw   x3, [x3]                       │
    │   0x4006c8 <daxpy+4>      mov     x4, #0x0             // #0     │
    │  >0x4006cc <daxpy+8>      ldr     d0, [x2]                       │
    │   0x4006d0 <daxpy+12>     b       0x4006e8 <.latch2>             │
    │   0x4006d4 <.loop2>       ldr     d1, [x0, x4, lsl #3]
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- asm/aarch64/daxpy-c.c
+++ asm/aarch64/daxpy-c.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
 void daxpy(double *x, double *y, double a, int n);
 void daxpy_sve(double *x, double *y, double a, int n);
 
 int main(void)
 {
-	double x[] = {1, 2, 3, 4};
-	double y[] = {1, 2, 3, 4};
-	daxpy(x, y, 2, 3);
+#define ELEMS 1,2,3,4,5,6,7,8,9,0,
+	int i;
+	double x[] = {ELEMS};
+	double y[] = {ELEMS};
+
+	daxpy(x, y, 2, ARRAY_SIZE(x));
+
+	for (i = 0; i < ARRAY_SIZE(x); i++) {
+		printf("%lf,", y[i]);
+	} printf("\n");
 	return 0;
 }
