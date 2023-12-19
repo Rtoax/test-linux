e8a62b51d3c0d600105d6eb6ed3865bb35cf5774
Rong Tao <rongtao@cestc.cn>
2023-12-19 01:18:41 -0500

    avx: Add mul-avx512.c
    Link: https://github.com/zenny-chen/Intel-AVX512-Brief-Introduction
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ instructions/examples/avx/mul-avx512.c
@@ -0,0 +1,55 @@
+#include <stdalign.h>
+#include <immintrin.h>
+
+
+int main(void)
+{
+	int i, len = 3200;
+
+	/* Dynamic memory allocation with 64-byte alignment */
+	float* pInVector = (float *)_mm_malloc(len * sizeof(float), 64);
+	float* pOutVector = (float *)_mm_malloc(len * sizeof(float), 64);
+
+	/* init data */
+	for (i = 0; i < len; i++)
+		pInVector[i] = 1;
+
+	float cos_teta = 0.8660254037;
+	float sin_teta = 0.5;
+
+	/* Static memory allocation of 16 floats with 64-byte alignments */
+	alignas(64) float cos_sin_teta_vec[16] = {cos_teta, sin_teta, cos_teta, sin_teta, cos_teta, sin_teta,
+						cos_teta, sin_teta, cos_teta, sin_teta, cos_teta, sin_teta,
+						cos_teta, sin_teta, cos_teta, sin_teta};
+	alignas(64) float sin_cos_teta_vec[16] = {sin_teta, cos_teta, sin_teta, cos_teta, sin_teta, cos_teta,
+						sin_teta, cos_teta, sin_teta, cos_teta, sin_teta, cos_teta,
+						sin_teta, cos_teta, sin_teta, cos_teta};
+
+	/* __m512 data type represents a Zmm register with 16 float elements */
+	__m512 Zmm_cos_sin = _mm512_load_ps(cos_sin_teta_vec);
+
+	/* Intel® AVX-512 512-bit packed single load */
+	__m512 Zmm_sin_cos = _mm512_load_ps(sin_cos_teta_vec);
+	__m512 Zmm0, Zmm1, Zmm2, Zmm3;
+
+	/* processing 32 elements in an unrolled twice loop */
+	for(i = 0; i < len; i += 32) {
+		Zmm0 = _mm512_load_ps(pInVector+i);
+		Zmm1 = _mm512_moveldup_ps(Zmm0);
+		Zmm2 = _mm512_movehdup_ps(Zmm0);
+		Zmm2 = _mm512_mul_ps(Zmm2,Zmm_sin_cos);
+		Zmm3 = _mm512_fmaddsub_ps(Zmm1, Zmm_cos_sin, Zmm2);
+		_mm512_store_ps(pOutVector + i,Zmm3);
+
+		Zmm0 = _mm512_load_ps(pInVector + i + 16);
+		Zmm1 = _mm512_moveldup_ps(Zmm0);
+		Zmm2 = _mm512_movehdup_ps(Zmm0);
+		Zmm2 = _mm512_mul_ps(Zmm2, Zmm_sin_cos);
+		Zmm3 = _mm512_fmaddsub_ps(Zmm1, Zmm_cos_sin, Zmm2);
+		_mm512_store_ps(pOutVector + i + 16, Zmm3);
+	}
+
+	_mm_free(pInVector);
+	_mm_free(pOutVector);
+	return 0;
+}
