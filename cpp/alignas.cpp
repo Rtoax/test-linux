ea98802c2298e86163eec8af8712369bd7442d61
Rong Tao <rongtao@cestc.cn>
2023-12-18 20:14:43 -0500

    {c,cpp}: Test alignas()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cpp/alignas.cpp
+++ cpp/alignas.cpp
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <stdalign.h>
 
-struct alignas(16) test1 {
+#if defined(CPP)
+struct alignas(64) test1 {
+#else
+struct test1 {
+#endif
 	char c;
 	unsigned long l;
 };
 
 int main(void)
 {
+	alignas(64) int buffer[10];
+	printf("Sizeof(buffer) = %ld\n", sizeof(buffer));
 	printf("Sizeof(test1) = %ld\n", sizeof(struct test1));
 	return 0;
 }
