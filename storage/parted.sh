509d0277daa5e33d5eb5190677fe1253c2c17818
Rong Tao <rongtao@cestc.cn>
2023-12-19 16:18:00 +0800

    storage/parted.sh: Make gpt
    Link: https://wiki.linuxchina.net/index.php/%E4%BD%BF%E7%94%A8GPT%E5%88%86%E5%8C%BA%E8%A1%A8%E5%88%86%E5%8C%BA%E5%B9%B6%E6%A0%BC%E5%BC%8F%E5%8C%96%E5%A4%A7%E7%A1%AC%E7%9B%98
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- storage/parted.sh
+++ storage/parted.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
+
 sudo parted -ls
+
+mkgpt() {
+	local disk=/dev/vdb
+
+	sudo parted ${disk} mklabel gpt
+
+	sudo parted ${disk} mkpart primary ext4 0 10M
+	sudo parted ${disk} mkpart logical ext4 10M 20M
+	sudo parted ${disk} mkpart extended ext4 20M 30M
+
+	sudo mkfs.ext4 -T largefile ${disk}1
+	sudo mkfs.ext4 -T largefile ${disk}2
+	sudo mkfs.ext4 -T largefile ${disk}3
+
+	sleep 1
+	lsblk -o +fstype,uuid
+}
+
+mkgpt
+
