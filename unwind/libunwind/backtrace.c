4b8b457d0598957ed2861b52f95c2f3724466c72
Rong Tao <rongtao@cestc.cn>
2023-12-21 19:04:15 +0800

    libunwind: backtrace: Code format
    Link: https://blog.csdn.net/pwl999/article/details/107569603
    Link: https://blog.csdn.net/Rong_Toa/article/details/110846509
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- unwind/libunwind/backtrace.c
+++ unwind/libunwind/backtrace.c
@@ -1,47 +1,47 @@
-/**
- *	https://blog.csdn.net/Rong_Toa/article/details/110846509
- *
- *	gcc -g libunwind_backtrace.c -lunwind
- *	Rong Tao
- *	
- */
-
 #include <stdio.h>
 #define UNW_LOCAL_ONLY
 #include <libunwind.h>
- 
+
+
 void do_backtrace()
 {
-	unw_cursor_t    cursor;
-	unw_context_t   context;
- 
+	unw_cursor_t cursor;
+	unw_context_t context;
+
 	unw_getcontext(&context);
 	unw_init_local(&cursor, &context);
- 
+
 	while (unw_step(&cursor) > 0) {
-		unw_word_t  offset, pc;
+		unw_word_t offset;
+		unw_word_t  pc, eax, ebx, ecx, edx;
 		char        fname[64];
- 
-		unw_get_reg(&cursor, UNW_REG_IP, &pc);
- 
+
+		unw_get_reg(&cursor, UNW_REG_IP,  &pc);
+		unw_get_reg(&cursor, UNW_X86_64_RAX, &eax);
+		unw_get_reg(&cursor, UNW_X86_64_RDX, &edx);
+		unw_get_reg(&cursor, UNW_X86_64_RCX, &ecx);
+		unw_get_reg(&cursor, UNW_X86_64_RBX, &ebx);
+
 		fname[0] = '\0';
-		(void) unw_get_proc_name(&cursor, fname, sizeof(fname), &offset);
- 
-		printf ("%p : (%s+0x%x) [%p]\n", pc, fname, offset, pc);
+		unw_get_proc_name(&cursor, fname, sizeof(fname), &offset);
+		printf ("%p : (%s+0x%lx) [%p]\n", (void*)pc, fname, offset, (void*)pc);
+		printf("\tEAX=0x%08lx EDX=0x%08lx ECX=0x%08lx EBX=0x%08lx\n",
+			eax, edx, ecx, ebx);
 	}
 }
 
-void func1()
+void func1(void)
 {
 	do_backtrace();
 }
 
-void func2()
+void func2(void)
 {
 	func1();
 }
 
-int main()
+int main(void)
 {
-	func2(); 
+	func2();
+	return 0;
 }
