024f7cb268f012e427d510bd9d97afbd4e9abdc9
Rong Tao <rongtao@cestc.cn>
2023-12-25 19:09:22 +0800

    gdb: pthread-gdb.sh: gdb pid will stop all threads.
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- gdb/samples/pthread-gdb.sh
+++ gdb/samples/pthread-gdb.sh
@@ -8,10 +8,12 @@ if [[ -z "$pid" ]]; then
 	sleep 1
 	pid=$(pidof pthread-gdb)
 fi
-tids=( $(ls /proc/$pid/task/ | tail -n1) )
-child_tid=${tids[0]}
+tids=( $(ls /proc/$pid/task/) )
+child_tid=${tids[1]}
+parent_tid=${tids[0]}
 
 gdb -p $child_tid -q < pthread-gdb.gdb
+gdb -p $parent_tid -q < pthread-gdb.gdb
 
 sleep 1
 kill -9 ${pid}
