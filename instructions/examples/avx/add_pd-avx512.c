0fe2d67dc06e122000194f3d4297e32bd1311f53
Rong Tao <rongtao@cestc.cn>
2023-12-26 05:47:48 -0500

    avx512: add_pd-avx512.c: Add daxpy2() from stackoverflow
    Link: https://stackoverflow.com/questions/54809132/bmi-for-generating-masks-with-avx512
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/avx/add_pd-avx512.c
+++ instructions/examples/avx/add_pd-avx512.c
@@ -2,6 +2,45 @@
 #include <stdio.h>
 
 
+#if 0
+void daxpy2(int n, double a, const double x[], double y[])
+{
+	__m512d av = _mm512_set1_pd(a);
+	int i, r = n & 7, n2 = n - r;
+
+	for(i = -n2; i < 0; i += 8) {
+		__m512d yv = _mm512_loadu_pd(&y[i + n2]);
+		__m512d xv = _mm512_loadu_pd(&x[i + n2]);
+		yv = _mm512_fmadd_pd(av, xv, yv);
+		_mm512_storeu_pd(&y[i + n2], yv);
+	}
+	__m512d yv = _mm512_loadu_pd(&y[n2]);
+	__m512d xv = _mm512_loadu_pd(&x[n2]);
+	yv = _mm512_fmadd_pd(av, xv, yv);
+	__mmask8 mask = (1 << r) - 1;
+	//__mmask8 mask = _bextr_u32(-1, 0, r);
+	_mm512_mask_storeu_pd(&y[n2], mask, yv);
+}
+#else
+void daxpy2(size_t n, double a, const double x[], double y[])
+{
+	__m512d av = _mm512_set1_pd(a);
+	size_t i, r = n&7, n2 = n & (-8);
+	for(i = -n2; i != 0; i += 8) {
+		__m512d yv = _mm512_loadu_pd(&y[i + n2]);
+		__m512d xv = _mm512_loadu_pd(&x[i + n2]);
+		yv = _mm512_fmadd_pd(av, xv, yv);
+		_mm512_storeu_pd(&y[i + n2], yv);
+	}
+	//__mmask8 mask = _bzhi_u32(-1, r);
+	__mmask8 mask = (1 << r) - 1;
+	__m512d yv = _mm512_mask_loadu_pd(_mm512_undefined_pd (), mask, &y[n2]);
+	__m512d xv = _mm512_mask_loadu_pd(_mm512_undefined_pd (), mask, &x[n2]);
+	yv = _mm512_mask_fmadd_pd(av, mask, xv, yv);
+	_mm512_mask_storeu_pd(&y[n2], mask, yv);
+}
+#endif
+
 void add512(double a[], double b[], double c[])
 {
 	__m512d aa, bb, cc;
@@ -11,22 +50,23 @@ void add512(double a[], double b[], double c[])
 	_mm512_storeu_pd(c, cc);
 }
 
+#define LEN	13
+
 int main(void)
 {
 	int i;
-	double a[8];
-	double b[8];
-	double c[8];
+	double a[LEN];
+	double b[LEN];
 
-	for (i = 0; i < 8; i++) {
+	for (i = 0; i < LEN; i++) {
 		a[i] = i + 1;
 		b[i] = i + 1;
 	}
 
-	add512(a, b, c);
+	daxpy2(LEN, 2, a, b);
 
-	for (i = 0; i < 8; i++)
-		printf("%f ", c[i]);
+	for (i = 0; i < LEN; i++)
+		printf("%f ", b[i]);
 	printf("\n");
 
 	return 0;
