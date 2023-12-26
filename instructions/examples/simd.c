11e7269ac61ed5f40b690b40b60ad818f1a4f246
Rong Tao <rongtao@cestc.cn>
2023-12-26 06:31:15 -0500

    simd.c: Add double_avx512_X_add_Y() test
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/simd.c
+++ instructions/examples/simd.c
@@ -10,6 +10,9 @@
 #if !defined(CONFIG_NO_SVE)
 #include <arm_sve.h>
 #endif
+#if defined(CPU_HAVE_AVX512F)
+#include <immintrin.h>
+#endif
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
@@ -375,6 +378,28 @@ void float_sve_X_add_Y(void *_x, void *_y, size_t n)
 }
 #endif
 
+#if defined(CPU_HAVE_AVX512F)
+void double_avx512_X_add_Y(void *_x, void *_y, size_t n)
+{
+	double *x = _x;
+	double *y = _y;
+	size_t i, r = n&7, n2 = n & (-8);
+	for(i = -n2; i != 0; i += 8) {
+		__m512d yv = _mm512_loadu_pd(&y[i + n2]);
+		__m512d xv = _mm512_loadu_pd(&x[i + n2]);
+		yv = _mm512_add_pd(xv, yv);
+		_mm512_storeu_pd(&y[i + n2], yv);
+	}
+	//__mmask8 mask = _bzhi_u32(-1, r);
+	__mmask8 mask = (1 << r) - 1;
+	__m512d src;
+	__m512d yv = _mm512_mask_loadu_pd(_mm512_undefined_pd (), mask, &y[n2]);
+	__m512d xv = _mm512_mask_loadu_pd(_mm512_undefined_pd (), mask, &x[n2]);
+	yv = _mm512_mask_add_pd(src, mask, xv, yv);
+	_mm512_mask_storeu_pd(&y[n2], mask, yv);
+}
+#endif
+
 enum {
 	T_BASE_U8_MUL,
 	T_BASE_U8_ADD,
@@ -590,6 +615,17 @@ struct test tests[] = {
 	{
 		.name = " Sve: y[i] = x[i] + y[i] (f64)",
 		.fn = double_sve_X_add_Y,
+		.init = init_arr_f64,
+		.cmp = cmp_arr_f64,
+		.elem_size = sizeof(double),
+		.spent_us = 0,
+		.cmp_with = &tests[T_BASE_F64_ADD],
+	},
+#endif
+#if defined(CPU_HAVE_AVX512F)
+	{
+		.name = "avx512: y[i] = x[i] + y[i] (f64)",
+		.fn = double_avx512_X_add_Y,
 		.init = init_arr_f64,
 		.cmp = cmp_arr_f64,
 		.elem_size = sizeof(double),
