52f84f89c61a7307f34d7ef77355c2083e0e940e
Rong Tao <rongtao@cestc.cn>
2023-12-28 20:56:54 +0800

    __builtin___clear_cache.c: Add __aarch64_sync_cache_range()
    Link: https://stackoverflow.com/questions/35741814/how-does-builtin-clear-cache-work
    Link: https://github.com/gcc-mirror/gcc/tree/master/libgcc/config/aarch64
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/__builtin_/__builtin___clear_cache.c
+++ compiler/__builtin_/__builtin___clear_cache.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <malloc.h>
 
+#if defined(__aarch64__)
+# define flush_cache(start, end) __aarch64_sync_cache_range(start, end)
+#else
+# define flush_cache(start, end) __builtin___clear_cache(start, end)
+#endif
+
 int main(void)
 {
 	char *str = malloc(sizeof(char) * 1024);
-	__builtin___clear_cache(str, str + 1024);
+	flush_cache(str, str + 1024);
 	free(str);
 	return 0;
 }
