df78c0af998f6a28a5e24adca93a43dc6ce9133c
Rong Tao <rongtao@cestc.cn>
2023-12-29 21:55:13 +0800

    install-deps.sh: Support debian
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- install-deps.sh
+++ install-deps.sh
@@ -1,58 +1,76 @@
 #!/bin/bash
 set -e
 
+. /etc/os-release
+
+OS=${ID}
+
+echo "OS: ${OS}"
+
 pkgs=( make cmake gcc clang gdb cgdb )
 pkgs+=( gcc-aarch64-linux-gnu )
 pkgs+=( bpftrace bpftool bcc ) # eBPF
-pkgs+=( bcc-devel )
-pkgs+=( binutils-devel )
-pkgs+=( capstone-devel )       # Capstone is a disassembly framework
-pkgs+=( cereal-devel )
-pkgs+=( clang-devel )
 pkgs+=( criu )
-pkgs+=( dtc )                  # device tree
 pkgs+=( dwarves )              # pahole
 pkgs+=( efivar mokutil )       # UEFI
-pkgs+=( elfutils-libelf-devel )
 pkgs+=( golang )               # go
-pkgs+=( grub2-tools-minimal grub2-tools-extra )
-pkgs+=( gtest-devel gmock-devel )
-pkgs+=( gtk3-devel )           # GTK3
 pkgs+=( hwloc )                # lstopo
 pkgs+=( inotify-tools )        # inotifywatch, ...
-pkgs+=( iproute-tc )           # tc
-pkgs+=( kernel-devel )         # kernel
-pkgs+=( libaio-devel )         # aio
-pkgs+=( libdwarf-tools )       # dwarfdump
-pkgs+=( libbpf-devel )         # libbpf
-pkgs+=( libpcap-devel )
-pkgs+=( libselinux-devel )     # SELinux
-pkgs+=( libunwind-devel )      # Unwind
-pkgs+=( liburing-devel )       # uring
-pkgs+=( llvm llvm-devel )      # llvm-as llvm-dis llc
+pkgs+=( llvm )                 # llvm-as llvm-dis llc
 pkgs+=( lshw )                 # lshw
-pkgs+=( ltrace )               # ltrace
-pkgs+=( mmc )                  # mmc
 pkgs+=( nasm )                 # nasm
 pkgs+=( numactl )              # numastat
 pkgs+=( python3-matplotlib )   # matplotlib
 pkgs+=( python3-pyroute2 )     # pyroute2
 pkgs+=( python3-scapy )
 pkgs+=( qemu-kvm qemu-user )
-pkgs+=( rust )                 # rustc
-pkgs+=( sg3_utils )            # sg_inq, etc.
 pkgs+=( sparse )               # sparse
 pkgs+=( strace )
 pkgs+=( smartmontools )        # smartctl
-pkgs+=( systemtap-sdt-devel )  # sdt.h
 pkgs+=( tree )
 
 if [[ $(uname -m) == x86_64 ]]; then
 	pkgs+=( glibc-devel.i686 )
 fi
 
-args=( --skip-broken )
-args+=( --nogpgcheck )
+case ${OS} in
+cclinux|fedora|centos|rhel)
+	pkgs+=( bcc-devel )
+	pkgs+=( binutils-devel )
+	pkgs+=( capstone-devel )       # Capstone is a disassembly framework
+	pkgs+=( cereal-devel )
+	pkgs+=( clang-devel )
+	pkgs+=( dtc )                  # device tree
+	pkgs+=( elfutils-libelf-devel )
+	pkgs+=( grub2-tools-minimal grub2-tools-extra )
+	pkgs+=( gtest-devel gmock-devel )
+	pkgs+=( gtk3-devel )           # GTK3
+	pkgs+=( iproute-tc )           # tc
+	pkgs+=( kernel-devel )         # kernel
+	pkgs+=( libaio-devel )         # aio
+	pkgs+=( libbpf-devel )         # libbpf
+	pkgs+=( libpcap-devel )
+	pkgs+=( libselinux-devel )     # SELinux
+	pkgs+=( libunwind-devel )      # Unwind
+	pkgs+=( liburing-devel )       # uring
+	pkgs+=( libdwarf-tools )       # dwarfdump
+	pkgs+=( llvm-devel )
+	pkgs+=( ltrace )               # ltrace
+	pkgs+=( mmc )                  # mmc
+	pkgs+=( rust )                 # rustc
+	pkgs+=( sg3_utils )            # sg_inq, etc.
+	pkgs+=( systemtap-sdt-devel )  # sdt.h
 
-sudo dnf up -y
-sudo dnf install ${args[@]} -y ${pkgs[@]}
+	args=( --skip-broken )
+	args+=( --nogpgcheck )
+
+	sudo dnf up -y
+	sudo dnf install ${args[@]} -y ${pkgs[@]}
+	;;
+debian|ubuntu)
+	sudo apt install ${pkgs[@]}
+	;;
+*)
+	echo "ERROR: Unknown OS ${OS}"
+	;;
+esac
