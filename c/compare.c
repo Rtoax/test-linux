e877c58b925ea4327f8013d05550cddfa817e900
Rong Tao <rongtao@cestc.cn>
2023-12-30 11:53:49 +0800

    c: compare.c: 1 < 16, -1 > 16
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/compare.c
+++ c/compare.c
@@ -6,8 +6,15 @@ int main(void)
 	size_t sz = 16;
 	int n = -1;
 
-	printf("%d %s %ld\n", n, n < sz ? "<" : ">=", sz);
+#define PRINT()	\
+	printf("%d %s %ld\n", n, n < sz ? "<" : ">=", sz);	\
 	printf("%d %s %ld\n", n, n < (int)sz ? "<" : ">=", sz);
+
+	PRINT();
+
+	n = 1;
+
+	PRINT();
 
 	return 0;
 }
