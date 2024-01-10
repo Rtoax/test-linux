4a440237ef08b16bf9eb87c07457ac276699e420
Rong Tao <rongtao@cestc.cn>
2024-01-10 08:56:53 +0800

    __attribute__: __cleanup__
    Link: https://mp.weixin.qq.com/s/kVdKcQUqm7vAYuljMtemuw
    Link: https://lwn.net/ml/linux-kernel/20230612090713.652690195@infradead.org/
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/__attribute__/__attribute__cleanup.c
+++ compiler/__attribute__/__attribute__cleanup.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <malloc.h>
 
 void clean_up(void)
 {
@@ -11,8 +12,18 @@ fun(void)
 	printf("fun\n");
 }
 
+void auto_free(void **p)
+{
+	printf("Free pointer. %p\n", *p);
+	free(*p);
+}
+
 int main(void)
 {
+	void *foo_ptr __attribute__((__cleanup__(auto_free))) = NULL;
+	foo_ptr = malloc(1024);
+	printf("Alloc pointer %p\n", foo_ptr);
 	fun();
 	return 0;
 }
+
