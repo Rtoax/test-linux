6c89fd88003fa5f7f095194c61c20ac31414ad6c
Rong Tao <rongtao@cestc.cn>
2024-02-02 18:17:02 +0800

    c: Add stub.c
    Link: https://en.wikipedia.org/wiki/Method_stub
    Link: https://www.quora.com/What-does-stub-mean-in-programming
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ c/stub.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* this is a stub funtion, A stub is a function that has the expected signature
+ * (i.e. name and accepted arguments), but an incomplete implementation. */
+int get_temperature(int outside)
+{
+	return 24;
+}
+
+int main(void)
+{
+
+	int t = get_temperature(0);
+	return t;
+}
