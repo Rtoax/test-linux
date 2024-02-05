409fb78f40a71c64dd3749e1940a38886b48fd7c
Rong Tao <rongtao@cestc.cn>
2024-02-05 11:35:19 +0800

    dlfcn: Add dlsym-RTLD_DEFAULT.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/dlfcn/dlsym-RTLD_DEFAULT.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <dlfcn.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	int (*p1)(const char *restrict format, ...);
+
+	p1 = dlsym(RTLD_DEFAULT, "printf");
+	if (p1 == NULL) {
+		printf("%s\n", dlerror());
+		exit(1);
+	}
+
+	p1("Hello, %s\n", "World");
+
+	return 0;
+}
+
