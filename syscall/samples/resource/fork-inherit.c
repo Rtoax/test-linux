59c61329ff210177f32c9b335e747ac53acbb9f0
Rong Tao <rongtao@cestc.cn>
2024-02-05 11:00:35 +0800

    syscall/rlimit: Add fork-inherit.c
    Child will inherit parent's rlimit.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/resource/fork-inherit.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+
+#include "helpers.h"
+
+
+int main(void)
+{
+	pid_t child;
+	struct rlimit rlimit;
+
+	getrlimit(RLIMIT_CORE, &rlimit);
+	print_rlimit("RLIMIT_CORE", &rlimit);
+
+	rlimit.rlim_cur = rlimit.rlim_max = 1024 * 1024 * 10;
+	setrlimit(RLIMIT_CORE, &rlimit);
+
+	getrlimit(RLIMIT_CORE, &rlimit);
+	print_rlimit("RLIMIT_CORE setted", &rlimit);
+
+	child = fork();
+	if (child == 0) {
+		/**
+		 * Children will inherit parent's rlimit
+		 */
+		printf("Child running.\n");
+		getrlimit(RLIMIT_CORE, &rlimit);
+		print_rlimit("RLIMIT_CORE child", &rlimit);
+	} else if (child > 0) {
+		printf("Parent running.\n");
+		getrlimit(RLIMIT_CORE, &rlimit);
+		print_rlimit("RLIMIT_CORE parent", &rlimit);
+		wait(NULL);
+	}
+
+	return 0;
+}
