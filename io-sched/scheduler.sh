c3ea6b479a5a2392ee7bc42ec618696347c0f03e
Rong Tao <rongtao@cestc.cn>
2024-02-06 12:01:31 +0800

    io-sched: Add scheduler.sh
    	$ ./scheduler.sh
    	nvme0n1          : [none] mq-deadline kyber bfq
    	sda              : none mq-deadline [kyber] bfq
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ io-sched/scheduler.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+blks=( $(ls /sys/block/) )
+
+for b in ${blks[@]}
+do
+	scheduler=/sys/block/${b}/queue/scheduler
+	if [[ ${b} == zram* ]]; then
+		continue
+	fi
+	printf "%-16s : " ${b}
+	cat ${scheduler}
+done
