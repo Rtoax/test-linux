fae0830d689de75e3296ad319bbaa0237645a636
Rong Tao <rtoax@foxmail.com>
2024-02-08 21:43:25 +0800

    glibc/dlfcn/hacking: Apply dlopen-dlsym to libdev_hack.so
    Finish: commit 7629ed9ba94f ("glibc/dlfcn: Add hacking/")
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- glibc/dlfcn/hacking/libdev_hack.c
+++ glibc/dlfcn/hacking/libdev_hack.c
@@ -1,18 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <dlfcn.h>
 
 #include "libdev.h"
 
 
+#define ORIG_LIBDEV	"libdev.so.1"
 
-static struct device dev = {
-	.totalmemsize = 1024,
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+
+
+struct symbol {
+	const char *name;
+	void *ptr;
 };
 
+static struct symbol libdev_table[] = {
+	{
+		.name = "dev_gettotalmemsize",
+		.ptr = NULL,
+	},
+};
+
+static pthread_once_t init_once = PTHREAD_ONCE_INIT;
+
+
+static inline void *find_fptr(const char *name)
+{
+	int i;
+	for (i = 0; i < ARRAY_SIZE(libdev_table); i++) {
+		if (!strcmp(name, libdev_table[i].name))
+			return libdev_table[i].ptr;
+	}
+	return NULL;
+}
+
+static void init_libdev(void)
+{
+	int i;
+	void *handle;
+
+	handle = dlopen(ORIG_LIBDEV, RTLD_NOW | RTLD_NODELETE);
+
+	for (i = 0; i < ARRAY_SIZE(libdev_table); i++) {
+		libdev_table[i].ptr = dlsym(handle, libdev_table[i].name);
+	}
+
+	dlclose(handle);
+}
+
+static int hacking_init(void)
+{
+	pthread_once(&init_once, init_libdev);
+	return 0;
+}
 
 int dev_gettotalmemsize(void)
 {
-	return dev.totalmemsize;
+	hacking_init();
+	int (*orig)(void) = find_fptr("dev_gettotalmemsize");
+	/**
+	 * Only give user 50% memory.
+	 */
+	if (orig)
+		return orig() / 2;
+	/* fake value */
+	return 64;
 }
 
--- glibc/dlfcn/hacking/run.sh
+++ glibc/dlfcn/hacking/run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 run_dev() {
-	LD_LIBRARY_PATH=$PWD ldd ./dev
 	LD_LIBRARY_PATH=$PWD ./dev
 }
 
+make clean
+make
+
+echo ==========================================================================
 run_dev
 
 rm libdev.so
 ln -s libdev_hack.so libdev.so
 
+echo ==========================================================================
 run_dev
 
