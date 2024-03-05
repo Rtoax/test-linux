e3746681d27351cc35d0bfe3cab2df8314f5f452
Rong Tao <rongtao@cestc.cn>
2024-03-05 14:17:06 +0800

    syscall/samples/capability: Add version.c
    Link: https://0x0916.gitbooks.io/linux-capability/content/chapter3.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/capability/capget.c
+++ syscall/samples/capability/capget.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <unistd.h>
 #include <sys/capability.h>
 
 
 int main(void)
 {
+	int ret;
 	char buf[1024];
 
 	cap_user_header_t hdrp = (cap_user_header_t)&buf[0];
 	cap_user_data_t datap = (cap_user_data_t)&buf[100];
 
+	ret = capget(hdrp, NULL);
+	if (ret < 0) {
+		fprintf(stderr, "capget error: %s", strerror(errno));
+	}
+
 	hdrp->pid = getpid();
-	hdrp->version = _LINUX_CAPABILITY_VERSION_1;
 
 	capget(hdrp, datap);
 
