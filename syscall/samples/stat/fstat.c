4dd3e22ba64afa3ba047cde57cc716ab9b9005d4
Rong Tao <rtoax@foxmail.com>
2024-03-06 21:13:16 +0800

    syscall/samples/stat: Add fstat, lstat, link listdir.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- syscall/samples/stat/fstat.c
+++ syscall/samples/stat/fstat.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 
 int main(void)
 {
 	struct stat buf;
 
-	lstat("/etc/os-release", &buf);
+	int fp = open("/etc/os-release", O_RDONLY);
+
+	fstat(fp, &buf);
 
 	printf("mode: %d\n", buf.st_mode);
 	printf("ino: %ld\n", buf.st_ino);
