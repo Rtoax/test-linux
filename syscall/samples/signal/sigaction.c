2eaada9d164cbf3d0936ad7f8030885fb3cc8862
Rong Tao <rongtao@cestc.cn>
2024-03-08 09:14:46 +0800

    syscall/samples/signal: Add sigaction.c
        $ ./sigaction
        time out!
        time out!
        time out!
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/signal/sigaction.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+
+void timeout(int signo)
+{
+	if (signo == SIGALRM)
+		printf("time out! \n");
+
+	alarm(1);
+}
+
+int main(void)
+{
+	int i;
+	struct sigaction act;
+
+	act.sa_handler = timeout;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+
+	sigaction(SIGALRM, &act, 0);
+
+	alarm(1);
+
+	for (i = 0; i < 3; i++)
+		sleep(100);
+
+	return 0;
+}
