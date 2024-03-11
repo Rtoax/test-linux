7cd2d5901774ffed400e6b324d9707eb47315f47
Rong Tao <rongtao@cestc.cn>
2024-03-11 13:56:01 +0800

    fs/xfs: Add mkfs.xfs.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ fs/xfs/mkfs.xfs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo dd if=/dev/zero of=xfs.img bs=4096 count=4096
+sudo mkfs.vfat xfs.img
+sudo mkdir -p tmp-mnt
+sudo mount xfs.img tmp-mnt -o loop,rw
+sudo cp /etc/os-release tmp-mnt/
+sudo tree tmp-mnt/
+sudo umount tmp-mnt
