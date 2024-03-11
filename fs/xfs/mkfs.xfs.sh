70bc68680859819c971295e065db2c1100042d70
Rong Tao <rongtao@cestc.cn>
2024-03-11 14:36:21 +0800

    fs/xfs: mkfs.xfs: Filesystem must be larger than 300MB.
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- fs/xfs/mkfs.xfs.sh
+++ fs/xfs/mkfs.xfs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo dd if=/dev/zero of=xfs.img bs=4096 count=4096
-sudo mkfs.xfs xfs.img
+sudo dd if=/dev/zero of=fs.xfs bs=1M count=310
+sudo mkfs.xfs fs.xfs
 sudo mkdir -p tmp-mnt
-sudo mount xfs.img tmp-mnt -o loop,rw
+sudo mount fs.xfs tmp-mnt -o loop,rw
 sudo cp /etc/os-release tmp-mnt/
 sudo tree tmp-mnt/
 sudo umount tmp-mnt
