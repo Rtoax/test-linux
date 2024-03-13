e3e9892256b1f3fa5eb1c0769a94e9d5e061efd8
Rong Tao <rongtao@cestc.cn>
2024-03-13 19:25:25 +0800

    getrusage(2): Print more rss
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/resource/getrusage.c
+++ syscall/samples/resource/getrusage.c
@@ -6,6 +6,9 @@ int main(void)
 	struct rusage rusage;
 	getrusage(RUSAGE_SELF, &rusage);
 	printf("maxrss = %ld\n", rusage.ru_maxrss);
+	printf("ixrss = %ld\n", rusage.ru_ixrss);
+	printf("isrss = %ld\n", rusage.ru_isrss);
+	printf("idrss = %ld\n", rusage.ru_idrss);
 	return 0;
 }
 
