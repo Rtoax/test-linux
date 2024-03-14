64f0bfc394655398b0ecb684b7852b078e710abe
Rong Tao <rongtao@cestc.cn>
2024-03-14 16:46:10 +0800

    nvme: Show block size
        $ sudo sg_inq /dev/nvme0n1
        Identify controller for /dev/nvme0n1:
          Model number: KINGSTON SNVS500G
          [...]
          Namespace 1 (deduced from device name):
            Namespace size/capacity: 976773168/976773168 blocks
            Namespace utilization: 976773168 blocks
            NGUID: 0x00000000000000000026b7684bad52d5
            EUI-64: 0x26b7684bad52d5
            Number of LBA formats: 2
            Index LBA size: 0
            LBA format 0 support: <-- active
              Logical block size: 512 bytes        <<<<<<<<<<<<<<< 512
              Approximate namespace size: 500 GB
              Metadata size: 0 bytes
              Relative performance: Better [0x1]
            LBA format 1 support:
              Logical block size: 4096 bytes
              Approximate namespace size: 0 GB
              Metadata size: 0 bytes
              Relative performance: Best [0x0]
    
        # Invalid argument if bs!=512
        $ sudo dd if=/dev/zero of=/dev/nvme0n1 bs=128 count=12 oflag=direct
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- install-deps.sh
+++ install-deps.sh
@@ -169,6 +169,7 @@ cclinux|fedora|centos|rhel|openEuler)
 	pkgs+=( ltrace )               # ltrace
 	pkgs+=( mmc )                  # mmc
 	pkgs+=( mpich mpich-devel )    # mpi
+	pkgs+=( nvme-cli )             # nvme
 	pkgs+=( overlayfs-tools )
 	pkgs+=( procps-ng )            # pidof, top, etc.
 	pkgs+=( scl-utils )
--- storage/nvme/info.sh
+++ storage/nvme/info.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+dev_nvme=( $(sudo nvme list | grep ^/dev/nvme | awk '{print $1}') )
+for dev in ${dev_nvme[@]}
+do
+	sudo sg_inq ${dev} | sed 's/^/\t/g'
+done
+
 lspci -vmmk | grep -i nvme -A 10 -B 10
 sudo nvme list
 sudo grep nvme /proc/iomem
