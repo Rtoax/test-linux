541c890be26f0a538a14c0b159cefae9cad116c5
Rong Tao <rongtao@cestc.cn>
2024-03-15 19:42:13 +0800

    glibc/unistd: Add getlogin,getlogin_r,cuserid
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/unistd/cuserid.c
@@ -0,0 +1,24 @@
+#define _GNU_SOURCE
+#define _XOPEN_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *id, *ret;
+
+	id = getlogin();
+	printf("userid = %s\n", id);
+
+	getlogin_r(id, 10);
+	printf("userid = %s\n", id);
+
+	ret = cuserid(id);
+	printf("id = %s\n", id);
+	printf("ret = %s\n", ret);
+
+	return 0;
+}
+
