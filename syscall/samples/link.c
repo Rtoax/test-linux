64493b437f40de177a4627ac0278a807da208d20
Rong Tao <rtoax@foxmail.com>
2024-03-18 21:58:48 +0800

    syscall/samples: Add link.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ syscall/samples/link.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#include "helpers.h"
+
+
+int main(void)
+{
+	int ret;
+
+	create_file("rong.out", 'a', 0);
+
+	/* Make hard link (same inode) */
+	ret = link("rong.out", "rong2.out");
+	printf("link ret: %d\n", ret);
+
+	unlink("rong.out");
+	unlink("rong2.out");
+
+	return 0;
+}
