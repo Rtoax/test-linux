455f1eecf9a243c7ef8b75ce01fdb3601221edaa
Rong Tao <rongtao@cestc.cn>
2024-03-18 17:17:30 +0800

    qemu: Add qemu-img.sh from ostools
    Commit:
    
        commit 8803cef27c3f519880b88963ee69ea089de25e41
        Author: Rong Tao <rongtao@cestc.cn>
        Date:   Thu Jun 15 13:03:13 2023 +0800
    
            qemu-img: qemu-img convert -O raw input.raw new.raw
    
            Signed-off-by: Rong Tao <rongtao@cestc.cn>
    
        commit c5493f3ae334c17c152c284df797eb67afd28c05
        Author: Rong Tao <rongtao@cestc.cn>
        Date:   Thu Jun 15 10:51:55 2023 +0800
    
            qemu-img: How to create raw image
    
                $ qemu-img create -f raw test.raw 100G
    
            Signed-off-by: Rong Tao <rongtao@cestc.cn>
    
        commit c941d4228014754225a552cc84030e357e72d9fa
        Author: Rong Tao <rongtao@cestc.cn>
        Date:   Wed Jun 7 14:59:35 2023 +0800
    
            qemu-img: Convert raw to qcow2
    
            Signed-off-by: Rong Tao <rongtao@cestc.cn>
    
        commit 5342d55b8a45411f92caf331226a5c387f0d3099
        Author: Rong Tao <rongtao@cestc.cn>
        Date:   Wed Mar 22 09:10:47 2023 +0800
    
            qeme-img: convert qcow2 into disk /dev/vdb
    
                    Example:
    
                    sudo wipefs --force --all /dev/sdb
                    sudo blockdev --getsz /dev/sdb
                    sudo dd bs=512 if=/dev/zero of=/dev/sdb count=33
                    sudo dd bs=512 if=/dev/zero of=/dev/sdb count=33 seek=419430367
                    sudo sgdisk -Z /dev/sdb
                    sudo fuser /dev/sdb
                    sudo udevadm settle
                    sudo qemu-img convert -t directsync -O host_device -W /home/a.qcow2 /dev/sdb
    
            Signed-off-by: Rong Tao <rongtao@cestc.cn>
    
        commit 87c0f397b8c04d1d81bacf09bfef6e1dd6cab18c
        Author: Rong Tao <rongtao@cestc.cn>
        Date:   Fri Feb 10 11:27:20 2023 +0800
    
            qemu-img convert -O qcow2 input.qcow2 new.qcow2
    
            Signed-off-by: Rong Tao <rongtao@cestc.cn>
    
        commit 7f8464cccf7822dc26a5d3aa4a8f8d83cd523136
        Author: Rong Tao <rtoax@foxmail.com>
        Date:   Thu Nov 17 13:48:16 2022 +0800
    
            qemu-img: create a qcow2
    
            Signed-off-by: Rong Tao <rtoax@foxmail.com>
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ qemu/qemu-img.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+qcow2_create()
+{
+	# Create qcow2 image
+	qemu-img create -f qcow2 test.qcow2 100G
+}
+
+raw_create()
+{
+	qemu-img create -f raw test.raw 100G
+}
+
+qcow2_convert()
+{
+	qemu-img convert -O qcow2 input.qcow2 new.qcow2
+}
+
+raw_convert()
+{
+	qemu-img convert -O raw input.raw new.raw
+}
+
+raw2qcow2()
+{
+	qemu-img convert -f raw -O qcow2 disk.raw disk.qcow2
+}
+
+qcow2_installer()
+{
+	local target_disk_dev=/dev/vdb
+	local src_qcow2=a.qcow2
+
+	sudo wipefs --force --all ${target_disk_dev}
+
+	# Get size of disk
+	sudo blockdev --getsz ${target_disk_dev}
+
+	# Zero
+	sudo dd bs=512 if=/dev/zero of=${target_disk_dev} count=33
+	sudo dd bs=512 if=/dev/zero of=${target_disk_dev} count=33 seek=419430367
+
+	# Creating new GPT entries in memory.
+	sudo sgdisk -Z ${target_disk_dev}
+
+	sudo fuser ${target_disk_dev}
+	sudo udevadm settle
+
+	sudo qemu-img convert \
+		-t directsync \
+		-O host_device \
+		-W ${src_qcow2} \
+		${target_disk_dev}
+}
+
