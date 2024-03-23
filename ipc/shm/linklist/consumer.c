ae7137a756692c47061bc1e1b05430ee9c488519
Rong Tao <rtoax@foxmail.com>
2024-03-23 16:04:41 +0800

    ipc/shm: Add linklist/
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ ipc/shm/linklist/consumer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <mlinklist.h>
+#include <common.h>
+
+
+void foreach(struct ll_node *node)
+{
+	printf("foreach: %d %ld.\n", node->i, node->offset);
+}
+
+int main(void)
+{
+	mll_t mll;
+
+	create_mll(&mll, MMAP_FILENAME, sizeof(struct ll_node) * LINKLIST_SIZE, 0);
+
+	mll_foreach(&mll, foreach);
+
+	return 0;
+}
