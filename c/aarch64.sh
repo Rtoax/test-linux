db549f2385a27fac204b801ec096fff4b7a7b86d
Rong Tao <rongtao@cestc.cn>
2024-03-25 16:06:55 +0800

    c/aarch64.sh: SKip if on aarch64
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/aarch64.sh
+++ c/aarch64.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+[[ $(uname -m) ]] && echo "ERROR: You alread on aarch64, no need to cross compile" && exit 1
+
 make ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu- SYSROOT=/home/rongtao/rootfs-aarch64
 
