796c1fac541543057cfb45a85ec812c3db3ccca3
Rong Tao <rongtao@cestc.cn>
2024-04-02 19:41:27 +0800

    c: aarch64.sh: cross compile: Fix and shell error
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/aarch64.sh
+++ c/aarch64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ $(uname -m) ]] && echo "ERROR: You alread on aarch64, no need to cross compile" && exit 1
+[[ $(uname -m) == aarch64 ]] && echo "ERROR: You already on aarch64, no need to cross compile" && exit 1
 
 make ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu- SYSROOT=/home/rongtao/rootfs-aarch64
 
