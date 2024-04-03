b25a8274e3fd0f575caf31dd45931784cf5c0608
Rong Tao <rtoax@foxmail.com>
2024-04-03 21:38:32 +0800

    aio(2): Add helpers.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ syscall/samples/aio/helpers.c
@@ -0,0 +1,47 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <syscall.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/epoll.h>
+#include <linux/aio_abi.h>
+
+int io_setup(unsigned nr_events, aio_context_t *ctx_idp)
+{
+	return (int)syscall(SYS_io_setup, nr_events, ctx_idp);
+}
+
+int io_destroy(aio_context_t ctx_id)
+{
+	return (int)syscall(SYS_io_setup, ctx_id);
+}
+
+int io_getevents(aio_context_t ctx_id, long min_nr, long nr,
+		 struct io_event *events, struct timespec *timeout)
+{
+	return (int)syscall(SYS_io_getevents, ctx_id, min_nr, nr, events, timeout);
+}
+
+int io_cancel(aio_context_t ctx_id, struct iocb *iocb,
+					 struct io_event *result)
+{
+	return (int)syscall(SYS_io_cancel, ctx_id, iocb, result);
+}
+
+int io_submit(aio_context_t ctx_id, long nr, struct iocb **iocbpp)
+{
+	return (int)syscall(SYS_io_submit, ctx_id, nr, iocbpp);
+}
+
+int eventfd(unsigned int initval, int flags)
+{
+	return  (int)syscall(SYS_eventfd, initval, flags);
+}
