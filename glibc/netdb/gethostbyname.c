fab71950d5ff792d70ce54a4d1a095857d05c990
Rong Tao <rtoax@foxmail.com>
2024-04-06 22:14:46 +0800

    glibc/netdb: Add gethostbyname.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ glibc/netdb/gethostbyname.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+
+#include "helpers.h"
+
+
+int main(int argc, char* argv[])
+{
+	struct hostent *host;
+	char name[] = "https://cn.bing.com/";
+
+	host = gethostbyname(name);
+	if (!host) {
+		perror("gethostbyname() error!");
+		exit(1);
+	}
+
+	print_hostent(host);
+
+	return 0;
+}
