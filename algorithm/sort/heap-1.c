d683241fe28341018ec4658ea4010607002ab94c
Rong Tao <rtoax@foxmail.com>
2024-04-06 09:30:17 +0800

    algorithm/sort: Add heap
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ algorithm/sort/heap-1.c
@@ -0,0 +1,66 @@
+/**
+ * 堆排序
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b)
+{
+	int temp = *b;
+	*b = *a;
+	*a = temp;
+}
+
+void max_heapify(int arr[], int start, int end)
+{
+	/**
+	 * 建立父節點指標和子節點指標
+	 */
+	int dad = start;
+	int son = dad * 2 + 1;
+	/**
+	 * 若子節點指標在範圍內才做比較
+	 */
+	while (son <= end) {
+		/* 先比較兩個子節點大小，選擇最大的 */
+		if (son + 1 <= end && arr[son] < arr[son + 1])
+			son++;
+		/* 如果父節點大於子節點代表調整完畢，直接跳出函數 */
+		if (arr[dad] > arr[son]) //
+			return;
+		/* 否則交換父子內容再繼續子節點和孫節點比較 */
+		else {
+			swap(&arr[dad], &arr[son]);
+			dad = son;
+			son = dad * 2 + 1;
+		}
+	}
+}
+
+void heap_sort(int arr[], int len)
+{
+	int i;
+	/* 初始化，i從最後一個父節點開始調整 */
+	for (i = len / 2 - 1; i >= 0; i--)
+		max_heapify(arr, i, len - 1);
+	/* 先將第一個元素和已排好元素前一位做交換，再重新調整，直到排序完畢 */
+	for (i = len - 1; i > 0; i--) {
+		swap(&arr[0], &arr[i]);
+		max_heapify(arr, 0, i - 1);
+	}
+}
+
+int main(void)
+{
+	int i;
+	int arr[] = { 3, 5, 3, 0, 8, 6, 1, 5, 8, 6, 2, 4, 9, 4, 7, 0, 1, 8, 9, 7, 3,
+			1, 2, 5, 9, 7, 4, 0, 2, 6 };
+	int len = (int) sizeof(arr) / sizeof(*arr);
+
+	heap_sort(arr, len);
+
+	for (i = 0; i < len; i++)
+		printf("%d%c", arr[i], i == len -1 ? '\n': ' ');
+
+	return 0;
+}
