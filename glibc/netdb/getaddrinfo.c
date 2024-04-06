9cda28e2abde440a76bd98ea041993344b394a87
Rong Tao <rongtao@cestc.cn>
2024-04-06 15:40:51 +0800

    glibc/netdb: Add getaddrinfo.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/netdb/getaddrinfo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+int main(void)
+{
+	int rc;
+	struct addrinfo hints, *res = NULL;
+
+	memset(&hints, 0, sizeof(hints));
+
+	hints.ai_family = PF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
+
+	rc = getaddrinfo("127.0.0.1", "123", &hints, &res);
+	if(rc != 0) {
+		perror("getaddrinfo error");
+		exit(1);
+	}
+	printf("getaddrinfo success\n");
+	return 0;
+}
+
