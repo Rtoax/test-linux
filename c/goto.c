5f11c41651a27c8309a07e036f3117731c923095
Rong Tao <rtoax@foxmail.com>
2024-04-06 10:00:32 +0800

    c/goto: Add jump_table test
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- c/goto.c
+++ c/goto.c
@@ -13,8 +13,32 @@ foo:
 		goto *ptr;
 }
 
+void test2(void)
+{
+	static void *jump_table[3] = {
+		&&label1, &&label2, && label3
+	};
+
+	int jmp = -1;
+
+start:
+	if (++jmp >= 3)
+		return;
+	goto *jump_table[jmp];
+
+label1:
+	printf("label1.\n");
+label2:
+	printf("label2.\n");
+label3:
+	printf("label3.\n");
+
+	goto start;
+}
+
 int main(void)
 {
 	test1();
+	test2();
 	return 0;
 }
