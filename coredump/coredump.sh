c39f718c34d3d39447fdb3260962df4fb5a4a644
Rong Tao <rongtao@cestc.cn>
2024-04-08 16:53:38 +0800

    coredump/coredump.sh: Replace '>' with '| sudo tee'
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- coredump/coredump.sh
+++ coredump/coredump.sh
@@ -3,11 +3,10 @@
 # run with it, generate a core.XXXX file.
 function coredump() {
 	ulimit -c unlimited
-	echo 1 > /proc/sys/kernel/core_uses_pid
-	#echo "./core.%t.%p" > /proc/sys/kernel/core_pattern
-	echo "./core.%p" > /proc/sys/kernel/core_pattern
-	echo 0 > /proc/sys/kernel/nmi_watchdog
-	echo 1 > /sys/module/rcupdate/parameters/rcu_cpu_stall_suppress
+	echo 1 | sudo tee /proc/sys/kernel/core_uses_pid
+	echo "./core.%p" | sudo tee /proc/sys/kernel/core_pattern
+	echo 0 | sudo tee /proc/sys/kernel/nmi_watchdog
+	echo 1 | sudo tee /sys/module/rcupdate/parameters/rcu_cpu_stall_suppress
 }
 
 if [ $# -gt 1 ]; then
