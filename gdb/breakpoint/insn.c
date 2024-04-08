5374f3d08288d28eb163121269a692fc631bb880
Rong Tao <rongtao@cestc.cn>
2024-04-08 10:01:46 +0800

    gdb/breakpoint/insn.c: catch SIGTRAP
        $ ./int3
        Usage: ./int3 [sig]
        i : 0
        Trace/breakpoint trap (core dumped)
    
        $ ./int3 sig
        Usage: ./int3 [sig]
        i : 0
        Get SIGTRAP.
        Get SIGTRAP.
        i : 1
        Get SIGTRAP.
        Get SIGTRAP.
        i : 2
        Get SIGTRAP.
        Get SIGTRAP.
        i : 3
        Get SIGTRAP.
        Get SIGTRAP.
        i : 4
        Get SIGTRAP.
        Get SIGTRAP.
        i : 5
        Get SIGTRAP.
        Get SIGTRAP.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- gdb/breakpoint/insn.c
+++ gdb/breakpoint/insn.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <signal.h>
 
 #if defined(__x86_64__) || defined(__i386__)
 # define BREAKPOINT()	__asm__("int3")
@@ -6,14 +7,25 @@
 # define BREAKPOINT()	__asm__("brk 0")
 #endif
 
+void sigtrap(int sig)
+{
+	printf("Get SIGTRAP.\n");
+}
+
 int func(void)
 {
 	BREAKPOINT();
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	int i = 0;
+
+	fprintf(stderr, "Usage: %s [sig]\n", argv[0]);
+
+	if (argc > 1)
+		signal(SIGTRAP, sigtrap);
+
 	while (i < 6){
 		printf("i : %d\n", i);
 		BREAKPOINT();
