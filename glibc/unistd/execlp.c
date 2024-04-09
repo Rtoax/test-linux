2e5f1c319f911ff57d0b85b5dbbde19d2e1601d9
Rong Tao <rongtao@cestc.cn>
2024-04-09 17:17:10 +0800

    dup2(2): Add fork-pipe.c sample
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/unistd/execlp.c
+++ glibc/unistd/execlp.c
@@ -7,8 +7,10 @@ int main(void)
 {
 	int fd;
 
-	fd = open("date.txt",O_RDWR | O_CREAT | O_TRUNC, 0644);
+	fd = open("date.txt", O_RDWR | O_CREAT | O_TRUNC, 0644);
+
 	dup2(fd, STDOUT_FILENO);
+
 	execlp("date", "date", NULL);
 
 	return 0;
