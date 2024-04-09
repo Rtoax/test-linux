66be898a64e2f0b3055917596098141ef1fde3fd
Rong Tao <rongtao@cestc.cn>
2024-04-09 17:33:44 +0800

    glibc: Add encrypt.c sample
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/crypt/encrypt.c
+++ glibc/crypt/encrypt.c
@@ -1,6 +1,20 @@
-#include <crypt.h>
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE
+#endif
+#include <unistd.h>
+#include <stdio.h>
 
 int main(void)
 {
+#if 0 // TODO
+	char key[64];	  /* bit pattern for key */
+	char txt[64];	  /* bit pattern for messages */
+
+	setkey(key);
+	encrypt(txt, 0);	/* encode */
+	encrypt(txt, 1);	/* decode */
+#endif
+
 	return 0;
 }
+
---
+++ glibc/unistd/encrypt.c
@@ -0,0 +1,1 @@
+../crypt/encrypt.c
