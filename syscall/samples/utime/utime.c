9affe96836c03aa3814115b704bbf6a0dbc8186f
Rong Tao <rongtao@cestc.cn>
2024-04-09 19:02:55 +0800

    utime(2): Add utime.c sample
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/utime/utime.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <utime.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[])
+{
+	int ret;
+	char *filename;
+	struct utimbuf utim;
+
+	filename = "a.txt";
+
+	utim.actime = 12;
+	utim.modtime = 13;
+
+	ret = utime(filename, &utim);
+	if (ret == -1) {
+		fprintf(stderr, "utime() %m\n");
+		return 0;
+	}
+
+	return 0;
+}
