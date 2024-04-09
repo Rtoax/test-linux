efb706b23cfda3375ae4e6823305a77afb79da50
Rong Tao <rongtao@cestc.cn>
2024-04-09 14:46:33 +0800

    gdb/breakpoint/insn.c: Update asm()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- gdb/breakpoint/insn.c
+++ gdb/breakpoint/insn.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <signal.h>
 
 #if defined(__x86_64__) || defined(__i386__)
-# define BREAKPOINT()	__asm__("int3")
+# define BREAKPOINT()	asm volatile("int3")
 #elif defined(__aarch64__)
-# define BREAKPOINT()	__asm__("brk 0")
+# define BREAKPOINT()	asm volatile("brk #0")
 #endif
+
+static char *prog = NULL;
 
 void sigtrap(int sig)
 {
@@ -17,14 +21,25 @@ int func(void)
 	BREAKPOINT();
 }
 
+void usage(void)
+{
+	fprintf(stderr, "Usage: %s [sig]\n", prog);
+}
+
 int main(int argc, char *argv[])
 {
 	int i = 0;
 
-	fprintf(stderr, "Usage: %s [sig]\n", argv[0]);
+	prog = argv[0];
 
-	if (argc > 1)
+	if (argc > 1) {
+		if (strcmp(argv[1], "sig")) {
+			usage();
+			exit(1);
+		}
 		signal(SIGTRAP, sigtrap);
+	}
+	usage();
 
 	while (i < 6){
 		printf("i : %d\n", i);
