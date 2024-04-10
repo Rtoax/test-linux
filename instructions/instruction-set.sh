a7a1738b7205e2d17084e3450034f5e587fafd54
Rong Tao <rongtao@cestc.cn>
2024-04-10 10:48:53 +0800

    instructions: instruction-set.sh: Add macros()
        $ ./instruction-set.sh
        skylake
        #define __SSE4_1__ 1
        #define __SSE4_2__ 1
        #define __MMX_WITH_SSE__ 1
        #define __SSE2_MATH__ 1
        #define __AVX__ 1
        #define __SSE_MATH__ 1
        #define __AVX2__ 1
        #define __SSSE3__ 1
        #define __SSE__ 1
        #define __SSE2__ 1
        #define __SSE3__ 1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/instruction-set.sh
+++ instructions/instruction-set.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-gcc -march=native -Q --help=target | grep -e '^  -march=' | awk '{print $2}'
+version() {
+	gcc -march=native -Q --help=target | grep -e '^  -march=' | awk '{print $2}'
+}
+
+macros() {
+	gcc -march=native -dM -E - < /dev/null
+}
+
+version
+macros | grep -e SSE -e AVX
