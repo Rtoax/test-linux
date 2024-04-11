778a84c060bddb43304d13710cdd84e068668c60
Rong Tao <rongtao@cestc.cn>
2024-04-11 18:48:32 +0800

    time/modules: clock-accuracy.c: Test ktime_get_real_ts64/ktime_get_coarse_real_ts64
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- time/modules/clock-accuracy.c
+++ time/modules/clock-accuracy.c
@@ -2,12 +2,37 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/printk.h>
+#include <linux/timekeeping.h>
 
+
+typedef void (*fn_ts64_t)(struct timespec64 *);
+
+static inline unsigned long nsecs64(fn_ts64_t fn)
+{
+	struct timespec64 ts;
+	fn(&ts);
+	return (ts.tv_sec * 1E9 + ts.tv_nsec);
+}
+
+static void test(char *name, fn_ts64_t fn)
+{
+	unsigned long start, end, cnt;
+
+	cnt = 0;
+	start = end = nsecs64(fn);
+	/* 5ms */
+	while (end - start < 5000000) {
+		cnt++;
+		end = nsecs64(fn);
+	}
+	printk(KERN_INFO "%-48s %-16ld %-16ld\n", name, end - start, cnt);
+}
 
 static int kernel_init(void)
 {
 	printk(KERN_INFO "Testing CLOCK accuracy.\n");
-	/* TODO */
+	test("REALTIME", ktime_get_real_ts64);
+	test("REALTIME_COARSE", ktime_get_coarse_real_ts64);
 	return -1;
 }
 
