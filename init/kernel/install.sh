a2d5f604827265cc48011de750c9a0927ab3864f
Rong Tao <rongtao@cestc.cn>
2024-04-11 11:14:47 +0800

    init: Add kernel/install.sh from ostools
    commit in ostools:
    
        commit 7b8f1c41d18cfb9291e4077317fa8a7a4f3b9742
        Author: Rong Tao <rongtao@cestc.cn>
        Date:   Fri Sep 22 19:24:06 2023 +0800
    
            kernel/install.sh: Add cross compile script
    
            Link: https://blog.csdn.net/shipinsky/article/details/127212874
            Signed-off-by: Rong Tao <rongtao@cestc.cn>
    
        commit be2921735287acaa3e5a4980d32443c7794ce880
        Author: Rong Tao <rongtao@cestc.cn>
        Date:   Tue Jan 17 16:39:04 2023 +0800
    
            grubby remove vmlinuz
    
            Signed-off-by: Rong Tao <rongtao@cestc.cn>
    
        commit fca4ea14140063f045e7a74b5e8c2257d74d844e
        Author: Rong Tao <rtoax@foxmail.com>
        Date:   Sat Nov 12 17:09:07 2022 +0800
    
            kernel: uninstall [TODO]
    
            Signed-off-by: Rong Tao <rtoax@foxmail.com>
    
        commit cf7f954e00d3c71faba40379ac1ed4f3544b9317
        Author: Rong Tao <rongtao@cestc.cn>
        Date:   Wed Sep 7 11:17:30 2022 +0800
    
            Install kernel from source
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ init/kernel/install.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -e
+
+cross_compile_env()
+{
+	export ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+	export CC=aarch64-linux-gnu-gcc
+	export INSTALL_PATH=$PWD
+	export INSTALL_MOD_PATH=$PWD/modules
+	export INSTALL_HDR_PATH=$PWD/headers
+}
+
+install_from_source()
+{
+	# Compile and install
+	sudo make -j$(nproc)
+	sudo make modules_install
+	sudo make headers_install
+	sudo make install
+
+	# Update grub
+	sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+	sudo grubby --set-default /boot/vmlinuz-6.0.0-rc4+
+
+	# Confirm
+	sudo grubby --info=ALL | more
+	sudo grubby --default-index
+	sudo grubby --default-kernel
+}
+
+uninstall_kernel()
+{
+	local version=$1
+	local modules=/lib/modules/$version
+	local vmlinuz=/boot/vmlinuz-$version
+	local initramfs=/boot/initramfs-$version.img
+
+	local curr_version=$(uname -r)
+
+	test $version == $curr_version && \
+		echo "Can't remove running kernel" && exit 1
+
+	test ! -d $modules && \
+		echo "$modules not exist" && exit 1
+	test ! -f $vmlinuz && \
+		echo "$vmlinuz not exist" && exit 1
+	test ! -f $initramfs && \
+		echo "$initramfs not exist" && exit 1
+
+
+	sudo grubby --remove-kernel /boot/vmlinuz-6.2.0-rc4+
+
+	sudo rm -rf $modules
+	sudo rm -f $vmlinuz
+	sudo rm -f $initramfs
+
+	sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+
+	# TODO
+	# grubby --set-default /boot/vmlinuz-...
+}
+
+case $1 in
+	install_from_source)
+		install_from_source
+		;;
+	*)
+echo -e "
+  install_from_source
+"
+		;;
+esac
+
