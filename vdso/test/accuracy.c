e7427573461e709124667b1a5ab642d095ba44ac
Rong Tao <rongtao@cestc.cn>
2024-04-11 15:53:55 +0800

    vdso/test: accuracy.c: Test on aarch64
        $ ./accuracy
        syscall REALTIME                                 5000448          15063
        syscall REALTIME_COARSE                          9999872          11038
        syscall MONOTONIC                                5000245          14786
        syscall MONOTONIC_COARSE                         10000037         16314
        syscall MONOTONIC_RAW                            5000140          14849
        syscall PROCESS_CPUTIME_ID                       5000033          11640
        vdso    REALTIME                                 5000192          83331
        vdso    REALTIME_COARSE                          10000128         249213
        vdso    MONOTONIC                                5000026          83325
        vdso    MONOTONIC_COARSE                         10000037         255129
        vdso    MONOTONIC_RAW                            5000042          79316
        vdso    PROCESS_CPUTIME_ID                       5000093          11512
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- vdso/test/accuracy.c
+++ vdso/test/accuracy.c
@@ -1,3 +1,10 @@
+/**
+ * Get syscall/vdso clock_gettime() accuracy
+ *
+ * Copyright CESTC, Co.
+ *
+ * 2024-04-11	Rong Tao	Create this.
+ */
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -44,14 +51,18 @@ void test(char *name, unsigned long (*nsecs)(clockid_t), clockid_t clockid)
 int main(int argc, char *argv[])
 {
 	test("syscall REALTIME", sys_nsecs, CLOCK_REALTIME);
+#if defined(__x86_64__) /* aarch64 run this clockid is too slow. */
 	test("syscall REALTIME_ALARM", sys_nsecs, CLOCK_REALTIME_ALARM);
+#endif
 	test("syscall REALTIME_COARSE", sys_nsecs, CLOCK_REALTIME_COARSE);
 	test("syscall MONOTONIC", sys_nsecs, CLOCK_MONOTONIC);
 	test("syscall MONOTONIC_COARSE", sys_nsecs, CLOCK_MONOTONIC_COARSE);
 	test("syscall MONOTONIC_RAW", sys_nsecs, CLOCK_MONOTONIC_RAW);
 	test("syscall PROCESS_CPUTIME_ID", sys_nsecs, CLOCK_PROCESS_CPUTIME_ID);
 	test("vdso    REALTIME", vdso_nsecs, CLOCK_REALTIME);
+#if defined(__x86_64__)
 	test("vdso    REALTIME_ALARM", vdso_nsecs, CLOCK_REALTIME_ALARM);
+#endif
 	test("vdso    REALTIME_COARSE", vdso_nsecs, CLOCK_REALTIME_COARSE);
 	test("vdso    MONOTONIC", vdso_nsecs, CLOCK_MONOTONIC);
 	test("vdso    MONOTONIC_COARSE", vdso_nsecs, CLOCK_MONOTONIC_COARSE);
