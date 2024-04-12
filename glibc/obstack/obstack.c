02874bfd0b444c5928bfb162a4020f31616cb8f0
Rong Tao <rongtao@cestc.cn>
2024-04-12 17:53:34 +0800

    glibc/obstack: Add obstack_room(), obstack_grow()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/obstack/obstack.c
+++ glibc/obstack/obstack.c
@@ -3,19 +3,48 @@
 #include <malloc.h>
 #include <obstack.h>
 
-#define obstack_chunk_alloc malloc
-#define obstack_chunk_free free
+#define debug() printf("%s:%d\n",__func__, __LINE__)
+
+void *mymalloc(ssize_t size)
+{
+	debug();
+	return malloc(size);
+}
+
+void myfree(void*ptr)
+{
+	debug();
+	free(ptr);
+}
+
+#define obstack_chunk_alloc mymalloc
+#define obstack_chunk_free myfree
 
 int main(void)
 {
-	struct obstack *obs;
+	struct obstack obs;
 	char *addr;
 
-	obstack_init(obs);
-	addr = obstack_alloc(obs, 100);
+	obstack_init(&obs);
 
-	obstack_copy(obs, addr, 20);
-	obstack_free(obs, addr);
+	printf("root = %d\n", obstack_room(&obs));
+
+	addr = obstack_alloc(&obs, 100);
+
+	printf("root = %d\n", obstack_room(&obs));
+
+	obstack_copy(&obs, addr, 20);
+
+	printf("root = %d\n", obstack_room(&obs));
+
+	obstack_grow(&obs, addr, 300);
+
+	printf("root = %d\n", obstack_room(&obs));
+
+	obstack_free(&obs, addr);
+	obstack_finish(&obs);
+
+	printf("root = %d\n", obstack_room(&obs));
 
 	return 0;
 }
