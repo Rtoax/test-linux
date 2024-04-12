9e6373f8e098f028971cb6fe4e9edeec596084cd
Rong Tao <rongtao@cestc.cn>
2024-04-12 20:34:07 +0800

    chmod(2): Good code.
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/stat/chmod.c
+++ syscall/samples/stat/chmod.c
@@ -4,9 +4,16 @@
 
 int main(void)
 {
-	mkdir("rong.out", 0777);
-	chmod("rong.out",
-	      S_ISUID | S_ISGID | S_ISVTX | S_IRUSR | S_IWUSR | S_IXUSR |
-	      S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+	char *dir = "rong.out";
+
+	mkdir(dir, 0777);
+
+	/* x for all */
+	chmod(dir, S_IXUSR | S_IXGRP | S_IXOTH);
+
+	chmod(dir,
+		S_ISUID | S_ISGID | S_ISVTX | S_IRUSR | S_IWUSR | S_IXUSR |
+		S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+
 	return 0;
 }
