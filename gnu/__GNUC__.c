b3435821f220a3cfacc589047c492899a7b47964
Rong Tao <rongtao@cestc.cn>
2024-04-12 19:50:20 +0800

    __GNUC__: Good code
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- gnu/__GNUC__.c
+++ gnu/__GNUC__.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-#if defined (__GNUC__) && ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 5) || __GNUC__ >= 5)
+#if defined(__GNUC__) && ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 5) || __GNUC__ >= 5)
+/* do some check */
 #endif
 
 int main(void)
