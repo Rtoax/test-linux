d0a8de6907f2b25a2cc23215c923368a5af40cff
Rong Tao <rongtao@cestc.cn>
2024-04-15 19:36:09 +0800

    macro: Add '#line' macro
        $ ./__FILE__
        __FILE__ = __FILE__.c
        __LINE__ = 6
        __FILE__ = xyh.c
        __LINE__ = 521
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- macro/__FILE__.c
+++ macro/__FILE__.c
@@ -4,5 +4,10 @@ int main(void)
 {
 	printf("__FILE__ = %s\n", __FILE__);
 	printf("__LINE__ = %d\n", __LINE__);
+
+#line 520 "xyh.c"
+	printf("__FILE__ = %s\n", __FILE__);
+	printf("__LINE__ = %d\n", __LINE__);
+
 	return 0;
 }
