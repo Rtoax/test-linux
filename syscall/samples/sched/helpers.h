e83cdcd2086a115bf3cccfa29720ea10a315c72a
Rong Tao <rongtao@cestc.cn>
2024-04-15 11:32:09 +0800

    {sched_getattr,sched_setattr}(2): Add sample
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/sched/helpers.h
+++ syscall/samples/sched/helpers.h
@@ -1,10 +1,39 @@
 #pragma once
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <stdint.h>
+#include <unistd.h>
 #include <sched.h>
+#include <sys/syscall.h>
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
+/* Could use linux/sched/types.h instead */
+struct sched_attr {
+	uint32_t size;
+
+	uint32_t sched_policy;
+	uint64_t sched_flags;
+
+	/* SCHED_NORMAL, SCHED_BATCH */
+	int32_t sched_nice;
+
+	/* SCHED_FIFO, SCHED_RR */
+	uint32_t sched_priority;
+
+	/* SCHED_DEADLINE (nsec) */
+	uint64_t sched_runtime;
+	uint64_t sched_deadline;
+	uint64_t sched_period;
+};
 
 long int sys_getcpu(unsigned *cpu, unsigned *node);
 void print_cpuset(cpu_set_t * cpuset);
 int str2cpuset(const char *cpulist, cpu_set_t * cpuset);
 const char *sched_policy_string(int policy);
+
+int sched_getattr(pid_t pid, struct sched_attr *attr, unsigned int size,
+		  unsigned int flags);
+int sched_setattr(pid_t pid, const struct sched_attr *attr, unsigned int flags);
 
---
+++ syscall/samples/sched/sched_setattr.c
@@ -0,0 +1,69 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
+#include <sched.h>
+#include <syscall.h>
+#include <sys/syscall.h>
+#include <pthread.h>
+
+#include "helpers.h"
+
+static volatile int done;
+
+void *run_deadline(void *data)
+{
+	struct sched_attr attr;
+	unsigned long x = 0;
+	int ret;
+	unsigned int flags = 0;
+
+	printf("deadline thread started [%d]\n", gettid());
+
+	attr.size = sizeof(attr);
+	attr.sched_flags = 0;
+	attr.sched_nice = 0;
+	attr.sched_priority = 0;
+
+	/* This creates a 10ms/30ms reservation */
+	attr.sched_policy = SCHED_DEADLINE;
+	attr.sched_runtime = 10 * 1000 * 1000;
+	attr.sched_period = attr.sched_deadline = 30 * 1000 * 1000;
+
+	ret = sched_setattr(0, &attr, flags);
+	if (ret < 0) {
+		done = 0;
+		perror("sched_setattr");
+		exit(-1);
+	}
+
+	while (!done)
+		x++;
+
+	printf("deadline thread dies [%d] x = %ld\n", gettid(), x);
+	return NULL;
+}
+
+int main(int argc, char **argv)
+{
+	pthread_t thread;
+
+	printf("main thread [%d]\n", gettid());
+
+	pthread_create(&thread, NULL, run_deadline, NULL);
+
+	sleep(2);
+
+	done = 1;
+
+	pthread_join(thread, NULL);
+
+	printf("main dies [%d]\n", gettid());
+	return 0;
+}
+
