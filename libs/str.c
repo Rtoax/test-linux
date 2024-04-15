1bb12842040086f629fbb7ba2f03d65bef5b08cd
Rong Tao <rongtao@cestc.cn>
2024-04-15 10:39:34 +0800

    libs/str: Add vstrjoint() vstrcat() vstrcat_r()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- libs/str.c
+++ libs/str.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include <string.h>
 #include <sys/types.h>
 
@@ -12,10 +13,64 @@ char *strcaseswap(char *str, ssize_t len)
 	return str;
 }
 
+char *vstrjoint(char *dst, const char *fmt, ...) __attribute__((__format__(printf, 2, 3)));
+char *vstrjoint(char *dst, const char *fmt, ...)
+{
+	if (dst == NULL || fmt == NULL)
+		return NULL;
+	va_list args;
+	va_start(args, fmt);
+	vsprintf(dst, fmt, args);
+	va_end(args);
+	return (char*)dst;
+}
+
+const char *vstrcat(int nstr, ...)
+{
+	int i;
+	va_list va;
+	static char S[1024] = {0};
+
+	memset(S, 0x00, sizeof(S));
+
+	va_start(va, nstr);
+	for (i = 0; i < nstr; i++) {
+		char *s = va_arg(va, char*);
+		strcat(S, s);
+	}
+	va_end(va);
+	return (char *)S;
+}
+
+const char *vstrcat_r(char buf[], int nstr, ...)
+{
+	int i;
+	va_list va;
+
+	va_start(va, nstr);
+	for (i = 0; i < nstr; i++) {
+		char *s = va_arg(va, char *);
+		strcat(buf, s);
+	}
+	va_end(va);
+	return (char *)buf;
+}
+
 #ifdef TEST
 int main(void)
 {
+	char buf[1024];
 	char str[] = {"AbasdADDadLJ"};
 	printf("%s->%s\n", str, strcaseswap(str, strlen(str)));
+
+	char cmd[256];
+	memset(cmd, 0, 256);
+	vstrjoint(cmd, "rongtao: %d", 123);
+	printf("cmd: %s\n", cmd);
+
+	printf("%s\n", vstrcat(3, "rongtao", "is", "me"));
+	printf("%s\n", vstrcat_r(buf, 3, "rongtao", "is", "me"));
+
+	return 0;
 }
 #endif
