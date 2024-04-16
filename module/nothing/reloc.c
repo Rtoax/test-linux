5c88724cb8d1f778f4d697b895619954e2f27215
Rong Tao <rongtao@cestc.cn>
2024-04-16 15:37:04 +0800

    module: nothing: Modify reloc.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- module/nothing/reloc.c
+++ module/nothing/reloc.c
@@ -1,5 +1,6 @@
 #include <linux/module.h>
 #include <linux/kthread.h>
+#include <linux/printk.h>
 
 int global_int1;
 int global_int2;
@@ -7,9 +8,11 @@ int global_int3 = 0xabcdffff;
 static int sglobal_int1;
 
 void func1(void) {}
-static void sfunc1(void) {
+static void sfunc1(void)
+{
 	/* This is not RELA item. */
 	kthread_run(NULL, NULL, NULL);
+	printk("%d\n", global_int1);
 }
 
 MODULE_LICENSE("Dual BSD/GPL");
