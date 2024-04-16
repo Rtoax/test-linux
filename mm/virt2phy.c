84b3ef56fb50ca8ba5b2133efde5c06da1076198
Rong Tao <rongtao@cestc.cn>
2024-04-16 19:22:35 +0800

    mm/virt2phy.c: Good code.
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- mm/virt2phy.c
+++ mm/virt2phy.c
@@ -9,28 +9,44 @@
 /**
  * https://www.kernel.org/doc/Documentation/vm/pagemap.txt
  */
-static uintptr_t virt_to_phy(void* virt)
+unsigned long virt_to_phy(unsigned long vaddr)
 {
+	int fd;
 	off_t ret;
-	uintptr_t entry = 0;
+	unsigned long pfn = 0;
 	ssize_t rc;
-	long pagesize = sysconf(_SC_PAGESIZE);
-	int fd = open("/proc/self/pagemap", O_RDONLY);
-	assert(fd != -1 && "failed to open /proc/self/pagemap");
-	ret = lseek(fd, (uintptr_t)virt / pagesize * sizeof(uintptr_t), SEEK_SET);
-	assert(ret != -1 && "lseek error");
-	rc = read(fd, &entry, sizeof(entry));
-	assert(rc > 0 && "read error");
-	assert(entry != 0 && "failed to get physical address (perhaps forgot sudo?)");
-	close(fd);
+	unsigned long pagesize = sysconf(_SC_PAGESIZE);
 
-	return (entry & 0x7fffffffffffffULL) * pagesize +
-		((uintptr_t)virt) % pagesize;
+	/* Get page frame number of address */
+	fd = open("/proc/self/pagemap", O_RDONLY);
+	if (fd == -1) {
+		fprintf(stderr, "open /proc/self/pagemap: %m\n");
+		goto failed;
+	}
+
+	ret = lseek(fd, vaddr / pagesize * sizeof(unsigned long), SEEK_SET);
+	if (ret == -1) {
+		fprintf(stderr, "lseek: %m\n");
+		goto failed;
+	}
+
+	rc = read(fd, &pfn, sizeof(pfn));
+	if (rc < sizeof(pfn) || pfn == 0) {
+		fprintf(stderr, "read: %m\n");
+		goto failed;
+	}
+
+	close(fd);
+	return (pfn & 0x7fffffffffffffULL) * pagesize + vaddr % pagesize;
+
+failed:
+	close(fd);
+	return 0;
 }
 
 int main(void)
 {
 	int i;
-	printf("%p %p\n", &i, virt_to_phy(&i));
+	printf("%#016lx %#016lx\n", &i, virt_to_phy(&i));
 	return 0;
 }
