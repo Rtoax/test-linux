8f304c6b830aecde599ad76ad06e52b3c753423e
Rong Tao <rongtao@cestc.cn>
2024-04-30 16:18:08 +0800

    glibc/math: Add some demos
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/math/sin.c
@@ -0,0 +1,32 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void)
+{
+	float fcosy, fsiny;
+	double x, cosy, siny;
+	long double cosx, sinx;
+
+	x = M_PI / 3;
+
+	printf("cos(%lf) = %f\n", x, cos(x));
+	printf("cosf(%lf) = %f\n", x, cosf(x));
+	printf("cosl(%Lf) = %Lf\n", (long double)x, cosl(x));
+
+	printf("sin(%lf) = %f\n", x, sin(x));
+	printf("sinf(%lf) = %f\n", x, sinf(x));
+	printf("sinl(%Lf) = %Lf\n", (long double)x, sinl(x));
+
+	printf("tan(%lf) = %f\n", x, tan(x));
+	printf("tanf(%lf) = %f\n", x, tanf(x));
+	printf("tanl(%Lf) = %Lf\n", (long double)x, tanl(x));
+
+	sincos(x, &siny, &cosy);
+	printf("sin(%f) = %lf, cos(%f) = %lf\n", x, siny, x, cosy);
+	sincosf(x, &fsiny, &fcosy);
+	sincosl(x, &sinx, &cosx);
+
+	return 0;
+}
