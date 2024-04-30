4399f17e6f9a5b96fbdc5d78c27c6f29aaf0a05a
Rong Tao <rongtao@cestc.cn>
2024-04-30 13:55:39 +0800

    glibc/malloc: Add malloc_trim.c and helpers.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/malloc/malloc_trim.c
+++ glibc/malloc/malloc_trim.c
@@ -5,10 +5,14 @@
 
 int main(void)
 {
-	char *p1 = malloc(1024);
+	char *p1, *p2;
+	p1 = malloc(1024);
+	(void)p1;
 	print_mallinfo();
-	free(p1);
-	p1 = malloc(2024);
+	p2 = malloc(2024);
+	(void)p2;
 	print_mallinfo();
-	free(p1);
+	malloc_trim(0);
+	print_mallinfo();
 }
+
