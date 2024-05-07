f45415b35193fbd2fe040a88972a45907604f625
Rong Tao <rongtao@cestc.cn>
2024-05-07 10:41:12 +0800

    glibc/stdio: Add setbuf.c
        $ ./setbuf
        Rong Tao ❤ Xue Yuehua
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/stdio/setvbuf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	char string[256] = {"I love you, Yuehua Xue!"};
+	char outbuf[BUFSIZ];
+
+	setbuf(stdout, outbuf);
+	setvbuf(stdout, outbuf, _IOFBF, sizeof(string));
+	setbuffer(stdout, outbuf, sizeof(string));
+	setlinebuf(stdout);
+
+	fflush(stdout);
+
+	for (i = 0; i < 32; i++)
+		fprintf(stdout, "Rong Tao \033[1;5;31m❤\033[m %s\n", "Xue Yuehua");
+
+	printf("%s\n", string);
+
+	return 0;
+}
+
+
