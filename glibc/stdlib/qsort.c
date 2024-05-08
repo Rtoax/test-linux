0bb62261e74fff0483bf5fa436db718614ebb793
Rong Tao <rongtao@cestc.cn>
2024-05-08 14:42:59 +0800

    glibc/stdlib: qsort.c: Add test_int()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/stdlib/qsort.c
+++ glibc/stdlib/qsort.c
@@ -9,6 +9,13 @@ int cmpstringp(const void *p1, const void *p2)
 	return strcmp(*(const char **)p1, *(const char **)p2);
 }
 
+int cmpintp(const void *p1, const void *p2)
+{
+	const int i1 = *(const int *)p1;
+	const int i2 = *(const int *)p2;
+	return i1 - i2;
+}
+
 void test_string(void)
 {
 	int j;
@@ -23,9 +30,22 @@ void test_string(void)
 		puts(strs[j]);
 }
 
+void test_int(void)
+{
+	int j;
+
+	int ints[] = { 5, 2, 0, 1, 3, 1, 4};
+
+	qsort(ints, ARRAY_SIZE(ints), sizeof(int), cmpintp);
+
+	for (j = 0; j < ARRAY_SIZE(ints); j++)
+		printf("%d\n", ints[j]);
+}
+
 int main(int argc, char *argv[])
 {
 	test_string();
+	test_int();
 
 	return 0;
 }
