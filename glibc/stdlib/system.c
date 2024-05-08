adcb00bf90721229be1204ca549747d8a7c01e40
Rong Tao <rongtao@cestc.cn>
2024-05-08 13:50:52 +0800

    glibc/stdlib: system.c: Add sh_cmd()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/stdlib/system.c
+++ glibc/stdlib/system.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <string.h>
+
+int sh_cmd(const char *fmt, ...)
+{
+	char cmd[1024];
+	va_list arg;
+
+	va_start(arg, fmt);
+	vsprintf(cmd, fmt, arg);
+	printf("DUMP: %s\n", cmd);
+
+	system(cmd);
+	va_end(arg);
+	return 0;
+}
 
 int main(void)
 {
 	system("mkdir a.dir");
+
+	sh_cmd("cat %s", "/etc/os-release");
+
 	return 0;
 }
