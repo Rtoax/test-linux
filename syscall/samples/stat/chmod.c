2ef235df9861944616be8a0bf2d6eefdb76981f5
Rong Tao <rongtao@cestc.cn>
2024-05-08 10:39:04 +0800

    chmod(2): 0777
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/stat/chmod.c
+++ syscall/samples/stat/chmod.c
@@ -5,15 +5,22 @@
 int main(void)
 {
 	char *dir = "rong.out";
+	int modeall = 0;
+
+	modeall |= S_ISUID | S_ISGID | S_ISVTX;
+	modeall |= S_IRUSR | S_IWUSR | S_IXUSR;
+	modeall |= S_IRGRP | S_IWGRP | S_IXGRP;
+	modeall |= S_IROTH | S_IWOTH | S_IXOTH;
+
+	printf("modeall = %x\n", modeall);
+	printf("0777 = %x\n", 0777);
 
 	mkdir(dir, 0777);
 
 	/* x for all */
 	chmod(dir, S_IXUSR | S_IXGRP | S_IXOTH);
 
-	chmod(dir,
-		S_ISUID | S_ISGID | S_ISVTX | S_IRUSR | S_IWUSR | S_IXUSR |
-		S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+	chmod(dir, modeall);
 
 	return 0;
 }
