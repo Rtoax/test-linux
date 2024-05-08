f0d710b7978b48cbb3259b6ddc10a4ce67c56489
Rong Tao <rongtao@cestc.cn>
2024-05-08 19:11:13 +0800

    chown(2): Chmod to root (uid = 0)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/chown.c
+++ syscall/samples/chown.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/stat.h>		/*S_IRUSR */
 
 int main(int argc, char *argv[])
 {
-	chown(argv[1], S_ISUID, S_ISGID);
+	int ret;
+	char *file = argv[1];
+
+	ret = chown(file, 0, 0);
+	if (ret == -1) {
+		fprintf(stderr, "chown %m\n");
+		return -errno;
+	}
 	return 0;
 }
