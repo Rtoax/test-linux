fbc247f1f04aa7b6de7a56fc5855a699e92606a2
Rong Tao <rongtao@cestc.cn>
2024-05-09 12:33:39 +0800

    glibc/stdlib: Add mkostemp.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/stdlib/mkostemp.c
@@ -0,0 +1,21 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int main(void)
+{
+	int ret;
+	char file[256] = {"/tmp/key-XXXXXX"};
+
+	printf("file: %s\n", file);
+	ret = mkostemp(file, O_RDWR | O_CREAT);
+	perror("mkostemp: ");
+	printf("file: %s, ret = %d\n", file, ret);
+
+	unlink(file);
+
+	return 0;
+}
