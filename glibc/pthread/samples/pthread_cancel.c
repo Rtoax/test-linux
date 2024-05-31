4690c1ee928e5f338588022581886874d9fcc36e
Rong Tao <rongtao@cestc.cn>
2024-05-31 17:27:55 +0800

    pthread/samples: Add comments for pthread cancel demos
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/pthread/samples/pthread_cancel.c
+++ glibc/pthread/samples/pthread_cancel.c
@@ -1,4 +1,5 @@
 /**
+ * Pthread cancel.
  *
  * PTHREAD_CANCEL_ENABLE: default
  * PTHREAD_CANCEL_DISABLE
@@ -24,21 +25,45 @@ void *test_task_fn(void *unused)
 {
 	printf("child thread sleeping.\n");
 
-	/* Disable cancel */
+	/**
+	 * Disable cancel
+	 * 线程准备执行一些关键工作，在这个过程中不希望被取消
+	 * 所以先通过pthread_setcancelstate()将本线程的cancel state 设为disabled
+	 */
 	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
 
 	/* Do some important job, that can't be cancelled */
+	printf("do something.\n");
 	sleep(2);
 
-	/* Enable cancel */
+	/**
+	 * Enable cancel
+	 * 键工作执行完成，可以被取消
+	 * 通过pthread_setcancelstate()将本线程的cancel state 设为enabled
+	 */
 	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
 
-	/* Check cancel request queue, and try exit */
+	/**
+	 * Check cancel request queue, and try exit
+	 * 调用pthread_testcancel()函数，检查一下在cancel state 为disabled状态的
+	 * 时候，是否有取消请求发送给本线程。如果有的话就取消（退出）.
+	 */
 	pthread_testcancel();
 
+	/**
+	 * pthread_testcancel()返回了，表明之前没有取消请求发送给本线程
+	 * 继续其余的工作.
+	 * 这时候如果有取消请求发送给本线程，会在下一次执行到cancellation point
+	 * 的时候(例如sleep(), read(), write(), ...)时取消。
+	 */
 	printf("Nobody give me a cancel.\n");
 
-	/* Cancel immediate if cancel request received */
+	/**
+	 * Cancel immediate if cancel request received
+	 * 从这里开始，函数里不再包含cancellation point了
+	 * 如果收到取消请求，将无法取消。所以先把本线程的cancel type
+	 * 设为asynchronous异步，收到取消请求将立即取消。
+	 */
 	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
 
 	static int status = 12121;
