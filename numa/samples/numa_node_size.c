ec8cf7d2554befdf1081c18b0b003356f97b18dc
Rong Tao <rongtao@cestc.cn>
2024-05-31 11:09:13 +0800

    numa/samples: numa_node_size() freep is long long int in any architecture
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- numa/samples/numa_node_size.c
+++ numa/samples/numa_node_size.c
@@ -4,16 +4,10 @@
 int main(void)
 {
 	long ret;
-#if defined(__sw_64__)
 	long long freep = 0;
-#define FREEP_FMT	"%lld"
-#else
-	long freep = 0;
-#define FREEP_FMT	"%ld"
-#endif
 
 	ret = numa_node_size(0, &freep);
-	printf("numa_node_size : ret = %ld G, freep = "FREEP_FMT" G\n",
+	printf("numa_node_size : ret = %ld G, freep = %lld G\n",
 		ret / 1024 / 1024, freep / 1024 / 1024);
 
 	return 0;
