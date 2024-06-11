ae2c3a2098db5fc4834d2787422c90b56bfb17d8
Rong Tao <rongtao@cestc.cn>
2024-06-11 17:13:45 +0800

    glibc/setjmp: Add sigsetjmp.c and link siglongjmp.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ glibc/setjmp/sigsetjmp.c
@@ -0,0 +1,27 @@
+#include <setjmp.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+sigjmp_buf main_loop;
+
+void abort_to_main_loop(int status)
+{
+	printf("%s:%d\n", __func__, status);
+	siglongjmp(main_loop, status);
+}
+
+void do_command(void)
+{
+	printf("%s\n", __func__);
+	abort_to_main_loop(1);
+}
+
+int main(void)
+{
+	if (sigsetjmp(main_loop, 1))
+		puts("Back at main loop....");
+	else
+		do_command();
+	return 0;
+}
+
