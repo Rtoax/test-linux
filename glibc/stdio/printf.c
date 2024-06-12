724b11024fe2774ffd344d5949b5ff7fdd5f98d0
Rong Tao <rongtao@cestc.cn>
2024-06-12 14:57:03 +0800

    glibc/stdio: printf.c: Add more format.
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/stdio/printf.c
+++ glibc/stdio/printf.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+
+#define STR	"hello world"
 
 int main(void)
 {
-	char tmp[256] = {"rong"};
-	printf("%s\n", tmp);
+	int i;
+
+	for (i = 0; i <= strlen(STR); i++)
+		printf("%3d/%ld  [%.*s]\n", i, strlen(STR), i, STR);
+
+	printf("KB/s \n");
+
+	printf("%*.*d\n", 3, 10, 10);
+	printf("%.*s\n", 4, "hello");
+	printf("%*s\n", 40, "hell0");
+	printf("%*.*s\n", 40, 4, "hell0");
+
+	printf("%.*d\n", 4, 10000);
+	printf("%*d\n", 40, 10000);
+	printf("%*.*d\n", 40, 4, 10000);
+
+	printf("Hello %.*d %.*d\n", 5, 2, 5, 3);
 	return 0;
 }
