7e343e4317473a918c30443e2690e4da689a71f3
Rong Tao <rongtao@cestc.cn>
2024-06-13 11:39:48 +0800

    glibc/termios: tcgetattr.c: Test socket fd
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/termios/tcgetattr.c
+++ glibc/termios/tcgetattr.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <sys/socket.h>
 #include <termios.h>
+#include <unistd.h>
 
 #include "helpers.h"
 
-int main(void)
+int socket_fd(void)
+{
+	int sockfd, ret;
+	struct termios t;
+
+	sockfd = socket(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0);
+	ret = tcgetattr(sockfd, &t);
+	if (ret) {
+		fprintf(stderr, "ERROR: failed tcgetattr from socket fd.\n");
+	} else {
+		print_termios(&t);
+	}
+
+	close(sockfd);
+	return ret;
+}
+
+int std_fd(void)
 {
 	struct termios t;
 
@@ -11,5 +30,12 @@ int main(void)
 
 	print_termios(&t);
 
+	return 0;
+}
+
+int main(void)
+{
+	std_fd();
+	socket_fd();
 	return 0;
 }
