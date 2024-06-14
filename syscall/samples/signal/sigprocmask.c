8a35fd9155bb8de7c3ec47fa426387c2873dd6a5
Rong Tao <rongtao@cestc.cn>
2024-06-14 09:11:23 +0800

    syscall/samples/signal: Add sigprocmask.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/signal/sigprocmask.c
+++ syscall/samples/signal/sigprocmask.c
@@ -1,1 +1,27 @@
-alarm-sleep-2.c
+#include <signal.h>
+#include <stdio.h>
+
+volatile sig_atomic_t flag = 0;
+
+int main(void)
+{
+	sigset_t block_alarm;
+
+	/* Initialize the signal mask. */
+	sigemptyset(&block_alarm);
+	sigaddset(&block_alarm, SIGALRM);
+
+	while (1) {
+		/* Check if a signal has arrived; if so, reset the flag. */
+		sigprocmask(SIG_BLOCK, &block_alarm, NULL);
+		if (flag) {
+			/* actions-if-not-arrived */
+			printf("flag = 0.\n");
+			flag = 0;
+		}
+		sigprocmask(SIG_UNBLOCK, &block_alarm, NULL);
+	}
+
+	return 0;
+}
+
