80b69b86e0f9d4b92356ee67fa3344edd7e9972d
Rong Tao <rongtao@cestc.cn>
2024-06-19 11:41:17 +0800

    uefi/qemu: mkimg.sh: Rename directory name
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- init/uefi/qemu/mkimg.sh
+++ init/uefi/qemu/mkimg.sh
@@ -19,11 +19,11 @@ esac
 
 dd if=/dev/zero of=boot.img bs=1M count=512
 sudo mkfs.vfat boot.img
-mkdir -p boot.mnt
-sudo mount boot.img boot.mnt
-sudo mkdir -p boot.mnt/efi/boot/
+mkdir -p boot.efi.mnt
+sudo mount boot.img boot.efi.mnt
+sudo mkdir -p boot.efi.mnt/efi/boot/
 
-sudo cp /boot/efi/EFI/${ID}/grub${EFI_ARCH}.efi boot.mnt/efi/boot/boot${EFI_ARCH}.efi
-sudo cp grub.cfg boot.mnt/efi/boot/grub.cfg
-sudo umount boot.mnt
-rmdir boot.mnt
+sudo cp /boot/efi/EFI/${ID}/grub${EFI_ARCH}.efi boot.efi.mnt/efi/boot/boot${EFI_ARCH}.efi
+sudo cp grub.cfg boot.efi.mnt/efi/boot/grub.cfg
+sudo umount boot.efi.mnt
+rmdir boot.efi.mnt
