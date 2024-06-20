78be26706103779a56cec21e5d1ba23ae3b0c115
Rong Tao <rongtao@cestc.cn>
2024-06-20 18:59:34 +0800

    uefi/qemu: qemu.sh: Adapt to aarch64
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- init/uefi/qemu/qemu.sh
+++ init/uefi/qemu/qemu.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+set -ex
+
+declare -a args
+
+. ../../../libs/qemu.sh
+
+qemu_emulator=$(get_qemu_kvm_emulator)
 
 cp /usr/share/OVMF/OVMF_CODE.fd ovmf.fd
 
+if [[ $(uname -m) == aarch64 ]]; then
+	args+=( -machine virt )
+	# Try to fix: device requires 67108864 bytes, block backend provides 1966080 bytes
+	truncate --size=64M ovmf.fd
+fi
+
 # Pass '-cdrom boot.img' if boot.img only is vfat
-qemu-system-x86_64 -m 2048 -drive file=./ovmf.fd,format=raw,if=pflash \
+${qemu_emulator} ${args[@]} \
+	-m 2048 -drive file=./ovmf.fd,format=raw,if=pflash \
 	-drive file=./boot.img,format=raw
