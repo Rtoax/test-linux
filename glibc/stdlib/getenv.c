2569d7bf6671531fb1f8b543075639e89811f018
Rong Tao <rongtao@cestc.cn>
2024-06-21 16:22:27 +0800

    getenv(3): C_INCLUDE_PATH
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/stdlib/getenv.c
+++ glibc/stdlib/getenv.c
@@ -7,9 +7,12 @@ int main(void)
 {
 	char *env = getenv("OLDPWD");
 	char *listenq = getenv("LISTENQ");
+	/* can't get GCC env C_INCLUDE_PATH */
+	char *c_include_path = getenv("C_INCLUDE_PATH");
 
 	printf("env = %s\n", env);
 	printf("listenq = %s\n", listenq);
+	printf("C_INCLUDE_PATH = %s\n", c_include_path);
 
 	return 0;
 }
