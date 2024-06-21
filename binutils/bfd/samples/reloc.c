3f13f0e498e30fef31009c948ae8000a1c72fecf
Rong Tao <rongtao@cestc.cn>
2024-06-20 23:06:56 -0400

    binutils/bfd/samples/reloc.c: Fix error: ‘BFD_RELOC_S12Z_OPR’ undeclared
    reloc.c: In function ‘main’:
    reloc.c:6:17: error: ‘BFD_RELOC_S12Z_OPR’ undeclared (first use in this function); did you mean ‘BFD_RELOC_RX_24_OP’?
      printf("%d\n", BFD_RELOC_S12Z_OPR);
                     ^~~~~~~~~~~~~~~~~~
                     BFD_RELOC_RX_24_OP
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- binutils/bfd/samples/reloc.c
+++ binutils/bfd/samples/reloc.c
@@ -3,6 +3,10 @@
 
 int main(void)
 {
-	printf("%d\n", BFD_RELOC_S12Z_OPR);
+#if defined(__aarch64__)
+	printf("%d\n", BFD_RELOC_AARCH64_JUMP26);
+#elif defined(__x86_64__)
+	printf("%d\n", BFD_RELOC_X86_64_GOT32);
+#endif
 	return 0;
 }
