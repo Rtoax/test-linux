40738d3158f4e9b95927ba463e9ef0e515966203
Rong Tao <rongtao@cestc.cn>
2024-06-23 10:43:40 +0800

    livepatch/modules: estimation_timer.c: Add ip_vs_chain_estimation()
    Kernel: commit 705dd3444081 ("ipvs: use kthreads for stats estimation")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- livepatch/modules/estimation_timer.c
+++ livepatch/modules/estimation_timer.c
@@ -5,18 +5,33 @@
 #include <linux/livepatch.h>
 #include <linux/timer.h>
 #include <linux/seq_file.h>
+#include <linux/version.h>
 
+
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0)
 static void livepatch_estimation_timer(struct timer_list *t)
 {
 	return;
 }
-
 static struct klp_func funcs[] = {
 	{
 		.old_name = "estimation_timer",
 		.new_func = livepatch_estimation_timer,
 	}, { }
 };
+#else
+static void livepatch_ip_vs_chain_estimation(struct hlist_head *chain)
+{
+	return;
+}
+static struct klp_func funcs[] = {
+	{
+		.old_name = "ip_vs_chain_estimation",
+		.new_func = livepatch_ip_vs_chain_estimation,
+	}, { }
+};
+#endif
 
 static struct klp_object objs[] = {
 	{
