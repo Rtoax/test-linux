a3f000a3c8cac6c8d45b824bd7ebe0221be075c4
Rong Tao <rongtao@cestc.cn>
2024-06-28 14:08:34 +0800

    procfs: Test /proc/thread-self
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- fs/procfs/self.c
+++ fs/procfs/self.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -13,9 +14,20 @@ pid_t get_pid_from_proc_self(void)
 	return (pid_t)pid;
 }
 
+pid_t get_tid_from_proc_thread_self(void)
+{
+	char target[64];
+	int tid;
+	readlink("/proc/thread-self", target, sizeof(target));
+	sscanf(target, "%d", &tid);
+	return (pid_t)tid;
+}
+
 int main(void)
 {
 	printf("/proc/self reports process id %d\n", get_pid_from_proc_self());
 	printf("getpid() reports process id %d\n", getpid());
+	printf("/proc/thread-self reports thread id %d\n", get_tid_from_proc_thread_self());
+	printf("gettid() reports thread id %d\n", gettid());
 	return 0;
 }
