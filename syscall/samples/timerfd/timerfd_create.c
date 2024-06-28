f505ebfcfe5b2c5a527c21a318bb7e1de485b84f
Rong Tao <rongtao@cestc.cn>
2024-06-28 19:22:34 +0800

    syscall/samples: Add timerfd/
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/timerfd/timerfd_create.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/timerfd.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+#define TFMT	"[%ld,%ld]"
+#define T(t)  t.tv_sec,t.tv_nsec
+
+void sig_handler(int signum)
+{
+	printf("signal handler.\n");
+	exit(0);
+	return;
+}
+
+int main(void)
+{
+	int ret;
+	uint64_t exp = 0;
+	struct itimerspec t1 = {{1,0},{1,0}}, t2;
+
+	signal(SIGINT, sig_handler);
+
+	int timerfd = timerfd_create(CLOCK_REALTIME, TFD_CLOEXEC);
+	printf("Timer FD: %d\n", timerfd);
+
+	ret = timerfd_settime(timerfd, 0, &t1, &t2);
+	printf("[%d, new]: "TFMT","TFMT"\n", ret, T(t1.it_interval), T(t1.it_value));
+	printf("[%d, old]: "TFMT","TFMT"\n", ret, T(t2.it_interval), T(t2.it_value));
+
+	ret = timerfd_gettime(timerfd, &t1);
+	printf("[%d, get]: "TFMT","TFMT"\n", ret, T(t1.it_interval), T(t1.it_value));
+
+	while (1) {
+		read(timerfd, &exp, sizeof(exp));
+
+		printf("exp  =%ld\n", exp);
+		close(timerfd);
+		break;
+	}
+
+	return 0;
+}
---
+++ syscall/samples/timerfd/timerfd_select.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/timerfd.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+void sig_handler(int signum)
+{
+	printf("signal handler.\n");
+	exit(0);
+	return;
+}
+
+#define TFMT	"[%ld,%ld]"
+#define T(t)  t.tv_sec,t.tv_nsec
+
+#define N_TIMER 3
+static int timersfd[N_TIMER] = {0};
+
+int main(void)
+{
+	int maxfd = 0, nready = 0;
+	int i;
+	uint64_t exp = 0;
+	struct itimerspec t1 = { {1, 0}, {1, 0}};
+	fd_set readtimersfd;
+
+	FD_ZERO(&readtimersfd);
+
+	signal(SIGINT, sig_handler);
+
+	for (i = 0; i < N_TIMER; i++) {
+		timersfd[i] = timerfd_create(CLOCK_REALTIME, TFD_CLOEXEC);
+		timerfd_settime(timersfd[i], 0, &t1, NULL);
+		printf("timerfd = %d\n", timersfd[i]);
+		FD_SET(timersfd[i], &readtimersfd);
+		if (maxfd < timersfd[i])
+			maxfd = timersfd[i];
+	}
+	printf("maxfd = %d\n", maxfd);
+
+	while (1) {
+		fd_set allset = readtimersfd;
+		static int count = 0;
+		nready = select(maxfd+1, &allset, NULL, NULL, NULL);
+		printf("nready = %d, count = %d\n", nready, count++);
+		for (i = 0; i < N_TIMER; i++) {
+			if (FD_ISSET(timersfd[i], &allset)) {
+				read(timersfd[i], &exp, sizeof(exp));
+				printf("exp = %ld\n", exp);
+				if (timersfd[i] == 4)
+					FD_CLR(timersfd[i], &readtimersfd);
+			}
+		}
+	}
+
+	return 0;
+}
+
