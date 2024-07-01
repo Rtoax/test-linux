63f353050a9f87887af9cd63f69a9b652d420d4c
Rong Tao <rongtao@cestc.cn>
2024-07-01 21:00:15 +0800

    sysctl(2): Add demo
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/deprecated/sysctl.c
+++ syscall/deprecated/sysctl.c
@@ -1,15 +1,45 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/syscall.h>
 #include <linux/sysctl.h>
 
+#define OSNAMESZ 128
+
+#if 0
 int sysctl(int *names, int nlen, void *oldval, size_t *oldlenp, void *newval,
 	   size_t newlen)
 {
 	return syscall(__NR__sysctl, names, nlen, oldval, oldlenp, newval, newlen);
 }
+#endif
+int sysctl(struct __sysctl_args *args)
+{
+	return syscall(__NR__sysctl, args);
+}
 
 int main(void)
 {
+	struct __sysctl_args args;
+	char osname[OSNAMESZ];
+	size_t osnamelth;
+	int name[] = {CTL_KERN, KERN_OSTYPE};
+
+	memset(&args, 0, sizeof(struct __sysctl_args));
+	args.name = name;
+	args.nlen = sizeof(name) / sizeof(name[0]);
+	args.oldval = osname;
+	args.oldlenp = &osnamelth;
+
+	osnamelth = sizeof(osname);
+
+	if (sysctl(&args) == -1) {
+		perror("_sysctl");
+		exit(EXIT_FAILURE);
+	}
+	printf("This machine is running %*s\n", (int)osnamelth, osname);
+	exit(EXIT_SUCCESS);
 	return 0;
 }
 
