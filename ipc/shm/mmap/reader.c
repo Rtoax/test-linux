d122fb781a1e6796d04f47a50ab34a277630ec84
Rong Tao <rongtao@cestc.cn>
2024-07-02 12:45:23 +0800

    ipc/shm/mmap: Perfect code
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- ipc/shm/mmap/reader.c
+++ ipc/shm/mmap/reader.c
@@ -10,14 +10,26 @@ int main(void)
 {
 	int fd;
 	char *p;
+	struct stat stat;
+	size_t len = 0x400000;
 
-	fd = shm_open("posix_shm", O_RDONLY, 0666);
-	ftruncate(fd, 0x400000);
+	fd = shm_open("posix_shm", O_RDONLY, S_IWUSR | S_IWOTH);
 
-	p = mmap(NULL, 0x400000, PROT_READ, MAP_SHARED, fd, 0);
+	ftruncate(fd, len);
+
+	fstat(fd, &stat);
+	printf("stat.st_size = %ld\n", stat.st_size);
+
+	p = mmap(NULL, len, PROT_READ, MAP_SHARED, fd, 0);
+	if (p == MAP_FAILED) {
+		fprintf(stderr, "mmap return NULL.\n");
+		goto exit;
+	}
 
 	printf("%c %c %c %c\n", p[0], p[1], p[2], p[3]);
-	munmap(p, 0x400000);
 
+exit:
+	munmap(p, len);
+	shm_unlink("posix_shm");
 	return 0;
 }
--- ipc/shm/mmap/writer.c
+++ ipc/shm/mmap/writer.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/mman.h>
@@ -9,14 +10,19 @@ int main(void)
 {
 	char *p;
 	int fd;
+	struct stat stat;
+	size_t len = 0x400000;
 
 	fd = shm_open("posix_shm", O_CREAT | O_RDWR, 0666);
-	ftruncate(fd, 0x400000);
+	ftruncate(fd, len);
 
-	p = mmap(NULL, 0x400000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	fstat(fd, &stat);
+	printf("stat.st_size = %ld\n", stat.st_size);
 
-	memset(p, 'A', 0x400000);
-	munmap(p, 0x400000);
+	p = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+	memset(p, 'A', len);
+	munmap(p, len);
 
 	return 0;
 }
