32073c4e57e87f0e6625744d4e2648f8ac32751d
Rong Tao <rongtao@cestc.cn>
2024-07-02 09:53:19 +0800

    accept(2): Return EINTR?? (todo)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- ipc/socket/syscall/tcp-srv-1.c
+++ ipc/socket/syscall/tcp-srv-1.c
@@ -6,9 +6,19 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define PORT 5678
 #define MAX 10
+
+void sig_handler(int sig)
+{
+	switch (sig) {
+	case SIGINT:
+		printf("catch ctrl-c.\n");
+		break;
+	}
+}
 
 int main(void)
 {
@@ -16,6 +26,8 @@ int main(void)
 
 	struct sockaddr_in addr;
 	socklen_t addr_len = sizeof(struct sockaddr_in);
+
+	signal(SIGINT, sig_handler);
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -28,6 +40,15 @@ int main(void)
 	listen(sockfd, MAX);
 
 	newsockfd = accept(sockfd, (struct sockaddr *)&addr, &addr_len);
+	/**
+	 * FIXME: 在多进程服务器编写时候，父进程accept，但是会遇到EINTR错误：
+	 * 父进程接收链接请求，accept 阻塞的时候被信号中断，处理信号对应的操作
+	 * 之后，回来之后不阻塞了，直接返回 -1 ，这时候 errno == EINTR。
+	 *
+	 * TODO: 如何测试？
+	 */
+	while (newsockfd == -1 && errno == EINTR)
+		newsockfd = accept(sockfd, (struct sockaddr *)&addr, &addr_len);
 
 	printf("Get new client.\n");
 
