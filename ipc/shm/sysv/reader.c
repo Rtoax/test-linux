549a0ff1b179a3580a630f8e0eb166ef2ce29e9a
Rong Tao <rongtao@cestc.cn>
2024-07-03 17:49:25 +0800

    ipc/shm/sysv: Code format
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- ipc/shm/sysv/reader.c
+++ ipc/shm/sysv/reader.c
@@ -6,9 +6,13 @@
 
 int main(void)
 {
-	key_t key = ftok("/dev/shm/myshm1", 0);
-	int shm_id = shmget(key, 0x400000, 0666);
-	char *p = (char*)shmat(shm_id, NULL, 0);
+	key_t key;
+	int shm_id;
+	char *p;
+
+	key = ftok("/dev/shm/myshm1", 0);
+	shm_id = shmget(key, 0x400000, 0666);
+	p = (char*)shmat(shm_id, NULL, 0);
 
 	printf("%c %c %c %c .\n", p[0], p[1], p[2], p[3]);
 	shmdt(p);
--- ipc/shm/sysv/writer.c
+++ ipc/shm/sysv/writer.c
@@ -5,9 +5,13 @@
 
 int main(void)
 {
-	key_t key = ftok("/dev/shm/myshm1", 0);
-	int shm_id = shmget(key, 0x400000, IPC_CREAT | 0666);
-	char *p = (char*)shmat(shm_id, NULL, 0);
+	key_t key;
+	int shm_id;
+	char *p;
+
+	key = ftok("/dev/shm/myshm1", 0);
+	shm_id = shmget(key, 0x400000, IPC_CREAT | 0666);
+	p = (char *)shmat(shm_id, NULL, 0);
 
 	memset(p, 'A', 0x400000);
 	shmdt(p);
