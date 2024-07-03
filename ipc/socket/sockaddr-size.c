546b17a612dcffdd7a7c39c2a56cc689bed2d46d
Rong Tao <rongtao@cestc.cn>
2024-07-03 16:59:25 +0800

    ipc/socket: Add sockaddr-size.c
        $ ./sockaddr-size
        sizeof(struct sockaddr_in) = 16
        sizeof(struct sockaddr_ll) = 20
        sizeof(struct sockaddr_un) = 110
        sizeof(struct sockaddr_storage) = 128
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ ipc/socket/sockaddr-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <netpacket/packet.h>   /* struct sockaddr_ll */
+#include <sys/un.h>             /* unsocket head file */
+#include <sys/types.h>
+#include <unistd.h>             /* socket close will use */
+#include <netinet/in.h>         /* htons/htonl*/
+
+int main(void)
+{
+#define SIZEOF(s) printf("sizeof(%s) = %ld\n", #s, sizeof(s));
+
+	SIZEOF(struct sockaddr_in);
+	SIZEOF(struct sockaddr_ll);
+	SIZEOF(struct sockaddr_un);
+	SIZEOF(struct sockaddr_storage);
+
+	return 0;
+}
