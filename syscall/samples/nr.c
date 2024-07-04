bf17d1aca848a82ddb642a6e2ba90d774bd28123
Rong Tao <rongtao@cestc.cn>
2024-07-04 14:00:24 +0800

    syscall/samples: nr.c: syscall number to string
        $ ./nr
         __NR_read :    0(0000) read
        __NR_write :    1(0x01) write
         __NR_open :    2(0x02) open
        __NR_close :    3(0x03) close
         __NR_stat :    4(0x04) stat
        __NR_fstat :    5(0x05) fstat
        __NR_lstat :    6(0x06) lstat
         __NR_poll :    7(0x07) poll
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/nr.c
+++ syscall/samples/nr.c
@@ -3,9 +3,20 @@
 #include <syscall.h>
 #include <sys/syscall.h>
 
+struct {
+	int num;
+	char *name;
+} syscall_str[] = {
+#define NR_SYS(nr)
+#define __NR_SYS(nr)	[__NR_##nr] = { .num = __NR_##nr, .name = #nr },
+#include "nr.h"
+#undef __NR_SYS
+#undef NR_SYS
+};
+
 int main(void)
 {
-#define NR_SYS(nr)	printf("%30s : %4d(%#04x)\n", #nr, nr, nr);
+#define NR_SYS(nr)	printf("%30s : %4d(%#04x) %s\n", #nr, nr, nr, syscall_str[nr].name);
 #include "nr.h"
 #undef NR_SYS
 
