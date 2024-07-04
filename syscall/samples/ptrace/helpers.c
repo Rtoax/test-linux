b76662243bc49aa064a730e861788d1c1cb39f12
Rong Tao <rongtao@cestc.cn>
2024-07-04 18:55:26 +0800

    syscall/samples/ptrace: helpers: Pretty print_registers() print on x86
    Like gdb did
    
    (gdb) info registers
    rax            0x40122d            4198957
    rbx            0x7fffffffd558      140737488344408
    rcx            0x403e00            4210176
    rdx            0x7fffffffd568      140737488344424
    rsi            0x7fffffffd558      140737488344408
    rdi            0x1                 1
    rbp            0x7fffffffd430      0x7fffffffd430
    rsp            0x7fffffffd420      0x7fffffffd420
    r8             0x0                 0
    r9             0x7ffff7fcded0      140737353932496
    r10            0x7fffffffd160      140737488343392
    r11            0x203               515
    r12            0x1                 1
    r13            0x0                 0
    r14            0x7ffff7ffd000      140737354125312
    r15            0x403e00            4210176
    rip            0x401235            0x401235 <main+8>
    eflags         0x202               [ IF ]
    cs             0x33                51
    ss             0x2b                43
    ds             0x0                 0
    es             0x0                 0
    fs             0x0                 0
    gs             0x0                 0
    fs_base        0x7ffff7dab740      140737351694144
    gs_base        0x0                 0
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/ptrace/helpers.c
+++ syscall/samples/ptrace/helpers.c
@@ -17,33 +17,35 @@ const char *find_syscall_symbol(int code)
 #ifdef __x86_64__
 void print_registers(struct user_regs_struct *regs)
 {
-	printf("r15     = 0x%016llx\n", regs->r15);
-	printf("r14     = 0x%016llx\n", regs->r14);
-	printf("r13     = 0x%016llx\n", regs->r13);
-	printf("r12     = 0x%016llx\n", regs->r12);
-	printf("rbp     = 0x%016llx\n", regs->rbp);
-	printf("rbx     = 0x%016llx\n", regs->rbx);
-	printf("r11     = 0x%016llx\n", regs->r11);
-	printf("r10     = 0x%016llx\n", regs->r10);
-	printf("r9      = 0x%016llx\n", regs->r9);
-	printf("r8      = 0x%016llx\n", regs->r8);
-	printf("rax     = 0x%016llx\n", regs->rax);
-	printf("rcx     = 0x%016llx\n", regs->rcx);
-	printf("rdx     = 0x%016llx\n", regs->rdx);
-	printf("rsi     = 0x%016llx\n", regs->rsi);
-	printf("rdi     = 0x%016llx\n", regs->rdi);
-	printf("orig_rax = 0x%016llx\n", regs->orig_rax);
-	printf("rip     = 0x%016llx\n", regs->rip);
-	printf("cs      = 0x%016llx\n", regs->cs);
-	printf("eflags  = 0x%016llx\n", regs->eflags);
-	printf("rsp     = 0x%016llx\n", regs->rsp);
-	printf("ss      = 0x%016llx\n", regs->ss);
-	printf("fs_base = 0x%016llx\n", regs->fs_base);
-	printf("gs_base = 0x%016llx\n", regs->gs_base);
-	printf("ds      = 0x%016llx\n", regs->ds);
-	printf("es      = 0x%016llx\n", regs->es);
-	printf("fs      = 0x%016llx\n", regs->fs);
-	printf("gs      = 0x%016llx\n", regs->gs);
+#define DOUBLE(a) a, a
+	printf("r15      = 0x%016llx %lld\n", DOUBLE(regs->r15));
+	printf("r14      = 0x%016llx %lld\n", DOUBLE(regs->r14));
+	printf("r13      = 0x%016llx %lld\n", DOUBLE(regs->r13));
+	printf("r12      = 0x%016llx %lld\n", DOUBLE(regs->r12));
+	printf("rbp      = 0x%016llx %lld\n", DOUBLE(regs->rbp));
+	printf("rbx      = 0x%016llx %lld\n", DOUBLE(regs->rbx));
+	printf("r11      = 0x%016llx %lld\n", DOUBLE(regs->r11));
+	printf("r10      = 0x%016llx %lld\n", DOUBLE(regs->r10));
+	printf("r9       = 0x%016llx %lld\n", DOUBLE(regs->r9));
+	printf("r8       = 0x%016llx %lld\n", DOUBLE(regs->r8));
+	printf("rax      = 0x%016llx %lld\n", DOUBLE(regs->rax));
+	printf("rcx      = 0x%016llx %lld\n", DOUBLE(regs->rcx));
+	printf("rdx      = 0x%016llx %lld\n", DOUBLE(regs->rdx));
+	printf("rsi      = 0x%016llx %lld\n", DOUBLE(regs->rsi));
+	printf("rdi      = 0x%016llx %lld\n", DOUBLE(regs->rdi));
+	printf("orig_rax = 0x%016llx %lld\n", DOUBLE(regs->orig_rax));
+	printf("rip      = 0x%016llx %lld\n", DOUBLE(regs->rip));
+	printf("cs       = 0x%016llx %lld\n", DOUBLE(regs->cs));
+	printf("eflags   = 0x%016llx %lld\n", DOUBLE(regs->eflags));
+	printf("rsp      = 0x%016llx %lld\n", DOUBLE(regs->rsp));
+	printf("ss       = 0x%016llx %lld\n", DOUBLE(regs->ss));
+	printf("fs_base  = 0x%016llx %lld\n", DOUBLE(regs->fs_base));
+	printf("gs_base  = 0x%016llx %lld\n", DOUBLE(regs->gs_base));
+	printf("ds       = 0x%016llx %lld\n", DOUBLE(regs->ds));
+	printf("es       = 0x%016llx %lld\n", DOUBLE(regs->es));
+	printf("fs       = 0x%016llx %lld\n", DOUBLE(regs->fs));
+	printf("gs       = 0x%016llx %lld\n", DOUBLE(regs->gs));
+#undef DOUBLE
 }
 #elif defined(__aarch64__)
 void print_registers(struct user_regs_struct *regs)
