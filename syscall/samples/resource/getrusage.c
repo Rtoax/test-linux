d0668d5e4a1bf69e7fc2e441e9d85dd9663ef526
Rong Tao <rongtao@cestc.cn>
2024-07-04 09:06:04 +0800

    getrusage(2): Call user and sys time (todo)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/resource/getrusage.c
+++ syscall/samples/resource/getrusage.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <sys/resource.h>
+#include <unistd.h>
 
 #include "helpers.h"
+
+void user_sys(void)
+{
+	struct rusage rsue, rsue2;
+	float user, sys;
+
+	getrusage(getpid(), &rsue);
+	usleep(100);
+	getrusage(getpid(), &rsue2);
+
+	user = (float)rsue.ru_utime.tv_sec + rsue.ru_utime.tv_usec / 1000000.0;
+	user += (float)rsue2.ru_utime.tv_sec + rsue2.ru_utime.tv_usec / 1000000.0;
+
+	sys = (float)rsue.ru_stime.tv_sec + rsue.ru_stime.tv_usec / 1000000.0;
+	sys += (float)rsue2.ru_stime.tv_sec + rsue2.ru_stime.tv_usec / 1000000.0;
+
+	/* FIXME: wrong value i think */
+	printf("user: %g, sys time: %g\n", user, sys);
+}
 
 int main(void)
 {
@@ -9,6 +29,8 @@ int main(void)
 
 	getrusage(RUSAGE_SELF, &rusage);
 	print_rusage(&rusage);
+
+	user_sys();
 
 	return 0;
 }
