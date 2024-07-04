089ead506458fe4abe0fce8b44bea6d336495829
Rong Tao <rongtao@cestc.cn>
2024-07-04 19:17:05 +0800

    ptrace(2): aarch64 have PTRACE_PEEKUSER ?? (todo)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/ptrace/PTRACE_PEEKUSER.c
+++ syscall/samples/ptrace/PTRACE_PEEKUSER.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <syscall.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#if defined(__x86_64__)
 #include <sys/reg.h>		/* For constants ORIG_RAX etc */
+#endif
 
 int main(void)
 {
 	pid_t child;
-	long orig_rax, rip;
 
 	child = fork();
 	if (child == 0) {
@@ -17,6 +19,9 @@ int main(void)
 		execl("/bin/ls", "ls", NULL);
 	} else {
 		wait(NULL);
+
+#if defined(__x86_64__)
+		long orig_rax, rip;
 
 		orig_rax = ptrace(PTRACE_PEEKUSER, child,
 				  sizeof(unsigned long) * ORIG_RAX, NULL);
@@ -26,6 +31,12 @@ int main(void)
 		printf("__NR_execve = %d\n", __NR_execve);
 		printf("The child made a system call %ld\n", orig_rax);
 		printf("RIP %lx\n", rip);
+#elif defined(__aarch64__)
+# pragma message("FIXME: aarch64 don't have PTRACE_PEEKUSER?????????")
+		ptrace(PTRACE_PEEKUSER, child, 0, NULL);
+		perror("ptrace");
+#endif
+
 		ptrace(PTRACE_CONT, child, NULL, NULL);
 	}
 	return 0;
