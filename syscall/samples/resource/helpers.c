d008a607b19ffacb744fa0da50775b4acef18056
Rong Tao <rongtao@cestc.cn>
2024-07-04 09:02:11 +0800

    syscall/samples/resource: print_rusage(): Add more fields
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/resource/helpers.c
+++ syscall/samples/resource/helpers.c
@@ -12,8 +12,18 @@ void print_rusage(const struct rusage *rusage)
 	printf ("CPU time: %ld.%06ld sec user, %ld.%06ld sec system\n",
 		rusage->ru_utime.tv_sec, rusage->ru_utime.tv_usec,
 		rusage->ru_stime.tv_sec, rusage->ru_stime.tv_usec);
-	printf("maxrss = %ld\n", rusage->ru_maxrss);
-	printf("ixrss = %ld\n", rusage->ru_ixrss);
-	printf("isrss = %ld\n", rusage->ru_isrss);
-	printf("idrss = %ld\n", rusage->ru_idrss);
+	printf("maxrss: %ld\n", rusage->ru_maxrss);
+	printf("ru_ixrss: %ld\n", rusage->ru_ixrss);
+	printf("ru_idrss: %ld\n", rusage->ru_idrss);
+	printf("ru_isrss: %ld\n", rusage->ru_isrss);
+	printf("ru_minflt: %ld\n", rusage->ru_minflt);
+	printf("ru_majflt: %ld\n", rusage->ru_majflt);
+	printf("ru_nswap: %ld\n", rusage->ru_nswap);
+	printf("ru_inblock: %ld\n", rusage->ru_inblock);
+	printf("ru_oublock: %ld\n", rusage->ru_oublock);
+	printf("ru_msgsnd: %ld\n", rusage->ru_msgsnd);
+	printf("ru_msgrcv: %ld\n", rusage->ru_msgrcv);
+	printf("ru_nsignals: %ld\n", rusage->ru_nsignals);
+	printf("ru_nvcsw: %ld\n", rusage->ru_nvcsw);
+	printf("ru_nivcsw: %ld\n", rusage->ru_nivcsw);
 }
