41bae8d75d317dc12b88944b2ef6cc37f9abf651
Rong Tao <rongtao@cestc.cn>
2024-07-04 19:24:20 +0800

    ptrace(2): PTRACE_PEEKDATA.c: Code format
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/ptrace/PTRACE_PEEKDATA.c
+++ syscall/samples/ptrace/PTRACE_PEEKDATA.c
@@ -18,7 +18,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define long_size sizeof(long)
+#define LONG_SIZE sizeof(long)
 
 void reverse(char *str)
 {
@@ -37,26 +37,26 @@ void getdata(pid_t child, long addr, char *str, int len)
 	int i, j;
 	union u {
 		long val;
-		char chars[long_size];
+		char chars[LONG_SIZE];
 	} data;
 	i = 0;
-	j = len / long_size;
+	j = len / LONG_SIZE;
 	laddr = str;
 	while (i < j) {
-		data.val =
-		    ptrace(PTRACE_PEEKDATA, child, addr + i * long_size, NULL);
+		data.val = ptrace(PTRACE_PEEKDATA, child, addr + i * LONG_SIZE,
+				  NULL);
 		if (data.val == -1) {
 			if (errno)
 				printf("READ error: %s\n", strerror(errno));
 		}
-		memcpy(laddr, data.chars, long_size);
+		memcpy(laddr, data.chars, LONG_SIZE);
 		++i;
-		laddr += long_size;
+		laddr += LONG_SIZE;
 	}
-	j = len % long_size;
+	j = len % LONG_SIZE;
 	if (j != 0) {
-		data.val =
-		    ptrace(PTRACE_PEEKDATA, child, addr + i * long_size, NULL);
+		data.val = ptrace(PTRACE_PEEKDATA, child, addr + i * LONG_SIZE,
+				  NULL);
 		memcpy(laddr, data.chars, j);
 	}
 	str[len] = '\0';
@@ -68,25 +68,25 @@ void putdata(pid_t child, long addr, char *str, int len)
 	int i, j;
 	union u {
 		long val;
-		char chars[long_size];
+		char chars[LONG_SIZE];
 	} data;
 	i = 0;
-	j = len / long_size;
+	j = len / LONG_SIZE;
 	laddr = str;
 	while (i < j) {
-		memcpy(data.chars, laddr, long_size);
-		ptrace(PTRACE_POKEDATA, child, addr + i * long_size, data.val);
+		memcpy(data.chars, laddr, LONG_SIZE);
+		ptrace(PTRACE_POKEDATA, child, addr + i * LONG_SIZE, data.val);
 		++i;
-		laddr += long_size;
+		laddr += LONG_SIZE;
 	}
-	j = len % long_size;
+	j = len % LONG_SIZE;
 	if (j != 0) {
 		/**
 		 * 注意：由于写入时也是按字写入的，所以正确的做法是先将该字的高
 		 * 地址数据读出保存在data的高地址上 ，然后将该字再写入
 		 */
 		memcpy(data.chars, laddr, j);
-		ptrace(PTRACE_POKEDATA, child, addr + i * long_size, data.val);
+		ptrace(PTRACE_POKEDATA, child, addr + i * LONG_SIZE, data.val);
 	}
 }
 
@@ -94,44 +94,46 @@ int main(void)
 {
 	pid_t child;
 	int status;
+	long orig_eax;
+	char *str;
+	int toggle = 0;
 	struct user_regs_struct regs;
+
 	child = fork();
 	if (child == 0) {
 		ptrace(PTRACE_TRACEME, 0, NULL, NULL);
 		execl("/bin/ls", "ls", NULL);
-	} else {
-		long orig_eax;
-		char *str;
-		int toggle = 0;
-		while (1) {
-			wait(&status);
-			if (WIFEXITED(status))
-				break;
-			orig_eax =
-			    ptrace(PTRACE_PEEKUSER, child, 8 * ORIG_RAX, NULL);
-			/**
-			 * ssize_t write(int fd, const void buf[.count], size_t count);
-			 * rax = fd;
-			 * rsi = buf;
-			 * rdx = count;
-			 */
-			if (orig_eax == SYS_write) {
-				if (toggle == 0) {
-					toggle = 1;
-					ptrace(PTRACE_GETREGS, child, NULL,
-					       &regs);
-					str =
-					    (char *)calloc((regs.rdx + 1),
-							   sizeof(char));
-					getdata(child, regs.rsi, str, regs.rdx);
-					reverse(str);
-					putdata(child, regs.rsi, str, regs.rdx);
-				} else {
-					toggle = 0;
-				}
+	}
+
+	/* Parent only */
+	while (1) {
+		wait(&status);
+		if (WIFEXITED(status))
+			break;
+
+		orig_eax = ptrace(PTRACE_PEEKUSER, child, 8 * ORIG_RAX,
+				  NULL);
+		/**
+		 * ssize_t write(int fd, const void buf[.count], size_t count);
+		 * rax = fd;
+		 * rsi = buf;
+		 * rdx = count;
+		 */
+		if (orig_eax == SYS_write) {
+			if (toggle == 0) {
+				toggle = 1;
+				ptrace(PTRACE_GETREGS, child, NULL,
+				       &regs);
+				str = (char *)calloc((regs.rdx + 1),
+						     sizeof(char));
+				getdata(child, regs.rsi, str, regs.rdx);
+				reverse(str);
+				putdata(child, regs.rsi, str, regs.rdx);
+			} else {
+				toggle = 0;
 			}
-			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 		}
+		ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 	}
 	return 0;
 }
