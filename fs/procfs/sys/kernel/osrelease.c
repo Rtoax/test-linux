fbe9b6a33f341b9a20168033432a58dc61a7e9f2
Rong Tao <rongtao@cestc.cn>
2024-07-11 19:10:15 +0800

    Add fs/procfs/sys/kernel/osrelease.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ fs/procfs/sys/kernel/osrelease.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void parse_version(void)
+{
+	int a1, a2, a3;
+	FILE *fp = fopen("/proc/sys/kernel/osrelease", "r");
+
+	if (fp == NULL || fscanf(fp, "%d.%d.%d", &a1, &a2, &a3) != 3)
+		return;
+
+	printf("actual %d %d %d\n", a1, a2,a3);
+
+	fclose(fp);
+}
+
+int main(void)
+{
+	parse_version();
+	return 0;
+}
