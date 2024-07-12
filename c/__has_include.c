2cf046935c207391fdfc035c3247cfd2b230fb2e
Rong Tao <rongtao@cestc.cn>
2024-07-12 09:08:55 +0800

    {c,macro}: Add __has_include macro test
    Link: https://gcc.gnu.org/onlinedocs/cpp/_005f_005fhas_005finclude.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ c/__has_include.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#if defined __has_include
+#  if __has_include (<stdatomic.h>)
+#    include <stdatomic.h>
+#    pragma message "Include stdatomic.h"
+#  endif
+#endif
+
+int main(void)
+{
+	return 0;
+}
