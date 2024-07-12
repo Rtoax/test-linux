7de71a4b546d400570b9d7ead83bf5077e49f0c4
Rong Tao <rongtao@cestc.cn>
2024-07-12 15:23:40 +0800

    procfs: Add kpageflags.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ fs/procfs/kpageflags.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define PROC_KPAGEFLAGS         "/proc/kpageflags"
+#define PROC_KPAGECOUNT         "/proc/kpagecount"
+#define PROC_KPAGECGROUP        "/proc/kpagecgroup"
+
+int main(void)
+{
+	// TODO
+	return 0;
+}
