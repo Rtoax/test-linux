2121d0311fe13dea05e80a8d7b8fd4a40e6f20e3
Rong Tao <rongtao@cestc.cn>
2024-07-16 17:29:07 +0800

    instructions/examples/sve: Makefile: Check compiler support SVE feature
    Add macro check_compiler_option()
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/sve/aarch64.sh
+++ instructions/examples/sve/aarch64.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+SYSROOT=/home/rongtao/rootfs-aarch64
+
 if [[ $(uname -m) == aarch64 ]]; then
 	echo "ERROR: You already on aarch64 host"
 	exit 1
 fi
 
+if [[ ! -d ${SYSROOT} ]]; then
+	echo "ERROR: ${SYSROOT} is not exist."
+	exit 1
+fi
+
 make clean
-make STATIC=1 CROSS_COMPILE=1 SYSROOT=/home/rongtao/rootfs-aarch64
+make STATIC=1 CROSS_COMPILE=1 SYSROOT=${SYSROOT} "$@"
