6d680f6e8b5ab0580511a22dfd7c6de945c4e983
Rong Tao <rongtao@cestc.cn>
2024-07-16 14:17:52 +0800

    sve: Makefile: Support static compile
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/sve/sve-width.c
+++ instructions/examples/sve/sve-width.c
@@ -8,10 +8,10 @@ int main(void)
 	uint64_t lanes;
 
 	__asm__ __volatile__("CNTB %[lanes]" : [lanes]"=r"(lanes) : : );
-	printf("SVE vector width is %d bytes or %d bits.\n", lanes, lanes * 8);
+	printf("SVE vector width is %d bytes(%d bits).\n", lanes, lanes * 8);
 
 	lanes = svcntb();
-	printf("SVE vector width is %d bytes or %d bits.\n", lanes, lanes * 8);
+	printf("SVE vector width is %d bytes(%d bits).\n", lanes, lanes * 8);
 
 	return 0;
 }
