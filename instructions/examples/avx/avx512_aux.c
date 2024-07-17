34aba1d665bf7972ba180eb63486245a81968ffc
Rong Tao <rongtao@cestc.cn>
2024-07-17 11:34:22 +0800

    instructions/examples/avx/Makefile: Add clang_emit_llvm if CC=clang
       $ ./clang.sh
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/avx/avx512_aux.c
+++ instructions/examples/avx/avx512_aux.c
@@ -3,7 +3,7 @@
 #include <immintrin.h>
 
 __m512i avx512_test(__m512i x0, __m512i x1, __m512i x2, __m512i x3,
-		__m512i x4, __m512i x5, __m512i x6, __m512i x7)
+		    __m512i x4, __m512i x5, __m512i x6, __m512i x7)
 {
 	__m512i zmm;
 
@@ -52,8 +52,10 @@ int tst_avx512_aux(void)
 	__m512i zmm5 = _mm512_set1_epi32(5);
 	__m512i zmm6 = _mm512_set1_epi32(6);
 	__m512i zmm7 = _mm512_set1_epi32(7);
+
 	__m512i ret = avx512_test(zmm0, zmm1, zmm2, zmm3, zmm4, zmm5, zmm6, zmm7);
 	zmm0 = _mm512_set1_epi32(0x12349876);
+
 	if (memcmp(&zmm0, &ret, sizeof(ret)))
 		abort();
 	return 0;
