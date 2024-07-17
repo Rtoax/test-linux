fb0bf1c62e69b1b13ac2495387725bc34d8824d0
Rong Tao <rongtao@cestc.cn>
2024-07-17 22:22:09 +0800

    ipc/socket/tcp: Add ftp/
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ ipc/socket/tcp/ftp/ftpclnt.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
+#define PORT 1234
+
+void error_handling(char* message);
+
+int main(int argc,char* argv[])
+{
+	int sock;
+	struct sockaddr_in addr;
+	FILE* fp;
+	char buf[BUF_SIZE];
+	int str_len;
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(PORT);
+
+	if (connect(sock, (struct sockaddr *)&addr, sizeof(addr))==-1)
+		error_handling("connect() error");
+
+	fp = fopen("os-release", "w+");
+	if (fp != NULL) {
+		while ((str_len = read(sock, buf, BUF_SIZE)) > 0) {
+			fwrite(buf, sizeof(char), str_len, fp);
+		}
+		write(sock,"Thank You!",11);
+		fclose(fp);
+	}
+	close(sock);
+	return 0;
+}
+
+void error_handling(char* message)
+{
+	fputs(message,stderr);
+	fputc('\n',stderr);
+	exit(1);
+}
