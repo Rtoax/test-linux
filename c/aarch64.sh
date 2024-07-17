655a1bd790bab9996a4e589dac46fa150befe0ec
Rong Tao <rongtao@cestc.cn>
2024-07-17 18:34:37 +0800

    c/aarch64.sh: Default -static
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/aarch64.sh
+++ c/aarch64.sh
@@ -2,5 +2,5 @@
 
 [[ $(uname -m) == aarch64 ]] && echo "ERROR: You already on aarch64, no need to cross compile" && exit 1
 
-make ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu- SYSROOT=/home/rongtao/rootfs-aarch64 "${@}"
+make STATIC=1 ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu- SYSROOT=/home/rongtao/rootfs-aarch64 "${@}"
 
