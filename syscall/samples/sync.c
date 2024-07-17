23cbbf60d46e210daeebb322ec818d116d8e6a02
Rong Tao <rongtao@cestc.cn>
2024-07-17 09:17:40 +0800

    syscall/samples: Seperate syncfs.c from sync.c and good Makefile
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/sync.c
+++ syscall/samples/sync.c
@@ -9,38 +9,8 @@ void sys_sync(void)
 	syscall(SYS_sync);
 }
 
-int sys_syncfs(int fd)
-{
-	return syscall(SYS_syncfs, fd);
-}
-
 int main(void)
 {
-	int fd, ret;
-	mode_t mode;
-	char *path;
-
-	path = "tmp.txt";
-
-	unlink(path);
-
-	/**
-	 * flag make file could not exist.
-	 */
-	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	fd = open(path, O_WRONLY | O_EXCL | O_CREAT, mode);
-	if (fd == -1) {
-		perror("open");
-		return 1;
-	}
-
 	sync();
-	ret = syncfs(fd);
-	if (ret) {
-		fprintf(stderr, "syncfs: %m\n");
-	}
-
-	close(fd);
-
 	return 0;
 }
--- syscall/samples/syncfs.c
+++ syscall/samples/syncfs.c
@@ -1,1 +1,40 @@
-sync.c
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <syscall.h>
+
+int sys_syncfs(int fd)
+{
+	return syscall(SYS_syncfs, fd);
+}
+
+int main(void)
+{
+	int fd, ret;
+	mode_t mode;
+	char *path;
+
+	path = "tmp.txt";
+
+	unlink(path);
+
+	/**
+	 * flag make file could not exist.
+	 */
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd = open(path, O_WRONLY | O_EXCL | O_CREAT, mode);
+	if (fd == -1) {
+		perror("open");
+		return 1;
+	}
+
+	ret = syncfs(fd);
+	if (ret) {
+		fprintf(stderr, "syncfs: %m\n");
+	}
+
+	close(fd);
+
+	return 0;
+}
