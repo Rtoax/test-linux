b119554295417e451026adaeef2926d29c6da792
Rong Tao <rongtao@cestc.cn>
2024-07-17 18:52:42 +0800

    java: Add url_GET.java
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ java/url_GET.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class url_GET
+{
+	public static void main(String[] args)
+	{
+		String url = "http://www.baidu.com";
+		String result = "";
+		BufferedReader in = null;
+		try {
+			URL realUrl = new URL(url);
+			URLConnection connection = realUrl.openConnection();
+			connection.connect();
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line + "\n";
+			}
+		} catch (Exception e) {
+			System.out.println("Send GET failed " + e);
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		System.out.println(result);
+	}
+}
+
