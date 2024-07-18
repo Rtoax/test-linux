21fd576511941282857724570c8f020e5b8d47bb
Rong Tao <rongtao@cestc.cn>
2024-07-18 09:44:44 +0800

    avx2: vperm2i128.asm: Use to check avx2 feature
    AVX [1] don't has AVX2 [0] instruction vperm2i128
    
    Link: https://docs.oracle.com/cd/E36784_01/html/E36859/gntae.html [0]
    Link: https://docs.oracle.com/cd/E36784_01/html/E36859/gntbd.html [1]
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/avx/vperm2i128.asm
+++ instructions/examples/avx/vperm2i128.asm
@@ -1,4 +1,6 @@
-; avx2
+; vperm2i128 is AVX2 instruction, not in AVX, this assembler use to check
+; cpu feature contains AVX2 or not, if support trigger segvfault.
+; Illegal instruction if not support.
 ; https://docs.oracle.com/cd/E36784_01/html/E36859/gntae.html
 	global  _start
 	section .text
