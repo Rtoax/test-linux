05fd92aa16e812df926237e595ee70746a101b51
Rong Tao <rongtao@cestc.cn>
2024-07-21 15:59:29 +0800

    glibc/string: Add strrchr() test
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/string/strchr.c
+++ glibc/string/strchr.c
@@ -1,11 +1,26 @@
 #define _GNU_SOURCE
+#include <malloc.h>
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(void)
 {
-	char *str = "rongtao zaima";
-	printf("%s\n", strchr(str, 't'));
+	char *s_addr, *filename;
+	char *str = "/bin/bash:0x00000000000d1c70";
+	unsigned long offset;
 
+	printf("%s\n", strchr(str, ':'));
+
+	/**
+	 * See kernel trace_uprobe_create()
+	 */
+	filename = strdup(str);
+	s_addr = strrchr(filename, ':');
+	*s_addr++ = '\0';
+	offset = strtoul(s_addr, NULL, 0);
+	printf("%s %s(%lx)\n", filename, s_addr, offset);
+
+	free(filename);
 	return 0;
 }
