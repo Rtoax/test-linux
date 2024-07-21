f3c3f82190cdafc072ef70d56cdb88c22a51e899
Rong Tao <rongtao@cestc.cn>
2024-07-21 11:35:28 +0800

    gcc: Fix 'gcc -o -' will generated file '-' error on fedora40 x86
    see man::gcc
    
      compiling to (x86_64) assembly via -S and emitting the result direct to stdout via -o -
    
          gcc -S test.c -fverbose-asm -Os -o -
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- scripts/check_compiler_option.mk
+++ scripts/check_compiler_option.mk
@@ -3,6 +3,6 @@
 # $(2) - compile option
 define check_compiler_option
   $(shell echo 'int main(void) { return 0; }' | \
-    $(1) -x c -Wall - $(2) -S -o - >/dev/null 2>&1 \
+    $(1) -x c -Wall - $(2) -S -o /dev/null >/dev/null 2>&1 \
       && echo 1)
 endef
