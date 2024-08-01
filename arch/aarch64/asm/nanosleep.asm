980d05a40570eec6798721c5b873a4393d1e15b6
Rong Tao <rongtao@cestc.cn>
2024-08-01 17:36:41 +0800

    arch/aarch64/asm: Add nanosleep.asm
    Link: https://stackoverflow.com/questions/72653344/how-to-call-nanosleep-from-assembly-for-aarch64-m1
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ arch/aarch64/asm/nanosleep.asm
@@ -0,0 +1,17 @@
+
+.text
+.globl _start
+_start:
+	/* int nanosleep(const struct timespec *req, struct timespec *rem); */
+	mov x0, 2
+	str x0, [sp, 56]
+	str xzr, [sp, 48]
+	ldr x0, [sp, 56]
+	str x0, [sp, 24]
+	ldr x0, [sp, 48]
+	str x0, [sp, 32]
+	add x0, sp, 24
+	mov x1, 0	/* rem = NULL */
+	mov x16, 0x65
+	svc #0		/* syscall */
+	ret
