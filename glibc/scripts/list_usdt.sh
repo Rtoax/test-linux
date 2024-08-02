0b0142ac01608cee01e5a5388662a0a3e4ce2c2f
Rong Tao <rtoax@foxmail.com>
2024-08-02 13:53:00 +0800

    glibc/scripts/list_usdt.sh: /lib/aarch64-linux-gnu/libc.so.6
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- glibc/scripts/list_usdt.sh
+++ glibc/scripts/list_usdt.sh
@@ -4,4 +4,22 @@ if ! [[ -e /usr/bin/bpftrace ]]; then
 	exit 0
 fi
 
-sudo bpftrace -l usdt:/lib64/libc.so.6:*
+libc=
+possible_libc=(
+	/lib64/libc.so.6
+	/lib/aarch64-linux-gnu/libc.so.6
+)
+for c in ${possible_libc[@]}
+do
+	if [[ -e $c ]]; then
+		libc=$c
+		break;
+	fi
+done
+
+if [[ -z ${libc} ]]; then
+	echo "ERROR: Not found libc.so.6"
+	exit 1
+fi
+
+sudo bpftrace -l usdt:${libc}:*
