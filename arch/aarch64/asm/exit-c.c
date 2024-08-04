0fe380fee53531bbb4a90276f37caec6f9d46e76
Rong Tao <rongtao@cestc.cn>
2024-08-04 02:23:19 -0400

    arch/aarch64/asm: Add exit-c.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ arch/aarch64/asm/exit-c.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+
+void asm_exit(int v)
+{
+	__asm__("mov x0, #0xff \n\t"
+		"mov w8, #0x5d \n\t"
+		"svc #0x0 \n\t"
+		: /* No return value */
+		: [v] "r"(v));
+}
+
+int main(void)
+{
+	asm_exit(3);
+	return 0;
+}
