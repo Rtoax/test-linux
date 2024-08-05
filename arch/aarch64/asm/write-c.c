406a5d8b9918701a075c652b09abb547a51ce9b3
Rong Tao <rongtao@cestc.cn>
2024-08-05 11:44:31 +0800

    arch/aarch64/asm: Add write-c.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- arch/aarch64/asm/write-c.c
+++ arch/aarch64/asm/write-c.c
@@ -1,28 +1,30 @@
 #include <string.h>
 #include <stdio.h>
-#include <errno.h>
-#include <time.h>
 
-
-void asm_write(void)
+int asm_write(void)
 {
 	int ret;
 	int fd = 1;
 	char msg[] = {"Hello\n"};
 	size_t len = 6;
 
-	__asm__("mov %[fd], %%edi \n\t"
-		"movq %[msg], %%rsi \n\t"
-		"movq %[len], %%rdx \n\t"
-		"movq $1, %%rax \n\t"
-		"syscall \n\t"
-		: "=r"(ret)
+	__asm__("stp x0, x1, [sp, #-32]! \n\t"
+		"mov x0, %[fd] \n\t"
+		"mov x1, %[msg] \n\t"
+		"mov x2, %[len] \n\t"
+		"mov x8, #64 \n\t"
+		"svc #0 \n\t"
+		"ldp x0, x1, [sp], #32 \n\t"
+		: "=g"(ret)
 		: [fd] "r"(fd), [msg] "r"(msg), [len] "r"(len));
+
+	return ret;
 }
 
 int main(void)
 {
 	asm_write();
 	printf("exit.\n");
+
 	return 0;
 }
