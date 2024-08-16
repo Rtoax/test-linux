e72ae358468adb35a575aa4a80c6facb5a145805
Rong Tao <rongtao@cestc.cn>
2024-08-16 15:13:21 +0800

    memfd_secret(2): Easy demo
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/mm/memfd/helpers.c
+++ syscall/samples/mm/memfd/helpers.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 #include <errno.h>
 #include <linux/memfd.h>
 #include <sys/syscall.h>
@@ -7,14 +8,28 @@
 
 int sys_memfd_create(const char *name, unsigned int flags)
 {
+	int fd;
+
 #if __GLIBC__ == 2 && __GLIBC_MINOR__ >= 26
-	return memfd_create(name, flags);
+	fd = memfd_create(name, flags);
 #else
-	return syscall(__NR_memfd_create, name, flags);
+	fd = syscall(__NR_memfd_create, name, flags);
 #endif
+	if (fd == -1) {
+		fprintf(stderr, "memfd_secret: %m\n");
+		return -errno;
+	}
+	return fd;
 }
 
 int sys_memfd_secret(unsigned int flags)
 {
-	return syscall(SYS_memfd_secret, flags);
+	int fd;
+
+	fd = syscall(SYS_memfd_secret, flags);
+	if (fd == -1) {
+		fprintf(stderr, "memfd_secret: %m\n");
+		return -errno;
+	}
+	return fd;
 }
--- syscall/samples/mm/memfd/memfd_secret.c
+++ syscall/samples/mm/memfd/memfd_secret.c
@@ -2,11 +2,22 @@
  * Since kernel 5.14 introduce memfd_secret(2)
  */
 #include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #include "helpers.h"
 
 int main(void)
 {
-	/* TODO */
-	return -ENOSYS;
+	int fd, ret = 0;
+
+	fd = sys_memfd_secret(0);
+	if (fd == -1) {
+		fprintf(stderr, "memfd_secret: %m\n");
+		ret = -errno;
+	}
+
+	close(fd);
+	return ret;
 }
