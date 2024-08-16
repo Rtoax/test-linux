2a50f812008c648b0ddbed4439493109e95fb917
Rong Tao <rongtao@cestc.cn>
2024-08-16 09:33:27 +0800

    syscall/samples/mm: Add process_{madvise,mrelease}.c demo (todo)
    Link: https://www.oschina.net/news/166690/linux-5-15-released
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/mm/process_madvise.c
@@ -0,0 +1,11 @@
+#include <sys/mman.h>      /* Definition of MADV_* constants */
+#include <sys/syscall.h>   /* Definition of SYS_* constants */
+#include <sys/uio.h>       /* Definition of struct iovec type */
+#include <unistd.h>
+#include <errno.h>
+
+int main(void)
+{
+	/* TODO */
+	return -ENOSYS;
+}
---
+++ syscall/samples/mm/process_mrelease.c
@@ -0,0 +1,10 @@
+/**
+ * Since kernel 5.15 introduce process_mrelease(2)
+ */
+#include <errno.h>
+
+int main(void)
+{
+	/* TODO */
+	return -ENOSYS;
+}
