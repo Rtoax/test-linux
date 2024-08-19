61df881841cc61dd54cdda9385b9f67846563f4c
Rong Tao <rongtao@cestc.cn>
2024-08-19 12:31:57 +0800

    qemu: support-cpu.sh: Check cpu features
    $ qemu-system-x86_64 -enable-kvm -cpu "base,check,${allflags}"
    qemu-system-x86_64: warning: host doesn't support requested feature: CPUID.07H:EBX.avx512pf [bit 26]
    qemu-system-x86_64: warning: host doesn't support requested feature: CPUID.07H:EBX.avx512er [bit 27]
    qemu-system-x86_64: warning: host doesn't support requested feature: CPUID.07H:EBX.avx512cd [bit 28]
    qemu-system-x86_64: warning: host doesn't support requested feature: CPUID.07H:EBX.sha-ni [bit 29]
    qemu-system-x86_64: warning: host doesn't support requested feature: CPUID.07H:EBX.avx512bw [bit 30]
    qemu-system-x86_64: warning: host doesn't support requested feature: CPUID.07H:EBX.avx512vl [bit 31]
    qemu-system-x86_64: warning: host doesn't support requested feature: CPUID.07H:ECX.avx512vbmi [bit 1]
    
    Link: https://stackoverflow.com/questions/66277749/how-do-i-get-every-supported-cpu-flags-as-qemu-cpu-flags
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- qemu/support-cpu.sh
+++ qemu/support-cpu.sh
@@ -5,3 +5,13 @@
 emulator=$(get_qemu_kvm_emulator)
 
 ${emulator} -M virt -cpu help
+
+allflags=( $(${emulator} -enable-kvm -cpu help | \
+	awk '/flags/ {y=1; getline}; y {print}' | \
+	tr ' ' '\n' | grep -Ev "^$" | \
+	sed -r 's|^|+|' | tr '\n' ',' | sed -r "s|,$||") )
+
+echo Flags: ${allflags}
+
+# warning: host doesn't support requested feature: CPUID.07H:EBX.intel-pt [bit 25]
+# ${emulator} -enable-kvm -cpu "base,check,${allflags}"
