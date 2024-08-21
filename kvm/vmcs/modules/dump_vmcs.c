bbbd4b2b38206734a8e864c4333e94fe151babf7
Rong Tao <rongtao@cestc.cn>
2024-08-21 14:59:24 +0800

    kvm/vmcs/modules/dump_vmcs.c: Auto get dump_vmcs() address value
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- kvm/vmcs/modules/dump_vmcs.c
+++ kvm/vmcs/modules/dump_vmcs.c
@@ -2,11 +2,33 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/printk.h>
+#include <linux/kprobes.h>
 
 typedef void (*dump_vmcs)(void);
+typedef unsigned long(*kallsymsFn)(const char *);
 
 static ulong addr = 0xffffffffc1abf810UL;
 module_param(addr, ulong, 0660);
+
+static kallsymsFn kallsyms = NULL;
+
+static unsigned long my_kallsyms_lookup_name(const char *name)
+{
+	return kallsyms(name);
+}
+
+static int init_kallsyms(void)
+{
+	struct kprobe kp = {0};
+	int ret = 0;
+	kp.symbol_name = "kallsyms_lookup_name";
+	ret = register_kprobe(&kp);
+	if (ret < 0)
+		return ret;
+	kallsyms = (kallsymsFn)kp.addr;
+	unregister_kprobe(&kp);
+	return ret;
+}
 
 /**
  * Need kvm_intel.dump_invalid_vmcs=1 like
@@ -16,7 +38,15 @@ dump_vmcs dump = NULL;
 
 static int kernel_init(void)
 {
-	printk(KERN_INFO "call dump_vmcs().\n");
+	printk(KERN_INFO "====== dump_vmcs ======\n");
+
+	init_kallsyms();
+
+	addr = my_kallsyms_lookup_name("dump_vmcs");
+
+	printk(KERN_INFO "dump_vmcs = %lx\n", addr);
+	printk(KERN_INFO "see also $ sudo grep -w dump_vmcs /proc/kallsyms\n");
+
 	dump = (dump_vmcs)addr;
 	if (dump)
 		dump();
