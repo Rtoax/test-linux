1442dbb623719194a8e72fa9e5a222c8b4911435
Rong Tao <rongtao@cestc.cn>
2024-08-26 13:47:02 +0800

    bfd: build_id
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- binutils/bfd/samples/bfd_init.c
+++ binutils/bfd/samples/bfd_init.c
@@ -5,12 +5,20 @@
 int main(void)
 {
 	bfd *abfd;
+	const struct bfd_build_id *build_id;
 
 	bfd_init();
-	abfd = bfd_openr("/bin/ls", NULL);
-	printf("%s\n", bfd_get_filename(abfd));
-	printf("%ld\n", bfd_get_size(abfd));
-	bfd_close(abfd);
 
+	abfd = bfd_openr("/bin/ls", NULL);
+
+	printf("get_filename: %s\n", bfd_get_filename(abfd));
+	printf("get_size: %ld\n", bfd_get_size(abfd));
+
+	if (abfd->build_id != NULL) {
+		build_id = abfd->build_id;
+		printf("BuildID size %ld\n", build_id->size);
+	}
+
+	bfd_close(abfd);
 	return 0;
 }
