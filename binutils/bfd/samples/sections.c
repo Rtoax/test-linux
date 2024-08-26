af8d4f0b367dccddd89ae3d060561b4206e6444c
Rong Tao <rongtao@cestc.cn>
2024-08-26 19:27:08 +0800

    bfd: Test bfd_get_section_by_name()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- binutils/bfd/samples/sections.c
+++ binutils/bfd/samples/sections.c
@@ -2,16 +2,22 @@
 #include <stdio.h>
 
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	bfd *abfd;
 	asection *asect;
+	char *filepath = argv[0];
 
 	bfd_init();
-	abfd = bfd_openr("/bin/ls", NULL);
+	abfd = bfd_openr(filepath, NULL);
 
 	for (asect = abfd->sections; asect != NULL; asect = asect->next) {
 		printf("%lx\n", bfd_section_vma(asect));
+	}
+
+	asect = bfd_get_section_by_name(abfd, ".plt");
+	if (!asect) {
+		fprintf(stderr, "Not found .plt section.\n");
 	}
 
 	bfd_close(abfd);
