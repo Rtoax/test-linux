232385dc9ef6287c9981863392996a9560a2f173
Rong Tao <rongtao@cestc.cn>
2024-08-26 17:52:59 +0800

    bfd: Add more tests
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- binutils/bfd/samples/bfd_init.c
+++ binutils/bfd/samples/bfd_init.c
@@ -2,17 +2,20 @@
 #include <stdio.h>
 
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	bfd *abfd;
 	const struct bfd_build_id *build_id;
 
 	bfd_init();
 
-	abfd = bfd_openr("/bin/ls", NULL);
+	abfd = bfd_openr(argv[0], NULL);
 
 	printf("get_filename: %s\n", bfd_get_filename(abfd));
 	printf("get_size: %ld\n", bfd_get_size(abfd));
+	printf("count_sections: %d\n", bfd_count_sections(abfd));
+	printf("get_symcount: %d\n", bfd_get_symcount(abfd));
+	printf("get_dynamic_symcount: %d\n", bfd_get_dynamic_symcount(abfd));
 
 	if (abfd->build_id != NULL) {
 		build_id = abfd->build_id;
