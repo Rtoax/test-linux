c303a2ea0468b0e65029402031528a86242963cb
Rong Tao <rongtao@cestc.cn>
2024-08-27 11:07:32 +0800

    scripts/check_compiler_option.mk: Add check_gcc_option()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- scripts/check_compiler_option.mk
+++ scripts/check_compiler_option.mk
@@ -1,6 +1,6 @@
 # Check compiler support option or not
-# $(1) - compiler
-# $(2) - compile option
+# $(1) - compiler, for example: gcc, clang, etc.
+# $(2) - compile option, for example: -flto
 define check_compiler_option
   $(shell echo 'int main(void) { return 0; }' | \
     $(1) -x c -Wall - $(2) -S -o /dev/null >/dev/null 2>&1 \
@@ -9,4 +9,8 @@ endef
 
 define check_clang_option
 	$(call check_compiler_option,clang,$(1))
+endef
+
+define check_gcc_option
+	$(call check_compiler_option,gcc,$(1))
 endef
