9445411a693a345ae59c5915bd822bce7c5e8b84
Rong Tao <rongtao@cestc.cn>
2024-08-29 01:52:56 -0400

    scripts/install-deps.sh: Debian add libcapstone-dev
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -426,6 +426,7 @@ debian|ubuntu)
 	pkgs_base+=( clang-format )
 	pkgs_base+=( libaio-dev )           # aio
 	pkgs_base+=( libbpfcc-dev )
+	pkgs_base+=( libcapstone-dev )
 	pkgs_base+=( libc6-dev )
 	pkgs_base+=( libc-bin )
 	pkgs_base+=( libmpich-dev )         # MPI
