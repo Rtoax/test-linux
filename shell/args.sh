fd7ea2531c5d16f3999664c11cf3c638a00dddfd
Rong Tao <rongtao@cestc.cn>
2024-09-03 16:26:59 +0800

    shell/args.sh: Pass params include space
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- shell/args.sh
+++ shell/args.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-for_each_args() {
+func_2nd()
+{
 	local i=0
 	for p; do
+		i=$(($i + 1))
 		echo $i : $p
 	done
+
+	echo "func_2nd: $@"
+	echo "func_2nd: $*"
+}
+
+for_each_args()
+{
+	local i=0
+	for p; do
+		i=$(($i + 1))
+		echo $i : $p
+	done
+	func_2nd "$@"
 }
 
 for_each_args 1 2 3 a b c
+
+for_each_args "1 2" "3 4" 5 6
