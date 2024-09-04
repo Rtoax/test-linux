da0a6130a5386606e2fb4a7c5980da535e3cec19
Rong Tao <rongtao@cestc.cn>
2024-09-04 15:36:26 +0800

    wait(2): Prase wstatus value
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ syscall/samples/wait/waitpid-status.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int status = 256;
+	char *val;
+
+	if (argc > 1) {
+		val = argv[1];
+		if (val[0] == '0' && val[1] == 'x')
+			status = strtoul(val, NULL, 16);
+		else
+			status = atoi(argv[1]);
+	}
+
+	printf("WIFEXITED(%d=0x%x)	= %d\n", status, status, WIFEXITED(status));
+	printf("WEXITSTATUS(%d=0x%x)	= %d\n", status, status, WEXITSTATUS(status));
+	printf("WTERMSIG(%d=0x%x)	= %d\n", status, status, WTERMSIG(status));
+	printf("WSTOPSIG(%d=0x%x)	= %d\n", status, status, WSTOPSIG(status));
+	printf("WIFSIGNALED(%d=0x%x)	= %d\n", status, status, WIFSIGNALED(status));
+	printf("WIFSTOPPED(%d=0x%x)	= %d\n", status, status, WIFSTOPPED(status));
+
+	return 0;
+}
