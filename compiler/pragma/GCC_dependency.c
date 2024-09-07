757956241a568742e426adc2474d7789e023514e
Rong Tao <rtoax@foxmail.com>
2024-09-07 15:14:49 +0800

    compiler/pragma: GCC dependency: Fix error: current file is older than /etc/os-release
    If system upgraded, /etc/os-release could newer than current source
    code, error looks like:
    
          CC  GCC_dependency.c to GCC_dependency.o
        GCC_dependency.c: In function 'main':
        GCC_dependency.c:8:32: error: current file is older than /etc/os-release [-Werror]
            8 |         #pragma GCC dependency "/etc/os-release"
              |                                ^~~~~~~~~~~~~~~~~
        GCC_dependency.c:9:23: error: current file is older than /etc/os-release [-Werror]
            9 |         DO_PRAGMA(GCC dependency "/etc/os-release")
              |                       ^~~~~~~~~~~~~~~~~
        cc1: all warnings being treated as errors
    
    Link: https://www.cnblogs.com/landy126/archive/2013/04/02/2995536.html
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- compiler/pragma/GCC_dependency.c
+++ compiler/pragma/GCC_dependency.c
@@ -5,6 +5,12 @@
 
 int main(void)
 {
+	/**
+	 * Test the timestamps of the current file (i.e., the program code in
+	 * which the statement resides) and the specified file (i.e., the file
+	 * listed last of the #pragma statement). If the specified file is
+	 * newer than the current file, a warning message is given.
+	 */
 	#pragma GCC dependency "/etc/os-release"
 	DO_PRAGMA(GCC dependency "/etc/os-release")
 	return 0;
