6a8750cb711854391f9bd49e8365503985ae9894
Rong Tao <rongtao@cestc.cn>
2024-09-10 09:19:32 +0800

    module: Move reloc.c into relocation.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- module/relocation/relocation.c
+++ module/relocation/relocation.c
@@ -2,15 +2,30 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/printk.h>
+#include <linux/kthread.h>
 
 int local_i = 123;
 int local_i_no;
 /* No rela entry */
 static int s_local_i = 123;
 
-#define P_int(i)	printk("I: %-16s  %-8d %016p.\n", #i, i, (void *)&i);
-#define P_long(l)	printk("L: %-16s  %-8lx %016p.\n", #l, l, (void *)&l);
-#define P_f(f)		printk("F: %-16s           %016p.\n", #f, (void *)f);
+int global_int1;
+int global_int2;
+int global_int3 = 0xabcdffff;
+static int sglobal_int1;
+
+#define P_int(i)	printk(KERN_INFO "I: %-16s  %-8d %016p.\n", #i, i, (void *)&i);
+#define P_long(l)	printk(KERN_INFO "L: %-16s  %-8lx %016p.\n", #l, l, (void *)&l);
+#define P_f(f)		printk(KERN_INFO "F: %-16s           %016p.\n", #f, (void *)f);
+
+void func1(void) {}
+static void sfunc1(void)
+{
+	/* This is not RELA item. */
+	kthread_run(NULL, NULL, NULL);
+	printk("%d\n", global_int1);
+}
+
 
 void print(void) {}
 
