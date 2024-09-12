792c2cff0c5c893d67aaa191f9b77865a14d8f47
Rong Tao <rongtao@cestc.cn>
2024-09-12 17:50:40 +0800

    c: Makefile: No strip if cross compile
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/aarch64.sh
+++ c/aarch64.sh
@@ -2,5 +2,6 @@
 
 [[ $(uname -m) == aarch64 ]] && echo "ERROR: You already on aarch64, no need to cross compile" && exit 1
 
+make clean
 make STATIC=1 ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu- SYSROOT=/home/rongtao/rootfs-aarch64 "${@}"
 
