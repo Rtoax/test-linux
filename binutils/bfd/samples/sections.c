ada16d30508c7661097c7f95bc246147fc9c07bd
Rong Tao <rongtao@cestc.cn>
2024-09-13 06:33:28 -0400

    bfd/samples: sections.c: Add argument -f -h
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- binutils/bfd/samples/sections.c
+++ binutils/bfd/samples/sections.c
@@ -1,8 +1,18 @@
 #include <bfd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 #define BFD_ERR	bfd_errmsg(bfd_get_error())
+
+void usage(const char *prog)
+{
+	fprintf(stderr, "\n"
+		"-f, --file     specify file to bfd, default: %s\n"
+		"-h, --help     print this info\n",
+		prog
+	);
+}
 
 int main(int argc, char *argv[])
 {
@@ -10,7 +20,42 @@ int main(int argc, char *argv[])
 	bfd *abfd;
 	char **matching;
 	asection *asect;
-	char *filepath = argv[0];
+	char *filepath;
+
+	struct option options[] = {
+		{"file", required_argument, 0, 'f'},
+		{"text-vma", required_argument, 0, 't'},
+		{"help", no_argument, 0, 'h'},
+		{0, 0, 0, 0}
+	};
+
+	filepath = argv[0];
+
+	while (1) {
+		int option_index = 0;
+		int c = getopt_long(argc, argv, "f:h", options, &option_index);
+
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case 'f':
+			filepath = optarg;
+			printf("Set file name %s\n", filepath);
+			break;
+		case 'h':
+			usage(argv[0]);
+			exit(0);
+			break;
+		case '?':
+			usage(argv[0]);
+			fprintf(stderr, "Unknown option or requires an argument.\n");
+			exit(1);
+			break;
+		default:
+			abort();
+		}
+	}
 
 	ret = bfd_init();
 	if (ret != BFD_INIT_MAGIC) {
