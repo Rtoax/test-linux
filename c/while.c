acd47ba16a256096f7e023a8622fee07e8467bf0
Rong Tao <rongtao@cestc.cn>
2024-09-14 00:35:05 -0400

    c: while.c: Print count if WHILE_SLEEP
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/while.c
+++ c/while.c
@@ -1,10 +1,16 @@
+#include <stdio.h>
 #include <unistd.h>
+
+unsigned long count = 0;
 
 int main(void)
 {
 	while (1)
 #ifdef WHILE_SLEEP
-		sleep(1)
+	{
+		printf("count %ld\n", count++);
+		sleep(1);
+	}
 #endif
 		;
 	return 0;
