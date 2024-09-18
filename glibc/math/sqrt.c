cf43508d7a507a15d25aec9311d80f14bc031ddc
Rong Tao <rongtao@cestc.cn>
2024-09-18 12:38:28 +0800

    glibc/math: Add sqrt-stress.c test
        $ sudo perf top -p $(pidof sqrt-stress)
        __sqrt  /usr/lib64/libm.so.6 [Percent: local period]
        Percent│
               │
               │
               │    Disassembly of section .text:
               │
               │    000000000000eed0 <sqrt>:
               │    __sqrt():
         49.92 │      fcmp d0, #0.0
               │      fmov d1, d0
               │    ↓ b.pl 20
               │      adrp x0, __FRAME_END__+0x1f4dc
               │      ldr  x0, [x0, #4016]
               │      ldr  w0, [x0]
               │      cmn  w0, #0x1
               │    ↓ b.ne 28
         50.08 │20:   fmov d0, d1
               │    → b    __sqrt_finite@GLIBC_2.17
               │28:   mov  w0, #0x1a                       // #26
               │    → b    __kernel_standard
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/math/sqrt.c
+++ glibc/math/sqrt.c
@@ -3,7 +3,20 @@
 
 int main(void)
 {
-	double d = sqrt(16.0);
+	double v, d;
+
+	v = 16.0;
+
+#if defined(STRESS)
+	/**
+	 * Test with: $ sudo perf top -p $(pidof sqrt-stress)
+	 *
+	 * Note: if sqrt(16.0) directly, sqrt will be optimized out.
+	 */
+	while (1)
+#endif
+		d = sqrt(v);
+
 	printf("%lf\n", d);
 	return 0;
 }
