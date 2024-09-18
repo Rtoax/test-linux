5457be516e60cc2c48b99002adbb8051ab5abc77
Rong Tao <rongtao@cestc.cn>
2024-09-18 11:28:06 +0800

    glibc/string: Add memcpy-stress.c
       $ sudo perf top -p $(pidof memcpu-stress)
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/string/memcpy.c
+++ glibc/string/memcpy.c
@@ -1,10 +1,19 @@
 #include <string.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	char buf[1024] = {'x'};
 	char buf2[1024];
 
+#if defined(STRESS)
+	/**
+	 * Check with:
+	 * $ sudo perf top -p $(pidof memcpu-stress)
+	 */
+	while (1)
+		memcpy(buf2, buf, sizeof(buf));
+#else
 	memcpy(buf2, buf, sizeof(buf));
+#endif
 	return 0;
 }
