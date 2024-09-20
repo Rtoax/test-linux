156f4ea1ea5495937ba750bdf664549352d6f72b
Rong Tao <rongtao@cestc.cn>
2024-09-20 12:54:56 +0800

    abbreviation.sh: Support getopt and filter by name
    Usage:  $ ./abbreviation.sh ACPI
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- abbreviation.sh
+++ abbreviation.sh
@@ -1,11 +1,57 @@
 #!/bin/bash
+set -e
+
+__usage__() {
+	echo -e "
+Usage:
+
+-h, --help         show this help information
+"
+	exit ${1-0}
+}
+
+ARG_TEMP=$(getopt \
+	--options h \
+	--long help \
+	-n ${0} -- "$@")
+
+test $? != 0 && __usage__ 1
+
+eval set -- "$ARG_TEMP"
+
+while true; do
+	case $1 in
+	-h|--help)
+		shift
+		__usage__
+		;;
+	--)
+		shift
+		break
+		;;
+	esac
+done
+
+name=$1
 files=( $(find -name abbreviation.md) )
 
-for f in ${files[@]}
-do
-	# - Delete white lines
-	# - Add filename suffix
-	cat ${f} | \
-		sed '/^$/d' | \
-		sed "s|$| <$(dirname ${f})>|g"
-done | sort | nl
+list_all() {
+	for f in ${files[@]}
+	do
+		# - Delete white lines
+		# - Add filename suffix
+		cat ${f} | \
+			sed '/^$/d' | \
+			sed "s|$| <$(dirname ${f})>|g"
+	done | sort | nl
+}
+
+find_name() {
+	list_all | grep ${name}
+}
+
+if [[ ${name} ]]; then
+	find_name
+else
+	list_all
+fi
