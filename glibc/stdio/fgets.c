7ec1f907baf1629de8eaba48425197bab92572c8
Rong Tao <rongtao@cestc.cn>
2024-09-24 11:54:01 +0800

    glibc: fgets(): add fgets_file() function
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/stdio/fgets.c
+++ glibc/stdio/fgets.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-int main(void)
-{
-	char cmd[123] = {0};
-	FILE *fp = fopen("/etc/os-release", "r");
 
-	while (fgets(cmd, 123, fp))
-		printf("%s", cmd);
+int fgets_file(const char *file)
+{
+	char str[123] = {0};
+	FILE *fp = fopen(file, "r");
+
+	while (fgets(str, sizeof(str), fp))
+		printf("%s", str);
 
 	fclose(fp);
+	return 0;
+}
+
+int main(void)
+{
+	fgets_file("/etc/os-release");
+	fgets_file("/proc/self/maps");
 	return 0;
 }
