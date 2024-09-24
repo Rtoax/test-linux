d87a60e0c7bcebeb66dfe0777256ed19a8c546d6
Rong Tao <rongtao@cestc.cn>
2024-09-24 16:29:45 +0800

    compiler/pragma: Add more GCC diagnostic ignore
    Link: https://lkml.kernel.org/r/tencent_51C4ACA8CB3895C2D7F35178440283602107@qq.com
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/pragma/GCC_diagnostic.c
+++ compiler/pragma/GCC_diagnostic.c
@@ -8,6 +8,11 @@ int main(void)
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
 #pragma GCC diagnostic ignored "-Wuninitialized"
 #pragma GCC diagnostic ignored "-Wunused-variable"
+#if __GNUC__ >= 11 && __GNUC_MINOR__ >= 1
+/* Ignore read(2) overflow and write(2) overread compile warnings */
+#pragma GCC diagnostic ignored "-Wstringop-overread"
+#pragma GCC diagnostic ignored "-Wstringop-overflow"
+#endif
 	printf("%d\n", i);
 #pragma GCC diagnostic pop
 	return 0;
