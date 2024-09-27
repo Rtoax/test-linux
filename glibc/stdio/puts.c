dc35d547744bbf93ca59e913979a53aae3f75bb9
Rong Tao <rongtao@cestc.cn>
2024-09-27 13:04:07 +0800

    puts(3): Test
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/stdio/puts.c
+++ glibc/stdio/puts.c
@@ -3,11 +3,14 @@
 int main(void)
 {
 #if defined(__x86_64__)
-	asm(
-	"push   $0x44434241\n"
-	"mov    %rsp,%rdi\n"
-	"call   puts\n"
-	"pop    %rsi\n");
+	__asm__("push   $0x44434241\n"
+		"mov    %rsp,%rdi\n"
+		"call   puts\n"
+		"pop    %rsi\n");
+#elif defined(__i386__)
 #endif
+	char s1[] = {"ABCD"};
+	puts(s1);
+	puts("ABCD");
 	return 0;
 }
