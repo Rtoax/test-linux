4d474688087850be11360d191f19df84a7c1054a
Rong Tao <rongtao@cestc.cn>
2024-10-08 15:27:15 +0800

    elf/library: call.h: Add type fn0_t
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- elf/library/call.h
+++ elf/library/call.h
@@ -1,22 +1,24 @@
 #pragma once
 
-void call1(void);
-void call2(void);
-void call3(void);
-void call4(void);
-void call5(void);
-void call6(void);
-void call7(void);
-void call8(void);
-void call9(void);
-void call10(void);
-void call11(void);
-void call12(void);
-void call13(void);
-void call14(void);
-void call15(void);
-void call16(void);
-void call17(void);
-void call18(void);
-void call19(void);
-void call20(void);
+typedef void (fn0_t)(void);
+
+fn0_t call1;
+fn0_t call2;
+fn0_t call3;
+fn0_t call4;
+fn0_t call5;
+fn0_t call6;
+fn0_t call7;
+fn0_t call8;
+fn0_t call9;
+fn0_t call10;
+fn0_t call11;
+fn0_t call12;
+fn0_t call13;
+fn0_t call14;
+fn0_t call15;
+fn0_t call16;
+fn0_t call17;
+fn0_t call18;
+fn0_t call19;
+fn0_t call20;
