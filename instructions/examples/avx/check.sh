1ce430123e30bcb1478b26971a73cab664cdd7bc
Rong Tao <rongtao@cestc.cn>
2024-10-09 03:15:00 -0400

    instructions/{avx,sse}: Detect features with 'gcc -dM -E - < /dev/null'
    Link: https://stackoverflow.com/questions/28939652/how-to-detect-sse-sse2-avx-avx2-avx-512-avx-128-fma-kcvi-availability-at-compile
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/avx/check.sh
+++ instructions/examples/avx/check.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+set -e
 
 features=(avx avx2 avx512f)
 
 ../sh/cpu_feature_check ${features[@]}
+
+gcc -mavx2 -dM -E - < /dev/null | grep AVX2
+gcc -mavx512f -dM -E - < /dev/null | grep AVX512F
+
+for i in 4fmaps 4vnniw ifma vbmi vpopcntdq
+do
+	echo "==== $i ===="
+	gcc -mavx512$i -dM -E - < /dev/null | egrep "AVX512" | sort
+done
--- instructions/examples/sse/check.sh
+++ instructions/examples/sse/check.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+set -e
 
 features=(sse sse2 ssse3 sse4_1 sse4_2)
 
 ../sh/cpu_feature_check ${features[@]}
+
+gcc -msse2 -dM -E - < /dev/null | grep SSE
+gcc -msse3 -dM -E - < /dev/null | grep SSE3
+gcc -msse4 -dM -E - < /dev/null | grep SSE4
