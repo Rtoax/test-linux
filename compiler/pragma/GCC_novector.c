708c252b7e27cc404b23fed26ffcb3bb6ee26c9d
Rong Tao <rongtao@cestc.cn>
2024-10-10 03:50:33 -0400

    compiler: pragma: Add -Werror=unknown-pragmas and fix novector pragma error
    see gcc git://gcc.gnu.org/git/gcc.git
    commit 6fb5da03107f ("frontend: Add novector C pragma")
    
        $ git describe 6fb5da03107f
        basepoints/gcc-14-2993-g6fb5da03107f
    
    We hope all programs compile success, if not support pragmas, trigger
    fatal.
    
        compiler/pragma$ make
          CC  GCC_novector.c to GCC_novector.o
        GCC_novector.c: In function ‘foo’:
        GCC_novector.c:5: error: ignoring ‘#pragma GCC novector’ [-Werror=unknown-pragmas]
            5 |         #pragma GCC novector
              |
        cc1: all warnings being treated as errors
        make: *** [Makefile:58: GCC_novector.o] Error 1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/pragma/GCC_novector.c
+++ compiler/pragma/GCC_novector.c
@@ -1,8 +1,13 @@
 void foo(int n, int *a, int *b, int *c)
 {
 	int i;
-
+/**
+ * see gcc git://gcc.gnu.org/git/gcc.git
+ * commit 6fb5da03107f ("frontend: Add novector C pragma")
+ */
+#if defined(__GNUC__) && (__GNUC__ >= 14)
 	#pragma GCC novector
+#endif
 	for (i = 0; i < n; ++i)
 		a[i] = b[i] + c[i];
 }
