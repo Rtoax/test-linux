fee969e75a8a7e091f0bd3a2a214ed8562df06f1
Rong Tao <rongtao@cestc.cn>
2024-10-10 22:38:11 -0400

    linker-script: Generate default.lds
    ld --verbose can display default linker script [1], and use sed to
    filter lds out from --verbose output [2].
    
    Link: https://stackoverflow.com/questions/28688484/actual-default-linker-script-and-settings-gcc-uses [1]
    Link: https://blog.csdn.net/weixin_42233789/article/details/103589999 [2]
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ compiler/linker/linker-script/default.lds.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+LD=
+[[ -z ${LD} ]] && LD=ld
+
+temp_file=$(mktemp -u tmp-lds-XXXXXX)
+
+${LD} --verbose > ${temp_file}
+
+# Seek two "======" line numbers
+start_end=( $(sed -n '/=====/=' ${temp_file}) )
+
+# Get start and end line number, 1 for strip '========='
+start=$(( ${start_end[0]} + 1 ))
+end=$(( ${start_end[1]} - 1 ))
+
+# Print
+sed -n "${start}, ${end}p" ${temp_file} > default.lds
+
+rm -f ${temp_file}
