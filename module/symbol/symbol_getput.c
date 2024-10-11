c9094be8500228bfed10052ba58fb9521441b453
Rong Tao <rongtao@cestc.cn>
2024-10-11 16:17:27 +0800

    Move hw_breakpoint/modules/symbol_getput.c to module/symbol/
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- module/symbol/symbol_getput.c
+++ module/symbol/symbol_getput.c
@@ -1,14 +1,10 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * symbol-put.c - test __symbol_get() - symbol_put()
  */
-#include <linux/module.h>	/* Needed by all modules */
-#include <linux/kernel.h>	/* Needed for KERN_INFO */
-#include <linux/init.h>		/* Needed for the macros */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
 #include <linux/kallsyms.h>
-
-#include <linux/perf_event.h>
-#include <linux/hw_breakpoint.h>
 
 static char ksym_name[KSYM_NAME_LEN] = "jiffies";
 
@@ -27,7 +23,7 @@ static int __init s_module_init(void)
 static void __exit s_module_exit(void)
 {
 	__symbol_put(ksym_name);
-	printk(KERN_INFO "symbol_put(%s) success\n", ksym_name);
+	printk(KERN_INFO "__symbol_put(%s) success\n", ksym_name);
 }
 
 module_init(s_module_init);
