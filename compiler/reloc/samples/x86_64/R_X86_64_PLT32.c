a4aa9591d5c659e53f3caabdd8e3468ef5cecd14
Rong Tao <rongtao@cestc.cn>
2024-10-12 04:32:19 -0400

    reloc/samples: x86_64: Add R_X86_64_JUMP_SLOT.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_PLT32.c
+++ compiler/reloc/samples/x86_64/R_X86_64_PLT32.c
@@ -2,6 +2,11 @@
 
 int main(void)
 {
+	/**
+	 * getchar():
+	 * - R_X86_64_PLT32 in ELF 64-bit LSB relocatable
+	 * - R_X86_64_JUMP_SLOT in ELF 64-bit LSB executable
+	 */
 	getchar();
 	return 0;
 }
