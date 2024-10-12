4b07a9b2f72b2ab55c44d8f52e1f4f9bfb8bce1e
Rong Tao <rongtao@cestc.cn>
2024-10-12 05:46:25 -0400

    elf/reloc: R_X86_64_PLT32 in ELF 64-bit LSB relocatable if has PLT
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_PLT32.c
+++ compiler/reloc/samples/x86_64/R_X86_64_PLT32.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-int main(void)
+int foo(void)
 {
 	/**
 	 * getchar():
-	 * - R_X86_64_PLT32 in ELF 64-bit LSB relocatable if has PLT
-	 * - R_X86_64_JUMP_SLOT in ELF 64-bit LSB executable if has PLT
-	 *
-	 * - R_X86_64_GOTPCRELX in ELF 64-bit LSB relocatable if no PLT
-	 * - R_X86_64_GLOB_DAT in ELF 64-bit LSB executable if no PLT
+	 * R_X86_64_PLT32 in ELF 64-bit LSB relocatable if has PLT,
+	 * and it's could becomes R_X86_64_JUMP_SLOT in ELF 64-bit LSB
+	 * executable if has PLT.
 	 */
 	getchar();
 	return 0;
