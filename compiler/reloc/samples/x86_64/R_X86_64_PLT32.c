2f00f553cf131195c253d0c2f38ee5dff1de22d9
Rong Tao <rongtao@cestc.cn>
2024-10-12 04:48:54 -0400

    reloc/samples: x86_64: Add R_X86_64_GLOB_DAT.c R_X86_64_GOTPCRELX.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_PLT32.c
+++ compiler/reloc/samples/x86_64/R_X86_64_PLT32.c
@@ -4,8 +4,10 @@ int main(void)
 {
 	/**
 	 * getchar():
-	 * - R_X86_64_PLT32 in ELF 64-bit LSB relocatable
-	 * - R_X86_64_JUMP_SLOT in ELF 64-bit LSB executable
+	 * - R_X86_64_PLT32 in ELF 64-bit LSB relocatable if has PLT
+	 * - R_X86_64_JUMP_SLOT in ELF 64-bit LSB executable if has PLT
+	 * - R_X86_64_GOTPCRELX in ELF 64-bit LSB relocatable if no PLT
+	 * - R_X86_64_GLOB_DAT in ELF 64-bit LSB executable if no PLT
 	 */
 	getchar();
 	return 0;
