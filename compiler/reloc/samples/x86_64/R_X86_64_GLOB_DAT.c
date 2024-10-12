5f19ee1ad2ba0e8ec310e9d48a6ebe8a53f11cdd
Rong Tao <rongtao@cestc.cn>
2024-10-12 05:41:02 -0400

    elf/reloc: R_X86_64_GLOB_DAT in ELF 64-bit LSB executable if no PLT
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_GLOB_DAT.c
+++ compiler/reloc/samples/x86_64/R_X86_64_GLOB_DAT.c
@@ -1,1 +1,13 @@
-R_X86_64_JUMP_SLOT.c
+#include <stdio.h>
+
+int main(void)
+{
+	/**
+	 * getchar():
+	 * R_X86_64_GLOB_DAT in ELF 64-bit LSB executable if no PLT
+	 * and it's could be R_X86_64_GOTPCRELX in ELF 64-bit LSB relocatable
+	 * if no PLT before link.
+	 */
+	getchar();
+	return 0;
+}
