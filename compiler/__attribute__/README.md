acd94ae40402037f20ad1cdb7c807887cca1f77d
Rong Tao <rongtao@cestc.cn>
2024-10-12 05:19:04 -0400

    compiler/attribute: nonnull: Add more test
    Link: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/__attribute__/README.md
+++ compiler/__attribute__/README.md
@@ -4,4 +4,5 @@ Compiler Attributes
 # Links
 
 - https://gcc.gnu.org/onlinedocs/gcc/x86-Function-Attributes.html
+- https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html
 
--- compiler/__attribute__/__attribute__nonnull.c
+++ compiler/__attribute__/__attribute__nonnull.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 
 
-#define __my_nonnull__(params) __attribute__((__nonnull__ params))
-#define my_nonnull(params) __attribute__((nonnull params))
+#define __my_nonnull1__ __attribute__((nonnull))
+#define __my_nonnull2__(params) __attribute__((__nonnull__ params))
+#define __my_nonnull3__(params) __attribute__((nonnull params))
 
-extern void print_ptr(void *p1, void *p2) my_nonnull((1, 2));
+extern void print_ptr1(void *p1, void *p2) __my_nonnull3__((1, 2));
 
 extern void *my_memcpy(void *dest, const void *src, size_t len)
 	__attribute__((nonnull(1, 2)));
 
-void __attribute__((noreturn)) __attribute__((nonnull(1)))
-fun(char *str)
+__attribute__((nonnull(1)))
+void fun(char *str)
 {
 	if(str != NULL)
 		printf("%s\n", str);
+	return;
+}
+
+/**
+ * if ptr: warning: ‘nonnull’ attribute only applies to function types
+ */
+void __my_nonnull1__ fun2(char *ptr)
+{
 	return;
 }
 
