318f1fe36e1d9f29f14a943d6a0c58ed544ab3a1
Rong Tao <rongtao@cestc.cn>
2024-10-12 05:43:17 -0400

    elf/reloc: R_X86_64_JUMP_SLOT in ELF 64-bit LSB executable if has PLT
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_JUMP_SLOT.c
+++ compiler/reloc/samples/x86_64/R_X86_64_JUMP_SLOT.c
@@ -1,1 +1,13 @@
-R_X86_64_PLT32.c
+#include <stdio.h>
+
+int main(void)
+{
+	/**
+	 * getchar():
+	 * R_X86_64_JUMP_SLOT in ELF 64-bit LSB executable if has PLT,
+	 * and it's could be R_X86_64_PLT32 in ELF 64-bit LSB relocatable if
+	 * has PLT before link.
+	 */
+	getchar();
+	return 0;
+}
