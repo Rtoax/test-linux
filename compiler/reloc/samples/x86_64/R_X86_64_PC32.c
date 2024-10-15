9d94e10d025829fcfee2c2635dee2ff0d5a8422a
Rong Tao <rongtao@cestc.cn>
2024-10-15 03:02:22 -0400

    R_X86_64_PC32: Add more static int var
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_PC32.c
+++ compiler/reloc/samples/x86_64/R_X86_64_PC32.c
@@ -1,14 +1,16 @@
 int gi = 0;	/* .bss, GLOBAL */
 int gii = 1;	/* .data, GLOBAL */
 
-static int si = 0;	/* .bss, LOCAL */
+static int si1 = 0;	/* .bss, LOCAL */
+static int si2 = 0;	/* .bss, LOCAL */
 static int sii = 1;	/* .data, LOCAL */
 
 /* R_X86_64_PC32 */
 int foo(void)
 {
 	gi = 10;	/* R_X86_64_PC32 */
-	si = 10;	/* R_X86_64_PC32 */
+	si1 = 10;	/* R_X86_64_PC32 */
+	si2 = 10;	/* R_X86_64_PC32 */
 	return 0;
 }
 
