e2a577a642bfd5e2da99a514149a8722470ae607
Rong Tao <rongtao@cestc.cn>
2024-10-14 21:14:12 -0400

    R_X86_64_PLT32: Use puts()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_PLT32.c
+++ compiler/reloc/samples/x86_64/R_X86_64_PLT32.c
@@ -5,12 +5,21 @@
 
 int foo(void)
 {
+	char buf[32];
+
+	buf[0] = 'H';
+	buf[1] = 'e';
+	buf[2] = 'l';
+	buf[3] = 'l';
+	buf[4] = 'o';
+	buf[5] = '\0';
+
 	/**
-	 * getchar():
+	 * puts():
 	 * R_X86_64_PLT32 in ELF 64-bit LSB relocatable if has PLT,
 	 * and it's could becomes R_X86_64_JUMP_SLOT in ELF 64-bit LSB
 	 * executable if has PLT.
 	 */
-	getchar();
+	puts(buf);
 	return 0;
 }
