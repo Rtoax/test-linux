2e815a845095f2f5575ead730f0459ae7da500e2
Rong Tao <rongtao@cestc.cn>
2024-10-15 03:07:46 -0400

    R_X86_64_PC32: long
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_PC32.c
+++ compiler/reloc/samples/x86_64/R_X86_64_PC32.c
@@ -1,9 +1,13 @@
 int gi = 0;	/* .bss, GLOBAL */
 int gii = 1;	/* .data, GLOBAL */
 
+long gl = 0;	/* .bss, GLOBAL */
+
 static int si1 = 0;	/* .bss, LOCAL */
 static int si2 = 0;	/* .bss, LOCAL */
 static int sii = 1;	/* .data, LOCAL */
+
+static int sl = 0;	/* .bss, LOCAL */
 
 /* R_X86_64_PC32 */
 int foo(void)
@@ -11,6 +15,9 @@ int foo(void)
 	gi = 10;	/* R_X86_64_PC32 */
 	si1 = 10;	/* R_X86_64_PC32 */
 	si2 = 10;	/* R_X86_64_PC32 */
+
+	gl = 10;	/* R_X86_64_PC32 */
+	sl = 10;	/* R_X86_64_PC32 */
 	return 0;
 }
 
