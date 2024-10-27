71b7c6ef241913ec2593902ba4ecb520bee7a795
Rong Tao <rtoax@foxmail.com>
2024-10-27 16:38:30 +0800

    elf/pie: is_pie.c: Impl with libelf
        elf/pie/samples$ ./is_pie
        ./is_pie found SHT_DYNAMIC.
        ./is_pie is not PIE
        ./is_pie found SHT_DYNAMIC.
        ./is_pie is not PIE
    
        elf/pie/samples$ ./is_pie hello
        hello found SHT_DYNAMIC.
        hello is not PIE
        hello found SHT_DYNAMIC.
        hello is not PIE
    
        elf/pie/samples$ ./is_pie hello-pie
        hello-pie found SHT_DYNAMIC.
        hello-pie is PIE
        hello-pie found SHT_DYNAMIC.
        hello-pie is PIE
    
        elf/pie/samples$ ./is_pie /bin/bash
        /bin/bash found SHT_DYNAMIC.
        /bin/bash is PIE
        /bin/bash found SHT_DYNAMIC.
        /bin/bash is PIE
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- elf/pie/samples/is_pie.c
+++ elf/pie/samples/is_pie.c
@@ -4,6 +4,7 @@
 #include <malloc.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <libelf.h>
 #include <sys/stat.h>
 
 
@@ -56,9 +57,49 @@ done:
 	return res;
 }
 
+int is_pie_libelf(const char *path)
+{
+	int res = false;
+	int i, j, fd;
+	Elf *elf;
+	size_t shdrnum;
+
+	fd = open(path, O_RDONLY);
+	elf_version(EV_CURRENT);
+	elf = elf_begin(fd, ELF_C_READ_MMAP, NULL);
+
+	elf_getshdrnum(elf, &shdrnum);
+	for (i = 0; i < shdrnum; i++) {
+		Elf_Scn *scn = elf_getscn(elf, i);
+		Elf64_Shdr *shdr = elf64_getshdr(scn);
+		Elf_Data *data = elf_getdata(scn, NULL);
+		if (shdr->sh_type == SHT_DYNAMIC) {
+			fprintf(stderr, "%s found SHT_DYNAMIC.\n", path);
+			Elf64_Dyn *dyns = data->d_buf;
+			for (j = 0; j * shdr->sh_entsize < shdr->sh_size; j++) {
+				Elf64_Dyn *dyn = &dyns[j];
+				if (dyn->d_tag == DT_FLAGS_1) {
+					if (dyn->d_un.d_val & DF_1_PIE) {
+						fprintf(stderr, "%s is PIE\n", path);
+						res = true;
+						goto done;
+					}
+				}
+			}
+		}
+	}
+
+	fprintf(stderr, "%s is not PIE\n", path);
+done:
+	elf_end(elf);
+	close(fd);
+	return res;
+}
+
 int main(int argc, char *argv[])
 {
 	char *path = (argc <= 1) ? argv[0] : argv[1];
 	is_pie(path);
+	is_pie_libelf(path);
 	return 0;
 }
