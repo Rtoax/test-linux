743aa8b3619bc74f680eef2798c60aa12838fbaa
Rong Tao <rongtao@cestc.cn>
2024-10-28 23:21:28 +0800

    R_X86_64_64: Test more
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_64.c
+++ compiler/reloc/samples/x86_64/R_X86_64_64.c
@@ -5,13 +5,15 @@
  */
 
 /**
- * s1 ~ s5 are R_X86_64_64 in ELF 64-bit LSB relocatable
+ * s1 ~ s7 are R_X86_64_64 in ELF 64-bit LSB relocatable
  */
 const char *s1 = "Hello";
 const char *s2 = "Hello";
 const char *s3 = "Hello2";
 const char *s4 = "Hello2";
 const char *s5 = "Hello2";
+char *s6 = "Hello2";
+static char *s7 = "Hello2";
 
 int main(void)
 {
