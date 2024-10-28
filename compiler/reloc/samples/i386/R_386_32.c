c7e97c44c9ea59db55b6201df8ea7d0709537afc
Rong Tao <rongtao@cestc.cn>
2024-10-28 23:41:34 +0800

    elf: reloc: Add i386/
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ compiler/reloc/samples/i386/R_386_32.c
@@ -0,0 +1,21 @@
+/**
+ * R_X86_64_64
+ *
+ * value = 1, field = word64, calculation = S + A
+ */
+
+/**
+ * s1 ~ s7 are R_X86_64_64 in ELF 64-bit LSB relocatable
+ */
+const char *s1 = "Hello";
+const char *s2 = "Hello";
+const char *s3 = "Hello2";
+const char *s4 = "Hello2";
+const char *s5 = "Hello2";
+char *s6 = "Hello2";
+static char *s7 = "Hello2";
+
+int main(void)
+{
+	return 0;
+}
