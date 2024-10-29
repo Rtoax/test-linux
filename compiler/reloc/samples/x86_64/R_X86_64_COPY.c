b8f7139d3da5c3d946a57c349b622f47e111da5e
Rong Tao <rongtao@cestc.cn>
2024-10-29 04:37:09 -0400

    R_X86_64_COPY: stderr in  ELF 64-bit LSB executable is R_X86_64_COPY
    Relocation section '.rela.dyn' at offset 0x4d0 contains 3 entries:
        Offset             Info             Type               Symbol's Value  Symbol's Name + Addend
    0000000000403fd8  0000000100000006 R_X86_64_GLOB_DAT      0000000000000000 __libc_start_main@GLIBC_2.34 + 0
    0000000000403fe0  0000000200000006 R_X86_64_GLOB_DAT      0000000000000000 __gmon_start__ + 0
    0000000000404020  0000000400000005 R_X86_64_COPY          0000000000404020 stderr@GLIBC_2.2.5 + 0
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_COPY.c
+++ compiler/reloc/samples/x86_64/R_X86_64_COPY.c
@@ -1,3 +1,14 @@
 /**
  * R_X86_64_COPY: value = 5, field = none,  calculation = none
  */
+
+#include <stdio.h>
+
+int main(void)
+{
+	/**
+	 * stderr is R_X86_64_COPY in ELF 64-bit LSB executable
+	 */
+	fprintf(stderr, "hello\n");
+	return 0;
+}
