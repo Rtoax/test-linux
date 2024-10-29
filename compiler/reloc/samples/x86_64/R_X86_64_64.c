50fc1d0aa1c13c3d976732b9c8dae569b2d90cfb
Rong Tao <rongtao@cestc.cn>
2024-10-29 16:55:46 +0800

    R_X86_64_64: Add more comment
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_64.c
+++ compiler/reloc/samples/x86_64/R_X86_64_64.c
@@ -5,15 +5,16 @@
  */
 
 /**
- * s1 ~ s7 are R_X86_64_64 in ELF 64-bit LSB relocatable
+ * s1 ~ s8 are R_X86_64_64 in ELF 64-bit LSB relocatable
  */
-const char *s1 = "Hello";
-const char *s2 = "Hello";
-const char *s3 = "Hello2";
-const char *s4 = "Hello2";
-const char *s5 = "Hello2";
-char *s6 = "Hello2";
-static char *s7 = "Hello2";
+const char *s1 = "Hello";	/* .data, GLOBAL */
+const char *s2 = "Hello";	/* .data, GLOBAL */
+const char *s3 = "Hello2";	/* .data, GLOBAL */
+const char *s4 = "Hello2";	/* .data, GLOBAL */
+const char *s5 = "Hello2";	/* .data, GLOBAL */
+char *s6 = "Hello2";		/* .data, GLOBAL */
+static char *s7 = "Hello2";	/* .data, LOCAL */
+char *const s8 = "Hello2";	/* .rodata, GLOBAL */
 
 int main(void)
 {
