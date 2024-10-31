77fe268d0e9587d9acd077cdd314bb58e14d9cb5
Rong Tao <rongtao@cestc.cn>
2024-10-31 06:19:38 -0400

    namespace/net: ip.sh: Whole test commands
    Initial script from <Linux开源网络全栈讲解> P38
    
    Link: https://trainingportal.linuxfoundation.org/learn/course/containers-fundamentals-lfs253/virtualization-fundamentals/lab-exercise?client=cecloud-computing-technology&page=2
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- namespace/net/ip.sh
+++ namespace/net/ip.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
-# 见 <Linux开源网络全栈讲解> P38
-# 荣涛 2021年7月27日
 
-sudo ip netns add ns1
-# 会出现在 /var/run/netns
+sudo ip netns add namespace1
+sudo ip netns add namespace2
 sudo ip netns list
 
-# 在 namespace 下运行命令
-sudo ip netns exec ns1 ip addr
+# Then create a pair of interconnected virtual ethernet devices:
+sudo ip link add veth1 type veth peer name veth2
 
-# 也可以运行多条指令
-function multi_cmd() {
-	sudo ip netns exec ns1 bash
+# Link each device to a namespace respectively:
+sudo ip link set veth1 netns namespace1
+sudo ip link set veth2 netns namespace2
+
+# Bring up the devices while assigning them IP addresses:
+sudo ip netns exec namespace1 ip link set dev veth1 up
+sudo ip netns exec namespace2 ip link set dev veth2 up
+sudo ip netns exec namespace1 ip addr add 192.168.1.1/24 dev veth1
+sudo ip netns exec namespace2 ip addr add 192.168.1.2/24 dev veth2
+
+sudo ip netns exec namespace1 ip addr
+# Or multi-cmd
+multi_cmd() {
+	sudo ip netns exec namespace1 bash
 	sudo ip link set dev lo up
 	ip address
 	exit
 }
 
-sudo ip netns del ns1
+# And now verify the connectivity between the two namespaces as it is enabled
+# by the veth pair tunnel.From the first namespace we should be able to ping
+# the second one, and from the second namespace weshould be able to ping the
+# first one:
+sudo ip netns exec namespace1 ping -c5 192.168.1.2
+sudo ip netns exec namespace2 ping -c5 192.168.1.1
+
+sudo ip netns delete namespace1
+sudo ip netns delete namespace2
+
+sudo ip netns list
