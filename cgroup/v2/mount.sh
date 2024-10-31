26cd4ffbcdb823a1af1462a4c239557eb0dfaaf6
Rong Tao <rongtao@cestc.cn>
2024-10-31 04:51:31 -0400

    cgroup/v2: Mount and set name
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cgroup/v2/mount.sh
+++ cgroup/v2/mount.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
-mkdir $PWD/cgroup-v2.dir
-sudo mount -t cgroup2 none $PWD/cgroup-v2.dir
+
+NAME=test-cgroupv2
+MNT_POINT=$PWD/cgroup-v2.dir
+
+mkdir -p ${MNT_POINT}
+
+sudo mount --type cgroup2 ${NAME} ${MNT_POINT}
+mount | grep ${NAME}
 
