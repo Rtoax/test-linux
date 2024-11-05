8d6ba4b95a60b7af4d8471af381a09abe7833dff
Rong Tao <rongtao@cestc.cn>
2024-11-05 16:02:35 +0800

    module/params: Add int.c
        $ sudo insmod int.ko num0=10 num1=20
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ module/params/int.c
@@ -0,0 +1,37 @@
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+
+/* Example 0 */
+static int num0 = 0;
+module_param(num0, int, 0660);
+
+/* Example 1 */
+static int set_num1(const char *val, const struct kernel_param *kp)
+{
+	int n = 0, ret;
+	ret = kstrtoint(val, 10, &n);
+	if (ret != 0 || n < 1 || n > 32)
+		return -EINVAL;
+	return param_set_int(val, kp);
+}
+
+static const struct kernel_param_ops param_ops = {
+	.set = set_num1,
+	.get = param_get_int,
+};
+
+static int num1 = 100;
+module_param_cb(num1, &param_ops, &num1, 0664);
+
+
+static int simple_init(void)
+{
+	printk(KERN_WARNING "num0=%d num1=%d\n", num0, num1);
+	return -EINVAL;
+}
+
+module_init(simple_init);
+MODULE_AUTHOR("Rong Tao");
+MODULE_LICENSE("GPL");
