c27cc0919b3fa48f00f73a6833f15b28405d626c
Rong Tao <rongtao@cestc.cn>
2024-11-05 15:11:49 +0800

    modules/params: Add array.c
        $ sudo insmod array.ko array_int=1,2,3,4,5,6,7,8
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ module/params/array.c
@@ -0,0 +1,24 @@
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>
+
+#define SIZE 8
+
+/**
+ * Format: array_int=1,2,3,4,5,6,7,8
+ */
+static int array_int[SIZE];
+static int count;
+module_param_array(array_int, int, &count, 0660);
+
+static int simple_init(void)
+{
+	int i;
+	for (i = 0; i < count; i++)
+		printk(KERN_WARNING "array_int[%d]=%d\n", i, array_int[i]);
+	return -EINVAL;
+}
+
+module_init(simple_init);
+MODULE_AUTHOR("Rong Tao");
+MODULE_LICENSE("GPL");
