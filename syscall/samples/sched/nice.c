8b9a9906772692f83627ab34f249a1e7e2644ea3
Rong Tao <rongtao@cestc.cn>
2024-11-07 19:28:00 +0800

    syscall/samples/sched: nice.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/sched/nice.c
+++ syscall/samples/sched/nice.c
@@ -4,7 +4,11 @@
 
 int nice_self(int increment)
 {
-	int result, old = getpriority(PRIO_PROCESS, 0);
+	int result, old;
+
+	old = getpriority(PRIO_PROCESS, 0);
+	fprintf(stderr, "old priority %d\n", old);
+
 	result = setpriority(PRIO_PROCESS, 0, old + increment);
 	if (result != -1)
 		return old + increment;
@@ -14,7 +18,8 @@ int nice_self(int increment)
 
 int main(void)
 {
-	nice(1);
 	nice_self(1);
+	nice(2);
+	nice_self(3);
 	return 0;
 }
