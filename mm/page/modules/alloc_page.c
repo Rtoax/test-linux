0887a0192f81ee65c644a1e36d7be124c492553d
Rong Tao <rongtao@cestc.cn>
2024-11-08 12:52:01 +0800

    mm/page/modules/alloc_page.c: Remove exit
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- mm/page/modules/alloc_page.c
+++ mm/page/modules/alloc_page.c
@@ -76,12 +76,6 @@ static int kernel_init(void)
 	return -1;
 }
 
-static void kernel_exit(void)
-{
-	printk(KERN_INFO "my exit.\n");
-}
-
 module_init(kernel_init);
-module_exit(kernel_exit);
 MODULE_AUTHOR("Rong Tao");
 MODULE_LICENSE("GPL");
