41c5bb9fc5a034905a1a6d6cf02ce8cb13e47559
Rong Tao <rongtao@cestc.cn>
2024-11-11 14:08:37 +0800

    libs: Makefile: Compile libtest-linux-c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- libs/ipaddr.c
+++ libs/ipaddr.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "ipaddr.h"
 
+#if defined(TEST)
 int main(void)
 {
 	int i = __S2I("12113");
@@ -8,3 +9,4 @@ int main(void)
 	printf("%x\n", IPv4S("1", "1", "1", "1"));
 	return 0;
 }
+#endif
