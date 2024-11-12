97d0d5774d1dd4331844d96aa378d469913d3df0
Rong Tao <rongtao@cestc.cn>
2024-11-12 19:37:05 +0800

    numa: info.sh: Add [raw] arg
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- numa/info.sh
+++ numa/info.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 set -e
 
-sudo dmesg | grep -oe "node[[:space:]]\{1,\}[[:digit:]]\{1,2\}: \[mem 0x[0-9a-fA-F]\{16\}-0x[0-9a-fA-F]\{16\}\]"
+# Match format: 'node   0: mem 0x0000000000001000-0x000000000009efff]'
+dmesg_node_mem_raw()
+{
+	sudo dmesg | \
+		grep -oe "node[[:space:]]\{1,\}[[:digit:]]\{1,2\}: \[mem 0x[0-9a-fA-F]\{16\}-0x[0-9a-fA-F]\{16\}\]"
+}
+
+case $1 in
+raw)
+	dmesg_node_mem_raw
+	;;
+*)
+	echo >&2 "Usage: $0 [raw]"
+	;;
+esac
