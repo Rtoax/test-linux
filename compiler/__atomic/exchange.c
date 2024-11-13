2f27ab12580eef21ab057917971a5175d46666d9
Rong Tao <rongtao@cestc.cn>
2024-11-13 19:05:35 +0800

    __atomic: Add __atomic_exchange_n() test (todo)
    Link: https://gcc.gnu.org/onlinedocs/gcc-14.2.0/gcc/_005f_005fatomic-Builtins.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ compiler/__atomic/exchange.c
@@ -0,0 +1,14 @@
+/**
+ * type __atomic_exchange_n (type *ptr, type val, int memorder)
+ *    tmp = *ptr; *ptr = val; return tmp;
+ * void __atomic_exchange (type *ptr, type *val, type *ret, int memorder)
+ *    *ret = *ptr; *ptr = *val;
+ */
+#include <stdio.h>
+
+
+int main(void)
+{
+
+	return 0;
+}
