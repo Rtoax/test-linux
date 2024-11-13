a62002862ae6f41f5739306876d0a6a7e6c12c2b
Rong Tao <rongtao@cestc.cn>
2024-11-13 19:18:45 +0800

    __atomic: exchange.c: Test __atomic_exchange() and __atomic_exchange_n()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/__atomic/exchange.c
+++ compiler/__atomic/exchange.c
@@ -9,6 +9,16 @@
 
 int main(void)
 {
+	int a, val, ret;
+
+	a = 10;
+	ret = __atomic_exchange_n(&a, 20, __ATOMIC_ACQ_REL);
+	printf("a = %d, ret = %d\n", a, ret);
+
+	a = 10;
+	val = 20;
+	__atomic_exchange(&a, &val, &ret, __ATOMIC_ACQ_REL);
+	printf("a = %d, ret = %d\n", a, ret);
 
 	return 0;
 }
