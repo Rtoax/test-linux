d94977ca90dd84efe777b7c7cdf5070b49390a8c
Rong Tao <rongtao@cestc.cn>
2024-11-14 19:21:25 +0800

    compiler/__attribute__: Update naked
    Link: https://gcc.gnu.org/onlinedocs/gcc-14.2.0/gcc/BPF-Function-Attributes.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/__attribute__/__attribute____naked__.c
+++ compiler/__attribute__/__attribute____naked__.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
-
 /* The 'naked' attribute prevents the compiler from generating any
  * function entry or exit code */
-#define _naked __attribute__((__naked__))
+#define _naked __attribute__((naked))
+#define _naked2 __attribute__((__naked__))
 
 _naked int func1(void)
 {
-	return printf("rongtao\n");
+	return printf("rongtao naked\n");
 }
 
-int func2(void)
+_naked2 int func2(void)
+{
+	return printf("rongtao naked2\n");
+}
+
+int func3(void)
 {
 	return printf("rongtao\n");
 }
@@ -19,5 +24,6 @@ int main(void)
 {
 	func1();
 	func2();
+	func3();
 	return 0;
 }
---
+++ compiler/__attribute__/__attribute__naked.c
@@ -0,0 +1,1 @@
+__attribute____naked__.c
