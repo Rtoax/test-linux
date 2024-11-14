bc7fb285a57f110b3a5871b76c08ca3341624b34
Rong Tao <rongtao@cestc.cn>
2024-11-14 10:20:54 +0800

    sequentially-consistent-waw.c: Make sure Start two threads running at the same time
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/pthread/out-of-order/sequentially-consistent-waw.c
+++ glibc/pthread/out-of-order/sequentially-consistent-waw.c
@@ -4,11 +4,17 @@
 #include <stdbool.h>
 #include <pthread.h>
 
+/**
+ * lock: Start two threads running at the same time
+ */
+int lock;
 int x, y;
 
 /* waw: Write After Write */
 void *task1_waw(void *arg)
 {
+	while (__atomic_load_n(&lock, __ATOMIC_RELAXED) != 1);
+
 	y = 1;
 	x = 2;
 	return NULL;
@@ -16,8 +22,10 @@ void *task1_waw(void *arg)
 
 void *task2_waw(void *arg)
 {
+	while (__atomic_load_n(&lock, __ATOMIC_RELAXED) != 1);
+
 	if (x == 2)
-		assert(y == 1 && "Non-Sequentially Consistent");
+		assert(y == 1 && "Nonsequentially Consistent happend!!");
 	return NULL;
 }
 
@@ -27,11 +35,15 @@ int main(void)
 	pthread_t tasks[2];
 
 	while (true) {
+		__atomic_store_n(&lock, 0, __ATOMIC_RELAXED);
+
 		x = y = 0;
 		count++;
 
 		pthread_create(&tasks[0], NULL, task1_waw, NULL);
 		pthread_create(&tasks[1], NULL, task2_waw, NULL);
+
+		__atomic_store_n(&lock, 1, __ATOMIC_RELAXED);
 
 		pthread_join(tasks[0], NULL);
 		pthread_join(tasks[1], NULL);
