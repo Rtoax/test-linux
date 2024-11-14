92fed80c497067ebccc8efbbc59cfe0404652685
Rong Tao <rongtao@cestc.cn>
2024-11-14 10:23:40 +0800

    pthread: out-of-order: Test r,w,rw barrier
    All three r,w,rw barrier could not out-of-order exec on x86_64
    Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/pthread/out-of-order/barrier.c
+++ glibc/pthread/out-of-order/barrier.c
@@ -1,8 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
 /**
  * Prove that the CPU executes out of order and use memory barriers to
  * eliminate the impact of out of order execution.
- *
- * 2021-05-26	Rong Tao	Create this.
  */
 #include <stdio.h>
 #include <stdbool.h>
@@ -10,53 +9,39 @@
 
 #include "barrier.h"
 
-#if defined(HAVE_MBARRIER)
+#if defined(HAVE_MRWBARRIER)
 /* Could avoid out-of-order */
-#define mrwbarrier() smp_rwmb()
-#define mrbarrier() smp_rmb()
-#define mwbarrier() smp_wmb()
+#define __test_barrier() smp_rwmb()
+#elif defined(HAVE_MRBARRIER)
+#define __test_barrier() smp_rmb()
+#elif defined(HAVE_MWBARRIER)
+#define __test_barrier() smp_wmb()
 #elif defined(HAVE___sync_synchronize)
 /* Could avoid out-of-order */
-#define mrwbarrier() __sync_synchronize()
-#define mrbarrier() __sync_synchronize()
-#define mwbarrier() __sync_synchronize()
+#define __test_barrier() __sync_synchronize()
 #elif defined(HAVE___atomic_thread_fence__ATOMIC_RELAXED)
 /* Could out-of-order */
-#define mrwbarrier() __atomic_thread_fence(__ATOMIC_RELAXED)
-#define mrbarrier() __atomic_thread_fence(__ATOMIC_RELAXED)
-#define mwbarrier() __atomic_thread_fence(__ATOMIC_RELAXED)
+#define __test_barrier() __atomic_thread_fence(__ATOMIC_RELAXED)
 #elif defined(HAVE___atomic_thread_fence__ATOMIC_CONSUME)
 /* Could out-of-order */
-#define mrwbarrier() __atomic_thread_fence(__ATOMIC_CONSUME)
-#define mrbarrier() __atomic_thread_fence(__ATOMIC_CONSUME)
-#define mwbarrier() __atomic_thread_fence(__ATOMIC_CONSUME)
+#define __test_barrier() __atomic_thread_fence(__ATOMIC_CONSUME)
 #elif defined(HAVE___atomic_thread_fence__ATOMIC_ACQUIRE)
-/* Could out-of-order */
-#define mrwbarrier() __atomic_thread_fence(__ATOMIC_ACQUIRE)
-#define mrbarrier() __atomic_thread_fence(__ATOMIC_ACQUIRE)
-#define mwbarrier() __atomic_thread_fence(__ATOMIC_ACQUIRE)
+/* load barrier, could out-of-order */
+#define __test_barrier() __atomic_thread_fence(__ATOMIC_ACQUIRE)
 #elif defined(HAVE___atomic_thread_fence__ATOMIC_RELEASE)
-/* Could out-of-order */
-#define mrwbarrier() __atomic_thread_fence(__ATOMIC_RELEASE)
-#define mrbarrier() __atomic_thread_fence(__ATOMIC_RELEASE)
-#define mwbarrier() __atomic_thread_fence(__ATOMIC_RELEASE)
+/* store barrier, could out-of-order */
+#define __test_barrier() __atomic_thread_fence(__ATOMIC_RELEASE)
 #elif defined(HAVE___atomic_thread_fence__ATOMIC_ACQ_REL)
 /**
- * Could out-of-order on x86_64, but aarch64 it's OK.
+ * store-load barrier, could out-of-order on x86_64, but aarch64 it's OK.
  */
-#define mrwbarrier() __atomic_thread_fence(__ATOMIC_ACQ_REL)
-#define mrbarrier() __atomic_thread_fence(__ATOMIC_ACQ_REL)
-#define mwbarrier() __atomic_thread_fence(__ATOMIC_ACQ_REL)
+#define __test_barrier() __atomic_thread_fence(__ATOMIC_ACQ_REL)
 #elif defined(HAVE___atomic_thread_fence__ATOMIC_SEQ_CST)
 /* Could avoid out-of-order */
-#define mrwbarrier() __atomic_thread_fence(__ATOMIC_SEQ_CST)
-#define mrbarrier() __atomic_thread_fence(__ATOMIC_SEQ_CST)
-#define mwbarrier() __atomic_thread_fence(__ATOMIC_SEQ_CST)
+#define __test_barrier() __atomic_thread_fence(__ATOMIC_SEQ_CST)
 #elif defined(HAVE_NO_BARRIER)
 /* Could out-of-order */
-#define mrwbarrier()
-#define mrbarrier()
-#define mwbarrier()
+#define __test_barrier()
 #else
 #error "Nothing to do."
 #endif
@@ -66,7 +51,7 @@ volatile int x, y, a, b;
 void *task1(void *arg)
 {
 	a = 1;
-	mrwbarrier();
+	__test_barrier();
 	x = b;
 	pthread_exit(NULL);
 }
@@ -74,7 +59,7 @@ void *task1(void *arg)
 void *task2(void *arg)
 {
 	b = 1;
-	mrwbarrier();
+	__test_barrier();
 	y = a;
 	pthread_exit(NULL);
 }
--- synchronization/barrier/barrier-x86.h
+++ synchronization/barrier/barrier-x86.h
@@ -1,5 +1,5 @@
 #pragma once
-#define barrier() __asm__ __volatile__("": : :"memory")
+#define barrier() __asm__ __volatile__("" ::: "memory")
 
 #if defined(__i386__)
 #define smp_mb()	asm volatile("lock; addl $0,0(%%esp)" ::: "memory")
@@ -7,8 +7,8 @@
 #define smp_wmb()	asm volatile("lock; addl $0,0(%%esp)" ::: "memory")
 #define smp_rwmb()	smp_mb()
 #elif defined(__x86_64__)
-#define smp_mb() 	asm volatile("mfence":::"memory")
-#define smp_rmb()	asm volatile("lfence":::"memory")
+#define smp_mb() 	asm volatile("mfence" ::: "memory")
+#define smp_rmb()	asm volatile("lfence" ::: "memory")
 #define smp_wmb()	asm volatile("sfence" ::: "memory")
 #define smp_rwmb()	smp_mb()
 #endif
