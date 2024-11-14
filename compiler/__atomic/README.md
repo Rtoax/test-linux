606cb88ae8d2547ae885d03170df711976b3a226
Rong Tao <rongtao@cestc.cn>
2024-11-14 09:42:52 +0800

    __atomic: Test __atomic_compare_exchange_n()
    Link: https://gcc.gnu.org/onlinedocs/gcc-14.2.0/gcc/_005f_005fatomic-Builtins.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/__atomic/README.md
+++ compiler/__atomic/README.md
@@ -12,16 +12,19 @@ These functions are intended to replace the legacy `__sync` builtins. The main d
 	* 意味着没有线程间排序约束。
 
 - `__ATOMIC_CONSUME`: This is currently implemented using the stronger `__ATOMIC_ACQUIRE` memory order because of a deficiency in C++11’s semantics for `memory_order_consume`.
-	* `__ATOMIC_ACQUIRE` 由于 C++11 的语义存在缺陷， 目前使用更强的内存顺序来实现`memory_order_consume`
+	* `__ATOMIC_ACQUIRE` 由于 C++11 的语义存在缺陷，目前使用更强的内存顺序来实现`memory_order_consume`
 
 - `__ATOMIC_ACQUIRE`: Creates an inter-thread happens-before constraint from the release (or stronger) semantic store to this acquire load. Can prevent hoisting of code to before the operation.
 	* 可以防止将代码提升到操作之前。
+	* load barrier
 
 - `__ATOMIC_RELEASE`: Creates an inter-thread happens-before constraint to acquire (or stronger) semantic loads that read from this release store. Can prevent sinking of code to after the operation.
 	* 可以防止代码沉入操作之后。
+	* store barrier
 
 - `__ATOMIC_ACQ_REL`: Combines the effects of both `__ATOMIC_ACQUIRE` and `__ATOMIC_RELEASE`.
 	* `__ATOMIC_ACQUIRE`和`__ATOMIC_RELEASE`结合。
+	* store-load barrier
 	* 在`aarch64`上是**读写屏障**
 
 - `__ATOMIC_SEQ_CST`: Enforces total ordering with all other `__ATOMIC_SEQ_CST` operations.
--- compiler/__atomic/compare_exchange.c
+++ compiler/__atomic/compare_exchange.c
@@ -14,16 +14,45 @@
  *     return false;
  *   }
  *
+ *   failure_memorder cannot be __ATOMIC_RELEASE nor __ATOMIC_ACQ_REL
+ *
  * bool __atomic_compare_exchange (type *ptr, type *expected, type *desired,
  *                                 bool weak, int success_memorder,
  *                                 int failure_memorder)
  *   - only desired value is a pointer
  */
 #include <stdio.h>
+#include <stdbool.h>
 
 
 int main(void)
 {
-	/* TODO */
+	int val, expected, desired;
+	bool weak, ret;
+
+	val = 1;
+	expected = 1;
+	desired = 2;
+	weak = true;
+	ret = __atomic_compare_exchange_n(&val, &expected, desired, weak,
+				  __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+	printf("val = %d, expected = %d, ret = %d\n", val, expected, ret);
+
+	val = 1;
+	expected = 2;
+	desired = 2;
+	weak = true;
+	ret = __atomic_compare_exchange_n(&val, &expected, desired, weak,
+				  __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+	printf("val = %d, expected = %d, ret = %d\n", val, expected, ret);
+
+	val = 1;
+	expected = 2;
+	desired = 2;
+	weak = true;
+	ret = __atomic_compare_exchange(&val, &expected, &desired, weak,
+				  __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+	printf("val = %d, expected = %d, ret = %d\n", val, expected, ret);
+
 	return 0;
 }
