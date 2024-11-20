5629e39a9da52e5d52174403d8653def5ebe8797
Rong Tao <rongtao@cestc.cn>
2024-11-20 12:19:52 +0800

    driver/char/shmqueue: Code style
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- driver/char/shmqueue/shmq.c
+++ driver/char/shmqueue/shmq.c
@@ -1,10 +1,10 @@
-/***********************************************************************************************************************
+/*****************************************************************************
  ** Share Memory Queue Driver
- **     Copyright(C) Sylincom Rong Tao  <rongtao@sylincom.com>
+ **	 Copyright(C) Sylincom Rong Tao  <rongtao@sylincom.com>
  **
- **    History: 2019.09.30  Rong Tao    Create
- **             2019.10.08  Rong Tao    Read Write lock
- **********************************************************************************************************************/
+ **	History: 2019.09.30  Rong Tao	Create
+ **			 2019.10.08  Rong Tao	Read Write lock
+ *****************************************************************************/
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/types.h>
@@ -41,73 +41,72 @@
 
 
 
-#define __SHMQ_MAJOR	    SHMQ_MAJOR
-#define __SHMQ_DEV_NAME	    SHMQ_DEV_NAME
+#define __SHMQ_MAJOR		SHMQ_MAJOR
+#define __SHMQ_DEV_NAME		SHMQ_DEV_NAME
 
 
-#define __SHMQ_RWLOCK       0   //使用读写锁
+#define __SHMQ_RWLOCK	   0   //使用读写锁
 
-/* type ***************************************************************************************************************/
 typedef struct shmq_dev_s {
 #if !__SHMQ_RWLOCK
-    struct mutex        mxlock;   
+	struct mutex		mxlock;   
 #else
-    rwlock_t            rwlock;
+	rwlock_t			rwlock;
 #endif
-    dev_t               dev_no;
-    struct class *      class;
-    struct device *     device;
-    struct cdev         cdev;    
-    wait_queue_head_t   queue;
-    atomic_t            qwake;
-    struct task_struct * thread;
+	dev_t			   dev_no;
+	struct class *	  class;
+	struct device *	 device;
+	struct cdev		 cdev;	
+	wait_queue_head_t   queue;
+	atomic_t			qwake;
+	struct task_struct * thread;
 
-    unsigned char * testmap;
-    unsigned char * kmalloc_area;
+	unsigned char * testmap;
+	unsigned char * kmalloc_area;
 
 }shmq_dev_t;
 
 
 /* log ****************************************************************************************************************/
 typedef enum {
-    SHMQ_DEBUG,   SHMQ_INFO,  SHMQ_DEFAULT,  SHMQ_NOTICE,  SHMQ_EMERG,  SHMQ_ALERT,  SHMQ_WARNING, SHMQ_ERR,  
+	SHMQ_DEBUG,   SHMQ_INFO,  SHMQ_DEFAULT,  SHMQ_NOTICE,  SHMQ_EMERG,  SHMQ_ALERT,  SHMQ_WARNING, SHMQ_ERR,  
 }shmq_log_lv;
 
 static struct {
-    int level;
-    char *str;
+	int level;
+	char *str;
 } shmq_log_prefix[] __attribute__((unused)) = {
-{SHMQ_DEBUG,    KERN_DEBUG},
-{SHMQ_INFO,     KERN_INFO},
+{SHMQ_DEBUG,	KERN_DEBUG},
+{SHMQ_INFO,	 KERN_INFO},
 {SHMQ_DEFAULT,  KERN_DEFAULT},
 {SHMQ_NOTICE,   KERN_NOTICE},
-{SHMQ_EMERG,    KERN_EMERG},
-{SHMQ_ALERT,    KERN_ALERT},
+{SHMQ_EMERG,	KERN_EMERG},
+{SHMQ_ALERT,	KERN_ALERT},
 {SHMQ_WARNING,  KERN_WARNING},
-{SHMQ_ERR,      KERN_ERR},
+{SHMQ_ERR,	  KERN_ERR},
 };
 
 static inline int __attribute__((unused)) __shmq_log(shmq_log_lv level, const char *func, int line, const char *fmt, ...)
 {
-    int n=0;
+	int n=0;
 #define SHMQ_LOG_PREFIX_FMT "[SHMQ %s | %s:%d] "
-    va_list va;
-    va_start(va, fmt);
-    n = printk(SHMQ_LOG_PREFIX_FMT, shmq_log_prefix[level].str, func, line);
-    n += vprintk(fmt, va);
-    va_end(va);
-    
-    return n;
+	va_list va;
+	va_start(va, fmt);
+	n = printk(SHMQ_LOG_PREFIX_FMT, shmq_log_prefix[level].str, func, line);
+	n += vprintk(fmt, va);
+	va_end(va);
+	
+	return n;
 }
 
-#define shmq_debug(fmt...)      __shmq_log(SHMQ_DEBUG, __func__, __LINE__, fmt)
-#define shmq_info(fmt...)       __shmq_log(SHMQ_INFO, __func__, __LINE__, fmt)
-#define shmq_default(fmt...)    __shmq_log(SHMQ_DEFAULT, __func__, __LINE__, fmt)
-#define shmq_notice(fmt...)     __shmq_log(SHMQ_NOTICE, __func__, __LINE__, fmt)
-#define shmq_emerg(fmt...)      __shmq_log(SHMQ_EMERG, __func__, __LINE__, fmt)
-#define shmq_alert(fmt...)      __shmq_log(SHMQ_ALERT, __func__, __LINE__, fmt)
-#define shmq_warning(fmt...)    __shmq_log(SHMQ_WARNING, __func__, __LINE__, fmt)
-#define shmq_err(fmt...)        __shmq_log(SHMQ_ERR, __func__, __LINE__, fmt)
+#define shmq_debug(fmt...)	  __shmq_log(SHMQ_DEBUG, __func__, __LINE__, fmt)
+#define shmq_info(fmt...)	   __shmq_log(SHMQ_INFO, __func__, __LINE__, fmt)
+#define shmq_default(fmt...)	__shmq_log(SHMQ_DEFAULT, __func__, __LINE__, fmt)
+#define shmq_notice(fmt...)	 __shmq_log(SHMQ_NOTICE, __func__, __LINE__, fmt)
+#define shmq_emerg(fmt...)	  __shmq_log(SHMQ_EMERG, __func__, __LINE__, fmt)
+#define shmq_alert(fmt...)	  __shmq_log(SHMQ_ALERT, __func__, __LINE__, fmt)
+#define shmq_warning(fmt...)	__shmq_log(SHMQ_WARNING, __func__, __LINE__, fmt)
+#define shmq_err(fmt...)		__shmq_log(SHMQ_ERR, __func__, __LINE__, fmt)
 
 
 /* Global Value *******************************************************************************************************/
@@ -120,8 +119,8 @@ static int __attribute__((unused)) _shmq_readable = 1;
 
 
 /* ioctl CMD **********************************************************************************************************/
-#define __SHMQ_IOC_WAIT       SHMQ_IOC_WAIT
-#define __SHMQ_IOC_WAKE_UP    SHMQ_IOC_WAKE_UP
+#define __SHMQ_IOC_WAIT	   SHMQ_IOC_WAIT
+#define __SHMQ_IOC_WAKE_UP	SHMQ_IOC_WAKE_UP
 
 
 
@@ -134,10 +133,10 @@ static int __attribute__((unused)) _shmq_readable = 1;
  */
 static int shmq_open(struct inode *inode, struct file *file)
 {
-    shmq_debug(__SHMQ_DEV_NAME"\n");
-    shmq_info("inode->i_nlink = %d\n", inode->i_nlink);
-    shmq_info("inode->i_count = %d\n", inode->i_count);
-    
+	shmq_debug(__SHMQ_DEV_NAME"\n");
+	shmq_info("inode->i_nlink = %d\n", inode->i_nlink);
+	shmq_info("inode->i_count = %d\n", inode->i_count);
+	
 	return 0;
 }
 
@@ -149,23 +148,23 @@ static int condition = 0;
 static ssize_t shmq_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos)
 {
 	shmq_debug(__SHMQ_DEV_NAME"\n");
-    
-#if __SHMQ_RWLOCK    
-    write_lock(&shmq_dev[0]->rwlock);
+	
+#if __SHMQ_RWLOCK	
+	write_lock(&shmq_dev[0]->rwlock);
 #else 
-    mutex_lock(&shmq_dev[0]->mxlock);
+	mutex_lock(&shmq_dev[0]->mxlock);
 #endif
 
-    /* copy from user space */
-    if(copy_from_user(shmq_dev[0]->testmap, buf, count))
-    {
-        shmq_err("write error!\n");
-    }
-    
-#if __SHMQ_RWLOCK    
-    write_unlock(&shmq_dev[0]->rwlock);
+	/* copy from user space */
+	if(copy_from_user(shmq_dev[0]->testmap, buf, count))
+	{
+		shmq_err("write error!\n");
+	}
+	
+#if __SHMQ_RWLOCK	
+	write_unlock(&shmq_dev[0]->rwlock);
 #else 
-    mutex_unlock(&shmq_dev[0]->mxlock);
+	mutex_unlock(&shmq_dev[0]->mxlock);
 #endif
 
 /*
@@ -178,9 +177,9 @@ sleep_on_interruptible(wait_queue_head_t *wqueue_h);
 void wake_up(wait_queue_t *wqueue);
 void wake_up_interruptible(wait_queue_t *wqueue);
 */
-    condition = 1;
-    wake_up_interruptible(&shmq_dev[0]->queue);
-    
+	condition = 1;
+	wake_up_interruptible(&shmq_dev[0]->queue);
+	
 	return count;
 }
 
@@ -193,30 +192,30 @@ static ssize_t shmq_read(struct file *file, char __user *buf, size_t count, loff
 {
 	shmq_debug(__SHMQ_DEV_NAME"\n");
 
-    if(!condition)    //条件可以在中断处理函数或另外的接口中置位
-        wait_event_interruptible((shmq_dev[0]->queue), condition);
-    
+	if(!condition)	//条件可以在中断处理函数或另外的接口中置位
+		wait_event_interruptible((shmq_dev[0]->queue), condition);
+	
 #if __SHMQ_RWLOCK  
-    read_lock(&shmq_dev[0]->rwlock);
+	read_lock(&shmq_dev[0]->rwlock);
 #else 
-    mutex_lock(&shmq_dev[0]->mxlock);
+	mutex_lock(&shmq_dev[0]->mxlock);
 #endif
 
-    /* copy to user space */
-    if(copy_to_user(buf, shmq_dev[0]->testmap, count))
-    {
-        shmq_err("read error!\n");
-    }
-    
-    condition = 0;
-    
+	/* copy to user space */
+	if(copy_to_user(buf, shmq_dev[0]->testmap, count))
+	{
+		shmq_err("read error!\n");
+	}
+	
+	condition = 0;
+	
 #if __SHMQ_RWLOCK  
-    read_unlock(&shmq_dev[0]->rwlock);
+	read_unlock(&shmq_dev[0]->rwlock);
 #else 
-    mutex_unlock(&shmq_dev[0]->mxlock);
+	mutex_unlock(&shmq_dev[0]->mxlock);
 #endif
 
-    return count;
+	return count;
 }
 
 /**
@@ -277,27 +276,27 @@ static unsigned int shmq_poll(struct file *file, struct poll_table_struct *tab)
  */
 static long shmq_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
-    int result;
-    
+	int result;
+	
 	shmq_debug(__SHMQ_DEV_NAME"\n");
-    
-    switch(cmd) 
-    {	
-        case __SHMQ_IOC_WAIT:
-            {	
-                result=10;
-            }			
-            break;
-        case __SHMQ_IOC_WAKE_UP:
-            {	
-                result=20;
-            }	
-            break;
-        default:
-            return -ENOTTY;			
-    }
-    
-    return(result);
+	
+	switch(cmd) 
+	{	
+		case __SHMQ_IOC_WAIT:
+			{	
+				result=10;
+			}			
+			break;
+		case __SHMQ_IOC_WAKE_UP:
+			{	
+				result=20;
+			}	
+			break;
+		default:
+			return -ENOTTY;			
+	}
+	
+	return(result);
 }
 /**
  *	kernel: int (*mmap) (struct file *, struct vm_area_struct *);
@@ -305,18 +304,18 @@ static long shmq_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
  */
 static int shmq_mmap(struct file *file, struct vm_area_struct *vma)
 {
-    int ret;
+	int ret;
 	shmq_debug(__SHMQ_DEV_NAME"\n");
-    ret = remap_pfn_range(vma,vma->vm_start,
-                        virt_to_phys((void *)((unsigned long)shmq_dev[0]->kmalloc_area))>>PAGE_SHIFT,
-                        vma->vm_end-vma->vm_start,
-                        PAGE_SHARED);
-    if(ret!=0)
-    {
-        shmq_warning("remap_pfn_range error.\n");
-        return -EAGAIN;
-    } 
-    return ret;
+	ret = remap_pfn_range(vma,vma->vm_start,
+						virt_to_phys((void *)((unsigned long)shmq_dev[0]->kmalloc_area))>>PAGE_SHIFT,
+						vma->vm_end-vma->vm_start,
+						PAGE_SHARED);
+	if(ret!=0)
+	{
+		shmq_warning("remap_pfn_range error.\n");
+		return -EAGAIN;
+	} 
+	return ret;
 }
 
 /**
@@ -355,115 +354,115 @@ static struct file_operations shmq_flops = {
 static int __init shmq_init(void)
 {
 	int ret;
-    unsigned char *virt_addr;
-    
-    /* init shmq_dev */
-    memset(&shmq_dev[0], 0, sizeof(shmq_dev_t*) * sizeof(shmq_dev)/sizeof(shmq_dev_t*));
-    shmq_dev[0]  = (shmq_dev_t*)kzalloc(sizeof(shmq_dev_t), GFP_KERNEL);
-    
+	unsigned char *virt_addr;
+	
+	/* init shmq_dev */
+	memset(&shmq_dev[0], 0, sizeof(shmq_dev_t*) * sizeof(shmq_dev)/sizeof(shmq_dev_t*));
+	shmq_dev[0]  = (shmq_dev_t*)kzalloc(sizeof(shmq_dev_t), GFP_KERNEL);
+	
 
-    
-    shmq_dev[0]->dev_no = MKDEV(__SHMQ_MAJOR , 0);
-       
-    ret = register_chrdev_region(shmq_dev[0]->dev_no , 1 ,__SHMQ_DEV_NAME);
-    if(ret < 0)
+	
+	shmq_dev[0]->dev_no = MKDEV(__SHMQ_MAJOR , 0);
+	   
+	ret = register_chrdev_region(shmq_dev[0]->dev_no , 1 ,__SHMQ_DEV_NAME);
+	if(ret < 0)
 	{
-	    shmq_warning(__SHMQ_DEV_NAME " can't register major number.\n");
+		shmq_warning(__SHMQ_DEV_NAME " can't register major number.\n");
 		goto err0;
 	}
-    
-    cdev_init(&shmq_dev[0]->cdev , &shmq_flops) ;
+	
+	cdev_init(&shmq_dev[0]->cdev , &shmq_flops) ;
 	shmq_dev[0]->cdev.owner = THIS_MODULE ;
 	shmq_dev[0]->cdev.ops = &shmq_flops ;
-    
+	
 	shmq_debug(__SHMQ_DEV_NAME " Initialized.\n");
-    
-    ret = cdev_add(&shmq_dev[0]->cdev , shmq_dev[0]->dev_no , 1) ;
+	
+	ret = cdev_add(&shmq_dev[0]->cdev , shmq_dev[0]->dev_no , 1) ;
 	if(ret < 0)
 	{
 		shmq_debug(__SHMQ_DEV_NAME "cdev add error !\n") ;
 		ret = -1;
-        goto err2;
+		goto err2;
 	}
-    
+	
 #if 0
-    shmq_dev[0]->class = class_create(THIS_MODULE, __SHMQ_DEV_NAME);
-    if (IS_ERR((void *)shmq_dev[0]->class)) 
-    {
-        shmq_err("failed create class");
-        shmq_dev[0]->class = NULL;
-    }
+	shmq_dev[0]->class = class_create(THIS_MODULE, __SHMQ_DEV_NAME);
+	if (IS_ERR((void *)shmq_dev[0]->class)) 
+	{
+		shmq_err("failed create class");
+		shmq_dev[0]->class = NULL;
+	}
 #endif
 
-    if(shmq_dev[0]->class)
-    {
-        shmq_dev[0]->device = device_create(shmq_dev[0]->class, NULL, shmq_dev[0]->dev_no, NULL, __SHMQ_DEV_NAME);
-        if (IS_ERR((void *)shmq_dev[0]->device)) 
-        {
-            shmq_err("failed create / device\n");
-            ret = -2;
-            goto err2;
-        }
-        /* 设备信息 <rt 2019.09>*/
-        dev_info(shmq_dev[0]->device, "Device: %s\n", __SHMQ_DEV_NAME);
-    }
-    
-    /* 初始化锁 <rt 2019.09>*/
+	if(shmq_dev[0]->class)
+	{
+		shmq_dev[0]->device = device_create(shmq_dev[0]->class, NULL, shmq_dev[0]->dev_no, NULL, __SHMQ_DEV_NAME);
+		if (IS_ERR((void *)shmq_dev[0]->device)) 
+		{
+			shmq_err("failed create / device\n");
+			ret = -2;
+			goto err2;
+		}
+		/* 设备信息 <rt 2019.09>*/
+		dev_info(shmq_dev[0]->device, "Device: %s\n", __SHMQ_DEV_NAME);
+	}
+	
+	/* 初始化锁 <rt 2019.09>*/
 #if __SHMQ_RWLOCK  
-    rwlock_init(&shmq_dev[0]->rwlock); //read write lock is better
-#else    
-    mutex_init(&shmq_dev[0]->mxlock);
+	rwlock_init(&shmq_dev[0]->rwlock); //read write lock is better
+#else	
+	mutex_init(&shmq_dev[0]->mxlock);
 #endif
-    
-    /* TEST */
-    shmq_dev[0]->testmap = kmalloc(4096,GFP_KERNEL);
-    shmq_dev[0]->kmalloc_area=(unsigned char *)(((unsigned long)shmq_dev[0]->testmap +PAGE_SIZE-1)&PAGE_MASK);
-    if(shmq_dev[0]->testmap==NULL)
-    {
-        shmq_err("Kernel mem get pages error\n");
-    } 
-    
-    for(virt_addr=(unsigned char *)shmq_dev[0]->kmalloc_area;
-        virt_addr<(unsigned char *)shmq_dev[0]->kmalloc_area+4096;
-        virt_addr+=PAGE_SIZE)
-    {
-        SetPageReserved(virt_to_page(virt_addr)); 
-    }
-    memset(shmq_dev[0]->testmap, 0, 100); 
+	
+	/* TEST */
+	shmq_dev[0]->testmap = kmalloc(4096,GFP_KERNEL);
+	shmq_dev[0]->kmalloc_area=(unsigned char *)(((unsigned long)shmq_dev[0]->testmap +PAGE_SIZE-1)&PAGE_MASK);
+	if(shmq_dev[0]->testmap==NULL)
+	{
+		shmq_err("Kernel mem get pages error\n");
+	} 
+	
+	for(virt_addr=(unsigned char *)shmq_dev[0]->kmalloc_area;
+		virt_addr<(unsigned char *)shmq_dev[0]->kmalloc_area+4096;
+		virt_addr+=PAGE_SIZE)
+	{
+		SetPageReserved(virt_to_page(virt_addr)); 
+	}
+	memset(shmq_dev[0]->testmap, 0, 100); 
 	shmq_debug("Test drv reg success !\n") ;
 
-    /* init queue 
-        void wait_event(wait_queue_head_t *waitq_h,int condition);
-        void wait_event_interruptible(wait_queue_head_t *waitq_h,int condition);
-        void wait_event_timeout(wait_queue_head_t *waitq_h,int condition);
-        void wait_event_interruptible_timeout(wait_queue_head_t *waitq_h,int condition);
-        sleep_on(wait_queue_head_t *wqueue_h);
-        sleep_on_interruptible(wait_queue_head_t *wqueue_h);
-        void wake_up(wait_queue_t *wqueue);
-        void wake_up_interruptible(wait_queue_t *wqueue);
+	/* init queue 
+		void wait_event(wait_queue_head_t *waitq_h,int condition);
+		void wait_event_interruptible(wait_queue_head_t *waitq_h,int condition);
+		void wait_event_timeout(wait_queue_head_t *waitq_h,int condition);
+		void wait_event_interruptible_timeout(wait_queue_head_t *waitq_h,int condition);
+		sleep_on(wait_queue_head_t *wqueue_h);
+		sleep_on_interruptible(wait_queue_head_t *wqueue_h);
+		void wake_up(wait_queue_t *wqueue);
+		void wake_up_interruptible(wait_queue_t *wqueue);
 
-    */
-    init_waitqueue_head(&shmq_dev[0]->queue);
+	*/
+	init_waitqueue_head(&shmq_dev[0]->queue);
 
-    
+	
 	return 0;
-    
+	
 err2:
 #if 0
-    device_destroy(shmq_dev[0]->class, shmq_dev[0]->dev_no);
-    class_destroy(shmq_dev[0]->class);
+	device_destroy(shmq_dev[0]->class, shmq_dev[0]->dev_no);
+	class_destroy(shmq_dev[0]->class);
 #endif
-    cdev_del(&shmq_dev[0]->cdev);
+	cdev_del(&shmq_dev[0]->cdev);
 
-    unregister_chrdev_region(shmq_dev[0]->dev_no, 1);
-    
+	unregister_chrdev_region(shmq_dev[0]->dev_no, 1);
+	
 err0:
 
-    kfree(shmq_dev[0]);
-    shmq_dev[0] = NULL;
-    shmq_err("init failed");
-    
-    return -1;
+	kfree(shmq_dev[0]);
+	shmq_dev[0] = NULL;
+	shmq_err("init failed");
+	
+	return -1;
 }
 
 /* Exit ***************************************************************************************************************/
@@ -471,27 +470,26 @@ static void __exit shmq_exit(void)
 {
 #if 1
 	if (shmq_dev[0]) 
-    {
-        if(shmq_dev[0]->class)
-        {
-            device_destroy(shmq_dev[0]->class, shmq_dev[0]->dev_no);
-            class_destroy(shmq_dev[0]->class);
-        }
-        
-        cdev_del(&shmq_dev[0]->cdev);
+	{
+		if(shmq_dev[0]->class)
+		{
+			device_destroy(shmq_dev[0]->class, shmq_dev[0]->dev_no);
+			class_destroy(shmq_dev[0]->class);
+		}
+		
+		cdev_del(&shmq_dev[0]->cdev);
 
-        unregister_chrdev_region(shmq_dev[0]->dev_no, 1);
-        
+		unregister_chrdev_region(shmq_dev[0]->dev_no, 1);
+		
 
-        kfree(shmq_dev[0]);
-        shmq_dev[0] = NULL;
-    }
+		kfree(shmq_dev[0]);
+		shmq_dev[0] = NULL;
+	}
 #else   
-    cdev_del(&shmq_dev[0]->cdev) ;
+	cdev_del(&shmq_dev[0]->cdev) ;
 	unregister_chrdev_region(shmq_dev[0]->dev_no , 1);
 #endif
 }
-
 
 /* MODULE MACRO *******************************************************************************************************/
 module_init(shmq_init);
--- driver/char/shmqueue/test.c
+++ driver/char/shmqueue/test.c
@@ -10,93 +10,82 @@
 
 static void *task1(void*arg)
 {
-    int fd;
+	int fd;
 	char val[]={"rongtao123123"};
-    int ret;
-    
-    //int open(const char *pathname, int flags, mode_t mode)
+	int ret;
+
 	fd = open(DEV_NAME, O_RDWR);
-	if(fd < 0)
+	if (fd < 0)
 		printf("task1: write: can't open %s \n", DEV_NAME);
-    printf("task1: write: fd = %d\n", fd);
+	printf("task1: write: fd = %d\n", fd);
 
-    fsync(fd);
+	fsync(fd);
 
-    flock(fd);
-    
-    ret = ioctl(fd, SHMQ_IOC_WAKE_UP);
-    printf("task1: write: ioctl: ret = %d\n", ret);
+	flock(fd);
 
-    while(1)
-    {
-        sleep(1);
-        val[0] += 1;
-        ret = write(fd, val, sizeof(val));
-        printf("task1: write: ret = %d, val = %s\n", ret, val);
-    }
-	
+	ret = ioctl(fd, SHMQ_IOC_WAKE_UP);
+	printf("task1: write: ioctl: ret = %d\n", ret);
 
-	
-
-    return NULL;
+	while (1) {
+		sleep(1);
+		val[0] += 1;
+		ret = write(fd, val, sizeof(val));
+		printf("task1: write: ret = %d, val = %s\n", ret, val);
+	}
+	return NULL;
 }
 
 static void *task2(void*arg)
 {
-    int fd;
-	char val[256]={0};
-    int ret;
+	int fd;
+	char val[256] = {0};
+	int ret;
 
-    fd = open(DEV_NAME, O_RDWR);
-	if(fd < 0)
+	fd = open(DEV_NAME, O_RDWR);
+	if (fd < 0)
 		printf("task2: read: can't open %s \n", DEV_NAME);
-    printf("task2: read: fd = %d\n", fd);
+	printf("task2: read: fd = %d\n", fd);
 
-    while(1)
-    {
-        ret = read(fd, val, sizeof(val));
-        printf("task2: read: ret = %d, val = %s\n", ret, val);
-    }
-    
-    
-
-    return NULL;
+	while (1) {
+		ret = read(fd, val, sizeof(val));
+		printf("task2: read: ret = %d, val = %s\n", ret, val);
+	}
+	return NULL;
 }
 
-static void *task3(void*arg)
+static void *task3(void *arg)
 {
-    int fd;
-	char val[256]={0};
-    int ret;
+	int fd;
+	char val[256] = {0};
+	int ret;
 
-    fd = open(DEV_NAME, O_RDWR);
-	if(fd < 0)
+	fd = open(DEV_NAME, O_RDWR);
+	if (fd < 0)
 		printf("task3: read: can't open %s \n", DEV_NAME);
-    printf("task3: read: fd = %d\n", fd);
 
-    while(1)
-    {
-        ret = read(fd, val, sizeof(val));
-        printf("task3: read: ret = %d, val = %s\n", ret, val);
-    }
-    
-    
+	printf("task3: read: fd = %d\n", fd);
 
-    return NULL;
+	while (1) {
+		ret = read(fd, val, sizeof(val));
+		printf("task3: read: ret = %d, val = %s\n", ret, val);
+	}
+
+	return NULL;
 }
 
-int main()
+int main(void)
 {
-    printf("Create 2 thread to test the shmq.\n");
-    
-    pthread_t t1, t2, t3;
-    pthread_create(&t1, NULL, &task1, NULL);
-    pthread_create(&t2, NULL, &task2, NULL);
-    pthread_create(&t3, NULL, &task3, NULL);
-    
-    printf("Sys running...\n");
-    
-	while(1)sleep(1);
+	printf("Create 2 thread to test the shmq.\n");
+
+	pthread_t t1, t2, t3;
+	pthread_create(&t1, NULL, &task1, NULL);
+	pthread_create(&t2, NULL, &task2, NULL);
+	pthread_create(&t3, NULL, &task3, NULL);
+
+	printf("Sys running...\n");
+
+	while (1)
+		sleep(1);
 
 	return 0;
 }
