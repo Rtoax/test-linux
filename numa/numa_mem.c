33845812eb2b2601e4abcae221944a7405aba028
Rong Tao <rongtao@cestc.cn>
2024-11-24 17:52:17 +0800

    numa: numa_mem.c: Display header
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- numa/numa_mem.c
+++ numa/numa_mem.c
@@ -11,9 +11,10 @@ int main(void)
 	int i;
 	struct numa_mem_info *info;
 
+	printf("%-8s %-16s %-16s %-8s\n", "NUMA", "START_ADDR", "END_ADDR", "SIZE(GiB)");
 	for (i = 0; i < ARRAY_SIZE(numa_mem_info); i++) {
 		info = &numa_mem_info[i];
-		printf("%-8d %016lx %016lx %4.4f GiB\n",
+		printf("%-8d %016lx %016lx %4.4f\n",
 			info->nid, info->mem_start, info->mem_end,
 			(info->mem_end - info->mem_start) * 1.0 / GB);
 	}
