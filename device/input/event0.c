5f3d8510c7ab6deeb5444d1e5a894517d54b2dab
Rong Tao <rongtao@cestc.cn>
2024-12-02 16:08:18 +0800

    device/input: Add /dev/input/eventX listen keyboard event
    Link: https://wenku.csdn.net/answer/bf89f410f8de4fddafdc17f3a3e7f44b
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ device/input/event0.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/input.h>
+
+
+int main(int argc, char *argv[])
+{
+	int i, fd, n, idx;
+	char buf[6], event[64];
+	struct input_event ev;
+
+
+	/* My NUC10's keyboard is index 4 */
+	idx = 4;
+
+	for (i = 1; i < argc; i++)
+		if (!strncmp(argv[i], "idx=", 4))
+			idx = atoi(argv[i] + 4);
+
+	fprintf(stderr, "Usage: %s [idx=IDX]\n", argv[0]);
+
+	sprintf(event, "/dev/input/event%d", idx);
+
+	if ((fd = open(event, O_RDONLY)) < 0) {
+		perror("open(/dev/input/eventX)");
+		exit(1);
+	}
+
+	fprintf(stderr, "Open %s\n", event);
+	fprintf(stderr, "Press any keyboard to see what happen!!!\n");
+
+	while (1) {
+		n = read(fd, &ev, sizeof(ev));
+		/* Keyboard event */
+		if (ev.type == EV_KEY) {
+			printf("Key %d (%s)\n", ev.code,
+				ev.value ? "pressed" : "released");
+		}
+	}
+
+	close(fd);
+
+	return 0;
+}
--- device/input/mice.c
+++ device/input/mice.c
@@ -3,21 +3,27 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#if defined(DEV_INPUT_MOUSE0)
+# define DEV	"/dev/input/mouse0"
+#else
+# define DEV	"/dev/input/mice"
+#endif
 
 int main(void)
 {
 	int fd;
 	char buf[6];
 
-	if ((fd = open("/dev/input/mice", O_RDONLY)) < 0) {
+	if ((fd = open("/dev/input/mouse0", O_RDONLY)) < 0) {
 		perror("open(2)");
 		exit(1);
 	}
 
+	fprintf(stderr, "Move your mouse to see what happen!!!\n");
+
 	while (1) {
 		read(fd, buf, 6);
-		printf("%-8d: (%-8d, %-8d)\n",
-			buf[0], buf[1], buf[2]);
+		printf("%-8d: (%-8d, %-8d)\n", buf[0], buf[1], buf[2]);
 	}
 	close(fd);
 
