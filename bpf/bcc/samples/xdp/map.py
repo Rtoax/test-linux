834b3966f1e89afc92044341c231fd6854bd7280
Rong Tao <rongtao@cestc.cn>
2024-12-03 14:29:12 +0800

    bcc/samples/xdp: map.py: Add ipv4_stat hash map
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/bcc/samples/xdp/map.py
+++ bpf/bcc/samples/xdp/map.py
@@ -8,6 +8,8 @@ import time
 import sys
 import ctypes as ct
 import argparse
+from struct import pack
+from socket import inet_ntop, AF_INET, AF_INET6
 
 examples = """examples:
     ./map.py -i eno1                 # Handle eno1 interface
@@ -43,8 +45,16 @@ b = BPF(text = """
 #define ETH_P_IP	0x0800		/* Internet Protocol packet	*/
 #endif
 
+struct ipv4_key_t {
+    u32 saddr;
+};
+struct ipv4_stat_t {
+    u64 npkt;   // statistic
+};
+
 BPF_ARRAY(port, uint32_t, 1);
 BPF_PERCPU_ARRAY(rxcnt, long, 1);
+BPF_HASH(ipv4_stat, struct ipv4_key_t, struct ipv4_stat_t);
 
 static __always_inline int handle_ipv4(struct xdp_md *ctx)
 {
@@ -52,6 +62,20 @@ static __always_inline int handle_ipv4(struct xdp_md *ctx)
     uint32_t key = 0;
     uint32_t *p_idx;
     long *value;
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    struct ethhdr *eth = data;
+    struct iphdr *iphdr = data + sizeof(struct ethhdr);
+    struct ipv4_stat_t *stat;
+    struct ipv4_stat_t newstat = {
+        .npkt = 1,
+    };
+    if (iphdr + 1 > data_end)
+        return XDP_DROP;
+
+    struct ipv4_key_t key2 = {
+        .saddr = iphdr->saddr,
+    };
 
     /* rxq->dev->ifindex */
     ingress_ifindex = ctx->ingress_ifindex;
@@ -64,6 +88,12 @@ static __always_inline int handle_ipv4(struct xdp_md *ctx)
         value = rxcnt.lookup(&key);
         if (value)
             *value += 1;
+        stat = ipv4_stat.lookup(&key2);
+        if (!stat) {
+            ipv4_stat.update(&key2, &newstat);
+        } else {
+            stat->npkt++;
+        }
         return XDP_DROP;
     }
 
@@ -84,7 +114,7 @@ int xdp_drop(struct xdp_md *ctx) {
     /* Only handle ipv4 */
     if (h_proto == bpf_htons(ETH_P_IP))
         return handle_ipv4(ctx);
-	else
+    else
 		return XDP_PASS;
 }
 """, cflags=["-w"])
@@ -97,6 +127,7 @@ fn = b.load_func("xdp_drop", BPF.XDP)
 b.attach_xdp(ifname, fn, flags)
 
 rxcnt = b.get_table("rxcnt");
+ipv4_stat = b.get_table("ipv4_stat");
 prev = 0
 print("Drop packets of %s, hit CTRL+C to stop" % ifname)
 while 1:
@@ -106,6 +137,9 @@ while 1:
             delta = val - prev
             prev = val
             print("{} pkt/s".format(delta))
+        for k, v in sorted(ipv4_stat.items(), key=lambda ipv4_stat: ipv4_stat[0]):
+            saddr = inet_ntop(AF_INET, pack("I", k.saddr))
+            print("%-16s %-16ld" % (saddr, v.npkt))
         time.sleep(1)
     except KeyboardInterrupt:
         print("Removing filter from device")
