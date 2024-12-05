2ab69473366ced12553094d52b58b092b4cd3d79
Rong Tao <rongtao@cestc.cn>
2024-12-05 11:24:11 +0800

    bpf/socket/packet-filtering-raw-sockets: Remove useless sock_example.h
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/socket/packet-filtering-raw-sockets/loader.c
+++ bpf/socket/packet-filtering-raw-sockets/loader.c
@@ -4,6 +4,9 @@
 #include <errno.h>
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/unistd.h>
+#include <net/if.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,9 +15,32 @@
 #include <unistd.h>
 
 #include "bpf_load.h"
-#include "sock_example.h"
 
 extern char bpf_log_buf[BPF_LOG_BUF_SIZE];
+
+int open_raw_sock(const char *name)
+{
+	struct sockaddr_ll sll;
+	int sock;
+
+	sock = socket(PF_PACKET, SOCK_RAW | SOCK_NONBLOCK | SOCK_CLOEXEC, htons(ETH_P_ALL));
+	if (sock < 0) {
+		printf("cannot create raw socket\n");
+		return -1;
+	}
+
+	memset(&sll, 0, sizeof(sll));
+	sll.sll_family = AF_PACKET;
+	sll.sll_ifindex = if_nametoindex(name);
+	sll.sll_protocol = htons(ETH_P_ALL);
+	if (bind(sock, (struct sockaddr *)&sll, sizeof(sll)) < 0) {
+		printf("bind to %s: %s\n", name, strerror(errno));
+		close(sock);
+		return -1;
+	}
+
+	return sock;
+}
 
 int main(int argc, char **argv)
 {
