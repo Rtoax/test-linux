7d8495a588be4e9a93517d7dd4acc6c7406d9725
Rong Tao <rongtao@cestc.cn>
2024-12-05 18:30:32 +0800

    bpf: c: Add skel/
    Link: https://github.com/libbpf/libbpf-bootstrap
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ bpf/c/skel/tc.bpf.c
@@ -0,0 +1,13 @@
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+#define TC_ACT_OK 0
+
+SEC("tc")
+int tc_1(struct __sk_buff *ctx)
+{
+	bpf_printk("Got sk");
+	return TC_ACT_OK;
+}
+char __license[] SEC("license") = "GPL";
---
+++ bpf/c/skel/tc.c
@@ -0,0 +1,5 @@
+int main(int argc, char **argv)
+{
+	/* See libbpf-bootstrap/examples/c/tc.c */
+	return 0;
+}
