968ef88c236d5c4e1c3d39b950ab0db89122498d
Rong Tao <rongtao@cestc.cn>
2024-12-06 15:17:55 +0800

    mbind(2): First test
        $ numactl -H
        available: 8 nodes (0-7)
        ...
        node   0   1   2   3   4   5   6   7
          0:  10  16  16  16  28  28  22  28
          1:  16  10  16  16  28  28  28  22
          2:  16  16  10  16  22  28  28  28
          3:  16  16  16  10  28  22  28  28
          4:  28  28  22  28  10  16  16  16
          5:  28  28  28  22  16  10  16  16
          6:  22  28  28  28  16  16  10  16
          7:  28  22  28  28  16  16  16  10
    
        [rongtao@hg2-7265-68 numa]$ ./mbind
        Number of node 8
        Pages now on node 4
        Moving pages via mbind to node 0 ...
        Pages now on node 0
        Moving pages via mbind to node 1 ...
        Pages now on node 1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/mm/numa/mbind.c
+++ syscall/samples/mm/numa/mbind.c
@@ -1,8 +1,84 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <numa.h>
 #include <numaif.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+
+int get_addr_node(void *vaddr)
+{
+	int ret, flags, mode;
+	flags = MPOL_F_NODE | MPOL_F_ADDR;
+	ret = get_mempolicy(&mode, NULL, 0, (void *)vaddr, flags);
+	if (ret != 0)
+		perror("get_mempolicy");
+	return mode;
+}
 
 int main(void)
 {
-	/* TODO */
+	int i, ret, mode, flags;
+	void *mem;
+	size_t msize;
+	int maxnode;
+	struct bitmask *nodemask;
+
+	msize = getpagesize() * 10;
+
+	/**
+	 * mem must be page alignment.
+	 */
+#if defined(ALLOC_WITH_MMAP)
+	mem = mmap(NULL, msize, PROT_READ | PROT_WRITE,
+		   MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+#elif defined(ALLOC_WITH_MALLOC)
+	mem = malloc(msize);
+#elif defined(ALLOC_WITH_POSIX_MEMALIGN)
+	posix_memalign((void **)&mem, getpagesize(), msize);
+#else
+# error "Must define one of ALLOC_WITH_MMAP,ALLOC_WITH_MALLOC,ALLOC_WITH_POSIX_MEMALIGN"
+#endif
+
+	/* _need_ pagefault */
+	for (i = 0; i < msize; i += getpagesize())
+		*((char *)mem + i) = 'a';
+
+	maxnode = numa_max_node() + 1;
+	nodemask = numa_bitmask_alloc(maxnode);
+
+	printf("Number of node %d\n", maxnode);
+
+	numa_bitmask_clearall(nodemask);
+	numa_bitmask_setbit(nodemask, 0);
+
+	mode = MPOL_BIND;
+	flags = MPOL_MF_MOVE | MPOL_MF_STRICT;
+
+	printf("Pages now on node %d\n", get_addr_node(mem));
+
+	printf("Moving pages via mbind to node 0 ...\n");
+	ret = mbind(mem, msize, mode, nodemask->maskp, nodemask->size, flags);
+	if (ret != 0)
+		perror("mbind");
+
+	printf("Pages now on node %d\n", get_addr_node(mem));
+
+	if (maxnode > 1) {
+		numa_bitmask_clearall(nodemask);
+		numa_bitmask_setbit(nodemask, 1);
+		printf("Moving pages via mbind to node 1 ...\n");
+		ret = mbind(mem, msize, mode, nodemask->maskp, nodemask->size, flags);
+		if (ret != 0)
+			perror("mbind");
+
+		printf("Pages now on node %d\n", get_addr_node(mem));
+	}
+
+#if defined(ALLOC_WITH_MMAP)
+	munmap(mem, msize);
+#else
+	free(mem);
+#endif
 	return 0;
 }
