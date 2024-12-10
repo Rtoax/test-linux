36d826fba81ddfb8a378197443806136739ff5a1
Rong Tao <rongtao@cestc.cn>
2024-12-10 19:06:04 +0800

    libbpf: Add kprobe demo
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ bpf/libbpf/kprobe.bpf.c
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+SEC("kprobe/do_execveat_common.isra.0")
+int BPF_KPROBE(do_execveat_common, int fd, struct filename *name)
+{
+	pid_t pid;
+	const char *filename;
+	pid = bpf_get_current_pid_tgid() >> 32;
+	filename = BPF_CORE_READ(name, name);
+	bpf_printk("KPROBE ENTRY pid = %d, filename = %s", pid, filename);
+	return 0;
+}
+
+SEC("kretprobe/do_execveat_common.isra.0")
+int BPF_KRETPROBE(do_execveat_common_exit, long ret)
+{
+	pid_t pid;
+	pid = bpf_get_current_pid_tgid() >> 32;
+	bpf_printk("KPROBE EXIT: pid = %d, ret = %ld", pid, ret);
+	return 0;
+}
