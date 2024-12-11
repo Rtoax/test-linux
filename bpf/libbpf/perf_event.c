230943dbfed26b715f30259c348817a465066728
Rong Tao <rongtao@cestc.cn>
2024-12-11 18:54:14 +0800

    libbpf: Add perf_event demo
    see linux/samples/bpf/sampleip_{kern,user}.c
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ bpf/libbpf/perf_event.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <linux/bpf.h>
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include <sys/syscall.h>
+#include <linux/perf_event.h>
+#include "perf_event.h"
+#include "perf_event.skel.h"
+
+#define DEFAULT_FREQ	99
+
+static inline int sys_perf_event_open(struct perf_event_attr *attr,
+				      pid_t pid, int cpu, int group_fd,
+				      unsigned long flags)
+{
+	return syscall(__NR_perf_event_open, attr, pid, cpu, group_fd, flags);
+}
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
+			   va_list args)
+{
+	if (level >= LIBBPF_DEBUG)
+		return 0;
+	return vfprintf(stderr, format, args);
+}
+
+static void print_ip_map(int fd)
+{
+	unsigned long key, next_key;
+	unsigned int value;
+	int max;
+
+	printf("%-19s %-16s\n", "ADDR", "COUNT");
+
+	key = 0;
+	max = 0;
+	while (bpf_map_get_next_key(fd, &key, &next_key) == 0) {
+		bpf_map_lookup_elem(fd, &next_key, &value);
+		printf("%-19lx %-16d\n", next_key, value);
+		key = next_key;
+	}
+
+	if (max == MAX_IPS) {
+		printf("WARNING: IP hash was full (max %d entries); ", max);
+		printf("may have dropped samples\n");
+	}
+}
+
+int main(void)
+{
+	int err, event_map_fd, pmu_fd;
+	struct perf_event_bpf *skel;
+
+	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
+	libbpf_set_print(libbpf_print_fn);
+
+#if defined(LIBBPF_OPTS)
+	LIBBPF_OPTS(bpf_object_open_opts, opts,
+		.kernel_log_level = LIBBPF_DEBUG,
+	);
+
+	skel = perf_event_bpf__open_opts(&opts);
+	err = perf_event_bpf__load(skel);
+	if (err) {
+		printf("Failed to load BPF object\n");
+		perf_event_bpf__destroy(skel);
+		return 1;
+	}
+#else
+	skel = perf_event_bpf__open_and_load();
+#endif
+	if (!skel) {
+		printf("Failed to open BPF object\n");
+		return 1;
+	}
+
+	err = perf_event_bpf__attach(skel);
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF skeleton: %d\n", err);
+		perf_event_bpf__destroy(skel);
+		return 1;
+	}
+
+	struct perf_event_attr pe_sample_attr = {
+		.type = PERF_TYPE_SOFTWARE,
+		.freq = 1,
+		.sample_period = DEFAULT_FREQ,
+		.config = PERF_COUNT_SW_CPU_CLOCK,
+		.inherit = 1,
+	};
+
+	pmu_fd = sys_perf_event_open(&pe_sample_attr, -1 /* pid */, 0 /* cpu */,
+				    -1 /* group_fd */, 0 /* flags */);
+	if (pmu_fd < 0) {
+		fprintf(stderr, "ERROR: Initializing perf sampling\n");
+		err = -1;
+		goto cleanup;
+	}
+	skel->links.do_sample = bpf_program__attach_perf_event(skel->progs.do_sample, pmu_fd);
+	if (libbpf_get_error(skel->links.do_sample)) {
+		fprintf(stderr, "ERROR: Attach perf event\n");
+		err = -1;
+		goto cleanup;
+	}
+
+	printf("Start sampling for 10 seconds ...\n");
+	sleep(10);
+
+cleanup:
+	if (!err) {
+		event_map_fd = bpf_map__fd(skel->maps.ip_map);
+		print_ip_map(event_map_fd);
+	}
+	close(pmu_fd);
+	perf_event_bpf__destroy(skel);
+	return -err;
+}
---
+++ bpf/libbpf/perf_event.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define MAX_IPS		8192
+
+struct ipcount {
+	unsigned long ip;
+	unsigned int count;
+};
