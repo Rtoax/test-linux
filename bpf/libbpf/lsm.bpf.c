165605f33e6428eaa87a2391b120291035782a97
Rong Tao <rongtao@cestc.cn>
2024-12-13 19:25:11 +0800

    libbpf: Add lsm.bpf.c demo from libbpf-bootstrap/examples/c/lsm
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ bpf/libbpf/lsm.bpf.c
@@ -0,0 +1,21 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+
+#define EPERM  1
+
+SEC("lsm/bpf")
+int BPF_PROG(lsm_bpf, int cmd, union bpf_attr *attr, unsigned int size, int ret)
+{
+	/* ret is the return value from the previous BPF program
+	 * or 0 if it's the first hook.
+	 */
+	if (ret != 0)
+		return ret;
+
+	bpf_printk("LSM: block bpf() worked");
+	return -EPERM;
+}
+
+char LICENSE[] SEC("license") = "GPL";
