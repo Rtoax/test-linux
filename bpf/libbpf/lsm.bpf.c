b62cdb603d97515b0d70927bd00a3bc52de0554b
Rong Tao <rongtao@cestc.cn>
2024-12-13 19:30:04 +0800

    libbpf: Fix lsm for read trace_pipe
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/libbpf/lsm.bpf.c
+++ bpf/libbpf/lsm.bpf.c
@@ -8,8 +8,9 @@
 SEC("lsm/bpf")
 int BPF_PROG(lsm_bpf, int cmd, union bpf_attr *attr, unsigned int size, int ret)
 {
-	/* ret is the return value from the previous BPF program
-	 * or 0 if it's the first hook.
+	/**
+	 * ret is the return value from the previous BPF program or 0 if
+	 * it's the first hook.
 	 */
 	if (ret != 0)
 		return ret;
--- bpf/libbpf/lsm.c
+++ bpf/libbpf/lsm.c
@@ -1,16 +1,31 @@
 // SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+/* Copyright (c) 2024 Rong Tao */
 /* Copyright (c) 2024 David Di */
 #include <stdio.h>
+#include <setjmp.h>
+#include <signal.h>
 #include <unistd.h>
 #include <sys/resource.h>
 #include <bpf/libbpf.h>
 #include "lsm.skel.h"
+#include "trace_helpers.h"
+
+static volatile sig_atomic_t stop = 0;
+static sigjmp_buf jmp;
+
+void sig_handler(int sig)
+{
+	fprintf(stderr, "get sig...\n");
+	stop = 1;
+	siglongjmp(jmp, 1);
+}
 
 /**
  * Notice: Ensure your kernel version is 5.7 or higher, BTF (BPF Type Format)
  * is enabled, and the file '/sys/kernel/security/lsm' includes 'bpf'.
  */
-static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
+			   va_list args)
 {
 	return vfprintf(stderr, format, args);
 }
@@ -19,6 +34,11 @@ int main(int argc, char **argv)
 {
 	struct lsm_bpf *skel;
 	int err;
+
+	signal(SIGINT, sig_handler);
+	sigsetjmp(jmp, 1);
+	if (stop)
+		goto cleanup;
 
 	/* Set up libbpf errors and debug info callback */
 	libbpf_set_print(libbpf_print_fn);
@@ -37,16 +57,12 @@ int main(int argc, char **argv)
 		goto cleanup;
 	}
 
-	printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
-	       "to see output of the BPF programs.\n");
+	printf("Successfully started!\n");
 
-	for (;;) {
-		/* trigger our BPF program */
-		fprintf(stderr, ".");
-		sleep(1);
-	}
+	read_trace_pipe();
 
 cleanup:
+	printf("Goodbye!!\n");
 	lsm_bpf__destroy(skel);
 	return -err;
 }
