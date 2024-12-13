9dc68d2f7da9f7a6ec00a0246365a21e75ce3f50
Rong Tao <rongtao@cestc.cn>
2024-12-13 15:39:25 +0800

    libbpf: netfilter.bpf.c: only kernel >= v6.3-rc6 support BPF_PROG_TYPE_NETFILTER
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/libbpf/netfilter.bpf.c
+++ bpf/libbpf/netfilter.bpf.c
@@ -6,6 +6,9 @@
  *
  * The hook can make a decision to drop or accept the packet by returning
  * NF_DROP (0) or NF_ACCEPT (1) respectively.
+ *
+ * linux commit fd9c663b9ad6 ("bpf: minimal support for programs hooked into
+ * netfilter framework") v6.3-rc6-1646-gfd9c663b9ad6 support.
  */
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
@@ -16,6 +19,7 @@
 #define ETH_P_IP        0x0800
 #define ETH_P_IPV6      0x86DD
 
+#if LINUX_VERSION_CODE > KERNEL_VERSION(6, 3, 0)
 SEC("netfilter")
 int netfilter1(struct bpf_nf_ctx *ctx)
 {
@@ -29,5 +33,8 @@ int netfilter1(struct bpf_nf_ctx *ctx)
 		return NF_DROP;
 	}
 }
+#else
+#warning "only kernel >= v6.3-rc6 support BPF_PROG_TYPE_NETFILTER"
+#endif
 
 char _license[] SEC("license") = "GPL";
