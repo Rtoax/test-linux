cc52c73f3c3960c4af63c98a1bd4f7aa7b654724
Rong Tao <rongtao@cestc.cn>
2024-12-16 17:14:59 +0800

    bpf: Add tlbuild.mk
    Move BPF_TARGET_ARCH from libbpf/Makefile to tlbuild.mk
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ bpf/tlbuild.mk
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: GPL-3.0
+#
+BPF_TARGET_ARCH ?= $(shell uname -m | sed 's/x86_64/x86/' \
+			 | sed 's/arm.*/arm/' \
+			 | sed 's/aarch64/arm64/' \
+			 | sed 's/ppc64le/powerpc/' \
+			 | sed 's/mips.*/mips/' \
+			 | sed 's/riscv64/riscv/' \
+			 | sed 's/loongarch64/loongarch/')
