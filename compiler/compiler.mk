d504076ab0ecb6c028b73044ce1c83fe14e6d70d
Rong Tao <rongtao@cestc.cn>
2024-12-16 17:34:15 +0800

    Move scripts/compiler.mk contains into compiler/compiler.mk
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/compiler.mk
+++ compiler/compiler.mk
@@ -1,8 +1,36 @@
 # SPDX-License-Identifier: GPL-3.0
-CC := gcc
+CC ?= gcc
+
+# Check compiler support option or not
+# $(1) - compiler, for example: gcc, clang, etc.
+# $(2) - compile option, for example: -flto
+define check_compiler_option
+  $(shell echo 'int main(void) { return 0; }' | \
+    $(1) -x c -Wall - $(2) -S -o /dev/null >/dev/null 2>&1 \
+      && echo 1)
+endef
+
+define check_compiler_option_noS
+  $(shell echo 'int main(void) { return 0; }' | \
+    $(1) -x c -Wall - $(2) -o /dev/null >/dev/null 2>&1 \
+      && echo 1)
+endef
+
+define check_clang_option
+	$(call check_compiler_option,clang,$(1))
+endef
+
+define check_gcc_option
+	$(call check_compiler_option,gcc,$(1))
+endef
+
+CC_M32 := $(findstring 1,$(call check_compiler_option_noS,$(CC),-m32))
+CC_STD_GNU11 := $(findstring 1,$(call check_compiler_option,$(CC),-std=gnu11))
+CC_STD_GNU17 := $(findstring 1,$(call check_compiler_option,$(CC),-std=gnu17))
 
 CC_FULLVERSION := $(shell $(CC) -dumpfullversion -dumpversion)
 CC_VERSION := $(shell $(CC) -dumpversion)
 CC_MAJOR := $(shell echo ${CC_FULLVERSION} | awk -F '.' '{print $$1}')
 CC_MINOR := $(shell echo ${CC_FULLVERSION} | awk -F '.' '{print $$2}')
 
+$(info CC: $(CC) ${CC_MAJOR}.${CC_MINOR} ${CC_FULLVERSION} ${CC_VERSION})
