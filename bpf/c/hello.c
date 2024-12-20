2cf23c52e4ee31e88460d99d286eee8152860f0a
Rong Tao <rongtao@cestc.cn>
2024-12-20 17:10:29 +0800

    bpf/c: hello.c: Add hint
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/c/hello.c
+++ bpf/c/hello.c
@@ -69,6 +69,7 @@ int main(void)
 		return 2;
 	}
 
+	fprintf(stdout, "Kprobe do_nanosleep(), test with 'sleep 0.1'.\n");
 	system("cat " DEBUGFS "/trace_pipe");
 	close(probe_fd);
 	bpf_detach_kprobe("hello_world");
