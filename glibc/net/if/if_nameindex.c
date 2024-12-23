e251e8b9eefc3c67feba7c250491beaba14ffa60
Rong Tao <rongtao@cestc.cn>
2024-12-23 19:07:44 +0800

    glibc: if_nameindex(3): Display all
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/net/if/if_nameindex.c
+++ glibc/net/if/if_nameindex.c
@@ -7,18 +7,27 @@ int main(void)
 {
 	struct if_nameindex *head, *ifni;
 
-	ifni = if_nameindex();
+	head = if_nameindex();
 
-	head = ifni;
+	ifni = head;
 	if (head == NULL) {
 		perror("if_nameindex");
 		exit(1);
 	}
 
+	system("ip address | grep ^[0-9]");
+
+	printf("----------\n");
+
 	while (ifni->if_index != 0) {
 		printf("interface %d: %s\n", ifni->if_index, ifni->if_name);
 		ifni++;
 	}
+
+	printf("----------\n");
+
+	for (ifni = head; !(ifni->if_index == 0 && ifni->if_name == NULL); ifni++)
+		printf("interface %u: %s\n", ifni->if_index, ifni->if_name);
 
 	if_freenameindex(head);
 
