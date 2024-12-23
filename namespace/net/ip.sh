f431906832b85b14468280be6c7510a841cfcb24
Rong Tao <rongtao@cestc.cn>
2024-12-23 17:00:53 +0800

    namespace/net: ip.sh: Rename namespace
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- namespace/net/ip.sh
+++ namespace/net/ip.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 set -ex
 
-sudo ip netns add namespace1
-sudo ip netns add namespace2
+ns1_veth1_ipv4=192.168.1.1
+ns2_veth2_ipv4=192.168.1.2
+
+sudo ip netns add ns1
+sudo ip netns add ns2
 sudo ip netns list
 
 # Then create a pair of interconnected virtual ethernet devices:
 sudo ip link add veth1 type veth peer name veth2
 
 # Link each device to a namespace respectively:
-sudo ip link set veth1 netns namespace1
-sudo ip link set veth2 netns namespace2
+sudo ip link set veth1 netns ns1
+sudo ip link set veth2 netns ns2
 
 # Bring up the devices while assigning them IP addresses:
-sudo ip netns exec namespace1 ip link set dev veth1 up
-sudo ip netns exec namespace2 ip link set dev veth2 up
-sudo ip netns exec namespace1 ip addr add 192.168.1.1/24 dev veth1
-sudo ip netns exec namespace2 ip addr add 192.168.1.2/24 dev veth2
+sudo ip netns exec ns1 ip link set dev veth1 up
+sudo ip netns exec ns2 ip link set dev veth2 up
+sudo ip netns exec ns1 ip addr add ${ns1_veth1_ipv4}/24 dev veth1
+sudo ip netns exec ns2 ip addr add ${ns2_veth2_ipv4}/24 dev veth2
 
-sudo ip netns exec namespace1 ip addr
-sudo ip netns exec namespace2 ip addr
+sudo ip netns exec ns1 ip addr
+sudo ip netns exec ns2 ip addr
 # Or multi-cmd
 multi_cmd() {
-	sudo ip netns exec namespace1 bash
+	sudo ip netns exec ns1 bash
 	sudo ip link set dev lo up
 	ip address
 	exit
@@ -32,10 +35,10 @@ multi_cmd() {
 # by the veth pair tunnel.From the first namespace we should be able to ping
 # the second one, and from the second namespace weshould be able to ping the
 # first one:
-sudo ip netns exec namespace1 ping -c5 192.168.1.2
-sudo ip netns exec namespace2 ping -c5 192.168.1.1
+sudo ip netns exec ns1 ping -c5 ${ns2_veth2_ipv4}
+sudo ip netns exec ns2 ping -c5 ${ns1_veth1_ipv4}
 
-sudo ip netns delete namespace1
-sudo ip netns delete namespace2
+sudo ip netns delete ns1
+sudo ip netns delete ns2
 
 sudo ip netns list
