a00b22ec2bfa812efeb81ac379c58993fd8f73d9
Rong Tao <rongtao@cestc.cn>
2024-12-26 20:06:40 +0800

    network/interface.sh: Query tx/rx queues
    Link: https://blog.csdn.net/weixin_53240476/article/details/132474493
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- network/interface.sh
+++ network/interface.sh
@@ -2,7 +2,8 @@
 set -e
 
 interfaces=( $(ls /sys/class/net/) )
-declare -a operstates carriers
+declare -a operstates carriers rx_queues tx_queues
+verbose=${V}
 
 getifspeed()
 {
@@ -15,24 +16,33 @@ for i in ${interfaces[@]}
 do
 	operstate=$(cat /sys/class/net/${i}/operstate 2>/dev/null || true)
 	carrier=$(cat /sys/class/net/${i}/carrier 2>/dev/null || true)
+	nr_rx_queues=$(echo "/sys/class/net/${i}/queues/rx-[0-9]*" 2>/dev/null | wc -l || true)
+	nr_tx_queues=$(echo "/sys/class/net/${i}/queues/tx-[0-9]*" 2>/dev/null | wc -l || true)
 
 	[[ -z $operstate ]] && operstate=Unknown
 	[[ -z $carrier ]] && carrier=Unknown
 
 	operstates+=( ${operstate} )
 	carriers+=( ${carrier} )
+	rx_queues+=( ${nr_rx_queues} )
+	tx_queues+=( ${nr_tx_queues} )
 done
 
 # Print interface informations
-printf "%-16s %-8s %-8s %-8s\n" INTERFACE STATE CARRIER SPEED
+printf "%-16s %-8s %-8s %-16s %-8s %-8s\n" INTERFACE STATE CARRIER SPEED RX_QUEUE TX_QUEUE
 for ((i = 0; i < ${#interfaces[@]}; i++))
 do
-	printf "%-16s %-8s %-8s %-8s\n" \
+	speed=$(getifspeed ${interfaces[$i]})
+	[[ -z ${speed} ]] && speed=Unknown
+
+	printf "%-16s %-8s %-8s %-16s %-8d %-8d\n" \
 		${interfaces[$i]} \
 		${operstates[$i]} \
 		${carriers[$i]} \
-		$(getifspeed ${interfaces[$i]})
-	sudo udevadm info /sys/class/net/${interfaces[$i]} | sed 's/^/\t/g'
+		${speed} \
+		${rx_queues[$i]} \
+		${tx_queues[$i]}
+	[[ ${verbose} ]] && sudo udevadm info /sys/class/net/${interfaces[$i]} | sed 's/^/\t/g'
 done
 
 echo
