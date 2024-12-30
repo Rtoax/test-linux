68188d114ebc6f161ffd792ba65c587589939e13
Rong Tao <rongtao@cestc.cn>
2024-12-30 11:10:13 +0800

    libbpf: Test bpf_override_return()
    Link: https://docs.ebpf.io/linux/helper-function/bpf_override_return/
    Link: https://mp.weixin.qq.com/s/Fi9joLhQ-jX3PvxkaJa-7g
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/libbpf/kprobe.bpf.c
+++ bpf/libbpf/kprobe.bpf.c
@@ -12,7 +12,11 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
+#include "bpf_misc.h"
 
+#ifndef EINVAL
+#define EINVAL 22
+#endif
 
 SEC("kprobe/"KSYM_DO_EXECVEAT_COMMON)
 int BPF_KPROBE(do_execveat_common, int fd, struct filename *name)
@@ -21,6 +25,33 @@ int BPF_KPROBE(do_execveat_common, int fd, struct filename *name)
 	const char *filename;
 	pid = bpf_get_current_pid_tgid() >> 32;
 	filename = BPF_CORE_READ(name, name);
+
+/**
+ * static long (* const bpf_override_return)(struct pt_regs *regs, __u64 rc) = (void *) 58;
+ *
+ * Used for error injection, this helper uses kprobes to override the return
+ * value of the probed function, and to set it to rc. The first argument is
+ * the context regs on which the kprobe works.
+ *
+ * This helper works by setting the PC (program counter) to an override function
+ * which is run in place of the original probed function. This means the probed
+ * function is not run at all. The replacement function just returns with the
+ * required value.
+ *
+ * This helper has security implications, and thus is subject to restrictions.
+ * It is only available if the kernel was compiled with the
+ * CONFIG_BPF_KPROBE_OVERRIDE configuration option, and in this case it only
+ * works on functions tagged with ALLOW_ERROR_INJECTION in the kernel code.
+ */
+#if defined(BPF_OVERRIDE_RETURN)
+	if (str_eq(filename, "ls", 2)) {
+		u64 err = EINVAL;
+		bpf_override_return(ctx, err);
+		bpf_printk("KPROBE ENTRY pid = %d, filename = %s override return",
+			   pid, filename);
+	}
+#endif
+
 	bpf_printk("KPROBE ENTRY pid = %d, filename = %s", pid, filename);
 	return 0;
 }
