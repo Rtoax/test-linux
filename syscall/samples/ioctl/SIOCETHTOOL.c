66cf3137c90c3a4e6f25e9fa6ae420c07c16f3ee
Rong Tao <rongtao@cestc.cn>
2025-01-02 11:29:13 +0800

    ioctl: SIOCETHTOOL: Get channels
    see linux xsk_get_max_queues()
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/ioctl/SIOCETHTOOL.c
+++ syscall/samples/ioctl/SIOCETHTOOL.c
@@ -23,6 +23,13 @@ int main(int argc, char *argv[])
 		unsigned int buf[1];
 	} sset_info;
 
+	if (argc < 2) {
+		fprintf(stderr, "%s [ifname]\n", argv[0]);
+		exit(1);
+	}
+
+	const char *ifname = argv[1];
+
 	fd = socket(AF_INET, SOCK_DGRAM, 0);
 
 	sset_info.hdr.cmd = ETHTOOL_GSSET_INFO;
@@ -30,14 +37,14 @@ int main(int argc, char *argv[])
 	sset_info.hdr.sset_mask = 1ULL << ETH_SS_STATS;
 
 	memset(&ifr, 0, sizeof(ifr));
-	strcpy(ifr.ifr_name, "eno1");
+	strcpy(ifr.ifr_name, ifname);
 
 	/* ETHTOOL_GSSET_INFO */
 	ifr.ifr_data = (void *)&sset_info;
 
 	ret = ioctl(fd, SIOCETHTOOL, &ifr);
-	if (0 != ret) {
-		printf("errno = %d\n", errno);
+	if (ret) {
+		printf("ioctl %m\n");
 		return -1;
 	}
 
@@ -49,8 +56,8 @@ int main(int argc, char *argv[])
 	ifr.ifr_data = (caddr_t)&edata;
 
 	ret = ioctl(fd, SIOCETHTOOL, &ifr);
-	if (0 != ret) {
-		printf("errno = %d\n", errno);
+	if (ret) {
+		printf("ioctl %m\n");
 		return -1;
 	}
 
@@ -58,12 +65,25 @@ int main(int argc, char *argv[])
 		printf("Link detected on %s\n", ifr.ifr_name);
 	}
 
+	struct ethtool_channels channels = { .cmd = ETHTOOL_GCHANNELS };
+	memset(&ifr, 0x00, sizeof(ifr));
+	ifr.ifr_data = (void *)&channels;
+	memcpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
+	ifr.ifr_name[IFNAMSIZ - 1] = '\0';
+	ret = ioctl(fd, SIOCETHTOOL, &ifr);
+	if (ret) {
+		printf("ioctl %m\n");
+		return -1;
+	}
+	printf("Channel: max rx %d, max tx %d\n", channels.max_rx, channels.max_tx);
+	printf("Channel: rx count %d, tx count %d\n", channels.rx_count, channels.tx_count);
+
 	/* ETHTOOL_GSET */
 	cmd.cmd = ETHTOOL_GSET;
 	ifr.ifr_data = (caddr_t)&cmd;
 	ret = ioctl(fd, SIOCETHTOOL, &ifr);
-	if (0 != ret) {
-		printf("errno = %d\n", errno);
+	if (ret) {
+		printf("ioctl %m\n");
 		return -1;
 	}
 	printf("supported           %d \n", cmd.supported);
