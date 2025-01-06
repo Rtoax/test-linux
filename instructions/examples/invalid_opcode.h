0e474deb1344522f7a240ad3dceda8861b816d91
Rong Tao <rongtao@cestc.cn>
2025-01-06 09:20:08 +0800

    instructions: invalid_opcode.h: Support arm64v8
    Link: https://stackoverflow.com/questions/67309253/does-arm64-have-intentionally-undefined-opcodes
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/invalid_opcode.h
+++ instructions/examples/invalid_opcode.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#if defined(__x86_64__)
 /* ud2 - undefined */
 /* TODO: How to emulate 'invalid opcode' error */
 #define INVALID_OP_BUG()       \
@@ -8,6 +9,26 @@
 		"\t.word %c0\n"        \
 		"\t.long %c1\n"        \
 		: : "i" (__LINE__), "i" (__FILE__))
+#elif defined(__aarch64__)
+/**
+ * udf - It appears in the ARMv8 Architecture Reference Manual on page C6-1380.
+ * It takes an immediate operand which can be any 16-bit value and is ignored,
+ * e.g. udf #0xdead.
+ *
+ * The GNU assembler added support for the udf mnemonic fairly recently (I
+ * believe in version 2.35, released July 2020), so if you have an older
+ * version, it may not be supported. However, the encoding of udf #0 is
+ * conveniently 0x00000000, so you can just write .inst 0 instead, or define
+ * a macro if you plan to use it a lot.
+ *
+ * In fact, any instruction whose high 16 bits are 0 is a guaranteed udf, and
+ * the low 16 bits are the immediate value. You could use different ones to
+ * mark different parts of your code, for easy spotting in a memory dump. So
+ * you can also do .inst 0x0000dead if you want.
+ */
+#define INVALID_OP_BUG()	\
+	__asm__ __volatile__("udf #0");
+#endif
 
 static inline __attribute__((unused)) int invalid_op_init(void)
 {
