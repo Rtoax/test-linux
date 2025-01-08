bbc22770c512f4a55f2ec9eaf7c37441934cf9fa
Rong Tao <rongtao@cestc.cn>
2025-01-08 15:35:30 +0800

    libbpf: libbpf_version.c: Read libbpf version from libbpf.so.A.B.C
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/libbpf/libbpf_version.c
+++ bpf/libbpf/libbpf_version.c
@@ -3,6 +3,20 @@
 
 int main(int argc, char *argv[])
 {
+/**
+ * libbpf commit 8ade99a6f84d ("libbpf: Make libbpf_version.h non-auto-generated")
+ * v0.6.0 add macro LIBBPF_MAJOR_VERSION and LIBBPF_MINOR_VERSION in libbpf_version.h
+ */
+#ifndef LIBBPF_MAJOR_VERSION
+#define LIBBPF_MAJOR_VERSION LIB_LIBBPF_MAJOR_VERSION
+#endif
+#ifndef LIBBPF_MINOR_VERSION
+#define LIBBPF_MINOR_VERSION LIB_LIBBPF_MINOR_VERSION
+#endif
+
+#if LIBBPF_MAJOR_VERSION != LIB_LIBBPF_MAJOR_VERSION || LIBBPF_MINOR_VERSION != LIB_LIBBPF_MINOR_VERSION
+#error "libbpf version fatal, 'ldconfig -p | grep libbpf' != /usr/include/bpf/libbpf_version.h"
+#endif
 	int i;
 	for (i = 1; i < argc; i++) {
 		if (!strcmp(argv[i], "cflags")) {
