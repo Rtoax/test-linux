fe318a48e866bbd6c038980cf46425ed382bf0d9
Rong Tao <rtoax@foxmail.com>
2025-01-08 20:45:39 +0800

    glibc: strstr: needle not exist return NULL
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- glibc/string/strstr.c
+++ glibc/string/strstr.c
@@ -1,11 +1,19 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(void)
 {
+	char *s;
 	char name[] = {"abcdefg"};
 
 	printf("%s\n", strstr(name, "cd"));
+	printf("%s\n", strstr(name, "cd"));
+
+	s = strstr(name, "??");
+	if (s)
+		abort();
 
 	return 0;
 }
