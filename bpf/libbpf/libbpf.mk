e5c94621e1eb290d1edc6b4b8e3dd1633aff78c5
Rong Tao <rongtao@cestc.cn>
2025-01-08 15:38:12 +0800

    libbpf: Add libbpf.mk
    Move some vars from Makefile to libbpf.mk
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ bpf/libbpf/libbpf.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-3.0
+
+LIBBPF_PATHS := $(shell ldconfig -p | grep libbpf.so | awk '{print $$NF}' || true)
+LIB_PATH := $(shell dirname $(LIBBPF_PATHS) | uniq || true)
+# Like /usr/lib64/libbpf.so.0.4.0
+LIBBPF_V_PATH := $(shell ls ${LIB_PATH}/libbpf.so.[0-9].[0-9].[0-9])
+
+LIBBPF_MAJOR_VERSION := $(shell echo ${LIBBPF_V_PATH} | awk -F '.' '{print $$3}')
+LIBBPF_MINOR_VERSION := $(shell echo ${LIBBPF_V_PATH} | awk -F '.' '{print $$4}')
+
+export LIBBPF_MAJOR_VERSION LIBBPF_MINOR_VERSION
+
+$(info LIBBPF_PATHS = ${LIBBPF_PATHS})
+$(info LIB_PATH = ${LIB_PATH})
+$(info LIBBPF_V_PATH = ${LIBBPF_V_PATH})
+$(info LIBBPF_MAJOR_VERSION = ${LIBBPF_MAJOR_VERSION})
+$(info LIBBPF_MINOR_VERSION = ${LIBBPF_MINOR_VERSION})
+
