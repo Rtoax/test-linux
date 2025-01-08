2d619948e957e561ca2cd233afc4c156d64b8553
Rong Tao <rongtao@cestc.cn>
2025-01-08 16:02:21 +0800

    shell: Add shell-run.mk to run bash scripts
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ shell/shell-run.mk
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: GPL-3.0
+SHELL = bash
+ifeq ($(V),1)
+  Q =
+else
+  Q = @
+  MAKEFLAGS += --no-print-directory
+endif
+export Q
+
+SCRIPTS := $(shell ls *.sh)
+LOGS := $(patsubst %.sh,%.log,$(SCRIPTS))
+
+.PHONY: build
+build: ${LOGS}
+	@echo -e " \033[1;33m Done \033[m"
+
+%.log: %.sh
+	@echo -e "  MK  \033[1;32m$(@)\033[m"
+	$(Q)./$(<) $(ARGS_$(*)) | tee $(@)
+
+.PHONY: test
+test:
+	@echo -e "  TEST  \033[1;32m$(@)\033[m"
+
+.PHONY: clean
+clean:
+	@echo -e "  CLEAN  \033[1;32m$(LOGS)\033[m"
+	${Q}rm -f ${LOGS}
