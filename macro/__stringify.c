3313bef7037e528095852d482ebf8d5a3aa66bf2
Rong Tao <rtoax@foxmail.com>
2025-01-18 16:58:03 +0800

    macro/__stringify.c: Use to include
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- macro/__stringify.c
+++ macro/__stringify.c
@@ -3,6 +3,18 @@
 #define __stringify_1(x...)	#x
 #define __stringify(x...)	__stringify_1(x)
 
+/* #include "stdint.h" */
+#include __stringify(stdint.h)
+#include __stringify(STDBOOL_HDR)
+
+#ifndef _STDINT_H
+# error include stdint.h failed
+#endif
+
+#ifndef _STDBOOL_H
+# error include stdbool.h failed
+#endif
+
 #define NUM	3
 
 int main(void)
