47bcf4f9b51fcf6d5547dfcf58ed4f9f881512a9
Rong Tao <rongtao@cestc.cn>
2025-01-21 19:06:02 +0800

    glibc: glibc.mk: Fix error on fedora41
        tst-linux/glibc$ make
        LIBC_SO_PATH = /lib64/libc.so.6
        LIBC___ERRNO_LOCATION_SYMADDR = 0x00000000000035c000000000000035c0
    
        $ readelf --syms --wide /lib64/libc.so.6 | grep -w __errno_location
          2926: 00000000000035c0    21 FUNC    GLOBAL DEFAULT    3 __errno_location@@GLIBC_2.2.5
          7976: 00000000000035c0    21 FUNC    GLOBAL DEFAULT    3 __errno_location
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/glibc.mk
+++ glibc/glibc.mk
@@ -4,7 +4,7 @@ LIBC_SO_PATH=$(shell ldconfig -p | grep libc.so.6 | grep 64 | awk '{printf $$4}'
 $(info LIBC_SO_PATH = ${LIBC_SO_PATH})
 
 LIBC___ERRNO_LOCATION_SYMADDR=0x$(shell readelf --syms --wide ${LIBC_SO_PATH} \
-				| grep -w __errno_location \
+				| grep -w __errno_location | head -1 \
 				| awk '{printf $$2}')
 $(info LIBC___ERRNO_LOCATION_SYMADDR = ${LIBC___ERRNO_LOCATION_SYMADDR})
 
