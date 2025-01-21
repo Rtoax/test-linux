a2a11c0c192a1968e84787b4f50d83c429f59856
Rong Tao <rongtao@cestc.cn>
2025-01-21 13:39:04 +0800

    glibc/string: memcpy-stress.sh: Use long arg
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/string/memcpy-stress.sh
+++ glibc/string/memcpy-stress.sh
@@ -7,6 +7,7 @@ firstline=YES
 
 for b in $(seq 16 31 4096)
 do
-	./memcpy-stress -b ${b} -s $((1024*1024*1024*4)) ${firstline:+--verbose}
+	./memcpy-stress --block-size ${b} --msize $((1024*1024*1024*4)) \
+		${firstline:+--verbose}
 	firstline=
 done
