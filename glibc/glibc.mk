2988b8227281a53e0ea00aa89ed8bbbb281c79df
Rong Tao <rongtao@cestc.cn>
2025-01-21 19:26:56 +0800

    glibc: glibc.mk: Revert add LIBC_PRINTF_SYMADDR
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/glibc.mk
+++ glibc/glibc.mk
@@ -3,6 +3,12 @@
 LIBC_SO_PATH=$(shell ldconfig -p | grep libc.so.6 | grep 64 | awk '{printf $$4}')
 $(info LIBC_SO_PATH = ${LIBC_SO_PATH})
 
+LIBC_PRINTF_SYMADDR=0x$(shell readelf --syms --wide ${LIBC_SO_PATH} \
+				| grep -w printf | head -1 \
+				| awk '{printf $$2}')
+$(info LIBC_PRINTF_SYMADDR = ${LIBC_PRINTF_SYMADDR})
+
+
 LIBC___ERRNO_LOCATION_SYMADDR=0x$(shell readelf --syms --wide ${LIBC_SO_PATH} \
 				| grep -w __errno_location | head -1 \
 				| awk '{printf $$2}')
