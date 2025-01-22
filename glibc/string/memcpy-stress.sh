630f2a68a7ea5b4b2a93c59dac0e289354ec88de
Rong Tao <rongtao@cestc.cn>
2025-01-22 14:59:35 +0800

    glibc: string: memcpy-stress.sh: Add length
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/string/memcpy-stress.sh
+++ glibc/string/memcpy-stress.sh
@@ -8,7 +8,8 @@ firstline=YES
 
 [[ $1 ]] && exe=$1
 
-for b in $(seq 16 31 4096)
+# To avoid some memory boundary issues, it is not a power of 2, so choose "31".
+for b in $(seq 16 31 8192)
 do
 	./${exe} --block-size ${b} --msize $((1024*1024*1024*4)) \
 		${firstline:+--verbose}
