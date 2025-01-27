9d13f0ac64609077ee8f3c06616f769bbf116610
Rong Tao <rtoax@foxmail.com>
2025-01-27 15:38:44 +0800

    vdso: dump.sh: Fix code format
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- vdso/dump.sh
+++ vdso/dump.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-set -ex
+set -e
 
 pid=$$
 vdso_so=vdso64.so
 vdso_sz=
-addr_range=( $(cat /proc/${pid}/maps | grep -w '\[vdso\]' | grep -o ^'[0-9a-f]*-[0-9a-f]*' | tr '-' ' '))
 
-echo "pid ${pid} [vdso] 0x${addr_range[0]}-0x${addr_range[1]}"
+readonly addr_range=( $(cat /proc/${pid}/maps | grep -w '\[vdso\]' | grep -o ^'[0-9a-f]*-[0-9a-f]*' | tr '-' ' '))
+readonly vdso_sz=$(printf "%ld" $(( 0x${addr_range[1]} - 0x${addr_range[0]} )))
 
-vdso_sz=$(printf "%ld" $(( 0x${addr_range[1]} - 0x${addr_range[0]} )))
-
-echo "vdso size ${vdso_sz}"
+echo "pid ${pid} [vdso] 0x${addr_range[0]}-0x${addr_range[1]}, size = ${vdso_sz}"
 
 dd if=/proc/${pid}/mem of=${vdso_so} ibs=1 iseek=$(printf %ld 0x${addr_range[0]}) count=${vdso_sz}
 
+readelf --syms --wide ${vdso_so}
 md5sum ${vdso_so}
