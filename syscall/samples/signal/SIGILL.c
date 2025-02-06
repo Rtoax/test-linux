e92000d8011d4c8fddde15c1cbc08f341f8e1365
Rong Tao <rongtao@cestc.cn>
2025-02-06 12:33:48 +0800

    syscall: SIGILL.c: Fix Error: unknown mnemonic `udf' -- `udf #0'
     [sub-dir] build setjmp.build
      CC  signal.c to signal.o
    /tmp/ccMLDnjq.s: Assembler messages:
    /tmp/ccMLDnjq.s:86: Error: unknown mnemonic `udf' -- `udf #0'
    make[1]: *** [Makefile:37: signal.o] Error 1
    make: *** [Makefile:154: setjmp.build] Error 2
    
    See also commit 077ff538dc16 ("instructions: invalid_opcode.h: Fix
    Error: unknown mnemonic `udf' -- `udf #0'")
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/signal/SIGILL.c
+++ syscall/samples/signal/SIGILL.c
@@ -39,7 +39,11 @@ int main(int argc, char *argv[])
 #if defined(__x86_64__)
 	__asm__ __volatile__("ud2\n");
 #elif defined(__aarch64__)
-	__asm__ __volatile__("udf #0\n");
+	/**
+	 * See test-linux/instructions/examples/invalid_opcode.h
+	 */
+	//__asm__ __volatile__("udf #0\n");
+	__asm__ __volatile__(".word 0xdeadbeef\n");
 #else
 	raise(SIGILL);
 #endif
