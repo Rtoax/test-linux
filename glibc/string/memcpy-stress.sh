c67a89863ec959219d4aeed5e31c2f616c89926c
Rong Tao <rongtao@cestc.cn>
2025-02-07 14:09:22 +0800

    glibc/string: memcpy-stress.sh: Add MiB var
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/string/memcpy-stress.sh
+++ glibc/string/memcpy-stress.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+readonly MiB=1024*1024
+
 make memcpy-stress
 
 exe=memcpy-stress
@@ -15,7 +17,7 @@ echo "${exe} $(./${exe} --version)"
 # To avoid some memory boundary issues, it is not a power of 2, so choose "31".
 for b in $(seq 16 31 8192)
 do
-	./${exe} --block-size ${b} --msize $((1024*1024*${msize_MB})) \
+	./${exe} --block-size ${b} --msize $((${MiB}*${msize_MB})) \
 		${firstline:+--verbose}
 	firstline=
 done
