2efc58a61edfc849df617fefd5d202588b0088a6
Rong Tao <rongtao@cestc.cn>
2025-02-08 11:05:14 +0800

    vdso: dump.sh: Format code style
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- vdso/dump.sh
+++ vdso/dump.sh
@@ -7,12 +7,20 @@ vdso_sz=
 
 exec >&2
 
-readonly addr_range=( $(cat /proc/${pid}/maps | grep -w '\[vdso\]' | grep -o ^'[0-9a-f]*-[0-9a-f]*' | tr '-' ' '))
+readonly addr_range=( $(cat /proc/${pid}/maps \
+			| grep -w '\[vdso\]' \
+			| grep -o ^'[0-9a-f]*-[0-9a-f]*' \
+			| tr '-' ' ')
+			)
 readonly vdso_sz=$(printf "%ld" $(( 0x${addr_range[1]} - 0x${addr_range[0]} )))
 
 echo "pid ${pid} [vdso] 0x${addr_range[0]}-0x${addr_range[1]}, size = ${vdso_sz}"
 
-dd if=/proc/${pid}/mem of=${vdso_so} ibs=1 skip=$(printf %ld 0x${addr_range[0]}) count=${vdso_sz}
+dd	if=/proc/${pid}/mem \
+	of=${vdso_so} \
+	ibs=1 \
+	skip=$(printf %ld 0x${addr_range[0]}) \
+	count=${vdso_sz}
 
 readelf --syms --wide ${vdso_so}
 md5sum ${vdso_so}
