7bb595132b73fe9b214a0a912ba621d1118e337b
Rong Tao <rtoax@foxmail.com>
2025-02-11 16:51:39 +0800

    bpftool: map name len need less than BPF_OBJ_NAME_LEN - 1 (=15)
    Link: https://github.com/Rtoax/linux/commit/f4f16d527f7551d00ab9cb8bfe55b652f03b341a
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- bpf/bpftool/scripts/map/create.sh
+++ bpf/bpftool/scripts/map/create.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -ex
 
-map_name=$(mktemp -u tstmap_XXX)
+[[ -z ${BPFTOOL} ]] && BPFTOOL=bpftool
+
+# BPF_OBJ_NAME_LEN=16U, map name length need smaller than 15
+# ref https://github.com/Rtoax/linux/commit/f4f16d527f7551d00ab9cb8bfe55b652f03b341a
+map_name=$(mktemp -u tstmap______XXX)
 map_type=array
 
-sudo bpftool map create /sys/fs/bpf/${map_name} \
+sudo ${BPFTOOL} map create /sys/fs/bpf/${map_name} \
 	type ${map_type} \
 	key 4 value 4 entries 5 \
 	name ${map_name}
 
-sudo bpftool map show name ${map_name}
-sudo bpftool map dump name ${map_name}
+sudo ${BPFTOOL} map show name ${map_name}
+sudo ${BPFTOOL} map dump name ${map_name}
 
 # Remove map from system
 sudo unlink /sys/fs/bpf/${map_name}
--- glibc/string/strlen.c
+++ glibc/string/strlen.c
@@ -10,12 +10,12 @@ int main(void)
 
 	char str2[10] = {'e','d','c','b','a'};
 
-	printf("%ld\n", strlen("\0"));
-	printf("%ls, %ld, %ld\n", str, strlen((char*)str), sizeof(str));
-	printf("%ls, %ld, %ld\n", str, wcslen(str), sizeof(str));
-	printf("%ls, %ld, %ld\n", str, wcsnlen(str, 4), sizeof(str));
-	printf("%s, %ld, %ld\n", str2, strlen((char*)str2), sizeof(str2));
-	printf("%s, %ld, %ld\n", str2, strnlen((char*)str2, 3), sizeof(str2));
+	printf("strlen: %ld\n", strlen("\0"));
+	printf("strlen: %ls, %ld, %ld\n", str, strlen((char *)str), sizeof(str));
+	printf("wcslen: %ls, %ld, %ld\n", str, wcslen(str), sizeof(str));
+	printf("wcsnlen: %ls, %ld, %ld\n", str, wcsnlen(str, 4), sizeof(str));
+	printf("strlen: %s, %ld, %ld\n", str2, strlen((char *)str2), sizeof(str2));
+	printf("strnlen: %s, %ld, %ld\n", str2, strnlen((char *)str2, 3), sizeof(str2));
 
 	char *s = NULL;
 
