705734627dc345b0e4d2dcad79d18cf45c68ae69
Rong Tao <rongtao@cestc.cn>
2025-02-26 09:16:37 +0800

    instructions: examples: simd.c: compile cross x86_64 on ft-S2500
        $ make simd BFD=1 BINUTILS_VERSION_MAJOR=2 V=1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- binutils/binutils.mk
+++ binutils/binutils.mk
@@ -4,12 +4,14 @@ SHELL = bash
 LD_BFD := $(shell which ld.bfd 2>/dev/null)
 ifeq ($(LD_BFD),)
   $(error "Not found linker ld.bfd, install binutils first")
+else
+  $(info LD_BFD = ${LD_BFD})
 endif
 
 # Get binutils version
-BINUTILS_VERSION = $(shell ${LD_BFD} --version | grep -o [0-9].[0-9]\. | sed -n '1p')
-BINUTILS_VERSION_MAJOR = $(shell echo ${BINUTILS_VERSION} | awk -F '.' '{print $$1}')
-BINUTILS_VERSION_MINOR = $(shell echo ${BINUTILS_VERSION} | awk -F '.' '{print $$2}')
+BINUTILS_VERSION := $(shell ${LD_BFD} --version | grep -o [0-9].[0-9]\. | sed -n '1p')
+BINUTILS_VERSION_MAJOR := $(shell echo ${BINUTILS_VERSION} | awk -F '.' '{print $$1}')
+BINUTILS_VERSION_MINOR := $(shell echo ${BINUTILS_VERSION} | awk -F '.' '{print $$2}')
 
 $(info Binutils version ${BINUTILS_VERSION_MAJOR}.${BINUTILS_VERSION_MINOR})
 ifneq (${BINUTILS_VERSION_MAJOR},2)
