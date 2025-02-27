e56ac26ea63a3824b298e8090e46bb18e8fb4c6c
Rong Tao <rongtao@cestc.cn>
2025-02-27 17:16:09 +0800

    python/psutil: process_start_time.py: Try-except
    Link: https://www.oryoy.com/news/ru-he-zai-ubuntu-xi-tong-zhong-shi-yong-python-jiao-ben-gao-xiao-cha-kan-jin-cheng-qi-dong-shi-jian.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- python/psutil/process_start_time.py
+++ python/psutil/process_start_time.py
@@ -1,12 +1,18 @@
 #!/bin/env python
-
+# Get process start time
 import psutil
 import time
 
 pid = 1
-process = psutil.Process(pid)
 
-start_time = process.create_time()
+def get_process_start_time(pid):
+    try:
+        process = psutil.Process(pid)
+        return process.create_time()
+    except psutil.NoSuchProcess:
+        return -1
+
+start_time = get_process_start_time(pid)
 start_time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))
 
 print("Process %d started at: %s" % (pid, start_time_str))
