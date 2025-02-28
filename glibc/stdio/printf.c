88fdc96af7a5f71460d12f878c792b623ac8e474
Rong Tao <rongtao@cestc.cn>
2025-02-28 16:40:20 +0800

    printf.c: End with '|'
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/stdio/printf.c
+++ glibc/stdio/printf.c
@@ -23,19 +23,19 @@ int main(void)
 #endif
 
 	for (i = 0; i <= strlen(STR); i++)
-		printf("%3d/%" PRId "  [%.*s]\n", i, strlen(STR), i, STR);
+		printf("%3d/%" PRId "  [%.*s]|\n", i, strlen(STR), i, STR);
 
 	printf("KB/s \n");
 
-	printf("%*.*d\n", 3, 10, 10);
-	printf("%.*s\n", 4, "hello");
-	printf("%*s\n", 40, "hell0");
-	printf("%*.*s\n", 40, 4, "hell0");
+	printf("%*.*d|\n", 3, 10, 10);
+	printf("%.*s|\n", 4, "hello");
+	printf("%*s|\n", 40, "hell0");
+	printf("%*.*s|\n", 40, 4, "hell0");
 
-	printf("%.*d\n", 4, 10000);
-	printf("%*d\n", 40, 10000);
-	printf("%*.*d\n", 40, 4, 10000);
+	printf("%.*d|\n", 4, 10000);
+	printf("%*d|\n", 40, 10000);
+	printf("%*.*d|\n", 40, 4, 10000);
 
-	printf("Hello %.*d %.*d\n", 5, 2, 5, 3);
+	printf("Hello %.*d %.*d|\n", 5, 2, 5, 3);
 	return 0;
 }
