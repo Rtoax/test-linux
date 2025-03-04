99b945e2444694c7afd3970ca5645d025acebc39
Rong Tao <rongtao@cestc.cn>
2025-03-04 11:28:02 +0800

    R_X86_64_64.c: Update comment
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_64.c
+++ compiler/reloc/samples/x86_64/R_X86_64_64.c
@@ -3,10 +3,17 @@
  *
  * value = 1, field = word64, calculation = S + A
  */
+#include <stddef.h>
 
-/* s1 ~ s8 are R_X86_64_64 in ELF 64-bit LSB relocatable */
+/**
+ * in ELF 64-bit LSB relocatable:
+ * s1 ~ s8 are R_X86_64_64
+ */
 
-/* s1 ~ s7 in .rela.data for non-pie, .rela.data.rel.local for pie */
+/**
+ * in ELF 64-bit LSB relocatable:
+ * s1 ~ s7 in ".rela.data" for non-pie, ".rela.data.rel.local" for pie
+ */
 const char *s1 = "Hello";	/* .data, GLOBAL */
 const char *s2 = "Hello";	/* .data, GLOBAL */
 const char *s3 = "Hello2";	/* .data, GLOBAL */
@@ -15,10 +22,16 @@ const char *s5 = "Hello2";	/* .data, GLOBAL */
 char *s6 = "Hello2";		/* .data, GLOBAL */
 static char *s7 = "Hello2";	/* .data, LOCAL */
 
-/* s8 in .rela.redata for non-pie, .rela.data.rel.ro.local for pie */
+/**
+ * in ELF 64-bit LSB relocatable:
+ * s8 in ".rela.redata" for non-pie, ".rela.data.rel.ro.local" for pie
+ */
 char *const s8 = "Hello2";	/* .rodata, GLOBAL */
 
-/* main is R_X86_64_PC32 in ELF 64-bit LSB relocatable */
+/**
+ * in ELF 64-bit LSB relocatable:
+ * main is R_X86_64_PC32
+ */
 int main(void)	/* .rela.eh_frame */
 {
 	return 0;
