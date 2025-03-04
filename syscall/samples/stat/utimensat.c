2ffca01c849a79d87031a989f756ef67f5649973
Rong Tao <rongtao@cestc.cn>
2025-03-04 16:05:44 +0800

    syscall: stat: Add futimens(2) test
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/stat/utimensat.c
+++ syscall/samples/stat/utimensat.c
@@ -20,10 +20,28 @@ int main(int argc, char *argv[])
 	times[1].tv_sec = time(NULL) - 3600 * 24;
 	times[1].tv_nsec = 0;
 
+	/**
+	 * NOTE: if you want to change "ctime", you could use chmod/chown.
+	 */
+
+#ifdef FUTIMENS
+	fprintf(stderr, "Test futimens.\n");
+
+	int fd = open(filename, O_RDONLY);
+	if (futimens(fd, times) == -1) {
+		perror("futimens");
+		close(fd);
+		return 1;
+	}
+	close(fd);
+#else
+	fprintf(stderr, "Test utimensat.\n");
+
 	if (utimensat(AT_FDCWD, filename, times, 0) == -1) {
 		perror("utimensat");
 		return 1;
 	}
+#endif
 
 	printf("Success modify %s's atime and mtime\n", filename);
 	return 0;
