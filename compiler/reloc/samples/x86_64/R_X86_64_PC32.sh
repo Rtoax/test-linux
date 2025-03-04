3e6af880378416f222f1b137b3dc4a35349e0434
Rong Tao <rongtao@cestc.cn>
2025-03-04 18:54:44 +0800

    R_X86_64_PC32.sh: Get section name
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_PC32.sh
+++ compiler/reloc/samples/x86_64/R_X86_64_PC32.sh
@@ -18,14 +18,21 @@ sym2value() {
 	readelf --syms --wide ${elf} | grep -w ${fun} | awk '{print "0x"$2}'
 }
 
-sec2sh_offset() {
+sec2name() {
+	local elf=$1
+	local sec=$2
+	readelf --sections --wide ${elf} | grep "\[[[:space:]]*${sec}\]" \
+		| awk '{print $(NF-9)}'
+}
+
+sec2offset() {
 	local elf=$1
 	local sec=$2
 	readelf --sections --wide ${elf} | grep "\[[[:space:]]*${sec}\]" \
 		| awk '{print "0x"$(NF-6)}'
 }
 
-sec2sh_addr() {
+sec2addr() {
 	local elf=$1
 	local sec=$2
 	readelf --sections --wide ${elf} | grep "\[[[:space:]]*${sec}\]" \
@@ -40,34 +47,38 @@ r_addend=${r_off_add[1]}
 printf "obj: %s : r_offset %s, r_addend %s\n" ${SYM} ${r_offset} ${r_addend}
 
 obj_func_sec=$(sym2sec ${OBJ} ${FUNC})
-obj_func_sh_addr=$(sec2sh_addr ${OBJ} ${obj_func_sec})
-obj_func_sh_offset=$(sec2sh_offset ${OBJ} ${obj_func_sec})
+obj_func_sh_name=$(sec2name ${OBJ} ${obj_func_sec})
+obj_func_sh_addr=$(sec2addr ${OBJ} ${obj_func_sec})
+obj_func_sh_offset=$(sec2offset ${OBJ} ${obj_func_sec})
 obj_func_st_value=$(sym2value ${OBJ} ${FUNC})
 
 exe_func_sec=$(sym2sec ${EXE} ${FUNC})
-exe_func_sh_addr=$(sec2sh_addr ${EXE} ${exe_func_sec})
-exe_func_sh_offset=$(sec2sh_offset ${EXE} ${exe_func_sec})
+exe_func_sh_name=$(sec2name ${EXE} ${exe_func_sec})
+exe_func_sh_addr=$(sec2addr ${EXE} ${exe_func_sec})
+exe_func_sh_offset=$(sec2offset ${EXE} ${exe_func_sec})
 exe_func_st_value=$(sym2value ${EXE} ${FUNC})
 
 obj_sym_st_value=$(sym2value ${OBJ} ${SYM})
 obj_sym_sec=$(sym2sec ${OBJ} ${SYM})
-obj_sym_sh_addr=$(sec2sh_addr ${OBJ} ${obj_sym_sec})
-obj_sym_sh_offset=$(sec2sh_offset ${OBJ} ${obj_sym_sec})
+obj_sym_sh_name=$(sec2name ${OBJ} ${obj_sym_sec})
+obj_sym_sh_addr=$(sec2addr ${OBJ} ${obj_sym_sec})
+obj_sym_sh_offset=$(sec2offset ${OBJ} ${obj_sym_sec})
 
 exe_sym_st_value=$(sym2value ${EXE} ${SYM})
 exe_sym_sec=$(sym2sec ${EXE} ${SYM})
-exe_sym_sh_addr=$(sec2sh_addr ${EXE} ${exe_sym_sec})
-exe_sym_sh_offset=$(sec2sh_offset ${EXE} ${exe_sym_sec})
+exe_sym_sh_name=$(sec2name ${EXE} ${exe_sym_sec})
+exe_sym_sh_addr=$(sec2addr ${EXE} ${exe_sym_sec})
+exe_sym_sh_offset=$(sec2offset ${EXE} ${exe_sym_sec})
 
-printf "obj: %s : section %-2d, sh_addr %s, sh_offset %s, st_value %s\n" \
-	${FUNC} ${obj_func_sec} ${obj_func_sh_addr} ${obj_func_sh_offset} ${obj_func_st_value}
-printf "exe: %s : section %-2d, sh_addr %s, sh_offset %s, st_value %s\n" \
-	${FUNC} ${exe_func_sec} ${exe_func_sh_addr} ${exe_func_sh_offset} ${exe_func_st_value}
+printf "obj: %s : section %-2d, sh_addr %s, sh_offset %s, st_value %s (%s)\n" \
+	${FUNC} ${obj_func_sec} ${obj_func_sh_addr} ${obj_func_sh_offset} ${obj_func_st_value} ${obj_func_sh_name}
+printf "exe: %s : section %-2d, sh_addr %s, sh_offset %s, st_value %s (%s)\n" \
+	${FUNC} ${exe_func_sec} ${exe_func_sh_addr} ${exe_func_sh_offset} ${exe_func_st_value} ${exe_func_sh_name}
 
-printf "obj: %s : section %-2d, sh_addr %s, sh_offset %s, st_value %s\n" \
-	${SYM} ${obj_sym_sec} ${obj_sym_sh_addr} ${obj_sym_sh_offset} ${obj_sym_st_value}
-printf "exe: %s : section %-2d, sh_addr %s, sh_offset %s, st_value %s\n" \
-	${SYM} ${exe_sym_sec} ${exe_sym_sh_addr} ${exe_sym_sh_offset} ${exe_sym_st_value}
+printf "obj: %s : section %-2d, sh_addr %s, sh_offset %s, st_value %s (%s)\n" \
+	${SYM} ${obj_sym_sec} ${obj_sym_sh_addr} ${obj_sym_sh_offset} ${obj_sym_st_value} ${obj_sym_sh_name}
+printf "exe: %s : section %-2d, sh_addr %s, sh_offset %s, st_value %s (%s)\n" \
+	${SYM} ${exe_sym_sec} ${exe_sym_sh_addr} ${exe_sym_sh_offset} ${exe_sym_st_value} ${exe_sym_sh_name}
 
 exe_func_sec_off=$(( ${exe_func_st_value} - (${exe_func_sh_addr} - ${exe_func_sh_offset}) ))
 
@@ -77,7 +88,7 @@ hexdump -C obj.bin
 hexdump -C exe.bin
 
 sym_offset=$( printf 0x%lx $(( ${exe_sym_st_value} - (${exe_sym_sh_addr} - ${exe_sym_sh_offset}) )) )
-printf "%s : sym_offset %s\n" ${SYM} ${sym_offset}
+printf "exe: %s : sym_offset %s\n" ${SYM} ${sym_offset}
 
 printf "%s : S + A - P = %s + %s + %s = %s\n" ${SYM} \
 	${exe_sym_st_value} ${r_addend} ?? \
