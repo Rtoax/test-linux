af2624bcf0454aaf39abe8f9a50f0dcab73c1a78
Rong Tao <rongtao@cestc.cn>
2025-03-04 10:38:46 +0800

    R_X86_64_64.c: Add more comment
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_64.c
+++ compiler/reloc/samples/x86_64/R_X86_64_64.c
@@ -4,19 +4,18 @@
  * value = 1, field = word64, calculation = S + A
  */
 
-/**
- * s1 ~ s8 are R_X86_64_64 in ELF 64-bit LSB relocatable
- */
-const char *s1 = "Hello";	/* .data, GLOBAL */
-const char *s2 = "Hello";	/* .data, GLOBAL */
-const char *s3 = "Hello2";	/* .data, GLOBAL */
-const char *s4 = "Hello2";	/* .data, GLOBAL */
-const char *s5 = "Hello2";	/* .data, GLOBAL */
-char *s6 = "Hello2";		/* .data, GLOBAL */
-static char *s7 = "Hello2";	/* .data, LOCAL */
-char *const s8 = "Hello2";	/* .rodata, GLOBAL */
+/* s1 ~ s8 are R_X86_64_64 in ELF 64-bit LSB relocatable */
+const char *s1 = "Hello";	/* .data, GLOBAL */	/* .rela.data */
+const char *s2 = "Hello";	/* .data, GLOBAL */	/* .rela.data */
+const char *s3 = "Hello2";	/* .data, GLOBAL */	/* .rela.data */
+const char *s4 = "Hello2";	/* .data, GLOBAL */	/* .rela.data */
+const char *s5 = "Hello2";	/* .data, GLOBAL */	/* .rela.data */
+char *s6 = "Hello2";		/* .data, GLOBAL */	/* .rela.data */
+static char *s7 = "Hello2";	/* .data, LOCAL */	/* .rela.data */
+char *const s8 = "Hello2";	/* .rodata, GLOBAL */	/* .rela.rodata */
 
-int main(void)
+/* main is R_X86_64_PC32 in ELF 64-bit LSB relocatable */
+int main(void)	/* .rela.eh_frame */
 {
 	return 0;
 }
