2df01daf80a850deffb28edbaafa43c470a9c84d
Rong Tao <rongtao@cestc.cn>
2025-03-05 14:21:44 +0800

    compiler/foption: -fsanitize=address
    DeepSeek: ask: corrupted size vs. prev_size in fastbins
    
    $ ./fsanitize
    =================================================================
    ==118393==ERROR: AddressSanitizer: heap-buffer-overflow on address 0x60200000001a at pc 0x7f6ca814bd63 bp 0x7ffd429bd6a0 sp 0x7ffd429bce48
    WRITE of size 20 at 0x60200000001a thread T0
        #0 0x7f6ca814bd62 in memset (/lib64/libasan.so.6+0x39d62)
        #1 0x40119f in main (/home/rongtao/Git/tst-linux/compiler/foption/fsanitize+0x40119f)
        #2 0x7f6ca7f48eaf in __libc_start_call_main (/lib64/libc.so.6+0x3feaf)
        #3 0x7f6ca7f48f5f in __libc_start_main_alias_1 (/lib64/libc.so.6+0x3ff5f)
        #4 0x4010a4 in _start (/home/rongtao/Git/tst-linux/compiler/foption/fsanitize+0x4010a4)
    
    0x60200000001a is located 0 bytes to the right of 10-byte region [0x602000000010,0x60200000001a)
    allocated by thread T0 here:
        #0 0x7f6ca81c69e7 in __interceptor_malloc (/lib64/libasan.so.6+0xb49e7)
        #1 0x401177 in main (/home/rongtao/Git/tst-linux/compiler/foption/fsanitize+0x401177)
        #2 0x7f6ca7f48eaf in __libc_start_call_main (/lib64/libc.so.6+0x3feaf)
    
    SUMMARY: AddressSanitizer: heap-buffer-overflow (/lib64/libasan.so.6+0x39d62) in memset
    Shadow bytes around the buggy address:
      0x0c047fff7fb0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
      0x0c047fff7fc0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
      0x0c047fff7fd0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
      0x0c047fff7fe0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
      0x0c047fff7ff0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    =>0x0c047fff8000: fa fa 00[02]fa fa 00 02 fa fa fa fa fa fa fa fa
      0x0c047fff8010: fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa
      0x0c047fff8020: fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa
      0x0c047fff8030: fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa
      0x0c047fff8040: fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa
      0x0c047fff8050: fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa fa
    Shadow byte legend (one shadow byte represents 8 application bytes):
      Addressable:           00
      Partially addressable: 01 02 03 04 05 06 07
      Heap left redzone:       fa
      Freed heap region:       fd
      Stack left redzone:      f1
      Stack mid redzone:       f2
      Stack right redzone:     f3
      Stack after return:      f5
      Stack use after scope:   f8
      Global redzone:          f9
      Global init order:       f6
      Poisoned by user:        f7
      Container overflow:      fc
      Array cookie:            ac
      Intra object redzone:    bb
      ASan internal:           fe
      Left alloca redzone:     ca
      Right alloca redzone:    cb
      Shadow gap:              cc
    ==118393==ABORTING
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ compiler/foption/fsanitize.c
@@ -0,0 +1,18 @@
+#include <malloc.h>
+#include <string.h>
+
+int main(void)
+{
+	char *a = malloc(10);
+	char *b = malloc(10);
+
+	(void)b;
+
+	/* Buffer overflow corrupts 'b's metadata */
+	/* Writes 20 bytes into a 10-byte buffer */
+	memset(a, 0, 20);
+	/* May trigger error when 'b' is freed later */
+	free(a);
+
+	return 0;
+}
--- glibc/malloc/malloc_trim.c
+++ glibc/malloc/malloc_trim.c
@@ -3,6 +3,8 @@
  * by calling brk(2), sbrk(2) or madvise(2) with suitable arguments.
  *
  * $ sudo strace -e trace=madvise,brk ./malloc_trim
+ *
+ * trim: 修剪, 消减
  */
 #include <stdio.h>
 #include <malloc.h>
@@ -18,13 +20,13 @@ int main(void)
 
 	print_mallinfo();
 
-	p2 = malloc(2024);
+	p2 = malloc(2048);
 	printf("p2 %p\n", p2);
 
 	print_mallinfo();
 
 	printf("release free memory from the heap\n");
-	malloc_trim(0);
+	malloc_trim(2048);
 
 	print_mallinfo();
 
