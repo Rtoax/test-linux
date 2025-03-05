eee2c9bce731b7f53d77eed0f2362795abe2b19a
Rong Tao <rongtao@cestc.cn>
2025-03-05 11:40:00 +0800

    malloc: malloc_trim: strace it
        glibc/malloc$ sudo strace -e trace=madvise,brk ./malloc_trim
        brk(NULL)                               = 0xd5ac000
        brk(NULL)                               = 0xd5ac000
        brk(0xd5cd000)                          = 0xd5cd000
        p1 0xd5ac2a0
        ------------------------------------------
        mallinfo.arena		= 135168.
        mallinfo.ordblks	= 1.
        mallinfo.smblks		= 0.
        mallinfo.hblks		= 0.
        mallinfo.hblkhd		= 0.
        mallinfo.usmblks	= 0.
        mallinfo.fsmblks	= 0.
        mallinfo.uordblks	= 2736.
        mallinfo.fordblks	= 132432.
        mallinfo.keepcost	= 132432.
        p2 0xd5acac0
        ------------------------------------------
        mallinfo.arena		= 135168.
        mallinfo.ordblks	= 1.
        mallinfo.smblks		= 0.
        mallinfo.hblks		= 0.
        mallinfo.hblkhd		= 0.
        mallinfo.usmblks	= 0.
        mallinfo.fsmblks	= 0.
        mallinfo.uordblks	= 4768.
        mallinfo.fordblks	= 130400.
        mallinfo.keepcost	= 130400.
        release free memory from the heap
        brk(0xd5ae000)                          = 0xd5ae000
        ------------------------------------------
        mallinfo.arena		= 8192.
        mallinfo.ordblks	= 1.
        mallinfo.smblks		= 0.
        mallinfo.hblks		= 0.
        mallinfo.hblkhd		= 0.
        mallinfo.usmblks	= 0.
        mallinfo.fsmblks	= 0.
        mallinfo.uordblks	= 4768.
        mallinfo.fordblks	= 3424.
        mallinfo.keepcost	= 3424.
        +++ exited with 0 +++
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/malloc/malloc_trim.c
+++ glibc/malloc/malloc_trim.c
@@ -1,3 +1,9 @@
+/**
+ * The  malloc_trim() function attempts to release free memory from the heap.
+ * by calling brk(2), sbrk(2) or madvise(2) with suitable arguments.
+ *
+ * $ sudo strace -e trace=madvise,brk ./malloc_trim
+ */
 #include <stdio.h>
 #include <malloc.h>
 
@@ -6,13 +12,22 @@
 int main(void)
 {
 	char *p1, *p2;
+
 	p1 = malloc(1024);
-	(void)p1;
+	printf("p1 %p\n", p1);
+
 	print_mallinfo();
+
 	p2 = malloc(2024);
-	(void)p2;
+	printf("p2 %p\n", p2);
+
 	print_mallinfo();
+
+	printf("release free memory from the heap\n");
 	malloc_trim(0);
+
 	print_mallinfo();
+
+	return 0;
 }
 
