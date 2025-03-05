ecda1bc62d31c1748ab9b7966fb45e4e56560667
Rong Tao <rongtao@cestc.cn>
2025-03-05 16:30:31 +0800

    glibc/malloc: Test 'corrupted size vs. prev_size'
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/malloc/malloc_trim.c
+++ glibc/malloc/malloc_trim.c
@@ -7,12 +7,14 @@
  * trim: 修剪, 消减
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <malloc.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "helpers.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	char *p1, *p2;
 	size_t size = getpagesize();
@@ -24,6 +26,25 @@ int main(void)
 
 	p2 = malloc(size);
 	printf("p2 %p\n", p2);
+
+#ifdef CORRUPTED_SIZE_VS_PREV_SIZE
+	/**
+	 * see glibc::malloc/malloc.c
+	 *
+	 * An allocated chunk looks like this:
+	 *
+	 * chunk-> +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	 *         |             Size of previous chunk, if unallocated (P clear)  |
+	 *         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	 *         |             Size of chunk, in bytes                     |A|M|P|
+	 *   mem-> +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	 *         |             User data starts here...                          .
+	 *         .                                                               .
+	 *
+	 * this overflow could cause "corrupted size vs. prev_size"
+	 */
+	*(size_t *)(p2 - 8) = 0;
+#endif
 
 	print_mallinfo();
 
