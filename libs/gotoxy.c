2f9a1ace0550c90768bdfb0e6d0a8eb89ec9c9cf
Rong Tao <rtoax@foxmail.com>
2025-03-08 10:59:59 +0800

    libs/gotoxy.c: Pretty looks
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- libs/gotoxy.c
+++ libs/gotoxy.c
@@ -4,42 +4,54 @@
 
 #include "gotoxy.h"
 
-#define P() do { \
-		fprintf(stdout, "\033[1;%dm#\033[m", 7); \
+#define WHITE	7
+#define RED	31
+#define P(color) do { \
+		fprintf(stdout, "\033[1;%dm#\033[m", color); \
 		fflush(stdout); \
 		usleep(300); \
 	} while (0)
 #define MIN(x, y) (x > y ? y : x)
 
+void fang(unsigned short row, unsigned short col, int bnd, int color)
+{
+	int x, y;
+
+	for (y = bnd; y < row - bnd; y++) {
+		gotoxy(bnd, y);
+		P(color);
+	}
+
+	for (x = bnd; x < col - bnd; x++) {
+		gotoxy(x, y);
+		P(color);
+	}
+
+	for (y = y; y >= bnd; y--) {
+		gotoxy(x, y);
+		P(color);
+	}
+
+	for (x = x; x >= bnd; x--) {
+		gotoxy(x, y);
+		P(color);
+	}
+}
+
 int main(void)
 {
-	int x, y, bnd;
+	int bnd;
 	unsigned short row, col;
 
 	bnd = 5;
 
 	getwinsz(&row, &col);
 
-	for (bnd = 5; bnd < MIN(row, col) / 2 + 1; bnd += 3) {
-		for (y = bnd; y < row - bnd; y++) {
-			gotoxy(bnd, y);
-			P();
-		}
-
-		for (x = bnd; x < col - bnd; x++) {
-			gotoxy(x, y);
-			P();
-		}
-
-		for (y = y; y >= bnd; y--) {
-			gotoxy(x, y);
-			P();
-		}
-
-		for (x = x; x >= bnd; x--) {
-			gotoxy(x, y);
-			P();
-		}
+	for (bnd = 4; bnd < MIN(row, col) / 2; bnd += 2) {
+		fang(row, col, bnd, WHITE);
+	}
+	for (bnd = MIN(row, col) / 2; bnd > 2; bnd -= 2) {
+		fang(row, col, bnd, RED);
 	}
 
 	printf("\n");
