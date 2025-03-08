d6608c6bb3f6d4bc1218d9983b53097e5074bc2d
Rong Tao <rtoax@foxmail.com>
2025-03-08 14:47:13 +0800

    libs/gotoxy.c: Pretty display
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- libs/gotoxy.c
+++ libs/gotoxy.c
@@ -6,52 +6,56 @@
 
 #define WHITE	7
 #define RED	31
-#define P(color) do { \
-		fprintf(stdout, "\033[1;%dm#\033[m", color); \
+#define GREEN	32
+#define YELLOW	33
+#define P(color, us) do { \
+		fprintf(stdout, "\033[7;%dm \033[m", color); \
 		fflush(stdout); \
-		usleep(300); \
+		usleep(us); \
 	} while (0)
 #define MIN(x, y) (x > y ? y : x)
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
-void fang(unsigned short row, unsigned short col, int bnd, int color)
+void fang(unsigned short row, unsigned short col, int bnd, int color, int us)
 {
 	int x, y;
 
 	for (y = bnd; y < row - bnd; y++) {
 		gotoxy(bnd, y);
-		P(color);
+		P(color, us);
 	}
 
 	for (x = bnd; x < col - bnd; x++) {
 		gotoxy(x, y);
-		P(color);
+		P(color, us);
 	}
 
 	for (y = y; y >= bnd; y--) {
 		gotoxy(x, y);
-		P(color);
+		P(color, us);
 	}
 
 	for (x = x; x >= bnd; x--) {
 		gotoxy(x, y);
-		P(color);
+		P(color, us);
 	}
 }
 
 int main(void)
 {
-	int bnd;
+	int bnd, i;
 	unsigned short row, col;
-
-	bnd = 5;
+	int colors[] = { WHITE, RED, GREEN, YELLOW };
 
 	getwinsz(&row, &col);
 
-	for (bnd = 4; bnd < MIN(row, col) / 2; bnd += 2) {
-		fang(row, col, bnd, WHITE);
-	}
-	for (bnd = MIN(row, col) / 2; bnd > 2; bnd -= 2) {
-		fang(row, col, bnd, RED);
+	for (i = 0; i < ARRAY_SIZE(colors); i++) {
+		for (bnd = 4; bnd < MIN(row, col) / 2; bnd += 2) {
+			fang(row, col, bnd, colors[i], 10);
+		}
+		for (bnd = MIN(row, col) / 2; bnd > 2; bnd -= 2) {
+			fang(row, col, bnd, colors[i], 10);
+		}
 	}
 
 	printf("\n");
