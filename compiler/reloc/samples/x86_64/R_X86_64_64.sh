ebe28f526dcb7fa3f07869643f64106cd78c0a41
Rong Tao <rongtao@cestc.cn>
2025-03-10 14:07:27 +0800

    R_X86_64_64.sh: Display rela symbol mem-address content
        $ readelf --relocs --wide R_X86_64_64.o
        Relocation section '.rela.data' at offset 0x3b0 contains 10 entries:
            Offset             Info             Type               Symbol's Value  Symbol's Name + Addend
        0000000000000010  0000000500000001 R_X86_64_64            0000000000000000 .rodata + 0
        0000000000000018  0000000500000001 R_X86_64_64            0000000000000000 .rodata + 0
        0000000000000020  0000000500000001 R_X86_64_64            0000000000000000 .rodata + 7
        0000000000000028  0000000500000001 R_X86_64_64            0000000000000000 .rodata + 7
        0000000000000030  0000000500000001 R_X86_64_64            0000000000000000 .rodata + 7
        0000000000000038  0000000500000001 R_X86_64_64            0000000000000000 .rodata + 7
        0000000000000040  0000000500000001 R_X86_64_64            0000000000000000 .rodata + 7
        0000000000000048  0000000300000001 R_X86_64_64            0000000000000000 .data + 0
        0000000000000050  0000000300000001 R_X86_64_64            0000000000000000 .data + 4
        0000000000000058  0000000300000001 R_X86_64_64            0000000000000000 .data + 8
    
        Relocation section '.rela.rodata' at offset 0x4a0 contains 2 entries:
            Offset             Info             Type               Symbol's Value  Symbol's Name + Addend
        0000000000000018  0000000500000001 R_X86_64_64            0000000000000000 .rodata + e
        0000000000000028  0000000500000001 R_X86_64_64            0000000000000000 .rodata + 20
    
        $ ./R_X86_64_64.sh
        rela: 0x10 0x500000001 R_X86_64_64 .rodata +0 .rela.data, data: 2 .data, sym: s1 sec 22 .data 0x404020 val 0x402128
        rela: 0x18 0x500000001 R_X86_64_64 .rodata +0 .rela.data, data: 2 .data, sym: s2 sec 22 .data 0x404028 val 0x402128
        rela: 0x20 0x500000001 R_X86_64_64 .rodata +0x7 .rela.data, data: 2 .data, sym: s3 sec 22 .data 0x404030 val 0x40212f
        rela: 0x28 0x500000001 R_X86_64_64 .rodata +0x7 .rela.data, data: 2 .data, sym: s4 sec 22 .data 0x404038 val 0x40212f
        rela: 0x30 0x500000001 R_X86_64_64 .rodata +0x7 .rela.data, data: 2 .data, sym: s5 sec 22 .data 0x404040 val 0x40212f
        rela: 0x38 0x500000001 R_X86_64_64 .rodata +0x7 .rela.data, data: 2 .data, sym: s6 sec 22 .data 0x404048 val 0x40212f
        rela: 0x40 0x500000001 R_X86_64_64 .rodata +0x7 .rela.data, data: 2 .data, sym: s7 sec 22 .data 0x404050 val 0x40212f
        rela: 0x48 0x300000001 R_X86_64_64 .data +0 .rela.data, data: 2 .data, sym: pi1 sec 22 .data 0x404058 val 0x404010
        rela: 0x50 0x300000001 R_X86_64_64 .data +0x4 .rela.data, data: 2 .data, sym: pi2 sec 22 .data 0x404060 val 0x404014
        rela: 0x58 0x300000001 R_X86_64_64 .data +0x8 .rela.data, data: 2 .data, sym: pi3 sec 22 .data 0x404068 val 0x404018
        rela: 0x18 0x500000001 R_X86_64_64 .rodata +0xe .rela.rodata, data: 5 .rodata, sym: s8 sec 14 .rodata 0x402140 val 0x402136
        rela: 0x28 0x500000001 R_X86_64_64 .rodata +0x20 .rela.rodata, data: 5 .rodata, sym: s9 sec 14 .rodata 0x402150 val 0x402148
    
        $ hexdump -C R_X86_64_64
        ...
        00002110  00 00 00 00 00 00 00 00  01 00 02 00 00 00 00 00  |................|
        00002120  00 00 00 00 00 00 00 00  48 65 6c 6c 6f 31 00 48  |........Hello1.H|
        00002130  65 6c 6c 6f 32 00 48 65  6c 6c 6f 33 00 00 00 00  |ello2.Hello3....|
        00002140  36 21 40 00 00 00 00 00  48 65 6c 6c 6f 34 00 00  |6!@.....Hello4..|
        00002150  48 21 40 00 00 00 00 00  01 1b 03 3b 24 00 00 00  |H!@........;$...|
        ...
        00003010  01 00 00 00 02 00 00 00  03 00 00 00 04 00 00 00  |................|
        00003020  28 21 40 00 00 00 00 00  28 21 40 00 00 00 00 00  |(!@.....(!@.....|
        00003030  2f 21 40 00 00 00 00 00  2f 21 40 00 00 00 00 00  |/!@...../!@.....|
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_64.sh
+++ compiler/reloc/samples/x86_64/R_X86_64_64.sh
@@ -19,9 +19,17 @@ do
 	secdataname=$(elf_sec2name ${ELF_OBJ} ${secdata})
 
 	sym=$(elf_off2object ${ELF_OBJ} ${r_offset} ${secdata})
+	symsec=$(elf_sym2sec ${ELF_EXE} ${sym})
+	symsecname=$(elf_sec2name ${ELF_EXE} ${symsec})
+	symsecoffset=$(elf_sec2offset ${ELF_EXE} ${symsec})
+	symsecaddr=$(elf_sec2addr ${ELF_EXE} ${symsec})
+	symvalue=$(elf_sym2value ${ELF_EXE} ${sym})
+
+	sym_file_off=$(( ${symvalue} - (${symsecaddr} - ${symsecoffset}) ))
+	sym_file_val=$(read_hex_from_file ${ELF_EXE} ${sym_file_off} 8)
 
 	printf "rela: %#lx %#lx %s ${sname} ${r_addend} ${r_secname}, " ${r_offset} ${r_info} ${r_type}
 	printf "data: ${secdata} ${secdataname}, "
-	printf "sym: %s" ${sym}
+	printf "sym: %s sec %d %s %#lx val %#lx" ${sym} ${symsec} ${symsecname} ${symvalue} ${sym_file_val}
 	printf "\n"
 done <<< $(elf_foreachreloc_sec ${ELF_OBJ})
