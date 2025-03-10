adaaa94b8a799700ba7b5a54264066783dab2edd
Rong Tao <rongtao@cestc.cn>
2025-03-10 11:13:55 +0800

    R_X86_64_64.c: Add RODATA_4
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/reloc/samples/x86_64/R_X86_64_64.c
+++ compiler/reloc/samples/x86_64/R_X86_64_64.c
@@ -7,13 +7,14 @@
 
 /**
  * in ELF 64-bit LSB relocatable:
- * s1 ~ s8 and pi1 ~ pi2 are R_X86_64_64
+ * s1 ~ s9 and pi1 ~ pi2 are R_X86_64_64
  */
 
 /* section ".rodata" */
 #define RODATA_1	"Hello1"
 #define RODATA_2	"Hello2"
 #define RODATA_3	"Hello3"
+#define RODATA_4	"Hello4"
 
 /* section ".data" */
 static int arr_i[] = { 1, 2, 3, 4 };
@@ -34,8 +35,8 @@ static char *s7 = RODATA_2;	/* .data, LOCAL */
  * in ELF 64-bit LSB relocatable:
  * s8 ~ s9 in ".rela.redata" for non-pie, ".rela.data.rel.ro.local" for pie
  */
-char *const s8 = RODATA_2;	/* .rodata, GLOBAL */
-char *const s9 = RODATA_3;	/* .rodata, GLOBAL */
+char *const s8 = RODATA_3;	/* .rodata, GLOBAL */
+char *const s9 = RODATA_4;	/* .rodata, GLOBAL */
 
 const char *s10 = NULL;
 
