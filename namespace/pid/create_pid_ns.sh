597681a2d712197407e28c71439bc6bdae21abbc
Rong Tao <rongtao@cestc.cn>
2025-03-10 18:21:05 +0800

    namespace/pid: create_pid_ns.sh: Add more 'init' test
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- namespace/pid/create_pid_ns.sh
+++ namespace/pid/create_pid_ns.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# execute:
+# - bash -c "sleep infinity & exec bash"
+# - bash -c "tail -f /dev/null & exec bash"
+sudo unshare --pid --fork --mount-proc bash
+
 # enter this pid namespace with:
 # $ sudo nsenter --pid --mount --target ${pid-of-bash}
-sudo unshare --pid --fork --mount-proc bash
