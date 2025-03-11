b4a6f24c745a9c15f297742c5d6a2a27cc4aa018
Rong Tao <rongtao@cestc.cn>
2025-03-11 14:02:39 +0800

    glibc/ld/ldd.sh: Get ld.so with ldconfig -p
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/ld/ldd.sh
+++ glibc/ld/ldd.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -ex
 
+# on fedora41 /usr/bin/ld.so -> ../lib64/ld-linux-x86-64.so.2
 ld_so=$( which ld.so 2>/dev/null || true )
+[[ -z ${ld_so} ]] && ld_so=$(ldconfig -p | awk '{print $NF}')
+
 if [[ -z ${ld_so} ]]; then
 	echo "WARNING: Not found ld.so"
 	exit 0
 fi
-ld.so --list /bin/ls
+
+${ld_so} --list /bin/ls
+
 # 可调参数
-ld.so --list-tunables
+${ld_so} --list-tunables
 # 诊断
-ld.so --list-diagnostics
+${ld_so} --list-diagnostics
