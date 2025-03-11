3e4ecf4dd38fb8ea159cfda7dcebb45693792719
Rong Tao <rongtao@cestc.cn>
2025-03-11 17:00:02 +0800

    syscall: namespace: unshare-execve.c: kill child when ctrl-c
    Finish: commit 6aecb963d3fd ("syscall: namespace: test fork(2) return ENOMEM (todo)")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/sched/namespace/unshare-execve.c
+++ syscall/samples/sched/namespace/unshare-execve.c
@@ -8,7 +8,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 
+static pid_t child_pid = -1;
 
 static void usage(char *pname)
 {
@@ -29,6 +31,16 @@ static void usage(char *pname)
 	exit(EXIT_FAILURE);
 }
 
+void sig_handler(int sig)
+{
+	psignal(sig, "Catch");
+	if (child_pid != -1) {
+		kill(child_pid, SIGKILL);
+		waitpid(child_pid, NULL, 0);
+	}
+	exit(0);
+}
+
 int unshare_execv(int flags, char *argv[])
 {
 	printf("EXEC: %s\n", argv[0]);
@@ -38,7 +50,6 @@ int unshare_execv(int flags, char *argv[])
 
 int main(int argc, char *argv[])
 {
-	pid_t pid;
 	int flags, opt;
 	bool flag_fork = false;
 
@@ -65,6 +76,8 @@ int main(int argc, char *argv[])
 	if (optind >= argc)
 		usage(argv[0]);
 
+	signal(SIGINT, sig_handler);
+
 	/**
 	 * CLONE_NEWPID - Unshare the PID namespace, so that the calling process
 	 *                has a new PID namespace for its children which is not
@@ -74,10 +87,10 @@ int main(int argc, char *argv[])
 		err(EXIT_FAILURE, "unshare");
 
 	if (flag_fork) {
-		pid = fork();
-		if (pid == 0)
+		child_pid = fork();
+		if (child_pid == 0)
 			unshare_execv(flags, &argv[optind]);
-		waitpid(pid, NULL, 0);
+		waitpid(child_pid, NULL, 0);
 	} else
 		unshare_execv(flags, &argv[optind]);
 
