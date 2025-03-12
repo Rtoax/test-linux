0e1d362ccf7edfe71de78e5d2b650e43ecfcb3d9
Rong Tao <rongtao@cestc.cn>
2025-03-12 18:33:10 +0800

    syscall: fork: symlink vfork.c to fork.c
        $ ./fork
        Parent 290666, val 0.
        Child 290667, val 1.
    
        $ ./vfork
        Child 290670, val 1.
        Parent 290669, val 1.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/sched/fork/fork.c
+++ syscall/samples/sched/fork/fork.c
@@ -1,17 +1,26 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 int main(int argc, char *argv[])
 {
+	int val = 0;
 	pid_t pid;
 
-	if ((pid = fork()) == 0) {
-		printf("Child %d.\n", getpid());
-	} else if (pid > 0) {
-		printf("Parent %d.\n", getpid());
-		waitpid(pid, NULL, 0);
+#ifdef VFORK
+	pid = vfork();
+#else
+	pid = fork();
+#endif
+	if (pid == 0) {
+		val = 1;
+		printf("Child %d, val %d.\n", getpid(), val);
+		exit(0);
 	}
+
+	printf("Parent %d, val %d.\n", getpid(), val);
+	waitpid(pid, NULL, 0);
 	return 0;
 }
--- syscall/samples/sched/fork/vfork.c
+++ syscall/samples/sched/fork/vfork.c
@@ -1,17 +1,1 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-int main(int argc, char *argv[])
-{
-	int pid;
-
-	if ((pid = vfork()) == 0) {
-		printf("Child %d.\n", getpid());
-	} else if (pid > 0) {
-		printf("Parent %d.\n", getpid());
-		waitpid(pid, NULL, 0);
-	}
-	return 0;
-}
+fork.c
