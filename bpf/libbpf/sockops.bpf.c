8274e9bf3b24de6a5958b3a37b4f36eaee96e953
Rong Tao <rongtao@cestc.cn>
2025-03-13 18:30:01 +0800

    libbpf: sockops.bpf.c: Fix kver compile errors on 4.19.90
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/libbpf/sockops.bpf.c
+++ bpf/libbpf/sockops.bpf.c
@@ -8,6 +8,7 @@
  * see linux/tools/testing/selftests/bpf/progs/test_tcp_hdr_options.c
  */
 #include <vmlinux.h>
+#include <linux/version.h>
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
@@ -19,12 +20,23 @@ SEC("sockops")
 int _sockops(struct bpf_sock_ops *skops)
 {
 	switch (skops->op) {
+/**
+ * v4.18-rc3-587-gf333ee0cdb27
+ * linux commit f333ee0cdb27 ("bpf: Add BPF_SOCK_OPS_TCP_LISTEN_CB")
+ */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 18, 0)
 	case BPF_SOCK_OPS_TCP_LISTEN_CB:
 		bpf_printk("BPF_SOCK_OPS_TCP_LISTEN_CB");
 		break;
+#endif
 	case BPF_SOCK_OPS_TCP_CONNECT_CB:
 		bpf_printk("BPF_SOCK_OPS_TCP_CONNECT_CB");
 		break;
+/**
+ * v5.9-rc1-156-g0813a841566f
+ * linux commit 0813a841566f ("bpf: tcp: Allow bpf prog to write and parse TCP header option")
+ */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)
 	case BPF_SOCK_OPS_PARSE_HDR_OPT_CB:
 		bpf_printk("BPF_SOCK_OPS_PARSE_HDR_OPT_CB");
 		break;
@@ -34,6 +46,7 @@ int _sockops(struct bpf_sock_ops *skops)
 	case BPF_SOCK_OPS_WRITE_HDR_OPT_CB:
 		bpf_printk("BPF_SOCK_OPS_WRITE_HDR_OPT_CB");
 		break;
+#endif
 	case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
 		bpf_printk("BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB");
 		break;
