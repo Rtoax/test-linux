f9bbd02858e28a3b1d66b7336992e66cab2be66a
Rong Tao <rongtao@cestc.cn>
2025-03-13 18:18:41 +0800

    glibc/ld/ldd.sh: Fix error on kylin v10
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/ld/ldd.sh
+++ glibc/ld/ldd.sh
@@ -3,16 +3,20 @@ set -ex
 
 # on fedora41 /usr/bin/ld.so -> ../lib64/ld-linux-x86-64.so.2
 ld_so=$( which ld.so 2>/dev/null || true )
-[[ -z ${ld_so} ]] && ld_so=$(ldconfig -p | awk '{print $NF}')
+[[ -z ${ld_so} ]] && ld_so=$(ldconfig -p | awk '{print $NF}' | \
+				grep ld-linux- | head -1)
 
 if [[ -z ${ld_so} ]]; then
 	echo "WARNING: Not found ld.so"
 	exit 0
 fi
 
-${ld_so} --list /bin/ls
+#${ld_so} --list /bin/ls
 
 # 可调参数
-${ld_so} --list-tunables
+# could grep rtld
+[[ "$(${ld_so} 2>&1 | grep list-tunables)" ]] && ${ld_so} --list-tunables
 # 诊断
-${ld_so} --list-diagnostics
+[[ "$(${ld_so} 2>&1 | grep list-diagnostics)" ]] && ${ld_so} --list-diagnostics
+
+true
