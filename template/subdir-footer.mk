e46035d12c7be704784e70fb333e5f93e4a21143
Rong Tao <rongtao@cestc.cn>
2025-03-14 14:52:54 +0800

    subdir-footer.mk: Remove .build,.test,.clean suffix
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/subdir-footer.mk
+++ template/subdir-footer.mk
@@ -3,13 +3,13 @@
 Q ?= @
 
 $(sub-dir-build):
-	@echo -e "\033[1;32m[build] $(@)\033[m"
+	@echo -e "\033[1;32m[build] $(patsubst %.build,%,$(@))\033[m"
 	${Q}@make -C $(@:.build=)
 
 $(sub-dir-test):
-	@echo -e "\033[1;32m[test] $(@)\033[m"
+	@echo -e "\033[1;32m[test] $(patsubst %.test,%,$(@))\033[m"
 	${Q}make -C $(@:.test=) test
 
 $(sub-dir-clean):
-	@echo -e "\033[1;32m[clean] $(@)\033[m"
+	@echo -e "\033[1;32m[clean] $(patsubst %.clean,%,$(@))\033[m"
 	${Q}make -C $(@:.clean=) clean
