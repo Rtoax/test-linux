044c2635901413f296693befa3918bf24da0faa2
Rong Tao <rongtao@cestc.cn>
2025-03-14 14:48:34 +0800

    subdir-footer.mk: Modify print format
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/subdir-footer.mk
+++ template/subdir-footer.mk
@@ -3,13 +3,13 @@
 Q ?= @
 
 $(sub-dir-build):
-	@echo -e "\033[1;32m [sub-dir] build $(@)\033[m"
+	@echo -e "\033[1;32m[build] $(@)\033[m"
 	${Q}@make -C $(@:.build=)
 
 $(sub-dir-test):
-	@echo -e "\033[1;32m [sub-dir] test $(@)\033[m"
+	@echo -e "\033[1;32m[test] $(@)\033[m"
 	${Q}make -C $(@:.test=) test
 
 $(sub-dir-clean):
-	@echo -e "\033[1;32m [sub-dir] clean $(@)\033[m"
+	@echo -e "\033[1;32m[clean] $(@)\033[m"
 	${Q}make -C $(@:.clean=) clean
