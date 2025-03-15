801f7e913561e99bff0ba5643318d7e09b4f0e00
Rong Tao <rongtao@cestc.cn>
2025-03-15 14:07:38 +0800

    template/subdir-{footer,header}.mk: fix
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/subdir-footer.mk
+++ template/subdir-footer.mk
@@ -4,7 +4,7 @@ Q ?= @
 
 $(sub-dir-build):
 	@echo -e "\033[1;32m[build] $(patsubst %.build,%,$(@))\033[m"
-	${Q}@make -C $(@:.build=)
+	${Q}make -C $(@:.build=)
 
 $(sub-dir-test):
 	@echo -e "\033[1;32m[test] $(patsubst %.test,%,$(@))\033[m"
--- template/subdir-header.mk
+++ template/subdir-header.mk
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0
+
+sub-dir ?=
+
 sub-dir-build := $(addprefix $(shell pwd)/,$(sub-dir:=.build))
 sub-dir-test := $(addprefix $(shell pwd)/,$(sub-dir:=.test))
 sub-dir-clean := $(addprefix $(shell pwd)/,$(sub-dir:=.clean))
-
