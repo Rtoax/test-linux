9dad9a5575ac14a2e576f7e27aca126b6fc758ff
Rong Tao <rongtao@cestc.cn>
2025-03-16 15:37:02 +0800

    make: patsubst.mk: $(NAME:a=b)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- gnu/make/patsubst.mk
+++ gnu/make/patsubst.mk
@@ -9,6 +9,8 @@ SRCS += ${SRCROOT}/module1/b.c
 
 # $(patsubst <pattern>,<replacement>,<text>)
 OBJS := $(patsubst %.c,%.o,$(SRCS))
+# equal to:
+OBJS_2 := $(SRCS:.c=.o)
 
 # $(subst <from>,<to>,<text>)
 DEPS := $(subst $(SRCROOT),$(DEPROOT),$(OBJS))
@@ -16,4 +18,5 @@ DEPS := $(subst $(SRCROOT),$(DEPROOT),$(OBJS))
 .PHONY: build
 build:
 	@echo OBJS=${OBJS}
+	@echo OBJS_2=${OBJS_2}
 	@echo DEPS=${DEPS}
