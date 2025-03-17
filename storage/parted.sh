aa3f664c2ee80b29477873a64eea53962ecaa205
Rong Tao <rongtao@cestc.cn>
2025-03-17 18:42:06 +0800

    storage: parted.sh: test parted with loop device
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- storage/parted.sh
+++ storage/parted.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-sudo parted -ls
+set -ex
 
 mkgpt() {
-	local disk=/dev/vdb
+	local disk=${1}
 
 	sudo parted ${disk} mklabel gpt
 
@@ -11,13 +10,22 @@ mkgpt() {
 	sudo parted ${disk} mkpart logical ext4 10M 20M
 	sudo parted ${disk} mkpart extended ext4 20M 30M
 
-	sudo mkfs.ext4 -T largefile ${disk}1
-	sudo mkfs.ext4 -T largefile ${disk}2
-	sudo mkfs.ext4 -T largefile ${disk}3
+	#sudo mkfs.ext4 -T largefile ${disk}1
+	#sudo mkfs.ext4 -T largefile ${disk}2
+	#sudo mkfs.ext4 -T largefile ${disk}3
 
 	sleep 1
 	lsblk -o +fstype,uuid
 }
 
-mkgpt
+sudo parted -ls
 
+BLK=blk.bin
+
+sudo dd if=/dev/zero of=${BLK} bs=1M count=310
+LOOP=$(sudo losetup --find --show ${BLK})
+sudo losetup -j ${BLK}
+
+mkgpt ${LOOP}
+
+sudo losetup --detach ${LOOP}
