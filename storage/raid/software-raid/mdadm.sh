f39c9c0a36f56f220a9c4f29e5887dc36fa5bbc0
Rong Tao <rongtao@cestc.cn>
2025-03-17 14:30:00 +0800

    software-raid: Move mdadm cmds from README.md to mdadm.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ storage/raid/software-raid/mdadm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# Create Software RAID1
+sudo mdadm -C /dev/md0 -ayes -l1 -n2 /dev/xvd[b,c]1
+
+# show stat
+cat /proc/mdstat
+# Or
+mdadm -D /dev/md0
+
+# Part
+sudo mkfs.ext4 /dev/md0
+
+# Emulate bad disk
+sudo mdadm /dev/md1 -f /dev/sdb5
+
+# Remove disk
+sudo mdadm /dev/md1 -r /dev/sdb5
+
+# Add new disk
+sudo mdadm /dev/md1 -a /dev/sdb7
+
+# Stop RAID
+sudo mdadm -S /dev/md1
+
