837679f4783fb02dad9974b203c2d85d28c1d0c3
Rong Tao <rongtao@cestc.cn>
2025-03-19 15:32:10 +0800

    fat: mkfs.fat.sh: pass volume name with -n
    $ hexdump -C F
    00000000  eb 3c 90 6d 6b 66 73 2e  66 61 74 00 02 04 04 00  |.<.mkfs.fat.....|
    00000010  02 00 02 00 50 f8 14 00  20 00 02 00 00 00 00 00  |....P... .......|
    00000020  00 00 00 00 80 00 29 d1  af fb 2b 54 4c 2d 54 45  |......)...+TL-TE|
    00000030  53 54 20 20 20 20 46 41  54 31 36 20 20 20 0e 1f  |ST    FAT16   ..|
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- fs/fat/mkfs.fat.sh
+++ fs/fat/mkfs.fat.sh
@@ -11,7 +11,7 @@ esac
 [[ ${strace} ]] && echo "Turn on strace"
 
 sudo dd if=/dev/zero of=fat.fs bs=1M count=10
-sudo ${strace:+strace} mkfs.fat fat.fs
+sudo ${strace:+strace} mkfs.fat -n TL-TEST fat.fs
 sudo mkdir -p tmp-mnt
 sudo mount fat.fs tmp-mnt -o loop,rw
 sudo cp /etc/os-release tmp-mnt/
