c65bb594c429cd39c481a2804b1272b3ec3575c2
Rong Tao <rongtao@cestc.cn>
2025-03-19 18:37:13 +0800

    storage: parted.sh: add xfs
    NAME        MAJ:MIN RM   SIZE RO TYPE MOUNTPOINTS FSTYPE LABEL         UUID
    loop0         7:0    0   512M  0 loop
    ├─loop0p1   259:6    0    61M  0 part             vfat   TL-FAT        E3F2-4759
    ├─loop0p2   259:7    0  30.5M  0 part             ext4   TL-EXT4       cd59a94d-157b-4946-ae89-e7c003c9aed3
    ├─loop0p3   259:8    0  30.5M  0 part             ext4   TL-EXT4       19c4bbe9-8458-4ad8-bcc5-83a091c3f82e
    └─loop0p4   259:9    0 366.2M  0 part             xfs    TL-XFS        46273959-4d46-4b99-9ded-d85e5b87e3b1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- storage/parted.sh
+++ storage/parted.sh
@@ -6,9 +6,10 @@ mkgpt() {
 
 	sudo parted ${disk} mklabel gpt
 
-	sudo parted ${disk} mkpart primary fat32 0 10M --align minimal
-	sudo parted ${disk} mkpart logical ext4 10M 20M
-	sudo parted ${disk} mkpart extended ext4 20M 30M
+	sudo parted ${disk} mkpart primary fat32 0 64M --align minimal
+	sudo parted ${disk} mkpart logical ext4 64M 96M --align minimal
+	sudo parted ${disk} mkpart extended ext4 96M 128M --align minimal
+	sudo parted ${disk} mkpart extended xfs 128M 512M --align minimal
 
 	# if use /dev/loop, partition with 'p' suffix
 	P=
@@ -18,6 +19,7 @@ mkgpt() {
 	sudo mkfs.fat -n TL-FAT ${disk}${P}1
 	sudo mkfs.ext4 -L TL-EXT4 -T largefile ${disk}${P}2
 	sudo mkfs.ext4 -L TL-EXT4 -T largefile ${disk}${P}3
+	sudo mkfs.xfs -L TL-XFS ${disk}${P}4
 
 	sleep 1
 	lsblk -o +fstype,label,uuid
@@ -29,10 +31,14 @@ BLK=blk.bin
 
 sudo rm -f ${BLK}
 
-sudo dd if=/dev/zero of=${BLK} bs=1M count=310
+sudo dd if=/dev/zero of=${BLK} bs=1M count=512
 LOOP=$(sudo losetup --find --show ${BLK})
 sudo losetup -j ${BLK}
 
+goodbye() {
+	sudo losetup --detach ${LOOP}
+}
+trap goodbye EXIT
+
 mkgpt ${LOOP}
 
-sudo losetup --detach ${LOOP}
