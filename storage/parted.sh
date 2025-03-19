ba9cf217cbaf3dda3c4fc4c4cd9de3b4d7930829
Rong Tao <rongtao@cestc.cn>
2025-03-19 14:29:26 +0800

    storage: parted.sh: Fix: Warning: The resulting partition is not properly aligned for best performance: 34s % 2048s != 0s
        + sudo parted /dev/loop0 mkpart primary ext4 0 10M
        Warning: The resulting partition is not properly aligned for best performance: 34s % 2048s != 0s
        Ignore/Cancel?
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- storage/parted.sh
+++ storage/parted.sh
@@ -6,7 +6,7 @@ mkgpt() {
 
 	sudo parted ${disk} mklabel gpt
 
-	sudo parted ${disk} mkpart primary ext4 0 10M
+	sudo parted ${disk} mkpart primary ext4 0 10M --align minimal
 	sudo parted ${disk} mkpart logical ext4 10M 20M
 	sudo parted ${disk} mkpart extended ext4 20M 30M
 
