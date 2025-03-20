4688032b1fd77ae6bd60186c73ae42e5b42ad39c
Rong Tao <rongtao@cestc.cn>
2025-03-20 16:50:32 +0800

    lvm: activate.sh: vgchange active command
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- storage/lvm/scripts/activate.sh
+++ storage/lvm/scripts/activate.sh
@@ -2,7 +2,9 @@
 
 # activate VG|LV
 lvchange -ay ${VG}
+vgchange -ay ${VG}
 
 # deactivate
 # WARNING: you need to deactivate vg first before $ wipefs --all --force /dev/vdb
 lvchange -an ${VG}
+vgchange -an ${VG}
