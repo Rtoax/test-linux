2c4270102cdc16e9c5b4eee74d1adcfc489a480b
Rong Tao <rongtao@cestc.cn>
2025-03-21 15:42:11 +0800

    macro: LINUX_VERSION_MAJOR: 5.4.241-30.0017.18 don't have it
    in /usr/include/linux/version.h
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- macro/LINUX_VERSION_CODE.c
+++ macro/LINUX_VERSION_CODE.c
@@ -13,9 +13,22 @@ int main(void)
 	pfx = name.release;
 
 	printf("%s: LINUX_VERSION_CODE = %d.\n", pfx, LINUX_VERSION_CODE);
+#ifdef LINUX_VERSION_MAJOR
 	printf("%s: LINUX_VERSION_MAJOR = %d.\n", pfx, LINUX_VERSION_MAJOR);
 	printf("%s: LINUX_VERSION_PATCHLEVEL = %d.\n", pfx, LINUX_VERSION_PATCHLEVEL);
 	printf("%s: LINUX_VERSION_SUBLEVEL = %d.\n", pfx, LINUX_VERSION_SUBLEVEL);
+#endif
+
+/* from ../kernel.mk */
+#ifdef KVERSION
+	printf("%s: KVERSION = %d.\n", pfx, KVERSION);
+#endif
+#ifdef KPATCHLEVEL
+	printf("%s: KPATCHLEVEL = %d.\n", pfx, KPATCHLEVEL);
+#endif
+#ifdef KSUBLEVEL
+	printf("%s: KSUBLEVEL = %d.\n", pfx, KSUBLEVEL);
+#endif
 
 	return 0;
 }
