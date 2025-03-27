66f726f2783f9266d38bd017ec2e9e067f6431d8
Rong Tao <rongtao@cestc.cn>
2025-03-27 14:23:54 +0800

    shell: IFS.sh: Only effect to ${arr[*]}
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- shell/IFS.sh
+++ shell/IFS.sh
@@ -5,5 +5,8 @@ my_array=("apple" "banana" "cherry" "date")
 new_separator="|"
 old_IFS=$IFS
 IFS=$new_separator
+# apple|banana|cherry|date
 echo "${my_array[*]}"
+# apple banana cherry date
+echo "${my_array[@]}"
 IFS=$old_IFS
