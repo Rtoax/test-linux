d531692cf105e7d819735583b436d0c1bf754e58
Rong Tao <rongtao@cestc.cn>
2025-03-27 15:25:28 +0800

    uefi: efibootmgr.sh: efivars and nvram
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- init/uefi/efibootmgr.sh
+++ init/uefi/efibootmgr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
 # Displaying the current settings
-display() {
-	sudo efibootmgr
-}
+# - if on physical machine, read from /sys/firmware/efi/efivars/Bootxxx
+# - if on virtual machine, from nvram like /var/lib/libvirt/qemu/nvram/fedora.qcow2
+sudo efibootmgr
 
