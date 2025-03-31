03512234d0aa1a99b0e7f7c04efdc1f2772c7bf9
Rong Tao <rongtao@cestc.cn>
2025-03-31 10:07:22 +0800

    libcgroup: Add cgroup_new_cgroup.c test
    see libcgroup[1] samples/c/empty_cgroup_v2.c
    
    Link: https://github.com/libcgroup/libcgroup
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ cgroup/libcgroup/samples/cgroup_new_cgroup.c
@@ -0,0 +1,32 @@
+#include <libcgroup.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define CGRP_NAME "empty_cgrp"
+
+int main(int argc, char **argv)
+{
+	struct cgroup *cgrp = NULL;
+	int ret = 0;
+
+	ret = cgroup_init();
+	if (ret) {
+		fprintf(stderr, "cgroup_init failed\n");
+		exit(1);
+	}
+
+	cgrp = cgroup_new_cgroup(CGRP_NAME);
+	if (!cgrp) {
+		fprintf(stderr, "Failed to allocate cgroup %s\n", CGRP_NAME);
+		exit(1);
+	}
+
+	ret = cgroup_create_cgroup(cgrp, 0);
+	if (ret)
+		fprintf(stderr, "Failed to create cgroup %s\n", CGRP_NAME);
+
+	cgroup_free(&cgrp);
+
+	return ret;
+}
