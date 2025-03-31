46234c1a37b242d218f62b6d850c6c88827109f9
Rong Tao <rtoax@foxmail.com>
2025-03-31 16:46:25 +0800

    init/rootfs: debootstrap.sh: try ubuntu loong64
    Link: https://mirrors.tuna.tsinghua.edu.cn/help/ubuntu/
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- init/rootfs/debootstrap.sh
+++ init/rootfs/debootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # debootstrap bootstraps a basic Debian system of suite into target from
 # mirror by running script.
-set -e
+#
+set -ex
 
 # amd64, arm64, loong64, etc.
 darch=
@@ -10,9 +11,14 @@ darch=
 
 mkdir rootfs-${darch}
 
-sudo debootstrap \
-	--arch=${darch} \
-	--foreign \
-	bookworm \
+debian12=( bookworm "http://deb.debian.org/debian/" )
+ubuntu=( oracular "https://mirrors.tuna.tsinghua.edu.cn/ubuntu/" ./usr/share/debootstrap/scripts/bookworm )
+
+os_ver=${ubuntu[0]}
+os_repo=${ubuntu[1]}
+os_script=${ubuntu[2]}
+
+sudo debootstrap --arch=${darch} --foreign --no-check-gpg \
+	${os_ver} \
 	./rootfs-${darch} \
-	http://deb.debian.org/debian/
+	${os_repo} ${os_script}
