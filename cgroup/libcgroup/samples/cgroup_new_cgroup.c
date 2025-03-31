871b12d36589f3ac2919e80c0b30709e81c5e749
Rong Tao <rongtao@cestc.cn>
2025-03-31 11:25:58 +0800

    libcgroup: delete after test and create memcg
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cgroup/libcgroup/samples/cgroup_new_cgroup.c
+++ cgroup/libcgroup/samples/cgroup_new_cgroup.c
@@ -8,6 +8,7 @@
 int main(int argc, char **argv)
 {
 	struct cgroup *cgrp = NULL;
+	struct cgroup_controller *cgroup_controller;
 	int ret = 0;
 
 	ret = cgroup_init();
@@ -18,7 +19,8 @@ int main(int argc, char **argv)
 
 	cgrp = cgroup_new_cgroup(CGRP_NAME);
 	if (!cgrp) {
-		fprintf(stderr, "Failed to allocate cgroup %s\n", CGRP_NAME);
+		fprintf(stderr, "Failed to allocate cgroup %s, %s\n", CGRP_NAME,
+			cgroup_strerror(cgroup_get_last_errno()));
 		exit(1);
 	}
 
@@ -26,7 +28,22 @@ int main(int argc, char **argv)
 	if (ret)
 		fprintf(stderr, "Failed to create cgroup %s\n", CGRP_NAME);
 
-	cgroup_free(&cgrp);
+	cgroup_controller = cgroup_add_controller(cgrp, "memory");
+	if (!cgroup_controller) {
+		fprintf(stderr, "Error adding memory controller to cgroup\n");
+		ret = 1;
+		goto free;
+	}
 
+	ret = cgroup_set_value_uint64(cgroup_controller, "memory.limit_in_bytes", 100 * 1024 * 1024);
+	if (ret) {
+		fprintf(stderr, "Error setting memory limit: %d\n", ret);
+		ret = 1;
+		goto free;
+	}
+
+free:
+	cgroup_delete_cgroup(cgrp, 0);
+	cgroup_free(&cgrp);
 	return ret;
 }
