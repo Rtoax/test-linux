64f990a5bc6de51dd571ae5fc0b3dce27008452d
Rong Tao <rongtao@cestc.cn>
2025-04-01 11:32:32 +0800

    make: test: nodir and basename
    Link: https://www.zhaixue.cc/makefile/makefile-filename-function.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- gnu/make/dir.mk
+++ gnu/make/dir.mk
@@ -1,6 +1,10 @@
 # SPDX-License-Identifier: GPL-3.0
+path := /a/b/c
 
-$(info dir = $(dir /a/b/c))
+$(info path = ${path})
+$(info dir = $(dir ${path}))
+$(info notdir = $(notdir ${path}))
+$(info basename = $(basename ${path}))
 
 .PHONY: build
 build:
