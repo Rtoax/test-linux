30b723e073f10a0ea25f5fa03fb432d11444919c
Rong Tao <rongtao@cestc.cn>
2025-04-01 10:31:35 +0800

    gnu,macro: add more macros for GNUC and clang
    clang:
    
        clang: __clang_major__ = 19
        clang: __clang_minor__ = 1
        clang: __clang_patchlevel__ = 7
        clang: __clang_version__ = 19.1.7 (Fedora 19.1.7-3.fc41)
        clang: __GNUC__ = 4.
        clang: __GNUC_MINOR__ = 2.
        clang: __GNUC_PATCHLEVEL__ = 1.
        clang: __ATOMIC_RELAXED = 0.
        clang: __ATOMIC_SEQ_CST = 5.
        clang: __ATOMIC_ACQUIRE = 2.
        clang: __ATOMIC_RELEASE = 3.
        clang: __ATOMIC_ACQ_REL = 4.
        clang: __ATOMIC_CONSUME = 1.
        clang: __VERSION__ = Clang 19.1.7 (Fedora 19.1.7-3.fc41)
    
    gcc:
    
        gcc: __GNUC__ = 14.
        gcc: __GNUC_MINOR__ = 2.
        gcc: __GNUC_PATCHLEVEL__ = 1.
        gcc: __ATOMIC_RELAXED = 0.
        gcc: __ATOMIC_SEQ_CST = 5.
        gcc: __ATOMIC_ACQUIRE = 2.
        gcc: __ATOMIC_RELEASE = 3.
        gcc: __ATOMIC_ACQ_REL = 4.
        gcc: __ATOMIC_CONSUME = 1.
        gcc: __VERSION__ = 14.2.1 20250110 (Red Hat 14.2.1-7)
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- gnu/__GNUC__.c
+++ gnu/__GNUC__.c
@@ -1,3 +1,10 @@
+/**
+ * The __GNUC__ symbol is defined in the GCC repository.
+ *
+ * gcc::gcc/cppbuiltin.cc: define__GNUC__()
+ * llvm::clang/lib/Frontend/InitPreprocessor.cpp: InitializePredefinedMacros()
+ */
+
 #include <stdio.h>
 
 #if defined(__GNUC__) && ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 5) || __GNUC__ >= 5)
@@ -6,13 +13,32 @@
 
 int main(void)
 {
-	const char *pfx = "GCC";
+	const char *pfx = "gcc";
 #ifdef __clang__
 	pfx = "clang";
+
+	printf("clang: __clang_major__ = %d\n", __clang_major__);
+	printf("clang: __clang_minor__ = %d\n", __clang_minor__);
+	printf("clang: __clang_patchlevel__ = %d\n", __clang_patchlevel__);
+	printf("clang: __clang_version__ = %s\n", __clang_version__);
 #endif
+
 	printf("%s: __GNUC__ = %d.\n", pfx, __GNUC__);
 	printf("%s: __GNUC_MINOR__ = %d.\n", pfx, __GNUC_MINOR__);
 	printf("%s: __GNUC_PATCHLEVEL__ = %d.\n", pfx, __GNUC_PATCHLEVEL__);
+
+	/**
+	 * macros for the C11 / C++11 memory orderings
+	 *
+	 * gcc::gcc/cppbuiltin.cc: define__GNUC__()
+	 * llvm::clang/lib/Frontend/InitPreprocessor.cpp: InitializePredefinedMacros()
+	 */
+	printf("%s: __ATOMIC_RELAXED = %d.\n", pfx, __ATOMIC_RELAXED);
+	printf("%s: __ATOMIC_SEQ_CST = %d.\n", pfx, __ATOMIC_SEQ_CST);
+	printf("%s: __ATOMIC_ACQUIRE = %d.\n", pfx, __ATOMIC_ACQUIRE);
+	printf("%s: __ATOMIC_RELEASE = %d.\n", pfx, __ATOMIC_RELEASE);
+	printf("%s: __ATOMIC_ACQ_REL = %d.\n", pfx, __ATOMIC_ACQ_REL);
+	printf("%s: __ATOMIC_CONSUME = %d.\n", pfx, __ATOMIC_CONSUME);
 
 	/* This macro expands to a string constant which describes the version
 	 * of the compiler in use. You should not rely on its contents having
--- macro/__clang__.c
+++ macro/__clang__.c
@@ -1,9 +1,21 @@
+/**
+ * llvm::clang/lib/Frontend/InitPreprocessor.cpp: InitializePredefinedMacros()
+ */
 #include <stdio.h>
 
 int main(void)
 {
+#ifdef __llvm__
+	printf("llvm:\n");
+#endif
 #ifdef __clang__
 	printf("clang:\n");
+	printf("clang: __clang_major__ = %d\n", __clang_major__);
+	printf("clang: __clang_minor__ = %d\n", __clang_minor__);
+	printf("clang: __clang_patchlevel__ = %d\n", __clang_patchlevel__);
+	printf("clang: __clang_version__ = %s\n", __clang_version__);
+#else
+	printf("not define __clang__, please compile with clang.\n");
 #endif
 	return 0;
 }
