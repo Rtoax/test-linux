b99ed70b6b04f811665b7773580bb3e364e5c866
Rong Tao <rongtao@cestc.cn>
2025-04-02 15:23:02 +0800

    mm/vmalloc/modules: add vmalloc_base.c for CONFIG_DYNAMIC_MEMORY_LAYOUT=y
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- mm/vmalloc/modules/vmalloc_base.c
+++ mm/vmalloc/modules/vmalloc_base.c
@@ -1,1 +1,15 @@
-VMALLOC_START.c
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/export.h>
+#include <linux/mm.h>
+#include <linux/vmalloc.h>
+
+static int __init mod_init(void)
+{
+	printk("vmalloc_base = 0x%lx\n", vmalloc_base);
+	printk("vmemmap_base = 0x%lx\n", vmemmap_base);
+	return -EINVAL;
+}
+
+module_init(mod_init);
+MODULE_LICENSE("GPL");
