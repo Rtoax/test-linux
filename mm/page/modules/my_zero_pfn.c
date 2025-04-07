3db57642e7e6f5aa9042dba988d531cc3d924f53
Rong Tao <rongtao@cestc.cn>
2025-04-07 19:35:07 +0800

    mm/page: my_zero_pfn.c: Update
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- mm/page/modules/my_zero_pfn.c
+++ mm/page/modules/my_zero_pfn.c
@@ -1,9 +1,3 @@
-/**
- *	File	./my_zero_pfn.c
- *	Time	2021.11.05
- *	Author	Rong Tao <rtoax@foxmail.com>
- */
-
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -13,17 +7,11 @@
 
 static int kernel_init(void)
 {
-    printk(KERN_INFO "========== my init =========\n");
+	printk(KERN_INFO "========== my init =========\n");
 	printk(KERN_INFO "zero pfn = %lx\n", my_zero_pfn(0));
-    return 0;
-}
-
-static void kernel_exit(void)
-{
-	printk(KERN_INFO "my exit.\n");
+	return -EINVAL;
 }
 
 module_init(kernel_init);
-module_exit(kernel_exit);
 MODULE_AUTHOR("Rong Tao");
 MODULE_LICENSE("GPL");
