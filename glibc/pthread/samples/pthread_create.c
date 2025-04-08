477d9a392e0a11ae9d0ed722ec5daf19406db749
Rong Tao <rongtao@cestc.cn>
2025-04-08 15:10:33 +0800

    pthread/samples: pthread_create.c: stress create threads
        $ ./pthread_create nr=27000 t=10000000
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/pthread/samples/pthread_create.c
+++ glibc/pthread/samples/pthread_create.c
@@ -1,16 +1,19 @@
 #define _GNU_SOURCE
+#include <assert.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <signal.h>
 #include <unistd.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <string.h>
 
-#ifndef NR_THREADS
-#define NR_THREADS	1
-#endif
 
-sig_atomic_t loop = true;
-const int us = 100000;
+static int nr_threads = 1;
+
+static sig_atomic_t loop = true;
+static int us = 100000;
 
 void sig_handler(int signum)
 {
@@ -33,14 +36,44 @@ void *print_xs(void *unused)
 	return NULL;
 }
 
-int main(void)
+void parse_args(int argc, char *argv[])
 {
 	int i;
-	pthread_t threads[NR_THREADS];
+	for (i = 0; i < argc; i++) {
+		if (!strncmp(argv[i], "nr=", 3)) {
+			nr_threads = atoi(argv[i] + 3);
+			if (nr_threads <= 1) {
+				fprintf(stderr, "ERROR: bad number %s\n", argv[i]);
+				exit(1);
+			}
+		} else if (!strncmp(argv[i], "t=", 2)) {
+			us = atoi(argv[i] + 2);
+			if (us <= 1000) {
+				fprintf(stderr, "ERROR: bad us %s\n", argv[i]);
+				exit(1);
+			}
+		} else {
+			fprintf(stderr, "ERROR: unknown %s\n", argv[i]);
+			exit(1);
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int i;
+	pthread_t *threads;
+
+	fprintf(stderr, "%s [nr=<number of threads>] [t=<us>]\n", argv[0]);
 
 	signal(SIGINT, sig_handler);
 
-	for (i = 0; i < NR_THREADS; i++)
+	parse_args(argc - 1, argv + 1);
+
+	threads = malloc(sizeof(pthread_t) * nr_threads);
+	assert(threads && "malloc failed.");
+
+	for (i = 0; i < nr_threads; i++)
 		pthread_create(&threads[i], NULL, &print_xs, NULL);
 
 	pthread_setname_np(pthread_self(), "pthread-parent");
@@ -50,8 +83,10 @@ int main(void)
 		usleep(us);
 	}
 
-	for (i = 0; i < NR_THREADS; i++)
+	for (i = 0; i < nr_threads; i++)
 		pthread_join(threads[i], NULL);
+
+	free(threads);
 
 	return 0;
 }
