bc884c8300c325e9e6fba5dd03cc7c5f18d60c62
Rong Tao <rongtao@cestc.cn>
2025-04-11 12:22:03 +0800

    cpp: add string.cpp
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ cpp/string.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * std::string& replace(size_t pos, size_t len, const std::string& str);
+ * std::string& replace(size_t pos, size_t len, const std::string& str, size_t subpos, size_t sublen);
+ * std::string& replace(size_t pos, size_t len, const char* s);
+ * std::string& replace(size_t pos, size_t len, const char* s, size_t n);
+ * std::string& replace(size_t pos, size_t len, size_t n, char c);
+ */
+int replace(void)
+{
+	std::string str1("1234567890");
+
+	std::cout << "str1 " << str1 << std::endl;
+	str1.replace(2, 3, "abc");
+	std::cout << "str1 " << str1 << std::endl;
+
+	return 0;
+}
+
+int main(void)
+{
+	replace();
+}
