706fde4b74d96ae14894d286e0ae850e9c10560c
Rong Tao <rongtao@cestc.cn>
2025-04-13 21:04:59 -0400

    btf.mk: rename function vmlinux_has_sym to vmlinux_has_struct
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/btf/btf.mk
+++ bpf/btf/btf.mk
@@ -2,9 +2,9 @@
 
 VMLINUX_H ?= vmlinux.h
 
-# $1 - symbol
-define vmlinux_has_sym
-$(shell if [[ $$(grep -wo '${1}' ${VMLINUX_H}) ]]; then \
+# $1 - struct name, like task_struct
+define vmlinux_has_struct
+$(shell if [[ "$$(grep -wo '^struct ${1} {' ${VMLINUX_H})" ]]; then \
 		echo y; \
 	else echo n; \
 	fi)
