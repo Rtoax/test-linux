fd57a356818c850723b1969b59adcb3b49d162f2
Rong Tao <rongtao@cestc.cn>
2025-04-16 19:13:12 +0800

    elf.mk: elf_is_pie(): modify
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- elf/elf.mk
+++ elf/elf.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0
 SHELL = bash
 
-# TODO: 'macro' not equal to '1'?????
+# return 'y' if is pie
 define elf_is_pie
-  $(shell file $(1) | grep -io pie 1>/dev/null && echo 1)
+$(shell file $(1) | grep -io pie 1>/dev/null && echo y)
 endef
