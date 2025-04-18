763269a7f882398f9e3519e33919d60707c28fef
Rong Tao <rongtao@cestc.cn>
2025-04-18 08:38:57 +0800

    vdso: dump.sh: code style
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- vdso/dump.sh
+++ vdso/dump.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+exec >&2
+
 readonly vdso_aarch64=linux-vdso.so.1
 readonly vdso_arm=linux-vdso.so.1
 readonly vdso_x86_64=linux-vdso.so.1
@@ -14,9 +16,8 @@ x86_32) vdso_so=${vdso_x86_32}-x86_32 ;;
 aarch64) vdso_so=${vdso_aarch64}-aarch64 ;;
 arm) vdso_so=${vdso_arm}-arm ;;
 esac
-vdso_sz=
 
-exec >&2
+rm -f ${vdso_so}
 
 readonly addr_range=( $(cat /proc/${pid}/maps \
 			| grep -w '\[vdso\]' \
@@ -27,11 +28,11 @@ readonly vdso_sz=$(printf "%ld" $(( 0x${addr_range[1]} - 0x${addr_range[0]} )))
 
 echo "pid ${pid} [vdso] 0x${addr_range[0]}-0x${addr_range[1]}, size = ${vdso_sz}"
 
-dd	if=/proc/${pid}/mem \
-	of=${vdso_so} \
-	ibs=1 \
-	skip=$(printf %ld 0x${addr_range[0]}) \
+dd if=/proc/${pid}/mem of=${vdso_so} \
+	ibs=1 skip=$(printf %ld 0x${addr_range[0]}) \
 	count=${vdso_sz}
 
 readelf --syms --wide ${vdso_so}
+# 'criu check' will check this
+readelf --dynamic --wide ${vdso_so}
 md5sum ${vdso_so}
