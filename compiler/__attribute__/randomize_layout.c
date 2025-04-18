b882c775f05ce9375b31d050e5f3121acbb41199
Rong Tao <rongtao@cestc.cn>
2025-04-18 16:53:55 +0800

    compiler/attribute: randomize_layout: warning: ‘randomize_layout’ attribute directive ignored on gcc 15.0.1
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/__attribute__/randomize_layout.c
+++ compiler/__attribute__/randomize_layout.c
@@ -2,15 +2,22 @@
 
 #define __randomize_layout __attribute__((randomize_layout))
 
-struct rt_struct {
+struct __randomize_layout rt_struct {
 	int a;
 	char b;
 	long c;
 	short d;
-} __randomize_layout;
+};
 
 int main(void)
 {
-	printf("sizeof rt_struct = %d\n", sizeof(struct rt_struct));
+	struct rt_struct rt;
+
+	printf("size of rt_struct is %d\n", sizeof(struct rt_struct));
+	printf("address of struct is %p\n", &rt);
+	printf("address of a is %p\n", &rt.a);
+	printf("address of b is %p\n", &rt.b);
+	printf("address of c is %p\n", &rt.c);
+	printf("address of d is %p\n", &rt.d);
 	return 0;
 }
