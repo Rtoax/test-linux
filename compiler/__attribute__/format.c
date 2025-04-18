25c619defc0c3e6d67571cfabe550707d81484c3
Rong Tao <rongtao@cestc.cn>
2025-04-18 17:08:51 +0800

    compiler/attribute: format: printf: fix warning: optional second parameter of ‘va_start’ not last named argument
      CC  format.c to format.o
    In file included from format.c:2:
    format.c: In function ‘error’:
    format.c:11:23: warning: optional second parameter of ‘va_start’ not last named argument [-Wvarargs]
       11 |         va_start(arg, fmt);
          |                       ^~~
    In file included from format.c:2:
    format.c: In function ‘error’:
    format.c:11:23: warning: optional second parameter of ‘va_start’ not last named argument [-Wvarargs]
       11 |         va_start(arg, fmt);
          |                       ^~~
      LD  format
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/__attribute__/format.c
+++ compiler/__attribute__/format.c
@@ -3,13 +3,13 @@
 #include <stdint.h>
 
 
-int __attribute__((format(printf, 1, 3)))
-error(const char *fmt, const char *err, ...)
+int __attribute__((format(printf, 2, 3)))
+log_error(const char *err, const char *fmt, ...)
 {
 	int ret;
 	va_list arg;
 	va_start(arg, fmt);
-	printf("%s: ", err);
+	printf("[%s] ", err);
 	ret = vprintf(fmt, arg);
 	va_end(arg);
 	return ret;
@@ -25,6 +25,6 @@ int rte_log(uint32_t level, uint32_t logtype, const char *format, ...)
 
 int main(void)
 {
-	error("%d, %d, %d\n", "Not this kind of type", 1,3,4);
+	log_error("This is an error type", "%d, %d, %d\n", 1, 3, 4);
 	return 0;
 }
