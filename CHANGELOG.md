814eb00d4eaa6f00c9c4ac2e7b0233ad37d80211
Rong Tao <rongtao@cestc.cn>
2025-04-24 17:26:11 +0800

    syscall: module: add create_module.c only for linux <= v2.6
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,7 +10,7 @@ and this project adheres to
 
 #### Breaking Changes
 #### Added
-- syscall/module: Add `mod_helpers.c`
+- syscall/module: Add `mod_helpers.c` and test `create_module(2)` for linux v2.6
 - module: Test linux/vermagic.h `VERMAGIC_STRING`
 - Add openssl.mk
 - Add distributions UOS
---
+++ syscall/samples/module/create_module.c
@@ -0,0 +1,17 @@
+/**
+ * caddr_t create_module(const char *name, size_t size);
+ *
+ * This system call is present only before Linux 2.6.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <linux/module.h>
+
+
+int main(int argc, char *argv[])
+{
+	/* TODO */
+	return 0;
+}
+
