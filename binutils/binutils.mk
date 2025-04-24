74009f8bcada5b862935a60f0da92f8e9d48e7a0
Rong Tao <rongtao@cestc.cn>
2025-04-24 15:11:40 +0800

    binutils.mk: display info only defined DEBUG
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- binutils/binutils.mk
+++ binutils/binutils.mk
@@ -4,8 +4,6 @@ SHELL = bash
 LD_BFD := $(shell which ld.bfd 2>/dev/null)
 ifeq ($(LD_BFD),)
   $(error "Not found linker ld.bfd, install binutils first")
-else
-  $(info LD_BFD = ${LD_BFD})
 endif
 
 # Get binutils version
@@ -13,8 +11,12 @@ BINUTILS_VERSION := $(shell ${LD_BFD} --version | grep -o [0-9].[0-9]\. | sed -n
 BINUTILS_VERSION_MAJOR := $(shell echo ${BINUTILS_VERSION} | awk -F '.' '{print $$1}')
 BINUTILS_VERSION_MINOR := $(shell echo ${BINUTILS_VERSION} | awk -F '.' '{print $$2}')
 
-$(info Binutils version ${BINUTILS_VERSION_MAJOR}.${BINUTILS_VERSION_MINOR})
 ifneq (${BINUTILS_VERSION_MAJOR},2)
   $(error Not support binutils ${BINUTILS_VERSION})
+endif
+
+ifdef DEBUG
+  $(info LD_BFD = ${LD_BFD})
+  $(info Binutils version ${BINUTILS_VERSION_MAJOR}.${BINUTILS_VERSION_MINOR})
 endif
 
