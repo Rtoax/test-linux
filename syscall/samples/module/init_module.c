bd7f533b6f4c7152520ce0250f28882630ddfef0
Rong Tao <rongtao@cestc.cn>
2025-04-24 19:21:02 +0800

    syscall: module: finit_module.c: add --force argument
        $ ./finit_module --help
        Usage: finit_module [OPTION...]
        USAGE: [-i <module file>] [-v|--verbose]
    
          -f, --force                Skip version magic and modversion check
          -i, --input=INPUT          Input kernel module file
          -M, --force-modversion     Skip modversion check
          -v, --verbose[=VERBOSE]    Display detail
          -V, --force-vermagic       Skip version magic check
          -?, --help                 Give this help list
              --usage                Give a short usage message
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/module/init_module.c
+++ syscall/samples/module/init_module.c
@@ -1,3 +1,4 @@
+#include <argp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,19 +6,81 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <linux/module.h>    /* Definition of MODULE_* constants */
 
 #include "mod_helpers.h"
+
+char *module_file = NULL;
+int verbose = false;
+#ifdef TEST_FINIT_MODULE /* finit_module(2) */
+bool force_vermagic = false;
+bool force_modversion = false;
+#endif
+
+const char argp_prog_doc[] =
+	"USAGE: [-i <module file>] [-v|--verbose]\n";
+
+static const struct argp_option opts[] = {
+	{ "input", 'i', "INPUT", 0, "Input kernel module file" },
+#ifdef TEST_FINIT_MODULE /* finit_module(2) */
+	/* see modprobe(8) */
+	{ "force-vermagic", 'V', NULL, 1, "Skip version magic check" },
+	{ "force-modversion", 'M', NULL, 1, "Skip modversion check" },
+	{ "force", 'f', NULL, 1, "Skip version magic and modversion check" },
+#endif
+	{ "verbose", 'v', "VERBOSE", 1, "Display detail" },
+	{},
+};
+
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+	switch (key) {
+	case 'i':
+		module_file = arg;
+		break;
+	case 'v':
+		verbose = true;
+		break;
+#ifdef TEST_FINIT_MODULE /* finit_module(2) */
+	case 'V':
+		force_vermagic = true;
+		break;
+	case 'M':
+		force_modversion = true;
+		break;
+	case 'f':
+		force_vermagic = true;
+		force_modversion = true;
+		break;
+#endif
+	case ARGP_KEY_ARG:
+		break;
+	case ARGP_KEY_END:
+		break;
+	default:
+		return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static const struct argp argp = {
+	.options = opts,
+	.parser = parse_arg,
+	.doc = argp_prog_doc,
+};
 
 int main(int argc, char *argv[])
 {
 	int err, fd;
-	char *module_file = NULL;
 
-	if (argc > 1)
-		module_file = argv[1];
+	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
+	if (err) {
+		fprintf(stderr, "argp_parse return %d\n", err);
+		return -err;
+	}
 
 	if (!module_file) {
-		fprintf(stderr, "Usage: %s <ko>\n", argv[0]);
+		fprintf(stderr, "Must specify kernel module file\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -28,7 +91,14 @@ int main(int argc, char *argv[])
 	}
 
 #ifdef TEST_FINIT_MODULE /* finit_module(2) */
-	err = sys_finit_module(fd, "", 0);
+	int flags = 0;
+
+	if (force_vermagic)
+		flags |= MODULE_INIT_IGNORE_VERMAGIC;
+	if (force_modversion)
+		flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+
+	err = sys_finit_module(fd, "", flags);
 	if (err) {
 		fprintf(stderr, "failed insmod %s, %m\n", module_file);
 	}
@@ -52,7 +122,8 @@ int main(int argc, char *argv[])
 	module_image = aligned_alloc(getpagesize(), len);
 # endif
 
-	printf("Ready %s size %ld, image %p\n", module_file, len, module_image);
+	if (verbose)
+		printf("Ready %s size %ld, image %p\n", module_file, len, module_image);
 
 	/* load file to memory */
 	read(fd, module_image, len);
