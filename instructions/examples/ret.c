63a246ad43e0afa4a287c4f9b84e798a2de4c60e
Rong Tao <rongtao@cestc.cn>
2025-04-25 21:52:01 +0800

    instructions/examples: ret.c: Add macro ret_x86_64_v2()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- instructions/examples/ret.c
+++ instructions/examples/ret.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
-#define ret_x86_64(__val)	__asm__("mov $"#__val", %rax; ret");
-
-#define ret_x86_64_TODO(__val) ({		\
-	long __rslt;			\
-	__asm__("mov %1, %%rax \n\t"	\
-		"mov %%rax, %0 \n\t"	\
-		"ret \n\t"		\
-		: "=r"(__rslt)		\
-		: "r"(__val)		\
-		: "%%rax");		\
-	__rslt;				\
+#define ret_x86_64_v1(__val)	__asm__("mov $"#__val", %rax; ret");
+#define ret_x86_64_v2(val) ({				\
+	register unsigned long __ret asm("rax") = val;	\
+	__asm__ volatile("ret" : : "r"(__ret));		\
 	})
+
+#if 1
+#define ret_x86_64_TODO(val) ({			\
+	__asm__ volatile("mov %0, %%rax \n\t"	\
+		"ret"				\
+		: /* no return */		\
+		: "r"(val)			\
+		: "rax", "memory");		\
+	})
+#endif
 
 #define ret_aarch64(__val)	__asm__("mov x0, #"#__val"; ret");
 
 #if defined(__x86_64__)
-#define RETURN(v) ret_x86_64(v)
+#define RETURN(v) ret_x86_64_v2(v)
 #elif defined(__aarch64__)
 #define RETURN(v) ret_aarch64(v)
 #else
@@ -32,12 +35,17 @@ int _omit_frame_pointer func1(void)
 
 int _omit_frame_pointer func2(void)
 {
-	RETURN(2);
+	RETURN(0xffffffff);
 }
 
 int _omit_frame_pointer func3(void)
 {
-	RETURN(3);
+	RETURN(-1);
+}
+
+long _omit_frame_pointer func4(void)
+{
+	RETURN(0xfffffffff);
 }
 
 int main(void)
@@ -45,5 +53,6 @@ int main(void)
 	printf("func1 return %d\n", func1());
 	printf("func2 return %d\n", func2());
 	printf("func3 return %d\n", func3());
+	printf("func4 return %lx\n", func4());
 	return 0;
 }
