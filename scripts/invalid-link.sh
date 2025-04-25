3435f51b4089e4dc684c0896c24684c42559e92a
Rong Tao <rongtao@cestc.cn>
2025-04-25 17:45:01 +0800

    scripts: {pre-commit,invalid-link.sh}: check broken symlinks with find -xtype l
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- scripts/invalid-link.sh
+++ scripts/invalid-link.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 set -e
 
-links=( $(find -path ".git" -a -prune -o -type l -exec echo {} +) )
-for f in ${links[@]}
-do
-	real_f=$(realpath $f 2>/dev/null || true)
-	if [[ ! -f $real_f ]] && [[ ! -d $real_f ]]; then
-		echo "Invalid-link: $f"
-		# Remove broken symlinks
-		# sudo find ./ -type l -xtype l -delete
-	fi
-done
+readonly GIT_TOPDIR=$(git rev-parse --show-toplevel 2>/dev/null || :)
+
+remove_broken_symlinks()
+{
+	sudo find ${GIT_TOPDIR} -type l -xtype l -delete
+}
+
+bad_way()
+{
+	local f
+	local symlinks=( $(find ${GIT_TOPDIR} -path ".git" -a -prune -o -type l -exec echo {} +) )
+	for f in ${symlinks[@]}
+	do
+		real_f=$(realpath $f 2>/dev/null || true)
+		if [[ ! -f $real_f ]] && [[ ! -d $real_f ]]; then
+			echo "Broken-symlink: $f"
+		fi
+	done
+}
+
+good_way()
+{
+	local f
+	local broken_symlinks=( $(find ${GIT_TOPDIR} -xtype l) )
+	for f in ${broken_symlinks[@]}
+	do
+		echo "Broken-symlink: ${f}"
+	done
+}
+
+good_way
