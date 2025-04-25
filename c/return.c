33d0ecbc0deef68d8e5441e4924fe72a54d3dec2
Rong Tao <rongtao@cestc.cn>
2025-04-25 09:21:36 +0800

    c: add return.c
    0000000000400480 <func4>:
      400480:	55                   	push   %rbp
      400481:	48 89 e5             	mov    %rsp,%rbp
      400484:	89 7d fc             	mov    %edi,-0x4(%rbp)
      400487:	31 c0                	xor    %eax,%eax
      400489:	5d                   	pop    %rbp
      40048a:	c3                   	ret
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ c/return.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+
+void func1(void)
+{
+}
+
+void func2(void)
+{
+	return;
+}
+
+int func3(void)
+{
+	return false;
+}
+
+int func4(int i)
+{
+	return false;
+}
+
+int main(void)
+{
+	return 0;
+}
