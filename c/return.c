0a74065fe7147e0a757feef0f1484f2bc9ea0fb8
Rong Tao <rongtao@cestc.cn>
2025-04-25 12:26:58 +0800

    c/return.c: return -1011
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/return.c
+++ c/return.c
@@ -33,13 +33,26 @@ unsigned long func6(void)
 	return false;
 }
 
+int func7(void)
+{
+	return -1011;
+}
+
+unsigned long func8(void)
+{
+	return -1011;
+}
+
 int main(void)
 {
 	while (1) {
+		printf("-------------------------\n");
 		printf("func3 return %d\n", func3());
 		printf("func4 return %d\n", func4(0));
 		printf("func5 return %ld\n", func5());
 		printf("func6 return %ld\n", func6());
+		printf("func7 return %d\n", func7());
+		printf("func8 return %ld\n", func8());
 		sleep(1);
 	}
 	return 0;
