af66c6d86571986eaf8a6ecec0b281e285a969da
Rong Tao <rongtao@cestc.cn>
2025-04-25 10:53:51 +0800

    c/return.c: return 64 bits
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/return.c
+++ c/return.c
@@ -23,11 +23,23 @@ int func4(int i)
 	return false;
 }
 
+unsigned long func5(void)
+{
+	return true;
+}
+
+unsigned long func6(void)
+{
+	return false;
+}
+
 int main(void)
 {
 	while (1) {
 		printf("func3 return %d\n", func3());
 		printf("func4 return %d\n", func4(0));
+		printf("func5 return %ld\n", func5());
+		printf("func6 return %ld\n", func6());
 		sleep(1);
 	}
 	return 0;
