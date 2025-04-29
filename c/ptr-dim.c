e50f30a8aa2d1cdb26d0d70eb1788dac6e770a15
Rong Tao <rongtao@cestc.cn>
2025-04-29 10:19:00 +0800

    c: ptr-dim.c: pass 2-dim ptr
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/ptr-dim.c
+++ c/ptr-dim.c
@@ -24,8 +24,16 @@ void test_struct_ptr(void)
 	memset(test, 'A', 1024);
 }
 
+void pass_ptr_ptr(struct test_struct **p)
+{
+	struct test_struct *t = *p;
+	t->i = 1;
+}
+
 int main(void)
 {
-
+	struct test_struct orig = { .i = 10, };
+	struct test_struct *pt = &orig;
+	pass_ptr_ptr(&pt);
 	return 0;
 }
