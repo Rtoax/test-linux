4d677c87ab1e9eef8e5b4e5ae3202241db11693b
Rong Tao <rongtao@cestc.cn>
2025-04-30 14:10:50 +0800

    rcu/modules: stall.c: Fix wrong timeout value
    Fix: commit 333e81798839 ("rcu/modules: Add stall.c")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- synchronization/rcu/modules/stall.c
+++ synchronization/rcu/modules/stall.c
@@ -1,4 +1,5 @@
 #include <linux/module.h>
+#include <linux/printk.h>
 #include <linux/delay.h>
 #include <linux/rcupdate.h>
 
@@ -6,9 +7,10 @@ static int rcu_cpu_stall_timeout = CONFIG_RCU_CPU_STALL_TIMEOUT + 1;
 
 static int __init test_init(void)
 {
+	printk(KERN_INFO "rcu_cpu_stall_timeout %d s\n", rcu_cpu_stall_timeout);
 	rcu_read_lock();
-	/* busy delay 20s */
-	mdelay(rcu_cpu_stall_timeout);
+	/* busy delay */
+	mdelay(rcu_cpu_stall_timeout * 1000);
 	rcu_read_unlock();
 	return 0;
 }
