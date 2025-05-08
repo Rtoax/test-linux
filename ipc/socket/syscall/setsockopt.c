aab64677bca1da7c04682d093c4fa97c39222d56
Rong Tao <rongtao@cestc.cn>
2025-05-08 14:17:30 +0800

    setsockopt(2): SO_SNDBUF and SO_RCVBUF must pass &size not &buff
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- ipc/socket/syscall/setsockopt.c
+++ ipc/socket/syscall/setsockopt.c
@@ -15,11 +15,13 @@ int main(int argc, const char *argv[])
 	const char *ip = "127.0.0.1";
 	int sock, option;
 	int port = 1234;
-	char recvbuf[BUFFER_SIZE], sendbuf[BUFFER_SIZE];
 	struct sockaddr_in address, client;
 	socklen_t len, optlen;
 	int ret, connfd;
-
+	char buffer[BUFFER_SIZE];
+	char client_ip[INET_ADDRSTRLEN];
+	int recvbuf_sz, sendbuf_sz;
+	int err;
 
 	bzero(&address, sizeof(address));
 
@@ -30,20 +32,22 @@ int main(int argc, const char *argv[])
 	sock = socket(PF_INET, SOCK_STREAM, 0);
 	assert(sock >= 0);
 
-	len = sizeof(recvbuf);
-
 	option = 1;
 	optlen = sizeof(option);
 	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &option, optlen);
 
-	setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &recvbuf, sizeof(recvbuf));
-	printf("the tcp receive buffer set to %p\n", &recvbuf);
-	getsockopt(sock, SOL_SOCKET, SO_RCVBUF, &recvbuf, (socklen_t *)&len);
+	recvbuf_sz = 1024 * 1024;
+	err = setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &recvbuf_sz, sizeof(recvbuf_sz));
+	if (err)
+		perror("setsockopt SO_RCVBUF");
+	getsockopt(sock, SOL_SOCKET, SO_RCVBUF, &len, &optlen);
 	printf("the tcp receive buffer size after setting is %d\n", len);
 
-	setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &sendbuf, sizeof(sendbuf));
-	printf("the tcp send buffer set to %p\n", &sendbuf);
-	getsockopt(sock, SOL_SOCKET, SO_SNDBUF, &sendbuf, (socklen_t *)&len);
+	sendbuf_sz = 1024 * 1024;
+	err = setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &sendbuf_sz, sizeof(sendbuf_sz));
+	if (err)
+		perror("setsockopt SO_SNDBUF");
+	getsockopt(sock, SOL_SOCKET, SO_SNDBUF, &len, &optlen);
 	printf("the tcp send buffer size after setting is %d\n", len);
 
 	ret = bind(sock, (struct sockaddr *)&address, sizeof(address));
@@ -56,16 +60,16 @@ int main(int argc, const char *argv[])
 	connfd = accept(sock, (struct sockaddr *)&client, &len);
 	if (connfd < 0) {
 		printf("errno is:  %d, errstr:%s\n", errno, strerror(errno));
-	} else {
-		char buffer[BUFFER_SIZE];
-		memset(buffer, '\0', BUFFER_SIZE);
-		while (recv(connfd, buffer, BUFFER_SIZE, 0) > 0);
-		close(connfd);
-		char client_ip[INET_ADDRSTRLEN];
-		inet_ntop(AF_INET, (void *)&client.sin_addr, client_ip, (socklen_t )INET_ADDRSTRLEN);
-		printf("Recv data from %s, data content is:%s\n", client_ip, buffer);
+		goto exit;
 	}
 
+	memset(buffer, '\0', BUFFER_SIZE);
+	while (recv(connfd, buffer, BUFFER_SIZE, 0) > 0);
+	close(connfd);
+	inet_ntop(AF_INET, (void *)&client.sin_addr, client_ip, (socklen_t)INET_ADDRSTRLEN);
+	printf("Recv data from %s, data content is:%s\n", client_ip, buffer);
+
+exit:
 	close(sock);
 	return 0;
 }
