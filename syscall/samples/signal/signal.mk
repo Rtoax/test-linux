36cbbcbb88fee52f23f52c65a0b122aac41173c3
Rong Tao <rongtao@cestc.cn>
2025-05-15 19:28:46 +0800

    glibc/signal: only SIGRTMAX use SIGRT_CFLAGS
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/signal/signal.mk
+++ syscall/samples/signal/signal.mk
@@ -1,12 +1,14 @@
 # SPDX-License-Identifier: GPL-3.0
 
-SIGNAL_PROBE := '\#include <stdio.h>\n'
-SIGNAL_PROBE += '\#include <signal.h>\n'
-SIGNAL_PROBE += 'int main(void) {\n'
-SIGNAL_PROBE += '	printf("-DCONST_SIGRTMIN=%d -DCONST_SIGRTMAX=%d\\n", SIGRTMIN, SIGRTMAX);\n'
-SIGNAL_PROBE += '}'
+SIGRT_PROBE := '\#include <stdio.h>\n'
+SIGRT_PROBE += '\#include <signal.h>\n'
+SIGRT_PROBE += 'int main(void) {\n'
+SIGRT_PROBE += '	printf("-DCONST_SIGRTMIN=%d -DCONST_SIGRTMAX=%d\\n", SIGRTMIN, SIGRTMAX);\n'
+SIGRT_PROBE += '}'
 
-SIGNAL_CFLAGS := $(shell printf '%b\n' $(SIGNAL_PROBE) | \
+SIGRT_CFLAGS := $(shell printf '%b\n' $(SIGRT_PROBE) | \
 	$(CC) -xc -Wall - -o sig.out 2>&1 && ./sig.out && rm -f sig.out)
 
-$(info SIGNAL_CFLAGS = ${SIGNAL_CFLAGS})
+ifdef DEBUG
+  $(info SIGRT_CFLAGS = ${SIGRT_CFLAGS})
+endif
