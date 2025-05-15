2bc1ba7a47c2652282520ef23e68f642ab3b050c
Rong Tao <rongtao@cestc.cn>
2025-05-15 13:55:29 +0800

    cpp: format.cpp: Check std 202002L
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cpp/format.cpp
+++ cpp/format.cpp
@@ -1,6 +1,10 @@
 #include <format>
 #include <iostream>
 
+#if __cplusplus < 202002L
+#error "only -std >= c++20 support std::format"
+#endif
+
 std::string format(void)
 {
 	return std::format("$${}_{}", "Hello", "World");
