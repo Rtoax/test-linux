8120b8154a1dfec0cb17ab9f9cba5af12c38d124
Rong Tao <rongtao@cestc.cn>
2025-05-19 09:59:07 +0800

    initrd: initrd.img.sh: update comment
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- init/initrd/initrd.img.sh
+++ init/initrd/initrd.img.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
+set -e
 
-
-# RHEL 6.2 版本
-# RHEL 6.0 - 6.2 都采用与RHEL 5.0 相同的格式进行打包，但从6.2版本开始，改用LZMA 进行压缩
+# RHEL 6.0 - <6.2 same as RHEL 5.0
+# RHEL >=6.2: LZMA
 # initrd.img: LZMA compressed data, streamed
 initrd_img_xz()
 {
-	# 解压
+	# decompress
 	xz -dc initrd.img | cpio -id
 
-	# 压缩
+	# compress
 	find . | cpio -c -o | xz -9 --format=lzma > initrd.img
 }
 
-# RHEL 5.0 版本
+# RHEL 5.0
 initrd_img_gunzip()
 {
 	mv initrd.img initrd.img.gz
