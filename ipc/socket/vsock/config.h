982d15b299f2261b91474a2ff4a2ba45a4486f78
Rong Tao <rongtao@cestc.cn>
2025-05-20 18:28:11 +0800

    vsock: use VMADDR_CID_ANY when binding
    see vsock(7) manual
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- ipc/socket/vsock/config.h
+++ ipc/socket/vsock/config.h
@@ -2,7 +2,7 @@
 #include <sys/socket.h>
 #include <linux/vm_sockets.h>
 
-#define PORT	6000
-#define CID_HOST	VMADDR_CID_HOST
-#define CID_GUEST	VMADDR_CID_ANY
+#define PORT	6789
+#define CID_HOST	VMADDR_CID_ANY
+#define CID_GUEST	VMADDR_CID_HOST
 
