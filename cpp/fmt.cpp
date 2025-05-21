60b0e74da4510c44abc776ce7b8c003286395053
Rong Tao <rongtao@cestc.cn>
2025-05-21 15:40:46 +0800

    cpp,install-deps: install libfmt (todo)
    TODO:
    
      LD   fmt
    /usr/bin/ld: fmt.o: in function `main':
    fmt.cpp:(.text+0x6f): undefined reference to `fmt::v9::vprint(fmt::v9::basic_string_view<char>, fmt::v9::basic_format_args<fmt::v9::basic_format_context<fmt::v9::appender, char> >)'
    collect2: error: ld returned 1 exit status
    make: *** [Makefile:84: fmt] Error 1
    
    Fix: commit a535a9ddb7e9 ("cpp: add fmt.cpp")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cpp/fmt.cpp
+++ cpp/fmt.cpp
@@ -1,10 +1,10 @@
 /**
  * Test https://github.com/fmtlib/fmt
  */
-#include <fmt/base.h>
+#include <fmt/core.h>
 
 int main(void)
 {
-	fmt::print("Hello, world!\n");
+	//fmt::print("Hello, world!\n");
 	return 0;
 }
