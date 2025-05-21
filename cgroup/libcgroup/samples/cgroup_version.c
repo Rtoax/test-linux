b9b724e781d70a1c9968dd52e49fdde8e4fbac2b
Rong Tao <rongtao@cestc.cn>
2025-05-21 10:55:23 +0800

    libcgroup: fix cgroup_version.c compile error
      CC  cgroup_version.c to cgroup_version.o
    cgroup_version.c: In function ‘main’:
    cgroup_version.c:7:19: error: implicit declaration of function ‘cgroup_version’ [-Werror=implicit-function-declaration]
        7 |         version = cgroup_version();
          |                   ^~~~~~~~~~~~~~
    cgroup_version.c:7:17: error: assignment to ‘const struct cgroup_library_version *’ from ‘int’ makes pointer from integer without a cast [-Werror=int-conversion]
        7 |         version = cgroup_version();
          |                 ^
    cgroup_version.c:8:37: error: invalid use of undefined type ‘const struct cgroup_library_version’
        8 |         printf("%d.%d.%d\n", version->major, version->minor, version->release);
          |                                     ^~
    cgroup_version.c:8:53: error: invalid use of undefined type ‘const struct cgroup_library_version’
        8 |         printf("%d.%d.%d\n", version->major, version->minor, version->release);
          |                                                     ^~
    cgroup_version.c:8:69: error: invalid use of undefined type ‘const struct cgroup_library_version’
        8 |         printf("%d.%d.%d\n", version->major, version->minor, version->release);
          |                                                                     ^~
    cc1: all warnings being treated as errors
    make: *** [Makefile:24: cgroup_version.o] Error 1
    
    Link: https://github.com/libcgroup/libcgroup.git
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cgroup/libcgroup/samples/cgroup_version.c
+++ cgroup/libcgroup/samples/cgroup_version.c
@@ -3,8 +3,16 @@
 
 int main(int argc, char **argv)
 {
+/**
+ * libcgroup commit a95532a8ea22 ("libcgroup.so: Add a function to get the library version")
+ * add CGROUP_VER_MAJOR and cgroup_version().
+ */
+#ifdef CGROUP_VER_MAJOR
 	const struct cgroup_library_version *version;
 	version = cgroup_version();
 	printf("%d.%d.%d\n", version->major, version->minor, version->release);
+#else
+# pragma message "libcgroup not support cgroup_version() yet"
+#endif
 	return 0;
 }
