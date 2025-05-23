a65310ec4613b96886a8971cb20a1b5a311f4fce
Rong Tao <rongtao@cestc.cn>
2025-05-23 14:54:21 +0800

    kata-containers: ctr: failed to create shim task: Failed to Check if grpc server is working
    sudo ctr run --runtime io.containerd.kata.v2 --rm -t quay.io/fedora/fedora:42 test-kata uname -r
    ctr: failed to create shim task: Failed to Check if grpc server is working: rpc error: code = DeadlineExceeded desc = timed out connecting to vsock 1115048379:1024
    
    Link: https://github.com/kata-containers/kata-containers/blob/main/docs/install/container-manager/containerd/containerd-install.md
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cloud-native/kata-containers/deploy.sh
+++ cloud-native/kata-containers/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# refs:
+# - https://github.com/kata-containers/kata-containers/blob/main/docs/install/container-manager/containerd/containerd-install.md
 set -ex
 
 sudo dnf install -y containerd cri-tools kata-containers qemu-kvm
@@ -9,15 +11,15 @@ containerd config default | tee containerd-config.toml
 
 # Modify containerd-config.toml to add runtime io.containerd.kata.v2
 #
-# 1. apply this patch:
+# 1. add kata runtime
 #
-#        [plugins.'io.containerd.cri.v1.runtime'.containerd.runtimes]
-# +
-# +        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.kata]
-# +          runtime_type = "io.containerd.kata.v2"
-# +
-#          [plugins.'io.containerd.cri.v1.runtime'.containerd.runtimes.runc]
-#            runtime_type = 'io.containerd.runc.v2'
+# [plugins]
+#   [plugins."io.containerd.grpc.v1.cri"]
+#     [plugins."io.containerd.grpc.v1.cri".containerd]
+#       default_runtime_name = "kata"
+#       [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+#         [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.kata]
+#           runtime_type = "io.containerd.kata.v2"
 #
 # TODO: modify with sed.
 
