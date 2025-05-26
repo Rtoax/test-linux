0b1a7575ce63665e51998bdf8c3b5f34b028ef12
Rong Tao <rongtao@cestc.cn>
2025-05-26 18:35:52 +0800

    subdir-footer.mk: log to TL_LOG
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/subdir-footer.mk
+++ template/subdir-footer.mk
@@ -1,18 +1,29 @@
 # SPDX-License-Identifier: GPL-3.0
 Q ?= @
 
-define make_sub_dir
-	@make -C ${1} || echo "Failed ${1}" | tee --append ${TL_FAILED_LOG}
+define make_sub_dir_build
+	$(call tl_log,"Build ${1}")
+	@make -C ${1} || echo "Failed build ${1}" | tee --append ${TL_FAILED_LOG}
+endef
+
+define make_sub_dir_test
+	$(call tl_log,"Test  ${1}")
+	@make -C ${1} test || echo "Failed test  ${1}" | tee --append ${TL_FAILED_LOG}
+endef
+
+define make_sub_dir_clean
+	$(call tl_log,"Clean ${1}")
+	@make -C ${1} clean || echo "Failed clean ${1}" | tee --append ${TL_FAILED_LOG}
 endef
 
 $(sub-dir-build):
 	@echo -e "\033[1;32m[build] $(patsubst %.build,%,$(@))\033[m"
-	${Q}$(call make_sub_dir,$(@:.build=))
+	${Q}$(call make_sub_dir_build,$(@:.build=))
 
 $(sub-dir-test):
 	@echo -e "\033[1;32m[test] $(patsubst %.test,%,$(@))\033[m"
-	${Q}make -C $(@:.test=) test
+	${Q}$(call make_sub_dir_test,$(@:.test=))
 
 $(sub-dir-clean):
 	@echo -e "\033[1;32m[clean] $(patsubst %.clean,%,$(@))\033[m"
-	${Q}make -C $(@:.clean=) clean
+	${Q}$(call make_sub_dir_clean,$(@:.clean=))
