45a6ee6d4b2f04b1a31a1cfe5ae3d7b652e4710c
Rong Tao <rongtao@cestc.cn>
2025-05-27 16:10:14 +0800

    qcow2_helpers.c: display all qcow2 header fields
        $ ./qcow2 -q a.qcow2
        version 3
        virtual size 1 GiB (1073741824 bytes)
        backing_file_offset: 0x0
        backing_file_size: 0x0
        cluster_bits: 0x10
        crypt_method: 0x0
        l1_size: 2
        l1_table_offset: 0x30000
        refcount_table_offset: 0x10000
        refcount_table_clusters: 1
        nb_snapshots: 0
        snapshots_offset: 0x0
        incompatible_features: 0x0
        compatible_features: 0x0
        autoclear_features: 0x0
        refcount_order: 4
        header_length: 112
        compression_type: 0
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- qemu/qcow/qcow2_helpers.c
+++ qemu/qcow/qcow2_helpers.c
@@ -23,5 +23,30 @@ void display_qcowhdr(QCowHeader *hdr)
 	printf("version %d\n", bswap_32(hdr->version));
 	printf("virtual size %ld GiB (%ld bytes)\n",
 		bswap_64(hdr->size) / 1024 / 1024 / 1024, bswap_64(hdr->size));
-	/* TODO: display more */
+
+	printf("backing_file_offset: 0x%lx\n", bswap_64(hdr->backing_file_offset));
+	printf("backing_file_size: 0x%x\n", bswap_32(hdr->backing_file_size));
+
+	printf("cluster_bits: 0x%x\n", bswap_32(hdr->cluster_bits));
+
+	printf("crypt_method: 0x%x\n", bswap_32(hdr->crypt_method));
+
+	printf("l1_size: %d\n", bswap_32(hdr->l1_size));
+	printf("l1_table_offset: 0x%lx\n", bswap_64(hdr->l1_table_offset));
+
+	printf("refcount_table_offset: 0x%lx\n", bswap_64(hdr->refcount_table_offset));
+	printf("refcount_table_clusters: %d\n", bswap_32(hdr->refcount_table_clusters));
+
+	printf("nb_snapshots: %d\n", bswap_32(hdr->nb_snapshots));
+	printf("snapshots_offset: 0x%lx\n", bswap_64(hdr->snapshots_offset));
+
+	printf("incompatible_features: 0x%lx\n", bswap_64(hdr->incompatible_features));
+	printf("compatible_features: 0x%lx\n", bswap_64(hdr->compatible_features));
+
+	printf("autoclear_features: 0x%lx\n", bswap_64(hdr->autoclear_features));
+
+	printf("refcount_order: %d\n", bswap_32(hdr->refcount_order));
+	printf("header_length: %d\n", bswap_32(hdr->header_length));
+
+	printf("compression_type: %d\n", hdr->compression_type);
 }
