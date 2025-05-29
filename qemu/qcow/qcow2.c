458a332043168fd4793b4ad40ed0dc45f6070329
Rong Tao <rongtao@cestc.cn>
2025-05-29 12:23:22 +0800

    fix compile errors on fedora41
      CC  qcow2.c to qcow2.o
    qcow2.c: In function ‘parse_arg’:
    qcow2.c:30:27: error: ‘true’ undeclared (first use in this function)
       30 |                 verbose = true;
          |                           ^~~~
    qcow2.c:9:1: note: ‘true’ is defined in header ‘<stdbool.h>’; this is probably fixable by adding ‘#include <stdbool.h>’
        8 | #include "qcow2_helpers.h"
      +++ |+#include <stdbool.h>
        9 |
    qcow2.c:30:27: note: each undeclared identifier is reported only once for each function it appears in
       30 |                 verbose = true;
          |                           ^~~~
    make: *** [Makefile:29: qcow2.o] Error 1
    
      CC  F_SETLKW.c to F_SETLKW.o
    F_SETLKW.c: In function ‘main’:
    F_SETLKW.c:23:24: error: ‘true’ undeclared (first use in this function)
       23 |         tl_fwrlock(fd, true);
          |                        ^~~~
    F_SETLKW.c:8:1: note: ‘true’ is defined in header ‘<stdbool.h>’; this is probably fixable by adding ‘#include <stdbool.h>’
        7 | #include "fcntl_helpers.h"
      +++ |+#include <stdbool.h>
        8 |
    F_SETLKW.c:23:24: note: each undeclared identifier is reported only once for each function it appears in
       23 |         tl_fwrlock(fd, true);
          |                        ^~~~
    make: *** [Makefile:58: F_SETLKW.o] Error 1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- qemu/qcow/qcow2.c
+++ qemu/qcow/qcow2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <argp.h>
 #include <errno.h>
--- syscall/samples/fcntl/F_SETLKW.c
+++ syscall/samples/fcntl/F_SETLKW.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
 
