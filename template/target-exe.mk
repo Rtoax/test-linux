45032502a2ff86f2a42e8175bd9273699e359cbb
Rong Tao <rongtao@cestc.cn>
2025-05-30 17:20:10 +0800

    Add template/target-exe.mk
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ template/target-exe.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-3.0
+CC ?= gcc
+Q ?= @
+
+%.o: %.c
+	@echo -e "  CC  \033[1m$(<)\033[m to \033[1m$(@)\033[m"
+	${Q}LD_LIBRARY_PATH=$(shell pwd) $(CC) -o $(@) -c $(<) $(CFLAGS) $(CFLAGS_$(*))
+
+$(TARGETS): %:
+	@echo -e "  LD  \033[1;32m$(@)\033[m"
+	${Q}LD_LIBRARY_PATH=$(shell pwd) $(CC) -o $(@) $(^) $(LDFLAGS) $(LDFLAGS_$(*))
