b9da768affbda2e9af9ef885d3e146ff51961842
Rong Tao <rongtao@cestc.cn>
2025-05-30 17:47:36 +0800

    template/subdir-pure.mk: target build,test,clean
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/subdir-pure.mk
+++ template/subdir-pure.mk
@@ -6,8 +6,22 @@ sub-dir ?=
 
 include ${SUBDIR_DIR}/../tlbuild.mk
 include ${SUBDIR_DIR}/subdir-header.mk
+
 .PHONY: build
-build: $(TARGETS) $(sub-dir-build)
+build: $(TARGETS) $(TARGETS_LIBA) $(TARGETS_LIBSO) $(sub-dir-build)
+	@echo -e " \033[1;33m Build done \033[m"
+
+.PHONY: test
+test: $(TARGETS) $(TARGETS_LIBA) $(TARGETS_LIBSO) $(sub-dir-test)
+	@echo -e " \033[1;33m Test done \033[m"
+
 .PHONY: clean
 clean: $(sub-dir-clean)
+	@echo -e "  CLEAN  \033[1;32m${TARGETS} ${TARGETS_LIBA} ${TARGETS_LIBSO}\033[m"
+	${Q}rm -rf ${TARGETS} ${TARGETS_LIBA} ${TARGETS_LIBSO} *.o *.d
+
+include ${SUBDIR_DIR}/target-exe.mk
+include ${SUBDIR_DIR}/target-liba.mk
+include ${SUBDIR_DIR}/target-libso.mk
+
 include ${SUBDIR_DIR}/subdir-footer.mk
