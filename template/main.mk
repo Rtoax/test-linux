32ce08780d170e378a25af6edca70b0e20cbcd33
Rong Tao <rtoax@foxmail.com>
2025-06-01 11:19:48 +0800

    cpp: apply main.mk
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- template/main.mk
+++ template/main.mk
@@ -4,21 +4,42 @@ SUBDIR_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
 sub-dir ?=
 
+ALL_TARGETS := $(TARGETS) $(TARGETS_LIBA) $(TARGETS_LIBSO)
+ALL_TARGETS += $(TARGETS_CPP)
+ALL_TARGETS += $(sub-dir-build)
+ALL_TARGETS += $(TARGETS_EXTRA)
+
+ifdef DEBUG
+  $(info Compile with DEBUG=1)
+  CFLAGS += -DDEBUG -O0 -g -ggdb
+  CXXFLAGS += -DDEBUG -O0 -g -ggdb
+endif
+ifdef ERROR
+  $(info Compile with ERROR=1)
+  CFLAGS += -DERROR=1
+  CXXFLAGS += -DERROR=1
+endif
+ifdef M32
+  $(info Compile 32bit ELF)
+  CXXFLAGS += -m32 -DM32=1
+  CXXFLAGS += -m32 -DM32=1
+endif
+
 include ${SUBDIR_DIR}/../tlbuild.mk
 include ${SUBDIR_DIR}/subdir-header.mk
 
 .PHONY: build
-build: $(TARGETS) $(TARGETS_LIBA) $(TARGETS_LIBSO) $(sub-dir-build) $(TARGETS_EXTRA)
+build: $(ALL_TARGETS) $(sub-dir-build)
 	@echo -e " \033[1;33m Build done \033[m"
 
 .PHONY: test
-test: $(TARGETS) $(TARGETS_LIBA) $(TARGETS_LIBSO) $(sub-dir-test)
+test: $(ALL_TARGETS) $(sub-dir-test)
 	@echo -e " \033[1;33m Test done \033[m"
 
 .PHONY: clean
 clean: $(sub-dir-clean)
-	@echo -e "  CLEAN  \033[1;32m${TARGETS} ${TARGETS_LIBA} ${TARGETS_LIBSO} ${TARGETS_EXTRA}\033[m"
-	${Q}rm -rf ${TARGETS} ${TARGETS_LIBA} ${TARGETS_LIBSO} ${TARGETS_EXTRA} *.o *.d
+	@echo -e "  CLEAN  \033[1;32m${ALL_TARGETS}\033[m"
+	${Q}rm -rf ${ALL_TARGETS} *.o *.opp *.d
 
 include ${SUBDIR_DIR}/target-exe.mk
 include ${SUBDIR_DIR}/target-liba.mk
--- template/target-exe.mk
+++ template/target-exe.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0
 CC ?= gcc
+CXX ?= g++
 Q ?= @
 
 %.o: %.c
@@ -9,3 +10,15 @@ Q ?= @
 $(TARGETS): %:
 	@echo -e "  LD  \033[1;32m$(@)\033[m"
 	${Q}LD_LIBRARY_PATH=$(shell pwd) $(CC) -o $(@) $(^) $(LDFLAGS) $(LDFLAGS_$(*)) $(CFLAGS) $(CFLAGS_$(*))
+
+%.opp: %.cpp
+	@echo -e "  CXX  \033[1m$(<)\033[m to \033[1m$(@)\033[m"
+	${Q}$(CXX) -o $(@) -c $(<) $(CXXFLAGS) $(CXXFLAGS_$(*))
+
+%.cpp.s: %.cpp
+	@echo -e "  CXX S  \033[1m$(<)\033[m to \033[1m$(@)\033[m"
+	${Q}$(CXX) -S -o $(@) -c $(<) $(CXXFLAGS) $(CXXFLAGS_$(*))
+
+${TARGETS_CPP}: %:
+	@echo -e "  LD   \033[1;32m$(@)\033[m"
+	${Q}$(CXX) -o $(@) $(^) $(CXXFLAGS) $(CXXFLAGS_$(*)) $(LDXXFLAGS) $(LDXXFLAGS_$(*))
