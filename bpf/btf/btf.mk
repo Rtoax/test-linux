a69a5627292d422a0ac8cecb0854dc23d29f6f3b
Rong Tao <rongtao@cestc.cn>
2025-06-03 19:38:06 +0800

    btf.mk: vmlinux_has_{struct,ksym}: check and generate vmlinux.h first
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/btf/btf.mk
+++ bpf/btf/btf.mk
@@ -1,10 +1,15 @@
 # SPDX-License-Identifier: GPL-3.0
 
 VMLINUX_H ?= vmlinux.h
+BPFTOOL ?= bpftool
 
 # $1 - struct name, like task_struct
 define vmlinux_has_struct
-$(shell if [[ "$$(grep -wo '^struct ${1} {' ${VMLINUX_H})" ]]; then \
+$(shell if [[ ! -e ${VMLINUX_H} ]]; then \
+		mkdir -p $$(dirname ${VMLINUX_H}); \
+		${BPFTOOL} btf dump file /sys/kernel/btf/vmlinux format c > ${VMLINUX_H}; \
+	fi; \
+	if [[ "$$(grep -wo '^struct ${1} {' ${VMLINUX_H})" ]]; then \
 		echo y; \
 	else echo n; \
 	fi)
@@ -12,7 +17,11 @@ endef
 
 # $1 - symbol name, like bpf_task_from_pid, task_struct.
 define vmlinux_has_sym
-$(shell if [[ "$$(grep -wo '${1}' ${VMLINUX_H})" ]]; then \
+$(shell if [[ ! -e ${VMLINUX_H} ]]; then \
+		mkdir -p $$(dirname ${VMLINUX_H}); \
+		${BPFTOOL} btf dump file /sys/kernel/btf/vmlinux format c > ${VMLINUX_H}; \
+	fi; \
+	if [[ "$$(grep -wo '${1}' ${VMLINUX_H})" ]]; then \
 		echo y; \
 	else echo n; \
 	fi)
