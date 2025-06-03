f724804053d0557219babe3683983b02bcb50969
Rong Tao <rongtao@cestc.cn>
2025-06-03 11:31:11 +0800

    libbpf: getcwd: try bpf_snprintf()
        $ sudo ./tracepoint
        libbpf: BTF loading error: -28
        libbpf: Error loading .BTF into kernel: -28. BTF is optional, ignoring.
        libbpf: prog 'tracepoint__syscalls__sys_exit_execve': BPF program load failed: Permission denied
        libbpf: prog 'tracepoint__syscalls__sys_exit_execve': failed to load: -13
        libbpf: failed to load object 'map_hash_bpf'
        libbpf: failed to load BPF skeleton 'map_hash_bpf': -13
        Failed to load BPF object
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/libbpf/bpf_helpers.h
+++ bpf/libbpf/bpf_helpers.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <bpf/bpf_helpers.h>
 
+#define MAX_D_ENTRIES	33
 #define bpf_getcwd(buf, buf_len) __bpf_getcwd(buf, buf_len)
 
 /**
@@ -9,13 +10,14 @@
  */
 static __always_inline int __bpf_getcwd(char *buf, u32 buf_len)
 {
+	struct task_struct __attribute__((unused)) *curtask;
+	curtask = (void *)bpf_get_current_task();
 #if defined(SUPPORT_BPF_D_PATH)
 # pragma message "use bpf_d_path(), please very strict use"
 /**
  * see linux::kernel/trace/bpf_trace.c btf_allowlist_d_path, only few fentry
  * could use bpf_d_path().
  */
-	struct task_struct *curtask = (void *)bpf_get_current_task();
 	if (curtask) {
 		struct fs_struct *fs;
 		struct path pwd;
@@ -25,7 +27,34 @@ static __always_inline int __bpf_getcwd(char *buf, u32 buf_len)
 			bpf_d_path(&pwd, buf, buf_len);
 		}
 	}
+#elif defined(SUPPORT_BPF_SNPRINTF)
+# pragma message "support bpf_snprintf()"
+/**
+ * linux 7b15523a989b ("bpf: Add a bpf_snprintf helper")
+ * v5.12-rc4-1654-g7b15523a989b
+ */
+# if 0 /* TODO: failed */
+	int i;
+	struct dentry *dentry;
+	char fname[64];
+	if (curtask) {
+		dentry = curtask->fs->pwd.dentry;
+		for (i = 1; i < MAX_D_ENTRIES; i++) {
+			bpf_probe_read_kernel(&fname, sizeof(fname), dentry->d_name.name);
+			bpf_snprintf(buf, buf_len, "/%s", fname, sizeof(fname));
+
+			if (dentry == dentry->d_parent)
+				break;
+
+			dentry = dentry->d_parent;
+		}
+	}
+# else
+	unsigned long long args[2] = {10,11};
+	bpf_snprintf(buf, buf_len, "<%ld,%ld>", &args, sizeof(args));
+# endif
 #else
+# pragma message "cwd=N/A"
 	buf[0] = 'N';
 	buf[1] = '/';
 	buf[2] = 'A';
