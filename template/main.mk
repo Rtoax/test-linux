c5052d74548f4d1d2930616c6c62ab379c241ef7
Rong Tao <rongtao@cestc.cn>
2025-06-04 09:20:34 +0800

    main.mk: Rename TARGETS_EXTRA to TARGETS_POST
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/main.mk
+++ template/main.mk
@@ -1,20 +1,20 @@
 # SPDX-License-Identifier: GPL-3.0
 
-SUBDIR_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+TEMPLATE_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
 sub-dir ?=
 
 ALL_TARGETS := $(TARGETS_PRE)
+ALL_TARGETS += $(OUTPUT)
 ALL_TARGETS += $(TARGETS) $(TARGETS_LIBA) $(TARGETS_LIBSO)
 ALL_TARGETS += $(TARGETS_CPP)
-ALL_TARGETS += $(sub-dir-build)
-ALL_TARGETS += $(TARGETS_EXTRA)
-ALL_TARGETS += $(OUTPUT)
+ALL_TARGETS += $(TARGETS_POST)
 
 ifdef DEBUG
   $(info Compile with DEBUG=1)
   CFLAGS += -DDEBUG -O0 -g -ggdb
   CXXFLAGS += -DDEBUG -O0 -g -ggdb
+  MAKEFLAGS += DEBUG=1
 endif
 
 ifdef ERROR
@@ -32,8 +32,8 @@ ifdef M32
   MAKEFLAGS += M32=1
 endif
 
-include ${SUBDIR_DIR}/../tlbuild.mk
-include ${SUBDIR_DIR}/subdir-header.mk
+include ${TEMPLATE_DIR}/../tlbuild.mk
+include ${TEMPLATE_DIR}/subdir-header.mk
 
 .PHONY: build
 build: $(ALL_TARGETS) $(sub-dir-build)
@@ -48,11 +48,11 @@ clean: $(sub-dir-clean) $(TARGETS_CLEAN)
 	@echo -e "  CLEAN  \033[1;32m${ALL_TARGETS}\033[m"
 	${Q}rm -rf ${ALL_TARGETS} *.o *.opp *.d
 
-include ${SUBDIR_DIR}/target-exe.mk
-include ${SUBDIR_DIR}/target-liba.mk
-include ${SUBDIR_DIR}/target-libso.mk
+include ${TEMPLATE_DIR}/target-exe.mk
+include ${TEMPLATE_DIR}/target-liba.mk
+include ${TEMPLATE_DIR}/target-libso.mk
 ifneq (${OUTPUT},)
-  include ${SUBDIR_DIR}/target-output.mk
+  include ${TEMPLATE_DIR}/target-output.mk
 endif
 
-include ${SUBDIR_DIR}/subdir-footer.mk
+include ${TEMPLATE_DIR}/subdir-footer.mk
