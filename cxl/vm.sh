b81ed59f530d2849194911fd2dd70fc7d2fcd023
Rong Tao <rongtao@cestc.cn>
2025-06-11 13:49:59 +0800

    cxl: qemu: whole test of pmem CXL.mem
    Finish commit [1]
    
        sh-5.2# ls -al /dev/cxl/mem0
        crw------- 1 root root 238, 0 Jun 11 05:48 /dev/cxl/mem0
    
        sh-5.2# cxl list
        [
          {
            "memdev":"mem0",
            "pmem_size":268435456,
            "serial":1,
            "host":"0000:0d:00.0",
            "firmware_version":"BWFW VERSION 00"
          }
        ]
    
    Finish: commit e4207aa3daf1 ("rootfs: qemu.sh: Test CXL pmem") [1]
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cxl/vm.sh
+++ cxl/vm.sh
@@ -1,1 +1,13 @@
-../init/rootfs/qemu.sh
+#!/bin/bash
+set -e
+
+sudo dnf install -y cxl-cli
+
+sudo dracut --kver $(uname -r) --no-hostonly --verbose --force \
+	--install 'insmod rmmod modprobe lspci ndctl cxl' \
+	--add-drivers 'cxl_acpi cxl_core cxl_mem cxl_pci cxl_pmem cxl_pmu cxl_port' \
+	initramfs.img
+
+sudo cp /boot/vmlinuz-$(uname -r) vmlinuz
+
+sudo ../init/rootfs/qemu.sh -k vmlinuz -r initramfs.img --initrd --cxl cxl-pmem --stdio
--- init/initrd/dracut.sh
+++ init/initrd/dracut.sh
@@ -1,10 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
 # newest-repo: https://github.com/dracut-ng/dracut-ng
 # other-repo:
 # - https://github.com/dracutdevs/dracut (older)
 # - https://web.git.kernel.org/pub/scm/boot/dracut/dracut
 
 sudo dracut --kver $(uname -r) --no-hostonly --verbose --force \
-	--install 'insmod rmmod modprobe lspci' \
+	--install 'insmod rmmod modprobe lspci ndctl cxl' \
 	--add-drivers 'cxl_acpi cxl_core cxl_mem cxl_pci cxl_pmem cxl_pmu cxl_port' \
 	initramfs-$(uname -r).img
