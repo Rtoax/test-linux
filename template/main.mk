311e3ed1de123d749fcc46123d787bddc0de074a
Rong Tao <rongtao@cestc.cn>
2025-06-12 17:02:41 +0800

    qemu: apply some main.mk
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/main.mk
+++ template/main.mk
@@ -47,8 +47,9 @@ test: $(build-targets) $(sub-dir-test) $(TARGETS_TEST)
 
 .PHONY: clean
 clean: $(sub-dir-clean) $(TARGETS_CLEAN)
-	@echo -e "  CLEAN  \033[1;32m${build-targets}\033[m"
+	@echo -e "  CLEAN  \033[1;32m${build-targets} ${TARGETS_CLEAN}\033[m"
 	${Q}rm -rf ${build-targets} *.o *.opp *.d
+	@echo -e " \033[1;33m Clean $(shell pwd) done \033[m"
 
 include ${TEMPLATE_DIR}/target-exe.mk
 include ${TEMPLATE_DIR}/target-liba.mk
