2b2a0d949a8d24866bf48d077223eb0c4912d4ff
Rong Tao <rongtao@cestc.cn>
2025-06-14 11:12:27 +0800

    libbpf: BPF_PROG_TYPE_CGROUP_SOCK_ADDR: add more type comment
    [1] <eBPF Mystery: When is IPv4 not IPv4? When it's pretending to be IPv6!>
        https://blog.gripdev.xyz/2025/05/06/ebpf-mystery-when-is-ipv4-not-ipv4-when-its-ipv6/
    
    Link: https://docs.ebpf.io/linux/program-type/BPF_PROG_TYPE_CGROUP_SOCK_ADDR/
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/libbpf/cgroup_sock_addr.bpf.c
+++ bpf/libbpf/cgroup_sock_addr.bpf.c
@@ -15,30 +15,59 @@
 #include <bpf/bpf_endian.h>
 
 
+/**
+ * BPF_CGROUP_INET4_BIND v4.17
+ */
 SEC("cgroup/bind4")
 int bind_v4_prog(struct bpf_sock_addr *ctx)
 {
 	return 1;
 }
 
+/**
+ * BPF_CGROUP_INET6_BIND v4.17
+ */
 SEC("cgroup/bind6")
 int bind_v6_prog(struct bpf_sock_addr *ctx)
 {
 	return 1;
 }
 
+/**
+ * BPF_CGROUP_INET4_CONNECT v4.17
+ */
 SEC("cgroup/connect4")
 int connect4(struct bpf_sock_addr *ctx)
 {
 	return 1;
 }
 
+/**
+ * BPF_CGROUP_INET6_CONNECT v4.17
+ */
+SEC("cgroup/connect6")
+int connect6(struct bpf_sock_addr *ctx)
+{
+	return 1;
+}
+
+/**
+ * BPF_CGROUP_UDP4_SENDMSG and BPF_CGROUP_UDP6_SENDMSG v4.18
+ * BPF_CGROUP_UDP4_RECVMSG and BPF_CGROUP_UDP6_RECVMSG v5.2
+ */
+
+/**
+ * BPF_CGROUP_INET4_GETSOCKNAME v5.8
+ */
 SEC("cgroup/getsockname4")
 int getsockname4(struct bpf_sock_addr *ctx)
 {
 	return 1;
 }
 
+/**
+ * BPF_CGROUP_INET6_GETSOCKNAME v5.8
+ */
 SEC("cgroup/getpeername4")
 int getpeername4(struct bpf_sock_addr *ctx)
 {
