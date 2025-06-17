21535dd38b6ed282bea86601e300c58303305692
Rong Tao <rongtao@cestc.cn>
2025-06-17 18:56:24 +0800

    cxl: dax0.0: use as system-ram
        [root@fedora libpmem]# daxctl online-memory dax0.0
        [ 1900.810405] Fallback order for Node 0: 0 1
        [ 1900.810410] Fallback order for Node 1: 1 0
        [ 1900.810411] Built 2 zonelists, mobility grouping on.  Total pages: 498827
        [ 1900.811474] Policy zone: DMA32
        [ 1900.819045] Demotion targets for Node 0: preferred: 1, fallback: 1
        [ 1900.819780] Demotion targets for Node 1: null
        onlined memory for 1 device
    
        [root@fedora libpmem]# free
                       total        used        free      shared  buff/cache   available
        Mem:         2980512      362400     2428040        4696      379032     2618112
        Swap:              0           0           0
    
        [root@fedora libpmem]# daxctl offline-memory dax0.0
        [ 1960.913347] Fallback order for Node 0: 0
        [ 1960.913351] Fallback order for Node 1: 0
        [ 1960.913352] Built 2 zonelists, mobility grouping on.  Total pages: 466091
        [ 1960.914514] Policy zone: DMA32
        [ 1960.933040] Demotion targets for Node 0: null
        offlined memory for 1 device
    
        [root@fedora libpmem]# free
                       total        used        free      shared  buff/cache   available
        Mem:         1931936      336384     1386284        4696      378952     1595552
        Swap:              0           0           0
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cxl/cxl.sh
+++ cxl/cxl.sh
@@ -17,6 +17,8 @@ sudo cxl create-region --decoder decoder0.0 --size 1024M --type ram --memdevs me
 sudo cxl create-region --decoder decoder0.0 --size 4096M --type ram --memdevs mem0 mem1 mem2 mem3
 sudo daxctl list
 sudo daxctl list -r region0
+sudo daxctl online-memory dax0.0
+sudo daxctl offline-memory dax0.0
 
 sudo cxl enable-region region0
 sudo cxl enable-region all
