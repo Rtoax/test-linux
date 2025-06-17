8c9b9e84b04ddaca2ddbec4da3124092be96c5c8
Rong Tao <rongtao@cestc.cn>
2025-06-17 17:12:14 +0800

    cxl.sh: Use long argument option
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cxl/cxl.sh
+++ cxl/cxl.sh
@@ -2,13 +2,13 @@
 set -e
 
 # create region 0-3
-sudo cxl create-region -d decoder0.0 -s 1024M -t ram -m mem0
-sudo cxl create-region -d decoder0.0 -s 1024M -t ram -m mem1
-sudo cxl create-region -d decoder0.0 -s 1024M -t ram -m mem2
-sudo cxl create-region -d decoder0.0 -s 1024M -t ram -m mem3
+sudo cxl create-region --decoder decoder0.0 --size 1024M --type ram --memdevs mem0
+sudo cxl create-region --decoder decoder0.0 --size 1024M --type ram --memdevs mem1
+sudo cxl create-region --decoder decoder0.0 --size 1024M --type ram --memdevs mem2
+sudo cxl create-region --decoder decoder0.0 --size 1024M --type ram --memdevs mem3
 
 # create region0
-sudo cxl create-region -d decoder0.0 -s 4096M -t ram -m mem0 mem1 mem2 mem3
+sudo cxl create-region --decoder decoder0.0 --size 4096M --type ram --memdevs mem0 mem1 mem2 mem3
 
 sudo cxl enable-region region0
 sudo cxl disable-region region0
