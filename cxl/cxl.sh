946108e217217e1898e10be41d0e646945172ce0
Rong Tao <rongtao@cestc.cn>
2025-06-17 17:18:30 +0800

    cxl.sh: cxl enable-region all
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cxl/cxl.sh
+++ cxl/cxl.sh
@@ -11,5 +11,7 @@ sudo cxl create-region --decoder decoder0.0 --size 1024M --type ram --memdevs me
 sudo cxl create-region --decoder decoder0.0 --size 4096M --type ram --memdevs mem0 mem1 mem2 mem3
 
 sudo cxl enable-region region0
+sudo cxl enable-region all
+
 sudo cxl disable-region region0
 sudo cxl destroy-region region0
