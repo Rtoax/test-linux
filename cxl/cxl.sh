d5a21a914482f8f2f1cb8bc43d5c9bcf3a751202
Rong Tao <rongtao@cestc.cn>
2025-06-17 17:07:29 +0800

    cxl.sh: multi memdevs to on region
        [root@fedora ~]# cxl create-region -d decoder0.0 -s 4096M -t ram -m mem0 mem1 mem2 mem3
        [ 1782.001594] Fallback order for Node 1: 0
        [ 1782.001597] Built 1 zonelists, mobility grouping on.  Total pages: 466091
        [ 1782.002695] Policy zone: DMA32
        {
          "region":"region0",
          "resource":"0x2290000000",
          "size":"4.00 GiB (4.29 GB)",
          "type":"ram",
          "interleave_ways":4,
          "interleave_granularity":256,
          "decode_state":"commit",
          "mappings":[
            {
              "position":3,
              "memdev":"mem3",
              "decoder":"decoder5.0"
            },
            ....
            {
              "position":0,
              "memdev":"mem0",
              "decoder":"decoder2.0"
            }
          ],
          "qos_class_mismatch":true
        }
        cxl region: cmd_create_region: created 1 region
    
        [root@fedora ~]# cxl list
        [
          {
            "memdevs":[
               ....
            ]
          },
          {
            "regions":[
              {
                "region":"region0",
                "resource":148444807168,
                "size":4294967296,
                "type":"ram",
                "interleave_ways":4,
                "interleave_granularity":256,
                "decode_state":"commit",
                "qos_class_mismatch":true
              }
            ]
          }
        ]
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cxl/cxl.sh
+++ cxl/cxl.sh
@@ -2,10 +2,13 @@
 set -e
 
 # create region 0-3
-sudo cxl create-region -d decoder0.0 -m mem0 -s 1024M -t ram
-sudo cxl create-region -d decoder0.0 -m mem1 -s 1024M -t ram
-sudo cxl create-region -d decoder0.0 -m mem2 -s 1024M -t ram
-sudo cxl create-region -d decoder0.0 -m mem3 -s 1024M -t ram
+sudo cxl create-region -d decoder0.0 -s 1024M -t ram -m mem0
+sudo cxl create-region -d decoder0.0 -s 1024M -t ram -m mem1
+sudo cxl create-region -d decoder0.0 -s 1024M -t ram -m mem2
+sudo cxl create-region -d decoder0.0 -s 1024M -t ram -m mem3
+
+# create region0
+sudo cxl create-region -d decoder0.0 -s 4096M -t ram -m mem0 mem1 mem2 mem3
 
 sudo cxl enable-region region0
 sudo cxl disable-region region0
