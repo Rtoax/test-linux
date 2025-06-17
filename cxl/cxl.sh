3403ee157191635d78ae73fa392e055d378b4aad
Rong Tao <rongtao@cestc.cn>
2025-06-17 16:53:17 +0800

    cxl.sh: destroy-region, disable-region
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cxl/cxl.sh
+++ cxl/cxl.sh
@@ -8,3 +8,5 @@ sudo cxl create-region -d decoder0.0 -m mem2 -s 1024M -t ram
 sudo cxl create-region -d decoder0.0 -m mem3 -s 1024M -t ram
 
 sudo cxl enable-region region0
+sudo cxl disable-region region0
+sudo cxl destroy-region region0
