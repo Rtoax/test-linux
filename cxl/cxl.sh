cd0731abbc0cf3fe9b07f949579a3a96d4f5f6ab
Rong Tao <rongtao@cestc.cn>
2025-06-18 15:44:30 +0800

    cxl.sh: comment for commits
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cxl/cxl.sh
+++ cxl/cxl.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
+# commit d5a21a914482 ("cxl.sh: multi memdevs to on region")
 cxl_ram_4way() {
-	# create region0
+	# create region0, commit b3c049d89aa9 ("cxl: cxl.sh: create-region")
 	# will create
 	# - /sys/bus/cxl/devices/region0/dax_region
 	# - /sys/devices/platform/ACPI0017:00/root0/decoder0.0/region0/dax_region0/dax0.0
 	# - /sys/bus/dax/devices/dax0.0
 	# - /sys/bus/dax/drivers/kmem/dax0.0
-	# - /dev/dax0.0 [Character Device]
+	# - /dev/dax0.0 [Character Device], commit 1227030c1d3f ("cxl.sh: region: character device /dev/dax0.0")
 	sudo cxl create-region --decoder decoder0.0 --size 4096M --type ram --memdevs mem0 mem1 mem2 mem3
 
 	sudo cxl enable-region region0
@@ -19,7 +20,10 @@ cxl_ram_4way() {
 	sudo daxctl list -r region0
 
 	# add memory to main RAM
+	# commit 21535dd38b6e ("cxl: dax0.0: use as system-ram")
 	sudo daxctl online-memory dax0.0
+
+	# Use CXL RAM
 
 	sudo daxctl offline-memory dax0.0
 
@@ -27,6 +31,7 @@ cxl_ram_4way() {
 	sudo cxl destroy-region region0
 }
 
+# commit d61a78f78d31 ("cxl: pmem: test block device of 1way pmem")
 cxl_pmem() {
 	sudo cxl create-region --decoder decoder0.0 --size 1024M --type pmem --memdevs mem0
 	sudo ndctl list -R
