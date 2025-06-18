18254901db8b15607ba55206f08e3e1b6dd1b997
Rong Tao <rongtao@cestc.cn>
2025-06-18 11:10:32 +0800

    libcgroup: cgroup_new_cgroup.c: try test OOM
    why not works?
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cgroup/libcgroup/samples/cgroup_new_cgroup.c
+++ cgroup/libcgroup/samples/cgroup_new_cgroup.c
@@ -4,12 +4,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <sys/wait.h>
 
 
 #define CGRP_NAME "test_cgroup"
 
 int main(int argc, char **argv)
 {
+	pid_t pid;
 	struct cgroup *cgrp = NULL;
 	struct cgroup_controller *cgroup_controller;
 	int ret = 0;
@@ -42,6 +44,7 @@ int main(int argc, char **argv)
 		goto delete;
 	}
 
+	/* 100M */
 	size_t limit_in_bytes = 100 * 1024 * 1024;
 	ret = cgroup_set_value_uint64(cgroup_controller, "memory.limit_in_bytes", limit_in_bytes);
 	if (ret) {
@@ -50,25 +53,39 @@ int main(int argc, char **argv)
 		goto delete;
 	}
 
-	/* TODO: cgroup OOM test */
-	{
+	limit_in_bytes = 0;
+	cgroup_get_value_uint64(cgroup_controller, "memory.limit_in_bytes", &limit_in_bytes);
+	printf("Set memory.limit_in_bytes to %ld\n", limit_in_bytes);
+
+	pid = fork();
+	if (pid < 0)
+		goto delete;
+
+	/* TODO: cgroup OOM test, why not OOM???????????? */
+	if (pid == 0) {
 		size_t i;
+
+		sleep(1);
+		printf("Child running...\n");
+
 		mlockall(MCL_CURRENT | MCL_FUTURE);
 		char *mem = malloc(limit_in_bytes * 10);
+		/* page fault */
 		for (i = 0; i < limit_in_bytes * 10; i += getpagesize())
 			mem[i] = 'a';
+
+		exit(1);
 	}
 
-#ifdef cgroup_attach_task
-	ret = cgroup_attach_task(cgrp);
-#else
-	ret = cgroup_attach_task_pid(cgrp, getpid());
-#endif
+	printf("Attaching pid %d to cgroup.\n", pid);
+	ret = cgroup_attach_task_pid(cgrp, pid);
 	if (ret) {
 		fprintf(stderr, "Error attaching task to cgroup: %d\n", ret);
 		ret = 1;
 		goto delete;
 	}
+
+	waitpid(pid, NULL, 0);
 
 delete:
 	cgroup_delete_cgroup(cgrp, 0);
