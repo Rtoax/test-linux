0aca0421d36a38f0e3df10abcbd89eef43ad16ed
Rong Tao <rongtao@cestc.cn>
2025-06-18 13:57:11 +0800

    cxl: vm: create pmem region
        [root@fedora /]# sudo cxl create-region --decoder decoder0.0 --size 4096M --type pmem --memdevs mem0 mem1 mem2 mem3
        [  419.807784] nd_region region0: nmem1: is disabled, failing probe
        {
          "region":"region0",
          "resource":"0x2290000000",
          "size":"4.00 GiB (4.29 GB)",
          "type":"pmem",
          "interleave_ways":4,
          "interleave_granularity":8192,
          "decode_state":"commit",
          "mappings":[
            {
              "position":3,
              "memdev":"mem3",
              "decoder":"decoder6.0"
            },
            {
              "position":2,
              "memdev":"mem2",
              "decoder":"decoder5.0"
            },
            {
              "position":1,
              "memdev":"mem1",
              "decoder":"decoder4.0"
            },
            {
              "position":0,
              "memdev":"mem0",
              "decoder":"decoder3.0"
            }
          ],
          "qos_class_mismatch":true
        }
        cxl region: cmd_create_region: created 1 region
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cxl/cxl.sh
+++ cxl/cxl.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 set -e
 
-# create region0
-# will create
-# - /sys/bus/cxl/devices/region0/dax_region
-# - /sys/devices/platform/ACPI0017:00/root0/decoder0.0/region0/dax_region0/dax0.0
-# - /sys/bus/dax/devices/dax0.0
-# - /sys/bus/dax/drivers/kmem/dax0.0
-# - /dev/dax0.0 [Character Device]
-sudo cxl create-region --decoder decoder0.0 --size 4096M --type ram --memdevs mem0 mem1 mem2 mem3
+cxl_ram() {
+	# create region0
+	# will create
+	# - /sys/bus/cxl/devices/region0/dax_region
+	# - /sys/devices/platform/ACPI0017:00/root0/decoder0.0/region0/dax_region0/dax0.0
+	# - /sys/bus/dax/devices/dax0.0
+	# - /sys/bus/dax/drivers/kmem/dax0.0
+	# - /dev/dax0.0 [Character Device]
+	sudo cxl create-region --decoder decoder0.0 --size 4096M --type ram --memdevs mem0 mem1 mem2 mem3
 
-sudo cxl enable-region region0
-sudo cxl enable-region all
-sudo cxl list --regions | jq .[].type
+	sudo cxl enable-region region0
+	sudo cxl enable-region all
+	sudo cxl list --regions | jq .[].type
 
-sudo daxctl list
-sudo daxctl list -r region0
-sudo daxctl online-memory dax0.0
-sudo daxctl offline-memory dax0.0
+	sudo daxctl list
+	sudo daxctl list -r region0
+	sudo daxctl online-memory dax0.0
+	sudo daxctl offline-memory dax0.0
 
-sudo cxl disable-region region0
-sudo cxl destroy-region region0
+	sudo cxl disable-region region0
+	sudo cxl destroy-region region0
+}
+
+cxl_pmem() {
+	sudo cxl create-region --decoder decoder0.0 --size 4096M --type pmem --memdevs mem0 mem1 mem2 mem3
+}
