e9fe2b1421d72856a3bb4df112462b56f3cf4969
Rong Tao <rongtao@cestc.cn>
2025-06-19 17:25:15 +0800

    shell: apply main.mk for all
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/target-shell.mk
+++ template/target-shell.mk
@@ -5,4 +5,4 @@ Q ?= @
 # see TARGETS_SHELL_LOGS
 %.sh.log: %.sh
 	@echo -e "  SHELL  \033[1;32m$(@)\033[m"
-	$(Q)${SHELL} $(<) $(ARGS_$(*)) | tee $(@)
+	$(Q)${SHELL} $(<) $(ARGS_$(*)) $(SHELL_ARGS_$(<)) | tee $(@)
