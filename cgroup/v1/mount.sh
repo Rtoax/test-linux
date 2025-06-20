f9fafaa47f644e78b168798247118ac446fd73f3
Rong Tao <rongtao@cestc.cn>
2025-06-20 14:44:59 +0800

    cgroup/v1: mount.sh: Test net_cls,net_prio
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cgroup/v1/mount.sh
+++ cgroup/v1/mount.sh
@@ -7,17 +7,18 @@ MNT=${NAME}.mnt
 sudo mkdir -p ${MNT}
 
 cleanall() {
-	sudo umount ${MNT}/memory ${MNT}/cpu,cpuacct ${MNT}/freezer ${MNT} || true
+	sudo umount ${MNT}/memory ${MNT}/cpu,cpuacct ${MNT}/freezer ${MNT}/net_cls,net_prio ${MNT} || true
 	sudo rm -rf ${MNT} || true
 }
 trap cleanall EXIT
 
 sudo mount -t cgroup -o none,name=${NAME} ${NAME} ${MNT}
 
-sudo mkdir -p ${MNT}/freezer ${MNT}/cpu,cpuacct ${MNT}/memory
+sudo mkdir -p ${MNT}/freezer ${MNT}/cpu,cpuacct ${MNT}/memory ${MNT}/net_cls,net_prio/
 
 sudo mount -t cgroup -o freezer ${NAME}-freezer ${MNT}/freezer
-sudo mount -t cgroup -o cpu,cpuacct ${NAME}-cpu,cpuacct ${MNT}/cpu,cpuacct
+sudo mount -t cgroup -o net_cls,net_prio ${NAME}-net ${MNT}/net_cls,net_prio/
+sudo mount -t cgroup -o cpu,cpuacct cgroup ${MNT}/cpu,cpuacct
 sudo mount -t cgroup -o memory ${NAME}-memory ${MNT}/memory
 
 mount | grep ${NAME}
