a29dfb7ce4c05058fcffd42d1b16296cfcdb8894
Rong Tao <rongtao@cestc.cn>
2025-06-23 19:30:05 +0800

    target-exe.mk: re-order targets
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/target-exe.mk
+++ template/target-exe.mk
@@ -13,10 +13,6 @@ ${OUTPUT}%.c.s: %.c | ${OUTPUT}
 	@echo -e "  CC S \033[1m$(<)\033[m to \033[1m$(@)\033[m"
 	${Q}${CC_PFX} $(CC) -S -o $(@) -c $(<) $(CFLAGS) $(CFLAGS_$(*))
 
-$(TARGETS): %:
-	@echo -e "  LD  \033[1;32m$(@)\033[m"
-	${Q}${CC_PFX} $(CC) -o $(@) $(^) $(LDFLAGS) $(LDFLAGS_$(*))
-
 ${OUTPUT}%.opp: %.cpp | ${OUTPUT}
 	@echo -e "  CXX  \033[1m$(<)\033[m to \033[1m$(@)\033[m"
 	${Q}${CC_PFX} $(CXX) -o $(@) -c $(<) $(CXXFLAGS) $(CXXFLAGS_$(*))
@@ -25,10 +21,6 @@ ${OUTPUT}%.cpp.s: %.cpp | ${OUTPUT}
 	@echo -e "  CXX S  \033[1m$(<)\033[m to \033[1m$(@)\033[m"
 	${Q}${CC_PFX} $(CXX) -S -o $(@) -c $(<) $(CXXFLAGS) $(CXXFLAGS_$(*))
 
-${TARGETS_CPP}: %:
-	@echo -e "  LD CXX   \033[1;32m$(@)\033[m"
-	${Q}${CC_PFX} $(CXX) -o $(@) $(^) $(LDXXFLAGS) $(LDXXFLAGS_$(*))
-
 ${OUTPUT}%.oasm: %.asm | ${OUTPUT}
 	@echo -e "  ASM  \033[1m$(<)\033[m to \033[1m$(@)\033[m"
 	${Q}${NASM} -o $(@) -felf64 $(<)
@@ -36,6 +28,14 @@ ${OUTPUT}%.oasm: %.asm | ${OUTPUT}
 ${OUTPUT}%.oS: %.S | ${OUTPUT}
 	@echo -e "  CC S  \033[1m$(<)\033[m to \033[1m$(@)\033[m"
 	${Q}$(CC) -o $(@) -c $(<) $(CFLAGS) $(CFLAGS_$(*))
+
+$(TARGETS): %:
+	@echo -e "  LD  \033[1;32m$(@)\033[m"
+	${Q}${CC_PFX} $(CC) -o $(@) $(^) $(LDFLAGS) $(LDFLAGS_$(*))
+
+${TARGETS_CPP}: %:
+	@echo -e "  LD CXX   \033[1;32m$(@)\033[m"
+	${Q}${CC_PFX} $(CXX) -o $(@) $(^) $(LDXXFLAGS) $(LDXXFLAGS_$(*))
 
 ${TARGETS_ASM}: %:
 	@echo -e "  LD ASM   \033[1;32m$(@)\033[m"
