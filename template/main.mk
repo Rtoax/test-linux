98dfbbce9d484e113f4e85d0ad428bcd0d5ef33c
Rong Tao <rongtao@cestc.cn>
2025-06-23 09:04:45 +0800

    template: add target-java.mk
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/main.mk
+++ template/main.mk
@@ -41,6 +41,7 @@ build-targets += $(OUTPUT)
 build-targets += $(TARGETS) $(TARGETS_LIBA) $(TARGETS_LIBSO)
 build-targets += $(TARGETS_CPP)
 build-targets += $(TARGETS_GO)
+build-targets += $(TARGETS_JAVA)
 TARGETS_SHELL_LOGS := $(patsubst %.sh,%.sh.log,$(TARGETS_SHELL))
 build-targets += $(TARGETS_SHELL_LOGS)
 build-targets += $(sub-dir-build)
@@ -57,7 +58,7 @@ test: $(build-targets) $(sub-dir-test) $(TARGETS_TEST)
 .PHONY: clean
 clean: $(sub-dir-clean) $(TARGETS_CLEAN)
 	@echo -e "  CLEAN  \033[1;32m${build-targets} ${TARGETS_CLEAN}\033[m"
-	${Q}rm -rf ${build-targets} *.o *.opp *.d *.log *.out
+	${Q}rm -rf ${build-targets} *.o *.opp *.d *.log *.out *.class
 	@echo -e " \033[1;33m Clean $(shell pwd) done \033[m"
 
 include ${TEMPLATE_DIR}/target-exe.mk
@@ -65,6 +66,7 @@ include ${TEMPLATE_DIR}/target-liba.mk
 include ${TEMPLATE_DIR}/target-libso.mk
 include ${TEMPLATE_DIR}/target-shell.mk
 include ${TEMPLATE_DIR}/target-golang.mk
+include ${TEMPLATE_DIR}/target-java.mk
 ifneq (${OUTPUT},)
   include ${TEMPLATE_DIR}/target-output.mk
 endif
---
+++ template/target-java.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-3.0
+JAVA ?= $(shell which java 2>/dev/null)
+JAVAC ?= $(shell which javac 2>/dev/null)
+
+ifeq ($(wildcard $(JAVAC)),)
+  $(error Please install java)
+endif
+
+${TARGETS_JAVA}:
+	@echo -e "  JAVAC  \033[1;32m$(@)\033[m"
+	${Q}$(JAVAC) $(^)
