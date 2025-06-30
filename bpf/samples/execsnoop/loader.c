d5edc0d1ecacae4e101bed878d5ca2a82a4e8c4d
Rong Tao <rongtao@cestc.cn>
2025-06-30 10:21:46 +0800

    bpf: apply some main.mk
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/samples/execsnoop/loader.c
+++ bpf/samples/execsnoop/loader.c
@@ -2,13 +2,14 @@
 #include "bpf_load.h"
 #include "trace_helpers.h"
 
-int main(int argc, char **argv) {
-  if (load_bpf_file("bpf_program.o") != 0) {
-    printf("The kernel didn't load the BPF program\n");
-    return -1;
-  }
+int main(int argc, char **argv)
+{
+	if (load_bpf_file("bpf_program.bpf.o") != 0) {
+		printf("The kernel didn't load the BPF program\n");
+		return -1;
+	}
 
-  read_trace_pipe();
+	read_trace_pipe();
 
-  return 0;
+	return 0;
 }
