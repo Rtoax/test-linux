58fc8992516b2ce571876a7557c67ba4e6c05db3
Rong Tao <rongtao@cestc.cn>
2025-07-01 16:43:53 +0800

    subdir-footer.mk: only make cmd has $(Q)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/subdir-footer.mk
+++ template/subdir-footer.mk
@@ -5,7 +5,7 @@ Q ?= @
 # $2: sub-dir
 define make_sub_dir
 	$(call tl_log,"${1} $(call git_relative_dir,$(2))")
-	@make -C ${2} || echo "Failed ${1} $(call git_relative_dir,$(2))" | tee --append ${TL_FAILED_LOG}
+	${Q}make -C ${2} || echo "Failed ${1} $(call git_relative_dir,$(2))" | tee --append ${TL_FAILED_LOG}
 endef
 
 define make_sub_dir_build
@@ -22,12 +22,12 @@ endef
 
 $(sub-dir-build):
 	$(call log_tgt_start,build,$(call git_relative_dir,$(patsubst %.build,%,$(@))))
-	${Q}$(call make_sub_dir_build,$(@:.build=))
+	$(call make_sub_dir_build,$(@:.build=))
 
 $(sub-dir-test):
 	$(call log_tgt_start,test,$(call git_relative_dir,$(patsubst %.test,%,$(@))))
-	${Q}$(call make_sub_dir_test,$(@:.test=))
+	$(call make_sub_dir_test,$(@:.test=))
 
 $(sub-dir-clean):
 	$(call log_tgt_start,clean,$(call git_relative_dir,$(patsubst %.clean,%,$(@))))
-	${Q}$(call make_sub_dir_clean,$(@:.clean=))
+	$(call make_sub_dir_clean,$(@:.clean=))
