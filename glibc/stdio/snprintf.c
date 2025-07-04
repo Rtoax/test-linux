467984afda8201916773f0959e4b514b8d6d74a3
Rong Tao <rongtao@cestc.cn>
2025-07-04 13:52:21 +0800

    snprintf: test overflow, may return ERANGE
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/stdio/snprintf.c
+++ glibc/stdio/snprintf.c
@@ -11,7 +11,7 @@ int snprintf_hex(char *buf, size_t size, char *data, size_t len)
 	return ret;
 }
 
-int main(void)
+int test0(void)
 {
 	char buf[1024] = {0};
 	char data[] = {"hello world.\n"};
@@ -19,5 +19,28 @@ int main(void)
 	snprintf_hex(buf, 1024, data, sizeof(data));
 
 	printf("%s\n", buf);
+	return 0;
+}
+
+int test1_overflow(void)
+{
+	int err;
+	char buffer[8];
+
+	/**
+	 * snprintf not set errno, should return -ERANGE here;
+	 */
+	err = snprintf(buffer, sizeof(buffer), "%s", "1234567890");
+	if (err >= sizeof(buffer))
+		fprintf(stderr, "ERANGE.\n");
+
+	printf("%d %s\n", err, buffer);
+	return 0;
+}
+
+int main(void)
+{
+	test0();
+	test1_overflow();
 	return 0;
 }
