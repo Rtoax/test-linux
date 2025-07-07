d286af61749b6e2ab06521329ec785f864a7556d
Rong Tao <rongtao@cestc.cn>
2025-07-07 20:02:16 +0800

    attribute: naked: clang error: non-ASM statement in naked function is not supported
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/__attribute__/__naked__.c
+++ compiler/__attribute__/__naked__.c
@@ -4,11 +4,16 @@
  * The 'naked' attribute prevents the compiler from generating any
  * function entry or exit code
  */
+#define ____naked __attribute__((naked))
+#define ____naked2 __attribute__((__naked__))
+
 #if !defined(__clang__)
-# define __naked __attribute__((naked))
-# define __naked2 __attribute__((__naked__))
+# define __naked	____naked
+# define __naked2	____naked2
 #else
-// FIXME: clang??
+/**
+ * FIXME: clang error: non-ASM statement in naked function is not supported
+ */
 # define __naked
 # define __naked2
 #endif
@@ -26,6 +31,11 @@ __naked2 int func2(void)
 int func3(void)
 {
 	return printf("rongtao\n");
+}
+
+__naked2 void func4(void)
+{
+	__asm__("nop\n");
 }
 
 int main(void)
