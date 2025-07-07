36558416551d09974762de0b93ec918a80fe41ab
Rong Tao <rongtao@cestc.cn>
2025-07-07 19:13:18 +0800

    bpf: tcpdump.bpf.c: display and commands comment
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/tcpdump.bpf.c
+++ bpf/tcpdump.bpf.c
@@ -1,3 +1,9 @@
+/**
+ * Usage:
+ * $ sudo ip link set dev lo xdp obj tcpdump.bpf.o
+ * $ sudo ip link set dev lo xdp off
+ * $ sudo cat /sys/kernel/tracing/trace_pipe
+ */
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
@@ -9,7 +15,7 @@
 /**
  * sudo tcpdump -d ip and tcp port 80
  */
-SEC("xdp")
+SEC("prog")
 int xdp_tcpdump_prog(struct xdp_md *ctx)
 {
 	void *data = (void *)(long)ctx->data;
@@ -19,23 +25,33 @@ int xdp_tcpdump_prog(struct xdp_md *ctx)
 	struct iphdr *iphdr;
 	struct tcphdr *tcphdr;
 
+	if ((void *)(ethhdr + 1) > data_end)
+		return XDP_PASS;
+
 	if (ethhdr->h_proto == ETH_P_IP) {
+		bpf_printk("Get IP");
+
 		iphdr = (void *)(ethhdr + 1);
 
-		if ((void *)iphdr > data_end)
+		if ((void *)(iphdr + 1) > data_end)
 			return XDP_PASS;
 
 		if (iphdr->protocol == IPPROTO_TCP) {
+
+			bpf_printk("Get TCP");
+
 			if (iphdr->frag_off & 0x1fff)
 				goto exit;
 
 			tcphdr = (void *)(iphdr + 1);
 
-			if ((void *)tcphdr > data_end)
+			if ((void *)(tcphdr + 1) > data_end)
 				return XDP_PASS;
 
 			if (tcphdr->source != 80 && tcphdr->dest != 80)
 				goto exit;
+
+			bpf_printk("Get IP->TCP->Port80");
 
 			/* 0x40000, see libcap,tcpdump source code */
 			return XDP_PASS;
@@ -48,3 +64,5 @@ int xdp_tcpdump_prog(struct xdp_md *ctx)
 exit:
 	return XDP_PASS;
 }
+
+char __license[] SEC("license") = "GPL";
