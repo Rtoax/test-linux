a739100b45a9b780649612846988d1643c4d3d0c
Rong Tao <rongtao@cestc.cn>
2025-07-07 19:43:37 +0800

    jit/samples: code style
    Link: https://www.youtube.com/watch?v=RzrHuP2aVEg 56:38
    Link: https://www.bilibili.com/video/BV1tu411v7XM/ 56:38
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/jit/samples/add.c
+++ bpf/jit/samples/add.c
@@ -1,8 +1,3 @@
-/**
- * links
- * - https://www.youtube.com/watch?v=RzrHuP2aVEg 56:38
- * - https://www.bilibili.com/video/BV1tu411v7XM/ 56:38
- */
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
@@ -14,9 +9,6 @@ long add(long num)
 	return num + 4;
 }
 
-/**
- * TODO: segvfault
- */
 void emit_code_into_memory(unsigned char *m)
 {
 	/**
@@ -31,16 +23,16 @@ void emit_code_into_memory(unsigned char *m)
 	};
 	/* objdump -d add.o */
 	unsigned char __attribute__((unused)) code2[] = {
-		0x55,                   // push   %rbp
-		0x48, 0x89, 0xe5,       // mov    %rsp,%rbp
-		0x48, 0x89, 0x7d, 0xf8, // mov    %rdi,-0x8(%rbp)
-		0x48, 0x8b, 0x45, 0xf8, // mov    -0x8(%rbp),%rax
-		0x48, 0x83, 0xc0, 0x04, // add    $0x4,%rax
-		0x5d,                   // pop    %rbp
-		0xc3,                   // retq
+		0x55,                   /* push   %rbp */
+		0x48, 0x89, 0xe5,       /* mov    %rsp,%rbp */
+		0x48, 0x89, 0x7d, 0xf8, /* mov    %rdi,-0x8(%rbp) */
+		0x48, 0x8b, 0x45, 0xf8, /* mov    -0x8(%rbp),%rax */
+		0x48, 0x83, 0xc0, 0x04, /* add    $0x4,%rax */
+		0x5d,                   /* pop    %rbp */
+		0xc3,                   /* retq */
 	};
 
-	memcpy(m, code1, sizeof(code1));
+	memcpy(m, code2, sizeof(code2));
 }
 
 const size_t SIZE = 1024;
@@ -50,8 +42,10 @@ void run_from_rwx(void)
 {
 	void *m = mmap(NULL, SIZE, PROT_READ | PROT_WRITE | PROT_EXEC,
 			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
 	emit_code_into_memory(m);
 	JittedFunc func = m;
+
 	int result = func(2);
 
 	printf("result = %d\n", result);
