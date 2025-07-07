8d642e079637290b21adc9751bd394231579ca7a
Rong Tao <rongtao@cestc.cn>
2025-07-07 18:57:09 +0800

    bpf: tcpdump.bpf.c: Fix verifier errors
        $ sudo ip link set dev lo xdp obj tcpdump.bpf.o
        libbpf: prog 'xdp_tcpdump_prog': BPF program load failed: Permission denied
        libbpf: prog 'xdp_tcpdump_prog': -- BEGIN PROG LOAD LOG --
        0: R1=ctx() R10=fp0
        ; void *data = (void *)(long)ctx->data; @ tcpdump.bpf.c:15
        0: (61) r1 = *(u32 *)(r1 +0)          ; R1_w=pkt(r=0)
        ; if (ethhdr->h_proto == ETH_P_IP) { @ tcpdump.bpf.c:20
        1: (69) r2 = *(u16 *)(r1 +12)
        invalid access to packet, off=12 size=2, R1(id=0,off=12,r=0)
        R1 offset is outside of the packet
        processed 2 insns (limit 1000000) max_states_per_insn 0 total_states 0 peak_states 0 mark_read 0
        -- END PROG LOAD LOG --
        libbpf: prog 'xdp_tcpdump_prog': failed to load: -13
        libbpf: failed to load object 'tcpdump.bpf.o'
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/tcpdump.bpf.c
+++ bpf/tcpdump.bpf.c
@@ -13,17 +13,27 @@ SEC("xdp")
 int xdp_tcpdump_prog(struct xdp_md *ctx)
 {
 	void *data = (void *)(long)ctx->data;
+	void *data_end = (void *)(long)ctx->data_end;
+
 	struct ethhdr *ethhdr = data;
 	struct iphdr *iphdr;
 	struct tcphdr *tcphdr;
 
 	if (ethhdr->h_proto == ETH_P_IP) {
 		iphdr = (void *)(ethhdr + 1);
+
+		if ((void *)iphdr > data_end)
+			return XDP_PASS;
+
 		if (iphdr->protocol == IPPROTO_TCP) {
 			if (iphdr->frag_off & 0x1fff)
 				goto exit;
 
 			tcphdr = (void *)(iphdr + 1);
+
+			if ((void *)tcphdr > data_end)
+				return XDP_PASS;
+
 			if (tcphdr->source != 80 && tcphdr->dest != 80)
 				goto exit;
 
