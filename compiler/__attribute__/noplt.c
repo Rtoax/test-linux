e64329620fecd0a1ff090681cba34d3529335956
Rong Tao <rongtao@cestc.cn>
2025-07-08 08:44:31 +0800

    compiler/attribute: fix compile error for clang
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/__attribute__/noplt.c
+++ compiler/__attribute__/noplt.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-#ifdef NOPLT
 /**
  * The noplt attribute is the counterpart to option -fno-plt. Calls to
  * functions marked with this attribute in position-independent code do not
  * use the PLT.
  */
-int getchar(void) __attribute__((noplt));
+#ifndef __clang__
+# define __noplt	__attribute__((noplt))
+#else
+# define __noplt
+#endif
+
+#ifdef NOPLT
+int getchar(void) __noplt;
 #endif
 
 int main(void)
--- compiler/__attribute__/optimize.h
+++ compiler/__attribute__/optimize.h
@@ -1,6 +1,13 @@
 #pragma once
 
+/**
+ * clang error: unknown attribute 'optimize'
+ */
+#if !defined(__clang__)
 #define _omit_frame_pointer	__attribute__((optimize("omit-frame-pointer")))
+#else
+#define _omit_frame_pointer
+#endif
 
 #if defined(__clang__)
 #define _OPTIMIZE_O0 __attribute__((optnone))
