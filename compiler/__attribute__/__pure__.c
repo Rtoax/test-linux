dc6135452a53d9c4f9020dff9f6e953405598d04
Rong Tao <rongtao@cestc.cn>
2025-07-08 14:10:29 +0800

    compiler: attribute: __pure__.c: code style
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/__attribute__/__pure__.c
+++ compiler/__attribute__/__pure__.c
@@ -3,24 +3,22 @@
  * value depends only on the parameters and global variables. Functions of
  * this kind can be subject to data flow analysis and might be eliminated.
  */
-
 #include <stdio.h>
 
-#if __GNUC_PREREQ(2, 96)
+#if ! __GNUC_PREREQ(2, 96)
+#error "Not support __attribute__ pure"
+#endif
+
 /*
  * gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-pure-function-attribute
  */
-# define __pure __attribute__((__pure__))
+#define __pure __attribute__((__pure__))
 /**
  * The __attribute__((pure)) and __attribute__((__pure__)) attributes in GCC
  * are actually the same. The difference in notation is just a matter of style
  * and does not affect the functionality.
  */
-# define __pure2 __attribute__((pure))
-#else
-# define __pure
-# define __pure2
-#endif
+#define __pure2 __attribute__((pure))
 
 int __pure func1(void)
 {
