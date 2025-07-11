2d1486abe510e631dde79b0e57f6b8b4fd5dbaf3
Rong Tao <rongtao@cestc.cn>
2025-07-11 16:39:14 +0800

    init/kernel: add Makefile
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- init/kernel/info.sh
+++ init/kernel/info.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
-which dpkg 2>&1 >/dev/null && dpkg --list | grep linux-image
-
+which dpkg 2>&1 >/dev/null && dpkg --list | grep linux-image || true
+which rpm 2>&1 >/dev/null && rpm -qa | grep -w kernel || true
