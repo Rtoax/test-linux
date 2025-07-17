0e0da0f47fea713ebff883cb62aa20f1bb27fdaa
Rong Tao <rongtao@cestc.cn>
2025-07-17 08:33:17 +0800

    target-cuda.mk: fix compile error of cflags
        [rongtao@NUC-VM-alma9 samples]$ make NVCC=/usr/local/cuda-12.9/bin/nvcc V=1
        make: Circular hello.cu <- hello.cu.o dependency dropped.
          NVCC     hello.cu -> hello.cu.o
        /usr/local/cuda-12.9/bin/nvcc -o hello.cu.o -c hello.cu -D_GNU_SOURCE -Werror -Wall -Wstrict-prototypes
        nvcc fatal   : Value '-Wall' is not defined for option 'Werror'
        make: *** [/home/rongtao/Git/test-linux/template//target-cuda.mk:9: hello.cu.o] Error 1
        [rongtao@NUC-VM-alma9 samples]$ ls
        hello.cu  Makefile
        [rongtao@NUC-VM-alma9 samples]$ make NVCC=/usr/local/cuda-12.9/bin/nvcc V=1
        make: Circular hello.cu <- hello.cu.o dependency dropped.
          NVCC     hello.cu -> hello.cu.o
        /usr/local/cuda-12.9/bin/nvcc -o hello.cu.o -c hello.cu -D_GNU_SOURCE  -Wall -Wstrict-prototypes
        nvcc fatal   : Unknown option '-Wall'
        make: *** [/home/rongtao/Git/test-linux/template//target-cuda.mk:11: hello.cu.o] Error 1
        [rongtao@NUC-VM-alma9 samples]$ make NVCC=/usr/local/cuda-12.9/bin/nvcc V=1
        make: Circular hello.cu <- hello.cu.o dependency dropped.
          NVCC     hello.cu -> hello.cu.o
        /usr/local/cuda-12.9/bin/nvcc -o hello.cu.o -c hello.cu -D_GNU_SOURCE   -Wstrict-prototypes
        nvcc fatal   : Unknown option '-Wstrict-prototypes'
        make: *** [/home/rongtao/Git/test-linux/template//target-cuda.mk:12: hello.cu.o] Error 1
        [rongtao@NUC-VM-alma9 samples]$ make
        /home/rongtao/Git/test-linux/template//target-cuda.mk:4: *** Not found nvcc, install cuda first.  Stop.
        [rongtao@NUC-VM-alma9 samples]$ make NVCC=/usr/local/cuda-12.9/bin/nvcc V=1
        make: Circular hello.cu <- hello.cu.o dependency dropped.
          NVCC     hello.cu -> hello.cu.o
        /usr/local/cuda-12.9/bin/nvcc -o hello.cu.o -c hello.cu -D_GNU_SOURCE
        nvcc warning : Support for offline compilation for architectures prior to '<compute/sm/lto>_75' will be removed in a future release (Use -Wno-deprecated-gpu-targets to suppress warning).
        <command-line>: fatal error: cuda_runtime.h: No such file or directory
        compilation terminated.
        make: *** [/home/rongtao/Git/test-linux/template//target-cuda.mk:13: hello.cu.o] Error 1
        [rongtao@NUC-VM-alma9 samples]$ vim Makefile
        [rongtao@NUC-VM-alma9 samples]$ make NVCC=/usr/local/cuda-12.9/bin/nvcc V=1
        make: Circular hello.cu <- hello.cu.o dependency dropped.
          NVCC     hello.cu -> hello.cu.o
        /usr/local/cuda-12.9/bin/nvcc -o hello.cu.o -c hello.cu -D_GNU_SOURCE
        nvcc warning : Support for offline compilation for architectures prior to '<compute/sm/lto>_75' will be removed in a future release (Use -Wno-deprecated-gpu-targets to suppress warning).
        <command-line>: fatal error: cuda_runtime.h: No such file or directory
        compilation terminated.
        make: *** [/home/rongtao/Git/test-linux/template//target-cuda.mk:19: hello.cu.o] Error 1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/target-cuda.mk
+++ template/target-cuda.mk
@@ -4,6 +4,16 @@ ifeq ($(wildcard $(NVCC)),)
   $(error Not found nvcc, install cuda first)
 endif
 
+# Fix errors, those cflags added in main.mk
+# nvcc fatal   : Value '-Wall' is not defined for option 'Werror'
+# nvcc fatal   : Unknown option '-Wall'
+# nvcc fatal   : Unknown option '-Wstrict-prototypes'
+CFLAGS := $(patsubst -Wall,,${CFLAGS})
+CFLAGS := $(patsubst -Werror,,${CFLAGS})
+CFLAGS := $(patsubst -Wstrict-prototypes,,${CFLAGS})
+LDFLAGS := $(patsubst -Wall,,${LDFLAGS})
+LDFLAGS := $(patsubst -Werror,,${LDFLAGS})
+
 %.cu.o: %.cu
 	$(call log_tgt_obj,NVCC,$(<),$(@))
 	${Q}$(NVCC) -o $(@) -c $(<) $(CFLAGS) $(CFLAGS_$(*))
