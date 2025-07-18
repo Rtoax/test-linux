eae0a8c99fe9b624aac34e36fcc6e63432bc0ab9
Rong Tao <rongtao@cestc.cn>
2025-07-18 11:36:46 +0800

    cuda/samples: parallel.cu: check gpu device
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/samples/parallel.cu
+++ hpc/nvidia/cuda/samples/parallel.cu
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 
+#include "cuda_helpers.h"
+
 
 __global__ void foo(int count)
 {
@@ -13,6 +15,10 @@ __global__ void foo(int count)
 int main(void)
 {
 	int count = 10000;
+
+	cudaSetDevice(0);
+	cuda_check_gpu_error("Failed to initialize device!");
+
 	foo<<<(count + 511) / 512, 512>>>(count);
 	return 0;
 }
