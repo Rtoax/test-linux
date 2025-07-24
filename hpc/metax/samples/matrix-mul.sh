852657ffeafd6566b1eaa969e779029639e1ab57
Rong Tao <rongtao@cestc.cn>
2025-07-24 18:17:02 +0800

    hpc: matrix-mul.sh: support set -g,--gpu
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/metax/samples/matrix-mul.sh
+++ hpc/metax/samples/matrix-mul.sh
@@ -2,9 +2,52 @@
 set -e
 
 readonly loop=10000
+program=matrix-mul
+gpu=0
 
-program=$1
-[[ -z ${program} ]] && program=matrix-mul
+__usage__()
+{
+	echo -e "
+matrix-mul.sh
+
+-g, --gpu [GPU]        running on GPU
+-p, --prog [PROG]      set ELF program
+-h, --help             show help info
+"
+	exit ${1-0}
+}
+
+TEMP_ARGS=$(getopt --options g:p:h \
+	--long gpu: \
+	--long prog: \
+	--long help \
+	--name $0 -- "$@")
+
+test $? != 0 && __usage__ 1
+
+eval set -- "$TEMP_ARGS"
+while true; do
+	case $1 in
+	-g | --gpu)
+		shift
+		gpu=$1
+		shift
+		;;
+	-p | --prog)
+		shift
+		program=$1
+		shift
+		;;
+	-h | --help)
+		shift
+		__usage__
+		;;
+	--)
+		shift
+		break
+		;;
+	esac
+done
 
 rm -f ${program}.log
 for m in $(seq 10 10 1000)
@@ -14,5 +57,5 @@ do
 	else
 		verbose=""
 	fi
-	./${program} -m${m} -n${m} -N${loop} -i2 ${verbose} | tee --append ${program}.log
+	./${program} -m${m} -n${m} -N${loop} -i2 ${verbose} --gpu ${gpu} | tee --append ${program}.log
 done
