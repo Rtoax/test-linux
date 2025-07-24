3e9b58b99824b540e843bba0fce6ec70062b0ac5
Rong Tao <rongtao@cestc.cn>
2025-07-24 12:39:15 +0800

    cudaGetDeviceProperties: blockDim,gridDim
        name NVIDIA H800
        totalGlobalMem 85028896768 (85 GiB)
        totalConstMem 65536
        maxThreadsPerBlock 1024 >= blockDim.x * blockDim.y * blockDim.z
        maxThreadsDim[0] = 1024     >= blockDim.x
        maxThreadsDim[1] = 1024     >= blockDim.y
        maxThreadsDim[2] = 64       >= blockDim.z
        maxGridSize[0] = 2147483647       >= gridDim.x
        maxGridSize[1] = 65535            >= gridDim.y
        maxGridSize[2] = 65535            >= gridDim.z
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/samples/cudaGetDeviceProperties.cu
+++ hpc/nvidia/cuda/samples/cudaGetDeviceProperties.cu
@@ -16,14 +16,21 @@ int main(void)
 	cudaGetDeviceProperties(&prop, 0);
 
 	printf("name %s\n", prop.name);
+
 	printf("totalGlobalMem %ld (%.0lf GiB)\n",
 		prop.totalGlobalMem, prop.totalGlobalMem / 1e9);
-	printf("totalConstMem %ld\n", prop.totalConstMem);
+	printf("totalConstMem %ld >= blockDim.x * blockDim.y * blockDim.z\n",
+		prop.totalConstMem);
+
 	printf("maxThreadsPerBlock %d\n", prop.maxThreadsPerBlock);
+
 	for (i = 0; i < 3; i++)
-		printf("maxThreadsDim[%d] = %d\n", i, prop.maxThreadsDim[i]);
+		printf("maxThreadsDim[%d] = %-8d >= blockDim.%s\n", i,
+			prop.maxThreadsDim[i], "x\0y\0z\0" + (2 * i));
+
 	for (i = 0; i < 3; i++)
-		printf("maxGridSize[%d] = %d\n", i, prop.maxGridSize[i]);
+		printf("maxGridSize[%d] = %-16d >= gridDim.%s\n", i,
+			prop.maxGridSize[i], "x\0y\0z\0" + (2 * i));
 
 	return 0;
 }
