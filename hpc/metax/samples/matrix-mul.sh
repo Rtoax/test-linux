5a3ef969a066795c2aa52bd6276659d426ead9bf
Rong Tao <rongtao@cestc.cn>
2025-07-24 15:22:56 +0800

    cudaGetDeviceProperties: add commit comment
    and matrix-mul.sh auto tee to .log
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/metax/samples/matrix-mul.sh
+++ hpc/metax/samples/matrix-mul.sh
@@ -6,6 +6,7 @@ readonly loop=10000
 program=$1
 [[ -z ${program} ]] && program=matrix-mul
 
+rm -f ${program}.log
 for m in $(seq 10 10 1000)
 do
 	if [[ $m -eq 10 ]]; then
@@ -13,5 +14,5 @@ do
 	else
 		verbose=""
 	fi
-	./${program} -m${m} -n${m} -N${loop} -i2 ${verbose}
+	./${program} -m${m} -n${m} -N${loop} -i2 ${verbose} | tee --append ${program}.log
 done
--- hpc/nvidia/cuda/samples/cudaGetDeviceProperties.cu
+++ hpc/nvidia/cuda/samples/cudaGetDeviceProperties.cu
@@ -1,8 +1,10 @@
 /**
  * - NVIDIA H800 device properties
  *   commit 3e9b58b99824 ("cudaGetDeviceProperties: blockDim,gridDim")
+ *   commit f803951f1f66 ("hpc: get more device prop of Nvidia H800 and MetaX Mars 03")
  * - MetaX Mars 03 device properties
  *   commit 4117d2538707 ("metax: Display Mars 03 DeviceProperties")
+ *   commit f803951f1f66 ("hpc: get more device prop of Nvidia H800 and MetaX Mars 03")
  * - cuda-12.9 struct cudaDeviceProp
  *   commit d072d32dcb00 ("cuda: cudaGetDeviceProperties.cu: display more")
  */
