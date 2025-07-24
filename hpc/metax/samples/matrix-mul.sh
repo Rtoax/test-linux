0bf177ef780d4cf13150b6b1d2cca23e22630975
Rong Tao <rongtao@cestc.cn>
2025-07-24 09:49:37 +0800

    hpc: matrix-mul.sh: verbose the first line
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/metax/samples/matrix-mul.sh
+++ hpc/metax/samples/matrix-mul.sh
@@ -3,7 +3,15 @@ set -e
 
 readonly loop=10000
 
+program=$1
+[[ -z ${program} ]] && program=matrix-mul
+
 for m in $(seq 10 10 1000)
 do
-	./matrix-mul -m${m} -n${m} -N${loop} -i2
+	if [[ $m -eq 10 ]]; then
+		verbose=-v
+	else
+		verbose=""
+	fi
+	./${program} -m${m} -n${m} -N${loop} -i2 ${verbose}
 done
