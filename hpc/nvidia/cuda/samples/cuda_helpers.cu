f906e46ab495dad694e41b21dc37cd2a47afab4a
Rong Tao <rongtao@cestc.cn>
2025-07-24 18:23:08 +0800

    hpcc_helpers.hpcc,cuda_helpers.cu: exit 1 if check failed
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/samples/cuda_helpers.cu
+++ hpc/nvidia/cuda/samples/cuda_helpers.cu
@@ -16,6 +16,6 @@ void cuda_check_gpu_error(const char *msg)
 	cudaError_t err = cudaGetLastError();
 	if (cudaSuccess != err) {
 		printf("Cuda error: %s: %s\n", msg, cudaGetErrorString(err));
-		exit(0);
+		exit(1);
 	}
 }
