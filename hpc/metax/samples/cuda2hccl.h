ab07328a917a438c37041b6cfba7491dda6a1c70
Rong Tao <rongtao@cestc.cn>
2025-07-28 11:06:41 +0800

    metax: cuda2hccl.h: update code style
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/metax/samples/cuda2hccl.h
+++ hpc/metax/samples/cuda2hccl.h
@@ -1,21 +1,26 @@
 // SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
-#pragma once
 /* Metax has CUDA-compatible APIs */
-#define cudaMalloc	hcMalloc
-#define cudaFree	hcFree
-#define cudaMemset	hcMemset
-#define cudaMemcpy	hcMemcpy
+#pragma once
+
+#define cudaMalloc(pp, sz)	hcMalloc(pp, sz)
+#define cudaFree(ptr)	hcFree(ptr)
+#define cudaMemset(ptr, v, size)	hcMemset(ptr, v, size)
+#define cudaMemcpy(dev, host, size, flag)	hcMemcpy(dev, host, size, flag)
 #define cudaMemcpyHostToDevice	hcMemcpyHostToDevice
 #define cudaMemcpyDeviceToHost	hcMemcpyDeviceToHost
+
 #define cudaEvent_t	hcEvent_t
-#define cudaEventCreate	hcEventCreate
-#define cudaEventDestroy	hcEventDestroy
-#define cudaEventRecord	hcEventRecord
-#define cudaEventElapsedTime	hcEventElapsedTime
-#define cudaEventSynchronize	hcEventSynchronize
+#define cudaEventCreate(pe)	hcEventCreate(pe)
+#define cudaEventDestroy(ev)	hcEventDestroy(ev)
+#define cudaEventRecord(ev)	hcEventRecord(ev)
+#define cudaEventElapsedTime(pt, start, stop)	hcEventElapsedTime(pt, start, stop)
+#define cudaEventSynchronize()	hcEventSynchronize()
+
 #define cudaGetDeviceProperties(prop, devid)	hcGetDeviceProperties(prop, devid)
+
 #define cudaDeviceGetP2PAttribute(v, attr, src, dst)	hcDeviceGetP2PAttribute(v, attr, src, dst)
 #define cudaDevP2PAttrPerformanceRank	hcDevP2PAttrPerformanceRank
 #define cudaDevP2PAttrAccessSupported	hcDevP2PAttrAccessSupported
 #define cudaDevP2PAttrNativeAtomicSupported	hcDevP2PAttrNativeAtomicSupported
+
 #define cudaDeviceSynchronize()	hcDeviceSynchronize()
