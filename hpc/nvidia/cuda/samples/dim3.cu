cf7d5152167bad559ed7ed7582d47a3e9e406580
Rong Tao <rongtao@cestc.cn>
2025-07-28 16:29:53 +0800

    cuda: dim3.cu: add it= argument
        (base) [root@ai-compute1 samples]# ./dim3 bx=200 it=50
        Usage: ./dim3 [bx|by|bz|gx|gy|gz=<N>] [it=<INTERVAL>]
        <<< grid(3,1,1), block(200,1,1) >>>
        threadIdx(0,0,0), blockIdx(200,1,1), blockDim(0,0,0), gridDim(3,1,1) (0,0,0)
        threadIdx(50,0,0), blockIdx(200,1,1), blockDim(0,0,0), gridDim(3,1,1) (50,0,0)
        threadIdx(100,0,0), blockIdx(200,1,1), blockDim(0,0,0), gridDim(3,1,1) (100,0,0)
        threadIdx(150,0,0), blockIdx(200,1,1), blockDim(0,0,0), gridDim(3,1,1) (150,0,0)
        threadIdx(0,0,0), blockIdx(200,1,1), blockDim(1,0,0), gridDim(3,1,1) (200,0,0)
        threadIdx(50,0,0), blockIdx(200,1,1), blockDim(1,0,0), gridDim(3,1,1) (250,0,0)
        threadIdx(100,0,0), blockIdx(200,1,1), blockDim(1,0,0), gridDim(3,1,1) (300,0,0)
        threadIdx(150,0,0), blockIdx(200,1,1), blockDim(1,0,0), gridDim(3,1,1) (350,0,0)
        threadIdx(0,0,0), blockIdx(200,1,1), blockDim(2,0,0), gridDim(3,1,1) (400,0,0)
        threadIdx(50,0,0), blockIdx(200,1,1), blockDim(2,0,0), gridDim(3,1,1) (450,0,0)
        threadIdx(100,0,0), blockIdx(200,1,1), blockDim(2,0,0), gridDim(3,1,1) (500,0,0)
        threadIdx(150,0,0), blockIdx(200,1,1), blockDim(2,0,0), gridDim(3,1,1) (550,0,0)
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/samples/dim3.cu
+++ hpc/nvidia/cuda/samples/dim3.cu
@@ -9,11 +9,14 @@
 #include "cuda_helpers.h"
 #endif
 
-__global__ void checkIndex(void)
+__global__ void checkIndex(int it)
 {
 	int ix = threadIdx.x + blockDim.x * blockIdx.x;
 	int iy = threadIdx.y + blockDim.y * blockIdx.y;
 	int iz = threadIdx.z + blockDim.z * blockIdx.z;
+
+	if (ix % it != 0 || iy % it != 0 || iz % it != 0)
+		return;
 
 	printf("threadIdx(%d,%d,%d), blockIdx(%d,%d,%d), blockDim(%d,%d,%d), gridDim(%d,%d,%d) (%d,%d,%d)\n",
 		threadIdx.x, threadIdx.y, threadIdx.z,
@@ -31,33 +34,36 @@ __global__ void checkIndex(void)
 
 int main(int argc, char *argv[])
 {
-	int i, bx, by, bz, gx, gy, gz;
+	int i, bx, by, bz, gx, gy, gz, it;
 
 	gpu_init(0);
 
+	it = 1;
 	bx = gx = 3;
 	by = gy = 1;
 	bz = gz = 1;
 
 	for (i = 1; i < argc; i++) {
-#define xyz(v) if (!strncmp(#v"=", argv[i], 3)) v = atoi(argv[i] + 3);
-		xyz(bx);
-		xyz(by);
-		xyz(bz);
-		xyz(gx);
-		xyz(gy);
-		xyz(gz);
-#undef xyz
+#define arg_eq(v) if (!strncmp(#v"=", argv[i], strlen(#v) + 1)) \
+			v = atoi(argv[i] + strlen(#v) + 1);
+		arg_eq(bx);
+		arg_eq(by);
+		arg_eq(bz);
+		arg_eq(gx);
+		arg_eq(gy);
+		arg_eq(gz);
+		arg_eq(it);
+#undef arg_eq
 	}
 
-	fprintf(stderr, "Usage: %s [bx|by|bz|gx|gy|gz=<N>]\n", argv[0]);
+	fprintf(stderr, "Usage: %s [bx|by|bz|gx|gy|gz=<N>] [it=<INTERVAL>]\n", argv[0]);
 	fprintf(stderr, "<<< grid(%d,%d,%d), block(%d,%d,%d) >>>\n",
 		gx, gy, gz, bx, by, bz);
 
 	dim3 block(bx, by, bz);
 	dim3 grid(gx, gy, gz);
 
-	checkIndex<<<grid, block>>>();
+	checkIndex<<<grid, block>>>(it);
 
 	/* flush printf */
 	cudaDeviceSynchronize();
