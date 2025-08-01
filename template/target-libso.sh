bb5b800ae25e277f9c7d690d3ec8eb255e18a8ae
Rong Tao <rongtao@cestc.cn>
2025-08-01 18:18:17 +0800

    target-libso.sh: support _+- in dynamic library name
    and libtest-linux-c.so release 0.1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/target-libso.sh
+++ template/target-libso.sh
@@ -3,9 +3,9 @@ set -e
 
 libso_multi_version() {
 	local name_orig=$1
-	local name_v_v=$(echo ${name_orig} | grep -oE "lib[0-9a-zA-Z]+\.so\.[0-9]+\.[0-9]+")
-	local name_v=$(echo ${name_orig} | grep -oE "lib[0-9a-zA-Z]+\.so\.[0-9]+")
-	local name=$(echo ${name_orig} | grep -oE "lib[0-9a-zA-Z]+\.so")
+	local name_v_v=$(echo ${name_orig} | grep -oE "lib[0-9a-zA-Z_\+\-]+\.so\.[0-9]+\.[0-9]+")
+	local name_v=$(echo ${name_orig} | grep -oE "lib[0-9a-zA-Z_\+\-]+\.so\.[0-9]+")
+	local name=$(echo ${name_orig} | grep -oE "lib[0-9a-zA-Z_\+\-]+\.so")
 	if [[ ${name_orig} == ${name_v_v} ]]; then
 		rm -f ${name_v} ${name}
 		ln -s ${name_orig} ${name_v}
