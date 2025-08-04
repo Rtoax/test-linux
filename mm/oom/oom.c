e0da745b12ab714630e23ec0761ed2c2eabd0a6b
Rong Tao <rongtao@cestc.cn>
2025-08-04 14:24:32 +0800

    Revert "oom.c: display cgroup in verbose mode"
    Since commit f1c7ac53ac96 ("Revert "proc_helpers: add proc_cgroup() and proc_pid_cgroup()"")
    
    This reverts commit 5174028ee59bb8f20edd885a510b681f2cf6d226.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- mm/oom/oom.c
+++ mm/oom/oom.c
@@ -335,9 +335,8 @@ int main(int argc, char *argv[])
 	}
 
 	if (verbose) {
-		char comm[64], cgroup[256];
+		char comm[64];
 		printf("pid %d, comm %s\n", getpid(), proc_comm(comm, sizeof(comm)));
-		printf("cgroup %s\n", proc_cgroup(cgroup, sizeof(cgroup)));
 		printf("oom_adj %d\n", get_oom_adj(getpid()));
 		printf("oom_score_adj %d\n", get_oom_score_adj(getpid()));
 		printf("oom_score %d\n", get_oom_score(getpid()));
