8e97a68bfbd86742159d0e9283c49818842de63b
Rong Tao <rongtao@cestc.cn>
2025-08-04 14:27:29 +0800

    oom: cgroup-oom.sh: pass param to ./oom
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- mm/oom/cgroup-oom.sh
+++ mm/oom/cgroup-oom.sh
@@ -15,4 +15,4 @@ echo ${pid} | sudo tee /sys/fs/cgroup/oom-test/cgroup.procs
 echo $((1024*1024*2)) | sudo tee /sys/fs/cgroup/oom-test/memory.max
 echo $((1024*1024*2)) | sudo tee /sys/fs/cgroup/oom-test/memory.high
 
-./oom -v
+./oom -v $@
