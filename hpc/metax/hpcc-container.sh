7772c8a74244cffeb260a0031905e0d19b3bb321
Rong Tao <rongtao@cestc.cn>
2025-08-05 11:10:49 +0800

    metax: hpcc-container.sh: update IMAGE
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/metax/hpcc-container.sh
+++ hpc/metax/hpcc-container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -ex
 
-IMAGE=registry.cn-shanghai.aliyuncs.com/insight-tech/x-deepspeed:hpcc2.32.0.5-torch2.4-py310-kylin2309a-arm64
+IMAGE=registry.cn-shanghai.aliyuncs.com/insight-tech/x-modelzoo.vllm:hpcc2.31.0.4-torch2.4-py310-kylin2309a-arm64
+#IMAGE=registry.cn-shanghai.aliyuncs.com/insight-tech/x-deepspeed:hpcc2.32.0.5-torch2.4-py310-kylin2309a-arm64
 
 sudo podman run --rm -ti --network=host --privileged \
-	-v /home/rongtao/:/root/ \
+	-v /home/rongtao/:/root/ ${IMAGE} \
 	bash
