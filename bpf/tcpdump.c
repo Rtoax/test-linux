0c769f01a87a8f9c0226e3124c64f8e1501b4359
Rong Tao <rongtao@cestc.cn>
2025-08-06 14:48:12 +0800

    bpf: tcpdump.c: tcpdump tcp port 80
        rongtao@NUC10:~/Git/tst-linux/bpf$ sudo ./tcpdump eno1
        IPv4 proto=6(tcp) src=10.56.52.9 dst=182.61.201.211
        IPv4 proto=6(tcp) src=182.61.201.211 dst=10.56.52.9
        IPv4 proto=6(tcp) src=10.56.52.9 dst=182.61.201.211
        IPv4 proto=6(tcp) src=10.56.52.9 dst=182.61.201.211
        IPv4 proto=6(tcp) src=182.61.201.211 dst=10.56.52.9
        IPv4 proto=6(tcp) src=182.61.201.211 dst=10.56.52.9
        IPv4 proto=6(tcp) src=182.61.201.211 dst=10.56.52.9
        IPv4 proto=6(tcp) src=182.61.201.211 dst=10.56.52.9
        IPv4 proto=6(tcp) src=182.61.201.211 dst=10.56.52.9
        IPv4 proto=6(tcp) src=10.56.52.9 dst=182.61.201.211
        IPv4 proto=6(tcp) src=10.56.52.9 dst=182.61.201.211
        IPv4 proto=6(tcp) src=10.56.52.9 dst=182.61.201.211
        IPv4 proto=6(tcp) src=182.61.201.211 dst=10.56.52.9
        IPv4 proto=6(tcp) src=182.61.201.211 dst=10.56.52.9
        IPv4 proto=6(tcp) src=182.61.201.211 dst=10.56.52.9
        IPv4 proto=6(tcp) src=10.56.52.9 dst=182.61.201.211
    
        rongtao@NUC10:~/Git/tst-linux/bpf$ curl baidu.com
        <html>
        <meta http-equiv="refresh" content="0;url=http://www.baidu.com/">
        </html>
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/tcpdump.c
+++ bpf/tcpdump.c
@@ -34,7 +34,7 @@
  * NOTE: you could use 'tcpdump -dd' to fill sock_filter structure.
  */
 static struct sock_filter bpfcode[] = {
-/* tcpdump tcp dst port 80 */
+#if 0 /* tcpdump tcp dst port 80 */
 /*(001)*/{ OP_LDH, 0, 0, 12          }, /* ldh [12] */
 /*(002)*/{ OP_JEQ, 0, 5, ETH_P_IP    }, /* jeq #0x800, jt 3, jf 8 */
 /*(003)*/{ OP_LDB, 0, 0, 23          }, /* ldb [23] # 14 Ether, 9 IP proto */
@@ -43,6 +43,18 @@ static struct sock_filter bpfcode[] = {
 /*(006)*/{ OP_JEQ, 0, 1, 80          }, /* jeq #0x50, jt 7, jf 8 */
 /*(007)*/{ OP_RET, 0, 0, -1,         }, /* ret #0xffffffff # (accept) */
 /*(008)*/{ OP_RET, 0, 0, 0           }, /* ret #0x0 # (reject) */
+#else /* tcpdump tcp port 80 */
+/*(001)*/{ OP_LDH, 0, 0, 12          }, /* ldh [12] */
+/*(002)*/{ OP_JEQ, 0, 7, ETH_P_IP    }, /* jeq #0x800, jt 3, jf 10 */
+/*(003)*/{ OP_LDB, 0, 0, 23          }, /* ldb [23] # 14 Ether, 9 IP proto */
+/*(004)*/{ OP_JEQ, 0, 5, IPPROTO_TCP }, /* jeq #0x6, jt 5, jf 10 */
+/*(005)*/{ OP_LDH, 0, 0, 36          }, /* ldh [36] # 14 Ether, 20 IPhdr, 2 dport */
+/*(006)*/{ OP_JEQ, 2, 0, 80          }, /* jeq #0x50, jt 9, jf 7 */
+/*(007)*/{ OP_LDH, 0, 0, 34          }, /* ldh [34] # 14 Ether, 20 IPhdr, 0 sport */
+/*(008)*/{ OP_JEQ, 0, 1, 80          }, /* jeq #0x50, jt 9, jf 10 */
+/*(009)*/{ OP_RET, 0, 0, -1,         }, /* ret #0xffffffff # (accept) */
+/*(010)*/{ OP_RET, 0, 0, 0           }, /* ret #0x0 # (reject) */
+#endif
 };
 
 int main(int argc, char **argv)
