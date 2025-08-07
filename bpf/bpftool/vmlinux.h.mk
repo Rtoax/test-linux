07b7ca4295b03420dce4506d9f3b730d7bacd19c
Rong Tao <rongtao@cestc.cn>
2025-08-07 09:33:30 +0800

    vmlinux.h.mk: Rename bpf_gen_vmlinux_h()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/bpftool/vmlinux.h.mk
+++ bpf/bpftool/vmlinux.h.mk
@@ -3,14 +3,14 @@ Q ?= @
 BPFTOOL ?= bpftool
 VMLINUX_H ?= vmlinux.h
 
-define gen_btf_h
+define bpf_gen_btf_h
 	$(Q)${BPFTOOL} btf dump file /sys/kernel/btf/${1} format c > ${2}
 endef
 
-define gen_vmlinux_h
-$(call gen_btf_h,vmlinux,${VMLINUX_H})
+define bpf_gen_vmlinux_h
+$(call bpf_gen_btf_h,vmlinux,${VMLINUX_H})
 endef
 
 ${VMLINUX_H}:
 	@echo -e "  BPFTOOL  \033[1;32m$(@)\033[m"
-	$(call gen_vmlinux_h,${VMLINUX_H})
+	$(call bpf_gen_vmlinux_h,${VMLINUX_H})
