284594f0851856f0d0a3e51bc2ed730af956b07f
Rong Tao <rongtao@cestc.cn>
2025-08-07 15:49:48 +0800

    kmod/kbuild: hello.c: add ok=1 param
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- module/kbuild/hello.c
+++ module/kbuild/hello.c
@@ -2,6 +2,9 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+static int ok = 0;
+module_param(ok, int, 0660);
+
 static int hello_init(void)
 {
 	printk("Hi, hello\n");
@@ -18,10 +21,16 @@ static int hello_init(void)
 #pragma message("hello2")
 #endif
 	printk("CURRENT_PATH = %s\n", CURRENT_PATH);
+	if (ok)
+		return 0;
 	return -EINVAL;
 }
 
+static void hello_exit(void)
+{}
+
 module_init(hello_init);
+module_exit(hello_exit);
 
 MODULE_DESCRIPTION("My kernel hello module");
 MODULE_AUTHOR("Rong Tao");
