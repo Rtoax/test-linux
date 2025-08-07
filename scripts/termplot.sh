a9bac18efd134b69da4f5365cb7c34d0b7709c7b
Rong Tao <rongtao@cestc.cn>
2025-08-07 18:14:23 +0800

    termplot.sh: update
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- scripts/termplot.sh
+++ scripts/termplot.sh
@@ -17,27 +17,30 @@ readonly B10='▼'
 readonly B11='◀'
 readonly B12='►'
 
-height_start=5
-width_start=5
-height=20
-width=$(( ${TERM_WIDTH} - ${width_start} * 2 ))
-
 gotoxy() {
 	local x=$2 y=$1
 	printf "\033[%d;%df" ${y} ${x}
 }
 
-# $1 - x
-# $2 - y
-# $3 - length
+# $1 - x start
+# $2 - x end
+# $3 - y
 # $4 - with arrow or not
 horizontal_line() {
-	local ix x y len arrow
-	x=$1
-	y=$2
-	len=$3
+	local ix xstart xend y arrow seq_args
+
+	xstart=$1
+	xend=$2
+	y=$3
 	arrow=$4
-	for ix in $(seq $x 1 $(($x + $len)))
+
+	if [[ $xstart -lt $xend ]]; then
+		seq_args="$xstart 1 $xend"
+	else
+		seq_args="$xstart -1 $xend"
+	fi
+
+	for ix in $(seq $seq_args)
 	do
 		gotoxy $y $ix
 		printf "%s" ${B8}
@@ -53,5 +56,5 @@ horizontal_line() {
 #}
 
 clear
-horizontal_line ${width_start} ${height_start} ${width} ${B12}
+horizontal_line 4 $((${TERM_WIDTH} - 0)) ${TERM_HEIGHT} ${B12}
 echo
