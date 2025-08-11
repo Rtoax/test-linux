27fd5389c8fc150e9b4b696e23fe0a437427282f
Rong Tao <rongtao@cestc.cn>
2025-08-11 19:45:16 +0800

    c/float.c: add function float_to_fp32()
    rongtao@NUC10:~/Git/tst-linux/c$ ./float
    -nan
          0.00 : 0  0        0
          1.00 : 1  31       64
          2.00 : 0  32       0
          4.00 : 0  32       64
         -1.00 : 1  95       64
      -1024.00 : 0  98       64
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/float.c
+++ c/float.c
@@ -3,19 +3,36 @@
 #include <byteswap.h>
 
 struct fp32 {
-	int sign:1;
-	int exponent:8;
-	int mantissa:23;
+	unsigned int sign:1;
+	unsigned int exponent:8;
+	unsigned int mantissa:23;
 } __attribute__((packed));
+
+static struct fp32 fp32_NaN = {1, 255, 255};
+
+void float_to_fp32(const float f)
+{
+	float tmp = f;
+	int32_t *i32 = (int32_t *)&tmp;
+	*i32 = bswap_32(*i32);
+	struct fp32 *fp32 = (struct fp32 *)i32;
+	printf("%10.2f : %-2d %-8d %-23d\n", f, fp32->sign, fp32->exponent, fp32->mantissa);
+}
 
 int main(void)
 {
-	struct fp32 fp32_Inf = {1, 255, 255};
-
-	int32_t *i32 = (int32_t *)&fp32_Inf;
-	bswap_32(*i32);
+	int32_t *i32 = (int32_t *)&fp32_NaN;
+	*i32 = bswap_32(*i32);
 
 	float *f32 = (float *)i32;
 	printf("%f\n", *f32);
+
+	float_to_fp32(0.0f);
+	float_to_fp32(1.0f);
+	float_to_fp32(2.0f);
+	float_to_fp32(4.0f);
+	float_to_fp32(-1.0f);
+	float_to_fp32(-1024.0f);
+
 	return 0;
 }
