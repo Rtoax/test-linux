b769ead1f847b5f6cec689ae47f7b2f362933f5f
Rong Tao <rongtao@cestc.cn>
2025-08-11 19:50:43 +0800

    c/float.c: display more
    rongtao@NUC10:~/Git/tst-linux/c$ ./float
    -nan
          0.00 : 00000000 : 00000000 : 0  0        0
          1.00 : 0000803f : 3f800000 : 1  31       64
          2.00 : 00000040 : 40000000 : 0  32       0
          4.00 : 00008040 : 40800000 : 0  32       64
          5.00 : 0000a040 : 40a00000 : 0  32       80
         -1.00 : 000080bf : bf800000 : 1  95       64
      -1024.00 : 000080c4 : c4800000 : 0  98       64
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/float.c
+++ c/float.c
@@ -14,9 +14,13 @@ void float_to_fp32(const float f)
 {
 	float tmp = f;
 	int32_t *i32 = (int32_t *)&tmp;
+	int32_t i32_orig = *i32;
+
 	*i32 = bswap_32(*i32);
 	struct fp32 *fp32 = (struct fp32 *)i32;
-	printf("%10.2f : %-2d %-8d %-23d\n", f, fp32->sign, fp32->exponent, fp32->mantissa);
+
+	printf("%10.2f : %08x : %08x : %-2d %-8d %-23d\n", f, *i32, i32_orig,
+		fp32->sign, fp32->exponent, fp32->mantissa);
 }
 
 int main(void)
@@ -31,6 +35,7 @@ int main(void)
 	float_to_fp32(1.0f);
 	float_to_fp32(2.0f);
 	float_to_fp32(4.0f);
+	float_to_fp32(5.0f);
 	float_to_fp32(-1.0f);
 	float_to_fp32(-1024.0f);
 
