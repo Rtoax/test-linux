20ef9e51c321953726352855da6a94b80d3cfad3
Rong Tao <rongtao@cestc.cn>
2025-08-15 09:26:30 +0800

    c/float: add double/fp64 support
    rongtao@NUC10:~/Git/tst-linux/c$ ./float
    0.000000 vs 0.000000 (0 0 0)
    1.200000 vs 1.200000 (0 7f 19999a)
    0.200000 vs 0.200000 (0 7c 4ccccd)
    1.234568 vs 1.234568 (0 7f 1e0652)
    0.234568 vs 0.234568 (0 7c 703291)
    3.141593 vs 3.141593 (0 80 490fdb)
    -3.141593 vs -3.141593 (1 80 490fdb)
    0.000000 vs 0.000000 (0 0 0)
    1.200000 vs 1.200000 (0 3ff 3333333333333)
    0.200000 vs 0.200000 (0 3fc 999999999999a)
    1.234568 vs 1.234568 (0 3ff 3c0ca4283de1b)
    0.234568 vs 0.234568 (0 3fc e0652141ef0dc)
    3.141593 vs 3.141593 (0 400 921fb53c8d4f1)
    -3.141593 vs -3.141593 (1 400 921fb53c8d4f1)
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/float.c
+++ c/float.c
@@ -1,7 +1,8 @@
 /**
- * https://en.wikipedia.org/wiki/IEEE_754-2008_revision
- * https://en.wikipedia.org/wiki/Single-precision_floating-point_format
- * https://en.wikipedia.org/wiki/Double-precision_floating-point_format
+ * Refs:
+ * - https://en.wikipedia.org/wiki/IEEE_754-2008_revision
+ * - https://en.wikipedia.org/wiki/Single-precision_floating-point_format
+ * - https://en.wikipedia.org/wiki/Double-precision_floating-point_format
  */
 #include <assert.h>
 #include <stdio.h>
@@ -37,11 +38,17 @@ typedef struct fp32 {
 #endif
 } __attribute__((packed)) fp32_t;
 
+/* https://en.wikipedia.org/wiki/Single-precision_floating-point_format */
 const fp32_t fp32_NaN = FP32(1, 255, 255);
 const fp32_t fp32_Inf = FP32(1, 255, 0);
 const fp32_t fp32_Zero = FP32(0, 0, 0);
-/* see https://en.wikipedia.org/wiki/Single-precision_floating-point_format */
 const fp32_t fp32_0dot15625 = FP32(0, 0x7c, 0x200000);
+
+/* https://en.wikipedia.org/wiki/Double-precision_floating-point_format */
+const fp64_t fp64_NaN = FP64(1, 0x7ff, 0xfffffffffffff);
+const fp64_t fp64_Inf = FP64(1, 0x7ff, 0);
+const fp64_t fp64_Zero = FP64(0, 0, 0);
+
 
 /* Could use to both float and double */
 double fraction_value(uint64_t fraction, uint64_t nbits)
@@ -56,6 +63,56 @@ double fraction_value(uint64_t fraction, uint64_t nbits)
 		fra += exp2f(-1.0f * i);
 	}
 	return fra;
+}
+
+void double_to_fp64(const double d, fp64_t *fp64)
+{
+	double tmp = d;
+	int64_t i64 = *(int64_t *)&tmp;
+
+	*fp64 = *(fp64_t *)&i64;
+}
+
+double fp64_to_double(const fp64_t *fp64)
+{
+	double f;
+	int64_t i64 = *(int64_t *)fp64;
+
+	f = *(double *)&i64;
+	(void)f;
+
+	double sign = 1 - 2 * (fp64->sign % 2);
+	double e2, fra;
+
+	if (fp64->exponent == 0) {
+		if (fp64->fraction == 0) {
+			return sign * 0.0;
+		} else {
+			e2 = exp2(-1022.0);
+			fra = 0 + fraction_value(fp64->fraction, 52);
+		}
+	} else if (fp64->exponent == 0xff) {
+		if (fp64->fraction == 0)
+			return sign * *(double *)&fp64_Inf;
+		else
+			return *(double *)&fp64_NaN;
+	} else {
+		e2 = exp2(fp64->exponent - 1023.0);
+		fra = 1 + fraction_value(fp64->fraction, 52);
+	}
+
+	return sign * e2 * fra;
+}
+
+void check_fp64(double f)
+{
+	double to;
+	fp64_t fp64;
+
+	double_to_fp64(f, &fp64);
+	to = fp64_to_double(&fp64);
+
+	printf("%lf vs %lf (%x %x %lx)\n", f, to, fp64.sign, fp64.exponent, (uint64_t)fp64.fraction);
 }
 
 void float_to_fp32(const float f, fp32_t *fp32)
@@ -116,8 +173,18 @@ int main(void)
 	check_fp32(0);
 	check_fp32(1.2f);
 	check_fp32(0.2f);
+	check_fp32(1.23456789f);
+	check_fp32(0.23456789f);
 	check_fp32(3.14159265f);
 	check_fp32(-3.14159265f);
+
+	check_fp64(0);
+	check_fp64(1.2);
+	check_fp64(0.2);
+	check_fp64(1.23456789);
+	check_fp64(0.23456789);
+	check_fp64(3.14159265);
+	check_fp64(-3.14159265);
 
 	return 0;
 }
