6b47bf0760e02ebe42d1df9bf1b28971fb29ef76
Rong Tao <rongtao@cestc.cn>
2025-08-18 16:08:24 +0800

    ksym_helpers: inseart ksym to tree
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- fs/procfs/kallsyms/ksym_helpers.c
+++ fs/procfs/kallsyms/ksym_helpers.c
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
 /* Copyright (c) 2025 Rong Tao */
+#include <assert.h>
 #include <errno.h>
 #include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <search.h>
 
 #include "ksym_helpers.h"
 
@@ -12,7 +14,7 @@
 
 
 enum ksym_type {
-	KSYM_LOCAL_ABS,		/* a */
+	KSYM_LOCAL_ABS = 1,	/* a */
 	KSYM_GLOBAL_ABS,	/* A */
 	KSYM_LOCAL_BSS,		/* b */
 	KSYM_GLOBAL_BSS,	/* B */
@@ -29,15 +31,36 @@ enum ksym_type {
 };
 
 struct ksym {
-	unsigned long address;
+	unsigned long addr;
 	enum ksym_type type;
 	char *name;
 	char *kmod;	/* optional */
 };
 
 struct ksyms {
-	void *tree_handle;
+	size_t nsyms;
+	void *root_nkta;
 };
+
+static struct ksyms ksyms = {0, NULL};
+
+static int ksym_cmp_nkta(const void *a1, const void *a2)
+{
+	int cmp;
+	const struct ksym *s1 = a1, *s2 = a2;
+	cmp = strcmp(s1->name, s2->name);
+	if (cmp)
+		return cmp;
+	if (s1->kmod && s2->kmod) {
+		cmp = strcmp(s1->kmod, s2->kmod);
+		if (cmp)
+			return cmp;
+	}
+	cmp = s1->type - s2->type;
+	if (cmp)
+		return cmp;
+	return s1->addr - s2->addr;
+}
 
 static struct ksym *alloc_ksym(unsigned long addr, char c_type, char *name,
 			       char *kmod)
@@ -45,7 +68,7 @@ static struct ksym *alloc_ksym(unsigned long addr, char c_type, char *name,
 	struct ksym *ksym = malloc(sizeof(struct ksym));
 
 	memset(ksym, 0, sizeof(struct ksym));
-	ksym->address = addr;
+	ksym->addr = addr;
 
 	switch (c_type) {
 #define CASE(c, e)	case c: ksym->type = e; break
@@ -68,9 +91,11 @@ static struct ksym *alloc_ksym(unsigned long addr, char c_type, char *name,
 		goto invalid;
 		break;
 	}
+
 	ksym->name = strdup(name);
+
 	if (kmod)
-		ksym->name = strdup(kmod);
+		ksym->kmod = strdup(kmod);
 
 	return ksym;
 
@@ -102,15 +127,31 @@ int load_kallsyms(void)
 		return -errno;
 
 	while (fgets(line, sizeof(line), fp)) {
+		memset(s_name, 0, sizeof(s_name));
 		memset(s_kmod, 0, sizeof(s_kmod));
 
 		n = sscanf(line, "%s %c %s %s\n", s_addr, &c_type, s_name, s_kmod);
 		if (n != 4 && n != 3)
 			continue;
 
-		struct ksym *sym = alloc_ksym(strtoull(s_addr, NULL, 16),
-				c_type, s_name, s_kmod);
-		free_ksym(sym);
+		struct ksym *new = alloc_ksym(strtoull(s_addr, NULL, 16),
+				c_type, s_name, n == 4 ? s_kmod : NULL);
+		if (!new)
+			continue;
+
+#ifdef DEBUG
+		printf("%d %lx %c %s %s\n", n, new->addr, c_type, new->name, new->kmod);
+#endif
+
+		struct ksym **old = tsearch(new, &ksyms.root_nkta, ksym_cmp_nkta);
+		assert(old && "tsearch() failed");
+
+		/* already exit */
+		if (*old != new) {
+			free_ksym(new);
+		} else {
+			ksyms.nsyms++;
+		}
 	}
 
 	fclose(fp);
