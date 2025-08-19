6e462c4ddb0080551c86d7b9db8b4c7fef48a820
Rong Tao <rongtao@cestc.cn>
2025-08-19 10:11:38 +0800

    hpc: Move code from metax to cuda and re-create symlinks
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/samples/parallel.cu
+++ hpc/nvidia/cuda/samples/parallel.cu
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0
 /* Copyright (c) 2025 Rong Tao */
 #include <stdio.h>
+#if defined(HAVE_HCCL)
+#include <hc_runtime.h>
+#include "hpcc_helpers.h"
+#include "cuda2hccl.h"
+#else
 #include <cuda_runtime.h>
 #include "cuda_helpers.h"
+#endif
 
 
 __global__ void foo(int count)
