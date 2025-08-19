5102d795253bb7d20890d1129d0af96362c381f4
Rong Tao <rongtao@cestc.cn>
2025-08-19 19:05:38 +0800

    metax: hpcc-container.sh: update hpcc 2.33.1.3
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/metax/hpcc-container.sh
+++ hpc/metax/hpcc-container.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 
-IMAGE=registry.cn-shanghai.aliyuncs.com/insight-tech/x-modelzoo.vllm:hpcc2.31.0.4-torch2.4-py310-kylin2309a-arm64
+IMAGE=sw-harbor-lg.mxcr.io/ai-release/hpcc/modelzoo.llm.vllm:hpcc.ai2.33.1.3-torch2.6-py310-kylin2309a-arm64
+#IMAGE=registry.cn-shanghai.aliyuncs.com/insight-tech/x-modelzoo.vllm:hpcc2.31.0.4-torch2.4-py310-kylin2309a-arm64
 #IMAGE=registry.cn-shanghai.aliyuncs.com/insight-tech/x-deepspeed:hpcc2.32.0.5-torch2.4-py310-kylin2309a-arm64
 
 sudo podman run --rm -ti --network=host --privileged \
