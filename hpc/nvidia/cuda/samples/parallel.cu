6d4e52d36f00bc821845b6f6253db9f26be05bd1
Rong Tao <rongtao@cestc.cn>
2025-08-20 09:43:29 +0800

    cuda: parallel.cu: test cudaDeviceSynchronize() for __device__ funcs
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/samples/parallel.cu
+++ hpc/nvidia/cuda/samples/parallel.cu
@@ -10,23 +10,41 @@
 #include "cuda_helpers.h"
 #endif
 
+__device__ void device1(int id)
+{
+	printf("1 %d\n", id);
+}
+
+__device__ void device2(int id)
+{
+	printf("2 %d\n", id);
+}
 
 __global__ void foo(int count)
 {
 	int id = threadIdx.x + blockDim.x * blockIdx.x;
 	if (id >= count)
 		return;
-	/* do something */
+
+	device1(id);
+
+	cudaDeviceSynchronize();
+
+	device2(id);
 
 	__syncthreads();
 }
 
 int main(void)
 {
-	int count = 10000;
+	int count = 10;
 
 	gpu_init(0);
 
 	foo<<<(count + 511) / 512, 512>>>(count);
+
+	/* flush printf */
+	cudaDeviceSynchronize();
+
 	return 0;
 }
