6b8aa9285bfa518a5cbd8903db4944e846d2aec3
Rong Tao <rongtao@cestc.cn>
2025-08-22 14:44:52 +0800

    pytorch: as_tensor.py: display shape,dtype,ndim
    torch.Size([512, 512, 3])
    torch.uint8
    3
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- ai/pytorch/samples/as_tensor.py
+++ ai/pytorch/samples/as_tensor.py
@@ -7,5 +7,10 @@ import matplotlib.pyplot as plt
 
 # wget https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png -O lenna.jpg
 np_image = np.array(Image.open("lenna.png"))
+# Or: torch.as_tensor(a, device=torch.device('cuda'))
 image = torch.as_tensor(np_image)
 plt.imshow(image)
+
+print(image.shape)
+print(image.dtype)
+print(image.ndim)
