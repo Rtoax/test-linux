7da395334f7ee63466542bb86b0204232c719cbf
Rong Tao <rongtao@cestc.cn>
2025-08-22 18:48:28 +0800

    python: add numpy
    Link: https://colab.research.google.com/github/dvgodoy/PyTorchStepByStep/blob/master/Chapter00.ipynb
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to
 
 #### Breaking Changes
 #### Added
+- python: add numpy;
 #### Changed
 #### Deprecated
 #### Removed
---
+++ python/numpy/random.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+
+import numpy as np
+
+true_b = 1
+true_w = 2
+N = 100
+
+# Data Generation
+np.random.seed(42)
+x = np.random.rand(N, 1)
+epsilon = (.1 * np.random.randn(N, 1))
+y = true_b + true_w * x + epsilon
