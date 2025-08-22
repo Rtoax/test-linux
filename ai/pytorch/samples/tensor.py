6de85fda9db08ed61e354f2a9b72233f028279d5
Rong Tao <rongtao@cestc.cn>
2025-08-22 14:25:45 +0800

    pytorch: tensor.py: display all
        rongtao@NUC10:~/Git/tst-linux/ai/pytorch/samples$ ./tensor.py
        tensor([[0.1000, 1.2000],
                [2.2000, 3.1000],
                [4.9000, 5.2000]])
        tensor([[0.1000, 1.2000],
                [2.2000, 3.1000],
                [4.9000, 5.2000]], dtype=torch.float64)
        tensor([[0., 0., 0.],
                [0., 0., 0.]])
        tensor([[1., 1., 1.],
                [1., 1., 1.]])
        tensor([[1., 0., 0.],
                [0., 1., 0.],
                [0., 0., 1.]])
        torch.Size([3, 2])
        torch.Size([3, 2])
        torch.float32
        torch.FloatTensor
        2
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- ai/pytorch/samples/tensor.py
+++ ai/pytorch/samples/tensor.py
@@ -4,18 +4,18 @@ import torch
 import numpy as np
 
 def test_tensor():
-    torch.tensor([[0.1, 1.2], [2.2, 3.1], [4.9, 5.2]])
-    torch.tensor(np.array([[0.1, 1.2], [2.2, 3.1], [4.9, 5.2]]))
-    torch.zeros(2,3)
-    torch.ones(2,3)
-    torch.eye(3)
+    print(torch.tensor([[0.1, 1.2], [2.2, 3.1], [4.9, 5.2]]))
+    print(torch.tensor(np.array([[0.1, 1.2], [2.2, 3.1], [4.9, 5.2]])))
+    print(torch.zeros(2,3))
+    print(torch.ones(2,3))
+    print(torch.eye(3))
 
     x = torch.tensor([[ 0.1000, 1.2000], [ 2.2000, 3.1000], [ 4.9000, 5.2000]])
-    x.shape
-    x.size()
-    x.dtype
-    x.type()
-    x.ndim
+    print(x.shape)
+    print(x.size())
+    print(x.dtype)
+    print(x.type())
+    print(x.ndim)
 
 
 if __name__ == "__main__":
