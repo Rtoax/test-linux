772485e24593581a5255004663aff1946ec92b98
Rong Tao <rongtao@cestc.cn>
2025-08-22 13:50:33 +0800

    pytorch: add cuda.py
    Link: https://github.com/pytorch/pytorch
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ ai/pytorch/cuda/cuda.py
@@ -0,0 +1,12 @@
+#!/bin/env python
+
+import torch
+
+def check_env():
+    if torch.cuda.is_available():
+        print("Support cuda")
+    else:
+        print("Not support cuda")
+
+if __name__ == "__main__":
+    check_env()
