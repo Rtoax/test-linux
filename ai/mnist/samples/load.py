11c6cbdd06fb95af68cb859a59765275a646eba3
Rong Tao <rongtao@cestc.cn>
2025-08-25 17:04:28 +0800

    mnist: load.py: load the MNIST
    Link: https://sbu-phy-ast-reu.github.io/reu-python-tutorial/11-machine-learning/keras-mnist.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- ai/mnist/samples/load.py
+++ ai/mnist/samples/load.py
@@ -1,7 +1,15 @@
 #!/bin/env python
+# Refs:
+# https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz
 
 from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
 
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data(path='mnist.npz')
 
 print(train_images.shape, train_labels.shape)
+print(train_images.dtype)
+
+print(train_labels[0])
+plt.imshow(train_images[0], cmap="gray_r")
+plt.show()
