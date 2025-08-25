032db1bb3162fe833a8e13f22def7f446e41985a
Rong Tao <rongtao@cestc.cn>
2025-08-25 10:08:15 +0800

    pytorch: cuda.py: cuda.get_device_name()
        (base) [root@muxi-4 samples]# ./cuda.py
        Is GPU[cuda] available? -> True
        We will be using device:  cuda:0
        Totally CUDA:0 Mars 03
        Totally CUDA:1 Mars 03
        Totally CUDA:2 Mars 03
        Totally CUDA:3 Mars 03
        Totally CUDA:4 Mars 03
        Totally CUDA:5 Mars 03
        Totally CUDA:6 Mars 03
        Totally CUDA:7 Mars 03
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- ai/pytorch/samples/cuda.py
+++ ai/pytorch/samples/cuda.py
@@ -6,6 +6,11 @@ def check_env():
     print("Is GPU[cuda] available? ->", torch.cuda.is_available())
     device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
     print("We will be using device: ", device)
+    if torch.cuda.is_available():
+        n_cudas = torch.cuda.device_count()
+        for i in range(n_cudas):
+            print("Totally CUDA:%d %s" % (i, torch.cuda.get_device_name(i)))
+
 
 if __name__ == "__main__":
     check_env()
