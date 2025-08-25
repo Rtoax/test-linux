398c1313341b7b81b18bf92fd48e0327d6246df4
Rong Tao <rongtao@cestc.cn>
2025-08-25 16:54:48 +0800

    ai: add mnist/samples/
    Link: https://sbu-phy-ast-reu.github.io/reu-python-tutorial/11-machine-learning/keras-mnist.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ ai/mnist/samples/load.py
@@ -0,0 +1,7 @@
+#!/bin/env python
+
+from tensorflow.keras.datasets import mnist
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+print(train_images.shape, train_labels.shape)
