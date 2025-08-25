c05a4c80068d583d63c65ca6b6c7fec283afbd1d
Rong Tao <rongtao@cestc.cn>
2025-08-25 19:59:12 +0800

    ai: move gradient-descent.py to algorithm/
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- ai/algorithm/gradient-descent.py
+++ ai/algorithm/gradient-descent.py
@@ -73,14 +73,12 @@ y = np.linspace(ymin, ymax, N)
 
 x2d, y2d = np.meshgrid(x, y, indexing="ij")
 
-fig, ax = plt.subplots()
-
-im = ax.imshow(np.log10(np.transpose(rosenbrock(x2d, y2d, a, b))),
+img = ax.imshow(np.log10(np.transpose(rosenbrock(x2d, y2d, a, b))),
                origin="lower", extent=[xmin, xmax, ymin, ymax])
-
-fig.colorbar(im, ax=ax)
 
 x0 = np.array([-1.0, 1.5])
 do_descent(drosdx, x0, args=(a, b), ax=ax)
 
+fig, ax = plt.subplots()
+fig.colorbar(img, ax=ax)
 plt.show()
