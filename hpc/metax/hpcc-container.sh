e9b51446e9c2493243dc9d66b85f26b002fde8ad
Rong Tao <rongtao@cestc.cn>
2025-08-26 09:20:20 +0800

    metax: hpcc-container.sh: update hpcc 3.0.0 to fix errors
    The following 2 problem was fixed:
    
    1. printf threadIdx.x in kernel with 0 value;
    2. printf thread_rank() in kernel with 0 value;
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/metax/hpcc-container.sh
+++ hpc/metax/hpcc-container.sh
@@ -2,7 +2,8 @@
 set -ex
 
 #IMAGE=docker.io/library/hpcc-torch2.4-py310:hc3.0.0.2-kylinv10-arm64
-IMAGE=sw-harbor-lg.mxcr.io/ai-release/hpcc/modelzoo.llm.vllm:hpcc.ai2.33.1.3-torch2.6-py310-kylin2309a-arm64
+#IMAGE=sw-harbor-lg.mxcr.io/ai-release/hpcc/modelzoo.llm.vllm:hpcc.ai2.33.1.3-torch2.6-py310-kylin2309a-arm64
+IMAGE=sw-harbor-lg.mxcr.io/ai-release/hpcc/modelzoo.llm.vllm:hpcc.ai3.0.0-torch2.6-py310-kylin2309a-arm64
 #IMAGE=registry.cn-shanghai.aliyuncs.com/insight-tech/x-modelzoo.vllm:hpcc2.31.0.4-torch2.4-py310-kylin2309a-arm64
 #IMAGE=registry.cn-shanghai.aliyuncs.com/insight-tech/x-deepspeed:hpcc2.32.0.5-torch2.4-py310-kylin2309a-arm64
 
