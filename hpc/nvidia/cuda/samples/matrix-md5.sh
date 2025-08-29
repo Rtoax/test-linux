bd65297ab915752e3d59e2f6f83180ade43ad0ab
Rong Tao <rongtao@cestc.cn>
2025-08-29 16:05:20 +0800

    hpc: matrix-md5.sh: add matrix-c-double test
    	$ make matrix-c-double targets-nvcc=
    	$ MATRIX=matrix-c-double ./matrix-md5.sh
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/samples/matrix-md5.sh
+++ hpc/nvidia/cuda/samples/matrix-md5.sh
@@ -12,6 +12,16 @@ c42ca9f2f331960dc0bcfc13dc34cd43  matrix-c-k500_m500_n500.bin
 2f65c29fa103f83db1308d9d0c9faadf  matrix-c-k700_m700_n700.bin
 28bbbaabef850ca0544a1d1150a8f682  matrix-c-k800_m800_n800.bin
 f93cbdecf242f096b0190d5746a15933  matrix-c-k900_m900_n900.bin
+c1c37b5889cc9ffe32d89889502d246d  matrix-c-double-k1000_m1000_n1000.bin
+e236c83f877bb81a80a22e2debeb8659  matrix-c-double-k100_m100_n100.bin
+cfdb9e909b9ec30f8c3a476902080a9f  matrix-c-double-k200_m200_n200.bin
+31524cb6ad131ffc1bf08d2480946637  matrix-c-double-k300_m300_n300.bin
+4940600445f44e3245c76b27d8e0e0b2  matrix-c-double-k400_m400_n400.bin
+71df988253b835226409be99fe3a81f1  matrix-c-double-k500_m500_n500.bin
+65d5c2768b0b03d71c4be24a1ca8669b  matrix-c-double-k600_m600_n600.bin
+fed78c0aea252faed8c8eb99804b321f  matrix-c-double-k700_m700_n700.bin
+18b7a82e52077e4011d2c9d3a39d7d33  matrix-c-double-k800_m800_n800.bin
+3a3571bd2277c32ee3994956035051a8  matrix-c-double-k900_m900_n900.bin
 629bbfbc540ecf9b8614c3bade229f0c  matrix-k1000_m1000_n1000.bin
 7e5ade454b857d4d9be5e53b6c7a1225  matrix-k100_m100_n100.bin
 c233b4cc0022c678d5cb6a9ea0c456d6  matrix-k200_m200_n200.bin
@@ -37,8 +47,6 @@ fmd5() {
 
 MATRIX=$(echo ${MATRIX} | tr -d './')
 
-make ${MATRIX}
-
 for x in $(seq 100 100 1000)
 do
 	./${MATRIX} -O ${MATRIX}-k${x}_m${x}_n${x}.bin -k $x -m $x -n $x
@@ -48,6 +56,8 @@ for ((i = 0; i < $(( ${#DATAS_MD5_FILE[@]} / 2 )); i++))
 do
 	md5_correct=${DATAS_MD5_FILE[ $(( $i * 2 )) ]}
 	file=${DATAS_MD5_FILE[ $(( $i * 2 + 1 )) ]}
+
+	[[ ! -e ${file} ]] && continue
 
 	md5_real=$(fmd5 ${file})
 
