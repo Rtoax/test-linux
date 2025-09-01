5053fb7ea13d2281904cc52fd9585b1e98f740a8
Rong Tao <rongtao@cestc.cn>
2025-09-01 11:29:55 +0800

    hpc: matrix-md5.sh: add TXT=1 env to test matrix -T
    See commit f53bce1b49dc ("hpc: matrix.cu: add -T argument to save to text file instead of binary")
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/samples/matrix-md5.sh
+++ hpc/nvidia/cuda/samples/matrix-md5.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# Usage: [TXT=1] ./matrix-md5.sh
 set -e
 
-DATAS_MD5_FILE=(
+DATAS_MD5_FILE_BIN=(
 d89606d5e485fff33fb295c6b5f2d974  matrix-c-k1000_m1000_n1000.bin
 db4121f6cb258573aacf608d75e89e12  matrix-c-k100_m100_n100.bin
 c3312f95aff9dfdeb8bfde5bc692dea2  matrix-c-k200_m200_n200.bin
@@ -34,6 +35,29 @@ c64e35c886346e397885c4c17a7174fc  matrix-k700_m700_n700.bin
 da851493a22d75385b8e50ead6869e2d  matrix-k900_m900_n900.bin
 )
 
+DATAS_MD5_FILE_TXT=(
+0cf9b2292c4a24d41b9ccaea5a29571f  matrix-c-k1000_m1000_n1000.log
+d04dcf17d1135ccb6458fb1efd605159  matrix-c-k100_m100_n100.log
+bbb8a1074f68b5c9735ae6c77ec0bdf4  matrix-c-k200_m200_n200.log
+05de7226992bda26ba86317efb1a8e47  matrix-c-k300_m300_n300.log
+7937cda9d14d7bc584ce0f7c0aa47850  matrix-c-k400_m400_n400.log
+9bde03b383a4fefae62821af2515c783  matrix-c-k500_m500_n500.log
+9abc774828ddb102b748dd11f6ee7707  matrix-c-k600_m600_n600.log
+ad5a9e526b90639a7794f75e4e1b8f65  matrix-c-k700_m700_n700.log
+f5902134613d73f204cf7b5f1f858beb  matrix-c-k800_m800_n800.log
+4e6be1c7a41e3cab390e518eb764fbbe  matrix-c-k900_m900_n900.log
+cf17414f95852dbe1b2478633d5edc6a  matrix-k1000_m1000_n1000.log
+05c6a3006b74f07df9e94d5145e37891  matrix-k100_m100_n100.log
+abf5abb1fb2363ce998aafd112d4adc2  matrix-k200_m200_n200.log
+d375ad416950f24302792003560df260  matrix-k300_m300_n300.log
+67cb3132b590074d034be0b072910c7b  matrix-k400_m400_n400.log
+1cca9b04dd8b72dbec4a6e04b4a764a2  matrix-k500_m500_n500.log
+a2ead9faf905c7e779f2de5e4c0a9851  matrix-k600_m600_n600.log
+1e56b5a58830ac120787e49d61b359d3  matrix-k700_m700_n700.log
+8b2fcdcf61bbd299f79aac4a63a47f27  matrix-k800_m800_n800.log
+4c04b497c8f779f87dc9fc766d132165  matrix-k900_m900_n900.log
+)
+
 fmd5() {
 	local file=$1
 	if [[ -e $file ]]; then
@@ -49,21 +73,32 @@ MATRIX=$(echo ${MATRIX} | tr -d './')
 
 for x in $(seq 100 100 1000)
 do
-	./${MATRIX} -O ${MATRIX}-k${x}_m${x}_n${x}.bin -k $x -m $x -n $x
+	suffix=bin
+	[[ ${TXT} ]] && suffix=log
+	./${MATRIX} ${TXT:+-T} -O ${MATRIX}-k${x}_m${x}_n${x}.${suffix} -k $x -m $x -n $x
 done
 
-for ((i = 0; i < $(( ${#DATAS_MD5_FILE[@]} / 2 )); i++))
-do
-	md5_correct=${DATAS_MD5_FILE[ $(( $i * 2 )) ]}
-	file=${DATAS_MD5_FILE[ $(( $i * 2 + 1 )) ]}
 
-	[[ ! -e ${file} ]] && continue
+check_md5() {
+	local md5_file=( ${@} )
 
-	md5_real=$(fmd5 ${file})
+	for ((i = 0; i < $(( ${#md5_file[@]} / 2 )); i++))
+	do
+		md5_correct=${md5_file[ $(( $i * 2 )) ]}
+		file=${md5_file[ $(( $i * 2 + 1 )) ]}
 
-	if [[ ${md5_correct} != ${md5_real} ]]; then
-		printf "\033[31mFAILED\t%s\t%s != %s\033[m\n" $file $md5_correct $md5_real
-	else
-		printf "\033[32mSUCCESS\t%s\t%s\033[m\n" $file $md5_correct
-	fi
-done
+		md5_real=$(fmd5 ${file})
+
+		if [[ ${md5_correct} != ${md5_real} ]]; then
+			printf "\033[31mFAILED\t%s\t%s != %s\033[m\n" $file $md5_correct $md5_real
+		else
+			printf "\033[32mSUCCESS\t%s\t%s\033[m\n" $file $md5_correct
+		fi
+	done
+}
+
+if [[ ${TXT} ]]; then
+	check_md5 ${DATAS_MD5_FILE_TXT[@]}
+else
+	check_md5 ${DATAS_MD5_FILE_BIN[@]}
+fi
