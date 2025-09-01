ad56e633e15949f294b51614c4659e84cde401b9
Rong Tao <rtoax@foxmail.com>
2025-09-01 15:55:25 +0800

    simd: update commit comment
    see commit da2b984e6486 ("simd: GenuineIntel Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz")
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- instructions/examples/simd.c
+++ instructions/examples/simd.c
@@ -8,8 +8,8 @@
  *
  * CPU Test
  * - commit 76aff9663c19 ("simd: GenuineIntel 11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz")
+ * - commit da2b984e6486 ("simd: GenuineIntel Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz")
  * - commit f7d73d7d348a ("simd: Phytium S5000C")
- * GenuineIntel Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz
  */
 #include <stdio.h>
 #include <stdint.h>
