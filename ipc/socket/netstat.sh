542d5702a589bff262fc023e9a4025f33ceb1fcb
Rong Tao <rongtao@cestc.cn>
2025-09-11 09:11:54 +0800

    netstat.sh: add more
    from ostools:
    
    commit f69bdf338dd0 ("net: stat.sh: Add protocol statistic")
    commit fb387c4ae022 ("net: Add net/stat.sh")
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- ipc/socket/netstat.sh
+++ ipc/socket/netstat.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+stat_port()
+{
+	netstat -anpltu
+}
+
+stat_protocol()
+{
+	# --statistics, -s: Display summary statistics for each protocol.
+	netstat -s
+
+	ss -s
+}
+
 netstat_unix() {
 	sudo netstat --protocol=unix --program
 }
