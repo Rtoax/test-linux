e2e7aa0f4ad60653b067ea60678bbde1155cdfae
Rong Tao <rongtao@cestc.cn>
2025-09-15 17:05:18 +0800

    fakeCUDA: runtime: add more internal function
    Add list:
    
         __cudaRegisterFunction;
         __cudaPushCallConfiguration;
         __cudaGetKernel;
         __cudaPopCallConfiguration;
         __cudaLaunchKernel;
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/fakeCUDA/runtime_internal.c
+++ hpc/nvidia/cuda/fakeCUDA/runtime_internal.c
@@ -1,5 +1,6 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <sys/types.h>
 
 
 void **__cudaRegisterFatBinary(void *fatCubin)
@@ -13,4 +14,37 @@ void __cudaRegisterFatBinaryEnd(void **fatCubinHandle)
 
 void __cudaUnregisterFatBinary(void **fatCubinHandle)
 {
+}
+
+void __cudaRegisterFunction(void **fatCubinHandle, const char *hostFun,
+			    char *deviceFun, const char *deviceName,
+			    int thread_limit, uint3 *tid, uint3 *bid,
+			    dim3 *bDim, dim3 *gDim, int *wSize)
+{
+
+}
+
+unsigned __cudaPushCallConfiguration(dim3 gridDim, dim3 blockDim,
+				     size_t sharedMem,
+				     struct CUstream_st *stream)
+{
+	return 0;
+}
+
+cudaError_t __cudaGetKernel(cudaKernel_t *kernel, const void *v)
+{
+	return cudaSuccess;
+}
+
+cudaError_t __cudaPopCallConfiguration(dim3 *gridDim, dim3 *blockDim,
+				       size_t *sharedMem, void *stream)
+{
+	return cudaSuccess;
+}
+
+cudaError_t __cudaLaunchKernel(cudaKernel_t kernel, dim3 gridDim, dim3 blockDim,
+			       void **args, size_t sharedMem,
+			       cudaStream_t stream)
+{
+	return cudaSuccess;
 }
