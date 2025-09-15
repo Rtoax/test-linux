c475ec39893e023acb80e46340b5f289b0f0d266
Rong Tao <rongtao@cestc.cn>
2025-09-15 17:07:16 +0800

    fakeCUDA: runtime: runtime_internal.c: add debug log
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/fakeCUDA/runtime_internal.c
+++ hpc/nvidia/cuda/fakeCUDA/runtime_internal.c
@@ -2,18 +2,23 @@
 #include <cuda_runtime.h>
 #include <sys/types.h>
 
+#include "debug.h"
+
 
 void **__cudaRegisterFatBinary(void *fatCubin)
 {
+	LOG_DEBUG("\n");
 	return NULL;
 }
 
 void __cudaRegisterFatBinaryEnd(void **fatCubinHandle)
 {
+	LOG_DEBUG("\n");
 }
 
 void __cudaUnregisterFatBinary(void **fatCubinHandle)
 {
+	LOG_DEBUG("\n");
 }
 
 void __cudaRegisterFunction(void **fatCubinHandle, const char *hostFun,
@@ -21,24 +26,27 @@ void __cudaRegisterFunction(void **fatCubinHandle, const char *hostFun,
 			    int thread_limit, uint3 *tid, uint3 *bid,
 			    dim3 *bDim, dim3 *gDim, int *wSize)
 {
-
+	LOG_DEBUG("\n");
 }
 
 unsigned __cudaPushCallConfiguration(dim3 gridDim, dim3 blockDim,
 				     size_t sharedMem,
 				     struct CUstream_st *stream)
 {
+	LOG_DEBUG("\n");
 	return 0;
 }
 
 cudaError_t __cudaGetKernel(cudaKernel_t *kernel, const void *v)
 {
+	LOG_DEBUG("\n");
 	return cudaSuccess;
 }
 
 cudaError_t __cudaPopCallConfiguration(dim3 *gridDim, dim3 *blockDim,
 				       size_t *sharedMem, void *stream)
 {
+	LOG_DEBUG("\n");
 	return cudaSuccess;
 }
 
@@ -46,5 +54,6 @@ cudaError_t __cudaLaunchKernel(cudaKernel_t kernel, dim3 gridDim, dim3 blockDim,
 			       void **args, size_t sharedMem,
 			       cudaStream_t stream)
 {
+	LOG_DEBUG("\n");
 	return cudaSuccess;
 }
