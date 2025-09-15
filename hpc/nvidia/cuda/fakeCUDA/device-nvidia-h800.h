93e1bba58f0d2d3f91cbdee3e2136969ec39b920
Rong Tao <rtoax@foxmail.com>
2025-09-15 22:02:38 +0800

    fakeCUDA: split device-nvidia-h800.h from device.c
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ hpc/nvidia/cuda/fakeCUDA/device-nvidia-h800.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define DEV_COUNT	8
+#define DEV_NAME	"NVIDIA H800"
+#define DEV_TOTAL_GLOBAL_MEM	8321499136	/* 8GiB */
+#define DEV_TOTAL_CONST_MEM	65536
+#define DEV_L22CACHESIZE	52428800
+#define DEV_globalL1CacheSupported	1
+#define DEV_localL1CacheSupported	1
+#define DEV_managedMemory	1
+#define DEV_ECCEnabled	1
+#define DEV_multiProcessorCount	132
+#define DEV_maxThreadsPerMultiProcessor	2048
+#define DEV_maxThreadsPerBlock	1024
+#define DEV_maxThreadsDimX	1024
+#define DEV_maxThreadsDimY	1024
+#define DEV_maxThreadsDimZ	64
+#define DEV_maxGridSizeX	2147483647
+#define DEV_maxGridSizeY	65535
+#define DEV_maxGridSizeZ	65535
+#define DEV_clockRate	1980000
+#define DEV_warpSize	32
+#define DEV_major	9
+#define DEV_minor	0
