b0d8678741ab9a49db5fdc07f45f4622979df073
Rong Tao <rongtao@cestc.cn>
2025-09-15 14:46:18 +0800

    cuda/samples: fakeCUDA.sh: add './' prefix
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/samples/fakeCUDA.sh
+++ hpc/nvidia/cuda/samples/fakeCUDA.sh
@@ -4,4 +4,4 @@ set -e
 FAKECUDA_ROOT=$PWD/../fakeCUDA
 make -C ${FAKECUDA_ROOT} --no-print-directory --silent 1>/dev/null
 
-LD_LIBRARY_PATH=${FAKECUDA_ROOT} "$@"
+LD_LIBRARY_PATH=${FAKECUDA_ROOT} "./$@"
