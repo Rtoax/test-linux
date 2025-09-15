4ad0ca407ea2e0a298d5fbdec1813f9db9708095
Rong Tao <rongtao@cestc.cn>
2025-09-16 07:23:50 +0800

    fakeCUDA: blas: add cublasSetMatrix()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/fakeCUDA/blas.c
+++ hpc/nvidia/cuda/fakeCUDA/blas.c
@@ -1,6 +1,7 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <cublas.h>
+#include <string.h>
 
 
 cublasStatus_t cublasCreate_v2(cublasHandle_t *handle)
@@ -10,5 +11,19 @@ cublasStatus_t cublasCreate_v2(cublasHandle_t *handle)
 
 cublasStatus_t cublasDestroy_v2(cublasHandle_t handle)
 {
+	return CUBLAS_STATUS_SUCCESS;
+}
+
+cublasStatus_t cublasSetMatrix(int rows, int cols, int elemSize,
+			       const void *A, int lda, void *B, int ldb)
+{
+	const char *src = (const char *)A;
+	char *dst = (char*)B;
+
+	for (int j = 0; j < cols; ++j) {
+		memcpy(dst + j * ldb * elemSize,
+			src + j * lda * elemSize,
+			rows * elemSize);
+	}
 	return CUBLAS_STATUS_SUCCESS;
 }
