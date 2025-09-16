ed1ff9759f12ca901f8a0db3474a5387111aaa33
Rong Tao <rongtao@cestc.cn>
2025-09-16 16:34:12 +0800

    fakeCUDA: runtime: add more display info
    rongtao@NUC10:~/Git/tst-linux/hpc/nvidia/cuda/samples$ FAKECUDA_DEBUG=1 ./fakeCUDA.sh dim3 gx=1 gy=2 gz=3 dim=3
    fakeCUDA: DEBUG: [_fakecuda_start:13] Start debug mode.
    fakeCUDA: DEBUG: [_fakecuda_start:13] Start debug mode.
    fakeCUDA: DEBUG: [_fakecuda_start:13] Start debug mode.
    fakeCUDA: DEBUG: [_fakecuda_start:13] Start debug mode.
    fakeCUDA: DEBUG: [_fakecuda_start:13] Start debug mode.
    fakeCUDA: DEBUG: [_fakecuda_start:13] Start debug mode.
    fakeCUDA: DEBUG: [_fakecuda_start:13] Start debug mode.
    fakeCUDA: DEBUG: [__cudaRegisterFatBinary:18] fatCubin 0x405fa0
    fakeCUDA: DEBUG: [__cudaRegisterFunction:37] hostFun 0x401094, deviceFun 0x402df1, deviceName _Z10checkIndexi, thread_limit -1
    fakeCUDA: DEBUG: [__cudaRegisterFunction:37] hostFun 0x400f14, deviceFun 0x402e01, deviceName _Z9checkInfov, thread_limit -1
    fakeCUDA: DEBUG: [__cudaRegisterFatBinaryEnd:24]
    fakeCUDA: DEBUG: [__cudaRegisterFatBinary:18] fatCubin 0x405fb8
    fakeCUDA: DEBUG: [__cudaRegisterFatBinaryEnd:24]
    Usage: ./dim3 [dev_id=<N>] [bx|by|bz|gx|gy|gz=<N>] [it=<INTERVAL>] [dim=<1|2|3>]
    fakeCUDA: DEBUG: [cudaSetDevice:33] set dev to 0
    fakeCUDA: DEBUG: [cudaGetLastError:44]
    Running on device 0
    <<< grid(1,2,3), block(2,2,2) >>>
    fakeCUDA: DEBUG: [__cudaPushCallConfiguration:45] grid(1,1,1), block(1,1,1), sharedMem 0
    fakeCUDA: DEBUG: [__cudaGetKernel:54]
    fakeCUDA: DEBUG: [__cudaPopCallConfiguration:61]
    fakeCUDA: DEBUG: [__cudaLaunchKernel:69] kernel (nil)
    fakeCUDA: DEBUG: [__cudaPushCallConfiguration:45] grid(1,2,3), block(2,2,2), sharedMem 0
    fakeCUDA: DEBUG: [__cudaGetKernel:54]
    fakeCUDA: DEBUG: [__cudaPopCallConfiguration:61]
    fakeCUDA: DEBUG: [__cudaLaunchKernel:69] kernel (nil)
    fakeCUDA: DEBUG: [__cudaUnregisterFatBinary:29]
    fakeCUDA: DEBUG: [__cudaUnregisterFatBinary:29]
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/fakeCUDA/runtime_internal.c
+++ hpc/nvidia/cuda/fakeCUDA/runtime_internal.c
@@ -5,9 +5,17 @@
 #include "debug.h"
 
 
+/**
+ * /usr/local/cuda-13.0/targets/x86_64-linux/include/driver_types.h
+ * typedef __device_builtin__ struct CUkern_st *cudaKernel_t;
+ */
+struct CUkern_st {
+};
+
+
 void **__cudaRegisterFatBinary(void *fatCubin)
 {
-	LOG_DEBUG("\n");
+	LOG_DEBUG("fatCubin %p\n", fatCubin);
 	return NULL;
 }
 
@@ -26,14 +34,18 @@ void __cudaRegisterFunction(void **fatCubinHandle, const char *hostFun,
 			    int thread_limit, uint3 *tid, uint3 *bid,
 			    dim3 *bDim, dim3 *gDim, int *wSize)
 {
-	LOG_DEBUG("\n");
+	LOG_DEBUG("hostFun %p, deviceFun %p, deviceName %s, thread_limit %d\n",
+		  hostFun, deviceFun, deviceName, thread_limit);
 }
 
 unsigned __cudaPushCallConfiguration(dim3 gridDim, dim3 blockDim,
 				     size_t sharedMem,
 				     struct CUstream_st *stream)
 {
-	LOG_DEBUG("\n");
+	LOG_DEBUG("grid(%d,%d,%d), block(%d,%d,%d), sharedMem %ld\n",
+		  gridDim.x, gridDim.y, gridDim.z,
+		  blockDim.x, blockDim.y, blockDim.z,
+		  sharedMem);
 	return 0;
 }
 
@@ -54,6 +66,6 @@ cudaError_t __cudaLaunchKernel(cudaKernel_t kernel, dim3 gridDim, dim3 blockDim,
 			       void **args, size_t sharedMem,
 			       cudaStream_t stream)
 {
-	LOG_DEBUG("\n");
+	LOG_DEBUG("kernel %p\n", kernel);
 	return cudaSuccess;
 }
