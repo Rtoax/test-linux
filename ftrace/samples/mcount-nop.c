0d6ae28dd18ff65726bb1d35da0dc1644fb09d0f
Rong Tao <rongtao@cestc.cn>
2025-09-16 16:07:58 +0800

    ftrace/samples/mcount-nop.c: print __builtin_return_address
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- ftrace/samples/mcount-nop.c
+++ ftrace/samples/mcount-nop.c
@@ -3,59 +3,62 @@
 #define __visible_default	__attribute__((visibility("default")))
 /* forbid mcount call mcount() it self */
 #define __no_instr		__attribute__((no_instrument_function))
-
 #define __pg_	__visible_default __no_instr
 
-#define print_func() printf("[%s:%d]\n", __func__, __LINE__)
+#define TRACE_DEBUG() do {	\
+		void *__return_addr = __builtin_return_address(0);	\
+		void *__caller_addr = __builtin_return_address(1);	\
+		fprintf(stderr, "[%s:%d] %p %p\n", __func__, __LINE__, __return_addr, __caller_addr);	\
+	} while (0)
 
 void __pg_ mcount(void)
 {
-	print_func();
+	TRACE_DEBUG();
 }
 
 void __pg_ _mcount(void)
 {
-	print_func();
+	TRACE_DEBUG();
 }
 
 void __pg_ __gnu_mcount_nc(void)
 {
-	print_func();
+	TRACE_DEBUG();
 }
 
 void __pg_ __fentry__(void)
 {
-	print_func();
+	TRACE_DEBUG();
 }
 
 void __pg_ __cyg_profile_func_enter(void *child, void *parent)
 {
-	print_func();
+	TRACE_DEBUG();
 }
 
 void __pg_ __cyg_profile_func_exit(void *child, void *parent)
 {
-	print_func();
+	TRACE_DEBUG();
 }
 
 void __pg_ __monstartup(unsigned long low, unsigned long high)
 {
-	print_func();
+	TRACE_DEBUG();
 }
 
 void __pg_ _mcleanup(void)
 {
-	print_func();
+	TRACE_DEBUG();
 }
 
 void __pg_ mcount_restore(void)
 {
-	print_func();
+	TRACE_DEBUG();
 }
 
 void __pg_ mcount_reset(void)
 {
-	print_func();
+	TRACE_DEBUG();
 }
 
 #ifdef TEST_MAIN
@@ -70,6 +73,11 @@ void bar(void)
 
 int main(void)
 {
+#define F(f)	printf("%-32s : %p\n", #f, f);
+	F(foo);
+	F(bar);
+	F(main);
+
 	foo();
 	bar();
 	return 0;
