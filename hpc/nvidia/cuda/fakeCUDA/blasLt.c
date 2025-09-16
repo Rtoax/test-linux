a28c338376da228b0f6145ac34fd508763a850c9
Rong Tao <rongtao@cestc.cn>
2025-09-16 14:37:28 +0800

    fakeCUDA: blasLt: add cublasLtCreate() and cublasLtDestroy()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/fakeCUDA/blasLt.c
+++ hpc/nvidia/cuda/fakeCUDA/blasLt.c
@@ -1,3 +1,19 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <cublas.h>
+#include <cublasLt.h>
+#include <math.h>
+#include <string.h>
+#include "debug.h"
+#include "types.h"
 
+
+cublasStatus_t cublasLtCreate(cublasLtHandle_t *lighthandle)
+{
+	return CUBLAS_STATUS_SUCCESS;
+}
+
+cublasStatus_t cublasLtDestroy(cublasLtHandle_t lightHandle)
+{
+	return CUBLAS_STATUS_SUCCESS;
+}
