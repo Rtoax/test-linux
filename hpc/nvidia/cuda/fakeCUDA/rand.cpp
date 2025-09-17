b2d35e5cbe00e7764bb03ea740c2e1962ff1a94d
Rong Tao <rongtao@cestc.cn>
2025-09-17 16:38:17 +0800

    fakeCUDA: fft,rand,sparse support hpcc
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/fakeCUDA/rand.cpp
+++ hpc/nvidia/cuda/fakeCUDA/rand.cpp
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0
 /* Copyright (c) 2025 Rong Tao */
+#ifdef HAVE_HPCC
+#include <hc_runtime.h>
+#include <hcrand/hcrand.h>
+#include <cuda_adapter.h>
+#else
 #include <cuda.h>
 #include <curand.h>
 #include <cuda_runtime.h>
+#endif
 
 
 #ifdef __cplusplus
--- hpc/nvidia/cuda/fakeCUDA/sparse.cpp
+++ hpc/nvidia/cuda/fakeCUDA/sparse.cpp
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 /* Copyright (c) 2025 Rong Tao */
+#ifdef HAVE_HPCC
+#include <hc_runtime.h>
+#include <hcsparse/hcsparse.h>
+#include <cuda_adapter.h>
+#else
 #define DISABLE_CUSPARSE_DEPRECATED	1
 #include <cusparse.h>
+#endif
 #include "debug.h"
 
 
