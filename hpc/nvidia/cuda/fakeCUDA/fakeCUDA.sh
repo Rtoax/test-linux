ed31e91389d80356993a9a375dd97d88b6c283e4
Rong Tao <rongtao@cestc.cn>
2025-09-18 08:42:04 +0800

    fakeCUDA.sh: gray debug log
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/fakeCUDA/fakeCUDA.sh
+++ hpc/nvidia/cuda/fakeCUDA/fakeCUDA.sh
@@ -3,9 +3,11 @@ set -e
 
 [[ ${FAKECUDA_DEBUG} ]] && set -x
 
+printf "\033[2m"
 readonly FAKECUDA_ROOT=$(realpath $(dirname $(readlink $0)))
 make -C ${FAKECUDA_ROOT} --no-print-directory --silent 1>/dev/null
 
 [[ -f $1 ]] && [[ -x $1 ]] && PREFIX=./
+printf "\033[m"
 
 LD_LIBRARY_PATH=${FAKECUDA_ROOT} "${PREFIX}$@"
