1fd82cc3100f1b76eb34aaf793b14d011889a393
Rong Tao <rtoax@foxmail.com>
2025-09-21 08:16:14 +0800

    install-deps.sh: add google-chrome-stable on ubuntu 25.04
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -726,6 +726,21 @@ if [[ $(is_rhel_like) ]] && [[ ${have_3rd_party} ]]; then
 		EOF
 	fi
 	pkgs_desktop+=( google-chrome-stable )
+
+elif [[ $(is_debian_like) ]] && [[ ${have_3rd_party} ]]; then
+	sudo wget http://www.linuxidc.com/files/repo/google-chrome.list \
+		-P /etc/apt/sources.list.d/
+
+	if [[ $(is_os ubuntu:25.04) ]]; then
+		wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | \
+			sudo gpg --dearmor -o /usr/share/keyrings/linux_signing_key.pub
+		# Then, add deb [signed-by=/usr/share/keyrings/linux_signing_key.pub arch=amd64]
+	else
+		wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | \
+			sudo apt-key add -
+	fi
+
+	sudo apt install -y google-chrome-stable
 fi
 
 # Database
