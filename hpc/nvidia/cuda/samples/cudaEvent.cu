f66e32df9d9762dbcc3f4a6b65aee7eb457ca349
Rong Tao <rongtao@cestc.cn>
2025-09-22 10:03:30 +0800

    hip: add symlink hipEvent.hip
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/samples/cudaEvent.cu
+++ hpc/nvidia/cuda/samples/cudaEvent.cu
@@ -10,6 +10,10 @@
 # include <hc_runtime.h>
 # include "hpcc_helpers.h"
 # include "cuda_adapter.h"
+#elif defined(HAVE_HIP)
+#include <hip/hip_runtime.h>
+#include "cuda_helpers.h"
+#include "cuda_adapter.h"
 #else
 # include <cuda_runtime.h>
 # include "cuda_helpers.h"
