0f628f802bda00509a7175a405210e748b03a6a1
Rong Tao <rongtao@cestc.cn>
2025-09-25 11:20:17 +0800

    arch.mk: use CFLAGS_ARCH instead of CFLAGS
    not use -mcpu on sw8a yet
    
    [root@90ec2ae2018b c]# make
    [03:19:28] CC E     arg.c -> .output/arg.E.c
    cc1: error: bad value ‘sw8a’ for ‘-mcpu’ switch
    make: *** [/root/Git/tst-linux/template//targets/exe.mk:17: .output/arg.E.c] Error 1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/arch.mk
+++ template/arch.mk
@@ -2,11 +2,14 @@
 # Copyright (c) 2025 Rong Tao
 _ARCH = 1
 
+CFLAGS_ARCH :=
+
 ifeq ($(shell uname -m),sw_64)
   ifeq ($(shell lscpu | grep -o WX-H8000 || true),WX-H8000)
-    CFLAGS += -mcpu=sw8a
+    CFLAGS_ARCH += -mcpu=sw8a
   else ifeq ($(shell lscpu | grep -o SW3231 || true),SW3231)
-    CFLAGS += -mcpu=sw6b
+    CFLAGS_ARCH += -mcpu=sw6b
   endif
 endif
 
+export CFLAGS_ARCH
