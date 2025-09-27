57eb0d465904aa3ee79478009c36fa68376d0d8e
Rong Tao <rtoax@foxmail.com>
2025-09-27 16:38:01 +0800

    btf.mk: rpi5 kernel has no btf
    The core philosophy of the rpi5 Kernel: This project aims to provide an
    extremely streamlined, highly customizable, and clean-coded Linux kernel.
    Its target users are embedded developers, researchers seeking ultimate
    performance, and geeks who want complete control over every byte of the
    kernel. It significantly reduces features, debugging information, drivers,
    and subsystems considered 'unnecessary' in the mainline kernel to achieve
    a smaller footprint, lower memory usage, and potential performance
    improvements.
    
    BTF is essentially a kind of 'metadata' or 'debugging information' added
    for convenience and portability. For Shumei, this information is considered
    'unnecessary overhead' and contrary to its minimalist philosophy. Compiling
    BTF increases the size of the kernel binary and memory usage; although this
    overhead is relatively small on modern hardware, it is unacceptable for
    Shumei, which pursues the ultimate minimalism.
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- bpf/btf/btf.mk
+++ bpf/btf/btf.mk
@@ -1,16 +1,33 @@
 # SPDX-License-Identifier: GPL-3.0
+# Provide the following macro definitions:
+#
+# HAVE_BTF: Check your system support BTF or not
+#
 _BTF = 1
 
 SHELL := bash
 BPFTOOL := bpftool
 VMLINUX_H := vmlinux.h
+BTF_ROOT := /sys/kernel/btf
+HAVE_BTF := yes
 
+ifeq ($(wildcard ${BTF_ROOT}),)
+  $(warning Not found BTF in your system, see CONFIG_DEBUG_INFO_BTF in your kernel config!!)
+  HAVE_BTF :=
+endif
+
+ifneq (${HAVE_BTF},)
 # Use the bpftool command to generate a series of btf header files.
 # $1 - btf file name
 # $2 - btf header file name
 define bpf_gen_btf_h
-${BPFTOOL} btf dump file /sys/kernel/btf/${1} format c > ${2}
+${BPFTOOL} btf dump file ${BTF_ROOT}/${1} format c > ${2}
 endef
+else
+define bpf_gen_btf_h
+$(error You system not support BTF, see CONFIG_DEBUG_INFO_BTF)
+endef
+endif
 
 define bpf_gen_vmlinux_h
 $(call bpf_gen_btf_h,vmlinux,${VMLINUX_H})
@@ -40,9 +57,17 @@ $(call chk_vmlinux_h)$(shell if [[ "$$(grep -wo '${1}' ${VMLINUX_H})" ]]; then \
 	fi)
 endef
 
-ifneq ($(call vmlinux_has_struct,task_struct),y)
-  $(error Not found task_struct in vmlinux.h: <$(call vmlinux_has_struct,task_struct)>)
+export HAVE_BTF
+
+ifdef DEBUG
+  $(info HAVE_BTF = ${HAVE_BTF})
 endif
-ifneq ($(call vmlinux_has_sym,task_struct),y)
-  $(error Not found task_struct in vmlinux.h: <$(call vmlinux_has_sym,task_struct)>)
+
+ifneq (${HAVE_BTF},)
+  ifneq ($(call vmlinux_has_struct,task_struct),y)
+    $(error Not found task_struct in vmlinux.h: <$(call vmlinux_has_struct,task_struct)>)
+  endif
+  ifneq ($(call vmlinux_has_sym,task_struct),y)
+    $(error Not found task_struct in vmlinux.h: <$(call vmlinux_has_sym,task_struct)>)
+  endif
 endif
