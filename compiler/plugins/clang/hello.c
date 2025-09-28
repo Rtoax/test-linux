cf962a3f75f0344a3f151153845e0bda044e25eb
Rong Tao <rongtao@cestc.cn>
2025-09-28 10:21:26 +0800

    clang: plugins: test llvm/clang/examples/Attribute/Attribute.cpp
    Link: https://clang.llvm.org/docs/ClangPlugins.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/plugins/clang/hello.c
+++ compiler/plugins/clang/hello.c
@@ -1,4 +1,10 @@
-int main(void)
+#ifdef TEST_ATTRIBUTE
+#define __example	__attribute__(("example"))
+#else
+#define __example
+#endif
+
+__example int main(void)
 {
 	return 0;
 }
--- compiler/plugins/clang/test-plugins.sh
+++ compiler/plugins/clang/test-plugins.sh
@@ -4,3 +4,5 @@ set -ex
 CLANG=clang
 
 ${CLANG} -cc1 -load $(pwd)/PrintFunctionNames.so -plugin print-fns hello.c
+#${CLANG} -cc1 -load $(pwd)/AnnotateFunctions.so -plugin annotate-fns hello.c
+${CLANG} -cc1 -load $(pwd)/Attribute.so -plugin example -DTEST_ATTRIBUTE=1 hello.c
