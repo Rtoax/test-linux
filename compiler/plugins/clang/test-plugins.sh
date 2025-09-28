0c1d7707f655b2f2fd15de3cd2e6356c52b13463
Rong Tao <rongtao@cestc.cn>
2025-09-28 17:29:58 +0800

    clang/plugins: fix test-plugins.sh for AnnotateFunctions.cpp
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/plugins/clang/test-plugins.sh
+++ compiler/plugins/clang/test-plugins.sh
@@ -4,6 +4,6 @@ set -ex
 CLANG=clang
 
 ${CLANG} -cc1 -load $(pwd)/PrintFunctionNames.so -plugin print-fns hello.c -o hello-PrintFunctionNames
-#${CLANG} -cc1 -load $(pwd)/AnnotateFunctions.so -plugin annotate-fns hello.c
+${CLANG} -fplugin=$(pwd)/AnnotateFunctions.so hello.c -o hello-AnnotateFunctions
 ${CLANG} -fplugin=$(pwd)/Attribute.so -DTEST_ATTRIBUTE=1 hello.c -o hello-Attribute
 ${CLANG} -fplugin=$(pwd)/pid.so -DTEST_PID=1 hello.c -o hello-pid
