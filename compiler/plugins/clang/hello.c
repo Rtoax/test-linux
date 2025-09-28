cddbc6ce6ece8847156b6f362bd3dcbca8054367
Rong Tao <rongtao@cestc.cn>
2025-09-28 15:28:43 +0800

    clang: plugins: test attribute
    see llvm/clang/test/Frontend/plugin-attribute.cpp
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- compiler/plugins/clang/hello.c
+++ compiler/plugins/clang/hello.c
@@ -1,10 +1,10 @@
 #ifdef TEST_ATTRIBUTE
-#define __example	__attribute__((example))
-#else
-#define __example
+void fn1(void) __attribute__((example)) {
+	__attribute__((example("abc", 3, 4.0))) for (int i = 0; i < 9; ++i) {}
+}
 #endif
 
-__example int main(void)
+int main(void)
 {
 	return 0;
 }
