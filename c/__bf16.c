65addc6e2c4aab5751da46ca85577c5f8a2b10a5
Rong Tao <rongtao@cestc.cn>
2025-09-29 11:10:18 +0800

    __bf16.c: add DIRECT_CONVERT macro
    on my nuc10, convert directly is ok
    	- Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz
    	- fedora 42
    	- gcc (GCC) 15.2.1 20250808 (Red Hat 15.2.1-1
    	- clang version 20.1.8 (Fedora 20.1.8-4.fc42)
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- c/__bf16.c
+++ c/__bf16.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define DIRECT_CONVERT	1
+
 __bf16 __floattobf16(float fp32)
 {
+#ifdef DIRECT_CONVERT
+	return fp32;
+#else
 	uint32_t float_bits = *(uint32_t *)&fp32;
 	uint16_t bf16_bits = (uint16_t)(float_bits >> 16);
 	__bf16 *bf16 = (__bf16 *)&bf16_bits;
 	return *bf16;
+#endif
 }
 
 float __bf16tofloat(__bf16 bf16)
 {
+#ifdef DIRECT_CONVERT
+	return bf16;
+#else
 	uint16_t bf16_bits = *(uint16_t *)&bf16;
 	uint32_t float_bits = (uint32_t)(bf16_bits << 16);
 	float fp32 = *(float *)&float_bits;
 	return fp32;
+#endif
 }
 
 int main(void)
