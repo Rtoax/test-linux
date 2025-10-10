5437e415410a156d36a2a529115dd841e9705a38
Rong Tao <rtoax@foxmail.com>
2025-10-10 21:42:01 +0800

    procfs: status.c: alloc memory
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- fs/procfs/process/status.c
+++ fs/procfs/process/status.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/resource.h>
+#include <unistd.h>
 
 /*
  * Measures the current (and peak) resident and virtual memories
@@ -11,11 +12,10 @@
 void getMemory(long int *currRealMem, long int *peakRealMem,
 	       long int *currVirtMem, long int *peakVirtMem)
 {
-	char buffer[1024];
+	char buffer[128];
+	FILE *file = fopen("/proc/self/status", "r");
 
-	FILE* file = fopen("/proc/self/status", "r");
-
-	while (fscanf(file, " %1023s", buffer) == 1) {
+	while (fscanf(file, " %127s", buffer) == 1) {
 		if (strcmp(buffer, "VmRSS:") == 0) {
 			fscanf(file, " %ld", currRealMem);
 		}
@@ -35,13 +35,18 @@ void getMemory(long int *currRealMem, long int *peakRealMem,
 int main(void)
 {
 	long int currRealMem, peakRealMem, currVirtMem, peakVirtMem;
+	size_t i, size = 1024 * 10240;
+	char *str;
 
-	char *str = malloc(1024 * 1024 * 1024);
-	str[1024 * 1024 - 1] = 'A';
+	str = malloc(size);
+
+	for (i = 0; i < size; i += getpagesize())
+		str[i] = 'A';
 
 	getMemory(&currRealMem, &peakRealMem, &currVirtMem, &peakVirtMem);
 
-	printf("%ld, %ld, %ld, %ld\n", currRealMem, peakRealMem, currVirtMem, peakVirtMem);
+	printf("RSS: %ld, HWM: %ld, Size: %ld, Peak: %ld\n",
+		currRealMem, peakRealMem, currVirtMem, peakVirtMem);
 
 	free(str);
 
