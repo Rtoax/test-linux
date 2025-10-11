332a4a58d7072fb295ae72ab3c16851b4de6cbc1
Rong Tao <rongtao@cestc.cn>
2025-10-11 16:59:42 +0800

    make: add MAKECMDGOALS.mk
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to
 
 #### Breaking Changes
 #### Added
+- make: add MAKECMDGOALS.mk;
 #### Changed
 #### Deprecated
 #### Removed
---
+++ gnu/make/MAKECMDGOALS.mk
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: GPL-3.0
+
+# MAKECMDGOALS is all target pass to make command.
+$(info MAKECMDGOALS = ${MAKECMDGOALS})
+
+.PHONY: build
+build:
+
+rong:
+tao:
