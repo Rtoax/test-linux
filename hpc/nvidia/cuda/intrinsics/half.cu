53c9f76648305095c47371c631bf7aabfe3c6c0b
Rong Tao <rongtao@cestc.cn>
2025-10-11 12:17:17 +0800

    half.cu: add line number prefix for display
    (base) [root@muxi-4 intrinsics]# ./half-hpcc
    57  : CUDART_INF_FP16 : inf
    58  : CUDART_INF_FP16 : inf
    59  : CUDART_MAX_NORMAL_FP16 : 65504.000000
    60  : CUDART_MIN_DENORM_FP16 : 0.000000
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/intrinsics/half.cu
+++ hpc/nvidia/cuda/intrinsics/half.cu
@@ -32,21 +32,24 @@
 #endif
 
 /* Print definitions */
-#define PHALF(v)	printf("%s : %f\n", #v, __half2float(v));
+#define PPFX()	printf("L%-4d: ", __LINE__);
+#define PHALF(v)	PPFX();printf("%s : %f\n", #v, __half2float(v));
 #define PHALF2(v) do {	\
 		half2 __h2 = v;	\
+		PPFX();	\
 		printf("%s : {%f, %f}\n", #v, __half2float(__h2.x), __half2float(__h2.y));	\
 	} while (0)
-#define PFLOAT(v)	printf("%s : %f\n", #v, v);
+#define PFLOAT(v)	PPFX();printf("%s : %f\n", #v, v);
 #define PFLOAT2(v) do {	\
 		float2 __f2 = v;	\
+		PPFX();	\
 		printf("%s : {%f, %f}\n", #v, __f2.x, __f2.y);	\
 	} while (0)
-#define PINT(v)	printf("%s : %d\n", #v, v);
-#define PSHORT(v)	printf("%s : %d\n", #v, v);
-#define PLONGLONG(v)	printf("%s : %lld\n", #v, v);
-#define PBOOL(v)	printf("%s : %s\n", #v, v ? "true" : "false");
-#define PCHAR(v)	printf("%s : %u (%c)\n", #v, v, v);
+#define PINT(v)	PPFX();printf("%s : %d\n", #v, v);
+#define PSHORT(v)	PPFX();printf("%s : %d\n", #v, v);
+#define PLONGLONG(v)	PPFX();printf("%s : %lld\n", #v, v);
+#define PBOOL(v)	PPFX();printf("%s : %s\n", #v, v ? "true" : "false");
+#define PCHAR(v)	PPFX();printf("%s : %u (%c)\n", #v, v, v);
 
 
 __global__ void k_half_constants(void)
