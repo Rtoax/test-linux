9fb950d7edabd4d48f3681e7f6ee4b681072087a
Rong Tao <rtoax@foxmail.com>
2025-10-12 13:25:00 +0800

    ubuntu: upgrade.sh: add command
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- distributions/ubuntu/upgrade.sh
+++ distributions/ubuntu/upgrade.sh
@@ -6,8 +6,10 @@ sudo apt install -y update-manager-core
 sudo apt update -y
 sudo apt dist-upgrade -y
 
+# Check Update
+sudo do-release-upgrade -c
+
 # Upgrade
 # It is recommended to upgrade the system using the do-release-upgrade command
 # on Server edition and cloud images.
 sudo do-release-upgrade -d
-
