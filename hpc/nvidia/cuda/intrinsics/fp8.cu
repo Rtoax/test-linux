5d50b1c0982783e3d218f3d5eaa61944b5ce8a4b
Rong Tao <rongtao@cestc.cn>
2025-10-15 19:23:42 +0800

    fp8.cu: test __nv_cvt_double_to_fp8
    L32  │ bf162_1_raw : {1.000000, 1.000000}
    L33  │ bf162_2_raw : {2.000000, 2.000000}
    L38  │ fp8_1 = __nv_cvt_double_to_fp8(PI_DOUBLE, __NV_NOSAT, __NV_E4M3)
    L39  │ fp8_1 <__NV_E4M3> : inf
    
    Link: https://docs.nvidia.com/cuda/cuda-math-api/cuda_math_api/group__CUDA__MATH__FP8__MISC.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/intrinsics/fp8.cu
+++ hpc/nvidia/cuda/intrinsics/fp8.cu
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 /* Copyright (c) 2025 Rong Tao */
 /**
+ * https://docs.nvidia.com/cuda/cuda-math-api/cuda_math_api/group__CUDA__MATH__INTRINSIC__FP8.html
  */
 #include <assert.h>
 #include <stdio.h>
@@ -12,6 +13,7 @@
 #include "cuda_compat.h"
 #include "compiler.h"
 #include "print.h"
+#include "types.h"
 
 
 __global__ void k_fp8_functions(void)
@@ -21,12 +23,22 @@ __global__ void k_fp8_functions(void)
 	const __nv_bfloat162_raw __unused bf162_1_raw = { .x = 1, .y = 1, };
 	const __nv_bfloat162_raw __unused bf162_2_raw = { .x = 2, .y = 2, };
 
+	__half_raw halfraw;
+	__half2_raw half2raw;
+
 	__nv_fp8_storage_t __unused fp8_1;
 	__nv_fp8x2_storage_t __unused fp8x2_1;
 
 	PBF162RAW(bf162_1_raw);
 	PBF162RAW(bf162_2_raw);
 
+	/**
+	 * FIXME: Why print fp8_1 <__NV_E4M3> : inf??
+	 */
+	PEXPR(fp8_1 = __nv_cvt_double_to_fp8(PI_DOUBLE, __NV_NOSAT, __NV_E4M3));
+	PFP8(fp8_1, __NV_E4M3);
+
+	halfraw = __nv_cvt_fp8_to_halfraw(fp8_1, __NV_E4M3);
 	fp8_1 = __nv_cvt_bfloat16raw_to_fp8(bf16_1_raw, __NV_NOSAT, __NV_E4M3);
 	fp8x2_1 = __nv_cvt_bfloat16raw2_to_fp8x2(bf162_1_raw, __NV_NOSAT, __NV_E4M3);
 #if !defined(__HPCC__) && !defined(__HIPCC__)
--- hpc/nvidia/cuda/intrinsics/print.h
+++ hpc/nvidia/cuda/intrinsics/print.h
@@ -7,6 +7,12 @@
 /* Print definitions */
 #define PPFX()	printf("L%-4d" SEP " ", __LINE__);
 #define PEXPR(expr)	PPFX();printf("%s\n", #expr); expr;
+
+#define PFP8(v, interpretation) do {	\
+		__half_raw ___hraw = __nv_cvt_fp8_to_halfraw(v, interpretation);	\
+		PPFX();printf("%s <%s> : %f\n", #v, #interpretation, __half2float(___hraw));	\
+	} while (0)
+
 #define PHALF(v)	PPFX();printf("%s : %f\n", #v, __half2float(v));
 #define PHALFRAW(v)	PHALF(v)
 #define PHALF2(v) do {	\
@@ -15,12 +21,14 @@
 		printf("%s : {%f, %f}\n", #v, __half2float(__h2.x), __half2float(__h2.y));	\
 	} while (0)
 #define PHALF2RAW(v)	PHALF2(v)
+
 #define PFLOAT(v)	PPFX();printf("%s : %f\n", #v, v);
 #define PFLOAT2(v) do {	\
 		float2 __f2 = v;	\
 		PPFX();	\
 		printf("%s : {%f, %f}\n", #v, __f2.x, __f2.y);	\
 	} while (0)
+
 #define PBF16(v)	PPFX();printf("%s : %f\n", #v, __bfloat162float(v));
 #define __PBF162(type, v) do {	\
 		type __bf162 = v;	\
@@ -29,8 +37,9 @@
 	} while (0)
 #define PBF162(v)	__PBF162(__nv_bfloat162, v)
 #define PBF162RAW(v)	__PBF162(__nv_bfloat162_raw, v)
+
 #define PDOUBLE(v)	PPFX();printf("%s : %lf\n", #v, v);
-#define PINT(v)	PPFX();printf("%s : %d\n", #v, v);
+#define PINT(v)		PPFX();printf("%s : %d\n", #v, v);
 #define PSHORT(v)	PPFX();printf("%s : %d\n", #v, v);
 #define PLONG(v)	PPFX();printf("%s : %ld\n", #v, v);
 #define PLONGLONG(v)	PPFX();printf("%s : %lld\n", #v, v);
