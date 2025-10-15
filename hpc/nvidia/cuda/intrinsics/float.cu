840eca076be1048cbae3089b9a5df0e5cf70fb70
Rong Tao <rongtao@cestc.cn>
2025-10-15 10:49:29 +0800

    float.cu: Single Precision Intrinsics
    Link: https://docs.nvidia.com/cuda/cuda-math-api/cuda_math_api/group__CUDA__MATH__INTRINSIC__SINGLE.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/intrinsics/float.cu
+++ hpc/nvidia/cuda/intrinsics/float.cu
@@ -126,12 +126,99 @@ __global__ void k_float_precision_mathematical(void)
 	PFLOAT(ynf(1, pi));
 }
 
+__global__ void k_float_precision_intrinsics(void)
+{
+	float pi = 3.141592653f;
+	float2 pi2 = make_float2(pi, pi);
+	float tmp, tmp2;
+
+	PFLOAT2(pi2);
+	PFLOAT(__cosf(pi));
+	PFLOAT(__exp10f(pi));
+	PFLOAT(__expf(pi));
+/**
+ * FIXME: CUDA V13.0.48: error: identifier "__fadd2_rd" is undefined
+ */
+#if 0
+	PFLOAT2(__fadd2_rd(pi2, pi2));
+	PFLOAT2(__fadd2_rn(pi2, pi2));
+	PFLOAT2(__fadd2_ru(pi2, pi2));
+	PFLOAT2(__fadd2_rz(pi2, pi2));
+	PFLOAT2(__fmul2_rd(pi2, pi2));
+	PFLOAT2(__fmul2_rn(pi2, pi2));
+	PFLOAT2(__fmul2_ru(pi2, pi2));
+	PFLOAT2(__fmul2_rz(pi2, pi2));
+	PFLOAT2(__ffma2_rd(pi2, pi2, pi2));
+	PFLOAT2(__ffma2_rn(pi2, pi2, pi2));
+	PFLOAT2(__ffma2_ru(pi2, pi2, pi2));
+	PFLOAT2(__ffma2_rz(pi2, pi2, pi2));
+#endif
+	PFLOAT(__fadd_rd(pi, pi));
+	PFLOAT(__fadd_rn(pi, pi));
+	PFLOAT(__fadd_ru(pi, pi));
+	PFLOAT(__fadd_rz(pi, pi));
+	PFLOAT(__fdiv_rd(pi, pi));
+	PFLOAT(__fdiv_rn(pi, pi));
+	PFLOAT(__fdiv_ru(pi, pi));
+	PFLOAT(__fdiv_rz(pi, pi));
+	PFLOAT(__fdividef(pi, pi));
+
+	PFLOAT(__fmaf_ieee_rd(pi, pi, pi));
+	PFLOAT(__fmaf_ieee_rn(pi, pi, pi));
+	PFLOAT(__fmaf_ieee_ru(pi, pi, pi));
+	PFLOAT(__fmaf_ieee_rz(pi, pi, pi));
+	PFLOAT(__fmaf_rd(pi, pi, pi));
+	PFLOAT(__fmaf_rn(pi, pi, pi));
+	PFLOAT(__fmaf_ru(pi, pi, pi));
+	PFLOAT(__fmaf_rz(pi, pi, pi));
+
+	PFLOAT(__fmul_rd(pi, pi));
+	PFLOAT(__fmul_rn(pi, pi));
+	PFLOAT(__fmul_ru(pi, pi));
+	PFLOAT(__fmul_rz(pi, pi));
+
+	PFLOAT(__frcp_rd(pi));
+	PFLOAT(__frcp_rn(pi));
+	PFLOAT(__frcp_ru(pi));
+	PFLOAT(__frcp_rz(pi));
+
+	PFLOAT(__frsqrt_rn(pi));
+	PFLOAT(__fsqrt_rd(pi));
+	PFLOAT(__fsqrt_rn(pi));
+	PFLOAT(__fsqrt_ru(pi));
+	PFLOAT(__fsqrt_rz(pi));
+
+	PFLOAT(__fsub_rd(pi, pi));
+	PFLOAT(__fsub_rn(pi, pi));
+	PFLOAT(__fsub_ru(pi, pi));
+	PFLOAT(__fsub_rz(pi, pi));
+
+	PFLOAT(__log10f(pi));
+	PFLOAT(__log2f(pi));
+	PFLOAT(__logf(pi));
+
+	PFLOAT(__powf(pi, pi));
+
+	PFLOAT(__saturatef(pi));
+
+	PEXPR(__sincosf(pi, &tmp, &tmp2));
+	PFLOAT(tmp);
+	PFLOAT(tmp2);
+
+	PFLOAT(__sinf(pi));
+	PFLOAT(__tanf(pi));
+#if !defined(__HPCC__)
+	PFLOAT(__tanhf(pi));
+#endif
+}
+
 int main(int argc, char *argv[])
 {
 	assert(sizeof(float) == 4 && "bad size of float");
 	assert(sizeof(float2) == 8 && "bad size of float2");
 
 	k_float_precision_mathematical<<<1, 1>>>();
+	k_float_precision_intrinsics<<<1, 1>>>();
 
 	(void)cudaDeviceSynchronize();
 	return 0;
