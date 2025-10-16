a3d955af475f1adc11ca1a9a021796d6d32535cb
Rong Tao <rtoax@foxmail.com>
2025-10-16 18:28:59 +0800

    cuda: __GNUC__.c: print CUDA_VERSION
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- gnu/__GNUC__.c
+++ gnu/__GNUC__.c
@@ -6,6 +6,9 @@
  */
 
 #include <stdio.h>
+#ifdef __NVCC__
+#include <cuda.h>
+#endif
 
 #if defined(__GNUC__) && ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 5) || __GNUC__ >= 5)
 /* do some check */
@@ -64,6 +67,9 @@ int main(void)
 	 * the release number. */
 	printf("%s: __VERSION__ = %s\n", pfx, __VERSION__);
 
+#ifdef CUDA_VERSION
+	printf("%s: CUDA_VERSION = %ld\n", pfx, CUDA_VERSION);
+#endif
 #ifdef __CUDA__
 	printf("%s: __CUDA__ = %d\n", pfx, __CUDA__);
 #endif
