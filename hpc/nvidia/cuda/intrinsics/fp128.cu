ef8fa36c1fe1a39b44ef96b9fd4f7ddf95bf22c5
Rong Tao <rongtao@cestc.cn>
2025-10-21 17:15:15 +0800

    fp128.cu: skip compile for luca
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/intrinsics/fp128.cu
+++ hpc/nvidia/cuda/intrinsics/fp128.cu
@@ -28,7 +28,7 @@ __global__ void k_float128_types(void)
 
 int main(int argc, char *argv[])
 {
-#if !defined(__HPCC__)
+#if !defined(__HPCC__) && !defined(__LUCA__)
 	assert(sizeof(__float128) == 16 && "bad size of __float128");
 #endif
 
