fc0fe1f4dea55481ef03b728ab56bcdc213fe371
Rong Tao <rongtao@cestc.cn>
2025-10-22 14:53:59 +0800

    make: test --include-dir
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- gnu/make/MAKEFILE_LIST.mk
+++ gnu/make/MAKEFILE_LIST.mk
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: GPL-3.0
 
+include template/ansi.mk
+include template/emoji.mk
+
+$(info MOONLIGHT = ${MOONLIGHT})
 $(info MAKEFILE_LIST = ${MAKEFILE_LIST})
 $(info MAKEFLAGS = ${MAKEFLAGS})
 
