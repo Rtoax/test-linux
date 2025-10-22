c75128ca7e1bd2d8d12cd1f0fce07ee0eeb4c229
Rong Tao <rongtao@cestc.cn>
2025-10-22 12:52:09 +0800

    arch.mk: add IS_X86_64 and IS_AARCH64 and fix vdso/test/Makefile
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/arch.mk
+++ template/arch.mk
@@ -1,10 +1,22 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (c) 2025 Rong Tao
+#
+# Output definitions:
+# - CFLAGS_ARCH=
+# - IS_X86_64=y
+# - IS_AARCH64=y
+
 _ARCH = 1
 
 CFLAGS_ARCH :=
 
-ifeq ($(shell uname -m),sw_64)
+ifeq ($(shell uname -m),x86_64)
+  IS_X86_64 := y
+  export IS_X86_64
+else ifeq ($(shell uname -m),aarch64)
+  IS_AARCH64 := y
+  export IS_AARCH64
+else ifeq ($(shell uname -m),sw_64)
   ifeq ($(shell lscpu | grep -o WX-H8000 || true),WX-H8000)
     CFLAGS_ARCH += -mcpu=sw8a
   else ifeq ($(shell lscpu | grep -o SW3231 || true),SW3231)
