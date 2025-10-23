84a119ecfc18487471f34b26fcace5df8e9aee1a
Rong Tao <rongtao@cestc.cn>
2025-10-23 16:55:54 +0800

    make: define.mk: add local_var
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- gnu/make/define.mk
+++ gnu/make/define.mk
@@ -8,8 +8,15 @@ define func_bar
 $(call func_foo)$(shell echo y)
 endef
 
+define local_var
+$(shell echo $(shell dirname ${1}) .. $(shell basename ${1}))
+endef
+
 flag-1 := $(call func_bar)
-$(info <${flag-1}>)
+$(info flag-1 = <${flag-1}>)
+
+flag-2 := $(call local_var,/home/rongtao)
+$(info flag-2 = <${flag-2}>)
 
 .PHONY: build
 build:
