3a8334f5b3aac1a58facedf538d94779f294a3e5
Rong Tao <rongtao@cestc.cn>
2025-10-24 09:08:28 +0800

    bpf/samples/execsnoop: remove extra trace_helpers.c
    use libtrace_helpers.so instead.
    
    Finish: commit 03ee4970f1bb ("helpers.ko: add libtrace_helpers.so")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/samples/execsnoop/loader.c
+++ bpf/samples/execsnoop/loader.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <signal.h>
 #include "bpf_load.h"
 #include "trace_helpers.h"
 
+
+void sig_handler(int sig)
+{
+	read_trace_pipe_stop();
+}
+
 int main(int argc, char **argv)
 {
+	signal(SIGINT, sig_handler);
+
 	if (load_bpf_file("bpf_program.bpf.o") != 0) {
 		printf("The kernel didn't load the BPF program\n");
 		return -1;
