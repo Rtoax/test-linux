0408fe7d953275f5c0f23df28e57dbd77fafe8ad
Rong Tao <rongtao@cestc.cn>
2025-10-28 09:29:22 +0800

    syscall: syscalls.sh: add s390
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/syscalls.sh
+++ syscall/syscalls.sh
@@ -21,5 +21,6 @@ if [[ -e ${LINUX_ROOT} ]]; then
 	linux_syscalls_name ${LINUX_ROOT}/arch/mips/kernel/syscalls/syscall_n64.tbl | tee syscalls-mips64.h
 	linux_syscalls_name ${LINUX_ROOT}/arch/x86/entry/syscalls/syscall_64.tbl | tee syscalls-x86_64.h
 	linux_syscalls_name ${LINUX_ROOT}/arch/powerpc/kernel/syscalls/syscall.tbl | tee syscalls-ppc64.h
+	linux_syscalls_name ${LINUX_ROOT}/arch/s390/kernel/syscalls/syscall.tbl | tee syscalls-s390.h
 fi
 
