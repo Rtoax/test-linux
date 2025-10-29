0f359fe27fa671e93b012d6deefc37d8b14d0361
Rong Tao <rongtao@cestc.cn>
2025-10-29 17:47:55 +0800

    cuda/intrinsics: add simd.cu
    Link: https://docs.nvidia.com/cuda/cuda-math-api/cuda_math_api/group__CUDA__MATH__INTRINSIC__SIMD.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ hpc/nvidia/cuda/intrinsics/simd.cu
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+/* Copyright (c) 2025 Rong Tao */
+/**
+ * https://docs.nvidia.com/cuda/cuda-math-api/cuda_math_api/group__CUDA__MATH__INTRINSIC__SIMD.html
+ */
+#include <assert.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+#include "cuda_compat.h"
+#include "compiler.h"
+#include "print.h"
+#include "types.h"
+
+
+__global__ void k_simd(void)
+{
+}
+
+int main(int argc, char *argv[])
+{
+	k_simd<<<1, 1>>>();
+
+	(void)cudaDeviceSynchronize();
+	return 0;
+}
