d3d5077e79275e076d3b1f5ec930ddc24cc2afd2
Rong Tao <rongtao@cestc.cn>
2025-10-29 19:26:13 +0800

    errno,strerror: display max error number
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- glibc/errno/nr_errno.c
+++ glibc/errno/nr_errno.c
@@ -5,12 +5,22 @@
 
 int main(void)
 {
+	int i, nr;
+
 #define NR_ERRNO(no) printf("%30s (%4d)(-0x%x) : %s\n", #no, no, -no, strerror(no));
 #include "nr_errno.h"
 #undef NR_ERRNO
 
 	printf("errno = %d\n", errno);
 	printf("errno addr = 0x%lx\n", (unsigned long)&errno);
+	printf("ELIBMAX = %d\n", ELIBMAX);
+
+	for (i = 0, nr = 0; i < 256; i++) {
+		if (strerror(i) != NULL &&
+		    strncmp(strerror(i), "Unknown error", 13) != 0)
+			nr++;
+	}
+	printf("max nr %d\n", nr);
 
 	return 0;
 }
--- glibc/string/strerror.c
+++ glibc/string/strerror.c
@@ -5,9 +5,21 @@
 
 int main(void)
 {
+	int i, nr, max_num = 0;
+
 	printf("%d\n", errno);
 	printf("%d:%s\n", errno, strerror(errno));
 	printf("%d:%s\n", EPERM, strerror(EPERM));
 	printf("%d:%s\n", EAGAIN, strerror(EAGAIN));
+
+	for (i = 0, nr = 0; i < 256; i++) {
+		if (strerror(i) != NULL &&
+		    strncmp(strerror(i), "Unknown error", 13) != 0) {
+			nr++;
+			max_num = max_num < i ? i : max_num;
+			printf("strerror(%d) = %s\n", i, strerror(i));
+		    }
+	}
+	printf("nr = %d, max number is %d\n", nr, max_num);
 	return 0;
 }
