bc5834c8da914bc9cea29668877302a7a4fa1f5a
Rong Tao <rongtao@cestc.cn>
2025-11-04 09:05:01 +0800

    targets: shell.mk: apply run_prog.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/targets/make.mk
+++ template/targets/make.mk
@@ -3,14 +3,14 @@ _TARGET_MAKE = 1
 
 MAKE := make
 
-MAKE_SH := ${TEMPLATE_DIR}/targets/run_prog.sh
+RUN_MAKE := ${TEMPLATE_DIR}/targets/run_prog.sh
 
 ifdef DEBUG
-  MAKE_SH += --verbose
+  RUN_MAKE += --verbose
 endif
 
-MAKE_SH += make
+RUN_MAKE += ${MAKE}
 
 %.mk.log: %.mk
 	$(call log_tgt_exe,MAKE,$(<),$(@))
-	$(Q)$(MAKE_SH) $(@) -f $(<) $(ARGS_$(*))
+	$(Q)$(RUN_MAKE) $(@) -f $(<) $(ARGS_$(*))
--- template/targets/shell.mk
+++ template/targets/shell.mk
@@ -4,7 +4,15 @@ _TARGET_SHELL = 1
 SHELL ?= bash
 Q ?= @
 
+RUN_SHELL := ${TEMPLATE_DIR}/targets/run_prog.sh
+
+ifdef DEBUG
+  RUN_SHELL += --verbose
+endif
+
+RUN_SHELL += ${SHELL}
+
 # see target-shell-logs
 %.sh.log: %.sh
 	$(call log_tgt_exe,SHELL,$(<),$(@))
-	$(Q)${SHELL} $(<) $(ARGS_$(*)) $(SHELL_ARGS_$(<)) | tee $(@)
+	$(Q)$(RUN_SHELL) $(@) $(<) $(ARGS_$(*)) $(SHELL_ARGS_$(<))
