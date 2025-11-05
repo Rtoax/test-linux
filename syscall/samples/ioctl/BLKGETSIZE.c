bd85dc127a8c9184278c188d8fca189c2252146a
Rong Tao <rongtao@cestc.cn>
2025-11-05 10:19:30 +0800

    ioctl: BLKGETSIZE: use to block device
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- syscall/samples/ioctl/BLKGETSIZE.c
+++ syscall/samples/ioctl/BLKGETSIZE.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -6,14 +7,25 @@
 
 int main(void)
 {
-	int err;
-	int fd = open("/etc/os-release", O_RDONLY);
+	int err, fd;
 	unsigned long long range[2] = {0, 0};
+	char *dev = "/dev/sda";
 
+	fd = open(dev, O_RDONLY);
+	if (fd == -1) {
+		fprintf(stderr, "open(%s) %m\n", dev);
+		exit(1);
+	}
+
+	/**
+	 * Get number of 512B blocks
+	 */
 	err = ioctl(fd, BLKGETSIZE, &range[1]);
 	if (err == -1) {
 		fprintf(stderr, "ioctl(): %m\n");
 	}
+
+	printf("device %s size %lld GB\n", dev, range[1] * 512 / 1024 / 1024 / 1024);
 
 	close(fd);
 	return 0;
