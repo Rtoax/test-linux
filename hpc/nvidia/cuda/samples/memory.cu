60b750f1289b0c32582b8800a2fcef86876cf0fe
Rong Tao <rongtao@cestc.cn>
2025-11-10 15:53:12 +0800

    memory.cu: add skeleton
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/samples/memory.cu
+++ hpc/nvidia/cuda/samples/memory.cu
@@ -5,34 +5,56 @@
 #include "cuda_compat.h"
 #include "cuda_helpers.h"
 
-struct dev_mem {
+struct device {
 	int dev_id;
-	float *dev_mem;
-	float *host_mem;
+	void *dev_mem;
+	void *host_mem;
 };
 
 #define SZ_MEM	(1024 * 1024 * 512)
 
-void dev_mem_alloc(int dev_id, struct dev_mem *dev)
+void dev_mem_alloc(int dev_id, struct device *dev)
 {
+	printf("Alloc memory for device %d\n", dev_id);
 	dev->dev_id = dev_id;
 	cudaSetDevice(dev_id);
-	cudaMalloc(&dev->dev_mem, SZ_MEM * sizeof(float));
+	cudaMalloc(&dev->dev_mem, SZ_MEM);
+	cudaMemset(dev->dev_mem, 0, SZ_MEM);
+	dev->host_mem = malloc(SZ_MEM);
+}
 
+void dev_mem_free(struct device *dev)
+{
+	printf("Free memory of device %d\n", dev->dev_id);
+	cudaFree(dev->dev_mem);
+	free(dev->host_mem);
+}
+
+void dev_mem_copy(struct device *from, struct device *to, int flags)
+{
 }
 
 int main(void)
 {
-	int i, devNum;
-	struct dev_mem *devs;
+	int i, j, devNum;
+	struct device *devices;
 
 	devNum = gpu_num();
-	devs = (struct dev_mem *)malloc(devNum * sizeof(struct dev_mem));
+	devices = (struct device *)malloc(devNum * sizeof(struct device));
 
 	for (i = 0; i < devNum; i++) {
-		dev_mem_alloc(i, &devs[i]);
+		dev_mem_alloc(i, &devices[i]);
 	}
 
-	free(devs);
+	for (i = 0; i < devNum; i++) {
+		for (j = 0; j < devNum; j++) {
+		}
+	}
+
+	for (i = 0; i < devNum; i++) {
+		dev_mem_free(&devices[i]);
+	}
+
+	free(devices);
 	return 0;
 }
