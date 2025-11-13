cf8e9176f3fc30117832038bf47a4b55f9c8a5c9
Rong Tao <rongtao@cestc.cn>
2025-11-13 23:33:34 +0800

    mlx5: MLX5_GET: test
    Nov 13 23:33:04 AMD-72 kernel: size of bits 128 vs original 16
    Nov 13 23:33:04 AMD-72 kernel: uid 4433
    Nov 13 23:33:04 AMD-72 kernel: op_mod 6655
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- driver/mlx5/MLX5_GET.c
+++ driver/mlx5/MLX5_GET.c
@@ -4,6 +4,7 @@
 #include <linux/printk.h>
 #include <linux/mlx5/device.h>
 
+/* linux/drivers/net/ethernet/mellanox/mlx5/core/cmd.c */
 struct mlx5_ifc_mbox_in_bits {
 	u8         opcode[0x10];
 	u8         uid[0x10];
@@ -17,6 +18,9 @@ struct mlx5_ifc_mbox_in_bits {
 struct data {
 	u16 opcode;
 	u16 uid;
+	u16 reserved_at_20;
+	u16 op_mod;
+	u64 reserved_at_40;
 };
 
 static int kernel_init(void)
@@ -24,8 +28,12 @@ static int kernel_init(void)
 	struct data in = {
 		.opcode = 0x1122,
 		.uid = 0x3344,
+		.op_mod = 0x5566,
 	};
-	printk(KERN_INFO "%x\n", MLX5_GET(mbox_in, &in, uid));
+	printk(KERN_INFO "size of bits %ld vs original %ld\n",
+		sizeof(struct mlx5_ifc_mbox_in_bits), sizeof(struct data));
+	printk(KERN_INFO "uid %x\n", MLX5_GET(mbox_in, &in, uid));
+	printk(KERN_INFO "op_mod %x\n", MLX5_GET(mbox_in, &in, op_mod));
 	return -EINVAL;
 }
 
