8d0c2649f4ba0d66eff4fff4f2e260ee888797e2
Rong Tao <rongtao@cestc.cn>
2025-11-13 14:07:12 +0800

    luca: add luca-envs.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ hpc/cestc/luca-envs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export LUCA_PATH=/opt/luca
+export LUCA_CLANG_PATH=/opt/luca/htgpu_llvm/bin
+export LUCA_CLANG=/opt/luca/htgpu_llvm
+export CUCC_PATH=/opt/luca/tools/cu-bridge
+export CUDA_PATH=/opt/luca/tools/cu-bridge
+export CUDA_HOME=/opt/luca/tools/cu-bridge
+export DEVINFO_ROOT=/opt/luca
+export LD_LIBRARY_PATH=/opt/luca/ompi/lib/:/opt/luca/lib:/opt/luca/htgpu_llvm/lib:${LD_LIBRARY_PATH}
+export PATH=/opt/luca/tools/cu-bridge/tools:/opt/luca/bin:/opt/luca/htgpu_llvm/bin/:${CUCC_PATH}/tools:${CUCC_PATH}/bin:${CUDA_PATH}/bin:$PATH
