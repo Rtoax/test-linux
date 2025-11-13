805c8771cb05ab6a1c510af74f2c684d2ca33001
Rong Tao <rongtao@cestc.cn>
2025-11-13 23:46:32 +0800

    MLX5_GET.c: test
    Nov 13 23:46:17 AMD-72 kernel: size of bits 128 vs original 16
    Nov 13 23:46:17 AMD-72 kernel: uid 4433, 0, 16
    Nov 13 23:46:17 AMD-72 kernel: op_mod 6655
    Nov 13 23:46:17 AMD-72 kernel: data2 88776655
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- driver/mlx5/MLX5_GET.c
+++ driver/mlx5/MLX5_GET.c
@@ -15,7 +15,7 @@ struct mlx5_ifc_mbox_in_bits {
 	u8         reserved_at_40[0x40];
 };
 
-struct data_bigendian {
+struct real_mbox_in {
 	u16 opcode;
 	u16 uid;
 	u16 reserved_at_20;
@@ -23,17 +23,39 @@ struct data_bigendian {
 	u64 reserved_at_40;
 };
 
+struct mlx5_ifc_rongtao_bits {
+	u8         data1[32];
+	u8         data2[32];
+	u8         data3[8];
+};
+
+struct real_rongtao {
+	u32 data1;
+	u32 data2;
+	u8 data3;
+};
+
 static int kernel_init(void)
 {
-	struct data_bigendian in = {
+	struct real_mbox_in in = {
 		.opcode = 0x1122,
 		.uid = 0x3344,
 		.op_mod = 0x5566,
 	};
+
 	printk(KERN_INFO "size of bits %ld vs original %ld\n",
-		sizeof(struct mlx5_ifc_mbox_in_bits), sizeof(struct data_bigendian));
-	printk(KERN_INFO "uid %x\n", MLX5_GET(mbox_in, &in, uid));
+		sizeof(struct mlx5_ifc_mbox_in_bits), sizeof(struct real_mbox_in));
+	printk(KERN_INFO "uid %x, %ld, %ld\n", MLX5_GET(mbox_in, &in, uid),
+		__mlx5_dw_off(mbox_in, uid), __mlx5_bit_off(mbox_in, uid));
 	printk(KERN_INFO "op_mod %x\n", MLX5_GET(mbox_in, &in, op_mod));
+
+	struct real_rongtao rongtao_in = {
+		.data1 = 0x11223344,
+		.data2 = 0x55667788,
+		.data3 = 0xff,
+	};
+
+	printk(KERN_INFO "data2 %x\n", MLX5_GET(rongtao, &rongtao_in, data2));
 	return -EINVAL;
 }
 
