bb62ca32f14642d7da3b61a3259326693ac2cb81
Rong Tao <rongtao@cestc.cn>
2025-11-14 23:21:38 +0800

    scripts/broken-symlinks.sh: Remove GIT_TOPDIR and TOPDIR vars
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- scripts/broken-symlinks.sh
+++ scripts/broken-symlinks.sh
@@ -2,19 +2,17 @@
 # Copyright (c) 2022-2025 Rong Tao
 set -e
 
-readonly GIT_TOPDIR=$(git rev-parse --show-toplevel 2>/dev/null || :)
-
-readonly TOPDIR=${GIT_TOPDIR}
+readonly ROOT_DIRECTORY=$(dirname $(realpath $0)/../)
 
 remove_broken_symlinks()
 {
-	find ${TOPDIR} -type l -xtype l -delete
+	find ${TEST_LINUX_ROOT_DIR} -type l -xtype l -delete
 }
 
 bad_way()
 {
 	local f
-	local symlinks=( $(find ${TOPDIR} -path ".git" -a -prune -o -type l -exec echo {} +) )
+	local symlinks=( $(find ${TEST_LINUX_ROOT_DIR} -path ".git" -a -prune -o -type l -exec echo {} +) )
 	for f in ${symlinks[@]}
 	do
 		real_f=$(realpath $f 2>/dev/null || true)
@@ -27,7 +25,7 @@ bad_way()
 good_way()
 {
 	local f
-	local broken_symlinks=( $(find ${TOPDIR} -xtype l) )
+	local broken_symlinks=( $(find ${TEST_LINUX_ROOT_DIR} -xtype l) )
 	for f in ${broken_symlinks[@]}
 	do
 		echo "Broken-symlink: ${f}"
