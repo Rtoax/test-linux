8a04c729b2718f17526c0119dd3b95d0d5945420
Rong Tao <rongtao@cestc.cn>
2025-11-14 15:31:41 +0800

    memshow: fix error: unknown type name ‘bool’
    (GNU libc) 2.34, (GCC) 11.3.1 20220421:
    
    [15:30:53] CC SO.o  memshow.c -> .output/memshow.so.o
    memshow.c: In function ‘fhexdump’:
    memshow.c:35:17: error: unknown type name ‘bool’
       35 |                 bool startline = i % width == 0;
          |                 ^~~~
    memshow.c:16:1: note: ‘bool’ is defined in header ‘<stdbool.h>’; did you forget to ‘#include <stdbool.h>’?
       15 | #include "memshow.h"
      +++ |+#include <stdbool.h>
       16 |
    memshow.c:36:17: error: unknown type name ‘bool’
       36 |                 bool newline = (i + 1) % width == 0;
          |                 ^~~~
    memshow.c:36:17: note: ‘bool’ is defined in header ‘<stdbool.h>’; did you forget to ‘#include <stdbool.h>’?
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- libs/memshow.c
+++ libs/memshow.c
@@ -4,11 +4,9 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdint.h>
-#include <stdint.h>
-#include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <sys/types.h>
 
