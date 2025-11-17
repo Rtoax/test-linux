bbc2a659db0f5633a3c0d5662687ba0506966c0a
Rong Tao <rongtao@cestc.cn>
2025-11-17 15:48:07 +0800

    bpf/kfunc: bpf_task_from_pid.bpf.c: add comment
    rongtao@NUC10:~/Git/tst-linux/bpf/kfunc/samples$ more bpf_task_from_pid.bpf.disasm
    
    bpf_task_from_pid.bpf.o:	file format elf64-bpf
    
    Disassembly of section .text:
    
    0000000000000000 <test>:
    ; 	struct task_struct *task = bpf_task_from_pid(pid);
           0:	b4 01 00 00 07 00 00 00	w1 = 0x7
           1:	85 10 00 00 ff ff ff ff	call -0x1
    ; 	if (task) {
           2:	15 00 02 00 00 00 00 00	if r0 == 0x0 goto +0x2 <test+0x28>
    ; 		bpf_task_release(task);
           3:	bf 01 00 00 00 00 00 00	r1 = r0
           4:	85 10 00 00 ff ff ff ff	call -0x1
    ; }
           5:	95 00 00 00 00 00 00 00	exit
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Versions: Fedora Linux 43, Kernel 6.17.7-300.fc43.x86_64, GCC 15.2.1, glibc 2.42, LLVM 21.1.5, BCC 0.35.0, LIBBPF 1.6.1

--- bpf/kfunc/samples/bpf_task_from_pid.bpf.c
+++ bpf/kfunc/samples/bpf_task_from_pid.bpf.c
@@ -1,7 +1,17 @@
+/**
+ * linux commit 3f0e6f2b41d3 ("bpf: Add bpf_task_from_pid() kfunc")
+ * v6.1-rc4-1163-g3f0e6f2b41d3
+ *
+ * linux commit bc638d8cb5be ("bpf: allow kfuncs within tracepoint and perf event programs")
+ * v6.11-rc4-276-gbc638d8cb5be
+ *
+ * or use CO-RE method https://github.com/iovisor/bcc/pull/5272
+ */
 #include "vmlinux.h"
 
-void test(pid_t pid)
+void test(void)
 {
+	pid_t pid = 7;
 	struct task_struct *task = bpf_task_from_pid(pid);
 	if (task) {
 		bpf_task_release(task);
