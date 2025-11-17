dc92eda184d78c66cb9fb9084a13f7cf31da5211
Rong Tao <rongtao@cestc.cn>
2025-11-17 19:22:46 +0800

    bpf/insn/samples: add bpf_task_from_pid() test
    Link: https://docs.ebpf.io/linux/kfuncs/bpf_task_from_pid/
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Versions: Fedora Linux 43, Kernel 6.17.7-300.fc43.x86_64, GCC 15.2.1, glibc 2.42, LLVM 21.1.5, BCC 0.35.0, LIBBPF 1.6.1

--- bpf/insn/samples/bpf_insn_samples.h
+++ bpf/insn/samples/bpf_insn_samples.h
@@ -8,3 +8,4 @@ struct bpf_insn *trace_printk_insns(size_t *cnt);
 struct bpf_insn *strncmp_insns(size_t *cnt);
 
 struct bpf_insn *cgroup_from_id_insns(size_t *cnt);
+struct bpf_insn *task_from_pid_insns(size_t *cnt);
---
+++ bpf/insn/samples/task_from_pid.c
@@ -0,0 +1,49 @@
+/**
+ * struct task_struct *bpf_task_from_pid(s32 pid);
+ *
+ * BPF_PROG_TYPE_LSM
+ * BPF_PROG_TYPE_PERF_EVENT  v6.12
+ * BPF_PROG_TYPE_SCHED_CLS
+ * BPF_PROG_TYPE_STRUCT_OPS
+ * BPF_PROG_TYPE_TRACEPOINT  v6.12
+ * BPF_PROG_TYPE_TRACING
+ * BPF_PROG_TYPE_XDP
+ *
+ * Refs:
+ * - https://docs.ebpf.io/linux/kfuncs/bpf_task_from_pid/
+ */
+#include <stdio.h>
+#include "bpf_insn_samples.h"
+#include "bpf_insn.h"
+#include "btf_helpers.h"
+
+static struct bpf_insn insns_buf[1024];
+
+struct bpf_insn *task_from_pid_insns(size_t *cnt)
+{
+	int bpf_task_from_pid_id;
+	int bpf_task_release_id;
+
+	bpf_task_from_pid_id = btf_has_kfunc("bpf_task_from_pid");
+	bpf_task_release_id = btf_has_kfunc("bpf_task_release");
+
+	if (bpf_task_from_pid_id <= 0 || bpf_task_release_id <= 0) {
+		fprintf(stderr, "ERROR: not found bpf_task_from_pid kfunc.\n");
+		*cnt = 0;
+		return NULL;
+	}
+
+	struct bpf_insn *insn = insns_buf;
+
+	*insn++ = BPF_MOV64_IMM(BPF_REG_1, 0);
+	*insn++ = BPF_CALL_KFUNC(0, bpf_task_from_pid_id);
+	*insn++ = BPF_JMP_IMM(BPF_JNE, BPF_REG_0, 0, 1);
+	*insn++ = BPF_JMP_IMM(BPF_JA, 0, 0, 2);
+	*insn++ = BPF_MOV64_REG(BPF_REG_1, BPF_REG_0);
+	*insn++ = BPF_CALL_KFUNC(0, bpf_task_release_id);
+	*insn++ = BPF_MOV64_IMM(BPF_REG_0, 0);
+	*insn++ = BPF_EXIT_INSN();
+
+	*cnt = insn - insns_buf;
+	return insns_buf;
+}
