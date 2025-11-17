0df1f705c739730728a9728955cadd396a811ead
Rong Tao <rongtao@cestc.cn>
2025-11-17 20:34:53 +0800

    bpf/kfunc: bpf_func_mapper.c: support __BPF_FUNC_MAPPER()
    [rongtao@AMD-72 kfunc]$ ./bpf_func_mapper
    BPF_FUNC_unspec                                  = 0
    ...
    BPF_FUNC_task_pt_regs                            = 175
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: AMD EPYC 7763 64-Core Processor, Arch x86_64
    Versions: CCLinux 22.09.2, Kernel 5.15.131-8.cl9.x86_64, GCC 11.3.1, glibc 2.34, LLVM 14.0.6, BCC 0.24.0, LIBBPF 0.6.0

--- bpf/kfunc/bpf_func_mapper.c
+++ bpf/kfunc/bpf_func_mapper.c
@@ -1,11 +1,23 @@
+/**
+ * - commit b290d3a5687e ("bpf/kfunc: add bpf_func_mapper.c to display all BPF_FUNC_xxx")
+ */
 #include <stdio.h>
 #include <linux/bpf.h>
 
 int main(void)
 {
+/**
+ * linux v6.0-2740-g8a76145a2ec2 move __BPF_FUNC_MAPPER(FN) to ___BPF_FUNC_MAPPER(FN, ctx...)
+ * commit 8a76145a2ec2 ("bpf: explicitly define BPF_FUNC_xxx integer values")
+ */
 #ifdef ___BPF_FUNC_MAPPER
 # define __BPF_PRINT_FN(x, y)	printf("%-48s = %d\n", "BPF_FUNC_" #x, y);
 	___BPF_FUNC_MAPPER(__BPF_PRINT_FN);
+#elif defined(__BPF_FUNC_MAPPER)
+# define __BPF_PRINT_FN(x)	printf("%-48s = %d\n", "BPF_FUNC_" #x, BPF_FUNC_ ## x)
+	__BPF_FUNC_MAPPER(__BPF_PRINT_FN)printf("\n");
+#else
+# error "Not found BPF_FUNC_MAPPER macro, please check /usr/include/linux/bpf.h"
 #endif
 	return 0;
 }
