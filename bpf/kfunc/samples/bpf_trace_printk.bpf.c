4cdf93e873d8c52026c45eff43d83e4061478a84
Rong Tao <rongtao@cestc.cn>
2025-11-17 16:12:08 +0800

    bpf/kfunc: bpf_trace_printk.bpf.c: add comment
    Link: https://docs.ebpf.io/linux/helper-function/bpf_trace_printk/
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- bpf/kfunc/samples/bpf_trace_printk.bpf.c
+++ bpf/kfunc/samples/bpf_trace_printk.bpf.c
@@ -1,8 +1,12 @@
 /**
  * int bpf_trace_printk(const char *fmt, int fmt_size, ...);
+ * BPF_FUNC_trace_printk = 6
  *
  * linux v4.0-rc5-419-g9c959c863f82
  * commit 9c959c863f82 ("tracing: Allow BPF programs to call bpf_trace_printk()")
+ *
+ * Refs:
+ * - https://docs.ebpf.io/linux/helper-function/bpf_trace_printk/
  */
 #include "vmlinux.h"
 
