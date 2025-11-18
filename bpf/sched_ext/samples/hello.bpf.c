1faf4eec7f4cd9fe6d583e7c9e858d3ab66f88a7
Rong Tao <rongtao@cestc.cn>
2025-11-18 16:10:13 +0800

    sched-ext: hello: fill empty functions
    $ journalctl -b 0 -k -n 30
    ...
    Nov 18 16:12:46 NUC10 kernel: sched_ext: BPF scheduler "hello" enabled
    Nov 18 16:12:46 NUC10 kernel: NOHZ tick-stop error: local softirq work is pending, handler #40!!!
    Nov 18 16:12:46 NUC10 kernel: sched_ext: BPF scheduler "hello" disabled (runnable task stall)
    Nov 18 16:12:46 NUC10 kernel: sched_ext: hello: watchdog failed to check in for 30.001s
    Nov 18 16:12:46 NUC10 kernel:    scx_exit+0x50/0x70
    Nov 18 16:12:46 NUC10 kernel:    scx_tick+0xa3/0xb0
    Nov 18 16:12:46 NUC10 kernel:    sched_tick+0xdd/0x290
    Nov 18 16:12:46 NUC10 kernel:    update_process_times+0xa4/0xd0
    Nov 18 16:12:46 NUC10 kernel:    tick_nohz_handler+0x8f/0x140
    Nov 18 16:12:46 NUC10 kernel:    __hrtimer_run_queues+0x110/0x2a0
    Nov 18 16:12:46 NUC10 kernel:    hrtimer_interrupt+0xfc/0x230
    Nov 18 16:12:46 NUC10 kernel:    __sysvec_apic_timer_interrupt+0x58/0x100
    Nov 18 16:12:46 NUC10 kernel:    sysvec_apic_timer_interrupt+0x6c/0x90
    Nov 18 16:12:46 NUC10 kernel:    asm_sysvec_apic_timer_interrupt+0x1a/0x20
    Nov 18 16:12:46 NUC10 kernel:    cpuidle_enter_state+0xcc/0x660
    Nov 18 16:12:46 NUC10 kernel:    cpuidle_enter+0x31/0x50
    Nov 18 16:12:46 NUC10 kernel:    cpuidle_idle_call+0xf5/0x160
    Nov 18 16:12:46 NUC10 kernel:    do_idle+0x78/0xd0
    Nov 18 16:12:46 NUC10 kernel:    cpu_startup_entry+0x29/0x30
    Nov 18 16:12:46 NUC10 kernel:    start_secondary+0x126/0x170
    Nov 18 16:12:46 NUC10 kernel:    common_startup_64+0x13e/0x141
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Versions: Fedora Linux 43, Kernel 6.17.7-300.fc43.x86_64, GCC 15.2.1, glibc 2.42, LLVM 21.1.5, BCC 0.35.0, LIBBPF 1.6.1

--- bpf/sched_ext/samples/hello.bpf.c
+++ bpf/sched_ext/samples/hello.bpf.c
@@ -4,4 +4,59 @@
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
 
+SEC("struct_ops/select_cpu")
+s32 hello_select_cpu(struct task_struct *p, s32 prev_cpu, u64 wake_flags)
+{
+	return 2;
+}
+
+SEC("struct_ops/enqueue")
+void hello_enqueue(struct task_struct *p, u64 enq_flags)
+{
+}
+
+SEC("struct_ops/dispatch")
+void hello_dispatch(s32 cpu, struct task_struct *prev)
+{
+}
+
+SEC("struct_ops/running")
+void hello_running(struct task_struct *p)
+{
+}
+
+SEC("struct_ops/stopping")
+void hello_stopping(struct task_struct *p, bool runnable)
+{
+}
+
+SEC("struct_ops/enable")
+void hello_enable(struct task_struct *p)
+{
+}
+
+SEC("struct_ops.s/init")
+s32 hello_init(void)
+{
+	return 0;
+}
+
+SEC("struct_ops/exit")
+void hello_exit(struct scx_exit_info *ei)
+{
+}
+
+SEC(".struct_ops.link")
+struct sched_ext_ops hello_ops = {
+	.name		= "hello",
+	.select_cpu	= (void *)hello_select_cpu,
+	.enqueue	= (void *)hello_enqueue,
+	.dispatch	= (void *)hello_dispatch,
+	.running	= (void *)hello_running,
+	.stopping	= (void *)hello_stopping,
+	.enable		= (void *)hello_enable,
+	.init		= (void *)hello_init,
+	.exit		= (void *)hello_exit,
+};
+
 char __license[] SEC("license") = "GPL";
--- bpf/sched_ext/samples/hello.c
+++ bpf/sched_ext/samples/hello.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
+#include <signal.h>
+#include "hello.skel.h"
+
+sig_atomic_t stop = 0;
+
+void sig_handler(int sig)
+{
+	stop = 1;
+}
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
+			   va_list args)
+{
+	if (level == LIBBPF_DEBUG)
+		return 0;
+	return vfprintf(stderr, format, args);
+}
 
 int main(void)
 {
+	int err;
+	struct hello_bpf *skel;
+
+	libbpf_set_print(libbpf_print_fn);
+	signal(SIGINT, sig_handler);
+
+	skel = hello_bpf__open();
+	if (!skel) {
+		fprintf(stderr, "ERROR: open skel failed.\n");
+		exit(1);
+	}
+
+	err = hello_bpf__load(skel);
+	if (err) {
+		fprintf(stderr, "ERROR: load skel failed.\n");
+		exit(1);
+	}
+
+	err = hello_bpf__attach(skel);
+	if (err) {
+		fprintf(stderr, "ERROR: attach skel failed.\n");
+		exit(1);
+	}
+
+	printf("Sched ext running...\n");
+
+	while (!stop) {
+		sleep(1);
+	}
+
+	printf("Bye...\n");
+
+	hello_bpf__detach(skel);
+	hello_bpf__destroy(skel);
 	return 0;
 }
