770ada2b6f1ef5b291af0c24f2b3e677701fff5e
Rong Tao <rongtao@cestc.cn>
2025-11-18 14:30:24 +0800

    sched/deadline.c: startup a deadline task
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- sched/deadline.c
+++ sched/deadline.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "sched_helpers.h"
 
 int main(void)
 {
-	// TODO
+	int err;
+	struct sched_attr attr;
+
+	attr.size = sizeof(attr);
+	attr.sched_flags = 0;
+	attr.sched_nice = 0;
+	attr.sched_priority = 0;
+
+	/* This creates a 10ms/30ms reservation */
+	attr.sched_policy = SCHED_DEADLINE;
+	attr.sched_runtime = 10 * 1000 * 1000;
+	attr.sched_period = attr.sched_deadline = 30 * 1000 * 1000;
+
+	err = sys_sched_setattr(0, &attr, 0);
+	if (err < 0) {
+		fprintf(stderr, "sched_setattr() failed, %m.\n");
+		exit(1);
+	}
+
+	printf("Startup a deadline task.\n");
+
+	while (1);
+
 	return 0;
 }
