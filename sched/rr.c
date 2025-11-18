ce07e50ad28e06c05684e1247edccb18f26f0c2d
Rong Tao <rongtao@cestc.cn>
2025-11-18 15:25:14 +0800

    sched: fifo.c and rr.c use one source code
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Versions: Fedora Linux 43, Kernel 6.17.7-300.fc43.x86_64, GCC 15.2.1, glibc 2.42, LLVM 21.1.5

--- sched/rr.c
+++ sched/rr.c
@@ -1,20 +1,27 @@
 /**
- * round-robin
+ * RR: round-robin
+ * FIFO: first-in, first-out
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sched.h>
 #include <unistd.h>
 #include <sys/resource.h>
+#include "sched_helpers.h"
 
 int main(void)
 {
-	int err;
+	int err, policy;
 	struct sched_param param;
 	struct rlimit rlimit;
 
+#ifdef TEST_FIFO
+	policy = SCHED_FIFO;
+#else
+	policy = SCHED_RR;
+#endif
 	param.sched_priority = 1;
-	err = sched_setscheduler(0, SCHED_RR, &param);
+	err = sched_setscheduler(0, policy, &param);
 	if (err < 0) {
 		fprintf(stderr, "sched_setscheduler() failed, %m.\n");
 		exit(1);
@@ -35,7 +42,7 @@ int main(void)
 		exit(1);
 	}
 
-	printf("Startup a RR task.\n");
+	printf("Startup a %s task.\n", sched_policy_string(policy));
 
 	/**
 	 * infinite loop will be killed if not setrlimit, see sched(7).
