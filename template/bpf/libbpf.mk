f642cc467dab65bbd910889b4f9f68419b19c00e
Rong Tao <rongtao@cestc.cn>
2025-11-19 15:48:06 +0800

    template: libbpf.mk: rename private vars
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/bpf/libbpf.mk
+++ template/bpf/libbpf.mk
@@ -1,22 +1,20 @@
 # SPDX-License-Identifier: GPL-3.0
+#
+# Output definitions:
+# - LIBBPF_MAJOR_VERSION=
+# - LIBBPF_MINOR_VERSION=
+#
 _LIBBPF = 1
 
 LDCONFIG ?= ldconfig
-LIBBPF_PATHES ?= $(shell ${LDCONFIG} -p | grep libbpf.so | awk '{print $$NF}' || true)
-LIBBPF_PATH ?= $(shell dirname $(LIBBPF_PATHES) | sort -u || true)
+
+private-libbpf-paths ?= $(shell ${LDCONFIG} -p | grep libbpf.so | awk '{print $$NF}' || true)
+private-libpf-path ?= $(shell dirname $(private-libbpf-paths) | sort -u || true)
 # Like /usr/lib64/libbpf.so.0.4.0
-LIBBPF_V_PATH := $(shell realpath ${LIBBPF_PATH}/libbpf.so)
+private-libbpf-v-path := $(shell realpath ${private-libpf-path}/libbpf.so)
 
-LIBBPF_MAJOR_VERSION := $(shell echo ${LIBBPF_V_PATH} | awk -F '.' '{print $$3}')
-LIBBPF_MINOR_VERSION := $(shell echo ${LIBBPF_V_PATH} | awk -F '.' '{print $$4}')
-
-ifdef DEBUG
-  $(info LIBBPF_PATHES = ${LIBBPF_PATHES})
-  $(info LIBBPF_PATH = ${LIBBPF_PATH})
-  $(info LIBBPF_V_PATH = ${LIBBPF_V_PATH})
-  $(info LIBBPF_MAJOR_VERSION = ${LIBBPF_MAJOR_VERSION})
-  $(info LIBBPF_MINOR_VERSION = ${LIBBPF_MINOR_VERSION})
-endif
+LIBBPF_MAJOR_VERSION := $(shell echo ${private-libbpf-v-path} | awk -F '.' '{print $$3}')
+LIBBPF_MINOR_VERSION := $(shell echo ${private-libbpf-v-path} | awk -F '.' '{print $$4}')
 
 ifeq (${LIBBPF_MAJOR_VERSION},)
   $(error "Could not get libbpf LIBBPF_MAJOR_VERSION")
@@ -25,4 +23,12 @@ ifeq (${LIBBPF_MINOR_VERSION},)
   $(error "Could not get libbpf LIBBPF_MINOR_VERSION")
 endif
 
-export LIBBPF_MAJOR_VERSION LIBBPF_MINOR_VERSION LIBBPF_PATH
+ifdef DEBUG
+  $(info private-libbpf-paths = ${private-libbpf-paths})
+  $(info private-libpf-path = ${private-libpf-path})
+  $(info private-libbpf-v-path = ${private-libbpf-v-path})
+  $(info LIBBPF_MAJOR_VERSION = ${LIBBPF_MAJOR_VERSION})
+  $(info LIBBPF_MINOR_VERSION = ${LIBBPF_MINOR_VERSION})
+endif
+
+export LIBBPF_MAJOR_VERSION LIBBPF_MINOR_VERSION
