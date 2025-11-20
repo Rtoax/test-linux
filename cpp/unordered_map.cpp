63d0dfe474fe599abfa8d1948ac0c0008a675373
Rong Tao <rongtao@cestc.cn>
2025-11-20 09:44:07 +0800

    cpp: unordered_map.cpp: use {string,string} as key
    rongtao@NUC10:~/Git/tst-linux/cpp$ make
    [09:43:50] CXX      unordered_map.cpp -> .output/unordered_map.cpp.o
    In file included from /usr/include/c++/15/bits/unordered_map.h:33,
                     from /usr/include/c++/15/unordered_map:43,
                     from unordered_map.cpp:2:
    /usr/include/c++/15/bits/hashtable.h: In instantiation of ‘class std::_Hashtable<std::pair<std::__cxx11::basic_string<char>, std::__cxx11::basic_string<char> >, std::pair<const std::pair<std::__cxx11::basic_string<char>, std::__cxx11::basic_string<char> >, void (*)()>, std::allocator<std::pair<const std::pair<std::__cxx11::basic_string<char>, std::__cxx11::basic_string<char> >, void (*)()> >, std::__detail::_Select1st, std::equal_to<std::pair<std::__cxx11::basic_string<char>, std::__cxx11::basic_string<char> > >, std::hash<std::pair<std::__cxx11::basic_string<char>, std::__cxx11::basic_string<char> > >, std::__detail::_Mod_range_hashing, std::__detail::_Default_ranged_hash, std::__detail::_Prime_rehash_policy, std::__detail::_Hashtable_traits<true, false, true> >’:
    /usr/include/c++/15/bits/unordered_map.h:115:18:   required from ‘class std::unordered_map<std::pair<std::__cxx11::basic_string<char>, std::__cxx11::basic_string<char> >, void (*)()>’
      115 |       _Hashtable _M_h;
          |                  ^~~~
    unordered_map.cpp:11:65:   required from here
       11 |         std::unordered_map<std::pair<std::string, std::string>, fn1_t> fn1_map2_;
          |                                                                        ^~~~~~~~~
    /usr/include/c++/15/bits/hashtable.h:210:51: error: static assertion failed: hash function must be copy constructible
      210 |       static_assert(is_copy_constructible<_Hash>::value,
          |                                                   ^~~~~
    /usr/include/c++/15/bits/hashtable.h:210:51: note: ‘std::integral_constant<bool, false>::value’ evaluates to false
    unordered_map.cpp: In function ‘int main()’:
    unordered_map.cpp:64:18: error: use of deleted function ‘func_map::func_map()’
       64 |         func_map funcMap;
          |                  ^~~~~~~
    unordered_map.cpp:13:9: note: ‘func_map::func_map()’ is implicitly deleted because the default definition would be ill-formed:
       13 |         func_map() = default;
          |         ^~~~~~~~
    unordered_map.cpp: At global scope:
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Versions: Fedora Linux 43, Kernel 6.17.7-300.fc43.x86_64, GCC 15.2.1, glibc 2.42, LLVM 21.1.5

--- cpp/unordered_map.cpp
+++ cpp/unordered_map.cpp
@@ -8,14 +8,28 @@ typedef void (*fn1_t)(void);
 class func_map {
 private:
 	std::unordered_map<std::string, fn1_t> fn1_map_;
+	std::unordered_map<std::pair<std::string, std::string>, fn1_t> fn1_map2_;
 public:
+	func_map() = default;
+
 	void register_fn1(const std::string& key, fn1_t func) {
 		fn1_map_[key] = func;
+	}
+	void register_fn1(const std::string& key1, const std::string& key2, fn1_t func) {
+		fn1_map2_[{key1, key2}] = func;
 	}
 
 	fn1_t get_fn1(const std::string& key) {
 		auto iter = fn1_map_.find(key);
 		if (iter != fn1_map_.end()) {
+			return iter->second;
+		}
+		return nullptr;
+	}
+
+	fn1_t get_fn1(const std::string& key1, const std::string& key2) {
+		auto iter = fn1_map2_.find({key1, key2});
+		if (iter != fn1_map2_.end()) {
 			return iter->second;
 		}
 		return nullptr;
@@ -27,6 +41,15 @@ public:
 			func();
 		} else {
 			std::cerr << "Error: Function '" << key << "' not found!" << std::endl;
+		}
+	}
+
+	void call_fn1(const std::string& key1, const std::string& key2) {
+		fn1_t func = get_fn1(key1, key2);
+		if (func != nullptr) {
+			func();
+		} else {
+			std::cerr << "Error: Function '" << key1 << ", " << key2 << "' not found!" << std::endl;
 		}
 	}
 };
@@ -43,9 +66,12 @@ int main(void)
 	funcMap.register_fn1("foo1.0", foo1);
 	funcMap.register_fn1("foo1.1", foo1);
 	funcMap.register_fn1("foo1.2", foo1);
+	funcMap.register_fn1("foo1.3", "key2", foo1);
 
 	funcMap.call_fn1("foo1.1");
 	funcMap.call_fn1("__not_exist__");
+	funcMap.call_fn1("foo1.3", "key2");
+	funcMap.call_fn1("foo1.3", "__not_exist__");
 
 	return 0;
 }
