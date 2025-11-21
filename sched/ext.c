da540c9011795f038c790f9f6306b9d1e5cb751b
Rong Tao <rongtao@cestc.cn>
2025-11-21 22:51:15 +0800

    sched_ext: must use sched_setattr(2) set SCHED_EXT instead of sched_setscheduler(2)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- sched/ext.c
+++ sched/ext.c
@@ -10,21 +10,25 @@
 
 int main(void)
 {
-	int err, policy = SCHED_EXT;
-	//struct sched_param param;
+	int err;
+	unsigned long count = 0;
+	struct sched_attr attr;
 
-	// TODO: create EXT process.
-
-	//param.sched_priority = 1;
-	err = sched_setscheduler(0, policy, NULL);
+	attr.size = sizeof(attr);
+	attr.sched_policy = SCHED_EXT;
+	attr.sched_priority = 0;
+	err = sys_sched_setattr(0, &attr, 0);
 	if (err < 0) {
-		fprintf(stderr, "sched_setscheduler() failed, %m.\n");
+		fprintf(stderr, "sched_setattr() failed, %m.\n");
 		exit(1);
 	}
 
-	printf("Startup a %s task.\n", sched_policy_string(policy));
+	printf("Startup a %s task.\n", sched_policy_string(attr.sched_policy));
 
-	while (1);
+	while (1) {
+		sleep(1);
+		printf("Count %ld\n", count++);
+	}
 
 	return 0;
 }
