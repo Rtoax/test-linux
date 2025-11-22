b5bdc9e9272171f9c803071e62b3d4ccc0038404
Rong Tao <rongtao@cestc.cn>
2025-11-22 16:14:30 +0800

    sched-ext: could use sched_setscheduler(SCHED_EXT)
    Correct: commit da540c901179 ("sched_ext: must use sched_setattr(2) set SCHED_EXT instead of sched_setscheduler(2)")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Versions: Ubuntu 25.10, Kernel 6.17.0-6-generic, GCC 15.2.0, glibc 2.42, LLVM 20.1.8

--- sched/ext.c
+++ sched/ext.c
@@ -8,22 +8,31 @@
 #include <sys/resource.h>
 #include "sched_helpers.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	int err;
+	int err, policy;
 	unsigned long count = 0;
+
+	policy = SCHED_EXT;
+#if 0
 	struct sched_attr attr;
 
 	attr.size = sizeof(attr);
-	attr.sched_policy = SCHED_EXT;
+	attr.sched_policy = policy;
 	attr.sched_priority = 0;
 	err = sys_sched_setattr(0, &attr, 0);
+#else
+	struct sched_param param;
+	param.sched_priority = 0;
+	err = sched_setscheduler(0, policy, &param);
+#endif
 	if (err < 0) {
-		fprintf(stderr, "sched_setattr() failed, %m.\n");
+		fprintf(stderr, "set sched ext failed, %m.\n");
 		exit(1);
 	}
 
-	printf("Startup a %s task.\n", sched_policy_string(attr.sched_policy));
+	printf("Startup a %s task.\n",
+		sched_policy_string(sched_getscheduler(getpid())));
 
 	while (1) {
 		sleep(1);
