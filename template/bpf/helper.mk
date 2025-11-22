f85e898f14a65b83c9a220f6872c46e4665975da
Rong Tao <rongtao@cestc.cn>
2025-11-22 12:55:27 +0800

    bpf: helper.mk: add bpf_loop and bpf_cgrp_storage_get
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Versions: Ubuntu 25.10, Kernel 6.17.0-6-generic, GCC 15.2.0, glibc 2.42, LLVM 20.1.8, BCC 0.35.0, LIBBPF 1.6.2

--- template/bpf/helper.mk
+++ template/bpf/helper.mk
@@ -19,13 +19,22 @@ define ___define_helper
   $(info Found ${1}())
 endef
 define define_helper
-  $(eval $(call ___define_helper,bpf_strncmp))
+  $(eval $(call ___define_helper,${1}))
 endef
 
-# linux v5.16-rc4-1160-gc5fb19937455
-# commit c5fb19937455 ("bpf: Add bpf_strncmp helper")
+# linux v5.16-rc4-1160-gc5fb19937455 commit c5fb19937455 ("bpf: Add bpf_strncmp helper")
 ifeq ($(call kver_gt,5,16,0),y)
   $(call define_helper,bpf_strncmp)
+endif
+
+# linux v6.0-11954-gc4bcfb38a95e commit c4bcfb38a95e ("bpf: Implement cgroup storage available to non-cgroup-attached bpf progs")
+ifeq ($(call kver_ge,6,0,0),y)
+  $(call define_helper,bpf_cgrp_storage_get)
+endif
+
+# linux v5.15-12938-ge6f2dd0f8067 commit e6f2dd0f8067 ("bpf: Add bpf_loop helper")
+ifeq ($(call kver_ge,5,16,0),y) # failed on 5.15.131
+  $(call define_helper,bpf_loop)
 endif
 
 export bpf-helper-cflags
