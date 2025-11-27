72ec6633254401bc4af874515b8b13de75794751
Rong Tao <rongtao@cestc.cn>
2025-11-27 14:16:07 +0800

    template/helpers.mk: add LUCA_HELPERS and apply it to cuda/fft/
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Phytium, Arch aarch64
    Versions: CCLinux 22.09.2, Kernel 5.15.131-27.cl9.aarch64, GCC 11.3.1, glibc 2.34, LLVM 14.0.6, CUDA V13.0.48

--- template/helpers.mk
+++ template/helpers.mk
@@ -20,6 +20,7 @@
 # - __USE_BTF_HELPERS__=y
 # - __USE_BPF_INSN_SAMPLES__=y
 # - __USE_CUDA_HELPERS__=y
+# - __USE_LUCA_HELPERS__=y
 #
 # Output definitions:
 # - PROC_HELPERS
@@ -31,6 +32,7 @@
 # - TRACE_HELPERS
 # - KSYM_HELPERS
 # - CUDA_HELPERS
+# - LUCA_HELPERS
 # - BPF_HELPERS
 # - BTF_HELPERS
 # - BPF_INSN_SAMPLES
@@ -40,6 +42,7 @@
 # - CFLAGS_A
 # - CFLAGS_SO
 # - CFLAGS_NVCC
+# - CFLAGS_LSCC
 # - LDFLAGS
 #
 _HELPERS = 1
@@ -56,6 +59,7 @@ ifeq ($(2),y)
   CFLAGS_A += -I$(shell dirname ${1})
   CFLAGS_SO += -I$(shell dirname ${1})
   CFLAGS_NVCC += -I$(shell dirname ${1})
+  CFLAGS_LSCC += -I$(shell dirname ${1})
   ifdef STATIC
     LDFLAGS += ${1}
   else
@@ -82,6 +86,7 @@ export OOM_HELPERS := ${TOPDIR}/mm/oom/liboom_helpers.${LIB_TYPE}
 export TRACE_HELPERS := ${TOPDIR}/bpf/libbpf/libtrace_helpers.${LIB_TYPE}
 export KSYM_HELPERS := ${TOPDIR}/fs/procfs/kallsyms/libksym_helpers.${LIB_TYPE}
 export CUDA_HELPERS := ${TOPDIR}/hpc/nvidia/cuda/libcuda_helper.${LIB_TYPE}
+export LUCA_HELPERS := ${TOPDIR}/hpc/nvidia/cuda/libluca_helper.${LIB_TYPE}
 export BPF_HELPERS := ${TOPDIR}/syscall/samples/bpf/libbpf_helpers.${LIB_TYPE}
 export BTF_HELPERS := ${TOPDIR}/bpf/btf/libbtf_helpers.${LIB_TYPE}
 export BPF_INSN_SAMPLES := ${TOPDIR}/bpf/insn/samples/libbpf_insn_samples.${LIB_TYPE}
@@ -96,6 +101,7 @@ $(eval $(call add_helper_target,${OOM_HELPERS},${__USE_OOM_HELPERS__}))
 $(eval $(call add_helper_target,${TRACE_HELPERS},${__USE_TRACE_HELPERS__}))
 $(eval $(call add_helper_target,${KSYM_HELPERS},${__USE_KSYM_HELPERS__}))
 $(eval $(call add_helper_target,${CUDA_HELPERS},${__USE_CUDA_HELPERS__}))
+$(eval $(call add_helper_target,${LUCA_HELPERS},${__USE_LUCA_HELPERS__}))
 $(eval $(call add_helper_target,${BPF_HELPERS},${__USE_BPF_HELPERS__}))
 $(eval $(call add_helper_target,${BTF_HELPERS},${__USE_BTF_HELPERS__}))
 $(eval $(call add_helper_target,${BPF_INSN_SAMPLES},${__USE_BPF_INSN_SAMPLES__}))
@@ -111,6 +117,7 @@ ifdef DEBUG
   $(info TRACE_HELPERS = ${TRACE_HELPERS})
   $(info KSYM_HELPERS = ${KSYM_HELPERS})
   $(info CUDA_HELPERS = ${CUDA_HELPERS})
+  $(info LUCA_HELPERS = ${LUCA_HELPERS})
   $(info BPF_HELPERS = ${BPF_HELPERS})
   $(info BTF_HELPERS = ${BTF_HELPERS})
   $(info BPF_INSN_SAMPLES = ${BPF_INSN_SAMPLES})
