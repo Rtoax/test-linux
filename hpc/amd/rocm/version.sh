5d2b7004a9f1b74dfdcb7f69d117b6b6d7a05559
Rong Tao <rongtao@cestc.cn>
2025-11-28 18:41:35 +0800

    rocm: version.sh: fix warning
    [rongtao@muxi-4 tst-linux]$ git commit -s
    ERROR: not found hipcc in anywhere
    /home/rongtao/Git/tst-linux/hpc/amd/rocm/version.sh: line 8: return: can only `return' from a function or sourced script
    [main da5667a72] cuda/dnn: support HPCC
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/amd/rocm/version.sh
+++ hpc/amd/rocm/version.sh
@@ -5,6 +5,6 @@ set -e
 HIPCC=$(which hipcc 2>/dev/null || :)
 if [[ -z ${HIPCC} ]]; then
 	echo >&2 "ERROR: not found hipcc in anywhere"
-	return 0
+	exit 0
 fi
 ${HIPCC} --version | grep ^HIP | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' 2>/dev/null || true
