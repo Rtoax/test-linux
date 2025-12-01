f838ed88c4c7a088b3c3eee2a1c10c065b5ece56
Rong Tao <rongtao@cestc.cn>
2025-12-01 15:43:19 +0800

    fakeCUDA: libamdhip64.map: add hipExtModuleLaunchKernel@hip_4.2
    rongtao@NUC10:~/Git/tst-linux/hpc/cuda/samples$ LD_LIBRARY_PATH=../fakeCUDA/ ./hello-hip
    ./hello-hip: symbol lookup error: /lib64/librocblas.so.4: undefined symbol: hipExtModuleLaunchKernel, version hip_4.2
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Versions: Fedora Linux 43, Kernel 6.17.7-300.fc43.x86_64, GCC 15.2.1, glibc 2.42, LLVM 21.1.6, CUDA V13.0.48, ROCm 6.4.43484

--- hpc/nvidia/cuda/fakeCUDA/cuda-module-mgmt-hip-private.cpp
+++ hpc/nvidia/cuda/fakeCUDA/cuda-module-mgmt-hip-private.cpp
@@ -4,6 +4,7 @@
 #include <hip/hip_runtime_api.h>
 #include <hip/hiprtc.h>
 
+extern "C" {
 
 hipError_t hipModuleLaunchKernel(hipFunction_t f,
 				 unsigned int gridDimX, unsigned int gridDimY,
@@ -13,4 +14,17 @@ hipError_t hipModuleLaunchKernel(hipFunction_t f,
 				 void** kernelParams, void** extra)
 {
 	return hipSuccess;
+}
+
+hipError_t hipExtModuleLaunchKernel(hipFunction_t f, uint32_t globalWorkSizeX,
+				    uint32_t globalWorkSizeY, uint32_t globalWorkSizeZ,
+				    uint32_t localWorkSizeX, uint32_t localWorkSizeY,
+				    uint32_t localWorkSizeZ, size_t sharedMemBytes,
+				    hipStream_t hStream, void** kernelParams, void** extra,
+				    hipEvent_t startEvent,
+				    hipEvent_t stopEvent,
+				    uint32_t flags)
+{
+	return hipSuccess;
+}
 }
