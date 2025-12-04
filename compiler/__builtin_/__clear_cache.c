f10d59853cb6d8b03ede7a01807ead5a2c7bf242
Rong Tao <rongtao@cestc.cn>
2025-12-04 20:14:41 +0800

    builtin: __clear_cache.c: write access memory block
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Vers: Fedora Linux 43, Linux 6.17.9-300.fc43.x86_64, GCC 15.2.1, glibc 2.42

--- compiler/__builtin_/__clear_cache.c
+++ compiler/__builtin_/__clear_cache.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <unistd.h>
 
 #if defined(__aarch64__)
 extern void __aarch64_sync_cache_range(void *, void *);
@@ -10,8 +11,17 @@ extern void __aarch64_sync_cache_range(void *, void *);
 
 int main(void)
 {
-	char *str = malloc(sizeof(char) * 1024);
-	flush_cache(str, str + 1024);
+	size_t i, size = getpagesize() * 1024;
+	char *str = malloc(size);
+
+	/* Maybe generate lots of Cache */
+	for (i = 0; i < size; i += getpagesize()) {
+		str[i] = 'a';
+	}
+
+	/* Flushing */
+	flush_cache(str, str + size - 1);
+
 	free(str);
 	return 0;
 }
