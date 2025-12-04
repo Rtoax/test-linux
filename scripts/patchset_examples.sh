e03eaf54c5465b3a38ad3a3ee153e66000b588b7
Rong Tao <rongtao@cestc.cn>
2025-12-04 14:18:45 +0800

    patchset_examples.sh: diff output dir for pure-diff
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- scripts/patchset_examples.sh
+++ scripts/patchset_examples.sh
@@ -3,5 +3,5 @@ set -e
 
 ./patchset.sh --help | cat
 ./patchset.sh --from 54103fd68173 --to b0cb7b6917c0 --no-cover-letter
-./patchset.sh --from 54103fd68173 --to b0cb7b6917c0 --no-cover-letter --pure-diff
+./patchset.sh --from 54103fd68173 --to b0cb7b6917c0 --no-cover-letter --pure-diff --output tmp.patch.diff
 ./patchset.sh -- HEAD 54103fd68173 b0cb7b6917c0
