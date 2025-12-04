bef6f12922544cbd046524265e43e23ece151189
Rong Tao <rongtao@cestc.cn>
2025-12-04 15:07:09 +0800

    cuda: kernellaunch.bt.sh: add tracing-type
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- hpc/nvidia/cuda/scripts/kernellaunch.bt.sh
+++ hpc/nvidia/cuda/scripts/kernellaunch.bt.sh
@@ -5,6 +5,11 @@ set -e
 
 declare -a UPROBES URETPROBES
 
+readonly TRACING_TYPE_LATENCY=latency
+readonly TRACING_TYPE_LAUNCH=launch
+readonly TRACING_TYPES=( ${TRACING_TYPE_LATENCY} ${TRACING_TYPE_LAUNCH} )
+TRACING_TYPE=${TRACING_TYPE_LAUNCH}
+
 __usage__()
 {
 	echo -e "
@@ -12,6 +17,8 @@ ${BOLD}NAME${RST}
 	Kernellaunch.bt.sh - Tracing CUDA kernel Launch
 
 ${BOLD}ARGUMENT${RST}
+	-T, --trace-type [T]     tracing type: ${TRACING_TYPES[@]}
+
 	-h, --help               show this help information
 	-v, --verbose            show detail during running
 
@@ -19,9 +26,15 @@ ${BOLD}ARGUMENT${RST}
 	exit ${1-0}
 }
 
+error() {
+	echo -e >&2 "${RED}ERROR: ${@}${RST}"
+	exit 1
+}
+
 TEMP_ARGS=$(getopt \
-	--options vh \
-	--long verbose: \
+	--options T:vh \
+	--long trace-type: \
+	--long verbose \
 	--long help \
 	-n patchset -- "$@")
 
@@ -31,6 +44,14 @@ eval set -- "$TEMP_ARGS"
 
 while true; do
 	case $1 in
+	-T|--trace-type)
+		shift
+		TRACING_TYPE=$1
+		if ! [[ " ${TRACING_TYPES[@]} " =~ " ${TRACING_TYPE} " ]]; then
+			error "Not support ${TRACING_TYPE}, only: ${TRACING_TYPES[@]}"
+		fi
+		shift
+		;;
 	-h|--help)
 		shift
 		__usage__
@@ -93,33 +114,36 @@ probes() {
 	IFS=$old_IFS
 }
 
-sudo tee tmp-klaunch.bt <<-EOF
-#!/bin/env bpftrace
 
-BEGIN {
-	printf("Tracing CUDA Kernel Launch, hit ctrl-c to end.\n");
-	printf("%-8s %-8s %-16s %s\n", "TIME", "PID", "COMM", "PROBE");
-}
-EOF
+case ${TRACING_TYPE} in
+${TRACING_TYPE_LAUNCH})
+	sudo tee tmp-klaunch.bt <<-EOF
+	#!/bin/env bpftrace
+	BEGIN {
+		printf("Tracing CUDA Kernel Launch, hit ctrl-c to end.\n");
+		printf("%-8s %-8s %-16s %s\n", "TIME", "PID", "COMM", "PROBE");
+	}
+	EOF
+	sudo tee --append tmp-klaunch.bt <<-EOF
+	$(probes ${UPROBES[@]}) {
+		time("%H:%M:%S ");
+		printf("%-8d %-16s %s\n", pid, comm, probe);
+	}
+	$(probes ${URETPROBES[@]}) {
+		time("%H:%M:%S ");
+		printf("%-8d %-16s %s %d\n", pid, comm, probe, retval);
+	}
+	EOF
 
-sudo tee --append tmp-klaunch.bt <<-EOF
-$(probes ${UPROBES[@]})
-{
-	time("%H:%M:%S ");
-	printf("%-8d %-16s %s\n", pid, comm, probe);
-}
-
-$(probes ${URETPROBES[@]})
-{
-	time("%H:%M:%S ");
-	printf("%-8d %-16s %s %d\n", pid, comm, probe, retval);
-}
-EOF
-
-sudo tee --append tmp-klaunch.bt <<-EOF
-END {
-	printf("Bye.\n");
-}
-EOF
+	sudo tee --append tmp-klaunch.bt <<-EOF
+	END {
+		printf("Bye.\n");
+	}
+	EOF
+	;;
+${TRACING_TYPE_LATENCY})
+	error TODO
+	;;
+esac
 
 sudo bpftrace tmp-klaunch.bt
