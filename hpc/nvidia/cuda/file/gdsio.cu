81a26d998b16cec902a65c98d5fe14bf9bb27675
Rong Tao <rtoax@foxmail.com>
2025-12-08 18:39:17 +0800

    cufile: gdsio.cu: support argp
    [root@553554a92625 file]# ./gdsio --help
    Usage: gdsio [OPTION...]
    USAGE: [-d <GPU>] [...]
    
      -d, --device=DEVICE        gpu index
      -f, --file=FILE            file name
      -V, --verify               verify IO
      -?, --help                 Give this help list
          --usage                Give a short usage message
      -v, --version              display version
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU: Intel(R) Xeon(R) Platinum 8462Y+, Arch x86_64
    Vers: CCLinux 22.09.2, Linux 5.15.131-26.cl9.x86_64, CUDA

--- hpc/nvidia/cuda/file/gdsio.cu
+++ hpc/nvidia/cuda/file/gdsio.cu
@@ -7,27 +7,77 @@
  * Refs:
  * - https://docs.nvidia.com/gpudirect-storage/getting-started/index.html
  */
+#include <argp.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include "cuda_compat.h"
 
+const char *version = "v0.0.1";
+
+struct {
+	int gpu;
+	const char *filename;
+} env = {
+	.gpu = 0,
+	.filename = "./testfile.out",
+};
+
+const char argp_prog_doc[] =
+	"USAGE: [-d <GPU>] [...]\n";
+
+static const struct argp_option opts[] = {
+	{ "file", 'f', "FILE", 0, "file name" },
+	{ "device", 'd', "DEVICE", 0, "gpu index" },
+	{ "verify", 'V', NULL, 1, "verify IO" },
+	{ "version", 'v', NULL, 1, "display version" },
+	{},
+};
+
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+	switch (key) {
+	case 'f':
+		env.filename = arg;
+		break;
+	case 'd':
+		env.gpu = strtoul(arg, NULL, 10);
+		break;
+	case 'v':
+		printf("gpsio %s\n", version);
+		exit(EXIT_SUCCESS);
+		break;
+	case ARGP_KEY_ARG:
+		break;
+	case ARGP_KEY_END:
+		break;
+	default:
+		return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static const struct argp argp = {
+	.options = opts,
+	.parser = parse_arg,
+	.doc = argp_prog_doc,
+};
+
 int main(int argc, char *argv[])
 {
 	CUfileHandle_t cfHandle;
 	CUfileDescr_t cfDescr = {};
-	int fd;
-	const char *filename = "./testfile.out";
+	int err, fd;
 
-	if (argc > 1) {
-		filename = argv[1];
+	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
+	if (err) {
+		fprintf(stderr, "argp_parse return %d\n", err);
+		return -err;
 	}
 
-	fprintf(stderr, "Usage: %s [filepath]\n", argv[0]);
-
-	fd = open(filename, O_CREAT | O_RDWR | O_DIRECT, 0664);
+	fd = open(env.filename, O_CREAT | O_RDWR | O_DIRECT, 0664);
 	if (fd < 0) {
-		fprintf(stderr, "Open %s failed\n", filename);
+		fprintf(stderr, "Open %s failed\n", env.filename);
 		return 1;
 	}
 
