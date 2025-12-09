0a2a5eb11c02f397b61cc4c52256bde35f177562
Rong Tao <rtoax@foxmail.com>
2025-12-09 16:13:59 +0800

    gdsio_benchmark.sh: support specify DIR=/mnt/nvme/
    [root@553554a92625 file]# DIR=/mnt/nvme/ SIZE=2G ./gdsio_benchmark.sh
    IoType: WRITE, XferType: Storage<->GPU, Threads: 1, DataSetSize: 2147483648 B, Throughput: 4.073219 GiB/sec, total_time 0.527220 secs
    IoType: READ, XferType: Storage<->GPU, Threads: 1, DataSetSize: 2147483648 B, Throughput: 6.598979 GiB/sec, total_time 0.325427 secs
    IoType: WRITE, XferType: Storage<->CPU, Threads: 1, DataSetSize: 2147483648 B, Throughput: 4.085183 GiB/sec, total_time 0.525676 secs
    IoType: READ, XferType: Storage<->CPU, Threads: 1, DataSetSize: 2147483648 B, Throughput: 6.622511 GiB/sec, total_time 0.324270 secs
    IoType: WRITE, XferType: Storage<->CPU<->GPU, Threads: 1, DataSetSize: 2147483648 B, Throughput: 1.452134 GiB/sec, total_time 1.478847 secs
    IoType: READ, XferType: Storage<->CPU<->GPU, Threads: 1, DataSetSize: 2147483648 B, Throughput: 4.188364 GiB/sec, total_time 0.512726 secs
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU: Intel(R) Xeon(R) Platinum 8462Y+, Arch x86_64
    Vers: CCLinux 22.09.2, Linux 5.15.131-26.cl9.x86_64, BASH 5.1.8

--- hpc/nvidia/cuda/file/gdsio_benchmark.sh
+++ hpc/nvidia/cuda/file/gdsio_benchmark.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+#
+# GDSIO benchmark
+#
+
 set -e
 
 [[ -z ${GDSIO} ]] && GDSIO=gdsio
 [[ ${VERIFY} ]] && VERIFY=-V
 [[ -z ${SIZE} ]] && SIZE=1G
+[[ ${DIR} ]] && DIR="-D ${DIR}"
 
 drop_cache() {
 	echo 3 > /proc/sys/vm/drop_caches
@@ -12,22 +17,22 @@ drop_cache() {
 drop_cache
 
 # GPU->Storage
-./${GDSIO} -f a.out -s ${SIZE} -x 0 -I 1
+./${GDSIO} ${DIR} -f a.out -s ${SIZE} -x 0 -I 1
 drop_cache
 # Storage->GPU
-./${GDSIO} -f a.out -s ${SIZE} -x 0 -I 0 ${VERIFY}
+./${GDSIO} ${DIR} -f a.out -s ${SIZE} -x 0 -I 0 ${VERIFY}
 drop_cache
 
 # CPU->Storage
-./${GDSIO} -f b.out -s ${SIZE} -x 1 -I 1
+./${GDSIO} ${DIR} -f b.out -s ${SIZE} -x 1 -I 1
 drop_cache
 # Storage->CPU
-./${GDSIO} -f b.out -s ${SIZE} -x 1 -I 0 ${VERIFY}
+./${GDSIO} ${DIR} -f b.out -s ${SIZE} -x 1 -I 0 ${VERIFY}
 drop_cache
 
 # GPU->CPU->Storage
-./${GDSIO} -f c.out -s ${SIZE} -x 2 -I 1
+./${GDSIO} ${DIR} -f c.out -s ${SIZE} -x 2 -I 1
 drop_cache
 # Storage->CPU->GPU
-./${GDSIO} -f c.out -s ${SIZE} -x 2 -I 0 ${VERIFY}
+./${GDSIO} ${DIR} -f c.out -s ${SIZE} -x 2 -I 0 ${VERIFY}
 drop_cache
