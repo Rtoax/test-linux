f895fa0ed9c56c120546cea94e87550aafaa3b76
Rong Tao <rongtao@cestc.cn>
2025-12-09 17:06:34 +0800

    pthread/samples: code style cond_broadcast-1.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Vers: Fedora Linux 43, Linux 6.17.9-300.fc43.x86_64, GCC 15.2.1, glibc 2.42

--- glibc/pthread/samples/cond_broadcast-1.c
+++ glibc/pthread/samples/cond_broadcast-1.c
@@ -23,6 +23,7 @@ void *thread_task1(void *arg)
 	printf("thread_task1 start working\n");
 	sleep(2);
 	printf("thread_task1 works over\n");
+
 	pthread_mutex_unlock(&mutex1);
 
 	return NULL;
@@ -37,6 +38,7 @@ void *thread_task2(void *arg)
 	printf("thread_task2 start working\n");
 	sleep(2);
 	printf("thread_task2 works over\n");
+
 	pthread_mutex_unlock(&mutex1);
 
 	return NULL;
@@ -51,19 +53,19 @@ void *thread_task3(void *arg)
 
 int main(void)
 {
-	pthread_t thread_1,thread_2,thread_3;
-	pthread_create(&thread_1,NULL,thread_task1,NULL);
-	pthread_create(&thread_2,NULL,thread_task2,NULL);
+	pthread_t thread_1, thread_2, thread_3;
+	pthread_create(&thread_1, NULL, thread_task1, NULL);
+	pthread_create(&thread_2, NULL, thread_task2, NULL);
 
 	sleep(2);
 
-	pthread_create(&thread_3,NULL,thread_task3,NULL);
+	pthread_create(&thread_3, NULL, thread_task3, NULL);
 
 	sleep(5);
 
-	pthread_join(thread_1,NULL);
-	pthread_join(thread_2,NULL);
+	pthread_join(thread_1, NULL);
+	pthread_join(thread_2, NULL);
+	pthread_join(thread_3, NULL);
 
 	return 0;
 }
-
