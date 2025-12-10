ba2fb65d2e6203a9580551e700ec6f75f35394f2
Rong Tao <rongtao@cestc.cn>
2025-12-10 16:01:22 +0800

    numactl.mk: add HAVE_LIBNUMA
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Vers: Fedora Linux 43, Linux 6.17.10-300.fc43.x86_64, MAKE 4.4.1

--- template/numactl.mk
+++ template/numactl.mk
@@ -1,13 +1,30 @@
 # SPDX-License-Identifier: GPL-3.0
+#
+# Output definitions:
+# - NUMACTL=
+# - HAVE_LIBNUMA=y
+# - NUMACTL_VERSION_MAJOR=
+# - NUMACTL_VERSION_MINOR=
+# - NUMACTL_VERSION_PATCH=
+#
 _NUMACTL = 1
 
 include shell.mk
+include values.mk
 
-NUMACTL := numactl
+NUMACTL := $(shell which numactl 2>/dev/null || :)
+LIBNUMA_HDR := /usr/include/numa.h
 
-# Get numactl version
-NUMACTL_VERSION := $(shell ${NUMACTL} --version 2>/dev/null \
-		| grep -o [0-9]*\.[0-9]*\.[0-9]* | sed -n '1p' || true)
+ifneq (${NUMACTL},)
+  # Get numactl version
+  NUMACTL_VERSION := $(shell ${NUMACTL} --version 2>/dev/null \
+			| grep -o [0-9]*\.[0-9]*\.[0-9]* | sed -n '1p' || true)
+else
+  $(warning Not found numactl, please install first)
+endif
+
+$(call check_file_and_def,${LIBNUMA_HDR},HAVE_LIBNUMA)
+
 # only numactl v2.0.16-47-gf117290fd85c support --version argument
 # https://github.com/numactl/numactl
 ifeq (${NUMACTL_VERSION},)
@@ -28,3 +45,5 @@ ifneq (${NUMACTL_VERSION_MAJOR},2)
   $(error Not support numactl ${NUMACTL_VERSION})
 endif
 
+export NUMACTL
+export NUMACTL_VERSION_MAJOR NUMACTL_VERSION_MINOR NUMACTL_VERSION_PATCH
