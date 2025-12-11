64ef79e1f1f345c283153fabf5f52182bc3268be
Rong Tao <rongtao@cestc.cn>
2025-12-11 11:06:59 +0800

    gdsio_benchmark.sh: each thread has same dataset size
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Hygon C86-4G 7490 64-core Processor, Arch x86_64
    Vers: CCLinux 22.09.2, Linux 5.15.131-26.cl9.x86_64, BASH 5.1.8, LUCA 3.2.1

--- hpc/nvidia/cuda/file/gdsio_benchmark.sh
+++ hpc/nvidia/cuda/file/gdsio_benchmark.sh
@@ -2,19 +2,23 @@
 #
 # GDSIO benchmark
 #
+# Example:
+# $ sudo DIR=/mnt/nvme/ SIZE=4G XFER_TYPES=0 OP_TYPES=0 gdsio_benchmark.sh
 
 set -e
 
 [[ -z ${GDSIO} ]] && GDSIO=gdsio # 1st choice
 [[ ! -e ${GDSIO} ]] && GDSIO=gdsio-luca # 2nd choice
-[[ -z ${SIZE} ]] && SIZE=4G
 [[ -z ${DIR} ]] && DIR="."
 
+# $1: logfile
 runprog() {
+	local logfile=$1
+	shift
 	# drop cache for each test
 	echo 3 > /proc/sys/vm/drop_caches
 	echo >&2 -e "\033[1;32m${@}\033[m"
-	eval ${@} | tee --append gdsio.log
+	eval ${@} | tee --append ${logfile}
 }
 
 readonly XFER_BETWEEN_STORAGE__GPU=0
@@ -30,18 +34,30 @@ readonly OP_READ=0
 readonly OP_WRITE=1
 [[ -z ${OP_TYPES} ]] && OP_TYPES=( ${OP_READ} ${OP_WRITE} )
 
-[[ -e gdsio.log ]] && mv gdsio.log gdsio.log.old
-
 for xfer in ${XFER_TYPES[@]}
 do
-	for thread in 1 2 4 8 16 32
+	for op in ${OP_TYPES[@]}
 	do
-		for op in ${OP_TYPES[@]}
+		logfile=gdsio-x${xfer}o${op}.log
+
+		[[ -e ${logfile} ]] && mv ${logfile} ${logfile}.old
+
+		for thread in 1 2 4 8 16 32
 		do
-			for iosize in 4K 32K 64K 128K 512K 1M 2M
+			for iosize in 4K 32K 64K 128K 512K 1M 2M 4M
 			do
-				runprog ./${GDSIO} -D ${DIR} -f a.out \
-					-s ${SIZE} -i ${iosize} \
+				if [[ ${SIZE} ]]; then
+					size=${SIZE}
+				else
+					# Each thread operates on the same
+					# amount of data in parallel; this
+					# is how the performance of the DMA
+					# engine can be tested.
+					size=$(( ${thread} * 512 ))M
+				fi
+				runprog ${logfile} \
+					./${GDSIO} -D ${DIR} -f gdsio-x${xfer}o${op}.out \
+					-s ${size} -i ${iosize} \
 					-x ${xfer} -I ${op} -w ${thread}
 			done
 		done
