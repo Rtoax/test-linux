b74a79d4ee49fc4023f496b2b889e07ec700cf4a
Rong Tao <rongtao@cestc.cn>
2025-12-11 22:02:35 +0800

    glibc.mk: update code style
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Vers: Ubuntu 25.10, Linux 6.17.0-7-generic, glibc 2.42, MAKE 4.4.1

--- template/glibc.mk
+++ template/glibc.mk
@@ -1,37 +1,35 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # Output definitions:
-# -
+# - LIBC_SO_PATH=
+#
+# Functions:
+# - probe_libc_printf()
+# - libc_sym_addr()
 #
 _GLIBC = 1
 
 GLIBC_VERSION := $(shell getconf GNU_LIBC_VERSION)
 
 # Get libc.so.6 abs-path
-LIBC_SO_PATH=$(shell ldconfig -p | grep libc.so.6 | awk '{printf $$NF"\n"}' | head -1)
+LIBC_SO_PATH := $(shell ldconfig -p | grep libc.so.6 | awk '{printf $$NF"\n"}' | head -1)
 
-LIBC_PRINTF_SYMADDR=0x$(shell readelf --syms --wide ${LIBC_SO_PATH} \
+LIBC_PRINTF_SYMADDR := 0x$(shell readelf --syms --wide ${LIBC_SO_PATH} \
 				| grep -w printf | head -1 \
 				| awk '{printf $$2}')
 
-LIBC___ERRNO_LOCATION_SYMADDR=0x$(shell readelf --syms --wide ${LIBC_SO_PATH} \
+LIBC___ERRNO_LOCATION_SYMADDR := 0x$(shell readelf --syms --wide ${LIBC_SO_PATH} \
 				| grep -w __errno_location | head -1 \
 				| awk '{printf $$2}')
 
-ifdef DEBUG
-  $(info GLIBC_VERSION = ${GLIBC_VERSION})
-  $(info LIBC_PRINTF_SYMADDR = ${LIBC_PRINTF_SYMADDR})
-  $(info LIBC_SO_PATH = ${LIBC_SO_PATH})
-  $(info LIBC___ERRNO_LOCATION_SYMADDR = ${LIBC___ERRNO_LOCATION_SYMADDR})
-endif
-
 # Probe printf(3) with non-output gcc command
-PRINTF_PROBE := '\#include <stdio.h>\n'
-PRINTF_PROBE += 'int main(void) {\n'
-PRINTF_PROBE += '	printf("hello\\n");\n'
-PRINTF_PROBE += '}'
+LIBC_PRINTF_PROBE := '\#include <stdio.h>\n'
+LIBC_PRINTF_PROBE += 'int main(void) {\n'
+LIBC_PRINTF_PROBE += '	printf("hello\\n");\n'
+LIBC_PRINTF_PROBE += '}'
+
 define probe_libc_printf
-  $(shell printf '%b\n' $(PRINTF_PROBE) | \
+  $(shell printf '%b\n' $(LIBC_PRINTF_PROBE) | \
     $(CC) -x c -Wall - $(1) -S -o - >/dev/null 2>&1 \
       && echo 1)
 endef
@@ -46,6 +44,13 @@ $(shell readelf --syms --wide ${LIBC_SO_PATH} \
 		| grep -w $(1) | grep -e GLOBAL -e LOCAL | head -1 \
 		| awk '{printf "0x"$$2}')
 endef
+
 ifdef DEBUG
+  $(info GLIBC_VERSION = ${GLIBC_VERSION})
+  $(info LIBC_PRINTF_SYMADDR = ${LIBC_PRINTF_SYMADDR})
+  $(info LIBC_SO_PATH = ${LIBC_SO_PATH})
+  $(info LIBC___ERRNO_LOCATION_SYMADDR = ${LIBC___ERRNO_LOCATION_SYMADDR})
   $(info printf = <$(call libc_sym_addr,printf)>)
 endif
+
+export LIBC_SO_PATH
