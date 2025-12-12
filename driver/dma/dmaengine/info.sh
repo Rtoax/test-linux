36910d6d3985c4f828fa3be6a8497bb191e01832
Rong Tao <rongtao@cestc.cn>
2025-12-12 10:22:44 +0800

    dma/dmaengine: info.sh: get more info
    rongtao@NUC10:~/Git/tst-linux/driver/dma/dmaengine$ ./info.sh
    DMA        USE  MEMCPY     XFER(B)
    dma0chan0  0    0          0
    dma0chan1  0    0          0
    dma1chan0  0    0          0
    dma1chan1  0    0          0
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Vers: Fedora Linux 43, Linux 6.17.10-300.fc43.x86_64, BASH 5.3.0

--- driver/dma/dmaengine/info.sh
+++ driver/dma/dmaengine/info.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -e
 
-ls /sys/class/dma/
+DMA=( $(ls /sys/class/dma/) )
+IN_USE=( $(cat /sys/class/dma/dma*chan*/in_use) )
+MEMCPY_COUNT=( $(cat /sys/class/dma/dma*chan*/memcpy_count) )
+BYTES_XFER=( $(cat /sys/class/dma/dma*chan*/bytes_transferred) )
+
+
+printf "%-10s %-4s %-10s %-16s\n" "DMA" "USE" "MEMCPY" "XFER(B)"
+for ((i = 0; i < ${#DMA[@]}; i++))
+do
+	printf "%-10s %-4d %-10ld %-16ld\n" ${DMA[i]} ${IN_USE[i]} \
+		${MEMCPY_COUNT[i]} ${BYTES_XFER[i]}
+done
