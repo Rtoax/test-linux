e6f7d7de85b0c115f1f9dcd0daec736aa39f13d1
Rong Tao <rtoax@foxmail.com>
2025-12-15 18:27:42 +0800

    arch.mk: add more and apply to arch/Makefile
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU Phytium, Arch aarch64
    Vers: CCLinux 22.09.2, Linux 5.15.131-28.cl9.aarch64+4k, MAKE 4.3

--- template/arch.mk
+++ template/arch.mk
@@ -3,8 +3,12 @@
 #
 # Output definitions:
 # - CFLAGS_ARCH=
+#
 # - IS_X86_64=y
 # - IS_AARCH64=y
+# - IS_LOONGARCH64=y
+# - IS_RISCV64=y
+# - IS_SW_64=y
 
 _ARCH = 1
 
@@ -22,6 +26,14 @@ else ifeq ($(shell uname -m),sw_64)
   else ifeq ($(shell lscpu | grep -o SW3231 || true),SW3231)
     CFLAGS_ARCH += -mcpu=sw6b
   endif
+  IS_SW_64 := y
+  export IS_SW_64
+else ifeq ($(shell uname -m),loongarch64)
+  IS_LOONGARCH64 := y
+  export IS_LOONGARCH64
+else ifeq ($(shell uname -m),riscv64)
+  IS_RISCV64 := y
+  export IS_RISCV64
 endif
 
 export CFLAGS_ARCH
