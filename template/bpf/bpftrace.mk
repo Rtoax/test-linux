a8b2b84745e1c4b7fd0a8eff2f5627039a3c5d6a
Rong Tao <rongtao@cestc.cn>
2025-12-16 09:09:40 +0800

    bpf/bpftrace.mk: add HAVE_BPFTRACE=y
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Phytium, Arch aarch64
    Vers: CCLinux 22.09.2, Linux 5.15.131-27.cl9.aarch64, MAKE 4.3, BPFTrace v0.13.1, BCC 0.24.0, LIBBPF 0.6.0

--- template/bpf/bpftrace.mk
+++ template/bpf/bpftrace.mk
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # Output definitions:
+# - HAVE_BPFTRACE=y
 # - BPFTRACE=
 # - BPFTRACE_VERSION_MAJOR=
 # - BPFTRACE_VERSION_MINOR=
@@ -9,17 +10,16 @@ _BPFTRACE = 1
 
 BPFTRACE := $(shell which bpftrace 2>/dev/null)
 ifeq ($(BPFTRACE),)
-  ifneq ($(wildcard /usr/bin/bpftrace),)
-    BPFTRACE := /usr/bin/bpftrace
-  else
-    $(error "Not found bpftrace, install first")
-  endif
+  $(error "Not found bpftrace, install first")
 endif
+
+HAVE_BPFTRACE := y
 
 BPFTRACE_VERSION := $(shell ${BPFTRACE} --version | grep -o v[0-9].[0-9]\. | sed -n '1p')
 BPFTRACE_VERSION_MAJOR := $(shell echo ${BPFTRACE_VERSION} | awk -F '.' '{print $$1}')
 BPFTRACE_VERSION_MINOR := $(shell echo ${BPFTRACE_VERSION} | awk -F '.' '{print $$2}')
 
+export HAVE_BPFTRACE
 export BPFTRACE BPFTRACE_VERSION_MAJOR BPFTRACE_VERSION_MINOR
 
 ifdef DEBUG
