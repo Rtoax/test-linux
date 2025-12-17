4cfe2f3d2ab902a6593c1cade3b5a24e6d235da2
Rong Tao <rongtao@cestc.cn>
2025-12-17 15:14:27 +0800

    nvme: nvme2pcie.sh: add numa
    [rongtao@710-E nvme]$ ./nvme2pcie.sh
    NVME             PCI_SLOT         NUMA
    nvme0n1          0002:01:00.0     2
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Phytium, Arch aarch64
    Vers: CCLinux 21.08.5, Linux 5.15.131-28.cl9.aarch64, BASH 5.1.8

--- storage/nvme/nvme2pcie.sh
+++ storage/nvme/nvme2pcie.sh
@@ -4,6 +4,16 @@ set -e
 NVME=( $(ls /sys/block/ | grep 'nvme[0-9]') )
 declare -a SLOT
 
+pcie_slot_to_numa() {
+	local slot=$1
+	local numa=$(lspci -s ${slot} -v 2>/dev/null | \
+		grep -Eo 'NUMA node [0-9]+' | grep -Eo '[0-9]+')
+	if [[ -z ${numa} ]]; then
+		numa='N/A'
+	fi
+	echo ${numa}
+}
+
 if [[ ${#NVME[@]} -eq 0 ]]; then
 	echo >&2 "ERROR: not found any NVME from /sys/block/"
 	exit 0
@@ -16,8 +26,9 @@ do
 		tail -n1) )
 done
 
-printf "%-16s %-16s\n" "NVME" "PCI_SLOT"
+printf "%-16s %-16s %-4s\n" "NVME" "PCI_SLOT" "NUMA"
 for ((i = 0; i < ${#NVME[@]}; i++))
 do
-	printf "%-16s %-16s\n" ${NVME[i]} ${SLOT[i]}
+	numa=$(pcie_slot_to_numa ${SLOT[i]})
+	printf "%-16s %-16s %-4s\n" ${NVME[i]} ${SLOT[i]} ${numa}
 done
