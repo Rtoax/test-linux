8dd50fda3011ea8798975ca7610851126152d306
Rong Tao <rongtao@cestc.cn>
2025-12-23 14:21:20 +0800

    memcpy-stress.c: add space
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Vers: Fedora Linux 43, Linux 6.17.12-300.fc43.x86_64, GCC 15.2.1, glibc 2.42

--- glibc/string/memcpy-stress.c
+++ glibc/string/memcpy-stress.c
@@ -25,32 +25,32 @@ typedef void *(*memcpy_fn)(void *, const void *, size_t);
 
 #ifdef SYMADDR___memcpy_ssse3
 static memcpy_fn __memcpy_ssse3;
-#define memcpy_stub __memcpy_ssse3
-#define memcpy_name "__memcpy_ssse3"
+# define memcpy_stub __memcpy_ssse3
+# define memcpy_name "__memcpy_ssse3"
 #elif defined(SYMADDR___memcpy_generic)
 static memcpy_fn __memcpy_generic;
-#define memcpy_stub __memcpy_generic
-#define memcpy_name "__memcpy_generic"
+# define memcpy_stub __memcpy_generic
+# define memcpy_name "__memcpy_generic"
 #elif defined(SYMADDR___memcpy_simd)
 static memcpy_fn __memcpy_simd;
-#define memcpy_stub __memcpy_simd
-#define memcpy_name "__memcpy_simd"
+# define memcpy_stub __memcpy_simd
+# define memcpy_name "__memcpy_simd"
 #elif defined(SYMADDR___memcpy_a64fx) /* arm SVE/SVE2 */
 static memcpy_fn __memcpy_a64fx;
-#define memcpy_stub __memcpy_a64fx
-#define memcpy_name "__memcpy_a64fx"
+# define memcpy_stub __memcpy_a64fx
+# define memcpy_name "__memcpy_a64fx"
 #elif defined(ARM_SOFTWARE___memcpy_aarch64) /* https://github.com/ARM-software/optimized-routines */
 extern void *__memcpy_aarch64(void *, const void *, size_t);
-#define memcpy_stub __memcpy_aarch64
-#define memcpy_name "__memcpy_aarch64"
+# define memcpy_stub __memcpy_aarch64
+# define memcpy_name "__memcpy_aarch64"
 #elif defined(ARM_SOFTWARE___memcpy_aarch64_sve)
 extern void *__memcpy_aarch64_sve(void *, const void *, size_t);
-#define memcpy_stub __memcpy_aarch64_sve
-#define memcpy_name "__memcpy_aarch64_sve"
+# define memcpy_stub __memcpy_aarch64_sve
+# define memcpy_name "__memcpy_aarch64_sve"
 #else
 /* fallback to glibc's memcpy */
-#define memcpy_stub memcpy
-#define memcpy_name "memcpy"
+# define memcpy_stub memcpy
+# define memcpy_name "memcpy"
 #endif
 
 static size_t block_size = 256;
