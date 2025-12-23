8ecd4ef7a28ec20f9cbc4c7cb31b1e7745cf8145
Rong Tao <rongtao@cestc.cn>
2025-12-23 16:25:27 +0800

    template: bits/cpu-feature.mk: add CPU_HAVE_AVX2 CPU_HAVE_AVX512F
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU 11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz, Arch x86_64
    Vers: Debian GNU/Linux 13, Linux 6.12.57+deb13-amd64, MAKE 4.4.1

--- template/bits/cpu-feature.mk
+++ template/bits/cpu-feature.mk
@@ -1,4 +1,19 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (C) 2025 Rong Tao
 #
+# Output definitions:
+# - CPU_HAVE_AVX2=y
+# - CPU_HAVE_AVX512F=y
+#
 _CPU_FEATURE = 1
+
+CPU_HAVE_AVX2 := $(shell lscpu | grep -wo avx512f >/dev/null && echo y)
+CPU_HAVE_AVX512F := $(shell lscpu | grep -wo avx512f >/dev/null && echo y)
+
+ifdef DEBUG
+  $(info CPU_HAVE_AVX512F = ${CPU_HAVE_AVX512F})
+  $(info CPU_HAVE_AVX2 = ${CPU_HAVE_AVX2})
+endif
+
+export CPU_HAVE_AVX2
+export CPU_HAVE_AVX512F
