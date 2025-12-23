7e1c173cb8d0a91e28c56da0dac9f8166056b3cd
Rong Tao <rtoax@foxmail.com>
2025-12-23 07:20:31 +0000

    memcpy-stress.sh: auto test all
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU Cortex-X925 Cortex-A725, Arch aarch64
    Vers: Ubuntu 24.04, Linux 6.14.0-1015-nvidia, glibc 2.39, BASH 5.2.21

--- glibc/string/memcpy-stress.sh
+++ glibc/string/memcpy-stress.sh
@@ -6,14 +6,21 @@ export -f make_tl
 
 readonly MiB=1024*1024
 
-make_tl memcpy-stress
+make_tl
 
 pfx=
-exe=memcpy-stress
-firstline=YES
+exes=( memcpy-stress
+	__memcpy_ssse3-stress
+	__memcpy_generic-stress
+	__memcpy_simd-stress
+	__memcpy_a64fx-stress
+	__memcpy_aarch64-stress
+	__memcpy_aarch64_simd-stress
+	__memcpy_aarch64_sve-stress )
+
 msize_MB=1024*4
 
-[[ $1 ]] && exe=$1
+[[ $1 ]] && exes=( $1 )
 [[ ${MB} ]] && msize_MB=${MB}
 
 if [[ ${CROSS_COMPILE} ]]; then
@@ -27,13 +34,23 @@ if [[ ${CROSS_COMPILE} ]]; then
 	esac
 fi
 
-echo "$(./${exe} --uname)"
-echo "${exe} $(./${exe} --version)"
-
-# To avoid some memory boundary issues, it is not a power of 2, so choose "31".
-for b in $(seq 16 31 8192)
+for exe in ${exes}
 do
-	${pfx} ./${exe} --iosize ${b} --msize $((${MiB}*${msize_MB})) \
-		${firstline:+--verbose}
-	firstline=
+	firstline=YES
+
+	if [[ ! -e ${exe} ]]; then
+		echo "WARNING: not support ${exe}"
+		continue
+	fi
+
+	echo "$(./${exe} --uname)"
+	echo "${exe} $(./${exe} --version)"
+
+	# To avoid some memory boundary issues, it is not a power of 2, so choose "31".
+	for b in $(seq 16 31 8192)
+	do
+		${pfx} ./${exe} --iosize ${b} --msize $((${MiB}*${msize_MB})) \
+			${firstline:+--verbose}
+		firstline=
+	done
 done
