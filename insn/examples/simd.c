a0cbfc1dcc17d472accff796997598d1e77422b0
Rong Tao <rtoax@foxmail.com>
2025-12-26 18:43:54 +0800

    simd.c: add commit comment
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU Cortex-X925 Cortex-A725, Arch aarch64
    Vers: Ubuntu 24.04, Linux 6.14.0-1015-nvidia, GCC 13.3.0, glibc 2.39

--- insn/examples/simd.c
+++ insn/examples/simd.c
@@ -12,6 +12,7 @@
  * - commit f7d73d7d348a ("simd: Phytium S5000C")
  * - commit 745fb7691dba ("simd: HiSilicon HUAWEI Kunpeng 920 V200 7280Z")
  * - commit 04214fc98386 ("simd: NVIDIA GB10 Spark Cortex-X925")
+ * - commit 6e4cf729faf9 ("simd: NVIDIA GB10 Spark Cortex-A725")
  */
 #include <sched.h>
 #include <stdio.h>
