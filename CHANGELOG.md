20df3897ac9b31202d92b1c2ca0c963a280b5612
Rong Tao <rongtao@cestc.cn>
2026-01-04 12:14:30 +0800

    template: add lingspeed.mk
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Hygon C86-4G 7490 64-core Processor, Arch x86_64
    Vers: CCLinux 22.09.2, Linux 5.15.131-26.cl9.x86_64, MAKE 4.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to
 
 #### Breaking Changes
 #### Added
+- template: add lingspeed.mk;
 - bpftrace: add bpftrace.vim;
 - template: add targets/helpers.mk;
 - template: add Makefile;
---
+++ template/lingspeed.mk
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: GPL-3.0
+# Copyright (C) 2026 Rong Tao
+#
+# Input definitions:
+# - __IGNORE_NOTFOUND_ERROR__
+#
+# Output definitions:
+# - LSSMI=/usr/bin/ls-smi
+# - HAVE_LINGSPEED_X710E=y
+# - HAVE_LINGSPEED_X710M=y
+# - HAVE_LINGSPEED_X710P=y
+#
+_LINGSPEED_MK = 1
+
+ifeq ($(wildcard ${LSSMI}),)
+  ifndef __IGNORE_NOTFOUND_ERROR__
+    $(error No found ls-smi in anywhere)
+  else
+    LSSMI :=
+    $(warning No found ls-smi but __IGNORE_NOTFOUND_ERROR__ defined)
+  endif
+endif
+
+ifneq ($(LSSMI),)
+  ifeq ($(shell ${LSSMI} | grep -oe "X710-E"),X710-E)
+    export HAVE_LINGSPEED_X710E := y
+  endif
+  ifeq ($(shell ${LSSMI} | grep -oe "X710-M"),X710-M)
+    export HAVE_LINGSPEED_X710M := y
+  endif
+  ifeq ($(shell ${LSSMI} | grep -oe "X710-P"),X710-P)
+    export HAVE_LINGSPEED_X710P := y
+  endif
+  export LSSMI
+endif
+
+ifdef DEBUG
+  $(info LSSMI = ${LSSMI})
+  $(info HAVE_LINGSPEED_X710E = ${HAVE_LINGSPEED_X710E})
+  $(info HAVE_LINGSPEED_X710M = ${HAVE_LINGSPEED_X710M})
+  $(info HAVE_LINGSPEED_X710P = ${HAVE_LINGSPEED_X710P})
+endif
