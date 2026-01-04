33a09b87c02d542df48c1143b11bc54d6132de6c
Rong Tao <rtoax@foxmail.com>
2026-01-04 17:34:07 +0800

    qemu: add version.sh
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU Hygon C86-4G (OPN:7470), Arch x86_64
    Vers: CCLinux 22.09.2, Linux 5.15.131-28.cl9.x86_64, BASH 5.1.8, QEMU 7.0.0, MAKE 4.3, GIT 2.39.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to
 
 #### Breaking Changes
 #### Added
+- qemu: add version.sh;
 - qemu: add support-machine.sh;
 - template: add lingspeed.mk;
 - bpftrace: add bpftrace.vim;
---
+++ qemu/version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script only display qemu version, do not display other anything,
+# because the git/hooks will use it.
+set -e
+
+readonly WHERE_AM_I=$(dirname $(realpath $0))
+. ${WHERE_AM_I}/libqemu.sh
+
+QEMU_KVM=$(get_qemu_kvm_emulator)
+if [[ -z ${QEMU_KVM} ]]; then
+	echo >&2 "ERROR: not found QEMU in your system"
+	exit 0
+fi
+${QEMU_KVM} --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' | head -1
