0e2612a53705df96299b913095a2831ea287dcc8
Rong Tao <rongtao@cestc.cn>
2026-01-05 17:04:20 +0800

    cuda_compat.h: Fix non-exist cupti header for CUDA V12.4.131
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU 11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz, Arch x86_64
    Vers: Debian GNU/Linux 13, Linux 6.12.57+deb13-amd64, GCC 14.2.0, glibc 2.41, LLVM 19.1.7, CUDA V12.4.131

--- hpc/nvidia/cuda/cuda_compat.h
+++ hpc/nvidia/cuda/cuda_compat.h
@@ -194,11 +194,13 @@
 # endif
 # ifdef HAVE_CUPTI
 #  include <cupti.h>
-#  include <cupti_pmsampling.h>
 #  include <cupti_profiler_target.h>
-#  include <cupti_profiler_host.h>
-#  include <cupti_range_profiler.h>
 #  include <cupti_target.h>
+#  if CUDA_VERSION > 12040
+#   include <cupti_pmsampling.h>
+#   include <cupti_profiler_host.h>
+#   include <cupti_range_profiler.h>
+#  endif
 # endif
 # define CUNAME	"NVIDIA CUDA"
 #endif
