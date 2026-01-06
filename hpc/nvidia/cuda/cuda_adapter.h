7f2b71fee3513aa2db923dbdfb4fd5d787eddc2e
Rong Tao <rongtao@cestc.cn>
2026-01-06 13:50:53 +0800

    cuda/samples: add version.cu
    $ ./version-nv
    Driver version is: 13.0
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Cortex-X925 Cortex-A725, Arch aarch64
    Vers: Ubuntu 24.04, Linux 6.14.0-1015-nvidia, GCC 13.3.0, glibc 2.39, MAKE 4.3, LLVM 18.1.3, CUDA V13.0.88

--- hpc/nvidia/cuda/cuda_adapter.h
+++ hpc/nvidia/cuda/cuda_adapter.h
@@ -127,6 +127,12 @@
 #define cudaDeviceGetLimit(limit, value)	__cuda(DeviceGetLimit(limit, value))
 
 /**
+ * CUDA
+ * cudaError_t cudaDriverGetVersion(int *driverVersion);
+ */
+#define cudaDriverGetVersion	__cuda(DriverGetVersion)
+
+/**
  * https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__MEMORY.html
  */
 #define cudaMalloc(pp, sz)	__cuda(Malloc(pp, sz))
---
+++ hpc/nvidia/cuda/samples/version.cu
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+/* Copyright (C) 2026 Rong Tao */
+#include <stdio.h>
+#include "cuda_compat.h"
+#include "cuda_helpers.h"
+
+
+int main(void)
+{
+	int driverVersion = 0;
+	cudaDriverGetVersion(&driverVersion);
+	printf("Driver version is: %d.%d\n", driverVersion / 1000, (driverVersion % 100) / 10);
+	return 0;
+}
