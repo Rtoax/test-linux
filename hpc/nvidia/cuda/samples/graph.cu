05b1d9b3e788f074a0333c7cd0a840d16c88a148
Rong Tao <rongtao@cestc.cn>
2026-01-06 14:06:35 +0800

    graph.cu: add check
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Cortex-X925 Cortex-A725, Arch aarch64
    Vers: Ubuntu 24.04, Linux 6.14.0-1015-nvidia, LLVM 18.1.3, CUDA V13.0.88

--- hpc/nvidia/cuda/samples/graph.cu
+++ hpc/nvidia/cuda/samples/graph.cu
@@ -5,7 +5,11 @@
 #include "cuda_helpers.h"
 
 
-int main(void)
+/**
+ * https://github.com/NVIDIA/cuda-samples.git
+ * Samples/3_CUDA_Features/graphMemoryNodes/graphMemoryNodes.cu
+ */
+void graph_memory(void)
 {
 	cudaGraph_t graph;
 	cudaGraphNode_t node;
@@ -16,5 +20,29 @@ int main(void)
 	//cudaGraphAddMemcpyNode1D();
 
 	cudaGraphDestroy(graph);
+}
+
+int main(void)
+{
+	int device = 0;
+	int driverVersion = 0;
+	int deviceSupportsMemoryPools = 0;
+
+	cudaDriverGetVersion(&driverVersion);
+	printf("Driver version is: %d.%d\n", driverVersion / 1000, (driverVersion % 100) / 10);
+
+#ifdef __NVCC__
+	if (driverVersion < 11040) {
+		printf("Waiving execution as driver does not support Graph Memory Nodes\n");
+		exit(1);
+	}
+#endif
+
+	cudaDeviceGetAttribute(&deviceSupportsMemoryPools, cudaDevAttrMemoryPoolsSupported, device);
+	if (!deviceSupportsMemoryPools) {
+		printf("Waiving execution as device does not support Memory Pools\n");
+		exit(1);
+	}
+
 	return 0;
 }
