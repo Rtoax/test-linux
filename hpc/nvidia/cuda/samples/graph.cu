ea99ee997a37df92aad1499ede7b0c5e11f2715d
Rong Tao <rongtao@cestc.cn>
2026-01-07 15:33:49 +0800

    graph.cu: done the bad version
    [1] Samples/3_CUDA_Features/graphMemoryNodes/graphMemoryNodes.cu
    
    Link: https://github.com/NVIDIA/cuda-samples.git [1]
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Cortex-X925 Cortex-A725, Arch aarch64
    Vers: Ubuntu 24.04, Linux 6.14.0-1015-nvidia, GIT 2.43.0, LLVM 18.1.3, CUDA V13.0.88

--- hpc/nvidia/cuda/samples/graph.cu
+++ hpc/nvidia/cuda/samples/graph.cu
@@ -4,6 +4,14 @@
 #include "cuda_compat.h"
 #include "cuda_helpers.h"
 
+__global__ void squareArray(const float *input, float *output, int numElements)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < numElements) {
+		printf("--------------\n");
+		output[idx] = input[idx] * input[idx];
+	}
+}
 
 /**
  * https://github.com/NVIDIA/cuda-samples.git
@@ -12,33 +20,76 @@
 void graph_memory(int device)
 {
 	float *d_input, *d_square;
+	float *input, *square;
+	size_t numElements = 16;
+	size_t bytes = numElements * sizeof(*input);
+	cudaStream_t stream;
 	cudaGraph_t graph;
+	cudaGraphExec_t graphExec;
 	cudaGraphNode_t allocNodeInput, allocNodeSquare;
 	cudaGraphNode_t copyNodeInput, copyNodeSquare;
-	cudaGraphExec_t exec;
+	cudaGraphNode_t squareKernelNode;
+	cudaGraphNode_t freeNodeInput, freeNodeSquare;
 	cudaMemAllocNodeParams allocParams;
 	cudaKernelNodeParams kernelNodeParams = {0};
 
-	allocParams.bytesize = 1024;
+	input = (float *)malloc(bytes);
+	square = (float *)malloc(bytes);
+
+	for (size_t i = 0; i < numElements; i++)
+		input[i] = 2.f;
+
+	allocParams.bytesize = bytes;
 	allocParams.poolProps.allocType = cudaMemAllocationTypePinned;
 	allocParams.poolProps.location.id = device;
 	allocParams.poolProps.location.type = cudaMemLocationTypeDevice;
-
-	kernelNodeParams.gridDim = dim3(512, 1, 1);
-	kernelNodeParams.blockDim = dim3(512, 1, 1);
-	kernelNodeParams.sharedMemBytes = 0;
-	kernelNodeParams.extra = NULL;
 
 	cudaGraphCreate(&graph, 0);
 	cudaGraphAddMemAllocNode(&allocNodeInput, graph, NULL, 0, &allocParams);
 	d_input = (float *)allocParams.dptr;
 
-	cudaGraphAddMemAllocNode(&allocNodeSquare, graph, &allocNodeInput, 1, &allocParams);
+	cudaGraphAddMemAllocNode(&allocNodeSquare, graph, &allocNodeInput, 1,
+				 &allocParams);
 	d_square = (float *)allocParams.dptr;
 
-	//cudaGraphAddMemcpyNode1D(&copyNodeInput, graph, &allocNodeSquare);
+	cudaGraphAddMemcpyNode1D(&copyNodeInput, graph, &allocNodeSquare, 1,
+				 d_input, input, bytes, cudaMemcpyHostToDevice);
 
+	void *squareKernelArgs[3] = { (void *)&d_input, (void *)&d_square,
+				      (void *)&numElements };
+	kernelNodeParams.gridDim = dim3(512, 1, 1);
+	kernelNodeParams.blockDim = dim3(512, 1, 1);
+	kernelNodeParams.sharedMemBytes = 0;
+	kernelNodeParams.extra = NULL;
+	kernelNodeParams.func = (void *)squareArray;
+	kernelNodeParams.kernelParams = (void **)squareKernelArgs;
+
+	cudaGraphAddKernelNode(&squareKernelNode, graph, &copyNodeInput, 1,
+			       &kernelNodeParams);
+
+	cudaGraphAddMemcpyNode1D(&copyNodeSquare, graph, &squareKernelNode, 1,
+				 square, d_square, bytes,
+				 cudaMemcpyDeviceToHost);
+
+	cudaGraphAddMemFreeNode(&freeNodeInput, graph, &squareKernelNode, 1,
+				d_input);
+	cudaGraphAddMemFreeNode(&freeNodeSquare, graph, &squareKernelNode, 1,
+				d_square);
+
+	cudaGraphInstantiate(&graphExec, graph, NULL, NULL, 0);
 	cudaGraphDestroy(graph);
+
+	cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking);
+	cudaGraphLaunch(graphExec, stream);
+	cudaStreamSynchronize(stream);
+	cudaStreamDestroy(stream);
+	cudaGraphExecDestroy(graphExec);
+
+	for (size_t i = 0; i < numElements; i++)
+		printf("input[%ld] ^ 2 = square[%ld] = %f ^ 2 = %f\n", i, i,
+		       input[i], square[i]);
+	free(input);
+	free(square);
 }
 
 int main(void)
