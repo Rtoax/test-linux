f2f8da01b5ac0dea7cbb169ed9d97297134aa09f
Rong Tao <rongtao@cestc.cn>
2026-01-08 10:28:54 +0800

    llvm: version.sh: use llvm-objdump if llvm-config is not exist
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Hygon C86-4G 7490 64-core Processor, Arch x86_64
    Vers: CCLinux 22.09.2, Linux 5.15.131-26.cl9.x86_64, GCC 11.3.1, BASH 5.1.8, LLVM 14.0.6

--- compiler/llvm/version.sh
+++ compiler/llvm/version.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# This script only display LLVM version, do not display other anything,
+# because the git/hooks will use it.
 set -e
-llvm-config --version 2>/dev/null
+llvm-config --version 2>/dev/null || {
+	llvm-objdump --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' 2>/dev/null
+}
