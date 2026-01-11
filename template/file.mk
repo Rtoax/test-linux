61f9fe1a41ef836a8d80887fbe54e5b75df77caa
Rong Tao <rongtao@cestc.cn>
2026-01-11 17:01:28 +0800

    file.mk: add comment
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Cortex-X925 Cortex-A725, Arch aarch64
    Vers: Ubuntu 24.04, Linux 6.14.0-1015-nvidia, MAKE 4.3

--- template/file.mk
+++ template/file.mk
@@ -2,21 +2,22 @@
 # Copyright (C) 2022-2026 Rong Tao
 #
 # Export functions:
-# - reset_file($file): rename file to file.old
+# - reset_file($file)
 #
 _FILE_MK = 1
 
 include shell.mk
 
+# Rename file to file.old, by recursion, old files are not deleted.
 # $1: filename
 define reset_file
-	function __reset_file() { \
-		if [[ -e $${1} ]]; then \
-			if [[ -e $${1}.old ]]; then \
-				__reset_file $${1}.old; \
-			fi; \
-			mv $${1} $${1}.old; \
-		fi; \
-	}; \
-	__reset_file $1
+  function __reset_file() { \
+    if [[ -e $${1} ]]; then \
+      if [[ -e $${1}.old ]]; then \
+        __reset_file $${1}.old; \
+      fi; \
+      mv $${1} $${1}.old; \
+    fi; \
+  }; \
+  __reset_file $1
 endef
