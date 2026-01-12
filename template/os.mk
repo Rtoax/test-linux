7be20f1851f442cf67c449fdf45f4f14bdf7646c
Rong Tao <rongtao@cestc.cn>
2026-01-12 11:13:52 +0800

    os.mk: update format
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Vers: Fedora Linux 43, Linux 6.18.4-200.fc43.x86_64, MAKE 4.4.1

--- template/os.mk
+++ template/os.mk
@@ -2,21 +2,21 @@
 # Copyright (C) 2025-2026 Rong Tao
 #
 # Output definitions:
-# - OS_ID=
-# - OS_VERSION_ID=
+# - OS_ID=[fedora]
+# - OS_VERSION_ID=[43]
 #
 _OS_MK = 1
 
 OS_ID := $(shell grep ^ID= /etc/os-release | sed 's/ID=//g' | tr -d '"')
 OS_VERSION_ID := $(shell grep ^VERSION_ID= /etc/os-release | sed 's/VERSION_ID=//g' | tr -d '"')
 
+ifeq ($(OS_VERSION_ID),)
+  $(error Not found VERSION_ID in /etc/os-release)
+endif
+
 ifdef DEBUG
   $(info OS_ID = ${OS_ID})
   $(info OS_VERSION_ID = ${OS_VERSION_ID})
-endif
-
-ifeq ($(OS_VERSION_ID),)
-  $(error Not found VERSION_ID in /etc/os-release)
 endif
 
 export OS_ID OS_VERSION_ID
