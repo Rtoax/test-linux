4734d32ae3e08aed79b0b929427f8eb6de0dcf7a
Rong Tao <rongtao@cestc.cn>
2026-01-13 21:45:01 +0800

    tracefs.mk: add PATH_TRACEFS=
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Vers: Ubuntu 25.10, Linux 6.17.0-8-generic, MAKE 4.4.1

--- template/fs/tracefs.mk
+++ template/fs/tracefs.mk
@@ -2,5 +2,12 @@
 # Copyright (C) 2026 Rong Tao
 #
 # Output definitions:
+# - TRACEFS=[/sys/kernel/tracing/,/sys/kernel/debug/tracing/]
 #
 _FS_TRACEFS_MK = 1
+
+PATH_TRACEFS := /sys/kernel/tracing/
+
+ifdef DEBUG
+  $(info PATH_TRACEFS = ${PATH_TRACEFS})
+endif
