bf10a5de987324b82faefcedafa5ffb4d6a424ee
Rong Tao <rongtao@cestc.cn>
2026-01-14 10:53:49 +0800

    cupy: cuda.py: get device count
    (base) [root@muxi-4 samples]# ./cuda.py
    Is GPU[cuda] available? -> True
    cupy version  12.0.0
    device count  8
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Phytium, Arch aarch64, Lingspeed X710-M:8
    Vers: CCLinux 22.09.2, Linux 5.15.131-28.cl9_gds.1.aarch64, CUDA V13.0.48, LLVM 14.0.6, LUCA 3.2.1, HPCC 3.0.0, Python 3.9.18

--- python/cupy/samples/cuda.py
+++ python/cupy/samples/cuda.py
@@ -3,3 +3,6 @@
 import cupy as cp
 
 print("Is GPU[cuda] available? ->", cp.cuda.is_available())
+if cp.cuda.is_available():
+    print("cupy version ", cp.__version__)
+    print("device count ", cp.cuda.runtime.getDeviceCount())
