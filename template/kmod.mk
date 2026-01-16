6dbda582f317fbd539a612c80e8331dc6952200d
Rong Tao <rongtao@cestc.cn>
2026-01-16 18:30:28 +0800

    template: add kmod.mk
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, Physical Machine
    Vers: Fedora Linux 43, Linux 6.18.5-200.fc43.x86_64, MAKE 4.4.1

---
+++ template/kmod.mk
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: GPL-3.0
+# Copyright (C) 2026 Rong Tao
+#
+# Exports:
+# - KDIR=
+#
+ifndef _KMOD_MK
+_KMOD_MK = 1
+
+KDIR := /lib/modules/$(shell uname -r)/build/
+
+ifeq ($(wildcard ${KDIR}),)
+  $(error Not found ${KDIR}, install kernel development package first)
+endif
+
+endif
