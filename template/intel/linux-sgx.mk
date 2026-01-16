c987221e936c60260498bcd41b72823a549567fb
Rong Tao <rongtao@cestc.cn>
2026-01-16 14:36:20 +0800

    intel/linux-sgx.mk: add SGX_SIGN and SGX_EDGER8R
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/intel/linux-sgx.mk
+++ template/intel/linux-sgx.mk
@@ -5,8 +5,18 @@
 # https://github.com/intel/linux-sgx
 #
 # Ouput definitions:
+# - SGX_SIGN=[/usr/bin/sgx_sign]
+# - SGX_EDGER8R=[/usr/bin/sgx_edger8r]
 #
 ifndef _INTEL_LINUX_SGX_MK
 _INTEL_LINUX_SGX_MK = 1
+
+SGX_SIGN := $(shell which sgx_sign 2>/dev/null)
+SGX_EDGER8R := $(shell which sgx_edger8r 2>/dev/null)
+
+ifdef DEBUG
+  $(info SGX_SIGN = ${SGX_SIGN})
+  $(info SGX_EDGER8R = ${SGX_EDGER8R})
+endif
 
 endif
