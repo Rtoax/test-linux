74b8d602803a05bf5bcb133ca09c04af50cc036e
Rong Tao <rongtao@cestc.cn>
2026-01-19 12:13:23 +0800

    vllm: build/ubuntu.sh: add
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Cortex-X925 Cortex-A725, Arch aarch64, Physical Machine
    Vers: Ubuntu 24.04, Linux 6.14.0-1015-nvidia, BASH 5.2.21

--- ai/vllm/build/ubuntu.sh
+++ ai/vllm/build/ubuntu.sh
@@ -2,5 +2,13 @@
 # see vllm::docker/Dockerfile
 set -e
 
+sudo apt update -y
+sudo apt install -y --no-install-recommends \
+	ccache \
+	software-properties-common \
+	git \
+	curl \
+	sudo \
+	python3-pip \
+	libibverbs-dev
 sudo pip install -r requirements/cuda.txt --break-system-packages
-
