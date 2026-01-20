db030e187618f54bde183bd325be421637c258b9
Rong Tao <rongtao@cestc.cn>
2026-01-20 15:31:08 +0800

    cuda: add mempool.cu
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Cortex-X925 Cortex-A725, Arch aarch64, Physical Machine, NVIDIA GB10
    Vers: Ubuntu 24.04, Linux 6.14.0-1015-nvidia, MAKE 4.3, LLVM 18.1.3, CUDA V13.0.88

---
+++ hpc/nvidia/cuda/samples/mempool.cu
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+/* Copyright (C) 2026 Rong Tao */
+/**
+ * cudaError_t cudaMemPoolCreate(cudaMemPool_t *memPool, const struct cudaMemPoolProps *poolProps);
+ */
+#include <argp.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <malloc.h>
+#include "cuda_compat.h"
+#include "cuda_helpers.h"
+
+int main(int argc, char *argv[])
+{
+	cudaMemPool_t pool;
+	cudaMemPoolProps props = {};
+
+	props.allocType = cudaMemAllocationTypePinned;
+	props.location.id = 0; /* device id */
+	props.location.type = cudaMemLocationTypeDevice;
+
+	cudaMemPoolCreate(&pool, &props);
+
+	return 0;
+}
