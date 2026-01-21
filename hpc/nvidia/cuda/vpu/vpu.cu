59bf2fb8f37deae3e358c972f25008c38d23cec4
Rong Tao <rongtao@cestc.cn>
2026-01-21 19:01:15 +0800

    vpu.cu: luca add lcVpuFrameInfo
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Hygon C86-4G 7490 64-core Processor, Arch x86_64, Physical Machine, Lingspeed X710-E:2
    Vers: CCLinux 22.09.2, Linux 5.15.131-26.cl9.x86_64, LLVM 14.0.6, CUDA V13.0.88, LUCA 3.2.1

--- hpc/nvidia/cuda/vpu/vpu.cu
+++ hpc/nvidia/cuda/vpu/vpu.cu
@@ -10,19 +10,24 @@
 
 int main(int argc, char *argv[])
 {
-	lcVPUInst encInst;
+	lcVPUInst inst;
 	lcVPUEncParamterType cfg;
+	lcVpuFrameInfo input_frame, output_frame;
 
-	CUDA_RUNTIME_CHECK_EXIT(lcVpueOpen(&encInst));
+	lcInit(0);
+	lcSetDevice(0);
+	CUDA_RUNTIME_CHECK_EXIT(lcVpueOpen(&inst));
 
 	memset(&cfg, 0, sizeof(lcVPUEncParamterType));
+	memset(&input_frame, 0, sizeof(lcVpuFrameInfo));
+
 	cfg.code_type = lcVpuH264;
 	cfg.width = 1920;
 	cfg.height = 1080;
 	cfg.pixfmt = lcEncYuv420Planar;
 
-	CUDA_RUNTIME_CHECK_EXIT(lcVpueCtrl(encInst, lcSetEncAll, (void *)&cfg));
+	CUDA_RUNTIME_CHECK_EXIT(lcVpueCtrl(inst, lcSetEncAll, (void *)&cfg));
 
-	CUDA_RUNTIME_CHECK_EXIT(lcVpueClose(encInst));
+	CUDA_RUNTIME_CHECK_EXIT(lcVpueClose(inst));
 	return 0;
 }
