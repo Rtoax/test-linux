b9150b37bc2999fe152302be3ae1b578c8e03039
Rong Tao <rongtao@cestc.cn>
2026-01-22 14:36:53 +0800

    cgroup/v1/cpu/chrt.sh: trap cleanup
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU AMD EPYC 7763 64-Core Processor, Arch x86_64, Physical Machine
    Vers: CCLinux 22.09.2, Linux 5.15.131-8.cl9.x86_64, BASH 5.1.8

--- cgroup/v1/cpu/chrt.sh
+++ cgroup/v1/cpu/chrt.sh
@@ -5,6 +5,13 @@ FS=/sys/fs/cgroup/
 
 sudo mkdir ${FS}/cpu/testa/ || true
 
+cleanup() {
+	# Move task to root cpu cgroup
+	echo $$ | sudo tee ${FS}/cpu/tasks
+	sudo rmdir ${FS}/cpu/testa
+}
+trap cleanup EXIT
+
 echo $$ | sudo tee ${FS}/cpu/testa/tasks
 echo 0 | sudo tee ${FS}/cpu/testa/cpu.rt_runtime_us
 
@@ -13,7 +20,3 @@ error=$( sudo chrt -f 99 ls 2>&1 | grep -o 'Operation not permitted' || true )
 if [[ "${error}" != "Operation not permitted" ]]; then
 	echo "ERROR: failed test chrt"
 fi
-
-# Move task to root cpu cgroup
-echo $$ | sudo tee ${FS}/cpu/tasks
-sudo rmdir ${FS}/cpu/testa
