17e37dcafef91b75140537320feea4fb69eeb5bd
Rong Tao <rongtao@cestc.cn>
2026-01-22 11:25:51 +0800

    gpu/rocm.mk: update comments
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, Physical Machine
    Vers: Fedora Linux 43, Linux 6.18.5-200.fc43.x86_64, MAKE 4.4.1, ROCm 6.4.43484, LLVM 21.1.8

--- template/gpu/rocm.mk
+++ template/gpu/rocm.mk
@@ -5,7 +5,7 @@
 # - __IGNORE_NOTFOUND_ERROR__
 #
 # Output macros:
-# - HAVE_HIP=y
+# - HAVE_HIP=[y|n]
 # - HAVE_HIPSOLVER=y
 # - HAVE_HIPBLASLT=y
 # - HAVE_HIP_FP8=y
@@ -18,6 +18,9 @@
 # - target-hipcc-liba-y
 # - target-hipcc-libso-y
 #
+# Functions:
+# - gen_hipcc_macro_hdr()
+#
 ifndef _GPU_ROCM_MK
 _GPU_ROCM_MK = 1
 
@@ -26,10 +29,7 @@ include define.mk
 HIPCC := $(shell which hipcc 2>/dev/null)
 HIPCONFIG := $(shell which hipconfig 2>/dev/null)
 
-ifneq ($(HIPCC),)
-  HAVE_HIP := y
-  export HAVE_HIP
-else
+ifeq ($(HIPCC),)
   ifneq ($(target-hipcc-y)$(target-hipcc-liba-y)$(target-hipcc-libso-y),)
     ifdef __IGNORE_NOTFOUND_ERROR__
       $(warning Not found hipcc with target hipcc not empty, but __IGNORE_NOTFOUND_ERROR__)
@@ -37,19 +37,18 @@ else
       $(error Not found hipcc with target hipcc not empty, install cuda first)
     endif
   else
-    $(warning Although not found hipcc, but you don't have target hipcc)
+    $(warning Although not found hipcc, but you don not have target hipcc)
   endif
 
   HIPCC :=
   HIPCONFIG :=
-endif
+  export HAVE_HIP := n
+else
 
 # $(1) - output file name
 define gen_hipcc_macro_hdr
 $(Q)${HIPCC} -E -dM - </dev/null -o $(1)
 endef
-
-export HIPCC HIPCONFIG
 
 $(call check_file_and_def,/usr/include/rccl/rccl.h,HAVE_RCCL)
 $(call check_file_and_def,/usr/include/hipsolver/hipsolver.h,HAVE_HIPSOLVER)
@@ -60,12 +59,13 @@ ifdef DEBUG
   $(info HAVE_HIP = ${HAVE_HIP})
   $(info HIPCC = ${HIPCC})
   $(info HIPCONFIG = ${HIPCONFIG})
-  ifneq (${HIPCC},)
-    $(info HIPCC: $(shell ${HIPCC} --version))
-  endif
-  ifneq (${HIPCONFIG},)
-    $(info HIPCONFIG: $(shell ${HIPCONFIG} --full))
-  endif
+  $(info HIPCC: $(shell ${HIPCC} --version))
+  $(info HIPCONFIG: $(shell ${HIPCONFIG} --full))
 endif
 
-endif
+export HAVE_HIP := y
+export HIPCC HIPCONFIG
+
+endif # end of HIPCC
+
+endif # end of _GPU_ROCM_MK
