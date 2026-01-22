f74d75b56b5d7b65b9e08db60ba5267fb9010633
Rong Tao <rongtao@cestc.cn>
2026-01-22 18:38:05 +0800

    gdsio.cu: rename --DIR to --dir
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Phytium, Arch aarch64, Physical Machine, Lingspeed X710-M:8
    Vers: CCLinux 22.09.2, Linux 5.15.131-28.cl9_GDS.aarch64, LLVM 14.0.6, CUDA V13.0.48, LUCA 3.2.1, HPCC 3.0.0

--- hpc/nvidia/cuda/file/gdsio.cu
+++ hpc/nvidia/cuda/file/gdsio.cu
@@ -24,7 +24,7 @@
 #include "cuda_compat.h"
 #include "cuda_helpers.h"
 
-#define VERSION "v1.0.2"
+#define VERSION "v1.0.3"
 
 #define __stringify_1(x...)	#x
 #define __stringify(x...)	__stringify_1(x)
@@ -131,14 +131,19 @@ const char argp_prog_doc[] =
 
 static const struct argp_option opts[] = {
 	{ "file", 'f', "FILE", 0, "file name" },
-	{ "DIR", 'D', "DIR", 0, "directory name" },
+	{ "dir", 'D', "DIR", 0, "directory name" },
 	{ "device", 'd', "DEVICE", 0, "gpu index" },
-	{ "fsize", 's', "SIZE", 0, "file size (K|M|G), default: " __stringify(DEFAULT_FSIZE) },
-	{ "iosize", 'i', "IOSIZE", 0, "io_size(K|M|G) <min_size:max_size:step_size>, default: " __stringify(DEFAULT_IOSIZE) },
+	{ "fsize", 's', "SIZE", 0,
+	  "file size (K|M|G), default: " __stringify(DEFAULT_FSIZE) },
+	{ "iosize", 'i', "IOSIZE", 0,
+	  "io_size(K|M|G) <min_size:max_size:step_size>, default: " __stringify(
+		  DEFAULT_IOSIZE) },
 	{ "numa", 'n', "NUMA", 0, "numa node (not yet been implemented)" },
 	{ "nthreads", 'w', "NTHREADS", 0, "number of threads for a job" },
-	{ "xfer_type", 'x', "XFER_TYPE", 0, "transfer type [0(GPU_DIRECT), 1(CPU_ONLY), 2(CPU_GPU), 3(CPU_ASYNC_GPU), 4(CPU_CACHED_GPU), 5(GPU_DIRECT_ASYNC), 6(GPU_BATCH), 7(GPU_BATCH_STREAM)]" },
-	{ "op_type", 'I', "OP_TYPE", 0, "[0(read), 1(write), 2(randread), 3(randwrite)]" },
+	{ "xfer_type", 'x', "XFER_TYPE", 0,
+	  "transfer type [0(GPU_DIRECT), 1(CPU_ONLY), 2(CPU_GPU), 3(CPU_ASYNC_GPU), 4(CPU_CACHED_GPU), 5(GPU_DIRECT_ASYNC), 6(GPU_BATCH), 7(GPU_BATCH_STREAM)]" },
+	{ "op_type", 'I', "OP_TYPE", 0,
+	  "[0(read), 1(write), 2(randread), 3(randwrite)]" },
 	{ "verify", 'V', NULL, 1, "verify IO" },
 	{ "bufregister", 'b', NULL, 1, "skip bufregister" },
 	{ "bufrand", 'R', NULL, 1, "fill io buffer with random data" },
