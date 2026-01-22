7072d776b1481d0aa92983c5da14a0001593748a
Rong Tao <rongtao@cestc.cn>
2026-01-22 15:00:47 +0800

    cgroup.mk: add CGROUP_V2=y/n
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, Physical Machine
    Vers: Fedora Linux 43, Linux 6.18.5-200.fc43.x86_64, MAKE 4.4.1

--- template/cgroup.mk
+++ template/cgroup.mk
@@ -2,8 +2,26 @@
 # Copyright (C) 2026 Rong Tao
 #
 # Output definitions:
+# - CGROUPFS_ROOT=[/sys/fs/cgroup]
+# - CGROUP_V2=[y|n]
 #
 ifndef _CGROUP_MK
 _CGROUP_MK = 1
+
+CGROUPFS_ROOT := /sys/fs/cgroup
+
+ifneq ($(wildcard ${CGROUPFS_ROOT}/cgroup.controllers),)
+  CGROUP_V2 := y
+else
+  CGROUP_V2 := n
+endif
+
+ifdef DEBUG
+  $(info CGROUPFS_ROOT = ${CGROUPFS_ROOT})
+  $(info CGROUP_V2 = ${CGROUP_V2})
+endif
+
+export CGROUPFS_ROOT
+export CGROUP_V2
 
 endif
