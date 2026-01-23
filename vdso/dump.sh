dc725ae52ba24fdb3e57c7002426845257d61ef3
Rong Tao <rtoax@foxmail.com>
2026-01-23 18:43:22 +0800

    vdso/dump.sh: add read perm to linux-vdso.so.1
    [rongtao@5000C-OAM vdso]$ ./dump.sh
    pid 174993 [vdso] 0xfffd36020000-0xfffd36030000, size = 65536
    linux-vdso.so.1
    readelf：错误：Input file 'linux-vdso.so.1' is not readable.
    [rongtao@5000C-OAM vdso]$ ll
    总用量 84
    ....
    -rwx------ 1 root    root    65536  1月 23 18:42 linux-vdso.so.1
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU Phytium, Arch aarch64, Physical Machine
    Vers: Kylin Linux Advanced Server V10, Linux 4.19.90-89.11.v2401.ky10.aarch64, BASH 5.0.18

--- vdso/dump.sh
+++ vdso/dump.sh
@@ -68,7 +68,7 @@ fi
 ${_sudo} dd if=/proc/${pid}/mem of=${vdso_so} \
 		ibs=1 skip=$(printf %ld 0x${addr_range[0]}) \
 		count=${vdso_sz} 2>/dev/null
-${_sudo} chmod +x ${vdso_so}
+${_sudo} chmod +x,a+r ${vdso_so}
 
 echo ${vdso_so}
 
