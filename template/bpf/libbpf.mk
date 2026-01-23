a3f7356f141b399d9b752e7f474659c44f10bbb6
Rong Tao <rtoax@foxmail.com>
2026-01-23 19:00:50 +0800

    bpf/libbpf.mk: add HAVE_LIBBPF=y and apply to ipc/socket/syscall/AF_XDP.c
    [rongtao@5000C-OAM syscall]$ make DEBUG=1 | grep LIBBPF
    HAVE_LIBBPF = n
    LIBBPF_MAJOR_VERSION =
    LIBBPF_MINOR_VERSION =
    AF_XDP.c:12:10: 致命错误：bpf/bpf.h：没有那个文件或目录
     #include <bpf/bpf.h>
              ^~~~~~~~~~~
    编译中断。
    make: *** [targets/exe.mk:13：AF_XDP.o] 错误 1
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU Phytium, Arch aarch64, Physical Machine
    Vers: Kylin Linux Advanced Server V10, Linux 4.19.90-89.11.v2401.ky10.aarch64, glibc 2.28, MAKE 4.3

--- template/bpf/libbpf.mk
+++ template/bpf/libbpf.mk
@@ -2,6 +2,7 @@
 # Copyright (C) 2025-2026 Rong Tao
 #
 # Output definitions:
+# - HAVE_LIBBPF=[y|n]
 # - LIBBPF_MAJOR_VERSION=
 # - LIBBPF_MINOR_VERSION=
 #
@@ -10,29 +11,40 @@ _BPF_LIBBPF_MK = 1
 
 include ldconfig.mk
 
-private-libbpf-paths ?= $(shell ${LDCONFIG} -p | grep libbpf.so | awk '{print $$NF}' || true)
-private-libpf-path ?= $(shell dirname $(private-libbpf-paths) | sort -u || true)
-# Like /usr/lib64/libbpf.so.0.4.0
-private-libbpf-v-path := $(shell realpath ${private-libpf-path}/libbpf.so.[0-9])
+private-libbpf-paths := $(shell ${LDCONFIG} -p | grep libbpf.so | awk '{print $$NF}' || true)
 
-LIBBPF_MAJOR_VERSION := $(shell echo ${private-libbpf-v-path} | awk -F '.' '{print $$3}')
-LIBBPF_MINOR_VERSION := $(shell echo ${private-libbpf-v-path} | awk -F '.' '{print $$4}')
+ifneq (${private-libbpf-paths},)
+  private-libpf-path := $(shell dirname $(private-libbpf-paths) | sort -u || true)
+endif
+
+ifneq (${private-libpf-path},)
+  # Like /usr/lib64/libbpf.so.0.4.0
+  private-libbpf-v-path := $(shell realpath ${private-libpf-path}/libbpf.so.[0-9])
+endif
+
+ifneq (${private-libbpf-v-path},)
+  export LIBBPF_MAJOR_VERSION := $(shell echo ${private-libbpf-v-path} | awk -F '.' '{print $$3}')
+  export LIBBPF_MINOR_VERSION := $(shell echo ${private-libbpf-v-path} | awk -F '.' '{print $$4}')
+
+  ifeq (${LIBBPF_MAJOR_VERSION},)
+    $(error "Could not get libbpf LIBBPF_MAJOR_VERSION")
+  endif
+  ifeq (${LIBBPF_MINOR_VERSION},)
+    $(error "Could not get libbpf LIBBPF_MINOR_VERSION")
+  endif
+
+  export HAVE_LIBBPF := y
+else
+  export HAVE_LIBBPF := n
+endif
 
 ifdef DEBUG
+  $(info HAVE_LIBBPF = ${HAVE_LIBBPF})
   $(info private-libbpf-paths = ${private-libbpf-paths})
   $(info private-libpf-path = ${private-libpf-path})
   $(info private-libbpf-v-path = ${private-libbpf-v-path})
   $(info LIBBPF_MAJOR_VERSION = ${LIBBPF_MAJOR_VERSION})
   $(info LIBBPF_MINOR_VERSION = ${LIBBPF_MINOR_VERSION})
-endif
-
-export LIBBPF_MAJOR_VERSION LIBBPF_MINOR_VERSION
-
-ifeq (${LIBBPF_MAJOR_VERSION},)
-  $(error "Could not get libbpf LIBBPF_MAJOR_VERSION")
-endif
-ifeq (${LIBBPF_MINOR_VERSION},)
-  $(error "Could not get libbpf LIBBPF_MINOR_VERSION")
 endif
 
 endif
