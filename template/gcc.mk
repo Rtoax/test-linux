4cb2d4ce97e47d4a39625316665bc5d8da483f8e
Rong Tao <rtoax@foxmail.com>
2026-01-26 19:01:51 +0800

    gcc.mk: add gcc version compare functions
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU S5000C, Arch aarch64, Physical Machine
    Vers: Kylin Linux Advanced Server V10, Linux 4.19.90-89.11.v2401.ky10.aarch64, GCC 7.3.0, MAKE 4.3

--- template/gcc.mk
+++ template/gcc.mk
@@ -13,6 +13,15 @@
 # - GCC_PATCHLEVEL=
 # - GCC_VERSION_CODE=
 #
+# Functions:
+# - gcc_version_code()
+# - gcc_version_compare()=[y|n]
+# - gcc_gt()=[y|n]
+# - gcc_ge()=[y|n]
+# - gcc_eq()=[y|n]
+# - gcc_le()=[y|n]
+# - gcc_lt()=[y|n]
+#
 ifndef _GCC_MK
 _GCC_MK = 1
 
@@ -32,6 +41,38 @@ include compiler.mk
 
 GCC_VERSION_CODE := $(shell echo "$$(( (${GCC_MAJOR}<<16) + (${GCC_MINOR}<<8) + (${GCC_PATCHLEVEL}>255?255:${GCC_PATCHLEVEL}) ))" )
 
+define gcc_version_code
+$(shell echo "$$(( (${1}<<16) + (${2}<<8) + (${3}>255?255:${3}) ))" )
+endef
+
+# Arguments:
+# $1: [-gt|-ge|-eq|-lt|-le]
+# $2: major
+# $3: minor
+# $4: patchlevel
+define gcc_version_compare
+$(shell if [[ ${GCC_VERSION_CODE} ${1} $(call gcc_version_code,${2},${3},${4}) ]]; then \
+		echo y; \
+	else echo n; \
+	fi)
+endef
+
+define gcc_gt
+$(call gcc_version_compare,-gt,${1},${2},${3})
+endef
+define gcc_ge
+$(call gcc_version_compare,-ge,${1},${2},${3})
+endef
+define gcc_eq
+$(call gcc_version_compare,-eq,${1},${2},${3})
+endef
+define gcc_lt
+$(call gcc_version_compare,-lt,${1},${2},${3})
+endef
+define gcc_le
+$(call gcc_version_compare,-le,${1},${2},${3})
+endef
+
 ifdef DEBUG
   $(info GCC = ${GCC})
   $(info GXX = ${GXX})
@@ -40,8 +81,18 @@ ifdef DEBUG
   $(info GCC_VERSION_CODE = ${GCC_VERSION_CODE})
 endif
 
+# Do some checks
 ifneq (${GCC_VERSION},${GCC_MAJOR}.${GCC_MINOR}.${GCC_PATCHLEVEL})
   $(error Failed to parse GCC version)
+endif
+
+ifneq ($(call gcc_version_code,${GCC_MAJOR},${GCC_MINOR},${GCC_PATCHLEVEL}),${GCC_VERSION_CODE})
+  $(error call gcc_version_code() failed)
+endif
+
+# newest gcc major is 16
+ifeq ($(call gcc_gt,17,0,0), y)
+  $(error call gcc_gt failed.)
 endif
 
 export GCC GXX
