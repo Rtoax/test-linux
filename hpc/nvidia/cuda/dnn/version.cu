8ac5c0ebfe5c351a5514073fae3f561fe29830a5
Rong Tao <rtoax@foxmail.com>
2026-01-26 18:25:02 +0800

    cuda/dnn: luca: fix compile error
    [18:22:27] LSCC     version.cu -> .output/version.luca.o
    version.cu:11:37: error: use of undeclared identifier 'HCDNN_MAJOR'
            printf("hcDNN version %d.%d.%d\n", HCDNN_MAJOR, HCDNN_MINOR, HCDNN_PATCHLEVEL);
                                               ^
    version.cu:11:50: error: use of undeclared identifier 'HCDNN_MINOR'
            printf("hcDNN version %d.%d.%d\n", HCDNN_MAJOR, HCDNN_MINOR, HCDNN_PATCHLEVEL);
                                                            ^
    version.cu:11:63: error: use of undeclared identifier 'HCDNN_PATCHLEVEL'
            printf("hcDNN version %d.%d.%d\n", HCDNN_MAJOR, HCDNN_MINOR, HCDNN_PATCHLEVEL);
                                                                         ^
    3 errors generated when compiling for host.
    make[1]: *** [targets/luca.mk:181: .output/version.luca.o] Error 1
    [18:22:27] ❌ Failed build hpc/nvidia/cuda/dnn
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU S5000C, Arch aarch64, Physical Machine, Lingspeed X710-M:8
    Vers: Kylin Linux Advanced Server V10, Linux 4.19.90-89.11.v2401.ky10.aarch64, LUCA 3.2.1

--- hpc/nvidia/cuda/dnn/version.cu
+++ hpc/nvidia/cuda/dnn/version.cu
@@ -7,8 +7,10 @@
 
 int main(int argc, char *argv[])
 {
-#if defined(__HPCC__) || defined(__LUCA__)
+#if defined(__HPCC__)
 	printf("hcDNN version %d.%d.%d\n", HCDNN_MAJOR, HCDNN_MINOR, HCDNN_PATCHLEVEL);
+#elif defined(__LUCA__)
+	printf("lcDNN version %d.%d.%d\n", LCDNN_MAJOR, LCDNN_MINOR, LCDNN_PATCHLEVEL);
 #endif
 	printf("cuDNN version %d.%d.%d\n", CUDNN_MAJOR, CUDNN_MINOR, CUDNN_PATCHLEVEL);
 	return 0;
