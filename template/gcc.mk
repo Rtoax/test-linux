8d568b406d77d545abcf0c2a90dc8240c6f70765
Rong Tao <rtoax@foxmail.com>
2026-01-26 18:46:53 +0800

    gcc.mk: add version macros
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU S5000C, Arch aarch64, Physical Machine
    Vers: Kylin Linux Advanced Server V10, Linux 4.19.90-89.11.v2401.ky10.aarch64, GCC 7.3.0, MAKE 4.3

--- template/gcc.mk
+++ template/gcc.mk
@@ -4,6 +4,14 @@
 # Input definitions:
 #
 # Output definitions:
+# - GCC=[gcc]
+# - GXX=[g++]
+# - GCC_FULLVERSION=
+# - GCC_VERSION=
+# - GCC_MAJOR=
+# - GCC_MINOR=
+# - GCC_PATCHLEVEL=
+# - GCC_VERSION_CODE=
 #
 ifndef _GCC_MK
 _GCC_MK = 1
@@ -14,11 +22,30 @@ GXX := g++
 CC = ${GCC}
 CXX = ${GXX}
 
+GCC_FULLVERSION := $(shell $(GCC) -dumpfullversion -dumpversion)
+GCC_VERSION := $(shell $(GCC) -dumpversion)
+GCC_MAJOR := $(shell echo ${GCC_FULLVERSION} | awk -F '.' '{print $$1}')
+GCC_MINOR := $(shell echo ${GCC_FULLVERSION} | awk -F '.' '{print $$2}')
+GCC_PATCHLEVEL := $(shell echo ${GCC_FULLVERSION} | awk -F '.' '{print $$3}')
+
 include compiler.mk
+
+GCC_VERSION_CODE := $(shell echo "$$(( (${GCC_MAJOR}<<16) + (${GCC_MINOR}<<8) + (${GCC_PATCHLEVEL}>255?255:${GCC_PATCHLEVEL}) ))" )
 
 ifdef DEBUG
   $(info GCC = ${GCC})
   $(info GXX = ${GXX})
+  $(info GCC_FULLVERSION = ${GCC_FULLVERSION})
+  $(info GCC_VERSION = ${GCC_VERSION} (${GCC_MAJOR}.${GCC_MINOR}.${GCC_PATCHLEVEL}))
+  $(info GCC_VERSION_CODE = ${GCC_VERSION_CODE})
 endif
+
+ifneq (${GCC_VERSION},${GCC_MAJOR}.${GCC_MINOR}.${GCC_PATCHLEVEL})
+  $(error Failed to parse GCC version)
+endif
+
+export GCC GXX
+export GCC_FULLVERSION GCC_VERSION GCC_MAJOR GCC_MINOR GCC_PATCHLEVEL
+export GCC_VERSION_CODE
 
 endif
