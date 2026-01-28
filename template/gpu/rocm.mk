187eac1d1ef75ecc8c4cc7936b5258a8b06915ad
Rong Tao <rtoax@foxmail.com>
2026-01-28 11:20:39 +0800

    gpu/rocm.mk: add rocm-cflags
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU Intel(R) Xeon(R) Platinum 8462Y+, Arch x86_64, Physical Machine
    Vers: CCLinux 22.09.2, Linux 5.15.131-26.cl9.x86_64, MAKE 4.3

--- template/gpu/rocm.mk
+++ template/gpu/rocm.mk
@@ -14,6 +14,7 @@
 # - HIPCONFIG=$(which hipconfig)
 # - HIP_PATH=[/usr]
 # - ROCM_PATH=[/usr]
+# - rocm-cflags=
 #
 # Modify definitions:
 # - target-hipcc-y
@@ -30,6 +31,7 @@ include define.mk
 
 HIPCC := $(shell which hipcc 2>/dev/null)
 HIPCONFIG := $(shell which hipconfig 2>/dev/null)
+rocm-cflags :=
 
 ifeq ($(HIPCC),)
   ifneq ($(target-hipcc-y)$(target-hipcc-liba-y)$(target-hipcc-libso-y),)
@@ -39,7 +41,7 @@ ifeq ($(HIPCC),)
       $(error Not found hipcc with target hipcc not empty, install cuda first)
     endif
   else
-    $(warning Although not found hipcc, but you don not have target hipcc)
+    $(warning Although not found hipcc, but you do not have target hipcc)
   endif
 
   HIPCC :=
@@ -57,6 +59,8 @@ $(call check_file_and_def,/usr/include/hipsolver/hipsolver.h,HAVE_HIPSOLVER)
 $(call check_file_and_def,/usr/include/hipblaslt/hipblaslt.h,HAVE_HIPBLASLT)
 $(call check_file_and_def,/usr/include/hip/hip_fp8.h,HAVE_HIP_FP8)
 
+rocm-cflags += -DHAVE_HIP=1
+
 HIP_PATH := $(shell ${HIPCONFIG} -p)
 ROCM_PATH := $(shell ${HIPCONFIG} -R)
 
@@ -66,12 +70,14 @@ ifdef DEBUG
   $(info HIPCC: $(shell ${HIPCC} --version))
   $(info HIPCONFIG = ${HIPCONFIG})
   $(info HIPCONFIG : $(shell ${HIPCONFIG} --full))
+  $(info rocm-cflags = ${rocm-cflags})
 endif
 
 export HAVE_HIP := y
 export HIPCC HIPCONFIG
 export HIP_PATH
 export ROCM_PATH
+export rocm-cflags
 
 endif # end of HIPCC
 
