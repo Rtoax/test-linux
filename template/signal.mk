f7e7370ff0fa4eba1343cabc6603fc666a53d6a1
Rong Tao <rongtao@cestc.cn>
2026-01-29 18:57:22 +0800

    signal.mk: Rename SIGRT_CFLAGS to sig-cflags
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, Physical Machine
    Vers: Fedora Linux 43, Linux 6.18.6-200.fc43.x86_64, Make 4.4.1

--- template/signal.mk
+++ template/signal.mk
@@ -1,4 +1,8 @@
 # SPDX-License-Identifier: GPL-3.0
+#
+# Output definitions:
+# - sig-cflags=
+#
 ifndef _SIGNAL_MK
 _SIGNAL_MK = 1
 
@@ -10,11 +14,11 @@ SIGRT_PROBE += 'int main(void) {\n'
 SIGRT_PROBE += '	printf("-DCONST_SIGRTMIN=%d -DCONST_SIGRTMAX=%d\\n", SIGRTMIN, SIGRTMAX);\n'
 SIGRT_PROBE += '}'
 
-SIGRT_CFLAGS := $(shell printf '%b\n' $(SIGRT_PROBE) | \
+sig-cflags := $(shell printf '%b\n' $(SIGRT_PROBE) | \
 	$(CC) -xc -Wall - -o sig.out 2>&1 && ./sig.out && rm -f sig.out)
 
 ifdef DEBUG
-  $(info SIGRT_CFLAGS = ${SIGRT_CFLAGS})
+  $(info sig-cflags = ${sig-cflags})
 endif
 
 endif
