7fd8e5e39b1c82e458838f7b05680b175ed102c6
Rong Tao <rongtao@cestc.cn>
2026-01-29 09:42:19 +0800

    string/replace.cmake: test REGEX REPLACE
    for pytorch/cmake/Modules_CUDA_fix/upstream/FindCUDA/select_compute_arch.cmake
    
        string(REPLACE "2.1" "2.1(2.0)" compute_capabilities "${compute_capabilities}")
    
    which is wrong for 12.1 compute capabilities on nvidia dgx spark.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, Physical Machine
    Vers: Fedora Linux 43, Linux 6.18.6-200.fc43.x86_64, CMake 3.31.10

--- tools/cmake/samples/string/replace.cmake
+++ tools/cmake/samples/string/replace.cmake
@@ -1,3 +1,10 @@
 set(versions "12.1 2.1 2.10 abc2.1def 2.1")
-string(REPLACE "2.1" "2.1(2.0)" versions "${versions}")
+set(versions1)
+set(versions2)
+
+string(REPLACE "2.1" "2.1(2.0)" versions1 "${versions}")
+string(REGEX REPLACE "(^|[^0-9])2\\.1([^0-9]|$)" "\\12.1(2.0)\\2" versions2 "${versions}")
+
 message(STATUS "versions: ${versions}")
+message(STATUS "versions1: ${versions1}")
+message(STATUS "versions2: ${versions2}")
