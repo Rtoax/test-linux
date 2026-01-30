11e76e5f336e6b2db10489a9d5aeda9b8dd4b4cf
Rong Tao <rongtao@cestc.cn>
2026-01-30 09:28:07 +0800

    shell.mk: add comment
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/shell.mk
+++ template/shell.mk
@@ -1,5 +1,10 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (C) 2025-2026 Rong Tao
+#
+# Output definitions:
+# - SHELL=[bash]
+# - .SHELLFLAGS=
+#
 ifndef _SHELL_MK
 _SHELL_MK = 1
 
@@ -11,5 +16,7 @@ endif
 ifdef DEBUG
   $(info SHELL = ${SHELL})
 endif
+
+export SHELL
 
 endif
