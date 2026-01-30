564e13c53f8a58ee7f710e688409aef8a4cd060e
Rong Tao <rongtao@cestc.cn>
2026-01-30 09:22:27 +0800

    template: move CFLAGS_PIE and LDFLAGS_PIE from main.mk to pie.mk
    Link: commit 0a84a726e207 ("main.mk: add PIE cflags/ldflags from pie.mk")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, Physical Machine
    Vers: Fedora Linux 43, Linux 6.18.7-200.fc43.x86_64, Make 4.4.1

--- template/main.mk
+++ template/main.mk
@@ -67,28 +67,7 @@ ifdef STATIC
   MAKEFLAGS += STATIC=1
 endif
 
-CFLAGS_PIE :=
-LDFLAGS_PIE :=
-# Debian 12 default compile PIE EXEC, Fedora40 default compile no-PIE EXEC.
-# Default no-PIE in my case.
-ifdef PIE
-  # clang: not support -pie in cflags
-  ifeq ($(CC),gcc)
-    CFLAGS_PIE += -pie
-  endif
-  CFLAGS_PIE += -fPIE
-  LDFLAGS_PIE += -pie -fPIE
-  MAKEFLAGS += PIE=1
-endif
-
-ifdef NOPIE
-  CFLAGS_PIE += -no-pie
-  LDFLAGS_PIE += -no-pie
-  MAKEFLAGS += NOPIE=1
-  ifdef PIE
-    $(error "Could no specify PIE and NOPIE at the same time")
-  endif
-endif
+include pie.mk
 CFLAGS += ${CFLAGS_PIE}
 LDFLAGS += ${LDFLAGS_PIE}
 
@@ -100,7 +79,6 @@ include arch.mk
 include dir.mk
 include log.mk
 
-include pie.mk
 ifneq ($(subdir-y),)
   include targets/subdir-header.mk
 endif
@@ -154,7 +132,7 @@ ifdef DEBUG
   $(info build-targets = ${build-targets})
 endif
 
-export CFLAGS LDFLAGS CXXFLAGS LDXXFLAGS CFLAGS_PIE LDFLAGS_PIE MAKEFLAGS
+export CFLAGS LDFLAGS CXXFLAGS LDXXFLAGS MAKEFLAGS
 
 .PHONY: build
 build: $(build-targets)
--- template/pie.mk
+++ template/pie.mk
@@ -1,7 +1,49 @@
 # SPDX-License-Identifier: GPL-3.0
+# Copyright (C) 2025-2026 Rong Tao
+#
+# Input definitions:
+# - PIE=1
+# - NOPIE=1
+#
+# Output definitions:
+# - CFLAGS_PIE=
+# - LDFLAGS_PIE=
+#
+# Append definitions:
+# - MAKEFLAGS+=
+#
 ifndef _PIE_MK
 _PIE_MK = 1
 
-# set PIE cflags/ldflags in main.mk
+CFLAGS_PIE :=
+LDFLAGS_PIE :=
+
+# Debian 12 default compile PIE EXEC, Fedora40 default compile no-PIE EXEC.
+# Default no-PIE in my case.
+ifdef PIE
+  # clang: not support -pie in cflags
+  ifeq ($(CC),gcc)
+    CFLAGS_PIE += -pie
+  endif
+  CFLAGS_PIE += -fPIE
+  LDFLAGS_PIE += -pie -fPIE
+  MAKEFLAGS += PIE=1
+endif
+
+ifdef NOPIE
+  CFLAGS_PIE += -no-pie
+  LDFLAGS_PIE += -no-pie
+  MAKEFLAGS += NOPIE=1
+  ifdef PIE
+    $(error "Could no specify PIE and NOPIE at the same time")
+  endif
+endif
+
+ifdef DEBUG
+  $(info CFLAGS_PIE = ${CFLAGS_PIE})
+  $(info LDFLAGS_PIE = ${LDFLAGS_PIE})
+endif
+
+export CFLAGS_PIE LDFLAGS_PIE
 
 endif
