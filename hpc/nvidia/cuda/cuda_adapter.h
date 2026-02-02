8d2ded1bd0664f0547572bcee9cc81fc388d6205
Rong Tao <rongtao@cestc.cn>
2026-02-02 16:35:38 +0800

    cuda/adapter: add cufile.h
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Cortex-X925 Cortex-A725, Arch aarch64, Physical Machine, NVIDIA GB10
    Vers: Ubuntu 24.04, Linux 6.14.0-1015-nvidia, GCC 13.3.0, glibc 2.39, LLVM 18.1.3, CUDA V13.0.88

--- hpc/nvidia/cuda/cuda_adapter.h
+++ hpc/nvidia/cuda/cuda_adapter.h
@@ -2003,69 +2003,7 @@
 \******************************************************************************/
 // TODO
 
-/******************************************************************************\
- * GPUDirect Storage, cuFile                                                  *
-\******************************************************************************/
-#define CUfileHandle_t	__CU(fileHandle_t)
-#define CUfileFileHandleType	__CU(fileFileHandleType)
-/* Linux based fd */
-#define CU_FILE_HANDLE_TYPE_OPAQUE_FD	__CU(_FILE_HANDLE_TYPE_OPAQUE_FD)
-/* Windows based handle (unsupported) */
-#define CU_FILE_HANDLE_TYPE_OPAQUE_WIN32	__CU(_FILE_HANDLE_TYPE_OPAQUE_WIN32)
-/* Userspace based FS */
-#define CU_FILE_HANDLE_TYPE_USERSPACE_FS	__CU(_FILE_HANDLE_TYPE_USERSPACE_FS)
-
-#define CUfileDescr_t	__CU(fileDescr_t)
-#define CUfileError_t	__CU(fileError_t)
-#define CU_FILE_SUCCESS	__CU(_FILE_SUCCESS)
-
-#define CUfileOpError	__CU(fileOpError)
-/**
- * static inline const char *cufileop_status_error(CUfileOpError status)
- */
-#define cufileop_status_error	__cu(fileop_status_error)
-
-/**
- * CUfileError_t cuFileDriverOpen(void);
- * LCfileError_t lcFileDriverOpen(void);
- */
-#define cuFileDriverOpen	__cu(FileDriverOpen)
-
-/**
- * LUCA 3.2.1
- * LCfileError_t lcFileHandleRegister(LCfileHandle_t *fh, LCfileDescr_t *descr);
- */
-#define cuFileHandleRegister	__cu(FileHandleRegister)
-#define cuFileHandleDeregister	__cu(FileHandleDeregister)
-
-/**
- * LCfileError_t lcFileBufRegister(const void *devPtr_base, size_t length, int flags);
- */
-#define cuFileBufRegister	__cu(FileBufRegister)
-/**
- * LCfileError_t lcFileBufDeregister(const void *devPtr_base);
- */
-#define cuFileBufDeregister	__cu(FileBufDeregister)
-
-/**
- * ssize_t cuFileWrite(CUfileHandle_t fh, const void *bufPtr_base, size_t size,
- *                     off_t file_offset, off_t bufPtr_offset);
- */
-#define cuFileWrite	__cu(FileWrite)
-/**
- * ssize_t cuFileRead(CUfileHandle_t fh, void *bufPtr_base, size_t size,
- *                    off_t file_offset, off_t bufPtr_offset);
- */
-#define cuFileRead	__cu(FileRead)
-
-/**
- * CUDA/HPCC/LUCA all have this macro
- * #define CUFILEOP_STATUS_ENTRIES \
- *     CUFILE_OP(0, LC_FILE_SUCCESS, lcfile success) \
- *     ....
- */
-#define CUFILEOP_STATUS_ENTRIES	__CU(FILEOP_STATUS_ENTRIES)
-
+#include "adapter/cufile.h"
 #include "adapter/cupti.h"
 
 #endif
