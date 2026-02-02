12703ecf810d82c9176b334e8a8033ad7387dc6c
Rong Tao <rtoax@foxmail.com>
2026-02-02 14:45:54 +0800

    luca: add cu-bridge-envs.sh
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU Hygon C86-4G (OPN:7470), Arch x86_64, Physical Machine, Lingspeed X710-M:8
    Vers: CCLinux 22.09.2, Linux 5.15.131-28.cl9.x86_64, BASH 5.1.8, LUCA 3.2.1, LLVM 14.0.6

---
+++ hpc/cestc/luca/cu-bridge-envs.sh
@@ -0,0 +1,6 @@
+export NV_TOOLS=/usr/local/cuda/
+export LUCA_PATH=/opt/luca
+export LUCA_CLANG_PATH=/opt/luca/htgpu_llvm/bin
+export CUCC_PATH=/opt/luca/tools/cu-bridge
+export PATH=$LUCA_CLANG_PATH:$NV_TOOLS/bin:$CUCC_PATH/tools/:$LUCA_PATH/ompi/bin:$PATH
+export LD_LIBRARY_PATH=$LUCA_PATH/ompi/lib:/opt/luca/lib:/opt/luca/htgpu_llvm/lib:$NV_TOOLS/lib64:$NV_TOOLS/lib64/stubs:$LD_LIBRARY_PATH
