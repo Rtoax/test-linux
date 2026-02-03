1713a069b85877c6bccb9091b156fd2ab467a424
Rong Tao <rongtao@cestc.cn>
2026-02-03 17:56:02 +0800

    llvm.mk: update
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, Physical Machine
    Vers: Fedora Linux 43, Linux 6.18.7-200.fc43.x86_64, Make 4.4.1, LLVM 21.1.8

--- template/llvm.mk
+++ template/llvm.mk
@@ -4,9 +4,9 @@
 # Output definitions:
 # - WHEREIS_LLVM=[/usr/include/llvm]
 # - HAVE_LLVM=[y|n]
-# - LLVM_CONFIG=
-# - LLVM_AS=
-# - LLVM_DIS=
+# - LLVM_CONFIG=[/usr/bin/llvm-config]
+# - LLVM_AS=[/usr/bin/llvm-as]
+# - LLVM_DIS=[/usr/bin/llvm-dis]
 # - LLVM_SRC_ROOT=
 # - CLANG_SRC_ROOT=
 # - llvm-cflags=
@@ -27,46 +27,47 @@ WHEREIS_LLVM := $(shell whereis llvm | awk '{print $$2}')
 ifeq (${WHEREIS_LLVM},)
   $(warning "Not found llvm, maybe you should install llvm first")
   export HAVE_LLVM := n
-else
+else # Found llvm
 
-  LLVM_CONFIG := $(shell which llvm-config 2>/dev/null)
-  LLVM_AS := $(shell which llvm-as 2>/dev/null)
-  LLVM_DIS := $(shell which llvm-dis 2>/dev/null)
-  LLVM_OBJDUMP := $(shell which llvm-objdump 2>/dev/null)
-  LLC := $(shell which llc 2>/dev/null)
+LLVM_CONFIG := $(shell which llvm-config 2>/dev/null)
+LLVM_AS := $(shell which llvm-as 2>/dev/null)
+LLVM_DIS := $(shell which llvm-dis 2>/dev/null)
+LLVM_OBJDUMP := $(shell which llvm-objdump 2>/dev/null)
+LLC := $(shell which llc 2>/dev/null)
 
-  # Note: Yep, i always store under this path
-  LLVM_SRC_ROOT := $(HOME)/Git/llvm/
-  CLANG_SRC_ROOT := ${LLVM_SRC_ROOT}/clang/
+# Note: Yep, i always store under this path
+LLVM_SRC_ROOT := $(HOME)/Git/llvm/
+CLANG_SRC_ROOT := ${LLVM_SRC_ROOT}/clang/
 
-  llvm-cflags := $(shell ${LLVM_CONFIG} --cflags)
-  llvm-cxxflags := $(shell ${LLVM_CONFIG} --cxxflags)
-  llvm-ldflags := $(shell ${LLVM_CONFIG} --ldflags)
+llvm-cflags := $(shell ${LLVM_CONFIG} --cflags)
+llvm-cxxflags := $(shell ${LLVM_CONFIG} --cxxflags)
+llvm-ldflags := $(shell ${LLVM_CONFIG} --ldflags)
 
-  # $1 - target name
-  define llvm_support_target
-  $(shell if [[ $$(${CLANG} -print-targets | grep -ow $1) == $1 ]]; then echo y; fi)
-  endef
+# $1 - target name
+define llvm_support_target
+$(shell if [[ $$(${CLANG} -print-targets | grep -ow $1) == $1 ]]; then echo y; fi)
+endef
 
-  ifdef DEBUG
-    $(info LLVM_CONFIG = ${LLVM_CONFIG})
-    $(info LLVM_AS = ${LLVM_AS})
-    $(info LLVM_DIS = ${LLVM_DIS})
-    $(info LLC = ${LLC})
-    $(info LLVM_SRC_ROOT = ${LLVM_SRC_ROOT})
-    $(info CLANG_SRC_ROOT = ${CLANG_SRC_ROOT})
-    $(info llvm-cflags = ${llvm-cflags})
-    $(info llvm-cxxflags = ${llvm-cxxflags})
-    $(info llvm-ldflags = ${llvm-ldflags})
-    $(info LLVM support NON_EXIST = $(call llvm_support_target,NON_EXIST))
-    $(info LLVM support NVPTX = $(call llvm_support_target,nvptx))
-    $(info LLVM support NVPTX64 = $(call llvm_support_target,nvptx64))
-    $(info LLVM support AMDGCN = $(call llvm_support_target,amdgcn))
-  endif
+ifdef DEBUG
+  $(info LLVM_CONFIG = ${LLVM_CONFIG})
+  $(info LLVM_AS = ${LLVM_AS})
+  $(info LLVM_DIS = ${LLVM_DIS})
+  $(info LLC = ${LLC})
+  $(info LLVM_SRC_ROOT = ${LLVM_SRC_ROOT})
+  $(info CLANG_SRC_ROOT = ${CLANG_SRC_ROOT})
+  $(info llvm-cflags = ${llvm-cflags})
+  $(info llvm-cxxflags = ${llvm-cxxflags})
+  $(info llvm-ldflags = ${llvm-ldflags})
+  $(info LLVM support NON_EXIST = $(call llvm_support_target,NON_EXIST))
+  $(info LLVM support NVPTX = $(call llvm_support_target,nvptx))
+  $(info LLVM support NVPTX64 = $(call llvm_support_target,nvptx64))
+  $(info LLVM support AMDGCN = $(call llvm_support_target,amdgcn))
+endif # end of DEBUG
 
-  export HAVE_LLVM := y
-  export LLVM_CONFIG LLVM_AS LLVM_DIS LLC
-  export LLVM_SRC_ROOT CLANG_SRC_ROOT
+export HAVE_LLVM := y
+export LLVM_CONFIG LLVM_AS LLVM_DIS LLC
+export LLVM_SRC_ROOT CLANG_SRC_ROOT
+
 endif # end of found WHEREIS_LLVM
 
 endif # end of _LLVM_MK
