9bf171f75665d5059293a2fe59cf321766a9967f
Rong Tao <rongtao@cestc.cn>
2026-02-03 17:47:28 +0800

    rdma.mk: check and define HAVE_INFINIBAND_IB_H and HAVE_RDMA_RSOCKET_H
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, Physical Machine
    Vers: Fedora Linux 43, Linux 6.18.7-200.fc43.x86_64, Make 4.4.1

--- template/rdma.mk
+++ template/rdma.mk
@@ -8,4 +8,9 @@
 ifndef _RDMA_MK
 _RDMA_MK = 1
 
+include define.mk
+
+$(call check_file_and_def,/usr/include/infiniband/ib.h,HAVE_INFINIBAND_IB_H)
+$(call check_file_and_def,/usr/include/rdma/rsocket.h,HAVE_RDMA_RSOCKET_H)
+
 endif
