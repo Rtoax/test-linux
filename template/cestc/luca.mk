6e287a3d60bac40a53af00627a3caa5bf501fe2b
Rong Tao <rongtao@cestc.cn>
2026-02-05 14:54:44 +0800

    cestc/luca.mk: update code style
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Cortex-X925 Cortex-A725, Arch aarch64, Physical Machine
    Vers: Ubuntu 24.04, Linux 6.14.0-1015-nvidia, Make 4.3, LLVM 18.1.3

--- template/cestc/luca.mk
+++ template/cestc/luca.mk
@@ -28,13 +28,33 @@ LUCA_ROOT := /opt/luca
 LUCA_ROOT := $(shell realpath ${LUCA_ROOT} 2>/dev/null || true)
 LUCA_CU_BRIDGE := ${LUCA_ROOT}/tools/cu-bridge/include/
 LUCA_LLVM := ${LUCA_ROOT}/htgpu_llvm/
+
+LUCA_VERSION_MAJOR :=
+LUCA_VERSION_MINOR :=
+LUCA_VERSION_PATCH :=
+
+ifeq ($(wildcard ${LUCA_ROOT}),)
+  $(warning Not found LUCA_ROOT="${LUCA_ROOT}")
+else # Found LUCA
+
+export HAVE_LUCA := y
+
+GREP := grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
+LUCA_VERSION_RAW := $(shell echo ${LUCA_ROOT} | ${GREP} | head -1)
+LUCA_VERSION_MAJOR := $(shell echo ${LUCA_VERSION_RAW} | awk -F '.' '{print $$1}')
+LUCA_VERSION_MINOR := $(shell echo ${LUCA_VERSION_RAW} | awk -F '.' '{print $$2}')
+LUCA_VERSION_PATCH := $(shell echo ${LUCA_VERSION_RAW} | awk -F '.' '{print $$3}')
+
 # Note: During the LUCA development process, the path and file name changed.
 ifeq ($(wildcard ${LUCA_LLVM}),)
   LUCA_LLVM := ${LUCA_ROOT}/lsgpu_llvm/
   export LUCA_PHASE_II_PROJECT := y
 endif
-LSCC := ${LUCA_LLVM}/bin/htcc
-ifeq ($(wildcard ${LSCC}),)
+
+HTCC := ${LUCA_LLVM}/bin/htcc
+ifneq ($(wildcard ${HTCC}),)
+  LSCC := ${HTCC}
+else
   LSCC := ${LUCA_LLVM}/bin/lscc
 endif
 
@@ -45,27 +65,6 @@ ifeq ($(wildcard ${LSCC}),)
     else
       $(error Not found lscc with target lscc not empty, install LUCA first)
     endif
-  endif
-endif
-
-ifeq ($(wildcard ${LUCA_ROOT}),)
-  $(warning Not found LUCA_ROOT="${LUCA_ROOT}")
-  LUCA_VERSION_MAJOR :=
-  LUCA_VERSION_MINOR :=
-  LUCA_VERSION_PATCH :=
-else
-  GREP := grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
-  LUCA_VERSION_RAW := $(shell echo ${LUCA_ROOT} | ${GREP} | head -1)
-  LUCA_VERSION_MAJOR := $(shell echo ${LUCA_VERSION_RAW} | awk -F '.' '{print $$1}')
-  LUCA_VERSION_MINOR := $(shell echo ${LUCA_VERSION_RAW} | awk -F '.' '{print $$2}')
-  LUCA_VERSION_PATCH := $(shell echo ${LUCA_VERSION_RAW} | awk -F '.' '{print $$3}')
-
-  export HAVE_LUCA := y
-endif
-
-ifneq (${LUCA_ROOT},)
-  ifeq (${LSCC},)
-    $(error Found ${LUCA_ROOT}, but not found lscc, do you set ENV correctly???)
   endif
 endif
 
@@ -85,6 +84,8 @@ export LUCA_ROOT
 export LSCC
 export LUCA_VERSION_MAJOR LUCA_VERSION_MINOR LUCA_VERSION_PATCH
 
+endif # End of found LUCA
+
 ifdef DEBUG
   $(info LUCA_ROOT = ${LUCA_ROOT})
   $(info LSCC = ${LSCC})
@@ -95,4 +96,4 @@ ifdef DEBUG
   $(info LUCA_VERSION_PATCH = ${LUCA_VERSION_PATCH})
 endif
 
-endif
+endif # End of _CESTC_LUCA_MK
