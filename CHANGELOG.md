34894f5bc2ada2ee70fdfcd473328e8f734438f6
Rong Tao <rongtao@cestc.cn>
2026-02-05 12:38:02 +0800

    podman: add privileged/reboot.sh from ostools
    ostools commits:
    
    commit 9e36979160fc ("container: shutdown --poweroff couldn't poweroff host machine")
    commit e93d3c19fd69 ("container: privileged could not reboot/shutdown host machine")
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, Physical Machine
    Vers: Fedora Linux 43, Linux 6.18.7-200.fc43.x86_64, BASH 5.3.0, Podman 5.7.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to
 
 #### Breaking Changes
 #### Added
+- podman: add privileged/reboot.sh;
 - tools: add cri/crictl.sh
 - tools: add containerd/containerd.sh;
 - template: add rdma.mk;
---
+++ tools/podman/privileged/reboot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ex
+
+IMAGE=fedora:latest
+NAME=fedora
+
+sudo podman rm -f ${NAME} || true
+
+sudo podman run --rm -d --privileged --name ${NAME} ${IMAGE} /sbin/init
+sudo podman exec ${NAME} ps -ef
+
+sudo podman ps --all
+
+# - reboot
+# - init 6
+# - shutdown -r now
+# - shutdown --poweroff now
+sudo podman exec ${NAME} shutdown --poweroff now
+
+sudo podman ps --all
