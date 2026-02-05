738514c0653203d3f94bd68e5e11118f77ae7331
Rong Tao <rongtao@cestc.cn>
2026-02-05 15:16:32 +0800

    metax/hpcc.mk: update format
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/metax/hpcc.mk
+++ template/metax/hpcc.mk
@@ -1,23 +1,15 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (C) 2025-2026 Rong Tao
 #
-# Input definitions:
-# - __IGNORE_NOTFOUND_ERROR__
-#
 # Output definitions:
-# - HAVE_HPCC=y
-# - HAVE_HCDNN=y
+# - HAVE_HPCC=[y]
+# - HAVE_HCDNN=[y]
 # - HPCC_ROOT=/opt/hpcc/
 # - HTCC=
 # - HPCC_VERSION_MAJOR=
 # - HPCC_VERSION_MINOR=
 # - HPCC_VERSION_PATCH=
 #
-# Modify definitions:
-# - target-htcc-y=
-# - target-htcc-liba-y=
-# - target-htcc-libso-y=
-
 ifndef _METAX_HPCC_MK
 _METAX_HPCC_MK = 1
 
@@ -29,53 +21,34 @@ HPCC_CU_BRIDGE := ${HPCC_ROOT}/tools/cu-bridge/include/
 HPCC_LLVM := ${HPCC_ROOT}/htgpu_llvm/
 HTCC := ${HPCC_LLVM}/bin/htcc
 
-ifeq ($(wildcard $(HTCC)),)
-  ifneq ($(target-htcc-y)$(target-htcc-liba-y)$(target-htcc-libso-y),)
-    ifdef __IGNORE_NOTFOUND_ERROR__
-      $(warning Not found htcc with target htcc not empty, but __IGNORE_NOTFOUND_ERROR__)
-      target-htcc-y :=
-      target-htcc-liba-y :=
-      target-htcc-libso-y :=
-    else
-      $(error Not found htcc with target htcc not empty, install hpcc first)
-    endif
-  endif
-endif
+HPCC_VERSION_MAJOR :=
+HPCC_VERSION_MINOR :=
+HPCC_VERSION_PATCH :=
 
-ifneq ($(wildcard ${HPCC_ROOT}),)
-  GREP := grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
-  HPCC_VERSION_RAW := $(shell echo ${HPCC_ROOT} | ${GREP} | head -1)
-  HPCC_VERSION_MAJOR := $(shell echo ${HPCC_VERSION_RAW} | awk -F '.' '{print $$1}')
-  HPCC_VERSION_MINOR := $(shell echo ${HPCC_VERSION_RAW} | awk -F '.' '{print $$2}')
-  HPCC_VERSION_PATCH := $(shell echo ${HPCC_VERSION_RAW} | awk -F '.' '{print $$3}')
+ifeq ($(wildcard ${HPCC_ROOT}),)
+  $(warning Not found HPCC_ROOT="${HPCC_ROOT}", please include HPCC SDK!)
+else # Found HPCC
+GREP := grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
+HPCC_VERSION_RAW := $(shell echo ${HPCC_ROOT} | ${GREP} | head -1)
+HPCC_VERSION_MAJOR := $(shell echo ${HPCC_VERSION_RAW} | awk -F '.' '{print $$1}')
+HPCC_VERSION_MINOR := $(shell echo ${HPCC_VERSION_RAW} | awk -F '.' '{print $$2}')
+HPCC_VERSION_PATCH := $(shell echo ${HPCC_VERSION_RAW} | awk -F '.' '{print $$3}')
 
-  HAVE_HPCC := y
-  export HAVE_HPCC
-else
-  $(warning Not found HPCC_ROOT="${HPCC_ROOT}")
-  HPCC_VERSION_MAJOR :=
-  HPCC_VERSION_MINOR :=
-  HPCC_VERSION_PATCH :=
-endif
-
-ifneq (${HPCC_ROOT},)
-  ifeq (${HTCC},)
-    $(error Found ${HPCC_ROOT}, but not found htcc, do you set ENV correctly???)
-  endif
-endif
+export HAVE_HPCC := y
 
 $(call check_file_and_def,${HPCC_ROOT}/include/hcdnn/hcdnn.h,HAVE_HCDNN)
 
-export HPCC_ROOT HTCC
+export HPCC_ROOT
+export HPCC_LLVM
+export HTCC
 export HPCC_VERSION_MAJOR HPCC_VERSION_MINOR HPCC_VERSION_PATCH
+
+endif # End of found HPCC
 
 ifdef DEBUG
   $(info HAVE_HPCC = ${HAVE_HPCC})
   $(info HPCC_ROOT = ${HPCC_ROOT})
   $(info HTCC = ${HTCC})
-  ifneq ($(HTCC),)
-    $(info $(shell ${HTCC} --version))
-  endif
   $(info HPCC_VERSION_RAW = ${HPCC_VERSION_RAW})
   $(info HPCC_VERSION_MAJOR = ${HPCC_VERSION_MAJOR})
   $(info HPCC_VERSION_MINOR = ${HPCC_VERSION_MINOR})
