6e41f2c634a0d89476f2cfa51828c5dd8d3433e4
Rong Tao <rongtao@cestc.cn>
2026-02-06 11:04:39 +0800

    distro: README.md.check.sh: check exist line
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, Physical Machine
    Vers: Fedora Linux 43, Linux 6.18.8-200.fc43.x86_64, BASH 5.3.0

--- distro/README.md.check.sh
+++ distro/README.md.check.sh
@@ -12,6 +12,11 @@ readonly README=${THISPATH}/README.md
 grep -E '^\* [0-9]+\.[0-9]+\.[0-9]+ \([^)]+\)$' ${README}
 
 if [[ -z "$(grep -E "^\* ${kver_short} \(.*${os_short}.*\)$" ${README})" ]]; then
-	echo >&2 "ERROR: Missing '* ${kver_short} (${os_short})' in ${README}"
+	oldline=$(grep -E "^\* ${kver_short} \([^)]+\)$" ${README})
+	if [[ ! -z ${oldline} ]]; then
+		echo >&2 "ERROR: Missing '${os_short}' in '${oldline}' in ${README}"
+	else
+		echo >&2 "ERROR: Missing '* ${kver_short} (${os_short})' in ${README}"
+	fi
 	exit 1
 fi
