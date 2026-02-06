d490eea6af55d01e02f226278c484b4c7561a9bf
Rong Tao <rtoax@foxmail.com>
2026-02-06 16:05:26 +0800

    cu-bridge/metax2lingspeed.sh: sed more
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: CPU Hygon C86-4G (OPN:7470), Arch x86_64, Physical Machine, Lingspeed Lingspeed X710Mx8
    Vers: CCLinux 22.09.2, Linux 5.15.131-29.cl9.x86_64, BASH 5.1.8, LLVM 14.0.6, CUDA V13.0.88, LUCA 3.3.0

--- hpc/nvidia/cuda/cu-bridge/metax2lingspeed.sh
+++ hpc/nvidia/cuda/cu-bridge/metax2lingspeed.sh
@@ -14,6 +14,8 @@ do
 		continue
 	fi
 
+	echo "Handling $f"
+
 	sed -i 's/MACA/LUCA/g' "$f"
 	sed -i 's/maca/luca/g' "$f"
 
@@ -22,7 +24,16 @@ do
 	sed -i 's/mxSml/lsSml/g' "$f"
 	sed -i 's/mxsml/lssml/g' "$f"
 
-	sed -i 's/mcrand/lcrand/g' "$f"
+	# Folders like /opt/luca/include/lcblas/
+	for key in blas c ctrl cub dnn fft file flashinfer image jpeg kl log \
+		pti rand solver solverIt sparse tlass tlassEx tx
+	do
+		sed -i "s|mc${key}|lc${key}|g" "$f"
+	done
+
 	sed -i 's/mcrtc/lcrtc/g' "$f"
 	sed -i 's/mcruntime/lcruntime/g' "$f"
+
+	# /opt/luca/include/mcr/
+	sed -i 's/\bmcr\b/lcr/g' "$f"
 done
