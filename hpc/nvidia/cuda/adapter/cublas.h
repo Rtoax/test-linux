397a1c26ab4ac8ba36fec99b7ef35b8c86294c7f
Rong Tao <rongtao@cestc.cn>
2026-02-11 17:11:04 +0800

    cuda_adapter: add cublas_api.h
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Phytium, Arch aarch64, Physical Machine, Lingspeed Lingspeed X710Mx8
    Vers: CCLinux 22.09.2, Linux 5.15.131-28.cl9_GDS.aarch64, GCC 11.3.1, glibc 2.34, LLVM 14.0.6, CUDA V13.0.48, LUCA 3.2.1, HPCC 3.0.0

--- hpc/nvidia/cuda/adapter/cublas.h
+++ hpc/nvidia/cuda/adapter/cublas.h
@@ -47,41 +47,11 @@
 #else
 #define cublasGetStatusString(status) __cu(blasGetStatusString(status))
 #endif
-#define cublasStatus_t __cu(blasStatus_t)
-#define CUBLAS_STATUS_SUCCESS __CU(BLAS_STATUS_SUCCESS)
-#define CUBLAS_STATUS_NOT_INITIALIZED __CU(BLAS_STATUS_NOT_INITIALIZED)
-#define CUBLAS_STATUS_ALLOC_FAILED __CU(BLAS_STATUS_ALLOC_FAILED)
-#define CUBLAS_STATUS_INVALID_VALUE __CU(BLAS_STATUS_INVALID_VALUE)
-#define CUBLAS_STATUS_ARCH_MISMATCH __CU(BLAS_STATUS_ARCH_MISMATCH)
-#define CUBLAS_STATUS_MAPPING_ERROR __CU(BLAS_STATUS_MAPPING_ERROR)
-#define CUBLAS_STATUS_EXECUTION_FAILED __CU(BLAS_STATUS_EXECUTION_FAILED)
-#define CUBLAS_STATUS_INTERNAL_ERROR __CU(BLAS_STATUS_INTERNAL_ERROR)
-#define CUBLAS_STATUS_NOT_SUPPORTED __CU(BLAS_STATUS_NOT_SUPPORTED)
 
-#define cublasHandle_t __cu(blasHandle_t)
 #define cublasCreate(handle) __cu(blasCreate(handle))
 #define cublasDestroy(handle) __cu(blasDestroy(handle))
 
 #define cublasGetProperty(type, pvalue) __cu(blasGetProperty(type, pvalue))
-
-/**
- * The cublasOperation_t type indicates which operation needs to be performed
- * with the dense matrix.
- */
-#define cublasOperation_t __cu(blasOperation_t)
-/* The non-transpose operation is selected. */
-#define CUBLAS_OP_N __CU(BLAS_OP_N)
-/* The transpose operation is selected. */
-#define CUBLAS_OP_T __CU(BLAS_OP_T)
-/* The conjugate transpose operation is selected. */
-#define CUBLAS_OP_C __CU(BLAS_OP_C)
-#define CUBLAS_OP_HERMITAN __CU(BLAS_OP_HERMITAN)
-#define CUBLAS_OP_CONJG __CU(BLAS_OP_CONJG)
-
-#define cublasFillMode_t __cu(blasFillMode_t)
-#define CUBLAS_FILL_MODE_LOWER __CU(BLAS_FILL_MODE_LOWER)
-#define CUBLAS_FILL_MODE_UPPER __CU(BLAS_FILL_MODE_UPPER)
-#define CUBLAS_FILL_MODE_FULL __CU(BLAS_FILL_MODE_FULL)
 
 #define cublasSetMatrix(rows, cols, elemsize, A, ola, B, ldb) \
  __cu(blasSetMatrix(rows, cols, elemsize, A, ola, B, ldb))
@@ -97,14 +67,6 @@
  __cu(blasSetVector(n, elemSize, x, incx, y, incy))
 #define cublasGetVector(n, elemSize, x, incx, y, incy) \
  __cu(blasGetVector(n, elemSize, x, incx, y, incy))
-
-/**
- * The cublasPointerMode_t type indicates whether the scalar values are passed
- * by reference on the host or device.
- */
-#define cublasPointerMode_t __cu(blasPointerMode_t)
-#define CUBLAS_POINTER_MODE_HOST __CU(BLAS_POINTER_MODE_HOST)
-#define CUBLAS_POINTER_MODE_DEVICE __CU(BLAS_POINTER_MODE_DEVICE)
 
 #define cublasGetPointerMode(handle, mode) __cu(blasGetPointerMode(handle, mode))
 #define cublasSetPointerMode(handle, mode) __cu(blasSetPointerMode(handle, mode))
@@ -388,102 +350,6 @@
         B, Btype, ldb, beta, C, Ctype, ldc) \
  __cu(blasSgemmEx(handle, transa, transb, m, n, k, alpha, A, Atype, lda, \
         B, Btype, ldb, beta, C, Ctype, ldc))
-
-
-/**
- * cublasGemmAlgo_t type is an enumerant to specify the algorithm for
- * matrix-matrix multiplication.
- */
-#define cublasGemmAlgo_t __cu(blasGemmAlgo_t)
-#define CUBLAS_GEMM_DEFAULT __CU(BLAS_GEMM_DEFAULT)
-#define CUBLAS_GEMM_ALGO0 __CU(BLAS_GEMM_ALGO0)
-#define CUBLAS_GEMM_ALGO1 __CU(BLAS_GEMM_ALGO1)
-#define CUBLAS_GEMM_ALGO2 __CU(BLAS_GEMM_ALGO2)
-#define CUBLAS_GEMM_ALGO3 __CU(BLAS_GEMM_ALGO3)
-#define CUBLAS_GEMM_ALGO4 __CU(BLAS_GEMM_ALGO4)
-#define CUBLAS_GEMM_ALGO5 __CU(BLAS_GEMM_ALGO5)
-#define CUBLAS_GEMM_ALGO6 __CU(BLAS_GEMM_ALGO6)
-#define CUBLAS_GEMM_ALGO7 __CU(BLAS_GEMM_ALGO7)
-#define CUBLAS_GEMM_ALGO8 __CU(BLAS_GEMM_ALGO8)
-#define CUBLAS_GEMM_ALGO9 __CU(BLAS_GEMM_ALGO9)
-#define CUBLAS_GEMM_ALGO10 __CU(BLAS_GEMM_ALGO10)
-#define CUBLAS_GEMM_ALGO11 __CU(BLAS_GEMM_ALGO11)
-#define CUBLAS_GEMM_ALGO12 __CU(BLAS_GEMM_ALGO12)
-#define CUBLAS_GEMM_ALGO13 __CU(BLAS_GEMM_ALGO13)
-#define CUBLAS_GEMM_ALGO14 __CU(BLAS_GEMM_ALGO14)
-#define CUBLAS_GEMM_ALGO15 __CU(BLAS_GEMM_ALGO15)
-#define CUBLAS_GEMM_ALGO16 __CU(BLAS_GEMM_ALGO16)
-#define CUBLAS_GEMM_ALGO17 __CU(BLAS_GEMM_ALGO17)
-#define CUBLAS_GEMM_ALGO18 __CU(BLAS_GEMM_ALGO18)
-#define CUBLAS_GEMM_ALGO19 __CU(BLAS_GEMM_ALGO19)
-#define CUBLAS_GEMM_ALGO20 __CU(BLAS_GEMM_ALGO20)
-#define CUBLAS_GEMM_ALGO21 __CU(BLAS_GEMM_ALGO21)
-#define CUBLAS_GEMM_ALGO22 __CU(BLAS_GEMM_ALGO22)
-#define CUBLAS_GEMM_ALGO23 __CU(BLAS_GEMM_ALGO23)
-#define CUBLAS_GEMM_DEFAULT_TENSOR_OP __CU(BLAS_GEMM_DEFAULT_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO0_TENSOR_OP __CU(BLAS_GEMM_ALGO0_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO1_TENSOR_OP __CU(BLAS_GEMM_ALGO1_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO2_TENSOR_OP __CU(BLAS_GEMM_ALGO2_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO3_TENSOR_OP __CU(BLAS_GEMM_ALGO3_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO4_TENSOR_OP __CU(BLAS_GEMM_ALGO4_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO5_TENSOR_OP __CU(BLAS_GEMM_ALGO5_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO6_TENSOR_OP __CU(BLAS_GEMM_ALGO6_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO7_TENSOR_OP __CU(BLAS_GEMM_ALGO7_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO8_TENSOR_OP __CU(BLAS_GEMM_ALGO8_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO9_TENSOR_OP __CU(BLAS_GEMM_ALGO9_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO10_TENSOR_OP __CU(BLAS_GEMM_ALGO10_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO11_TENSOR_OP __CU(BLAS_GEMM_ALGO11_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO12_TENSOR_OP __CU(BLAS_GEMM_ALGO12_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO13_TENSOR_OP __CU(BLAS_GEMM_ALGO13_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO14_TENSOR_OP __CU(BLAS_GEMM_ALGO14_TENSOR_OP)
-#define CUBLAS_GEMM_ALGO15_TENSOR_OP __CU(BLAS_GEMM_ALGO15_TENSOR_OP)
-#define CUBLAS_GEMM_AUTOTUNE __CU(BLAS_GEMM_AUTOTUNE)
-
-/**
- * cublasComputeType_t enumerate type is used in cublasGemmEx() and
- * cublasLtMatmul() (including all batched and strided batched variants) to
- * choose compute precision modes as defined below.
- */
-#define cublasComputeType_t __cu(blasComputeType_t)
-/**
- * This is the default and highest-performance mode for 16-bit half precision
- * floating point and all compute and intermediate storage precisions with at
- * least 16-bit half precision. Tensor Cores will be used whenever possible.
- */
-#define CUBLAS_COMPUTE_16F __CU(BLAS_COMPUTE_16F)
-/**
- * This mode uses 16-bit half precision floating point standardized arithmetic
- * for all phases of calculations and is primarily intended for numerical
- * robustness studies, testing, and debugging. This mode might not be as
- * performant as the other modes since it disables use of tensor cores.
- */
-#define CUBLAS_COMPUTE_16F_PEDANTIC __CU(BLAS_COMPUTE_16F_PEDANTIC)
-/**
- * This is the default 32-bit single precision floating point and uses compute
- * and intermediate storage precisions of at least 32-bits.
- */
-#define CUBLAS_COMPUTE_32F __CU(BLAS_COMPUTE_32F)
-#define CUBLAS_COMPUTE_32F_PEDANTIC __CU(BLAS_COMPUTE_32F_PEDANTIC)
-/**
- * Allows the library to use Tensor Cores with automatic down-conversion and
- * 16-bit half-precision compute for 32-bit input and output matrices.
- */
-#define CUBLAS_COMPUTE_32F_FAST_16F __CU(BLAS_COMPUTE_32F_FAST_16F)
-#define CUBLAS_COMPUTE_32F_FAST_16BF __CU(BLAS_COMPUTE_32F_FAST_16BF)
-#define CUBLAS_COMPUTE_32F_FAST_TF32 __CU(BLAS_COMPUTE_32F_FAST_TF32)
-#define CUBLAS_COMPUTE_32F_EMULATED_16BFX9 __CU(BLAS_COMPUTE_32F_EMULATED_16BFX9)
-/**
- * This is the default 64-bit double precision floating point and uses compute
- * and intermediate storage precisions of at least 64-bits.
- */
-#define CUBLAS_COMPUTE_64F __CU(BLAS_COMPUTE_64F)
-#define CUBLAS_COMPUTE_64F_PEDANTIC __CU(BLAS_COMPUTE_64F_PEDANTIC)
-/**
- * This is the default 32-bit integer mode and uses compute and intermediate
- * storage precisions of at least 32-bits.
- */
-#define CUBLAS_COMPUTE_32I __CU(BLAS_COMPUTE_32I)
-#define CUBLAS_COMPUTE_32I_PEDANTIC __CU(BLAS_COMPUTE_32I_PEDANTIC)
 
 #if defined(__USE_HPCC__)
 /**
