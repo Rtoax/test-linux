1068b93802d6c27a8f5ca9826361c688b6b4a28e
Rong Tao <rongtao@cestc.cn>
2026-02-11 17:27:43 +0800

    cuda/adapter: add empty cublasXt.h
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Phytium, Arch aarch64, Physical Machine, Lingspeed Lingspeed X710Mx8
    Vers: CCLinux 22.09.2, Linux 5.15.131-28.cl9_GDS.aarch64, GCC 11.3.1, glibc 2.34, LLVM 14.0.6, CUDA V13.0.48, LUCA 3.2.1, HPCC 3.0.0

---
+++ hpc/nvidia/cuda/adapter/cublasXt.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+/* Copyright (C) 2026 Rong Tao */
+#ifndef __CUDA_ADAPTER_CUBLASXT_H
+#define __CUDA_ADAPTER_CUBLASXT_H 1
+
+#include "wrapper_defs.h"
+
+#endif
