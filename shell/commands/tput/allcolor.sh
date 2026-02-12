53f61f4f3ed2e491d3395343ea4a672f531152d1
Rong Tao <rongtao@cestc.cn>
2026-02-12 22:21:58 +0800

    tput: allcolor.sh: show all background color of tput
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, Physical Machine
    Vers: Ubuntu 25.10, Linux 6.17.0-12-generic, BASH 5.2.37

--- shell/commands/tput/allcolor.sh
+++ shell/commands/tput/allcolor.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -e
 
-for i in $(seq 0 1 256)
+for i in $(seq 0 1 255)
 do
+	# set front
 	tput setaf $i
-	echo "This is color of number $i."
+	echo -n "This is front color of number $i."
+	# Reset all attribute
 	tput sgr0
+
+	# set background
+	tput setab $i
+	echo -n " This is background color of number $i."
+	# Reset all attribute
+	tput sgr0
+
+	echo
 done
