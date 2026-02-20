a93b36ecc551c18708a8f6cdb95737eb06c396c6
Rong Tao <rongtao@cestc.cn>
2026-02-20 14:58:58 +0800

    vim: version.sh: add [sharepath] argument
    $ ./version.sh sharepath
    /usr/share/vim/vim91/
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Intel(R) Core(TM) i7-8700 CPU @ 3.20GHz, Arch x86_64, Physical Machine
    Vers: Fedora Linux 42, Linux 6.18.12-100.fc42.x86_64, BASH 5.2.37, Make 4.4.1, Vim 9.1

--- tools/vim/version.sh
+++ tools/vim/version.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 # This script only display vim version, do not display other anything,
 # because the git/hooks will use it.
+#
+# Usage: version.sh [sharepath]
+#
 set -e
 VIM=$(which vim 2>/dev/null || :)
-if [[ -z ${VIM} ]]; then
-	echo >&2 "ERROR: not found VIM in your system"
-	exit 0
-fi
-${VIM} --version | grep -Eo '[0-9]+\.[0-9]+' 2>/dev/null || true
+
+vim_version() {
+	if [[ -z ${VIM} ]]; then
+		echo >&2 "ERROR: not found VIM in your system"
+		exit 0
+	fi
+	${VIM} --version | grep -Eo '[0-9]+\.[0-9]+' 2>/dev/null || true
+}
+
+case $1 in
+sharepath)
+	if [[ -d /usr/share/vim/ ]]; then
+		echo /usr/share/vim/vim$(vim_version | tr -d .)/
+	elif [[ -d /usr/local/share/vim/ ]]; then
+		echo /usr/local/share/vim/vim$(vim_version | tr -d .)/
+	fi
+	;;
+"")
+	vim_version
+	;;
+*)
+	exit 1
+	;;
+esac
