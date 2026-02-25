897d38109d80558b878e41873aa266f01fe9053e
Rong Tao <rongtao@cestc.cn>
2026-02-25 18:35:14 +0800

    cuda/adapter: add nvblas.h
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU Cortex-X925 Cortex-A725, Arch aarch64, Physical Machine, NVIDIA GB10
    Vers: Ubuntu 24.04, Linux 6.14.0-1015-nvidia, GCC 13.3.0, glibc 2.39, LLVM 18.1.3, CUDA V13.0.88

---
+++ hpc/nvidia/cuda/adapter/include/nvblas.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+/* Copyright (C) 2026 Rong Tao */
+#ifndef __CUDA_ADAPTER_CUBLAS_H
+#define __CUDA_ADAPTER_CUBLAS_H 1
+
+#ifdef NVBLAS_H_
+#error "CudaAdapter not allow include origin CUDA nvblas.h"
+#endif
+
+#endif
