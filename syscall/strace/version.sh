d36188ca3ce15ae7c536ac58d95237b8e87b270c
Rong Tao <rongtao@cestc.cn>
2026-02-26 15:12:19 +0800

    strace: add version.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU AMD EPYC 7763 64-Core Processor, Arch x86_64, Physical Machine
    Vers: CCLinux 22.09.2, Linux 5.15.131-8.cl9.x86_64, BASH 5.1.8, Strace 5.18, Make 4.3, Git 2.39.3

---
+++ syscall/strace/version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script only display strace version, do not display other anything,
+# and don't excute failed, because the git/hooks will use it.
+set -e
+STRACE=$(which strace 2>/dev/null || :)
+if [[ -z ${STRACE} ]]; then
+	echo >&2 "ERROR: not found STRACE in your system"
+	exit 0
+fi
+${STRACE} --version | grep -Eo '[0-9]+\.[0-9]+' 2>/dev/null || true
