0b3fca98a99753dc14a1aa29d6c505c7a5ec82df
Rong Tao <rongtao@cestc.cn>
2026-02-26 16:53:56 +0800

    sched/reparent: orphan.sh: test setsid
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU AMD EPYC 7763 64-Core Processor, Arch x86_64, Physical Machine
    Vers: CCLinux 22.09.2, Linux 5.15.131-8.cl9.x86_64, BASH 5.1.8, Make 4.3

--- sched/reparent/orphan.sh
+++ sched/reparent/orphan.sh
@@ -3,8 +3,16 @@ set -e
 
 cmd="sleep 2"
 
-bash -c "${cmd} & exit"
+if [[ $1 == setsid ]]; then
+	setsid --fork ${cmd}
+elif [[ $1 == bash ]]; then
+	bash -c "${cmd} & exit"
+else
+	echo "Usage: $0 [setsid|bash]"
+	exit 1
+fi
 
+# Get parent PID and check
 pid=$(pgrep -n -f "${cmd}")
 ppid=( $(ps -p ${pid} -o ppid | grep -v PPID) )
 
