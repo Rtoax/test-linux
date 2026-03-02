a96c273f1895d3b981ee3d2c38e76b9b3750bc14
Rong Tao <rongtao@cestc.cn>
2026-03-02 16:55:52 +0800

    scripts/clang-format.sh: rename F1 to FCF
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: CCLinux 22.09.2, Linux 5.15.131-28.cl9_GDS.aarch64, BASH 5.1.8

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Usage: [F1=1] clang-format.sh
-
+# Check code format with clang-format.
+#
+# Usage: [FCF=1] clang-format.sh
+# - FCF: Force clang-format
 set -e
 
 fatal() {
@@ -15,9 +17,9 @@ readonly GIT_TOPDIR=$(git rev-parse --show-toplevel 2>/dev/null || :)
 # Check code format wich git-clang-format
 readonly clang_format=$(which git-clang-format 2>/dev/null || :)
 
-if  [[ -z ${clang_format} ]] && [[ -z ${F1} ]]; then
-	fatal "Not found git-clang-format, please install or F1=1"
-elif [[ -z ${F1} ]]; then
+if  [[ -z ${clang_format} ]] && [[ -z ${FCF} ]]; then
+	fatal "Not found git-clang-format, please install or FCF=1"
+elif [[ -z ${FCF} ]]; then
 	repository=$(basename ${GIT_TOPDIR})
 	if [[ " ostools tools ulpatch " =~ " ${repository} " ]]; then
 		branch=origin/master
@@ -31,7 +33,7 @@ elif [[ -z ${F1} ]]; then
 				-e 'clang-format did not modify any files' || :)"
 	if [[ $? != 0 ]] || [[ "${patch}" ]]; then
 		echo "${patch}"
-		fatal "Bad code format, please modify according to the above diff or F1=1"
+		fatal "Bad code format, please modify according to the above diff or FCF=1"
 	fi
 fi
 
