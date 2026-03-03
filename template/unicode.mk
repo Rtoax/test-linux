305bd3fcd855715ecb947c84d651a840538e1490
Rong Tao <rongtao@cestc.cn>
2026-03-03 09:34:24 +0800

    template: add unicode.mk
    git/clean.sh remove unicode.mk by mistake.
    
    Link: commit 43125a99d51a ("git/clean.sh: do not clean if there are no-add files")
    Fix: commit be89d51953d2 ("template: add unicode.mk")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.18.13-200.fc43.x86_64, Make 4.4.1

---
+++ template/unicode.mk
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: GPL-3.0
+# Copyright (C) 2026 Rong Tao
+ifndef _UNICODE_MK
+_UNICODE_MK = 1
+
+U2665 = ♥
+U2764 = ❤
+U2765 = ❥
+U2766 = ❦
+U2767 = ❧
+
+ifdef DEBUG
+  $(info UNICODE)
+endif
+
+endif
