cd5a69ecaf39b4571f1112f33d6a75f02fa81dda
Rong Tao <rongtao@cestc.cn>
2026-03-04 09:28:34 +0800

    redis: redis maybe is Valkey on fedora because of the license
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.18.13-200.fc43.x86_64, Bash 5.3.0, Make 4.4.1, Redis(Valkey) 8.1.5, Git 2.53.0

--- tools/redis/version.sh
+++ tools/redis/version.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 # This script only display redis-cli version, do not display other anything,
 # and don't execute failed, because the git/hooks will use it.
+#
+# Usage: version.sh [--name]
+#
 set -e
 REDIS_CLI=$(which redis-cli 2>/dev/null || :)
+REDIS_SERVER=$(which redis-server 2>/dev/null || :)
 if [[ -z ${REDIS_CLI} ]]; then
 	echo >&2 "ERROR: not found REDIS_CLI in your system"
 	exit 0
 fi
-${REDIS_CLI} --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' 2>/dev/null || true
+
+if [[ $1 == --name ]]; then
+	if [[ "$(${REDIS_SERVER} --version | grep -wo Valkey)" ]]; then
+		echo "Redis(Valkey)"
+	else
+		echo "Redis"
+	fi
+else
+	${REDIS_CLI} --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' 2>/dev/null || true
+fi
