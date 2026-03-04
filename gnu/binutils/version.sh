d1957bc3aa4e051cbf3e26a41c8de10d12142bdf
Rong Tao <rongtao@cestc.cn>
2026-03-04 14:57:31 +0800

    binutils: add version.sh and apply to binutils.mk
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 24.04, Linux 6.17.0-1008-nvidia, Bash 5.2.21, Make 4.3

---
+++ gnu/binutils/version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This script only display binutils version, do not display other anything,
+# because the git/hooks will use it.
+#
+# Usage: version.sh [--major|--minor]
+#
+set -e
+ldbfd=$(which ld.bfd 2>/dev/null || :)
+if [[ -z ${ldbfd} ]]; then
+	echo >&2 "ERROR: not found binutils in your system"
+	exit 0
+fi
+
+version=$( ${ldbfd} --version | grep -Eo '[0-9]+\.[0-9]+' 2>/dev/null || true )
+
+case $1 in
+--major)
+	echo ${version%%.*}
+	;;
+--minor)
+	echo ${version##*.}
+	;;
+"")
+	echo ${version}
+	;;
+*)
+	exit 1
+	;;
+esac
--- template/binutils.mk
+++ template/binutils.mk
@@ -4,18 +4,20 @@
 ifndef _BINUTILS_MK
 _BINUTILS_MK = 1
 
+include dir.mk
+
 LD_BFD := $(shell which ld.bfd 2>/dev/null)
 ifeq ($(LD_BFD),)
-  $(error "Not found linker ld.bfd, install binutils first")
+  $(warning "Not found linker ld.bfd, install binutils first")
 endif
 
 # Get binutils version
-BINUTILS_VERSION := $(shell ${LD_BFD} --version | grep -o [0-9].[0-9]\. | sed -n '1p')
-BINUTILS_VERSION_MAJOR := $(shell echo ${BINUTILS_VERSION} | awk -F '.' '{print $$1}')
-BINUTILS_VERSION_MINOR := $(shell echo ${BINUTILS_VERSION} | awk -F '.' '{print $$2}')
+BINUTILS_VERSION := $(shell ${TOPDIR}/gnu/binutils/version.sh)
+BINUTILS_VERSION_MAJOR := $(shell ${TOPDIR}/gnu/binutils/version.sh --major)
+BINUTILS_VERSION_MINOR := $(shell ${TOPDIR}/gnu/binutils/version.sh --minor)
 
 ifneq (${BINUTILS_VERSION_MAJOR},2)
-  $(error Not support binutils ${BINUTILS_VERSION})
+  $(error Not support binutils major=${BINUTILS_VERSION})
 endif
 
 ifdef DEBUG
