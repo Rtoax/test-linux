738bf137b1f2f677965040b7f8906c608661219c
Rong Tao <rongtao@cestc.cn>
2026-03-04 18:49:34 +0800

    bpftrace.mk: rename version vars
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- template/bpf/bpftrace.mk
+++ template/bpf/bpftrace.mk
@@ -4,8 +4,9 @@
 # Output definitions:
 # - HAVE_BPFTRACE=[y|n]
 # - BPFTRACE=[/usr/bin/bpftrace]
-# - BPFTRACE_VERSION_MAJOR=
-# - BPFTRACE_VERSION_MINOR=
+# - BPFTRACE_MAJOR=
+# - BPFTRACE_MINOR=
+# - BPFTRACE_PATCHLEVEL=
 #
 ifndef _BPFTRACE_MK
 _BPFTRACE_MK = 1
@@ -23,20 +24,20 @@ else
 include dir.mk
 
 BPFTRACE_VERSION := $(shell ${TOPDIR}/bpf/bpftrace/version.sh)
-BPFTRACE_VERSION_MAJOR := $(shell ${TOPDIR}/bpf/bpftrace/version.sh --major)
-BPFTRACE_VERSION_MINOR := $(shell ${TOPDIR}/bpf/bpftrace/version.sh --minor)
-BPFTRACE_VERSION_PATCHLEVEL := $(shell ${TOPDIR}/bpf/bpftrace/version.sh --patchlevel)
+BPFTRACE_MAJOR := $(shell ${TOPDIR}/bpf/bpftrace/version.sh --major)
+BPFTRACE_MINOR := $(shell ${TOPDIR}/bpf/bpftrace/version.sh --minor)
+BPFTRACE_PATCHLEVEL := $(shell ${TOPDIR}/bpf/bpftrace/version.sh --patchlevel)
 
 ifdef DEBUG
   $(info BPFTRACE = ${BPFTRACE})
-  $(info BPFTRACE_VERSION_MAJOR = ${BPFTRACE_VERSION_MAJOR})
-  $(info BPFTRACE_VERSION_MINOR = ${BPFTRACE_VERSION_MINOR})
-  $(info BPFTRACE_VERSION_PATCHLEVEL = ${BPFTRACE_VERSION_PATCHLEVEL})
+  $(info BPFTRACE_MAJOR = ${BPFTRACE_MAJOR})
+  $(info BPFTRACE_MINOR = ${BPFTRACE_MINOR})
+  $(info BPFTRACE_PATCHLEVEL = ${BPFTRACE_PATCHLEVEL})
 endif
 
 export HAVE_BPFTRACE := y
 export BPFTRACE
-export BPFTRACE_VERSION_MAJOR BPFTRACE_VERSION_MINOR BPFTRACE_VERSION_PATCHLEVEL
+export BPFTRACE_MAJOR BPFTRACE_MINOR BPFTRACE_PATCHLEVEL
 
 endif # End of Found bpftrace
 
