80bd9857550500bbbe4e3cb7fc339453dab0bb9c
Rong Tao <rongtao@cestc.cn>
2026-03-04 14:37:04 +0800

    make.mk: add make_{gt,ge,eq,lt,le}
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 24.04, Linux 6.17.0-1008-nvidia, Make 4.3

--- template/make.mk
+++ template/make.mk
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (C) 2026 Rong Tao
 #
+# Functions:
+# - make_{gt,ge,eq,lt,le}=[y|n]
+#
 ifndef _MAKE_MK
 _MAKE_MK = 1
 
@@ -11,10 +14,29 @@ MAKE_VERSION := $(shell ${TOPDIR}/gnu/make/version.sh)
 MAKE_MAJOR := $(shell ${TOPDIR}/gnu/make/version.sh --major)
 MAKE_MINOR := $(shell ${TOPDIR}/gnu/make/version.sh --minor)
 
+MAKE_VERSION_CODE := $(call version2_code100010,${MAKE_MAJOR},${MAKE_MINOR})
+
+define make_gt
+$(call version2_code100010_cmp,${MAKE_VERSION_CODE},-gt,${1},${2})
+endef
+define make_ge
+$(call version2_code100010_cmp,${MAKE_VERSION_CODE},-ge,${1},${2})
+endef
+define make_eq
+$(call version2_code100010_cmp,${MAKE_VERSION_CODE},-eq,${1},${2})
+endef
+define make_lt
+$(call version2_code100010_cmp,${MAKE_VERSION_CODE},-lt,${1},${2})
+endef
+define make_le
+$(call version2_code100010_cmp,${MAKE_VERSION_CODE},-le,${1},${2})
+endef
+
 ifdef DEBUG
   $(info MAKE_VERSION = ${MAKE_VERSION})
   $(info MAKE_MAJOR = ${MAKE_MAJOR})
   $(info MAKE_MINOR = ${MAKE_MINOR})
+  $(info MAKE_VERSION_CODE = ${MAKE_VERSION_CODE})
 endif
 
 endif
