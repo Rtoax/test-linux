7e5ea6527eba7237700efc923e9aca2362b20505
Rong Tao <rongtao@cestc.cn>
2026-03-04 16:16:40 +0800

    patchset_examples.sh: check --patch option
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- scripts/patchset_examples.sh
+++ scripts/patchset_examples.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+./patchset.sh --version
 ./patchset.sh --help | cat
 ./patchset.sh --from 54103fd68173 --to b0cb7b6917c0 --no-cover-letter
 ./patchset.sh --from 54103fd68173 --to b0cb7b6917c0 --no-cover-letter --pure-diff --output tmp.patch.diff
@@ -11,5 +12,8 @@ set -e
 # get patch info
 {
 	./patchset.sh --from=1f28d343262b --to=1f28d343262b --no-cover-letter
-	./patchset.sh --patch tmp.patch/0001-patchset.sh-update-help-info.patch
+	if [[ "$(./patchset.sh --patch tmp.patch/0001-patchset.sh-update-help-info.patch)" != "patchset.sh: update help info" ]]; then
+		echo >&2 "ERROR: --patch run failed."
+		exit 1
+	fi
 }
