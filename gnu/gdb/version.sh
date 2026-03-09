4c4e4ae1a7be4b328fe8e895921c9f2bad3fddbe
Rong Tao <rongtao@cestc.cn>
2026-03-09 23:43:44 +0800

    gdb: version.sh: uniq the version
    $ gdb --version
    GNU gdb (Ubuntu 16.3-1ubuntu2) 16.3
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 25.10, Linux 6.17.0-14-generic, GDB 16.3, Bash 5.2.37

--- gnu/gdb/version.sh
+++ gnu/gdb/version.sh
@@ -7,4 +7,4 @@ if [[ -z ${GDB} ]]; then
 	echo >&2 "ERROR: not found GDB in your system"
 	exit 0
 fi
-${GDB} --version | grep -Eo '[0-9]+\.[0-9]+' 2>/dev/null || true
+( ${GDB} --version | grep -Eo '[0-9]+\.[0-9]+' 2>/dev/null || true ) | uniq
