cc2d99448d8659ba93c7009b94e2e474a91a75de
Rong Tao <rongtao@cestc.cn>
2026-03-10 15:30:04 +0800

    target/cuda.mk: target-nvcc-{liba,libso}-y: use ${name}-objs
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.18.16-200.fc43.x86_64, Make 4.4.1, LLVM 21.1.8, CUDA V13.0.48, ROCm 6.4.43484

--- template/targets/cuda.mk
+++ template/targets/cuda.mk
@@ -190,6 +190,9 @@ $(target-nvcc-liba-y): %:
 	$(call log_tgt,NVCC AR,$(@))
 	${Q}ar rcs $(@) $(^)
 
+$(foreach lib, ${target-nvcc-libso-y}, $(eval ${lib}: $${${lib}-objs}))
+$(foreach lib, ${target-nvcc-liba-y}, $(eval ${lib}: $${${lib}-objs}))
+
 $(foreach t, ${target-nvcc-y}, \
   $(if $(shell test -f ${t}.cu && echo yes), \
     $(if ${DEBUG}, $(info Dep ${t}: ${OUTPUT}${t}.cu.o $${${t}-objs} ${CUDA_HELPERS})) \
