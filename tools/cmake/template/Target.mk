ff818992e99ad12e8ffcb69e82c43cd5d61b146a
Rong Tao <rongtao@cestc.cn>
2026-03-12 17:00:48 +0800

    cmake/template: remove kernel module
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tools/cmake/template/Target.mk
+++ tools/cmake/template/Target.mk
@@ -3,8 +3,6 @@ build0:
 	@echo -e " \033[1;33m Building User \033[m"
 	${Q}make -f Makefile.user
 	${Q}mkdir -p build && pushd build && cmake .. && make && popd
-	@echo -e " \033[1;33m Building Kernel \033[m"
-	${Q}make -f Makefile.kernel
 
 .PHONY: clean0
 clean0:
