921a197b6230c18e45c14a485612a6ec6ecbe43f
Rong Tao <rongtao@cestc.cn>
2026-03-12 23:33:50 +0800

    subdir-footer.mk: indent tab with 2 space
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 25.10, Linux 6.17.0-14-generic, Make 4.4.1

--- template/targets/subdir-footer.mk
+++ template/targets/subdir-footer.mk
@@ -14,32 +14,32 @@ endif
 # $1: build, test, clean
 # $2: subdir-y
 define make_sub_dir
-	$(call log_info,${1} $(call strip_topdir_prefix,$(2)))
-	$(Q)pushd $(2) >/dev/null; \
-	start_ms=$$(date +%s%3N); \
-	${MAKE} ${1} ${SUBMKFLAGS}; \
-	makeret=$$?; \
-	end_ms=$$(date +%s%3N); \
-	cost_ms=$$((end_ms - start_ms)); \
-	if [ $${makeret} -ne 0 ]; then \
-		$(call log_failed,${EMOJI_CROSS} Failed ${1} $(call strip_topdir_prefix,$(2)) cost $${cost_ms} ms); \
-		${CHECK_ERROR_EXIT} \
-	else	\
-		$(call log_success,${EMOJI_CHECK} Success ${1} $(call strip_topdir_prefix,$(2)) cost $${cost_ms} ms); \
-	fi; \
-	popd >/dev/null
+  $(call log_info,${1} $(call strip_topdir_prefix,$(2)))
+  $(Q)pushd $(2) >/dev/null; \
+  start_ms=$$(date +%s%3N); \
+  ${MAKE} ${1} ${SUBMKFLAGS}; \
+  makeret=$$?; \
+  end_ms=$$(date +%s%3N); \
+  cost_ms=$$((end_ms - start_ms)); \
+  if [ $${makeret} -ne 0 ]; then \
+    $(call log_failed,${EMOJI_CROSS} Failed ${1} $(call strip_topdir_prefix,$(2)) cost $${cost_ms} ms); \
+    ${CHECK_ERROR_EXIT} \
+  else  \
+    $(call log_success,${EMOJI_CHECK} Success ${1} $(call strip_topdir_prefix,$(2)) cost $${cost_ms} ms); \
+  fi; \
+  popd >/dev/null
 endef
 
 define make_sub_dir_build
-	$(call make_sub_dir,build,${1})
+  $(call make_sub_dir,build,${1})
 endef
 
 define make_sub_dir_test
-	$(call make_sub_dir,test,${1})
+  $(call make_sub_dir,test,${1})
 endef
 
 define make_sub_dir_clean
-	$(call make_sub_dir,clean,${1})
+  $(call make_sub_dir,clean,${1})
 endef
 
 .PHONY: $(subdir-y-build)
