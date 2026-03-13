a081d8c9ef4780f27d6a7a48b6567ff17976a337
Rong Tao <rongtao@cestc.cn>
2026-03-13 10:33:35 +0800

    bpf/iter.mk: Debian 13.3 6.12.73+deb13-amd64 not support iter_kmem_cache
    libbpf$ make
    ....
    [2026/03/13 10:32:32] BPF      .output/iter_kmem_cache.bpf.o
    iter_kmem_cache.bpf.c:19:28: warning: declaration of 'struct bpf_iter__kmem_cache' will not be visible outside of this function [-Wvisibility]
       19 | int dump_kmem_cache(struct bpf_iter__kmem_cache *ctx)
          |                            ^
    iter_kmem_cache.bpf.c:21:28: error: incomplete definition of type 'struct bpf_iter__kmem_cache'
       21 |         struct seq_file *seq = ctx->meta->seq;
          |                                ~~~^
    iter_kmem_cache.bpf.c:19:28: note: forward declaration of 'struct bpf_iter__kmem_cache'
       19 | int dump_kmem_cache(struct bpf_iter__kmem_cache *ctx)
          |                            ^
    iter_kmem_cache.bpf.c:22:28: error: incomplete definition of type 'struct bpf_iter__kmem_cache'
       22 |         struct kmem_cache *s = ctx->s;
          |                                ~~~^
    iter_kmem_cache.bpf.c:19:28: note: forward declaration of 'struct bpf_iter__kmem_cache'
       19 | int dump_kmem_cache(struct bpf_iter__kmem_cache *ctx)
          |                            ^
    1 warning and 2 errors generated.
    make: *** [targets/bpf.mk:42: .output/iter_kmem_cache.bpf.o] Error 1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Debian GNU/Linux 13.3, Linux 6.12.73+deb13-amd64, Make 4.4.1, BCC 0.34.0, LibBpf 1.7.0

--- template/bpf/iter.mk
+++ template/bpf/iter.mk
@@ -56,7 +56,8 @@ endif
 
 # linux v6.12-rc1-177-g4971266e1595
 # commit 4971266e1595 ("bpf: Add kmem_cache iterator")
-ifeq ($(call kver_gt,6,12,0),y)
+# - debian13 6.12.73+deb13-amd64 not support
+ifeq ($(call kver_gt,6,12,73),y)
   $(call bpf_def_iter,iter_kmem_cache)
 endif
 
