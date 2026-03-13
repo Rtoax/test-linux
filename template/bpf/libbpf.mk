5ad09acb0aface19d08790f5cda0b9e50ea0d956
Rong Tao <rongtao@cestc.cn>
2026-03-13 10:56:20 +0800

    bpf/libbpf.mk: use version.sh;
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.18.16-200.fc43.x86_64, Make 4.4.1, Pahole 1.30, BCC 0.35.0, LibBpf 1.6.1

--- template/bpf/libbpf.mk
+++ template/bpf/libbpf.mk
@@ -1,51 +1,49 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (C) 2025-2026 Rong Tao
 #
+# Note:
+# - libbpf commit 8ade99a6f84d ("libbpf: Make libbpf_version.h non-auto-generated")
+#   v0.6.0 add macro LIBBPF_MAJOR_VERSION and LIBBPF_MINOR_VERSION in
+#   libbpf_version.h
+#
 # Output definitions:
 # - HAVE_LIBBPF=[y|n]
 # - LIBBPF_MAJOR_VERSION=
 # - LIBBPF_MINOR_VERSION=
+# - LIBBPF_PATCHLEVEL_VERSION=
 #
 ifndef _BPF_LIBBPF_MK
 _BPF_LIBBPF_MK = 1
 
-include ldconfig.mk
+include dir.mk
 
-libbpf-paths := $(shell ${LDCONFIG} -p | grep libbpf.so | awk '{print $$NF}' || true)
+LIBBPF_VERSION := $(shell ${TOPDIR}/bpf/libbpf/version.sh)
 
-ifneq (${libbpf-paths},)
-  libbpf-path := $(shell dirname $(libbpf-paths) | sort -u || true)
-endif
-
-ifneq (${libbpf-path},)
-  # Like /usr/lib64/libbpf.so.0.4.0
-  libbpf-so := $(shell realpath ${libbpf-path}/libbpf.so.[0-9])
-endif
-
-ifeq (${libbpf-so},)
+ifeq (${LIBBPF_VERSION},)
   export HAVE_LIBBPF := n
 else
 
-  export LIBBPF_MAJOR_VERSION := $(shell echo ${libbpf-so} | awk -F '.' '{print $$3}')
-  export LIBBPF_MINOR_VERSION := $(shell echo ${libbpf-so} | awk -F '.' '{print $$4}')
+export LIBBPF_MAJOR_VERSION := $(shell ${TOPDIR}/bpf/libbpf/version.sh --major)
+export LIBBPF_MINOR_VERSION := $(shell ${TOPDIR}/bpf/libbpf/version.sh --minor)
+export LIBBPF_PATCHLEVEL_VERSION := $(shell ${TOPDIR}/bpf/libbpf/version.sh --patchlevel)
 
-  ifeq (${LIBBPF_MAJOR_VERSION},)
-    $(error "Could not get libbpf LIBBPF_MAJOR_VERSION")
-  endif
-  ifeq (${LIBBPF_MINOR_VERSION},)
-    $(error "Could not get libbpf LIBBPF_MINOR_VERSION")
-  endif
-
-  export HAVE_LIBBPF := y
+ifeq (${LIBBPF_MAJOR_VERSION},)
+  $(error "Could not get libbpf LIBBPF_MAJOR_VERSION")
 endif
+ifeq (${LIBBPF_MINOR_VERSION},)
+  $(error "Could not get libbpf LIBBPF_MINOR_VERSION")
+endif
+
+export HAVE_LIBBPF := y
+
+endif # end of Found libbpf
 
 ifdef DEBUG
   $(info HAVE_LIBBPF = ${HAVE_LIBBPF})
-  $(info libbpf-paths = ${libbpf-paths})
-  $(info libbpf-path = ${libbpf-path})
-  $(info libbpf-so = ${libbpf-so})
+  $(info LIBBPF_VERSION = ${LIBBPF_VERSION})
   $(info LIBBPF_MAJOR_VERSION = ${LIBBPF_MAJOR_VERSION})
   $(info LIBBPF_MINOR_VERSION = ${LIBBPF_MINOR_VERSION})
+  $(info LIBBPF_PATCHLEVEL_VERSION = ${LIBBPF_PATCHLEVEL_VERSION})
 endif
 
-endif
+endif # end of _BPF_LIBBPF_MK
