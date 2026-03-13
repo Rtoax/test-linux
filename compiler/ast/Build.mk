d5a3d62744fd71b07d70323aac98279c01327138
Rong Tao <rongtao@cestc.cn>
2026-03-13 16:58:08 +0800

    compiler/ast: use target-llvm-ll-y
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 24.04, Linux 6.17.0-1008-nvidia, GCC 13.3.0, Make 4.3

--- compiler/ast/Build.mk
+++ compiler/ast/Build.mk
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0
-USE_LLVM = 1
+target-llvm-ll-y := hello
+hello-objs := hello.llvm.ll
 
-CC := clang
-
-target-y := hello
-target-prep-y := hello.llvm.ll
 target-prep-y += hello.llvm.ast
+target-prep-y += hello.llvm.bc
