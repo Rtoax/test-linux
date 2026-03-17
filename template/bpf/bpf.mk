4c09cedd3cf22587ec2e51c83768a8b6fc09f263
Rong Tao <rongtao@cestc.cn>
2026-03-17 09:08:39 +0800

    bpf/xdp.mk: fix wrong use of /sys/kernel/btf/ and /sys/fs/bpf/
    never mount bpf to /sys/kernel/btf/, it's should be /sys/fs/bpf/.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.7-200.fc43.x86_64, Make 4.4.1, BCC 0.35.0, LibBpf 1.6.1

--- template/bpf/bpf.mk
+++ template/bpf/bpf.mk
@@ -2,11 +2,14 @@
 # Copyright (C) 2024-2026 Rong Tao
 #
 # Output definitions:
+# - BPFFS=[/sys/fs/bpf/]
 # - BPF_TARGET_ARCH=
 # - CFLAG_BPF_TARGET_ARCH=
 #
 ifndef _BPF_BPF_MK
 _BPF_BPF_MK = 1
+
+BPFFS := /sys/fs/bpf/
 
 BPF_TARGET_ARCH := $(shell uname -m | sed 's/x86_64/x86/' \
 			 | sed 's/arm.*/arm/' \
@@ -19,10 +22,12 @@ ifeq (${BPF_TARGET_ARCH},$(shell uname -m))
   $(error Not handle arch ${shell uname -m} yet, please do)
 endif
 
+export BPFFS
 export BPF_TARGET_ARCH
 export CFLAG_BPF_TARGET_ARCH := -D__TARGET_ARCH_$(BPF_TARGET_ARCH)
 
 ifdef DEBUG
+  $(info BPFFS = ${BPFFS})
   $(info BPF_TARGET_ARCH = ${BPF_TARGET_ARCH})
   $(info CFLAG_BPF_TARGET_ARCH = ${CFLAG_BPF_TARGET_ARCH})
 endif
--- template/bpf/xdp.mk
+++ template/bpf/xdp.mk
@@ -12,8 +12,8 @@ ifndef _BPF_XDP_MK
 _BPF_XDP_MK = 1
 
 include sudo.mk
+include bpf/bpf.mk
 include bpf/bpftool.mk
-include bpf/btf.mk
 
 # Attach XDP to network interface, the 'xdp' could be change to one of `xdpdrv`
 # `xdpgeneric`, `xdpoffload`.
@@ -22,14 +22,14 @@ include bpf/btf.mk
 # $2: object bpf file
 # $3: bpf program name
 define attach_xdp
-${SUDO} ${BPFTOOL} prog load ${2} ${BTF_ROOT}/${3} && \
-${SUDO} ${BPFTOOL} net attach xdp pinned ${BTF_ROOT}/${3} dev ${1}
+${SUDO} ${BPFTOOL} prog load ${2} ${BPFFS}/${3} && \
+${SUDO} ${BPFTOOL} net attach xdp pinned ${BPFFS}/${3} dev ${1}
 endef
 
 # $1: network interface, like 'eth0'
 # $2: bpf program name
 define detach_xdp
-${SUDO} ${BPFTOOL} net detach xdp dev ${1} && ${SUDO} rm -f ${BTF_ROOT}/${2}
+${SUDO} ${BPFTOOL} net detach xdp dev ${1} && ${SUDO} rm -f ${BPFFS}/${2}
 endef
 
 # $1: network interface, like 'eth0'
