e563b0e2c9c27ae6eb8daeb2c37b1f4dc1d2caec
Rong Tao <rongtao@cestc.cn>
2026-03-17 09:12:58 +0800

    libxdp: Target.mk: add log for post test
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.7-200.fc43.x86_64, Make 4.4.1, BCC 0.35.0, LibBpf 1.6.1

--- bpf/xdp/libxdp/Target.mk
+++ bpf/xdp/libxdp/Target.mk
@@ -1,13 +1,16 @@
+include log.mk
 include bpf/xdp.mk
 
 ${OUTPUT}xsk_user.o: ${OUTPUT}xdp_xsk.skel.h
 
 .PHONY: post-xdp_elfsec
 post-xdp_elfsec:
+	$(call log_info,Test xdp_elfsec)
 	${Q}$(call attach_xdp_with_ip,lo,${OUTPUT}xdp_elfsec.bpf.o,mysection)
 	${Q}$(call detach_xdp_with_ip,lo)
 
 .PHONY: post-xdp_drop
 post-xdp_drop:
+	$(call log_info,Test xdp_drop)
 	${Q}$(call attach_xdp,lo,${OUTPUT}xdp_drop.bpf.o,drop)
 	${Q}$(call detach_xdp,lo,drop)
