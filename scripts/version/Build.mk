44f8a583b0f7e1edf6bd1c857a1ad36368b6bab6
Rong Tao <rongtao@cestc.cn>
2026-03-18 19:16:39 +0800

    scripts/version: use libversion.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.8-200.fc43.x86_64, Bash 5.3.0, Systemd 258.7, Make 4.4.1, CMake 3.31.10, NUMACTL 2.0.19

--- scripts/version/Build.mk
+++ scripts/version/Build.mk
@@ -8,7 +8,6 @@ target-prog-y += linux.sh.1 linux.sh.2 linux.sh.3 linux.sh.4
 target-prog-y += linux.sh.5 linux.sh.6 linux.sh.7 linux.sh.8
 target-prog-y += systemd.sh
 target-prog-y += uefi.sh uefi.sh.1 uefi.sh.2 uefi.sh.3
-target-prog-y += numactl.sh
 target-prog-y += pahole.sh pahole.sh.1 pahole.sh.2
 target-prog-y += python.sh
 target-prog-y += rust.sh
@@ -33,7 +32,7 @@ endef
 
 $(foreach software, bcc binutils bpftool bpftrace cmake cuda findutils gcc \
 			gdb git gnome go hpcc java libbpf libvirt libxdp \
-			llvm lsof luca make memcached nasm qemu, \
+			llvm lsof luca make memcached nasm numactl qemu, \
   $(eval $(call test_mmp,${software})) \
 )
 
--- scripts/version/numactl.sh
+++ scripts/version/numactl.sh
@@ -11,7 +11,11 @@ fi
 # only numactl v2.0.16-47-gf117290fd85c support --version argument
 # https://github.com/numactl/numactl
 # numactl <= 2.0.14 don't has --version argument.
-${NUMACTL} --version 2>/dev/null || \
+version=$(${NUMACTL} --version 2>/dev/null || \
 	rpm -q --queryformat='%{VERSION}\n' numactl 2>/dev/null || \
 	dpkg-query -W -f='${Version}\n' numactl 2>/dev/null || \
-	false
+	false)
+
+source $(dirname $(realpath $0))/libversion.sh
+
+version_parser ${@} -- ${version}
