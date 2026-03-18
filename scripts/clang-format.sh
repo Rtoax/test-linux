b7a85c22c06790afd8bb9b054a5fde1af58f0d59
Rong Tao <rongtao@cestc.cn>
2026-03-18 15:36:26 +0800

    scripts/clang-format.sh: code style
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.8-200.fc43.x86_64, Bash 5.3.0

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -3,6 +3,7 @@
 #
 # Usage: [FCF=1] clang-format.sh
 # - FCF: Force mode of Clang-Format
+#
 set -e
 
 fatal() {
@@ -18,7 +19,7 @@ readonly GIT_TOPDIR=$(git rev-parse --show-toplevel 2>/dev/null || :)
 readonly clang_format=$(which git-clang-format 2>/dev/null || :)
 
 if  [[ -z ${clang_format} ]] && [[ -z ${FCF} ]]; then
-	fatal "Not found git-clang-format, please install or FCF=1"
+	fatal "Not found git-clang-format, please install or setenv FCF=1"
 elif [[ -z ${FCF} ]]; then
 	repository=$(basename ${GIT_TOPDIR})
 	if [[ " ostools tools ulpatch " =~ " ${repository} " ]]; then
@@ -26,13 +27,13 @@ elif [[ -z ${FCF} ]]; then
 	elif [[ " test-linux tst-linux " =~ " ${repository} " ]]; then
 		branch=origin/main
 	else
-		fatal "Unknown repository '${repository}', please develop me!"
+		fatal "Unknown repository '${repository}', please modify ${0}!"
 	fi
-	patch="$( ${clang_format} --diff ${branch} --extensions c,cpp,cu,h,hpp | \
+	c_patch="$( ${clang_format} --diff ${branch} --extensions c,cpp,cu,h,hpp | \
 			grep -v -e 'no modified files to format' \
 				-e 'clang-format did not modify any files' || :)"
-	if [[ $? != 0 ]] || [[ "${patch}" ]]; then
-		echo "${patch}"
+	if [[ $? != 0 ]] || [[ "${c_patch}" ]]; then
+		echo "${c_patch}"
 		fatal "Bad code format, please modify according to the above diff or FCF=1"
 	fi
 fi
