998547d5cf3d21faeaa7528fe66680ffd3212daf
Rong Tao <rongtao@cestc.cn>
2026-03-19 15:44:23 +0800

    python/sphinx.mk: add HAVE_PYTHON_SPHINX_RTD_THEME
    $ make MK_ALL=python/sphinx.mk DEBUG=1 NOSUBDIR=1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.8-200.fc43.x86_64, Sphinx 8.2.3, Make 4.4.1, Python 3.14.3

--- template/python/sphinx.mk
+++ template/python/sphinx.mk
@@ -3,6 +3,7 @@
 #
 # Output definitions:
 # - HAVE_PYTHON_SPHINX=[y|n]
+# - HAVE_PYTHON_SPHINX_RTD_THEME=[y|n]
 # - SPHINX_BUILD=[/usr/bin/sphinx-build]
 #
 ifndef _PYTHON_SPHINX_MK
@@ -13,9 +14,11 @@ include python/module.mk
 SPHINX_BUILD := $(shell which sphinx-build 2>/dev/null || true)
 
 export HAVE_PYTHON_SPHINX := $(call python_has_module,sphinx)
+export HAVE_PYTHON_SPHINX_RTD_THEME := $(call python_has_module,sphinx_rtd_theme)
 
 ifdef DEBUG
   $(info HAVE_PYTHON_SPHINX = ${HAVE_PYTHON_SPHINX})
+  $(info HAVE_PYTHON_SPHINX_RTD_THEME = ${HAVE_PYTHON_SPHINX_RTD_THEME})
   $(info SPHINX_BUILD = ${SPHINX_BUILD})
 endif
 
