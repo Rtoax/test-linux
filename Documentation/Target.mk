1ef58d9e9fb4374699ce1cc27db1200a9f9855bc
Rong Tao <rongtao@cestc.cn>
2026-03-20 12:33:39 +0800

    Doc: rename html to htmldocs like linux
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- Documentation/Target.mk
+++ Documentation/Target.mk
@@ -1,11 +1,11 @@
 include python/sphinx.mk
 
-.PHONY: html
-html:
+.PHONY: htmldocs
+htmldocs:
 	${Q}${SPHINX_BUILD} -M html . build --fail-on-warning
 	$(call log_success, Startup HTTP server with $$ make serve)
 
 .PHONY: serve
-serve: html
+serve: htmldocs
 	$(call log_success, Access: http://localhost:8888/)
 	${Q}python -m http.server 8888 --directory ./build/html/
