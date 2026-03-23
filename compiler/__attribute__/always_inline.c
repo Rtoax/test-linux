3ea8a66d4899dc63c8195e6fcf62706216d3181d
Rong Tao <rongtao@cestc.cn>
2026-03-23 13:54:48 +0800

    __attribute__: always_inline
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.8-200.fc43.x86_64, GCC 15.2.1, glibc 2.42

--- compiler/__attribute__/always_inline.c
+++ compiler/__attribute__/always_inline.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-static inline void __attribute__((always_inline)) foo(void)
+/* avoid sys/cdefs.h definition of '__always_inline' */
+#undef __always_inline
+#define __always_inline inline __attribute__((always_inline))
+
+static void __always_inline foo(void)
 {
 	printf("hello\n");
 }
