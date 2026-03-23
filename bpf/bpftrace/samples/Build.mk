cdc51f05835298e53cf22f0a95abdc687512905c
Rong Tao <rongtao@cestc.cn>
2026-03-23 14:47:40 +0800

    bpftrace: Looks like the BPF stack limit is exceeded
    See llvm-project: llvm/lib/Target/BPF/BPFRegisterInfo.cpp, default size
    is BPFStackSizeOption("bpf-stack-size", ..., 512).
    
    Error in bpftrace:
    
        $ sudo ./stack-limit-is-exceeded.bt
        error: <unknown>:0:0: in function tracepoint_syscalls_sys_exit_openat_1 i64 (ptr): Looks like the BPF stack limit is exceeded. Please move large on stack variables into BPF per-cpu array map. For non-kernel uses, the stack can be increased using -mllvm -bpf-stack-size.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.8-200.fc43.x86_64, Make 4.4.1, BPFTrace 0.25.0, LLVM 21.1.8, BCC 0.35.0, LibBpf 1.6.1

--- bpf/bpftrace/samples/Build.mk
+++ bpf/bpftrace/samples/Build.mk
@@ -2,6 +2,7 @@ include bpf/bpftrace.mk
 
 target-bt-y := arithmetic.bt
 target-bt-$(call bpftrace_gt,0,21,0) += getopt.bt getopt.bt.1 getopt.bt.9 getopt.bt.10
+target-bt-y += stack-limit-is-exceeded.bt
 target-bt-y += unroll.bt
 
 ARGS_getopt.bt.1 := -- --num=1
