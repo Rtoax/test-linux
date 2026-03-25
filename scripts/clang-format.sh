a1dc7e9486b4bf0db95a01e4d065a371cd97628e
Rong Tao <rongtao@cestc.cn>
2026-03-25 15:49:37 +0800

    scripts: clang-format.sh: check FCF in fatal()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.8-200.fc43.x86_64, GCC 15.2.1, glibc 2.42, Bash 5.3.0

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -10,7 +10,10 @@ fatal() {
 	echo >&2 -en "\033[31m"
 	echo >&2 -e "FATAL: "${@}
 	echo >&2 -en "\033[0m"
-	exit 1
+	if [[ -z ${FCF} ]]; then
+		echo >&2 "WARNING: skip this error with env FCF=1"
+		exit 1
+	fi
 }
 
 readonly GIT_TOPDIR=$(git rev-parse --show-toplevel 2>/dev/null || :)
@@ -18,24 +21,24 @@ readonly GIT_TOPDIR=$(git rev-parse --show-toplevel 2>/dev/null || :)
 # Check code format wich git-clang-format
 readonly clang_format=$(which git-clang-format 2>/dev/null || :)
 
-if  [[ -z ${clang_format} ]] && [[ -z ${FCF} ]]; then
-	fatal "Not found git-clang-format, please install or setenv FCF=1"
-elif [[ -z ${FCF} ]]; then
-	repository=$(basename ${GIT_TOPDIR})
-	if [[ " ostools tools ulpatch " =~ " ${repository} " ]]; then
-		branch=origin/master
-	elif [[ " test-linux tst-linux " =~ " ${repository} " ]]; then
-		branch=origin/main
-	else
-		fatal "Unknown repository '${repository}', please modify ${0}!"
-	fi
-	c_patch="$( ${clang_format} --diff ${branch} --extensions c,cpp,cu,h,hpp | \
-			grep -v -e 'no modified files to format' \
-				-e 'clang-format did not modify any files' || :)"
-	if [[ $? != 0 ]] || [[ "${c_patch}" ]]; then
-		echo "${c_patch}"
-		fatal "Bad code format, please modify according to the above diff or FCF=1"
-	fi
+if  [[ -z ${clang_format} ]]; then
+	fatal "Not found git-clang-format, please install"
+fi
+
+repository=$(basename ${GIT_TOPDIR})
+if [[ " ostools tools ulpatch " =~ " ${repository} " ]]; then
+	branch=origin/master
+elif [[ " test-linux tst-linux " =~ " ${repository} " ]]; then
+	branch=origin/main
+else
+	fatal "Unknown repository '${repository}', please modify ${0}!"
+fi
+c_patch="$( ${clang_format} --diff ${branch} --extensions c,cpp,cu,h,hpp | \
+		grep -v -e 'no modified files to format' \
+			-e 'clang-format did not modify any files' || :)"
+if [[ $? != 0 ]] || [[ "${c_patch}" ]]; then
+	echo "${c_patch}"
+	fatal "Bad code format, please modify according to the above diff"
 fi
 
 exit 0
