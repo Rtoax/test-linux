8e810989df58d94dc9831963d60256afb8fa926f
Rong Tao <rongtao@cestc.cn>
2026-03-25 18:31:22 +0800

    scripts/version: linux-firmware.sh: filter-out no-found log
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.8-200.fc43.x86_64, Linux-Firmware 20260309-1.fc43, Bash 5.3.0

--- scripts/version/linux-firmware.sh
+++ scripts/version/linux-firmware.sh
@@ -2,11 +2,13 @@
 set -e
 
 debver() {
-	dpkg-query -W -f='${Version}\n' firmware-linux-free
+	dpkg-query -W -f='${Version}\n' firmware-linux-free 2>/dev/null | \
+		grep -v 'no packages found'
 }
 
 rpmver() {
-	rpm -q --queryformat='%{version}-%{release}\n' linux-firmware 2>/dev/null
+	rpm -q --queryformat='%{version}-%{release}\n' linux-firmware 2>/dev/null | \
+		grep -v 'is not installed'
 }
 
 debver || rpmver
