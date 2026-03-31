589cc48a8bf6b2b06c3f13802ddf169207f648b4
Rong Tao <rongtao@cestc.cn>
2026-03-31 09:13:24 +0800

    ls: gpuinfo.sh: display index and name of GPU
    [rongtao@muxi-4 lingspeed]$ ./gpuinfo.sh
    NUM      NAME     PCI_SLOT         NUMA
    GPU#0    X710-M   0002:04:00.0     2
    GPU#1    X710-M   0002:05:00.0     2
    GPU#2    X710-M   0003:04:00.0     3
    GPU#3    X710-M   0003:05:00.0     3
    GPU#4    X710-M   0006:03:00.0     6
    GPU#5    X710-M   0006:09:00.0     6
    GPU#6    X710-M   0007:03:00.0     7
    GPU#7    X710-M   0007:09:00.0     7
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Lingspeed X710Mx8
    Vers: CCLinux 22.09.2, Linux 5.15.131-28.cl9_GDS.aarch64, Bash 5.1.8, LUCA 3.2.1, LLVM 14.0.6

--- hpc/cestc/lingspeed/gpuinfo.sh
+++ hpc/cestc/lingspeed/gpuinfo.sh
@@ -3,9 +3,20 @@ set -e
 
 . ../../../driver/pcie/libpcie.sh
 
-printf "%-16s %-4s\n" "PCI_SLOT" "NUMA"
-for slot in $(pcie_cestc_slots)
+# Format, Number-Name-Slot:
+# GPU#0  X710-M  0002:04:00.0
+# GPU#1  X710-M  0002:05:00.0
+N_N_S=( $(ls-smi --show-pcie | grep -E 'GPU#[0-9]+') )
+
+printf "%-8s %-8s %-16s %-4s\n" "NUM" "NAME" "PCI_SLOT" "NUMA"
+for ((i = 0; i < ${#N_N_S[@]}; i+=3))
 do
+	idx=$(( i / 3 ))
+
+	num=${N_N_S[$(( idx * 3 ))]}
+	name=${N_N_S[$(( idx * 3 + 1 ))]}
+	slot=${N_N_S[$(( idx * 3 + 2 ))]}
 	numa=$(pcie_slot_to_numa ${slot})
-	printf "%-16s %-4s\n" ${slot} ${numa}
+
+	printf "%-8s %-8s %-16s %-4s\n" ${num} ${name} ${slot} ${numa}
 done
