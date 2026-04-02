f8d374040834bd2d1560bb26dd6e798da1131b8d
Rong Tao <rongtao@cestc.cn>
2026-04-02 12:48:37 +0800

    oom.cu: add --nopf arg for disable pagefault
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: NVIDIA GB10
    Vers: Ubuntu 24.04, Linux 6.17.0-1008-nvidia, LLVM 18.1.3, CUDA 13.0.88

--- hpc/nvidia/cuda/samples/oom.cu
+++ hpc/nvidia/cuda/samples/oom.cu
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 /* Copyright (C) 2026 Rong Tao */
+#include <argp.h>
 #include <stdio.h>
 #include <cuda_runtime.h>
 #include "cuda_helpers.h"
@@ -7,6 +8,45 @@
 #define KiB 1024
 #define MiB (KiB * 1024)
 #define GiB (MiB * 1024)
+
+const char argp_prog_doc[] = "USAGE: cuda oom [-v]\n\n";
+
+static const struct argp_option opts[] = {
+	{ "nopf", 'n', NULL, 1, "Disable page fault" },
+	{},
+};
+
+struct {
+	/**
+	 * Actually, if not page-fault, the GPU Memory Usage still rise up, see
+	 * nvidia-smi.
+	 */
+	bool nopagefault;
+} env = {
+	.nopagefault = false,
+};
+
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+	switch (key) {
+	case 'n':
+		env.nopagefault = true;
+		break;
+	case ARGP_KEY_ARG:
+		break;
+	case ARGP_KEY_END:
+		break;
+	default:
+		return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static const struct argp argp = {
+	.options = opts,
+	.parser = parse_arg,
+	.doc = argp_prog_doc,
+};
 
 __global__ void kern_pagefault(void *mem, size_t sz)
 {
@@ -17,15 +57,24 @@ __global__ void kern_pagefault(void *mem, size_t sz)
 		*(char *)((char *)mem + i) = i % 26 + 'a';
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	int err;
 	void *mem;
 	size_t total = 0;
+
+	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
+	if (err) {
+		fprintf(stderr, "argp_parse return %d\n", err);
+		return -err;
+	}
 
 	for (;;) {
 		size_t blksz = MiB;
 		CUDA_RUNTIME_CHECK(cudaMalloc(&mem, blksz), break);
-		kern_pagefault<<<1, 1, 0>>>(mem, blksz);
+		if (!env.nopagefault) {
+			kern_pagefault<<<1, 1, 0>>>(mem, blksz);
+		}
 		total += blksz;
 		printf("Alloc %ld B, %ld MiB, %ld GiB\n", total, total / MiB,
 		       total / GiB);
