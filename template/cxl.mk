60519b053d305146c64bcb60e9e32420473dd15f
Rong Tao <rtoax@foxmail.com>
2026-04-09 09:09:12 +0000

    cxl.mk: add HAVE_CXL_DEVICES=y
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Vers: Fedora Linux 42, Linux 6.14.11-300.fc42_cxl.x86_64, Make 4.4.1, ndctl 82

--- template/cxl.mk
+++ template/cxl.mk
@@ -2,8 +2,17 @@
 # Copyright (C) 2026 Rong Tao
 #
 # Output definitions:
+# HAVE_CXL_DEVICES=[y|n]
 #
 ifndef _CXL_MK
 _CXL_MK = 1
+
+include define.mk
+
+$(call check_file_and_def,/sys/bus/cxl/devices,HAVE_CXL_DEVICES)
+
+ifdef DEBUG
+  $(info HAVE_CXL_DEVICES = ${HAVE_CXL_DEVICES})
+endif
 
 endif
