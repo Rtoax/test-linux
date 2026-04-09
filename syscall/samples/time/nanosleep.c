54ff4b72e79fa5d0f01acc847b010c90f4038beb
Rong Tao <rongtao@cestc.cn>
2026-04-09 20:39:32 +0800

    nanosleep(2): test timespec{-1,-2} return -1
    $ ./nanosleep
    nanosleep(tss + i, NULL) = 0
    better_sleep(1.001) = 0
    sys_nanosleep(tss + i, NULL) = 0
    nanosleep(tss + i, NULL) = -1
    better_sleep(1.001) = 0
    sys_nanosleep(tss + i, NULL) = -1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 25.10, Linux 6.17.0-20-generic, GCC 15.2.0, glibc 2.42

--- syscall/samples/time/nanosleep.c
+++ syscall/samples/time/nanosleep.c
@@ -3,19 +3,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <errno.h>
-
 #include "time_helpers.h"
-
 
 int better_sleep(double s)
 {
+	int rval = 0;
 	struct timespec ts;
 
 	ts.tv_sec = (time_t)s;
 	ts.tv_nsec = (long)((s - ts.tv_sec) * 1e+9);
 
 	while (1) {
-		int rval = nanosleep(&ts, &ts);
+		rval = nanosleep(&ts, &ts);
 		if (rval == 0)
 			return 0;
 		/* Interrupted by a signal. Try again. */
@@ -24,16 +23,21 @@ int better_sleep(double s)
 		else
 			return rval;
 	}
-	return 0;
+	return rval;
 }
 
 int main(void)
 {
-	struct timespec ts = {1, 0};
+	int i;
+	struct timespec tss[] = { { 1, 0 }, { -1, -2 } };
 
-	nanosleep(&ts, NULL);
-	better_sleep(1.001);
-	sys_nanosleep(&ts, NULL);
+#define RET(call) printf("%s = %d\n", #call, call)
+	for (i = 0; i < 2; i++) {
+		RET(nanosleep(tss + i, NULL));
+		RET(better_sleep(1.001));
+		RET(sys_nanosleep(tss + i, NULL));
+	}
+#undef RET
 
 	return 0;
 }
