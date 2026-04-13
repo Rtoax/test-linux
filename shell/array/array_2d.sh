404ab702d5f30d701a2241309a4e8d01a5490961
Rong Tao <rongtao@cestc.cn>
2026-04-13 15:34:46 +0800

    array_2d.sh: use string instead of arr[]
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.11-200.fc43.x86_64, Bash 5.3.0

--- shell/array/array_2d.sh
+++ shell/array/array_2d.sh
@@ -5,4 +5,19 @@ set -e
 declare -A skills
 
 # WARNING: not support
-# skills["Rong Tao"]+=( basketball )
+skills["Rong Tao"]+="basketball"
+skills["Rong Tao"]+=" eat"
+skills["Rong Tao"]+=" coding"
+
+skills["Xue Yuehua"]+="learn"
+skills["Xue Yuehua"]+=" eat"
+
+for name in "${!skills[@]}"
+do
+	printf "%-16s" "$name"
+	for skill in ${skills[$name]}
+	do
+		printf " <%s>" ${skill}
+	done
+	echo
+done
