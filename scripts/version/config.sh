bd462ab65e6c993df764a2e2def15dd8ee7b2e9b
Rong Tao <rongtao@cestc.cn>
2026-04-15 12:23:59 +0800

    version: config.sh: add getversion()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.11-200.fc43.x86_64, Bash 5.3.0

--- scripts/version/config.sh
+++ scripts/version/config.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 set -e
 
-common_vlens=( $(jq '.common.version.length[]' config.json) )
-common_vargs=( $(jq '.common.version.argument[]' config.json) )
-common_vseps=( $(jq '.common.version.seperator[]' config.json) )
+readonly common_vlens=( $(jq '.common.version.length[]' config.json) )
+readonly common_vargs=( $(jq '.common.version.argument[]' config.json) )
+readonly common_vseps=( $(jq '.common.version.seperator[]' config.json) )
 
-softwares=( $(jq '.software' config.json  | jq 'keys[]') )
+readonly softwares=( $(jq '.software' config.json  | jq 'keys[]') )
 
-for sw in ${softwares[@]}
-do
-	cmds=( $(jq ".software.${sw}.command[]" config.json 2>/dev/null) )
-	vargs=( $(jq ".software.${sw}.version.argument[]" config.json 2>/dev/null || true) )
-	vlens=( $(jq ".software.${sw}.version.length[]" config.json 2>/dev/null || true) )
-	vseps=( $(jq ".software.${sw}.version.seperator[]" config.json 2>/dev/null || true) )
+getversion() {
+	local sw=$1
+	local cmds=( $(jq ".software.${sw}.command[]" config.json 2>/dev/null) )
+	if [[ ${#cmds[@]} -lt 1 ]]; then
+		exit 0
+	fi
+	local vargs=( $(jq ".software.${sw}.version.argument[]" config.json 2>/dev/null || true) )
+	local vlens=( $(jq ".software.${sw}.version.length[]" config.json 2>/dev/null || true) )
+	local vseps=( $(jq ".software.${sw}.version.seperator[]" config.json 2>/dev/null || true) )
 
 	[[ ${#vargs} -eq 0 ]] && vargs=( ${common_vargs[@]} )
 	[[ ${#vlens} -eq 0 ]] && vlens=( ${common_vlens[@]} )
 	[[ ${#vseps} -eq 0 ]] && vseps=( ${common_vseps[@]} )
 
 	echo "${sw}: ${cmds[@]}, ${vargs[@]}, ${vlens[@]}, ${vseps[@]}"
+}
+
+for sw in ${softwares[@]}
+do
+	getversion ${sw}
 done
