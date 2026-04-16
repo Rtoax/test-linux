ab60c46dbc78bdedfd0db040f02070baa278ccf5
Rong Tao <rongtao@cestc.cn>
2026-04-16 09:35:24 +0800

    version: bcc.sh: use version.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.11-200.fc43.x86_64, Bash 5.3.0, BCC 0.35.0, LibBpf 1.6.1

--- scripts/version/bcc.sh
+++ scripts/version/bcc.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 set -e
 
-source $(dirname $(realpath $0))/libversion.sh
+readonly VERDIR=$(dirname $(realpath $0))
+source ${VERDIR}/libversion.sh
 
-LIBBCC_PATH=$(ldconfig_libver libbcc.so)
-
-if [[ -z ${LIBBCC_PATH} ]]; then
-	echo >&2 "ERROR: not found libbcc.so in anywhere"
-	exit 1
-fi
-
-version=$(echo ${LIBBCC_PATH} | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' 2>/dev/null || true)
+version=$(${VERDIR}/version.sh -n bcc -V)
 
 version_parser ${@} -- ${version}
