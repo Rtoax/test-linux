c849e6479072d6d692fbbab380c678c5c2830ad0
Rong Tao <rongtao@cestc.cn>
2026-04-16 19:03:46 +0800

    version: qemu: add qemu-ga
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Physical Machine
    Vers: CCLinux 22.09.2, Linux 5.15.131-28.cl9_GDS.aarch64, Bash 5.1.8, Qemu 7.0.0

--- scripts/version/qemu.sh
+++ scripts/version/qemu.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 set -e
-
-readonly WHERE_AM_I=$(dirname $(realpath $0))
-. ${WHERE_AM_I}/../libqemu.sh
-. ${WHERE_AM_I}/libversion.sh
-
-QEMU_KVM=$(get_qemu_kvm_emulator)
-if [[ -z ${QEMU_KVM} ]]; then
-	echo >&2 "ERROR: not found QEMU in your system"
-	exit 0
-fi
-
-version=$(${QEMU_KVM} --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' | head -1)
-
-version_parser ${@} -- ${version}
+$(dirname $(realpath $0))/version.sh -n qemu -V -- ${@}
