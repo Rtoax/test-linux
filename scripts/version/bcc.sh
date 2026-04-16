1697fef9504eb8c29bceca5796a008ac68e52442
Rong Tao <rongtao@cestc.cn>
2026-04-16 18:34:16 +0800

    version.sh: apply to git/hooks/prepare-commit-msg
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.12-200.fc43.x86_64, Bash 5.3.0, Git 2.53.0, BCC 0.35.0, Binutils 2.45.1, Python 3.14.3

--- scripts/version/bcc.sh
+++ scripts/version/bcc.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e
-readonly VERDIR=$(dirname $(realpath $0))
-${VERDIR}/version.sh -n bcc -V -- ${@}
+$(dirname $(realpath $0))/version.sh -n bcc -V -- ${@}
--- scripts/version/python.sh
+++ scripts/version/python.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 set -e
-
-[[ -z ${PYTHON} ]] && PYTHON=$(which python 2>/dev/null || true)
-[[ -z ${PYTHON} ]] && PYTHON=$(which python3 2>/dev/null || true)
-
-if [[ -z ${PYTHON} ]]; then
-	echo >&2 "ERROR: not found python in anywhere"
-	exit 1
-fi
-
-version=$(${PYTHON} --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' 2>/dev/null || true)
-
-source $(dirname $(realpath $0))/libversion.sh
-
-version_parser ${@} -- ${version}
+$(dirname $(realpath $0))/version.sh -n python -V -- ${@}
