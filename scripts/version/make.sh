255e658b2be2fc599761cccd4e2351815fbe6242
Rong Tao <rongtao@cestc.cn>
2026-04-17 11:21:42 +0800

    version: symlinks
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.12-200.fc43.x86_64, Bash 5.3.0, Git 2.53.0, Make 4.4.1, Memcached 1.6.39

--- scripts/version/make.sh
+++ scripts/version/make.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-set -e
-make=$(which make 2>/dev/null || :)
-if [[ -z ${make} ]]; then
-	echo >&2 "ERROR: not found make in your system"
-	exit 0
-fi
-
-version=$( ${make} --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' 2>/dev/null || true )
-if [[ -z ${version} ]]; then
-	version=$( ${make} --version | grep -Eo '[0-9]+\.[0-9]+' 2>/dev/null || true )
-fi
-
-source $(dirname $(realpath $0))/libversion.sh
-
-version_parser ${@} -- ${version}
+version.sh
--- scripts/version/memcached.sh
+++ scripts/version/memcached.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-set -e
-MEMCACHED=$(which memcached 2>/dev/null || :)
-if [[ -z ${MEMCACHED} ]]; then
-	echo >&2 "ERROR: not found MEMCACHED in your system"
-	exit 0
-fi
-
-version=$(${MEMCACHED} --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' 2>/dev/null || true)
-
-source $(dirname $(realpath $0))/libversion.sh
-
-version_parser ${@} -- ${version}
+version.sh
