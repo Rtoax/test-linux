eced1354325acdcdc23eea3f1103177da83c560c
Rong Tao <rongtao@cestc.cn>
2026-04-17 14:53:17 +0800

    version: redis.sh: use version.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.12-200.fc43.x86_64, Redis(Valkey) 8.1.6, Redis 8.1.6, bash 5.3.0

--- scripts/version/redis.sh
+++ scripts/version/redis.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
-REDIS_CLI=$(which redis-cli 2>/dev/null || :)
 REDIS_SERVER=$(which redis-server 2>/dev/null || :)
-if [[ -z ${REDIS_CLI} ]]; then
-	echo >&2 "ERROR: not found REDIS_CLI in your system"
+if [[ -z ${REDIS_SERVER} ]]; then
+	echo >&2 "ERROR: not found redis-server in your system"
 	exit 0
 fi
 
@@ -14,9 +13,5 @@ if [[ $1 == --name ]] || [[ $1 == --pretty-name ]]; then
 		echo "Redis"
 	fi
 else
-	version=$(${REDIS_CLI} --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' 2>/dev/null || true)
-
-	source $(dirname $(realpath $0))/libversion.sh
-
-	version_parser ${@} -- ${version}
+	$(dirname $(realpath $0))/version.sh -n redis -V -- ${@}
 fi
