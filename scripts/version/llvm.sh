53305d712ae34e9dc992a218a60dbe86fe796ce5
Rong Tao <rongtao@cestc.cn>
2026-04-17 11:18:40 +0800

    version: symlink llvm.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.12-200.fc43.x86_64, Bash 5.3.0, Git 2.53.0, LLVM 21.1.8, Lsof 4.98.0

--- scripts/version/llvm.sh
+++ scripts/version/llvm.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-set -e
-version=$(llvm-config --version 2>/dev/null || {
-	llvm-objdump --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' 2>/dev/null
-	}
-)
-
-source $(dirname $(realpath $0))/libversion.sh
-
-version_parser ${@} -- ${version}
+version.sh
--- scripts/version/lsof.sh
+++ scripts/version/lsof.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-set -e
-LSOF=$(which lsof 2>/dev/null || :)
-if [[ -z ${LSOF} ]]; then
-	echo >&2 "ERROR: not found LSOF in your system"
-	exit 0
-fi
-version=$(${LSOF} -v 2>&1 | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' 2>/dev/null || true)
-
-source $(dirname $(realpath $0))/libversion.sh
-
-version_parser ${@} -- ${version}
+version.sh
