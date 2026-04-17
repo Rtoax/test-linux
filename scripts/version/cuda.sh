57c416b5c0ce440f5ba8f80af759fb7f17691fa2
Rong Tao <rongtao@cestc.cn>
2026-04-17 09:57:21 +0800

    version: swap to symlinks
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.12-200.fc43.x86_64, Bash 5.3.0, Git 2.53.0, LLVM 21.1.8, ROCm 6.4.43484, BCC 0.35.0, Binutils 2.45.1, BPFTool 7.7.0, BPFTrace 0.25.0, Capstone 5.0.5, CMake 3.31.11, CUDA 13.0.48, Dracut 107-8, ETHTool 6.19, FFmeg 7.1.2, Findutils 4.10.0

--- scripts/version/cuda.sh
+++ scripts/version/cuda.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-set -e
-$(dirname $(realpath $0))/version.sh -n cuda -V -- ${@}
+version.sh
--- scripts/version/ethtool.sh
+++ scripts/version/ethtool.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-set -e
-ETHTOOL=$(which ethtool 2>/dev/null || :)
-
-if [[ -z ${ETHTOOL} ]]; then
-	echo >&2 "ERROR: not found ETHTOOL in your system"
-	exit 0
-fi
-version=$(${ETHTOOL} --version | grep -Eo '[0-9]+\.[0-9]+' 2>/dev/null | head -1 || true)
-
-source $(dirname $(realpath $0))/libversion.sh
-
-version_parser ${@} -- ${version}
+version.sh
