83a66b9beb42c33cf35d28412d429dcc28898089
Rong Tao <rongtao@cestc.cn>
2026-04-17 09:42:12 +0800

    version: symlink bpftool
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.12-200.fc43.x86_64, Bash 5.3.0, Make 4.4.1, LibBpf 1.6.1, BPFTool 7.7.0

--- template/bpf/bpftool.mk
+++ template/bpf/bpftool.mk
@@ -18,7 +18,7 @@ ifeq ($(BPFTOOL),)
   export HAVE_BPFTOOL := n
 else
 
-versh := ${TOPDIR}/scripts/version/bpftool.sh
+versh := ${TOPDIR}/scripts/version/bpftool
 
 BPFTOOL_VERSION := $(shell ${versh})
 BPFTOOL_MAJOR := $(shell ${versh} --major)
