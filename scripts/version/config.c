d550c23744ee777388673f7e14cef56ce611bfed
Rong Tao <rongtao@cestc.cn>
2026-04-22 11:23:07 +0800

    version: config.c: parse config.json common
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.12-200.fc43.x86_64, GCC 15.2.1, GLibc 2.42, json-c 5.4.0

--- scripts/version/config.c
+++ scripts/version/config.c
@@ -1,7 +1,10 @@
 #include <argp.h>
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <json-c/json.h>
+#include "file.h"
 
 static char *json = NULL;
 static bool verbose = false;
@@ -39,6 +42,69 @@ static const struct argp argp = {
 	.doc = argp_prog_doc,
 };
 
+struct version_format {
+	const char *seperator;
+	const char *major, *minor, *patch;
+};
+
+struct json {
+	const char *version;
+	struct {
+		struct {
+			char length[16];
+			struct {
+				const char *argument[16];
+			} command;
+			struct version_format format;
+		} version;
+	} common;
+};
+
+int json_common(struct json *j, json_object *c)
+{
+	int len;
+	json_object *version, *length, *command, *format;
+
+	json_object_object_get_ex(c, "version", &version);
+
+	json_object_object_get_ex(version, "length", &length);
+	assert(json_object_get_type(length) == json_type_array);
+
+	len = json_object_array_length(length);
+	for (int i = 0; i < len; i++) {
+		json_object *elem = json_object_array_get_idx(length, i);
+		assert(json_object_get_type(elem) == json_type_int);
+
+		int val = json_object_get_int(elem);
+		j->common.version.length[i] = val;
+	}
+
+	json_object_object_get_ex(version, "command", &command);
+	json_object *argument;
+	json_object_object_get_ex(command, "argument", &argument);
+	len = json_object_array_length(argument);
+	for (int i = 0; i < len; i++) {
+		json_object *elem = json_object_array_get_idx(argument, i);
+		assert(json_object_get_type(elem) == json_type_string);
+
+		j->common.version.command.argument[i] =
+			json_object_get_string(elem);
+	}
+
+	json_object_object_get_ex(version, "format", &format);
+	json_object *seperator, *major, *minor, *patch;
+	json_object_object_get_ex(format, "seperator", &seperator);
+	j->common.version.format.seperator = json_object_get_string(seperator);
+	json_object_object_get_ex(format, "major", &major);
+	j->common.version.format.major = json_object_get_string(major);
+	json_object_object_get_ex(format, "minor", &minor);
+	j->common.version.format.minor = json_object_get_string(minor);
+	json_object_object_get_ex(format, "patch", &patch);
+	j->common.version.format.patch = json_object_get_string(patch);
+
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	int err;
@@ -53,5 +119,47 @@ int main(int argc, char *argv[])
 		return -ENOENT;
 	}
 
+	char *content = alloc_buf_read_file(json);
+	json_object *root, *version, *common, *software;
+
+	struct json *j = malloc(sizeof(struct json));
+	memset(j, 0, sizeof(struct json));
+
+	root = json_tokener_parse(content);
+
+	/* version */
+	json_object_object_get_ex(root, "version", &version);
+	j->version = json_object_get_string(version);
+
+	/* common */
+	json_object_object_get_ex(root, "common", &common);
+	json_common(j, common);
+
+	/* software */
+	json_object_object_get_ex(root, "software", &software);
+
+	if (verbose) {
+		fprintf(stderr, "version %s\n", j->version);
+		for (int i = 0; j->common.version.length[i]; i++) {
+			fprintf(stderr, "common.version.length[%d] %d\n", i,
+				j->common.version.length[i]);
+		}
+		for (int i = 0; j->common.version.command.argument[i]; i++) {
+			fprintf(stderr,
+				"common.version.command.argument[%d] %s\n", i,
+				j->common.version.command.argument[i]);
+		}
+		fprintf(stderr, "common.version.format.seperator %s\n",
+			j->common.version.format.seperator);
+		fprintf(stderr, "common.version.format.major %s\n",
+			j->common.version.format.major);
+		fprintf(stderr, "common.version.format.minor %s\n",
+			j->common.version.format.minor);
+		fprintf(stderr, "common.version.format.patch %s\n",
+			j->common.version.format.patch);
+	}
+
+	free(j);
+	free(content);
 	return 0;
 }
