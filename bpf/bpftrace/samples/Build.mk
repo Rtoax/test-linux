491f54e8b578c06165c9b86d4360e53f6da1f82f
Rong Tao <rongtao@cestc.cn>
2026-04-23 18:34:45 +0800

    bpftrace/samples: add pcomm.bt
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.13-200.fc43.x86_64, BPFTrace 0.25.0, LibBpf 1.6.1, Make 4.4.1

--- bpf/bpftrace/samples/Build.mk
+++ bpf/bpftrace/samples/Build.mk
@@ -3,6 +3,7 @@ include bpf/bpftrace.mk
 target-bt-y := arithmetic.bt
 target-bt-$(call bpftrace_gt,0,21,0) += getopt.bt getopt.bt.1 getopt.bt.9 getopt.bt.10
 target-bt-y += parent.bt
+target-bt-y += pcomm.bt
 target-bt-y += stack-limit-is-exceeded.bt
 target-bt-y += return.bt
 target-bt-y += unroll.bt
