a0b52656af46ae6f3573c9d9886a7a574fadf499
Rong Tao <rongtao@cestc.cn>
2026-04-23 17:04:02 +0800

    update copyright format
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, x86_64, Physical Machine
    Vers: Fedora Linux 43, Linux 6.19.12-200.fc43.x86_64, BPFTrace 0.25.0, Python 3.14.4

--- cgroup/v1/devices/devices.deny/devdeny.py
+++ cgroup/v1/devices/devices.deny/devdeny.py
@@ -2,7 +2,7 @@
 #
 # devdeny.py - check cgroup-v1 devices.deny
 #
-# Copyright (C) Rong Tao 2022-2025
+# Copyright (C) 2022-2026 Rong Tao
 #
 # 2023-08-17    Rong Tao    Create this.
 # 2023-08-18    Rong Tao    Apply log level for journal.
