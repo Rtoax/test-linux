2356873af2a73e994328366ebdecb31c4cbb116f
Rong Tao <rongtao@cestc.cn>
2026-04-24 18:16:48 +0800

    glibc: define __MY_GLIBC_PREREQ(maj, min) to test __GLIBC_PREREQ(maj, min)
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.13-200.fc43.x86_64, GCC 15.2.1, GLibc 2.42

--- glibc/gnu_get_libc_version.c
+++ glibc/gnu_get_libc_version.c
@@ -4,7 +4,15 @@
  * char *__libc_release;
  */
 #include <stdio.h>
+#include <features.h>
 #include <gnu/libc-version.h>
+
+#define __MY_GLIBC_PREREQ(maj, min) \
+	((__GLIBC__ << 16) + __GLIBC_MINOR__ >= ((maj) << 16) + (min))
+
+#if __MY_GLIBC_PREREQ(22, 22) != __GLIBC_PREREQ(22, 22)
+#error "Glibc __GLIBC_PREREQ() macro has been changed"
+#endif
 
 #if defined(__GLIBC__) && defined(__GLIBC_PREREQ)
 # if __GLIBC_PREREQ(2, 16)
