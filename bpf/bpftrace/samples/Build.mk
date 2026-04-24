24a37825651f5c6549a4f766fb9685e8c4982f8c
Rong Tao <rongtao@cestc.cn>
2026-04-24 18:10:19 +0800

    bpftrace/samples: adapt to 0.20.2
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 24.04, Linux 6.8.0-110-generic, BPFTrace 0.20.2, LibBpf 1.3.0, Make 4.3

--- bpf/bpftrace/samples/Build.mk
+++ bpf/bpftrace/samples/Build.mk
@@ -2,12 +2,13 @@ include bpf/bpftrace.mk
 
 target-bt-y := arithmetic.bt
 target-bt-y += begin.bt
+target-bt-$(call bpftrace_gt,0,20,2) += begin-multi.bt
 target-bt-$(call bpftrace_gt,0,21,0) += getopt.bt getopt.bt.1 getopt.bt.9 getopt.bt.10
-target-bt-y += import.bt
+target-bt-$(call bpftrace_gt,0,20,2) += import.bt
 target-bt-y += parent.bt
-target-bt-y += pcomm.bt
-target-bt-y += stack-limit-is-exceeded.bt
-target-bt-y += return.bt
+target-bt-$(call bpftrace_gt,0,20,2) += pcomm.bt
+target-bt-$(call bpftrace_gt,0,20,2) += stack-limit-is-exceeded.bt
+target-bt-$(call bpftrace_gt,0,20,2) += return.bt
 target-bt-y += unroll.bt
 
 ARGS_getopt.bt.1 := -- --num=1
