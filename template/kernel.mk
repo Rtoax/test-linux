980d347abad81f864e668b6458da334c6f21ded3
Rong Tao <rongtao@cestc.cn>
2026-04-27 13:46:25 +0800

    kernel.mk: apply define_version() of version.mk
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.13-200.fc43.x86_64, Make 4.4.1

--- template/kernel.mk
+++ template/kernel.mk
@@ -17,14 +17,8 @@
 # - KFLAGS=
 #
 # Functions:
-# - kver_gt()=[y|n]
-# - kver_ge()=[y|n]
-# - kver_eq()=[y|n]
-# - kver_lt()=[y|n]
-# - kver_le()=[y|n]
-# - kver_uapi_gt()=[y|n]
-# - kver_uapi_eq()=[y|n]
-# - kver_uapi_lt()=[y|n]
+# - kver_{gt,ge,eq,lt,le}()=[y|n]
+# - kver_uapi_{gt,ge,eq,lt,le}()=[y|n]
 #
 ifndef _KERNEL_MK
 _KERNEL_MK = 1
@@ -50,30 +44,8 @@ KFLAGS += -DKVERSION=$(KVERSION)
 KFLAGS += -DKPATCHLEVEL=$(KPATCHLEVEL)
 KFLAGS += -DKSUBLEVEL=$(KSUBLEVEL)
 
-define kver_gt
-$(call version3_code1688_cmp,${KVERSION_CODE},-gt,${1},${2},${3})
-endef
-define kver_ge
-$(call version3_code1688_cmp,${KVERSION_CODE},-ge,${1},${2},${3})
-endef
-define kver_eq
-$(call version3_code1688_cmp,${KVERSION_CODE},-eq,${1},${2},${3})
-endef
-define kver_lt
-$(call version3_code1688_cmp,${KVERSION_CODE},-lt,${1},${2},${3})
-endef
-define kver_le
-$(call version3_code1688_cmp,${KVERSION_CODE},-le,${1},${2},${3})
-endef
-define kver_uapi_gt
-$(call version3_code1688_cmp,${KUAPIVERSION_CODE},-gt,${1},${2},${3})
-endef
-define kver_uapi_eq
-$(call version3_code1688_cmp,${KUAPIVERSION_CODE},-eq,${1},${2},${3})
-endef
-define kver_uapi_lt
-$(call version3_code1688_cmp,${KUAPIVERSION_CODE},-lt,${1},${2},${3})
-endef
+$(eval $(call define_version,kver,version3_code1688,${KVERSION},${KPATCHLEVEL},${KSUBLEVEL}))
+$(eval $(call define_version,kver_uapi,version3_code1688,${KUAPIVERSION},${KUAPIPATCHLEVEL},${KUAPISUBLEVEL}))
 
 export KVERSION KPATCHLEVEL KSUBLEVEL KVERSION_CODE
 export KUAPIVERSION KUAPIPATCHLEVEL KUAPISUBLEVEL KUAPIVERSION_CODE
@@ -83,20 +55,8 @@ ifdef DEBUG
   $(info KVERSION = ${KVERSION}.${KPATCHLEVEL}.${KSUBLEVEL}, CODE ${KVERSION_CODE})
   $(info KUAPIVERSION = ${KUAPIVERSION}.${KUAPIPATCHLEVEL}.${KUAPISUBLEVEL}, CODE ${KUAPIVERSION_CODE})
   $(info KFLAGS = ${KFLAGS})
-  $(info version3_code1688(${KVERSION},${KPATCHLEVEL},${KSUBLEVEL}) = \
-         $(call version3_code1688,${KVERSION},${KPATCHLEVEL},${KSUBLEVEL}))
-  $(info kver_gt(1,1,1) = $(call kver_gt,1,1,1))
-  $(info kver_eq(${KVERSION},${KPATCHLEVEL},${KSUBLEVEL}) = \
-         $(call kver_eq,${KVERSION},${KPATCHLEVEL},${KSUBLEVEL}))
-  $(info kver_lt(7,0,0) = $(call kver_lt,7,0,0))
 endif
 
-ifneq (${KVERSION_CODE},$(call version3_code1688,${KVERSION},${KPATCHLEVEL},${KSUBLEVEL}))
-  $(error "Bad KVERSION_CODE ${KVERSION_CODE}")
-endif
-ifneq (${KUAPIVERSION_CODE},$(call version3_code1688,${KUAPIVERSION},${KUAPIPATCHLEVEL},${KUAPISUBLEVEL}))
-  $(error "Bad KUAPIVERSION_CODE ${KUAPIVERSION_CODE}")
-endif
 # no body use linux v1.1.1 i think
 ifneq ($(call kver_gt,1,1,1),y)
   $(error "call kver_gt failed")
@@ -104,18 +64,18 @@ endif
 ifneq ($(call kver_uapi_gt,1,1,1),y)
   $(error "call kver_uapi_gt failed")
 endif
-ifneq ($(call kver_eq,${KVERSION},${KPATCHLEVEL},${KSUBLEVEL}),y)
-  $(error "call kver_eq failed")
-endif
-ifneq ($(call kver_uapi_eq,${KUAPIVERSION},${KUAPIPATCHLEVEL},${KUAPISUBLEVEL}),y)
-  $(error "call kver_uapi_eq failed")
-endif
 # newest kernel is v7.0 right now
 ifneq ($(call kver_lt,7,1,0),y)
   $(error "call kver_lt failed")
 endif
 ifneq ($(call kver_uapi_lt,7,0,0),y)
   $(error "call kver_uapi_lt failed")
+endif
+ifneq (${KVERSION_CODE},${kver_VERSION_CODE})
+  $(error define_version failed for kver)
+endif
+ifneq (${KUAPIVERSION_CODE},${kver_uapi_VERSION_CODE})
+  $(error define_version failed for kver_uapi)
 endif
 
 endif
