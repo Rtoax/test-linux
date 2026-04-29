2d6b27dafba6c233e49bc3a9a3b39f506bd75389
Rong Tao <rongtao@cestc.cn>
2026-04-29 16:59:54 +0800

    bash-completion: features.sh: support list and fix error
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 43, Linux 6.19.14-200.fc43.x86_64, bash 5.3.0

--- shell/bash-completion/features.sh
+++ shell/bash-completion/features.sh
@@ -1,34 +1,55 @@
 #!/bin/bash
 set -e
 
-# --no-feature feature1,feature2,...
-features=( kprobe_multi kprobe_session uprobe_multi )
-prev=--no-feature
-
 comp() {
+	echo >&2 "------------ ${@} -------------"
+	COMPREPLY=()
+
 	local cur=${1}
-	if [[ -z ${cur} ]]; then
-		COMPREPLY=( $(compgen -W "${features[*]}" -- ${cur}) )
-	else
+	# --no-feature feature1,feature2,...
+	local features=( kprobe_multi kprobe_session uprobe_multi )
+	local prev="--no-feature"
+
+	if [[ ${cur} ]]; then
+		local prevfeatures=( $(echo ${cur} | tr ',' ' ') )
+		local leftfeatures=( )
+
+		for pf in ${features[@]}
+		do
+			if [[ ! " ${prevfeatures[@]} " =~ " ${pf} " ]]; then
+				leftfeatures+=( ${pf} )
+			fi
+		done
+
+		# if cur end with ',', just complete left features
 		if [[ ${cur:${#cur}-1} == , ]]; then
-			local prevfeatures=( $(echo ${cur} | tr ',' ' ') )
-			local leftfeatures=( )
-			for pf in ${features[@]}
-			do
-				if [[ ! " ${prevfeatures[@]} " =~ " ${pf} " ]]; then
-					leftfeatures+=( ${pf} )
-				fi
-			done
 			local hints=( )
 			for f in ${leftfeatures[@]}
 			do
 				hints+=( ${cur}${f} )
 			done
 			COMPREPLY=( $(compgen -W "${hints[*]}" -- ${cur}) )
-		else
-			return 0
+		# only one feature and cur not end with ','
+		elif [[ ${#prevfeatures[@]} -eq 1 ]]; then
+			COMPREPLY=( $(compgen -W "${features[*]}" -- ${cur}) )
+		# for example: 'kprobe_multi,kprobe_'
+		elif [[ ${#prevfeatures[@]} -gt 1 ]]; then
+			local lastfeature=${prevfeatures[-1]}
+			local hints=( )
+			for f in ${leftfeatures[@]}
+			do
+				if [[ " ${f} " =~ " ${lastfeature}" ]]; then
+					hints+=( ${cur:0:-${#lastfeature}}${f} )
+				fi
+			done
+			if [[ ${hints} ]]; then
+				COMPREPLY=( $(compgen -W "${hints[*]}" -- ${cur}) )
+			fi
 		fi
+	else
+		COMPREPLY=( $(compgen -W "${features[*]}" -- ${cur}) )
 	fi
+
 	echo ${COMPREPLY[@]}
 	return 0
 }
@@ -41,3 +62,6 @@ comp u
 comp uprobe_multi,
 comp uprobe_multi,kprobe_multi,
 comp kprobe_multi,uprobe_multi,kprobe_session
+comp kprobe_multi,uprobe_
+comp uprobe_multi,k
+comp uprobe_multi,kprobe_m
