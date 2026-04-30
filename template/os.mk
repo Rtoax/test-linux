b08a640a896ae8b0691adfa83c29bb026e25cbef
Rong Tao <rongtao@cestc.cn>
2026-04-30 15:01:36 +0800

    os.mk: use -D__${OS_ID}__ instead of -D__fedora__
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 6.19.14-300.fc44.x86_64, Make 4.4.1

--- template/os.mk
+++ template/os.mk
@@ -15,13 +15,8 @@
 # - __os_minor__=[0|04]
 #
 # - OS_CFLAGS+=-DOS_ID=${OS_ID}
-#   OS_CFLAGS+=-D__centos__=1
-#   OS_CFLAGS+=-D__fedora__=1
-#   OS_CFLAGS+=-D__ubuntu__=1
-#   OS_CFLAGS+=-D__debian__=1
-#   OS_CFLAGS+=-D__cclinux__=1
-#   OS_CFLAGS+=-D__kylin__=1
 #   OS_CFLAGS+=-DOS_VERSION_ID=${OS_VERSION_ID}
+#   OS_CFLAGS+=-D__${OS_ID}__=1
 #   OS_CFLAGS+=-D__os_major__=[43|24]
 #   OS_CFLAGS+=-D__os_minor__=[|04]
 #      C source code should use OS_ID like:
@@ -38,7 +33,15 @@ get_distr_info = $(patsubst "%",%,$(shell grep $(1) /etc/os-release 2>/dev/null 
 					awk -F'=' '{print $$2}'))
 
 OS_ID := $(call get_distr_info, '^ID=')
+ifeq ($(OS_ID),)
+  $(error Not found ID in /etc/os-release)
+endif
+
 OS_VERSION_ID := $(call get_distr_info, '^VERSION_ID=')
+ifeq ($(OS_VERSION_ID),)
+  $(error Not found VERSION_ID in /etc/os-release)
+endif
+
 DEBIAN_VERSION_FULL := $(call get_distr_info, '^DEBIAN_VERSION_FULL=')
 
 __os_major__ := $(shell echo ${OS_VERSION_ID} | awk -F '.' '{print $$1}')
@@ -52,34 +55,16 @@ ifeq (${__os_minor__},)
   __os_minor__ := 0
 endif
 
-ifeq ($(OS_VERSION_ID),)
-  $(error Not found VERSION_ID in /etc/os-release)
-endif
+# $1: distrobution name, like fedora
+define define_os
+  $(eval OS_CFLAGS += -D__${1}__=1)
+  $(eval export __${1}__ = 1)
+endef
 
 OS_CFLAGS += -DOS_ID=${OS_ID}
-ifeq (${OS_ID},fedora)
-  OS_CFLAGS := -D__fedora__=1
-  export __fedora__ = 1
-else ifeq (${OS_ID},ubuntu)
-  OS_CFLAGS := -D__ubuntu__=1
-  export __ubuntu__ = 1
-else ifeq (${OS_ID},centos)
-  OS_CFLAGS := -D__centos__=1
-  export __centos__ = 1
-else ifeq (${OS_ID},debian)
-  OS_CFLAGS := -D__debian__=1
-  export __debian__ = 1
-else ifeq (${OS_ID},cclinux)
-  OS_CFLAGS := -D__cclinux__=1
-  export __cclinux__ = 1
-else ifeq (${OS_ID},kylin)
-  OS_CFLAGS := -D__kylin__=1
-  export __kylin__ = 1
-else
-  $(error "Not support ${OS_ID}, please add it to template/os.mk")
-endif
-
 OS_CFLAGS += -DOS_VERSION_ID=${OS_VERSION_ID}
+
+$(call define_os,${OS_ID})
 OS_CFLAGS += -D__os_major__=${__os_major__}
 OS_CFLAGS += -D__os_minor__=${__os_minor__}
 
