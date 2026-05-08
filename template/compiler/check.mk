443ad19b7e1500b6aa7285a6dcc873bde53fee2a
Rong Tao <rongtao@cestc.cn>
2026-05-08 17:10:36 +0800

    template/compiler/check.mk: uniq check_compiler_option()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 6.19.14-300.fc44.x86_64, Clang 22.1.4, GCC 16.1.1, Make 4.4.1, bash 5.3.9

--- template/compiler/check.mk
+++ template/compiler/check.mk
@@ -5,12 +5,12 @@
 # - CC=[cc]
 #
 # Functions:
-# - check_compiler_option()=[y]
 # - check_compiler_option_noS()=[y]
-# - check_compiler_support_type()=[y]
-# - check_compiler_support_header()=[y]
+# - check_compiler_option()=[y]
 # - check_clang_option()=[y]
 # - check_gcc_option()=[y]
+# - check_compiler_support_type()=[y]
+# - check_compiler_support_header()=[y]
 #
 ifndef _COMPILER_CHECK_MK
 _COMPILER_CHECK_MK = 1
@@ -18,23 +18,27 @@ _COMPILER_CHECK_MK = 1
 # Check compiler support option or not
 # $(1) - compiler, for example: gcc, clang, etc.
 # $(2) - compile option, for example: -flto
-define check_compiler_option
-$(shell echo 'int main(void) { return 0; }' | \
-  $(1) -x c -Wall - $(2) -S -o /dev/null >/dev/null 2>&1 \
+define check_compiler_option_noS
+$(shell printf 'int main(void) { return 0; }' | \
+  $(1) -x c -Wall - $(2) -o /dev/null >/dev/null 2>&1 \
     && echo y)
 endef
 
-define check_compiler_option_noS
-$(shell echo 'int main(void) { return 0; }' | \
-  $(1) -x c -Wall - $(2) -o /dev/null >/dev/null 2>&1 \
-    && echo y)
+define check_compiler_option
+$(call check_compiler_option_noS,${1},${2} -S)
+endef
+define check_clang_option
+$(call check_compiler_option,clang,$(1))
+endef
+define check_gcc_option
+$(call check_compiler_option,gcc,$(1))
 endef
 
 # Check compiler support type
 # $(1) - compiler, gcc, clang, etc.
 # $(2) - type name, such as _Float16, float, etc.
 define check_compiler_support_type
-$(shell echo 'int main(void) { $(2) v; return 0; }' | \
+$(shell printf 'int main(void) { $(2) v; return 0; }' | \
 	$(1) -x c -Werror - -o /dev/null 2>/dev/null && echo y)
 endef
 
@@ -42,17 +46,13 @@ endef
 # $(1) - compiler, gcc, clang, etc.
 # $(2) - header name, like quadmath.h
 define check_compiler_support_header
-$(shell echo -e '#include <$(2)> \nint main(void) { return 0; }' | \
+$(shell printf '#include <$(2)>\nint main(void) { return 0; }' | \
 	$(1) -x c -Werror - -o /dev/null 2>/dev/null && echo y)
 endef
 
-define check_clang_option
-$(call check_compiler_option,clang,$(1))
-endef
-
-define check_gcc_option
-$(call check_compiler_option,gcc,$(1))
-endef
+ifneq ($(call check_compiler_option,${CC},),y)
+  $(error check_compiler_option failed)
+endif
 
 ifneq ($(call check_compiler_support_type,${CC},int),y)
   $(error "${CC} not support int type!!")
