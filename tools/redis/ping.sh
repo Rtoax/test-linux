2aa71c27599acac16f7a42d4b2ae44918cbb213f
Rong Tao <rongtao@cestc.cn>
2026-05-09 08:41:24 +0800

    redis: ping.sh: check redis-server exist or not
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 6.19.14-300.fc44.x86_64, Redis(Valkey) 9.0.3, Redis 9.0.3, bash 5.3.9

--- tools/redis/ping.sh
+++ tools/redis/ping.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 set -e
+
+if [[ -z "$(sudo systemctl status redis.service | \
+		grep -o 'Active: active' 2>/dev/null)" ]] ||
+   [[ -z "$(pidof valkey-server)$(pidof redis-server)" ]]; then
+	echo "WARNING: Not found redis server, skipping"
+	exit 0
+fi
+
 redis-cli ping
