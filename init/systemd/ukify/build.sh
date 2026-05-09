0691a372396950f6b305a17be45cfceb81457d93
Rong Tao <rongtao@cestc.cn>
2026-05-09 14:31:03 +0800

    systemd/ukify: build.sh: adapt to debian
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Debian GNU/Linux 13.4, Linux 6.12.74+deb13+1-amd64, Dracut 106-6, Make 4.4.1, bash 5.2.37, Systemd 257.9

--- init/systemd/ukify/build.sh
+++ init/systemd/ukify/build.sh
@@ -6,7 +6,11 @@ build() {
 	sudo ukify build "${@}"
 }
 
+vmlinuz=/boot/vmlinuz-$(uname -r)
+initrd=/boot/initramfs-$(uname -r).img
+test ! -f ${initrd} && initrd=/boot/initrd.img-$(uname -r)
+
 # Example 1. Minimal invocation
-build --linux=/boot/vmlinuz-$(uname -r) \
-	--initrd=/boot/initramfs-$(uname -r).img \
+build --linux=${vmlinuz} \
+	--initrd=${initrd} \
 	--cmdline="systemd.volatile=overlay"
