0710c3e60f75e82ede60611e589fa436702de1ae
Rong Tao <rongtao@cestc.cn>
2026-05-11 12:31:11 +0800

    qemu-nbd.sh: add find_valid_nbd_dev()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.4-200.fc44.x86_64, Qemu 10.2.2, bash 5.3.9

--- qemu/qemu-nbd.sh
+++ qemu/qemu-nbd.sh
@@ -4,16 +4,30 @@ set -ex
 img_type=qcow2
 img_name=test.${img_type}
 
+find_valid_nbd_dev() {
+	local nbd nbds=( $(ls /sys/block/ | grep nbd) )
+	for nbd in ${nbds[@]}
+	do
+		# Found valid nbd
+		if [[ ! -e /sys/block/${nbd}/pid ]]; then
+			echo ${nbd}
+		fi
+	done
+	# Not found valid nbd
+}
+
 sudo modprobe nbd max_part=16
 lsmod | grep nbd
 
+nbd_dev=$(find_valid_nbd_dev)
+
 # Create image file and connect
 sudo qemu-img create -f qcow2 ${img_name} 100G
-sudo qemu-nbd --connect /dev/nbd0 ${img_name} -f ${img_type}
-sudo fdisk -l /dev/nbd0
+sudo qemu-nbd --connect /dev/${nbd_dev} ${img_name} -f ${img_type}
+sudo fdisk -l /dev/${nbd_dev}
 
 # Make partitions
-sudo fdisk -l /dev/nbd0
+sudo fdisk -l /dev/${nbd_dev}
 cat >tmp.txt<<-EOF
 n
 p
@@ -27,23 +41,23 @@ p
 
 w
 EOF
-sudo fdisk /dev/nbd0 < tmp.txt
+sudo fdisk /dev/${nbd_dev} < tmp.txt
 rm tmp.txt
-sudo fdisk -l /dev/nbd0
+sudo fdisk -l /dev/${nbd_dev}
 
 # Create filesystem for 2 partitions
-sudo mkfs.fat /dev/nbd0p1
+sudo mkfs.fat /dev/${nbd_dev}p1
 mkdir fat.out
-sudo mount /dev/nbd0p1 fat.out
+sudo mount /dev/${nbd_dev}p1 fat.out
 pushd fat.out
 sudo sh -c 'echo "RT is fat" > README'
 popd
 sudo umount fat.out
 rmdir fat.out
 
-sudo mkfs.ext4 /dev/nbd0p2
+sudo mkfs.ext4 /dev/${nbd_dev}p2
 mkdir ext4.out
-sudo mount /dev/nbd0p2 ext4.out
+sudo mount /dev/${nbd_dev}p2 ext4.out
 pushd ext4.out
 sudo sh -c 'echo "RT is ext4" > README'
 popd
@@ -51,5 +65,5 @@ sudo umount ext4.out
 sudo rmdir ext4.out
 
 # Disconnect nbd
-sudo qemu-nbd --disconnect /dev/nbd0
+sudo qemu-nbd --disconnect /dev/${nbd_dev}
 sudo rmmod nbd
