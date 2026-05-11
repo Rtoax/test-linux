c5a6a7ab83bb3e1393fc072f37fe3f4481f2bd67
Rong Tao <rtoax@foxmail.com>
2026-05-11 02:45:06 +0000

    binutils/opcode: link -lzstd if possible
    Error:
    
     CC       .output/disassembler.o
     LD       disassembler
    /usr/bin/ld.bfd: /usr/lib64/libbfd.a(compress.o): in function `decompress_contents':
    /usr/src/debug/binutils-2.46.50-7.fc45.x86_64/build-x86_64-redhat-linux/bfd/../../bfd/compress.c:533:(.text+0x1cd): undefined reference to `ZSTD_decompress'
    /usr/bin/ld.bfd: /usr/src/debug/binutils-2.46.50-7.fc45.x86_64/build-x86_64-redhat-linux/bfd/../../bfd/compress.c:535:(.text+0x1d5): undefined reference to `ZSTD_isError'
    /usr/bin/ld.bfd: /usr/lib64/libbfd.a(compress.o): in function `bfd_compress_section_contents':
    /usr/src/debug/binutils-2.46.50-7.fc45.x86_64/build-x86_64-redhat-linux/bfd/../../bfd/compress.c:659:(.text+0x12a9): undefined reference to `ZSTD_compress'
    /usr/bin/ld.bfd: /usr/src/debug/binutils-2.46.50-7.fc45.x86_64/build-x86_64-redhat-linux/bfd/../../bfd/compress.c:664:(.text+0x12b5): undefined reference to `ZSTD_isError'
    collect2: error: ld returned 1 exit status
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Vers: Fedora Linux 45, Linux 7.0.4-200.fc44.x86_64, Binutils 2.46.50, Make 4.4.1, ZStd 1.5.7

--- gnu/binutils/opcode/samples/Build.mk
+++ gnu/binutils/opcode/samples/Build.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0
 include binutils.mk
+include zstd.mk
 
 target-y += disassemble_init_for_target
 target-y += disassembler
@@ -9,3 +10,6 @@ CFLAGS += -Wno-unused-variable
 CFLAGS += ${binutils-cflags}
 
 LDFLAGS += -lopcodes -lbfd
+ifeq (${HAVE_LIBZSTD},y)
+  LDFLAGS += -lzstd
+endif
