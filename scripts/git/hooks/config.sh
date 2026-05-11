a4e599b781a02dcb7c044d18b872a93285425954
Rong Tao <rongtao@cestc.cn>
2026-05-11 10:03:36 +0800

    git/hooks: config.sh: exit 0 if none git
    Error:
    
    [root@91c3edf8d59d tst-linux]# make install
    Uninstall
    Install
    make: *** [Makefile:129: install] Error 1
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.4-200.fc44.x86_64, Git 2.54.0, bash 5.3.9

--- scripts/git/hooks/config.sh
+++ scripts/git/hooks/config.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
+
+# Skip if none git
+if [[ -z "$(which git)" ]] ||
+   [[ -z "$(git rev-parse --show-toplevel 2>/dev/null)" ]]; then
+	exit 0
+fi
+
 git config core.hooksPath scripts/git/hooks/ 2>/dev/null
 # This script called in top Makefile, need this 'get'.
 git config get core.hooksPath 2>/dev/null || {
