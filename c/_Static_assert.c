60e9a1cee52be7a8546049ec09a7d0d67a3b1775
Rong Tao <rongtao@cestc.cn>
2026-05-12 09:57:52 +0800

    _Static_assert.c: test static_assert() and define STATIC_ASSERT()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.4-200.fc44.x86_64, GCC 16.1.1, GLibc 2.43

--- c/_Static_assert.c
+++ c/_Static_assert.c
@@ -1,9 +1,16 @@
 #include <math.h>
 #include <stdio.h>
 
+#define STATIC_ASSERT(expr, msg) \
+	typedef char static_assert_failed_##msg[(expr) ? 1 : -1]
+
 _Static_assert(1, "ERROR");
+static_assert(1, "ERROR");
+STATIC_ASSERT(1, ERROR);
 #ifdef ERROR
 _Static_assert(0, "ERROR");
+static_assert(0, "ERROR");
+STATIC_ASSERT(0, ERROR);
 #endif
 
 int main(void)
