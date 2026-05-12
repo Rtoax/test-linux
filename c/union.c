ac6f96af3fe095f46ca135505aa51f181e212855
Rong Tao <rongtao@cestc.cn>
2026-05-12 14:31:46 +0800

    bpftrace/samples: struct.bt: test __attribute__ after struct {}
    Link: https://github.com/bpftrace/bpftrace/pull/5153/changes/de96a2499f5a8169830f04def6dab89813400000
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.4-200.fc44.x86_64, BPFTrace 0.25.0, GCC 16.1.1, GLibc 2.43, LibBpf 1.8.0

--- c/union.c
+++ c/union.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 struct foo {};
 #if ERROR
@@ -6,7 +7,20 @@ union foo {};
 #endif
 union bar {};
 
+union u1 {
+	struct {
+		int a;
+		char c;
+		int b;
+	} s0 __attribute__((packed));
+	struct {
+		int a;
+		int b;
+	} s1 __attribute__((packed));
+} __attribute__((packed)) /* seems like this packed is not work */;
+
 int main(void)
 {
+	printf("size of u1 %ld\n", sizeof(union u1));
 	return 0;
 }
