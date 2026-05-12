1bf8a01c393db71da258447c5fbbbe408259de9e
Rong Tao <rongtao@cestc.cn>
2026-05-12 23:38:24 +0800

    make.mk: add include_stat() from template/Makefile
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 26.04, Linux 7.0.0-15-generic, CoreUtils 0.8, Make 4.4.1, bash 5.3.9

--- template/make.mk
+++ template/make.mk
@@ -6,6 +6,7 @@
 #
 # Functions:
 # - make_{gt,ge,eq,lt,le}()=[y|n]
+# - include_stat()
 #
 ifndef _MAKE_MK
 _MAKE_MK = 1
@@ -23,15 +24,31 @@ MAKE_MINOR := $(shell ${makeversh} --minor)
 
 $(eval $(call define_version,make,version2_code100010,${MAKE_MAJOR},${MAKE_MINOR}))
 
-ifneq ($(call make_ge,4,5), n)
-  $(error make_ge failed, $(call make_ge,4,4))
-endif
+# Sometimes including an mk file takes too long, and this function can be
+# used to effectively measure the time required to include the mk.
+# Set V or DEBUG to enable debug mode.
+# Arguments:
+# $1: makefile to include
+define include_stat
+  $(eval _mk := $(1))
+  $(eval _mk_id := $(subst /,_,$(subst .,_,$(1))))
+  $(eval _start_$(_mk_id) := $(shell date +%s%6N))
+  $(if ${V}${DEBUG},$(info Include $(_mk) -> Startup.))
+  $(eval include $(_mk))
+  $(eval _end_$(_mk_id) := $(shell date +%s%6N))
+  $(eval _cost_$(_mk_id) := $(shell expr $$(expr $(_end_$(_mk_id)) - $(_start_$(_mk_id))) / 1000000))
+  $(if ${V}${DEBUG},$(info Include $(_mk) -> Done. Took $(_cost_$(_mk_id)) ms))
+endef
 
 ifdef DEBUG
   $(info MAKE = ${MAKE})
   $(info MAKE_VERSION = ${MAKE_VERSION})
   $(info MAKE_MAJOR = ${MAKE_MAJOR})
   $(info MAKE_MINOR = ${MAKE_MINOR})
+endif
+
+ifneq ($(call make_ge,4,5), n)
+  $(error make_ge failed, $(call make_ge,4,4))
 endif
 
 endif
