Rtoax/test-linux

commit a8329c8f50797fc9572de1356cbd29976eafd2c4

make_tl.sh: support kmod
Signed-off-by: Rong Tao <rongtao@cestc.cn>
Vers: Fedora Linux 44, Linux 7.0.4-200.fc44.x86_64, KMod 34.2, Make 4.4.1, RPM 6.0.1, bash 5.3.9

scripts/make_tl.sh
@@ -16,18 +16,18 @@ declare workdir=$PWD
 declare -a make_args
 declare TEST_LINUX_ROOT=$(realpath $(dirname $(realpath ${BASH_SOURCE[0]}))/../)
 
-# FIXME: remove this check after a little while
-if [[ -L /etc/profile.d/make_tl.sh ]]; then
-	echo >&2 "ERROR: 'make uninstall' first, then 'make install' again"
-	exit 1
-fi
-
 # Use origin make command
 if ! [[ " $(realpath .)" =~ " ${TEST_LINUX_ROOT}" ]] &&
    ! [[ "$(realpath .)" =~ "ostools" ]] &&
    ! [[ "$(realpath .)" =~ "test-linux" ]]; then
 	${sys_make} $@
 	return $?
+fi
+
+# FIXME: remove this check after a little while
+if [[ -L /etc/profile.d/make_tl.sh ]]; then
+	echo >&2 "ERROR: 'make uninstall' first, then 'make install' again"
+	exit 1
 fi
 
 ARGS=( "${@}" )
@@ -43,19 +43,26 @@ for ((i = 0; i < ${#ARGS[@]}; i++)); do
 done
 
 make_args+=( __USE_TEST_LINUX_MAKE__=1 )
-make_args+=( -I${TEST_LINUX_ROOT}/template/ )
 
-if [[ -z ${makefile} ]] && [[ -f ${workdir}/Build.mk ]]; then
+if [[ -z ${makefile} ]] && [[ -f ${workdir}/Build.mk ]] &&
+   [[ ! "${ARGS[@]}" =~ "/lib/modules" ]]; then
 	# It is not supported to use Build.mk and Makefile at
 	# the same time.
 	if [[ -f ${workdir}/Makefile ]]; then
 		echo >&2 "ERROR: Not allow Build.mk and Makefile at the same time"
 		exit 1
 	fi
+	make_args+=( -I${TEST_LINUX_ROOT}/template/ )
 	make_args+=( -f ${TEST_LINUX_ROOT}/scripts/Makefile.build )
-elif [[ -z ${makefile} ]] && [[ -f ${workdir}/Kbuild.mk ]]; then
-	# TODO
+elif [[ -z ${makefile} ]] && [[ -f ${workdir}/Kbuild ]]; then
+	make_args+=( __KMOD__=1 )
 	make_args+=( -f ${TEST_LINUX_ROOT}/scripts/Makefile.kmod )
 fi
 
-${sys_make} ${make_args[@]} ${@}
+_eval() {
+	if [[ ${VERBOSE} ]]; then
+		echo >&2 -e "\033[1;32m${@}\033[m"
+	fi
+	eval "${@}"
+}
+_eval ${sys_make} ${make_args[@]} ${@}

module/export/Kbuild
@@ -1,0 +1,2 @@
+obj-m += export-syms.o
+obj-m += extern-syms.o

module/export/Makefile
@@ -1,11 +1,0 @@
-obj-m += export-syms.o
-obj-m += extern-syms.o
-
-KERNEL_DIR = /lib/modules/`uname -r`/build
-MODULE_DIR := $(shell pwd)
-
-modules:
-	${Q}make -C $(KERNEL_DIR) M=$(MODULE_DIR) modules
-
-clean:
-	${Q}make -C $(KERNEL_DIR) M=$(MODULE_DIR) clean

module/kbuild/Makefile
@@ -1,8 +1,0 @@
-# NOTE: dnf install kernel-debug-devel kernel-debuginfo kernel-debug-modules
-include kcompile.mk
-
-build:
-	${Q}make -C $(KDIR) M=`pwd`
-
-clean:
-	${Q}make -C $(KDIR) M=`pwd` clean

module/license/Kbuild
@@ -1,0 +1,2 @@
+obj-m += callee.o
+obj-m += caller.o

module/license/Makefile
@@ -1,11 +1,0 @@
-obj-m += callee.o
-obj-m += caller.o
-
-KERNEL_DIR = /lib/modules/`uname -r`/build
-MODULE_DIR := $(shell pwd)
-
-build:
-	${Q}make -C $(KERNEL_DIR) M=$(MODULE_DIR) modules
-
-clean:
-	${Q}make -C $(KERNEL_DIR) M=$(MODULE_DIR) clean

module/nothing/Kbuild
@@ -1,0 +1,1 @@
+obj-m += nothing.o

module/nothing/Makefile
@@ -1,10 +1,0 @@
-obj-m += nothing.o
-
-KERNEL_DIR = /lib/modules/`uname -r`/build
-MODULE_DIR := $(shell pwd)
-
-build:
-	${Q}make -C $(KERNEL_DIR) M=$(MODULE_DIR) modules
-
-clean:
-	${Q}make -C $(KERNEL_DIR) M=$(MODULE_DIR) clean

commit d7d660980480c4e4df7e85b7ad947a61fb5af01e

make_tl.sh: Only modules without -I../template/
rongtao@NUC10:~/Git/tst-linux$ make
Makefile:60: logo.mk: No such file or directory
make: *** No rule to make target 'logo.mk'.  Stop.

Fixes: a8329c8f5079 ("make_tl.sh: support kmod")
Signed-off-by: Rong Tao <rongtao@cestc.cn>
Vers: Fedora Linux 44, Linux 7.0.4-200.fc44.x86_64, bash 5.3.9

scripts/make_tl.sh
@@ -43,16 +43,18 @@ for ((i = 0; i < ${#ARGS[@]}; i++)); do
 done
 
 make_args+=( __USE_TEST_LINUX_MAKE__=1 )
+# TODO: Linux driver build not support template yet.
+if [[ ! "${ARGS[@]}" =~ "/lib/modules" ]]; then
+	make_args+=( -I${TEST_LINUX_ROOT}/template/ )
+fi
 
-if [[ -z ${makefile} ]] && [[ -f ${workdir}/Build.mk ]] &&
-   [[ ! "${ARGS[@]}" =~ "/lib/modules" ]]; then
+if [[ -z ${makefile} ]] && [[ -f ${workdir}/Build.mk ]]; then
 	# It is not supported to use Build.mk and Makefile at
 	# the same time.
 	if [[ -f ${workdir}/Makefile ]]; then
 		echo >&2 "ERROR: Not allow Build.mk and Makefile at the same time"
 		exit 1
 	fi
-	make_args+=( -I${TEST_LINUX_ROOT}/template/ )
 	make_args+=( -f ${TEST_LINUX_ROOT}/scripts/Makefile.build )
 elif [[ -z ${makefile} ]] && [[ -f ${workdir}/Kbuild ]]; then
 	make_args+=( __KMOD__=1 )
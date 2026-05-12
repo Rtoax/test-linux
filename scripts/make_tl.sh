d7d660980480c4e4df7e85b7ad947a61fb5af01e
Rong Tao <rongtao@cestc.cn>
2026-05-12 18:37:49 +0800

    make_tl.sh: Only modules without -I../template/
    rongtao@NUC10:~/Git/tst-linux$ make
    Makefile:60: logo.mk: No such file or directory
    make: *** No rule to make target 'logo.mk'.  Stop.
    
    Fixes: a8329c8f5079 ("make_tl.sh: support kmod")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.4-200.fc44.x86_64, bash 5.3.9

--- scripts/make_tl.sh
+++ scripts/make_tl.sh
@@ -43,16 +43,18 @@ for ((i = 0; i < ${#ARGS[@]}; i++)); do
 done
 
 make_args+=( __USE_TEST_LINUX_MAKE__=1 )
+# TODO: Linux driver build not support template yet.
+if [[ ! "${ARGS[@]}" =~ "/lib/modules" ]]; then
+	make_args+=( -I${TEST_LINUX_ROOT}/template/ )
+fi
 
-if [[ -z ${makefile} ]] && [[ -f ${workdir}/Build.mk ]] &&
-   [[ ! "${ARGS[@]}" =~ "/lib/modules" ]]; then
+if [[ -z ${makefile} ]] && [[ -f ${workdir}/Build.mk ]]; then
 	# It is not supported to use Build.mk and Makefile at
 	# the same time.
 	if [[ -f ${workdir}/Makefile ]]; then
 		echo >&2 "ERROR: Not allow Build.mk and Makefile at the same time"
 		exit 1
 	fi
-	make_args+=( -I${TEST_LINUX_ROOT}/template/ )
 	make_args+=( -f ${TEST_LINUX_ROOT}/scripts/Makefile.build )
 elif [[ -z ${makefile} ]] && [[ -f ${workdir}/Kbuild ]]; then
 	make_args+=( __KMOD__=1 )
