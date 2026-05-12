5c1cab4e0e93c01d72c142cf38f08ade0c92265a
Rong Tao <rongtao@cestc.cn>
2026-05-12 09:52:36 +0800

    _Static_assert.c: static test
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.4-200.fc44.x86_64, GCC 16.1.1, GLibc 2.43

--- c/_Static_assert.c
+++ c/_Static_assert.c
@@ -1,11 +1,12 @@
 #include <math.h>
 #include <stdio.h>
 
+_Static_assert(1, "ERROR");
+#ifdef ERROR
+_Static_assert(0, "ERROR");
+#endif
+
 int main(void)
 {
-	_Static_assert(1, "ERROR");
-#ifdef ERROR
-	_Static_assert(0, "ERROR");
-#endif
 	return 0;
 }
