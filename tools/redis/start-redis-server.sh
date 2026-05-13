43983f8b95cb921bbe7de51743d445a993ec154c
Rong Tao <rongtao@cestc.cn>
2026-05-13 13:48:10 +0800

    redis: start-redis-server.sh: redis-server.service on debian
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Debian GNU/Linux 13.4, Linux 6.12.86+deb13-amd64, Redis 8.0.2, bash 5.2.37, Systemd 257.9

--- tools/redis/start-redis-server.sh
+++ tools/redis/start-redis-server.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 set -e
 
-if [[ -z "$(sudo systemctl status redis.service | \
+# /usr/lib/systemd/system/valkey.service
+if [[ -e /usr/lib/systemd/system/redis.service ]]; then
+	SERVICE=redis.service
+elif [[ -e /usr/lib/systemd/system/redis-server.service ]]; then
+	SERVICE=redis-server.service
+else
+	echo >&2 "ERROR: not found redis systemd service"
+	exit 1
+fi
+
+
+if [[ -z "$(sudo systemctl status ${SERVICE} | \
 		grep -o 'Active: active' 2>/dev/null)" ]] ||
    [[ -z "$(pidof valkey-server)$(pidof redis-server)" ]]; then
 	echo "WARNING: Try (re)start redis server..."
-	sudo systemctl start redis.service
+	sudo systemctl start ${SERVICE}
 fi
