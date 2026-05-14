719a803406cd82ee715cce90479e7ec80165e7dd
Rong Tao <rongtao@cestc.cn>
2026-05-14 10:49:13 +0800

    init: modules: use subdir-y instead of make.list
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.4-200.fc44.x86_64, acpica-tools 20260408, EDK-II 20260213, KMod 34.2, Linux-FirmWare 20260410-1, Make 4.4.1, bash 5.3.9

--- init/Build.mk
+++ init/Build.mk
@@ -1,8 +1,9 @@
 # SPDX-License-Identifier: GPL-3.0
-subdir-y := acpi
+subdir-y += acpi
 subdir-y += bios
 subdir-y += firmware
 subdir-y += kernel
 subdir-y += initrd
+subdir-y += modules
 subdir-y += systemd
 subdir-y += uefi
