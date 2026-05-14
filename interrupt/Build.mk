19367838657fa43547f63edc658119c4d2178d4f
Rong Tao <rongtao@cestc.cn>
2026-05-14 11:04:32 +0800

    modules: use subdir-y instead of make.list
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.4-200.fc44.x86_64, KMod 34.2, Make 4.4.1, bash 5.3.9

--- interrupt/Build.mk
+++ interrupt/Build.mk
@@ -1,4 +1,11 @@
 subdir-y += gic
+subdir-y += idt
+subdir-y += ioapic
+subdir-y += request_irq
+subdir-y += softirq
+subdir-y += tasklet
+subdir-y += trace-irqoff
+subdir-y += workqueue
 subdir-y += Rescheduling-interrupts
 
 target-${IS_X86_64} += cli
