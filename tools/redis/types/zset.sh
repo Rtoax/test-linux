63e93adbc0272aa170439e348893b6aa0db10a7b
Rong Tao <rongtao@cestc.cn>
2026-05-18 17:26:23 +0800

    redis: zset: test zpopmin zpopmax
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.8-200.fc44.x86_64, Redis(Valkey) 9.0.4, Redis 9.0.4, bash 5.3.9

--- tools/redis/types/zset.sh
+++ tools/redis/types/zset.sh
@@ -4,13 +4,18 @@ set -e
 zadd() {
 	redis-cli ZADD zset1 $@
 }
-zdel() {
-	redis-cli ZDEL zset1 $@
-}
 zrange() {
 	redis-cli ZRANGE zset1 $@
+}
+zpopmin() {
+	redis-cli ZPOPMIN zsert $@
+}
+zpopmax() {
+	redis-cli ZPOPMAX zsert $@
 }
 
 zadd 1 rongtao
 zadd 2 lihaiqiang
 zrange 0 10
+zpopmin
+zpopmax
