234b98d3b2eae592ae9a2d5148e4da8a16cb775e
Rong Tao <rongtao@cestc.cn>
2026-05-21 11:49:31 +0800

    findelf.sh: speed up
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.9-202.fc44.x86_64, Findutils 4.10.0, bash 5.3.9

--- scripts/findelf.sh
+++ scripts/findelf.sh
@@ -3,9 +3,9 @@ set -e
 
 while read file
 do
-	read -r -N 8 bin < $file
+	read -r -N 4 bin < $file
 	# ELF header magic
 	if [[ $bin == $'\x7f'ELF* ]]; then
 		echo "$file"
 	fi
-done <<< $(find ./ -type f -exec file {} \; | awk -F ':' '{print $1}')
+done <<< $(find ./ -type f)
