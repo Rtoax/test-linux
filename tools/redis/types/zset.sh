c7cbd34e6d4c317c5b1b55ea4f0302ab38fad99f
Rong Tao <rongtao@cestc.cn>
2026-05-22 14:26:57 +0800

    redis: zset: test ZINCRBY
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.9-204.fc44.x86_64, Redis(Valkey) 9.0.4, Redis 9.0.4, bash 5.3.9

--- tools/redis/types/zset.sh
+++ tools/redis/types/zset.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 set -e
 
+key="zset2"
+
 zadd() {
-	redis-cli ZADD zset1 $@
+	redis-cli ZADD ${key} $@
 }
 zrange() {
-	redis-cli ZRANGE zset1 $@
+	redis-cli ZRANGE ${key} $@
 }
 zpopmin() {
-	redis-cli ZPOPMIN zsert $@
+	redis-cli ZPOPMIN ${key} $@
 }
 zpopmax() {
-	redis-cli ZPOPMAX zsert $@
+	redis-cli ZPOPMAX ${key} $@
 }
+zincrby() {
+	redis-cli ZINCRBY ${key} $@
+}
+zadd 1 rongtao1
+zadd 2 rongtao2
+zadd 3 rongtao3
 
-zadd 1 rongtao
-zadd 2 lihaiqiang
 zrange 0 10
+
+zincrby 10 rongtao1
+
+zrange 0 10
+
 zpopmin
 zpopmax
