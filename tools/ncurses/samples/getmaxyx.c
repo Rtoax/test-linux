ebe4abdbc4755479834cbf992ad015023a7892cf
Rong Tao <rongtao@cestc.cn>
2026-05-25 12:22:22 +0800

    loadavg.c: SET_COLOR() and handle SIGINT
    see ncurses/test/worm.c
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.9-204.fc44.x86_64, GCC 16.1.1, GLibc 2.43, Ncurses 6.6.20251230

--- tools/ncurses/samples/getmaxyx.c
+++ tools/ncurses/samples/getmaxyx.c
@@ -4,11 +4,13 @@
 #include <unistd.h>
 
 volatile sig_atomic_t winch_flag = 0;
+volatile sig_atomic_t winch_cnt = 0;
 
 void window_change(int signo)
 {
 	if (signo == SIGWINCH) {
 		winch_flag = 1;
+		winch_cnt++;
 	}
 }
 
@@ -17,6 +19,7 @@ void print_winsz(void)
 	int err, rows, cols;
 
 	clear();
+	printw("winch_cnt %d\n", winch_cnt);
 
 	err = getmaxyx(stdscr, rows, cols);
 	printw("screen rows %d, cols %d, err %d\n", rows, cols, err);
@@ -24,6 +27,8 @@ void print_winsz(void)
 	rows = getmaxy(stdscr);
 	cols = getmaxx(stdscr);
 	printw("screen rows %d, cols %d\n", rows, cols);
+
+	printw("macro rows %d, cols %d\n", LINES, COLS);
 
 	refresh();
 }
@@ -40,7 +45,6 @@ int main(void)
 		if (winch_flag) {
 			winch_flag = 0;
 			endwin();
-			refresh();
 			print_winsz();
 		}
 		napms(100);
--- tools/ncurses/tools/loadavg.c
+++ tools/ncurses/tools/loadavg.c
@@ -7,10 +7,25 @@
  * see also test-linux/scripts/loadavg.sh
  */
 #include <errno.h>
+#include <locale.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
 #include <ncurses.h>
 #include <unistd.h>
+
+static chtype flavor[] = {
+	'O', '*', '#', '$', '%', '0', '@',
+};
+
+void sig_handler(int signo)
+{
+	switch (signo) {
+	case SIGINT:
+		endwin();
+		exit(EXIT_SUCCESS);
+	}
+}
 
 int getload(int *l1, int *l5, int *l15, int scale)
 {
@@ -38,7 +53,8 @@ void load_window(void)
 		int l1, l5, l15;
 		getload(&l1, &l5, &l15, 1000);
 
-		printw("%d %d %d\n", l1, l5, l15);
+		printw("%d %d %d, row %d, col %d\n", l1, l5, l15, LINES, COLS);
+
 		refresh();
 		sleep(1);
 	}
@@ -46,10 +62,32 @@ void load_window(void)
 
 int main(void)
 {
+	setlocale(LC_ALL, "");
+
+	signal(SIGINT, sig_handler);
 	initscr();
 	cbreak();
 	noecho();
+	nonl();
+
 	curs_set(0);
+
+	if (has_colors()) {
+		int bg = COLOR_BLACK;
+		start_color();
+#define SET_COLOR(num, fg)                        \
+	init_pair(num + 1, (short)fg, (short)bg); \
+	flavor[num] |= (chtype)COLOR_PAIR(num + 1) | A_BOLD
+
+		SET_COLOR(0, COLOR_GREEN);
+		SET_COLOR(1, COLOR_RED);
+		SET_COLOR(2, COLOR_CYAN);
+		SET_COLOR(3, COLOR_WHITE);
+		SET_COLOR(4, COLOR_MAGENTA);
+		SET_COLOR(5, COLOR_BLUE);
+		SET_COLOR(6, COLOR_YELLOW);
+#undef SET_COLOR
+	}
 
 	load_window();
 
