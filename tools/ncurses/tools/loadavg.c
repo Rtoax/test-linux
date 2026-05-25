77d5cae7ce7b7b0d7b0c6dd399b25bfeb3a6d428
Rong Tao <rongtao@cestc.cn>
2026-05-25 11:17:51 +0800

    loadavg.c: add load_window()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.9-204.fc44.x86_64, GCC 16.1.1, GLibc 2.43, Ncurses 6.6.20251230

--- tools/ncurses/tools/loadavg.c
+++ tools/ncurses/tools/loadavg.c
@@ -6,7 +6,43 @@
  *
  * see also test-linux/scripts/loadavg.sh
  */
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <ncurses.h>
+#include <unistd.h>
+
+int getload(int *l1, int *l5, int *l15, int scale)
+{
+	FILE *fp = fopen("/proc/loadavg", "r");
+	if (!fp) {
+		fprintf(stderr, "open /proc/loadavg failed, %m\n");
+		return -errno;
+	}
+
+	float load1, load5, load15;
+	fscanf(fp, "%f %f %f", &load1, &load5, &load15);
+
+	*l1 = load1 * scale;
+	*l5 = load5 * scale;
+	*l15 = load15 * scale;
+
+	fclose(fp);
+	return 0;
+}
+
+void load_window(void)
+{
+	// TODO
+	while (1) {
+		int l1, l5, l15;
+		getload(&l1, &l5, &l15, 1000);
+
+		printw("%d %d %d\n", l1, l5, l15);
+		refresh();
+		sleep(1);
+	}
+}
 
 int main(void)
 {
@@ -15,7 +51,7 @@ int main(void)
 	noecho();
 	curs_set(0);
 
-	/* do more/something */
+	load_window();
 
 	endwin();
 	return 0;
