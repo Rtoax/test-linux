b79aa618641a416da543a35f4fdf0e6e5a6128b6
Rong Tao <rongtao@cestc.cn>
2026-05-26 13:53:26 +0800

    stdio: stdin.c: check read() return
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.9-204.fc44.x86_64, GCC 16.1.1, GLibc 2.43

--- glibc/stdio/stdin.c
+++ glibc/stdio/stdin.c
@@ -1,14 +1,21 @@
+/**
+ * test:
+ * $ cat /etc/os-release | ./stdin
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main(void)
 {
-	char ch;
-
 	while (1) {
-		read(STDIN_FILENO, &ch, 1);
-		printf("ch = %c\n", ch);
+		char ch = '\0';
+		int err = read(STDIN_FILENO, &ch, 1);
+		if (err <= 0 || ch == EOF) {
+			fprintf(stderr, "read('%c') = %d\n", ch, err);
+			break;
+		}
+		putchar(ch);
 	}
 	return 0;
 }
