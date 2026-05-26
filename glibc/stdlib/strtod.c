16a7fe49aa7eb3075c3620a3a2f3cc52d33ef2b5
Rong Tao <rongtao@cestc.cn>
2026-05-26 16:02:42 +0800

    stdlib: strtod(3),strtof(3): pass argv
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.9-204.fc44.x86_64, GCC 16.1.1, GLibc 2.43

--- glibc/stdlib/strtod.c
+++ glibc/stdlib/strtod.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	char *buf;
 
 	printf("%f\n", strtod("123.34asdf", &buf));
 	printf("%s\n", buf);
+	if (argc > 1) {
+		printf("strtod(%s) = %lf\n", argv[1], strtod(argv[1], &buf));
+	}
 
 	return 0;
 }
--- glibc/stdlib/strtof.c
+++ glibc/stdlib/strtof.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	char *buf;
 
 	printf("%f\n", strtof("123.34asdf", &buf));
 	printf("%s\n", buf);
+	if (argc > 1) {
+		printf("strtof(%s) = %f\n", argv[1], strtof(argv[1], &buf));
+	}
 
 	return 0;
 }
