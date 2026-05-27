32d6477a1d40989870c009f77eaba15f0f0349e2
Rong Tao <rongtao@cestc.cn>
2026-05-27 18:41:12 +0800

    ncurses/tools: examples.sh: you could copy it directly
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.9-204.fc44.x86_64, Ncurses 6.6.20251230, bash 5.3.9

--- tools/ncurses/tools/examples.sh
+++ tools/ncurses/tools/examples.sh
@@ -2,7 +2,7 @@
 # -m: (set -o monitor) monitor mode
 set -em
 
-tmout=2
+args=( --tmout 2 )
 
 # loadavg will send SIGINT to every processes in it's group, thus, we just
 # catch SIGINT wo avoid this script execute failed.
@@ -15,19 +15,19 @@ trap sigint INT
 # display the loadavg
 while sleep .2; do
 	awk '{print $1, $2, $3}' /proc/loadavg
-done | ./loadavg -t ${tmout} --title 'Loadavg' --xlabel 'Time' --ylabel 'Load' \
+done | ./loadavg ${args[@]} --title 'Loadavg' --xlabel 'Time' --ylabel 'Load' \
 		-l 'Load1' -l 'Load5' -l 'Load15'
 
 # display the memory usage
 while sleep .2; do
 	free -m | grep ^Mem | awk '{print $2, $3, $4, $5, $6, $7}'
-done | ./loadavg -t ${tmout} --title 'Memory Usage' --xlabel 'Time' --ylabel 'Size(MB)' \
+done | ./loadavg ${args[@]} --title 'Memory Usage' --xlabel 'Time' --ylabel 'Size(MB)' \
 		-l total -l used -l free -l shared -l buff/cache -l avail
 
 # display the process number
 while sleep .2; do
 	ls /proc/ | grep -E '[0-9]+' | wc -w
-done | ./loadavg -t ${tmout} --title 'Process Number' --xlabel 'Time' --ylabel 'Number' \
+done | ./loadavg ${args[@]} --title 'Process Number' --xlabel 'Time' --ylabel 'Number' \
 		-l Proc
 
 echo "Byebye"
