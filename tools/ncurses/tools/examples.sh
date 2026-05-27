63e4925d8b6324d49ce0e0f1ffeb36bad2f4c8a4
Rong Tao <rongtao@cestc.cn>
2026-05-27 16:15:23 +0800

    ncurses/tools: examples.sh: add comment
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.9-204.fc44.x86_64, Ncurses 6.6.20251230, bash 5.3.9

--- tools/ncurses/tools/examples.sh
+++ tools/ncurses/tools/examples.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# -m: (set -o monitor) monitor mode
 set -em
+
+tmout=2
 
 # loadavg will send SIGINT to every processes in it's group, thus, we just
 # catch SIGINT wo avoid this script execute failed.
@@ -11,10 +14,10 @@ trap sigint INT
 
 while sleep .2; do
 	awk '{print $1, $2, $3}' /proc/loadavg
-done | ./loadavg -t 2
+done | ./loadavg -t ${tmout}
 
 while sleep .2; do
 	free -m | grep ^Mem | awk '{print $2, $3, $4, $5, $6, $7}'
-done | ./loadavg -t 2
+done | ./loadavg -t ${tmout}
 
 echo "Byebye"
