341a3988895e39f22eb069b28301bb55fe1dc671
Rong Tao <rongtao@cestc.cn>
2026-05-27 16:11:39 +0800

    ncurses/tools: examples.sh: add memory test
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.9-204.fc44.x86_64, Ncurses 6.6.20251230, bash 5.3.9

--- tools/ncurses/tools/examples.sh
+++ tools/ncurses/tools/examples.sh
@@ -13,4 +13,8 @@ while sleep .2; do
 	awk '{print $1, $2, $3}' /proc/loadavg
 done | ./loadavg -t 2
 
+while sleep .2; do
+	free -m | grep ^Mem | awk '{print $2, $3, $4, $5, $6, $7}'
+done | ./loadavg -t 2
+
 echo "Byebye"
