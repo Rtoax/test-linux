ccc9cfa73ef343e6717141de38413d01d8029d85
Rong Tao <rongtao@cestc.cn>
2026-05-27 16:56:15 +0800

    ncurses/tools: examples.sh: add process test
                                       stdin
           ▲
    471.000│                                                      ┏┓
           │                                                      ┃┃
    463.000│                                                      ┃┃ ┏┓
    461.000│                                                   ┏┓┏┛┗┓┃┃
    459.000│                                                  ┏┛┃┃  ┗┛┗┓
           │                                                  ┃ ┃┃     ┃
    454.000│                                                  ┃ ┗┛     ┃
           │                                                  ┃        ┃
           │                                                  ┃        ┃
           │                                                  ┃        ┃
           │                                                  ┃        ┃
           │                                                  ┃        ┃
    437.000│                                                  ┃        ┗┓
           │                                                  ┃         ┃
           │                                                  ┃         ┃
    428.000│                                  ┏┓       ┏┓     ┃         ┃
    425.000│                                  ┃┃       ┃┗┓    ┃         ┃
    423.000│                                 ┏┛┃       ┃ ┃    ┃         ┃
    421.000│                                 ┃ ┃     ┏━┛ ┃    ┃         ┃
    417.000│                                ┏┛ ┃     ┃   ┃   ┏┛         ┗┓
    414.000│                                ┃  ┗━━━━━┛   ┗━━━┛           ┗line0
           │                                ┃
    411.000│                          ━━━━━━┛
           └──────────────────────────────────────────────────────────────►
                                      16:55:29  16:55:39  16:55:49  16:55:59
                                                      Wed May 27 16:56:04 2026
                                             github.com/rtoax/test-linux v1.1.0
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.9-204.fc44.x86_64, Ncurses 6.6.20251230, bash 5.3.9

--- tools/ncurses/tools/examples.sh
+++ tools/ncurses/tools/examples.sh
@@ -12,12 +12,19 @@ sigint() {
 }
 trap sigint INT
 
+# display the loadavg
 while sleep .2; do
 	awk '{print $1, $2, $3}' /proc/loadavg
 done | ./loadavg -t ${tmout}
 
+# display the memory usage
 while sleep .2; do
 	free -m | grep ^Mem | awk '{print $2, $3, $4, $5, $6, $7}'
+done | ./loadavg -t ${tmout}
+
+# display the process number
+while sleep .2; do
+	ls /proc/ | grep -E '[0-9]+' | wc -w
 done | ./loadavg -t ${tmout}
 
 echo "Byebye"
