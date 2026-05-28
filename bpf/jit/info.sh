6a122501d5945cbde833d01c7cb8f26affcbc8a6
Rong Tao <rongtao@cestc.cn>
2026-05-28 09:27:37 +0800

    bpf/jit: info.sh: get jited info
        $ ./info.sh
        bpf_jit_enable = 1
        bpf_jit_harden = 0
        bpf_jit_kallsyms = 1
        bpf_jit_limit = 528482304
        jited = 7486B
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.9-204.fc44.x86_64, BPFTool 7.6.0, LibBpf 1.8.0, bash 5.3.9

--- bpf/jit/info.sh
+++ bpf/jit/info.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+set -e
+
 echo "bpf_jit_enable = $(cat /proc/sys/net/core/bpf_jit_enable)"
+echo "bpf_jit_harden = $(sudo cat /proc/sys/net/core/bpf_jit_harden)"
+echo "bpf_jit_kallsyms = $(sudo cat /proc/sys/net/core/bpf_jit_kallsyms)"
+echo "bpf_jit_limit = $(sudo cat /proc/sys/net/core/bpf_jit_limit)"
+
+jited=$(sudo bpftool prog list | grep -oE 'jited [0-9]+' | \
+		awk 'BEGIN {sum=0} {sum+=$2} END {printf(sum"B\n")}')
+echo "jited = ${jited}"
