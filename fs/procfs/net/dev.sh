1068a12815ad3a31528d398c3254d9510c485d8b
Rong Tao <rongtao@cestc.cn>
2026-05-28 18:10:28 +0800

    proc: /proc/net/dev Receive bytes
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.10-200.fc44.x86_64, bash 5.3.9

--- fs/procfs/net/dev.sh
+++ fs/procfs/net/dev.sh
@@ -4,3 +4,10 @@ set -e
 # ifconfig's RX,TX packages, ...
 # same as /proc/self/net/dev
 cat /proc/net/dev
+
+# Receive bytes is not zero
+cat /proc/net/dev | awk '{
+	if ($2 != 0) {
+		printf($1"\n")
+	}
+}'
