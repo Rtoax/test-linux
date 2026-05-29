a4c9f7d1a7cade21df0ac1fd7ce5e89850b66000
Rong Tao <rongtao@cestc.cn>
2026-05-29 14:35:31 +0800

    ncurses/tools: tools/open.sh: add close(2)
             N/s                  Open Syscall Rate
             ▲
    12454.000│                                                       ┏┓
             │                                                       ┃┃
             │                                                       ┃┃
             │                                                       ┃┃
    10171.000│                                ┏┓                     ┃┃
    9752.000 │                                ┃┃                     ┃┃   ┏┓
    9104.000 │                             ┏┓ ┃┗┓                    ┃┃   ┃┃
    8526.000 │                             ┃┃ ┃ ┗┓               ┏┓  ┃┃   ┃┃
    7985.000 │                             ┏┓┏┛  ┃               ┏┓  ┏┓   ┃┃
    7728.000 │                             ┃┃┃   ┃              ┏┛┃  ┃┃   ┃┃
    7100.000 │                             ┃┃┃   ┃              ┃ ┃  ┃┃┓  ┃┃
    6807.000 │                           ┏┓┃┃┃   ┃             ┏┛ ┃┓ ┃┃┃  ┃┃
    6102.000 │                           ┃┃┃┗┓  ┏┓             ┃  ┗┓ ┃┃┃  ┃┃
    5665.000 │                           ┃┃┃ ┗━━┛┃             ┃   ┃ ┃┃┃┏┓┏━close
    5038.000 │                           ┃┃┃     ┃             ┃   ┃┏┛┗┓┃┃┃┗open
    4808.000 │                          ┏┛┗┛     ┃            ┏┛   ┗┛  ┃┏┓┃
    4182.000 │                          ┃┛       ┃            ┃        ┃┃┃┃
    3816.000 │                         ┏┛        ┃            ┃        ┃┃┗┛
    3329.000 │                         ┃         ┃           ┏┛        ┗┛
    2926.000 │                         ┃         ┃           ┃┛
             │                         ┃         ┃           ┃
    1886.000 │                         ┃         ┗┓          ┃
    1419.000 │                         ┃         ┗┃         ┏┛
    633.000  │                        ┏┛          ┃        ┏┛
    189.000  │                   ━━━━━┛┛          ┗━━━━━━━━┛┛
    29.000   │                                          ┗┛
             └──────────────────────────────────────────────────────────────►
                                 14:34:05  14:34:15  14:34:25  14:34:35  14:34:45
                                                        Fri May 29 14:34:47 2026
                                               github.com/rtoax/test-linux v1.1.0
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.10-200.fc44.x86_64, Ncurses 6.6.20251230, bash 5.3.9

--- tools/ncurses/tools/tools/open.sh
+++ tools/ncurses/tools/tools/open.sh
@@ -5,8 +5,17 @@ sudo bpftrace -e \
 'tracepoint:syscalls:sys_enter_open* {
 	@open += 1;
 }
+tracepoint:syscalls:sys_enter_close* {
+	@close += 1;
+}
 i:s:1 {
-	printf("%ld\n", @open);
+	printf("%ld %ld\n", @open, @close);
 	clear(@open);
-}' 2>/dev/null | \
-../loadavg --title 'Open Syscall Rate' --ylabel 'N/s' -l 'open'
+	clear(@close);
+}
+END {
+	clear(@open);
+	clear(@close);
+}
+' 2>/dev/null | ../loadavg --title 'Open Syscall Rate' --ylabel 'N/s' \
+			-l 'open' -l 'close'
