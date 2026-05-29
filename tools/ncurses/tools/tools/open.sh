1d30c50bb383affe278db1a198d7bcd9bac27a23
Rong Tao <rongtao@cestc.cn>
2026-05-29 12:53:47 +0800

    ncurses/tools: add tools/open.sh
             N/s              Open Syscall Rate
             ▲
    14671.000│                                                  ┏━━open
    13738.000│                                               ┏┓┏┛
             │                                               ┃┃┃
    12305.000│                                            ┏┓ ┃┃┃
             │                                            ┃┃ ┃┃┃
    10875.000│                                 ┏┓         ┃┃ ┃┃┃
    10438.000│                                ┏┛┃┏┓ ┏┓    ┃┃ ┃┃┃
    9289.000 │                                ┃ ┃┃┃ ┃┃ ┏┓ ┃┃ ┃┃┃
    8756.000 │                                ┃ ┃┃┃┏┛┃┏┛┃ ┃┃ ┃┃┃
    8183.000 │                                ┃ ┃┃┗┛ ┗┛ ┗┓┃┃ ┃┃┃
             │                                ┃ ┃┃       ┃┃┃ ┃┃┃
    6572.000 │                              ┏━┛ ┗┛       ┗┛┗┓┃┃┃
    6060.000 │                              ┃               ┗┛┗┛
             │                              ┃
             │                              ┃
             │                              ┃
             │                              ┃
    2637.000 │            ┏┓  ┏┓┏┓  ┏┓     ┏┛
             │            ┃┃  ┃┃┃┃  ┃┃     ┃
             │            ┃┃  ┃┃┃┃  ┃┃     ┃
    117.000  │      ━┓┏━━━┛┗━━┛┗┛┗━━┛┗━━━━━┛
    24.000   │       ┗┛
             └─────────────────────────────────────────────────────►
                    12:52:39  12:52:49  12:52:59  12:53:09  12:53:19ime
                                               Fri May 29 12:53:25 2026
                                      github.com/rtoax/test-linux v1.1.0
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.10-200.fc44.x86_64, BPFTrace 0.26.0, Ncurses 6.6.20251230, bash 5.3.9

---
+++ tools/ncurses/tools/tools/open.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+sudo bpftrace -e \
+	'tracepoint:syscalls:sys_enter_open* {
+		@open += 1;
+	}
+	i:s:1 {
+		printf("%ld\n", @open);
+		clear(@open);
+	}' 2>/dev/null | ../loadavg --title 'Open Syscall Rate' --ylabel 'N/s' -l 'open'
