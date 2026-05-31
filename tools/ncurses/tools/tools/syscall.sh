46e6ebe26ee01b2a31f9de9e507eb4de976c0cf8
Rong Tao <rtoax@foxmail.com>
2026-05-31 14:43:04 +0800

    ncurses/tools: add tools/syscall.sh
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Vers: Ubuntu 26.04, Linux 7.0.0-22-generic, BPFTrace 0.25.0, Ncurses 6.6.20251231, bash 5.3.9

---
+++ tools/ncurses/tools/tools/syscall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+sudo bpftrace -e \
+'tracepoint:raw_syscalls:sys_enter {
+	@enter += 1;
+}
+i:s:2 {
+	printf("%ld\n", @enter);
+	clear(@enter);
+}
+END {
+	clear(@enter);
+}
+' 2>/dev/null | ../loadavg --title 'Syscalls' --ylabel 'N/s' \
+			-l 'syscall'
