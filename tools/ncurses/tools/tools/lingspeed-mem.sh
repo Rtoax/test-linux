02f2a62b034cb83436c71018c3fcc950c8ff803a
Rong Tao <rongtao@cestc.cn>
2026-06-02 02:02:23 +0800

    ncurses/tools: add tools/lingspeed-mem.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Kylin Linux Advanced Server V10, Linux 4.19.90-52.23.v2207.gfb08.ky10.aarch64, LUCA 3.7.0, Ncurses 6.2.20200411, bash 5.0.18

---
+++ tools/ncurses/tools/tools/lingspeed-mem.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+gpus=( $(ls-smi --show-memory | grep -oE '^GPU.[0-9]+') )
+
+for gpu in ${gpus[@]}
+do
+	labels+=( -l ${gpu} )
+done
+
+while sleep 0.5; do
+	usage=( $(ls-smi --show-memory | grep 'vis_vram usage' | awk '{print $4}') )
+	echo ${usage[@]}
+done | ../loadavg --title 'LingSpeed Memory Usage' --ylabel 'usage*100%' ${labels[@]}
