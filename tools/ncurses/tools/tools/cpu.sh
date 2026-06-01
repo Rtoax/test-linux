38bd6d2cf6b94ff25a467a82578119532ac5bc90
Rong Tao <rongtao@cestc.cn>
2026-06-01 13:58:18 +0800

    ncurses/tools: tools/cpu.sh: add line labels
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, x86_64
    Vers: Fedora Linux 44, Linux 7.0.10-201.fc44.x86_64, Ncurses 6.6.20251230, bash 5.3.9

--- tools/ncurses/tools/tools/cpu.sh
+++ tools/ncurses/tools/tools/cpu.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+declare -a labels
+for ((i = 0; i < $(nproc); i++)); do
+	labels+=( -l cpu${i} )
+done
+
 # TODO
 while sleep 1; do
 	loads=( $(mpstat -P ALL | awk '{print $NF}' | grep -E '[0-9]+\.[0-9]+') )
 	echo ${loads[@]}
-done | ../loadavg --title 'CPU Loads' --ylabel '%idle'
+done | ../loadavg --title 'CPU Loads' --ylabel '%idle' ${labels[@]}
