111cd80b02b34449adace5f2d213d78c726e2986
Rong Tao <rongtao@cestc.cn>
2026-06-02 18:29:47 +0800

    btf_dump: btf_dump__dump_type()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.10-201.fc44.x86_64, BPFTool 7.6.0, GCC 16.1.1, GLibc 2.43, LibBpf 1.8.0

--- bpf/btf/btf_dump.c
+++ bpf/btf/btf_dump.c
@@ -13,12 +13,20 @@ static void dump_printf(void *ctx, const char *fmt, va_list args)
 
 int main(int argc, char **argv)
 {
-	struct btf *btf = btf_load_vmlinux();
-	struct btf_dump *dump = btf_dump__new(btf, dump_printf, NULL, NULL);
+	struct btf *btf;
+	struct btf_dump *dump;
+	int btf_id;
+
+	btf = btf_load_vmlinux();
+	dump = btf_dump__new(btf, dump_printf, NULL, NULL);
 	if (!dump) {
 		fprintf(stderr, "btf dump new failed.\n");
 	}
-	// TODO
+
+	btf_id = btf_has_struct("task_struct");
+
+	btf_dump__dump_type(dump, btf_id);
+
 	btf_dump__free(dump);
 	btf__free(btf);
 	return 0;
