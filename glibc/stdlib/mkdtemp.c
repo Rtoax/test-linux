5e948338d94293e2e863ca76a48a3f9ddbb2c96d
Rong Tao <rtoax@foxmail.com>
2026-06-03 22:24:51 +0800

    glibc/stdlib: code format
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Vers: Ubuntu 26.04, Linux 7.0.0-22-generic, GCC 15.2.0, GLibc 2.43

--- glibc/stdlib/mkdtemp.c
+++ glibc/stdlib/mkdtemp.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-
 int main(void)
 {
 	char path[] = "tmpd-XXXXXX";
 	char *tmpd = mkdtemp(path);
 	printf("tmpd = %s\n", tmpd ?: "NULL");
-
 	rmdir(tmpd);
 	return 0;
 }
--- glibc/stdlib/mktemp.c
+++ glibc/stdlib/mktemp.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-
 int main(void)
 {
 	char path[] = "tmp-XXXXXX";
 	char *tmp = mktemp(path);
 	printf("tmp = %s\n", tmp ?: "NULL");
-
 	unlink(tmp);
 	return 0;
 }
