3eae2f8556a3b58db6932a14ea94ce22117aaa1e
Rong Tao <rtoax@foxmail.com>
2026-06-03 22:02:41 +0800

    plotcake: loadavg.sh: line type and color
    	$ I=1 ./loadavg.sh
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Vers: Ubuntu 26.04, Linux 7.0.0-22-generic, Ncurses 6.6.20251231, bash 5.3.9

--- tools/ncurses/tools/tools/loadavg.sh
+++ tools/ncurses/tools/tools/loadavg.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # display the loadavg
+#
+# Usage: ./loadavg.sh [I=<secs>]
 set -e
 
-while sleep 1; do
+[[ -z ${I} ]] && I=1
+
+while sleep ${I}; do
 	awk '{print $1, $2, $3}' /proc/loadavg
 done | ../plotcake ${args[@]} --title 'Loadavg' --xlabel 'Time' --ylabel 'Load' \
-		-l 'Load1' -l 'Load5' -l 'Load15'
+		-l 'Load1' -l 'Load5' -l 'Load15' \
+		-L unicode-boldbold -L unicode-bold -L unicode \
+		-C red -C blue -C white
