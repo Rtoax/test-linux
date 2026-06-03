753a0f2262e80a0cfb510299a2806486a60b2359
Rong Tao <rtoax@foxmail.com>
2026-06-03 22:07:54 +0800

    plotcake: loadavg.sh: do not sleep first
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Vers: Ubuntu 26.04, Linux 7.0.0-22-generic, Ncurses 6.6.20251231, bash 5.3.9

--- tools/ncurses/tools/tools/loadavg.sh
+++ tools/ncurses/tools/tools/loadavg.sh
@@ -6,8 +6,8 @@ set -e
 
 [[ -z ${I} ]] && I=1
 
-while sleep ${I}; do
-	awk '{print $1, $2, $3}' /proc/loadavg
+while awk '{print $1, $2, $3}' /proc/loadavg; do
+	sleep ${I}
 done | ../plotcake ${args[@]} --title 'Loadavg' --xlabel 'Time' --ylabel 'Load' \
 		-l 'Load1' -l 'Load5' -l 'Load15' \
 		-L unicode-boldbold -L unicode-bold -L unicode \
