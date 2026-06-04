2b04f55669b1270b524e343919b996eabb8bb479
Rong Tao <rongtao@cestc.cn>
2026-06-04 13:48:32 +0800

    plotcake: add struct key_handler{}
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tools/ncurses/tools/keyboard.h
+++ tools/ncurses/tools/keyboard.h
@@ -1,3 +1,9 @@
 // SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
 /* Copyright (C) 2026 Rong Tao */
 #pragma once
+
+struct key_handler {
+	int key;
+	void *arg;
+	void (*handler)(int key, void *arg);
+};
