88a4ad912bff5b2138bc8dc4c6b425035d03c3cb
Rong Tao <rongtao@cestc.cn>
2026-06-04 14:48:43 +0800

    ncurses/samples: initscr.c: new window with new thread
    But garbled text:
    
    Hello World 0
    d1
      ┌──────────────────┐
     1│New Window 0      │
      │                  │
      │                  │
      │                  x
      │[0Gmx22HG
    │ │                  │
                         mx
    └└──────────────────┘ mx
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.10-201.fc44.x86_64, GCC 16.1.1, GLibc 2.43, Ncurses 6.6.20251230

--- tools/ncurses/samples/initscr.c
+++ tools/ncurses/samples/initscr.c
@@ -1,28 +1,46 @@
 #include <ncurses.h>
+#include <pthread.h>
 #include <string.h>
 #include <unistd.h>
 
+void *thread_win(void *arg)
+{
+	WINDOW *warn_win = newwin(10, 20, 2, 2);
+	box(warn_win, 0, 0);
+
+	for (int i = 0; i < 3; i++) {
+		mvwprintw(warn_win, 1, 1, "New Window %d", i);
+		wrefresh(warn_win);
+		sleep(1);
+	}
+
+	werase(warn_win);
+	wrefresh(warn_win);
+	delwin(warn_win);
+
+	sleep(3);
+
+	return NULL;
+}
+
 int main(void)
 {
+	pthread_t warn_thread;
+
 	initscr();
 	cbreak();
 	noecho();
 	curs_set(0);
 
-	printw("Hello World.\n");
-	refresh();
-	sleep(1);
+	pthread_create(&warn_thread, NULL, thread_win, NULL);
 
-	WINDOW *warn_win = newwin(10, 10, 2, 2);
-	box(warn_win, 0, 0);
-	wprintw(warn_win, "New Window");
-	wrefresh(warn_win);
-	sleep(1);
-	werase(warn_win);
-	wrefresh(warn_win);
-	delwin(warn_win);
+	for (int i = 0; i < 3; i++) {
+		printw("Hello World %d\n", i);
+		refresh();
+		sleep(1);
+	}
 
-	sleep(1);
+	pthread_join(warn_thread, NULL);
 
 	endwin();
 	return 0;
