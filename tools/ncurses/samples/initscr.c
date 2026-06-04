1d36974dd1aa6744341d455fb874ecbf689b676e
Rong Tao <rongtao@cestc.cn>
2026-06-04 14:52:59 +0800

    ncurses/samples: ncurses is not thread safety
    Hello World 0
    Hello World 1
    Hello World 2
      │New Window 2      │
      │                  │
      │                  │
      │                  │
      │                  │
      │                  │
      │                  │
      │                  │
      └──────────────────┘
    
    Fixes: 88a4ad912bff ("ncurses/samples: initscr.c: new window with new thread")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.10-201.fc44.x86_64, GCC 16.1.1, GLibc 2.43, Ncurses 6.6.20251230

--- tools/ncurses/samples/initscr.c
+++ tools/ncurses/samples/initscr.c
@@ -3,20 +3,30 @@
 #include <string.h>
 #include <unistd.h>
 
+pthread_mutex_t ncurses_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 void *thread_win(void *arg)
 {
+	pthread_mutex_lock(&ncurses_mutex);
+
 	WINDOW *warn_win = newwin(10, 20, 2, 2);
 	box(warn_win, 0, 0);
 
+	pthread_mutex_unlock(&ncurses_mutex);
+
 	for (int i = 0; i < 3; i++) {
+		pthread_mutex_lock(&ncurses_mutex);
 		mvwprintw(warn_win, 1, 1, "New Window %d", i);
 		wrefresh(warn_win);
+		pthread_mutex_unlock(&ncurses_mutex);
 		sleep(1);
 	}
 
+	pthread_mutex_lock(&ncurses_mutex);
 	werase(warn_win);
 	wrefresh(warn_win);
 	delwin(warn_win);
+	pthread_mutex_unlock(&ncurses_mutex);
 
 	sleep(3);
 
@@ -27,21 +37,27 @@ int main(void)
 {
 	pthread_t warn_thread;
 
+	pthread_mutex_lock(&ncurses_mutex);
 	initscr();
 	cbreak();
 	noecho();
 	curs_set(0);
+	pthread_mutex_unlock(&ncurses_mutex);
 
 	pthread_create(&warn_thread, NULL, thread_win, NULL);
 
 	for (int i = 0; i < 3; i++) {
+		pthread_mutex_lock(&ncurses_mutex);
 		printw("Hello World %d\n", i);
 		refresh();
+		pthread_mutex_unlock(&ncurses_mutex);
 		sleep(1);
 	}
 
 	pthread_join(warn_thread, NULL);
 
+	pthread_mutex_lock(&ncurses_mutex);
 	endwin();
+	pthread_mutex_unlock(&ncurses_mutex);
 	return 0;
 }
