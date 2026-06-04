5f00a0a5a237aa7a856271a8babcf780affaaaaf
Rong Tao <rongtao@cestc.cn>
2026-06-04 12:43:17 +0800

    plotcake: tools/cpu.sh: support filter cpu index
    $ ./cpu.sh 1 10
    
           %idle            CPU Loads %idle
           ▲
    100.000│  ━┓ ┏┓    ┏━┓┏┓            ┏┓  ┏┓
    99.000 │   ┃ ┃┃   ┏┛ ┃┃┗┓┏┓  ┏┓ ┏┏┓ ┃┃  ┃┃┏━┓    ┏━┓ ┏┓
    98.000 │   ┃ ┃┃ ┏━━┓ ┏━━━┓┃┏┏┛┃┏━┛┗━┛┃  ┃┗┛ ┃┏━━┓┃━┗━┛┃┏┓
    97.000 │   ┃ ┃┗┓┃ ┃┃ ┃┃  ┗┓┃┃ ┃┃┃ ┗┓ ┗┓ ┃┏┓┏┗┛┗━┃┃ ┗┓┃┗┛┃
    96.000 │   ┗┓┃ ┗┛ ┃┗━┛┃   ┃┏┛ ┗┛┃  ┗━┛┃ ┃┛┗┛    ┗┛  ┗┛  ┃┏cpu10
           │    ┃┃ ┃  ┃  ┃┃   ┃┃┃   ┃     ┃ ┃               ┃┃
    95.000 │   ┏━┛ ┗┓ ┃  ┃┃   ┗┛┗┓ ┏┛     ┃ ┃               ┃┃
    94.000 │  ━┛    ┃ ┃  ┃┃   ┃┃ ┃ ┃      ┃ ┃               ┃┃
    93.000 │        ┗┓┃  ┃┃   ┃┃ ┃ ┃      ┗┓┃               ┗┛
           │         ┃┃  ┃┃   ┃┃ ┃ ┃      ┃┃┃                ┃
           │         ┃┃  ┃┃   ┃┃ ┃ ┃      ┃┃┃                ┃
           │         ┃┃  ┃┃   ┃┃ ┃ ┃      ┃┃┃                ┃
    90.000 │         ┃┃  ┃┃   ┃┃ ┃┏┛      ┃┃┃                ┗cpu1
           │         ┃┃  ┃┃   ┃┃ ┃┃       ┃┃┃
    88.000 │         ┃┃  ┃┃   ┃┃ ┃┃       ┃┃┃
    87.000 │         ┃┃  ┃┃   ┃┃ ┃┃       ┃┗┛
           │         ┃┃  ┃┃   ┃┃ ┃┃       ┃┃
           │         ┃┃  ┃┃   ┃┃ ┃┃       ┃┃
           │         ┃┃  ┃┃   ┃┃ ┃┃       ┃┃
           │         ┃┃  ┃┃   ┃┃ ┃┃       ┃┃
           │         ┃┃  ┃┃   ┃┃ ┃┃       ┃┃
    82.000 │         ┃┃  ┃┃   ┃┃ ┗┛       ┃┃
    81.000 │         ┃┃  ┃┃   ┃┃          ┗┛
           │         ┃┃  ┃┃   ┃┃
    80.000 │         ┃┃  ┗┛   ┃┃
           │         ┃┃       ┃┃
    78.000 │         ┃┃       ┗┛
           │         ┃┃
           │         ┃┃
           │         ┃┃
           │         ┃┃
           │         ┃┃
           │         ┃┃
           │         ┃┃
           │         ┃┃
           │         ┃┃
    70.000 │         ┗┛
           └──────────────────────────────────────────────────►
              12:42:50  12:43:00  12:43:10  12:43:20  12:43:30Time
                                          Thu Jun  4 12:43:37 2026
                                 github.com/rtoax/test-linux v1.3.6
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, x86_64
    Vers: Fedora Linux 44, Linux 7.0.10-201.fc44.x86_64, Ncurses 6.6.20251230, bash 5.3.9

--- tools/ncurses/tools/tools/cpu.sh
+++ tools/ncurses/tools/tools/cpu.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
-# Usage: ./cpu.sh [I=<secs>]
+# Usage: [I=<secs>] ./cpu.sh [cpu indexes]
+# Example: I=1 ./cpu.sh 1 2 3
 set -e
 
 [[ -z ${I} ]] && I=1
 
+indexes=( ${@} )
+
 declare -a labels
 declare -A prev_idle prev_total percent_idle
+declare -a detected_cpus
 
-cpus=( $(grep -Eo '^cpu[0-9]+' /proc/stat) )
+readonly all_cpus=( $(grep -Eo '^cpu[0-9]+' /proc/stat) )
 
-for cpu in all ${cpus[@]}; do
-	labels+=( -l ${cpu} )
+for i in $(seq 0 1 $((${#all_cpus[@]}-1)) ); do
+	if [[ ${indexes} ]] && [[ ! " ${indexes[@]} " =~ " ${i} " ]]; then
+		continue
+	fi
+	labels+=( -l cpu${i} )
+	detected_cpus+=( cpu${i} )
 done
+
+echo ${all_cpus[@]}
+echo ${detected_cpus[@]}
+echo ${labels[@]}
 
 # ref test-linux/fs/procfs/stat.sh
 while true; do
-	for cpu in cpu ${cpus[@]}
+	for cpu in ${detected_cpus[@]}
 	do
 		datas=( $(grep -w ^${cpu} /proc/stat) )
 		user=${datas[1]}
