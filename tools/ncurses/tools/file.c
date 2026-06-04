6740f756b92b8bd36955279ea032ded38002e9f4
Rong Tao <rongtao@cestc.cn>
2026-06-04 18:46:36 +0800

    plotcake: save more to plotcake.dat
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tools/ncurses/tools/file.c
+++ tools/ncurses/tools/file.c
@@ -7,8 +7,12 @@
 
 #define FILE_DATA "plotcake.dat"
 
+/**
+ * only have one plot
+ */
 int save_plot(struct plot *p)
 {
+	int lg_idx;
 	char *path = FILE_DATA;
 	FILE *fp = fopen(path, "w");
 	if (!fp) {
@@ -16,7 +20,30 @@ int save_plot(struct plot *p)
 		return -errno;
 	}
 
+	fprintf(fp, "#    lgroup count\n");
 	fprintf(fp, "plot %d\n", p->lgcount);
+
+	lg_idx = 0;
+	for_each_lg(p, lg)
+	{
+		int ln_idx = 0;
+
+		fprintf(fp, "#      idx nline\n");
+		fprintf(fp, "lgroup %d %d\n", lg_idx, lg->count);
+
+		for_each_line(lg, ln)
+		{
+			fprintf(fp, "#    lgidx lnidx nvals\n");
+			fprintf(fp, "line %d %d %d\n", lg_idx, ln_idx,
+				ln->count);
+
+			// For each value
+
+			ln_idx++;
+		}
+
+		lg_idx++;
+	}
 
 	fclose(fp);
 	return 0;
