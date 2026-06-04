0e8ef4d1ca81f75831f52001f00f220a8d345780
Rong Tao <rongtao@cestc.cn>
2026-06-04 15:41:29 +0800

    ncurses/samples: test mouse
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.10-201.fc44.x86_64, GCC 16.1.1, GLibc 2.43, Make 4.4.1, Ncurses 6.6.20251230

--- tools/ncurses/samples/Build.mk
+++ tools/ncurses/samples/Build.mk
@@ -4,6 +4,10 @@ target-y += getch
 target-y += getmaxyx
 target-y += keyname
 target-y += initscr
+target-y += mouse
 target-y += setlocale
 
+CFLAGS_mouse := -DTEST_MOUSE=1
+
+LDFLAGS += -pthread
 LDFLAGS += ${ncurses-ldflags}
--- tools/ncurses/samples/getch.c
+++ tools/ncurses/samples/getch.c
@@ -1,7 +1,37 @@
 #include <ncurses.h>
 
+#ifdef TEST_MOUSE
+void handle_mouse_event(void)
+{
+	MEVENT event;
+	if (getmouse(&event) != OK)
+		return;
+
+	move(7, 0);
+	clrtoeol();
+	move(8, 0);
+	clrtoeol();
+
+	if (event.bstate & BUTTON1_CLICKED) {
+		mvprintw(7, 0, "left row=%d, col=%d", event.y, event.x);
+	} else if (event.bstate & BUTTON3_CLICKED) {
+		mvprintw(7, 0, "right click");
+	} else if (event.bstate & BUTTON4_PRESSED) {
+		mvprintw(7, 0, "scroll up");
+	} else if (event.bstate & BUTTON5_PRESSED) {
+		mvprintw(7, 0, "scroll down");
+	} else if (event.bstate & REPORT_MOUSE_POSITION) {
+		mvprintw(7, 0, "move row=%d, col=%d", event.y, event.x);
+	}
+
+	refresh();
+}
+#endif
+
 int main(void)
 {
+	int err = 0;
+
 	initscr();
 	cbreak();
 	noecho();
@@ -14,11 +44,34 @@ int main(void)
 	 */
 	nodelay(stdscr, TRUE);
 
+#ifdef TEST_MOUSE
+#if 0 /* TODO: why failed? */
+	if (has_mouse() != TRUE) {
+		fprintf(stderr, "Not found mouse.\n");
+		err = 1;
+		goto end;
+	}
+#endif
+	mmask_t mask = BUTTON1_CLICKED | BUTTON3_CLICKED | BUTTON4_PRESSED |
+		       BUTTON5_PRESSED | REPORT_MOUSE_POSITION;
+	mmask_t oldmask;
+	if (mousemask(mask, &oldmask) == 0) {
+		fprintf(stderr, "Not support mouse event.\n");
+		err = 2;
+		goto end;
+	}
+#endif
+
 	while (1) {
 		/* actually, getch() read from stdin */
 		int c = getch();
 		if (c != ERR) {
 			switch (c) {
+#ifdef TEST_MOUSE
+			case KEY_MOUSE:
+				handle_mouse_event();
+				break;
+#endif
 			case KEY_LEFT:
 				printw("KEY_LEFT %d\n", c);
 				break;
@@ -36,6 +89,7 @@ int main(void)
 	}
 
 end:
+	refresh();
 	endwin();
-	return 0;
+	return err;
 }
