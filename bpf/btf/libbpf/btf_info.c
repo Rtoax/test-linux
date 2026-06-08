a93b7fde4bcb6bb628653e26ea353b476caa17b3
Rong Tao <rongtao@cestc.cn>
2026-06-08 14:28:16 +0800

    btf_info.c: call bpf_obj_get_info_by_fd() twice
    TODO: Bad address
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.11-200.fc44.x86_64, BPFTool 7.6.0, GCC 16.1.1, GLibc 2.43, LibBpf 1.8.0, Pahole 1.30

--- bpf/btf/libbpf/btf_info.c
+++ bpf/btf/libbpf/btf_info.c
@@ -8,6 +8,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 #include <unistd.h>
 
 int main(int argc, char **argv)
@@ -34,9 +35,35 @@ int main(int argc, char **argv)
 		goto done;
 	}
 
-	printf("info len=%d\n", info_len);
+	printf("info1 len=%d, name=%llx, namelen=%d\n", info_len, info.name,
+	       info.name_len);
 
+	char *name_buf;
+	posix_memalign((void **)&name_buf, 64, info.name_len + 1);
+#if 0
+	name_buf = mmap(NULL, info.name_len + 1, PROT_READ | PROT_WRITE,
+			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+#endif
+	memset(name_buf, 0, info.name_len + 1);
+
+	info.name = (uint64_t)name_buf;
+	info.name_len = info.name_len + 1;
+
+	info_len = sizeof(info);
+
+	/**
+	 * FIXME: 'Bad address' here
+	 */
+	err = bpf_obj_get_info_by_fd(fd, &info, &info_len);
+	if (err) {
+		fprintf(stderr, "failed get info from fd %d at 2nd, %m.\n", fd);
+		goto free_done;
+	}
+
+free_done:
+	free(name_buf);
 done:
-	close(fd);
+	if (fd > 0)
+		close(fd);
 	return err;
 }
