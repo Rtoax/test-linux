26ad9b6ebff28420fcf59f72331c21c2eeb53229
Rong Tao <rongtao@cestc.cn>
2026-06-09 10:12:11 +0800

    btf_ksym.c: use params seperate BTF_KIND_ instead of macro
    rongtao@NUC10:~/Git/tst-linux/bpf/btf/libbpf$ ./btf_ksym struct=file
    struct file {
    	.....
    };
    Kernel symbol 'file' STRUCT exist, btf id 106889.
    
    rongtao@NUC10:~/Git/tst-linux/bpf/btf/libbpf$ ./btf_ksym func=vfs_write
    ssize_t vfs_write(struct file *file, char *buf, size_t count, loff_t *pos) = btf_id 153897;
    Kernel symbol 'vfs_write' FUNC exist, btf id 153897.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.11-200.fc44.x86_64, BPFTool 7.6.0, GCC 16.1.1, GLibc 2.43, LibBpf 1.8.0, Make 4.4.1, Pahole 1.30

--- bpf/btf/libbpf/btf_ksym.c
+++ bpf/btf/libbpf/btf_ksym.c
@@ -7,37 +7,61 @@
 #include <unistd.h>
 #include "btf_helpers.h"
 
+static void usage(void)
+{
+	fprintf(stderr,
+		"\033[2m\n"
+		"Usage: ./btf_dump struct=<ksym> func=<ksym> decl-tag=<ksym>\n"
+		"\033[m");
+}
+
 int main(int argc, char **argv)
 {
 	int err = 0;
 	int btf_id;
 	char *s_type = "";
 	const char *ksym;
+	int kind;
 
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s <ksym>\n", argv[0]);
 		return 1;
 	}
 
-	ksym = argv[1];
+	for (int i = 1; i < argc; i++) {
+		if (!strncmp(argv[i], "struct=", 7)) {
+			ksym = argv[i] + 7;
+			s_type = "STRUCT";
+			kind = BTF_KIND_STRUCT;
+		} else if (!strncmp(argv[i], "func=", 5)) {
+			ksym = argv[i] + 5;
+			s_type = "FUNC";
+			kind = BTF_KIND_FUNC;
+		} else if (!strncmp(argv[i], "decl-tag=", 9)) {
+			ksym = argv[i] + 9;
+			s_type = "DECL_TAG";
+			kind = BTF_KIND_DECL_TAG;
+		} else {
+			usage();
+			fprintf(stderr, "ERROR: Unknown %s\n", argv[i]);
+			exit(EXIT_FAILURE);
+		}
+	}
 
-#ifdef TEST_BTF_KIND_FUNC
-#pragma message "defined TEST_BTF_KIND_FUNC"
-	btf_id = btf_has_kfunc(ksym, true);
-	s_type = "FUNC";
-#elif defined(TEST_BTF_KIND_STRUCT)
-#pragma message "defined TEST_BTF_KIND_STRUCT"
-	btf_id = btf_has_struct(ksym);
-	s_type = "STRUCT";
-#elif defined(TEST_BTF_KIND_DECL_TAG)
-#pragma message "defined TEST_BTF_KIND_DECL_TAG"
-	btf_id = btf_has_decl_tag(ksym);
-	s_type = "DECL_TAG";
-#else
-	btf_id = btf_has_ksym(ksym);
-	if (btf_id > 0)
-		printf("Kernel symbol '%s' exist.\n", ksym);
-#endif
+	switch (kind) {
+	case BTF_KIND_STRUCT:
+		btf_id = btf_has_struct(ksym);
+		break;
+	case BTF_KIND_FUNC:
+		btf_id = btf_has_kfunc(ksym, true);
+		break;
+	case BTF_KIND_DECL_TAG:
+		btf_id = btf_has_decl_tag(ksym);
+		break;
+	default:
+		btf_id = btf_has_ksym(ksym);
+		break;
+	}
 
 	if (btf_id <= 0) {
 		printf("Kernel symbol '%s' %s does not exist.\n", ksym, s_type);
