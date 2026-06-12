f199ce177bd111e638e7771d4820904c551054e5
Rong Tao <rongtao@cestc.cn>
2026-06-12 16:20:57 +0800

    install-deps: add expect to base
    Link: commit d249d9ed87ca ("expect: add keyboard.exp demo")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.12-200.fc44.x86_64, Expect 5.45.4, bash 5.3.9

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -612,6 +612,7 @@ pkgs_base+=( dwz )                  # DWARF optimization and duplicate removal t
 pkgs_base+=( dwarves )              # pahole
 pkgs_base+=( efivar )               # UEFI
 pkgs_base+=( elfutils )             # eu- prefix tools
+pkgs_base+=( expect )
 pkgs_base+=( flatpak )
 pkgs_base+=( gdb cgdb )
 pkgs_base+=( git )
