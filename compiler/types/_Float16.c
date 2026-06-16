e745060abca4605e19907db9dcab5b9caa54c446
Rong Tao <rongtao@cestc.cn>
2026-06-16 17:01:17 +0800

    _Float16.c: calculate precision error
    rongtao@spark-d9c2:~/Git/tst-linux/compiler/types$ ./_Float16
    3.14062500000000000
    FP64 Reference Add: 6.28318530600000003
    CPU FP16 Test Add:  6.28125000000000000
    Absolute Error Add: 1.935306e-03
    
    FP64 Reference Mul: 9.86960439738357920
    CPU FP16 Test Mul:  9.86718750000000000
    Absolute Error Mul: 2.416897e-03
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 24.04, Linux 6.17.0-1014-nvidia, GCC 13.3.0, GLibc 2.39

--- compiler/types/_Float16.c
+++ compiler/types/_Float16.c
@@ -1,8 +1,44 @@
+#include <math.h>
 #include <stdio.h>
+
+#define PI 3.141592653
+
+void base(void)
+{
+	_Float16 f16 = (_Float16)PI;
+	printf("%.17f\n", (float)f16);
+}
+
+/**
+ * Assuming FP64 is an accurate value, calculate the floating-point precision
+ * error of FP16.
+ */
+void precision_error(void)
+{
+	double fp64 = PI;
+	_Float16 fp16 = (_Float16)PI;
+
+	double res_mul_fp64 = fp64 * fp64;
+	_Float16 res_mul_fp16 = fp16 * fp16;
+
+	double res_add_fp64 = fp64 + fp64;
+	_Float16 res_add_fp16 = fp16 + fp16;
+
+	double err_add = fabs(res_add_fp64 - (double)res_add_fp16);
+	double err_mul = fabs(res_mul_fp64 - (double)res_mul_fp16);
+
+	printf("FP64 Reference Add: %.17f\n", res_add_fp64);
+	printf("CPU FP16 Test Add:  %.17f\n", (double)res_add_fp16);
+	printf("Absolute Error Add: %e\n\n", err_add);
+
+	printf("FP64 Reference Mul: %.17f\n", res_mul_fp64);
+	printf("CPU FP16 Test Mul:  %.17f\n", (double)res_mul_fp16);
+	printf("Absolute Error Mul: %e\n\n", err_mul);
+}
 
 int main(void)
 {
-	_Float16 f16 = (_Float16)3.141592653;
-	printf("%f\n", (float)f16);
+	base();
+	precision_error();
 	return 0;
 }
