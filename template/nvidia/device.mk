9ad938485d3eb2e055a47c2f5f76f077f1d69a3c
Rong Tao <rongtao@cestc.cn>
2026-06-17 15:42:03 +0800

    nvidia/device.mk: add cflags-nvidia-device-y
    and rename HAVE_NVIDIA_GPU_xxx
    
    Link: https://admin.pci-ids.ucw.cz/read/PC/10DE
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: NVIDIA GB10
    Vers: Ubuntu 24.04, Linux 6.17.0-1014-nvidia, CUDA 13.0.88, Make 4.3

--- template/nvidia/device.mk
+++ template/nvidia/device.mk
@@ -3,9 +3,13 @@
 #
 # Output definitions:
 # - HAVE_NVIDIA_GPU=[y|n]
+# - HAVE_NVIDIA_GPU_GB20B_GB10=[y|n]
+# - HAVE_NVIDIA_GPU_GH100_H800=[y|n]
 #
 # - NVIDIA_PCI_VENDOR_ID
-# - NV_GB20B_PCI_DEV_ID
+# - NV_GB20B_GB10_PCI_DEV_ID
+#
+# - cflags-nvidia-device-y
 #
 # Refs:
 # - https://admin.pci-ids.ucw.cz/read/PC/10DE
@@ -17,8 +21,8 @@ include pciutils.mk
 
 NVIDIA_PCI_VENDOR_ID := 10DE
 
-NV_GB20B_PCI_DEV_ID := 2E12 # NVIDIA Spark DGX
-NV_H800_PCI_DEV_ID := 2324
+NV_GB20B_GB10_PCI_DEV_ID := 2E12 # NVIDIA Spark DGX
+NV_GH100_H800_PCI_DEV_ID := 2324
 
 HAVE_NVIDIA_GPU := n
 
@@ -28,13 +32,17 @@ ifneq ($(shell lspci -d ${NVIDIA_PCI_VENDOR_ID}:*),)
   HAVE_NVIDIA_GPU := y
 endif
 
-HAVE_NVIDIA_GPU_GB20B := $(call find_pci_device,${NVIDIA_PCI_VENDOR_ID},${NV_GB20B_PCI_DEV_ID})
-HAVE_NVIDIA_GPU_H800 := $(call find_pci_device,${NVIDIA_PCI_VENDOR_ID},${NV_H800_PCI_DEV_ID})
+export HAVE_NVIDIA_GPU_GB20B_GB10 := $(call find_pci_device,${NVIDIA_PCI_VENDOR_ID},${NV_GB20B_GB10_PCI_DEV_ID})
+export HAVE_NVIDIA_GPU_GH100_H800 := $(call find_pci_device,${NVIDIA_PCI_VENDOR_ID},${NV_GH100_H800_PCI_DEV_ID})
+
+cflags-nvidia-device-${HAVE_NVIDIA_GPU_GB20B_GB10} += -DHAVE_NVIDIA_GPU_GB20B_GB10=1
+cflags-nvidia-device-${HAVE_NVIDIA_GPU_GH100_H800} += -DHAVE_NVIDIA_GPU_GH100_H800=1
 
 ifdef DEBUG
   $(info HAVE_NVIDIA_GPU = ${HAVE_NVIDIA_GPU})
-  $(info HAVE_NVIDIA_GPU_GB20B = ${HAVE_NVIDIA_GPU_GB20B})
-  $(info HAVE_NVIDIA_GPU_H800 = ${HAVE_NVIDIA_GPU_H800})
+  $(info HAVE_NVIDIA_GPU_GB20B_GB10 = ${HAVE_NVIDIA_GPU_GB20B_GB10})
+  $(info HAVE_NVIDIA_GPU_GH100_H800 = ${HAVE_NVIDIA_GPU_GH100_H800})
+  $(info cflags-nvidia-device-y = ${cflags-nvidia-device-y})
 endif
 
 export HAVE_NVIDIA_GPU
