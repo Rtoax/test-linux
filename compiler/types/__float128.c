830afa0b80be2db0061757e1cecb9f0ee1b04def
Rong Tao <rongtao@cestc.cn>
2026-06-17 10:08:27 +0800

    compiler/types: __float80.c: test precision error with __float128 compare
    compiler/types$ ./__float80
    size of __float80 16
    size of _Float64x 16
    FP128 Reference Add: res_add_fp128 = 6.28318530717958647692528676655900559
    CPU FP16 Test Add:  6.28318530717958623
    Absolute Error Add: err_add = 0.00000000000000024492935982947063527
    FP128 Reference Mul: res_mul_fp128 = 9.86960440108935861883449099987615081
    CPU FP16 Test Mul:  9.86960440108935785
    Absolute Error Mul: err_mul = 0.00000000000000076964423764205775878
    
    Fixes: eaeaef6e0aad ("compiler/types: __float80.c: test precision error")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.12-201.fc44.x86_64, GCC 16.1.1, GLibc 2.43

--- compiler/types/__float128.c
+++ compiler/types/__float128.c
@@ -25,7 +25,12 @@ void test__float128(void)
 	fp128_printf(pi, "%.35Qg");
 	fp128_printf(e, "%.35Qf");
 	fp128_printf(pi + e, "%.35Qf");
+
 	fp128_printf(fmaq(pi, pi, pi), "%.35Qf");
+
+	fp128_printf(sinq(pi), "%.35Qf");
+	fp128_printf(cosq(pi), "%.35Qf");
+	fp128_printf(tanq(pi), "%.35Qf");
 #endif
 }
 
@@ -41,7 +46,12 @@ void test_Float128(void)
 	fp128_printf(pi, "%.35Qf");
 	fp128_printf(e, "%.35Qf");
 	fp128_printf(pi + e, "%.35Qf");
+
 	fp128_printf(fmaq(pi, pi, pi), "%.35Qf");
+
+	fp128_printf(sinq(pi), "%.35Qf");
+	fp128_printf(cosq(pi), "%.35Qf");
+	fp128_printf(tanq(pi), "%.35Qf");
 #endif
 }
 
--- compiler/types/__float80.c
+++ compiler/types/__float80.c
@@ -7,39 +7,12 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdint.h>
+#include "__float128.h"
 
 #define PI 3.14159265358979323846264338327950288419716939937510
+#define PIQ 3.14159265358979323846264338327950288419716939937510Q
 
-/**
- * Assuming FP64 is an accurate value, calculate the floating-point precision
- * error of FP16.
- *
- * TODO: do not use fp64, use fp128 as accurate value.
- */
-void precision_error(void)
-{
-	double fp64 = PI;
-	__float80 fp80 = (__float80)PI;
-
-	double res_mul_fp64 = fp64 * fp64;
-	__float80 res_mul_fp80 = fp80 * fp80;
-
-	double res_add_fp64 = fp64 + fp64;
-	__float80 res_add_fp80 = fp80 + fp80;
-
-	double err_add = fabs(res_add_fp64 - (double)res_add_fp80);
-	double err_mul = fabs(res_mul_fp64 - (double)res_mul_fp80);
-
-	printf("FP64 Reference Add: %.17f\n", res_add_fp64);
-	printf("CPU FP16 Test Add:  %.17f\n", (double)res_add_fp80);
-	printf("Absolute Error Add: %e\n\n", err_add);
-
-	printf("FP64 Reference Mul: %.17f\n", res_mul_fp64);
-	printf("CPU FP16 Test Mul:  %.17f\n", (double)res_mul_fp80);
-	printf("Absolute Error Mul: %e\n\n", err_mul);
-}
-
-int main(void)
+void base(void)
 {
 	assert(sizeof(__float80) == 16 &&
 	       "size of __float80 is not equal to 16");
@@ -49,7 +22,44 @@ int main(void)
 
 	printf("size of __float80 %ld\n", sizeof(f80));
 	printf("size of _Float64x %ld\n", sizeof(f64x));
+}
 
+/**
+ * Assuming FP128 is an accurate value, calculate the floating-point precision
+ * error of FP80.
+ */
+void precision_error(void)
+{
+#ifdef SUPPORT___float128
+	__float128 fp128 = PIQ;
+	__float80 fp80 = (__float80)PI;
+
+	__float128 res_mul_fp128 = fp128 * fp128;
+	__float80 res_mul_fp80 = fp80 * fp80;
+
+	__float128 res_add_fp128 = fp128 + fp128;
+	__float80 res_add_fp80 = fp80 + fp80;
+
+	__float128 err_add = fabsq(res_add_fp128 - (__float128)res_add_fp80);
+	__float128 err_mul = fabsq(res_mul_fp128 - (__float128)res_mul_fp80);
+
+	printf("FP128 Reference Add: ");
+	fp128_printf(res_add_fp128, "%.35Qf");
+	printf("CPU FP16 Test Add:  %.17Lf\n", res_add_fp80);
+	printf("Absolute Error Add: ");
+	fp128_printf(err_add, "%.35Qf");
+
+	printf("FP128 Reference Mul: ");
+	fp128_printf(res_mul_fp128, "%.35Qf");
+	printf("CPU FP16 Test Mul:  %.17Lf\n", res_mul_fp80);
+	printf("Absolute Error Mul: ");
+	fp128_printf(err_mul, "%.35Qf");
+#endif
+}
+
+int main(void)
+{
+	base();
 	precision_error();
 
 	return 0;
