da53c3872f1cd0c889d46e1196bbac5c2b6e93b3
Rong Tao <rongtao@cestc.cn>
2026-06-18 12:31:49 +0800

    macro: arg.c: update COUNT_NARG()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 24.04, Linux 6.17.0-1014-nvidia, GCC 13.3.0, GLibc 2.39

--- macro/arg.c
+++ macro/arg.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
-#define COUNT_ARGS(...) COUNT_ARGS_IMPL(__VA_ARGS__, 8, 7, 6, 5, 4, 3, 2, 1, 0)
-#define COUNT_ARGS_IMPL(_1, _2, _3, _4, _5, _6, _7, _8, N, ...) N
+#define COUNT_NARG(...) COUNT_NARG_(__VA_ARGS__, COUNT_RSEQ_N())
+#define COUNT_NARG_(...) COUNT_ARG_N(__VA_ARGS__)
+#define COUNT_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...) N
+#define COUNT_RSEQ_N() 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 
 void count(void)
 {
-	printf("%d\n", COUNT_ARGS(1));
-	printf("%d\n", COUNT_ARGS(1, 2, 3));
-	printf("%d\n", COUNT_ARGS(1, 2, 3, 4, 5));
-	printf("%d\n", COUNT_ARGS(1, 2, 3, 4, 5, 6, 7));
-	printf("%d\n", COUNT_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9));
+	printf("%d\n", COUNT_NARG(1));
+	printf("%d\n", COUNT_NARG(x));
+	printf("%d\n", COUNT_NARG(1, 2, 3));
+	printf("%d\n", COUNT_NARG(1, 2, 3, 4, 5));
+	printf("%d\n", COUNT_NARG(1, 2, 3, 4, 5, 6, 7));
+	printf("%d\n", COUNT_NARG(1, 2, 3, 4, 5, 6, 7, 8, 9));
+	printf("%d\n", COUNT_NARG(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+	printf("%d\n", COUNT_NARG(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10));
 }
 
 int main(void)
