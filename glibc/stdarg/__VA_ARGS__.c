96833c3d2bcbfde71f77f761cc4d1eb62cd76b0a
Rong Tao <rongtao@cestc.cn>
2026-06-18 13:44:17 +0800

    __VA_ARGS__: FUNC_CALL() macro
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 24.04, Linux 6.17.0-1014-nvidia, GCC 13.3.0, GLibc 2.39

--- glibc/stdarg/__VA_ARGS__.c
+++ glibc/stdarg/__VA_ARGS__.c
@@ -4,7 +4,10 @@
 #define print2(fmt, ...) printf(fmt, ##__VA_ARGS__)
 #define print3(fmt...) printf(fmt)
 
-int main(void)
+/* GNU extend */
+#define FUNC_CALL(func, ...) func(__VA_ARGS__)
+
+void test_print(int a, int b)
 {
 	printf("hello, %s %s\n", "rong", "tao");
 	print1("hello, %s %s\n", "rong", "tao");
@@ -12,5 +15,14 @@ int main(void)
 	print2("hello, rong tao\n");
 	print3("hello, %s %s\n", "rong", "tao");
 	print3("hello, rong tao\n");
+}
+
+int main(void)
+{
+	FUNC_CALL(test_print, 1, 2);
+
+	FUNC_CALL(printf, "hello\n");
+	FUNC_CALL(printf, "hello %s\n", "rongtao");
+
 	return 0;
 }
