e1d68ac786f93ada7585c7db2830e7ffd6010f06
Rong Tao <rongtao@cestc.cn>
2026-06-18 18:07:07 +0800

    plotcake: config.h: no flink hint in help info
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.12-201.fc44.x86_64, GCC 16.1.1, GLibc 2.43, Ncurses 6.6.20251230

--- tools/ncurses/tools/config.h
+++ tools/ncurses/tools/config.h
@@ -4,9 +4,8 @@
 
 #define MY_VERSION "v1.4.1"
 
-#define LONG_PRESS_FLICK "(long press to prevent flickering)"
-#define KEY_HELP_h "'h': show the help info" LONG_PRESS_FLICK
-#define KEY_HELP_l "'l': display the label for each line" LONG_PRESS_FLICK
+#define KEY_HELP_h "'h': show the help info"
+#define KEY_HELP_l "'l': show the label for each line"
 #define KEY_HELP_q "'q' and Esc: quit"
 #define KEY_HELP_r "'r': reset plot"
 #define KEY_HELP_t "'t': change numerical scaling for paint"
