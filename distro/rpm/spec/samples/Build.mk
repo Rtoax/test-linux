038a53ec63d1a2183f7a9a54b65739c71446724b
Rong Tao <rongtao@cestc.cn>
2026-06-23 08:56:35 +0800

    rpm-spec: Build.mk: use runprog.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.12-201.fc44.x86_64, Make 4.4.1, RPM 6.0.1, RPM-Build 6.0.1, bash 5.3.9

--- distro/rpm/spec/samples/Build.mk
+++ distro/rpm/spec/samples/Build.mk
@@ -1,8 +1,9 @@
 include rpm.mk
+include runprog.mk
 
 # $1: spec file
 define rpmbuild
-$(shell ${RPMBUILD} -ba --define "_topdir $(shell echo $$PWD)" ${1})
+$(shell ${RUNPROG} -- ${RPMBUILD} -ba --define \"_topdir ${CURDIR}\" ${1})
 endef
 
 $(call rpmbuild,Conflicts.spec)
