bb9eabc2f46e13863f9180c31b8c15a5c42df30f
Rong Tao <rongtao@cestc.cn>
2026-06-30 17:03:38 +0800

    plotcake: add comment in output txt file
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.13-200.fc44.x86_64, GCC 16.1.1, GLibc 2.43, Ncurses 6.6.20251230

--- tools/ncurses/tools/config.h
+++ tools/ncurses/tools/config.h
@@ -2,7 +2,7 @@
 /* Copyright (C) 2026 Rong Tao */
 #pragma once
 
-#define MY_VERSION "v1.5.2"
+#define MY_VERSION "v1.5.3"
 #define GIT_REPO "github.com/rtoax/plotcake"
 
 #define KEY_HELP_h "'h': show the help info"
