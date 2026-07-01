96d05fc65613008f4d5004a401886de0dda04909
Rong Tao <rtoax@foxmail.com>
2026-07-01 23:27:40 +0800

    mpi/samples: add done log for post0
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Vers: Ubuntu 26.04, Linux 7.0.0-27-generic, Make 4.4.1, MPICH 4.3.2, OpenMPI 5.0.10

--- hpc/mpi/samples/Target.mk
+++ hpc/mpi/samples/Target.mk
@@ -3,3 +3,4 @@ post0:
 	$(Q)timeout 1 $(MPIRUN) -n 2 ./MPI_Init || true
 	$(Q)timeout 1 $(MPIRUN) -n 2 ./MPI_Send || true
 	$(Q)timeout 1 $(MPIRUN) -n 10 ./MPI_Send-2 || true
+	$(call log_tgt,TEST DONE,$(@))
