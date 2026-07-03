94ea45cfa94ae62524075fa43cdbca605845ba10
Rong Tao <rongtao@cestc.cn>
2026-07-03 17:02:10 +0800

    ncurses: attributes.c: print attr name
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.14-201.fc44.x86_64, GCC 16.1.1, GLibc 2.43, Ncurses 6.6.20251230

--- tools/ncurses/samples/attributes.c
+++ tools/ncurses/samples/attributes.c
@@ -14,7 +14,7 @@ enum color_enum {
 	C_MAX,
 	C_UNKNOWN = C_MAX,
 };
-char *color_names[] = {
+const char *color_names[] = {
 	[C_GREEN] = "GREEN",   [C_RED] = "RED",		[C_CYAN] = "CYAN",
 	[C_WHITE] = "WHITE",   [C_MAGENTA] = "MAGENTA", [C_BLUE] = "BLUE",
 	[C_YELLOW] = "YELLOW",
@@ -27,6 +27,41 @@ chtype attrs[] = {
 	A_ALTCHARSET, A_INVIS,	    A_PROTECT,	A_HORIZONTAL, A_LEFT,
 	A_LOW,	      A_RIGHT,	    A_TOP,	A_VERTICAL,   A_ITALIC,
 };
+struct attr_name_st {
+	chtype v;
+	const char *name;
+} __attr_names[] = {
+	{ .v = A_NORMAL, .name = "A_NORMAL", },
+	{ .v = A_ATTRIBUTES, .name = "A_ATTRIBUTES", },
+	{ .v = A_CHARTEXT, .name = "A_CHARTEXT", },
+	{ .v = A_COLOR, .name = "A_COLOR", },
+	{ .v = A_STANDOUT, .name = "A_STANDOUT", },
+	{ .v = A_UNDERLINE, .name = "A_UNDERLINE", },
+	{ .v = A_REVERSE, .name = "A_REVERSE", },
+	{ .v = A_BLINK, .name = "A_BLINK", },
+	{ .v = A_DIM, .name = "A_DIM", },
+	{ .v = A_BOLD, .name = "A_BOLD", },
+	{ .v = A_ALTCHARSET, .name = "A_ALTCHARSET", },
+	{ .v = A_INVIS, .name = "A_INVIS", },
+	{ .v = A_PROTECT, .name = "A_PROTECT", },
+	{ .v = A_HORIZONTAL, .name = "A_HORIZONTAL", },
+	{ .v = A_LEFT, .name = "A_LEFT", },
+	{ .v = A_LOW, .name = "A_LOW", },
+	{ .v = A_RIGHT, .name = "A_RIGHT", },
+	{ .v = A_TOP, .name = "A_TOP", },
+	{ .v = A_VERTICAL, .name = "A_VERTICAL", },
+	{ .v = A_ITALIC, .name = "A_ITALIC", },
+};
+
+const char *attr_name(chtype a)
+{
+	for (int i = 0; i < sizeof(__attr_names) / sizeof(__attr_names[0]);
+	     i++) {
+		if (a == __attr_names[i].v)
+			return __attr_names[i].name;
+	}
+	return "??";
+}
 
 void init_colors(void)
 {
@@ -56,10 +91,11 @@ void print_attr(void)
 		for (int j = 0; j < sizeof(attrs) / sizeof(attrs[0]); j++) {
 			chtype a = attrs[j];
 			attron(c | a);
-			printw("%s", color_names[i]);
+			printw("[%s, %s]", color_names[i], attr_name(attrs[j]));
 			attroff(c | a);
 		}
 	}
+	printw("\nPress 'q' or ctrl-c to quit\n");
 	refresh();
 }
 
