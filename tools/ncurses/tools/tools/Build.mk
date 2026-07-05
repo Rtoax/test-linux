02f51c3c9358fdbfe5a2c9397d4e45f64ee627a9
Rong Tao <rtoax@foxmail.com>
2026-07-05 16:55:22 +0800

    plotcake: tools: add Build.mk
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ tools/ncurses/tools/tools/Build.mk
@@ -0,0 +1,15 @@
+prog-y += blkio.sh
+prog-y += cake.sh
+prog-y += cpu.sh
+prog-y += file.sh
+prog-y += lingspeed-mem.sh
+prog-y += loadavg.sh
+prog-y += memory.sh
+prog-y += net.sh
+prog-y += open.sh
+prog-y += process.sh
+prog-y += syscall.sh
+prog-y += txrx.sh
+prog-y += vfs.sh
+
+PROG_ARGS := --tmout 2s
