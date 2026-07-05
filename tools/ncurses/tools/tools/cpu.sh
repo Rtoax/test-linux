e58258b03a20ccc3a768a10501dad6cd1a823212
Rong Tao <rtoax@foxmail.com>
2026-07-05 16:30:11 +0800

    plotcake: tools: output specify txt/json file
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, x86_64
    Vers: Ubuntu 26.04, Linux 7.0.0-27-generic, Ncurses 6.6.20251231, bash 5.3.9

--- tools/ncurses/tools/tools/cpu.sh
+++ tools/ncurses/tools/tools/cpu.sh
@@ -62,4 +62,4 @@ while true; do
 		echo "${percent_idle[@]}"
 	fi
 	sleep ${I}
-done | ../plotcake --title 'CPU Loads %idle' --ylabel '%idle' ${labels[@]} ${@}
+done | ../plotcake --title 'CPU Loads %idle' --ylabel '%idle' -o cpu ${labels[@]} ${@}
