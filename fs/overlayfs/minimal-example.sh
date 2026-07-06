e81fb837bdb46b195e75ecc5c8922142781e3741
Rong Tao <rongtao@cestc.cn>
2026-07-06 15:02:08 +0800

    overlayfs: minimal-example.sh: rename var
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.14-201.fc44.x86_64, bash 5.3.9

--- fs/overlayfs/minimal-example.sh
+++ fs/overlayfs/minimal-example.sh
@@ -5,7 +5,7 @@ sudo dd if=/dev/zero of=lower.ext4 bs=4096 count=4096
 sudo mkfs -t ext4 lower.ext4
 sudo cp lower.ext4 upper.ext4
 
-mkdir lower upper overlay
+mkdir lower upper merged
 
 sudo mount lower.ext4 lower
 sudo mount upper.ext4 upper
@@ -20,19 +20,21 @@ printf 'upper-content' > upper/upper/upper-file
 
 # Work must be empty. E.g. this would be bad:
 #printf work-content > upper/work/work-file
+
 # Make the lower readonly to show that that is possible:
 # writes actually end up on the upper filesystem.
 sudo mount -o remount,ro lower.ext4 lower
 
 # Create the overlay mount.
-sudo mount -t overlay \
+sudo mount -t overlay overlay \
 	-o lowerdir=lower,upperdir=upper/upper,workdir=upper/work \
-	none \
-	overlay
+	merged
 
 # Interact with the mount.
-printf 'overlay-content' > overlay/overlay-file
-sudo tree lower upper/upper upper/work overlay
+printf 'overlay-content' > merged/overlay-file
+
+echo "--------- merged ---------"
+sudo tree lower upper/upper upper/work merged
 
 # Write to underlying directories while mounted
 # gives undefined behaviour.
@@ -40,11 +42,12 @@ sudo tree lower upper/upper upper/work overlay
 #printf upper-content-2 > upper/upper-file-2
 
 # Unmount the overlay and observe state.
-sudo umount overlay
-sudo tree lower upper/upper upper/work overlay
+sudo umount merged
+echo "--------- after umount merged ---------"
+sudo tree lower upper/upper upper/work merged
 
 # Cleanup.
 sudo umount upper
 sudo umount lower
 sudo rm lower.ext4 upper.ext4
-sudo rmdir lower upper overlay
+sudo rmdir lower upper merged
