e6c4235ccdbb6ef99a085abf7b1f3b461cd29975
Rong Tao <rongtao@cestc.cn>
2026-07-06 15:09:58 +0800

    overlayfs: minimal-example.sh: add funcs
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.0.14-201.fc44.x86_64, bash 5.3.9

--- fs/overlayfs/minimal-example.sh
+++ fs/overlayfs/minimal-example.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 set -e
 
-sudo dd if=/dev/zero of=lower.ext4 bs=4096 count=4096
-sudo mkfs -t ext4 lower.ext4
-sudo cp lower.ext4 upper.ext4
+create_fs() {
+	sudo dd if=/dev/zero of=lower.ext4 bs=4096 count=4096
+	sudo mkfs -t ext4 lower.ext4
+	sudo cp lower.ext4 upper.ext4
+}
 
-mkdir lower upper merged
+mount_fs() {
+	mkdir lower upper merged
 
-sudo mount lower.ext4 lower
-sudo mount upper.ext4 upper
-sudo chown "$USER:$USER" lower upper
+	sudo mount lower.ext4 lower
+	sudo mount upper.ext4 upper
+	sudo chown "$USER:$USER" lower upper
+}
+
+# Unmount the overlay and observe state.
+umount_fs() {
+	sudo umount merged
+	echo "--------- after umount merged ---------"
+	sudo tree lower upper/upper upper/work merged
+
+	sudo umount upper
+	sudo umount lower
+}
+
+create_fs
+mount_fs
 
 printf 'lower-content' > lower/lower-file
-mkdir lower/tmp/
+mkdir -p lower/tmp/
 
 # Note: workdir and upperdir must reside under the same mount.
-mkdir upper/{upper,work}
+mkdir -p upper/{upper,work}
 printf 'upper-content' > upper/upper/upper-file
 
 # Work must be empty. E.g. this would be bad:
@@ -30,7 +47,7 @@ sudo mount -t overlay overlay \
 	-o lowerdir=lower,upperdir=upper/upper,workdir=upper/work \
 	merged
 
-# Interact with the mount.
+# Create files in overlayfs
 printf 'overlay-content' > merged/overlay-file
 
 echo "--------- merged ---------"
@@ -41,13 +58,8 @@ sudo tree lower upper/upper upper/work merged
 #printf lower-content-2 > lower/lower-file-2
 #printf upper-content-2 > upper/upper-file-2
 
-# Unmount the overlay and observe state.
-sudo umount merged
-echo "--------- after umount merged ---------"
-sudo tree lower upper/upper upper/work merged
+umount_fs
 
 # Cleanup.
-sudo umount upper
-sudo umount lower
 sudo rm lower.ext4 upper.ext4
 sudo rmdir lower upper merged
