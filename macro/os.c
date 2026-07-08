39e0480dc2b1557169c55e9ad3d1363058e806af
Rong Tao <rtoax@foxmail.com>
2026-07-08 10:17:46 +0800

    macro/os.c: add __tencentos__
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: kvm
    Vers: TencentOS Server 3.1, Linux 5.4.119-19.0009.54_8, GCC 8.5.0, GLibc 2.28

--- macro/os.c
+++ macro/os.c
@@ -17,8 +17,10 @@ void display(void)
 	printf("OS is CentOS\n");
 #elif defined(__openEuler__)
 	printf("OS is openEuler\n");
+#elif defined(__tencentos__)
+	printf("OS is TencentOS\n");
 #else
-#error Must define OS_$distro
+#error Must define __${distro}__, see template/os.mk
 #endif
 	printf("VERSION_ID=%s\n", __stringify(OS_VERSION_ID));
 }
