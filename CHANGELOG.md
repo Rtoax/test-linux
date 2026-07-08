6b6ec0fa6cf7867f31f0bd8564aded847242c12d
Rong Tao <rongtao@cestc.cn>
2026-07-08 14:29:21 +0800

    clang: test #pragma clang loop unroll
    For example: __bpf_str_prepend() in bpftrace/stdlib:
    
        int i;
        int dst_len = (int)__bpf_strnlen(dst, dst_sz);
        int src_len = (int)__bpf_strnlen(src, src_sz);
    
        #pragma clang loop unroll(disable)
        for (i = 0; i < dst_len && dst[i] != '\0'; i++) {
            int idx = dst_len - i - 1;
            dst[src_len + idx] = dst[idx];
        }
        dst[src_len + dst_len] = '\0';
    
        #pragma clang loop unroll(disable)
        for (i = 0; i < src_len && i < (int)dst_sz && i < (int)src_sz && src[i] != '\0'; i++) {
            dst[i] = src[i];
        }
    
        return src_len;
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.1.3-200.fc44.x86_64, Clang 22.1.8, GCC 16.1.1, GLibc 2.43, Make 4.4.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to
 ## Unreleased
 #### Breaking Changes
 #### Added
+- clang: test #pragma clang loop unroll(disable);
 - patchset.sh: add --single-diff argument;
 - bpftrace/samples: add macro subdir;
 - scripts/git: add commit-message-stat.sh;
--- compiler/pragma/GCC_unroll.c
+++ compiler/pragma/GCC_unroll.c
@@ -6,6 +6,15 @@ int main(void)
 
 	#if __GNUC__ >= 8
 	#pragma GCC unroll 10
+	#elif defined(__clang__)
+	/* 'enable', 'full' or 'disable' */
+	#pragma clang loop unroll (enable)
+	#endif
+	for (i = 0; i < n; i++)
+		printf("%d\n", i);
+
+	#if defined(__clang__)
+	#pragma clang loop unroll (disable)
 	#endif
 	for (i = 0; i < n; i++)
 		printf("%d\n", i);
