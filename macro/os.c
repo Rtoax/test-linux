d88ebbd3d3a21f469903742cb80a5c2565844c2b
Rong Tao <rongtao@cestc.cn>
2026-07-08 11:00:13 +0800

    macro/os.c: add __almalinux__
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: kvm
    Vers: AlmaLinux 9.7, Linux 5.14.0-611.55.1.el9_7.x86_64, GCC 11.5.0, GLibc 2.34

--- macro/os.c
+++ macro/os.c
@@ -5,22 +5,24 @@ void display(void)
 {
 	/* see template/os.mk */
 	printf("ID=%s\n", __stringify(OS_ID));
-#ifdef __fedora__
-	printf("OS is Fedora\n");
-#elif defined(__ubuntu__)
-	printf("OS is Ubuntu\n");
-#elif defined(__debian__)
-	printf("OS is Debian\n");
+#ifdef __almalinux__
+	printf("OS is AlmaLinux\n");
 #elif defined(__cclinux__)
 	printf("OS is CCLinux\n");
 #elif defined(__centos__)
 	printf("OS is CentOS\n");
+#elif defined(__debian__)
+	printf("OS is Debian\n");
+#elif defined(__fedora__)
+	printf("OS is Fedora\n");
+#elif defined(__ubuntu__)
+	printf("OS is Ubuntu\n");
 #elif defined(__openEuler__)
 	printf("OS is openEuler\n");
-#elif defined(__tencentos__)
-	printf("OS is TencentOS\n");
 #elif defined(__opensuse_leap__)
 	printf("OS is openSUSE Leap\n");
+#elif defined(__tencentos__)
+	printf("OS is TencentOS\n");
 #else
 #error Must define __${distro}__, see template/os.mk
 #endif
