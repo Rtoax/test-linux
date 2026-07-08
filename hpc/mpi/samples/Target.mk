fc513c22ca932332ee1f483e7e4f9c38ff8fdf99
Rong Tao <rtoax@foxmail.com>
2026-07-08 22:00:50 +0800

    mpi/samples: Target.mk: post0: use 'timeout --signal KILL 1 --preserve-status'
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Vers: Ubuntu 26.04, Linux 7.0.0-27-generic, Make 4.4.1, MPICH 4.3.2, OpenMPI 5.0.10

--- hpc/mpi/samples/Target.mk
+++ hpc/mpi/samples/Target.mk
@@ -1,6 +1,8 @@
+tmout = timeout --signal KILL 1 --preserve-status
+
 post0:
 	$(call log_tgt,TEST,$(@))
-	$(Q)timeout 1 $(MPIRUN) -n 2 ./MPI_Init || true
-	$(Q)timeout 1 $(MPIRUN) -n 2 ./MPI_Send || true
-	$(Q)timeout 1 $(MPIRUN) -n 10 ./MPI_Send-2 || true
+	$(Q)${tmout} $(MPIRUN) -n 2 ./MPI_Init || true
+	$(Q)${tmout} $(MPIRUN) -n 2 ./MPI_Send || true
+	$(Q)${tmout} $(MPIRUN) -n 10 ./MPI_Send-2 || true
 	$(call log_tgt,TEST DONE,$(@))
