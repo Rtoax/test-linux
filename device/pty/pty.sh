0418d0ab714dbb84a521441bf9bf989d6f61b64f
Rong Tao <rongtao@cestc.cn>
2026-07-09 18:18:26 +0800

    pty/pty.sh: add hostname
    Hello from test-linux rongtao@NUC10, you are pty 4!
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44, Linux 7.1.3-200.fc44.x86_64, bash 5.3.9

--- device/pty/pty.sh
+++ device/pty/pty.sh
@@ -3,5 +3,5 @@ set -e
 
 for i in $(ls /dev/pts)
 do
-	echo "Hello from test-linux, you are pty $i!" | sudo tee /dev/pts/$i
+	echo "Hello from test-linux ${USER}@$(hostname), you are pty $i!" | sudo tee /dev/pts/$i
 done
