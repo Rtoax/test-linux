707a990b09f6bfda651d6577bcecf45e2d33e834
Rong Tao <rtoax@foxmail.com>
2026-07-09 19:52:51 +0800

    __attribute__/__mode__.c: Ignore error: unknown machine mode ‘__HF__’
     CC               .output/__mode__.o
    __mode__.c:4:1: error: unknown machine mode ‘__HF__’
        4 | typedef float fp16_t __attribute__ ((__mode__ (__HF__)));
          | ^~~~~~~
    make: *** [targets/c.mk:14: .output/__mode__.o] Error 1
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: kvm
    Vers: CentOS Stream 9, Linux 5.14.0-710.el9.x86_64, GCC 11.5.0, GLibc 2.34

--- compiler/__attribute__/__mode__.c
+++ compiler/__attribute__/__mode__.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-/* see /usr/include/bits/floatn-common.h */
+/**
+ * see /usr/include/bits/floatn-common.h
+ * typedef float _Float16 __attribute__ ((__mode__ (__HF__)));
+ */
+#if !__GNUC_PREREQ(7, 0) || (defined __cplusplus && !__GNUC_PREREQ(13, 0))
+#define have_mode_HF 1
+#endif
+
+#ifdef have_mode_HF
 typedef float fp16_t __attribute__ ((__mode__ (__HF__)));
+#endif
 
 int main(void)
 {
+#ifdef have_mode_HF
 	printf("size of fp16_t is %ld\n", sizeof(fp16_t));
+#endif
 	return 0;
 }
