4dd9a9b6a2083b24e0f7de60906928f6ef8d3a24
Rong Tao <rongtao@cestc.cn>
2026-07-10 11:14:07 +0800

    bpf/map.mk: add SUPPORT_BPF_MAP_TYPE_ARENA
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, kvm
    Vers: AlmaLinux 9.8, Linux 5.14.0-611.55.1.el9_7.x86_64, GCC 11.5.0, GLibc 2.34, LibBpf 1.5.0, Make 4.3, Pahole 1.31

--- template/bpf/map.mk
+++ template/bpf/map.mk
@@ -2,8 +2,19 @@
 # Copyright (C) 2026 Rong Tao
 #
 # Output definitions:
+# - SUPPORT_BPF_MAP_TYPE_ARENA=[y|n]
 #
 ifndef _BPF_MAP_MK
 _BPF_MAP_MK = 1
+
+include kernel.mk
+
+# linux v6.8-rc6-1938-g317460317a02 >= v6.9
+# commit 317460317a02 ("bpf: Introduce bpf_arena.")
+SUPPORT_BPF_MAP_TYPE_ARENA := $(call kver_gt,6,8,0)
+
+ifdef DEBUG
+  $(info SUPPORT_BPF_MAP_TYPE_ARENA = ${SUPPORT_BPF_MAP_TYPE_ARENA})
+endif
 
 endif
