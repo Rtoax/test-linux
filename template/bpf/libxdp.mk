c3756274aa42a9e12ca52445a2ec10ac0301e98a
Rong Tao <rongtao@cestc.cn>
2026-07-13 18:27:10 +0800

    bpf/libxdp.mk: use mk cache
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Vers: Fedora Linux 44, Linux 7.1.3-200.fc44.x86_64, LibBpf 1.8.0, LibXDP 1.6.0, Make 4.4.1

--- template/bpf/libxdp.mk
+++ template/bpf/libxdp.mk
@@ -11,7 +11,20 @@
 ifndef _BPF_LIBXDP_MK
 _BPF_LIBXDP_MK = 1
 
+include dir.mk
+include file.mk
+
+cachefile := ${TOPDIR}/template/bpf/.libxdp.mk.cache
+origfile := ${TOPDIR}/template/bpf/libxdp.mk
+
+ifeq ($(call is_newer,${cachefile},${origfile}),y)
+  include ${cachefile}
+else
+
 include define.mk
+include bits/mk-cache.mk
+
+$(call make_gen_cachefile,${cachefile})
 
 LIBXDP_H := /usr/include/xdp/libxdp.h
 XSK_H := /usr/include/xdp/xsk.h
@@ -30,6 +43,13 @@ endif
 ifneq (${HAVE_LIBXDP_XSK_H},)
   cflags-libxdp-y += -DHAVE_LIBXDP_XSK_H=1
 endif
+
+$(call make_append_var_to_file,HAVE_LIBXDP,${cachefile})
+$(call make_append_var_to_file,HAVE_LIBXDP_H,${cachefile})
+$(call make_append_var_to_file,HAVE_LIBXDP_XSK_H,${cachefile})
+$(call make_append_var_to_file,cflags-libxdp-y,${cachefile})
+
+endif # End of cachefile
 
 ifdef DEBUG
   $(info cflags-libxdp-y = ${cflags-libxdp-y})
