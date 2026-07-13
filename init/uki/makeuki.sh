4bfe09e406f1cbea63890f1d805c9d15e32b2b78
Rong Tao <rongtao@cestc.cn>
2026-07-13 16:05:01 +0800

    uki: makeuki.sh: add linux compile procedures comment
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Arch aarch64, kvm
    Vers: Fedora Linux 43, Linux 7.0.12-101.fc43.aarch64, Dracut 107-8, Grub 2.12, Make 4.4.1, bash 5.3.0

--- init/uki/makeuki.sh
+++ init/uki/makeuki.sh
@@ -4,24 +4,33 @@ set -ex
 
 MACHINE_ID=$(cat /etc/machine-id)
 KVER=$(make kernelrelease)
-UKI=/boot/efi/EFI/Linux/$MACHINE_ID-$KVER.efi
+UKI=/boot/efi/EFI/Linux/${MACHINE_ID}-${KVER}.efi
 
 use_ukify() {
 	sudo ukify build \
-		--linux=/boot/vmlinuz-$KVER \
-		--initrd=/boot/initramfs-$KVER.img \
+		--linux=/boot/vmlinuz-${KVER} \
+		--initrd=/boot/initramfs-${KVER}.img \
 		--cmdline=@/etc/kernel/cmdline \
 		--output=${UKI}
 }
 
 use_dracut() {
 	sudo dracut --uefi --force \
-		--kernel-image /boot/vmlinuz-$KVER \
-		--kver $KVER \
+		--kernel-image /boot/vmlinuz-${KVER} \
+		--kver ${KVER} \
 		${UKI}
 }
+
+# make menuconfig
+# make -j$(nproc)
+# sudo make modules_install
+# sudo make headers_install
+# sudo make install
 
 # Or use use_ukify()
 use_dracut
 
 sudo du -sh ${UKI}
+
+# sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+# sudo grubby --set-default /boot/vmlinuz-${KVER}
