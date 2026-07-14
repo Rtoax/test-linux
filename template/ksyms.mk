e2472bc1387488f0ae20dc644699bc05de816ad0
Rong Tao <rongtao@cestc.cn>
2026-07-14 15:41:53 +0800

    ksyms.mk: add ksyms_have_func()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Arch x86_64
    Vers: Fedora Linux 44, Linux 7.1.3-200.fc44.x86_64, Make 4.4.1, bash 5.3.9

--- template/ksyms.mk
+++ template/ksyms.mk
@@ -5,18 +5,37 @@
 # - KALLSYMS=[/proc/kallsyms]
 #
 # Functions:
+# - ksyms_have_func()=[y]
 #
-ifndef _KERNEL_MK
-_KERNEL_MK = 1
+ifndef _KSYMS_MK
+_KSYMS_MK = 1
 
 include shell.mk
 include sudo.mk
 
 KALLSYMS := /proc/kallsyms
 
+# Check kernel func
+# $1: function name, like 'schedule'
+#
+# @return: echo 'y' if found
+define ksyms_have_func
+$(shell \
+  cat ${KALLSYMS} | \
+    awk -v kfunc="${1}" '{ \
+      if ($$2=="t" || $$2=="T") { \
+        if ($$3 == kfunc) {print "y"} \
+      } \
+    }' \
+)
+endef
 
 ifdef DEBUG
   $(info KALLSYMS = ${KALLSYMS})
 endif
 
+ifneq ($(call ksyms_have_func,schedule),y)
+  $(error Not found function schedule from ${KALLSYMS})
 endif
+
+endif # end of _KSYMS_MK
