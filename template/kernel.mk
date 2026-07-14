15fbb9f5f7b4d1397b0fb3884030c02b4324b2f0
Rong Tao <rongtao@cestc.cn>
2026-07-14 10:33:28 +0800

    kernel.mk: test newest kernel
    [rongtao@71-fedora-39 libbpf]$ make
    kernel.mk:73: *** "call kver_uapi_lt failed, kuapi version >= 7.2.0".  Stop.
    [rongtao@71-fedora-39 libbpf]$ uname -r
    7.2.0-rc1+
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Arch aarch64, kvm
    Vers: Fedora Linux 39, Linux 7.2.0-rc1+, Make 4.4.1

--- template/kernel.mk
+++ template/kernel.mk
@@ -64,13 +64,13 @@ endif
 ifneq ($(call kver_uapi_gt,1,1,1),y)
   $(error "call kver_uapi_gt failed")
 endif
-# Newest kernel is v7.1
+# Newest kernel is v7.2.0 rc1
 # see https://github.com/torvalds/linux
 ifneq ($(call kver_lt,7,3,0),y)
   $(error "call kver_lt failed, kver >= 7.3.0")
 endif
-ifneq ($(call kver_uapi_lt,7,2,0),y)
-  $(error "call kver_uapi_lt failed, kuapi version >= 7.2.0")
+ifneq ($(call kver_uapi_lt,7,3,0),y)
+  $(error "call kver_uapi_lt failed, kuapi version >= 7.3.0")
 endif
 ifneq (${KVERSION_CODE},${kver_VERSION_CODE})
   $(error define_version failed for kver)
