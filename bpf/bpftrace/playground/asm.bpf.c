5528f4e620032e980412d5a6b8cc88773275202e
Rong Tao <rongtao@cestc.cn>
2026-07-16 15:39:42 +0800

    bpftrace: playground: asm.bpf.c: pass val to asm()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, kvm
    Vers: CentOS Stream 9, Linux 5.14.0-721.el9.x86_64, Binutils 2.35.2, BPFTrace 0.26.0, GCC 11.5.0, GLibc 2.34, LibBpf 1.8.0

--- bpf/bpftrace/playground/asm.bpf.c
+++ bpf/bpftrace/playground/asm.bpf.c
@@ -6,3 +6,13 @@ int __bpf_asm_ret(void)
   asm volatile ("r6 = 123\n" : "=r"(ret));
   return ret;
 }
+
+int __bpf_asm_ret_v2(void)
+{
+  register int ret asm("r6");
+  asm volatile ("r6 = %[val]\n"
+    : "=r"(ret)
+    : [val] "i" (123)
+    :);
+  return ret;
+}
