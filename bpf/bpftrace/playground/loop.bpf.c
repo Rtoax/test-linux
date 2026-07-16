a8eebc123993fa9096f0d3e4b85b808a9974f454
Rong Tao <rongtao@cestc.cn>
2026-07-16 15:49:39 +0800

    bpftrace/playground: loop.bpf.c: add struct callback_ctx {}
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, kvm
    Vers: CentOS Stream 9, Linux 5.14.0-721.el9.x86_64, BPFTrace 0.26.0, GCC 11.5.0, GLibc 2.34, LibBpf 1.8.0

--- bpf/bpftrace/playground/loop.bpf.c
+++ bpf/bpftrace/playground/loop.bpf.c
@@ -14,27 +14,34 @@
  */
 #define __KERNEL__
 
-static int loop_cnt = 0;
+struct callback_ctx {
+  int loop_cnt;
+};
 static int callback(unsigned int index, void *data)
 {
-  loop_cnt++;
+  struct callback_ctx *ctx = data;
+  ctx->loop_cnt++;
   return 0;
 }
 
 int __bpf_count(void)
 {
   int BPF_FUNC_loop = 181;
+  struct callback_ctx ctx = {
+    .loop_cnt = 0,
+  };
 
   /* bpf_loop(10, callback, 0, 0); */
   asm volatile (
       "r1 = 10\n"                      // BPF_MOV64_IMM(BPF_REG_1, 10)
       "r2 = %[callback] ll\n"          // BPF_MOV64_IMM(BPF_REG_2, callback)
-      "r3 = 0\n"                       // BPF_MOV64_IMM(BPF_REG_3, 0)
+      "r3 = %[callback_ctx]\n"         // BPF_MOV64_IMM(BPF_REG_3, callback_ctx)
       "r4 = 0\n"                       // BPF_MOV64_IMM(BPF_REG_4, 0)
       "call %[bpf_loop]\n"             // call bpf_loop()
       "r0 = %[cnt]\n"                  // BPF_MOV64_IMM(BPF_REG_0, cnt)
       :
       : [callback] "i" (callback),
+        [callback_ctx] "r" (&ctx),
         [bpf_loop] "i" (BPF_FUNC_loop),
 	[cnt] "i" (10) /* TODO: replace to loop_cnt */
       : "r0", "r1", "r2", "r3", "r4"
