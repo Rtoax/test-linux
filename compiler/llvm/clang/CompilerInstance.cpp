5d88e557aa98b406e693f889f3d2d9181da0ae2b
Rong Tao <rtoax@foxmail.com>
2026-07-17 18:17:32 +0800

    clang: CompilerInstance.cpp: add more from bpftrace
    see bpftrace/src/ast/passes/clang_build.cpp
    
    Bpftrace: commit cde97c2142f8 ("Fix mismatch between size_t and unsigned long on armhf (#5261)")
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: Arch x86_64
    Vers: Fedora Linux 44, Linux 7.1.3-201.fc44.x86_64, BPFTrace 0.26.0, Clang 22.1.8, GCC 16.1.1, GLibc 2.43, LibBpf 1.8.0, LLVM 22.1.8

--- compiler/llvm/clang/CompilerInstance.cpp
+++ compiler/llvm/clang/CompilerInstance.cpp
@@ -1,10 +1,46 @@
 #include <clang/Driver/Driver.h>
 #include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/TextDiagnosticPrinter.h>
 #include <iostream>
 
 int main(void)
 {
+	std::string errstr;
+	llvm::raw_string_ostream err(errstr);
+
+	llvm::IntrusiveRefCntPtr<llvm::vfs::InMemoryFileSystem> vfs(
+		new llvm::vfs::InMemoryFileSystem());
+
+	std::vector<const char *> args;
+	args.push_back("-O0");
+	args.push_back("-I.");
+
+	static std::shared_ptr<clang::DiagnosticOptions> diagOpts =
+		std::make_shared<clang::DiagnosticOptions>();
+	llvm::IntrusiveRefCntPtr<clang::DiagnosticIDs> diagID(
+		new clang::DiagnosticIDs());
+	auto client =
+		std::make_unique<clang::TextDiagnosticPrinter>(err, *diagOpts);
+	auto diags = std::make_unique<clang::DiagnosticsEngine>(
+		diagID, *diagOpts, client.release());
+
+	auto inv = std::make_shared<clang::CompilerInvocation>();
+	inv->getTargetOpts().Triple = "bpf";
+	inv->getCodeGenOpts().setDebugInfo(llvm::codegenoptions::FullDebugInfo);
+	inv->getCodeGenOpts().DebugColumnInfo = true;
+
+	clang::CompilerInvocation::CreateFromArgs(
+		*inv, llvm::ArrayRef<const char *>(args), *diags);
+
 	clang::CompilerInstance ci;
+	ci.getInvocation() = *inv;
+	ci.setDiagnostics(diags.release());
+	/* LLVM_VERSION_MAJOR >= 22 */
+	ci.setVirtualFileSystem(vfs);
+	ci.createFileManager();
+	ci.createSourceManager();
+
+	/* TODO: bpftrace/src/ast/passes/clang_build.cpp */
 
 	return 0;
 }
