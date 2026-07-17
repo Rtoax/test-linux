285a556cb11f4bce356c5a0c61ed83d4d5ea7d35
Rong Tao <rongtao@cestc.cn>
2026-07-17 13:38:09 +0800

    asm.bpf.c: add asm __bpf_helper_call()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, kvm
    Vers: CentOS Stream 9, Linux 5.14.0-721.el9.x86_64, Binutils 2.35.2, BPFTrace 0.26.0, GCC 11.5.0, GLibc 2.34, LibBpf 1.8.0

--- bpf/bpftrace/playground/asm.bpf.c
+++ bpf/bpftrace/playground/asm.bpf.c
@@ -1,4 +1,6 @@
 #define __KERNEL__
+#include <linux/types.h>
+#include <stddef.h>
 
 int __bpf_asm_ret(void)
 {
@@ -24,5 +26,32 @@ int __bpf_asm_ret_val(int val)
     : "=r"(ret)
     : [val] "r" (val)
     :);
+  return ret;
+}
+
+/**
+ * TODO: move it to bpf.h, included by bpf.c
+ */
+static int __bpf_helper_call(__u64 _nr_helper, __u64 arg0, __u64 arg1,
+                             __u64 arg2, __u64 arg3, __u64 arg4)
+{
+  register int ret asm("r0");
+
+  asm volatile (
+      "r1 = %[arg0] ll\n"
+      "r2 = %[arg1] ll\n"
+      "r3 = %[arg2] ll\n"
+      "r4 = %[arg3] ll\n"
+      "r5 = %[arg4] ll\n"
+      "call %[nr]\n"
+      : "=r"(ret)
+      : [arg0] "r" (arg0),
+        [arg1] "r" (arg1),
+        [arg2] "r" (arg2),
+        [arg3] "r" (arg3),
+        [arg4] "r" (arg4),
+        [nr] "i" (_nr_helper)
+      : "r0", "r1", "r2", "r3", "r4", "r5"
+  );
   return ret;
 }
