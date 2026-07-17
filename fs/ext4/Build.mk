75a44e28d787614b68168c1c6d99592b5ddb6def
Rong Tao <rtoax@foxmail.com>
2026-07-17 09:24:33 +0800

    ext4: add e2fsprogs subdir
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: Arch x86_64
    Vers: Fedora Linux 44, Linux 7.1.3-201.fc44.x86_64, e2fsprogs 1.47.3, Make 4.4.1, bash 5.3.9

--- fs/ext4/Build.mk
+++ fs/ext4/Build.mk
@@ -1,4 +1,8 @@
+subdir-y += e2fsprogs
+
 prog-y += mkfs.ext4.sh
 prog-y += mkfs.ext4.sh.1
+prog-y += mkfs.ext4.sh.2
 
 PROG_ARGS_mkfs.ext4.sh.1 := fstype ext3
+PROG_ARGS_mkfs.ext4.sh.2 := fstype ext2
