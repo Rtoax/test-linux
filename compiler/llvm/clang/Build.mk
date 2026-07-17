16e2369be50c2b8b15d182a7f0d36c1eb64905de
Rong Tao <rtoax@foxmail.com>
2026-07-17 18:03:23 +0800

    clang: CompilerInstance.cpp: link -lclang-cpp
    rongtao@NUC10:/home/sda/git-repos/rtoax/test-linux/compiler/llvm/clang$ make V=1
    make: Entering directory '/home/sda/git-repos/rtoax/test-linux/compiler/llvm/clang'
     CLANG++ LD       CompilerInstance
    clang++ -o CompilerInstance .output/CompilerInstance.cpp.o -L/usr/lib64/llvm22/lib64  -lLLVM-22
    /usr/bin/ld.bfd: .output/CompilerInstance.cpp.o: in function `main':
    CompilerInstance.cpp:(.text+0x57): undefined reference to `clang::CompilerInstance::CompilerInstance(std::shared_ptr<clang::CompilerInvocation>, std::shared_ptr<clang::PCHContainerOperations>, std::shared_ptr<clang::ModuleCache>)'
    /usr/bin/ld.bfd: CompilerInstance.cpp:(.text+0x8e): undefined reference to `clang::CompilerInstance::~CompilerInstance()'
    /usr/bin/ld.bfd: .output/CompilerInstance.cpp.o: in function `clang::CompilerInvocation::CompilerInvocation()':
    CompilerInstance.cpp:(.text._ZN5clang18CompilerInvocationC2Ev[_ZN5clang18CompilerInvocationC2Ev]+0x11): undefined reference to `clang::CompilerInvocationBase::CompilerInvocationBase()'
    /usr/bin/ld.bfd: .output/CompilerInstance.cpp.o: in function `void std::_Construct<clang::PCHContainerOperations>(clang::PCHContainerOperations*)':
    CompilerInstance.cpp:(.text._ZSt10_ConstructIN5clang22PCHContainerOperationsEJEEvPT_DpOT0_[_ZSt10_ConstructIN5clang22PCHContainerOperationsEJEEvPT_DpOT0_]+0x11): undefined reference to `clang::PCHContainerOperations::PCHContainerOperations()'
    clang++: error: linker command failed with exit code 1 (use -v to see invocation)
    make: *** [targets/cpp.mk:29: CompilerInstance] Error 1
    make: Leaving directory '/home/sda/git-repos/rtoax/test-linux/compiler/llvm/clang'
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    Envs: Arch x86_64
    Vers: Fedora Linux 44, Linux 7.1.3-201.fc44.x86_64, Clang 22.1.8, GCC 16.1.1, LLVM 22.1.8, Make 4.4.1

--- compiler/llvm/clang/Build.mk
+++ compiler/llvm/clang/Build.mk
@@ -6,3 +6,4 @@ target-cpp-y += CompilerInstance
 CXXFLAGS += -Wno-error=unused-command-line-argument
 CXXFLAGS += $(shell llvm-config --cxxflags --system-libs --libs core)
 LDXXFLAGS += $(shell llvm-config --ldflags --system-libs --libs core)
+LDXXFLAGS += -lclang-cpp
