d2e07e29d0cfc834ec885a782db5b55fa90616d7
Rong Tao <rongtao@cestc.cn>
2026-07-22 08:58:58 +0000

    cxl: dax0.0: pmem_map_file.c: len=0 for auto-size
    Error:
    
        $ sudo ./pmem_map_file
        pmem_map_file failed: Invalid argument
    
    Info:
    
    $ sudo daxctl list --human --regions --devices --mappings
    {
      "path":"\/platform\/ACPI0017:00\/root0\/nvdimm-bridge0\/ndbus0\/region0\/dax0.0",
      "id":0,
      "size":"1006.00 MiB (1054.87 MB)",
      "align":2097152,
      "devices":[
        {
          "chardev":"dax0.0",
          "size":"1006.00 MiB (1054.87 MB)",
          "target_node":1,
          "align":2097152,
          "mode":"devdax",
          "mappings":[
            {
              "page_offset":"0",
              "start":"0xa91200000",
              "end":"0xacfffffff",
              "size":"1006.00 MiB (1054.87 MB)"
            }
          ]
        }
      ]
    }
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Arch x86_64, kvm
    Vers: Fedora Linux 44 (Forty Four), Linux 7.1.4-200.fc44_cxlvm.x86_64, GCC 16.1.1, GLibc 2.43, Jq 1.8.1, json-c 5.4.0, Libpmem 2.1.0, RPM 6.0.1

--- mm/pmem/libpmem/pmem_map_file.c
+++ mm/pmem/libpmem/pmem_map_file.c
@@ -3,21 +3,22 @@
 
 /* "/dev/dax0.0" was created by cxl */
 #define DAX_DEVICE "/dev/dax0.0"
-#define MAP_SIZE (1 * 1024 * 1024 * 1024UL)
 
 int main(void)
 {
-	void *pmem = pmem_map_file(DAX_DEVICE, MAP_SIZE, 0, 0666, NULL, NULL);
+	/* 0: auto size */
+	void *pmem = pmem_map_file(DAX_DEVICE, 0, 0, 0666, NULL, NULL);
 	if (pmem == NULL) {
 		perror("pmem_map_file failed");
 		return 1;
 	}
 
 	int *data = (int *)pmem;
+	printf("Origin data: 0x%x\n", data[0]);
 	data[0] = 0xdeadbeef;
 	pmem_persist(data, sizeof(int));
-	printf("Read data: 0x%x\n", data[0]);
+	printf("Write data: 0x%x\n", data[0]);
 
-	pmem_unmap(pmem, MAP_SIZE);
+	pmem_unmap(pmem, 0);
 	return 0;
 }
