c47362532db571b5c60a6a47bd2fdb7965fbd028
Rong Tao <rongtao@cestc.cn>
2026-07-28 08:47:10 +0800

    macro: arch.c: uniq sizeof() print
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.5-200.fc44.x86_64, GCC 16.1.1, GLibc 2.43

--- macro/arch.c
+++ macro/arch.c
@@ -3,39 +3,34 @@
 
 int main(void)
 {
-	int __unused *p = NULL;
-
 #if defined(__i386__)
 	printf("x86-32 ");
-	printf("sizeof(*) = %d\n", sizeof(p));
 #elif defined(__x86_64__)
 	printf("x86-64 ");
-	printf("sizeof(*) = %ld\n", sizeof(p));
 #elif defined(__aarch64__)
 	printf("aarch64 ");
-	printf("sizeof(*) = %ld\n", sizeof(p));
 #elif defined(__riscv)
 # if __riscv_xlen == 64
 	printf("riscv64 ");
 # else
 	printf("riscv32 ");
 # endif
-	printf("sizeof(*) = %ld\n", sizeof(p));
 #elif defined(__loongarch64)
 	printf("loongarch64 ");
-	printf("sizeof(*) = %ld\n", sizeof(p));
 #elif defined(__powerpc64__)
-#if defined(__PPC64__)
-	printf("__PPC64__: ");
-#endif
 	printf("ppc64 ");
-	printf("sizeof(*) = %ld\n", sizeof(p));
+#if defined(__PPC64__)
+	printf("(PPC64) ");
+#endif
 #elif defined(__s390x__)
 	printf("s390x ");
 #if defined(__s390__)
 	printf("(s390) ");
 #endif
-	printf("sizeof(*) = %ld\n", sizeof(p));
 #endif
+
+#define szof(x) printf("%s = %d\n", #x, (int)x);
+	szof(sizeof(void *));
+
 	return 0;
 }
