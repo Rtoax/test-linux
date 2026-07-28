5995adc525b4c910a9fe397991ed308faaae36db
Rong Tao <rongtao@cestc.cn>
2026-07-28 09:16:44 +0800

    pkg-config.mk: add PKG_CONFIG and HAVE_PKG_CONFIG
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Arch x86_64
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.5-200.fc44.x86_64, Jq 1.8, json-c 5.4.0, KMod 34.2, Make 4.4.1, PKG-Config 2.5.1, bash 5.3.9

--- template/pkg-config.mk
+++ template/pkg-config.mk
@@ -1,6 +1,36 @@
 # SPDX-License-Identifier: GPL-3.0
 #
+# Definitions:
+# - HAVE_PKG_CONFIG=[y]
+# - PKG_CONFIG=[/usr/bin/pkg-config]
+#
+# Functions:
+# - pkg_config_cflags()
+# - pkg_config_libs()
+#
 ifndef _PKG_CONFIG_MK
 _PKG_CONFIG_MK = 1
+
+include define.mk
+
+$(call find_cmd_and_def,pkg-config)
+
+ifeq (${HAVE_PKG_CONFIG},y)
+  # $1: module, such as: json-c
+  define pkg_config_cflags
+  $(shell ${PKG_CONFIG} --cflags ${1})
+  endef
+  # $1: module, such as: json-c
+  define pkg_config_libs
+  $(shell ${PKG_CONFIG} --libs ${1})
+  endef
+else
+  define pkg_config_cflags
+  $(warning Not found pkg-config)
+  endef
+  define pkg_config_libs
+  $(warning Not found pkg-config)
+  endef
+endif
 
 endif
