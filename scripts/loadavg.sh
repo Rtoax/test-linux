a76483416b4a7d71b2e3ee23a1c54733b4ba2c30
Rong Tao <rongtao@cestc.cn>
2026-07-29 11:19:16 +0800

    scripts/loadavg.sh: update comment of plotcake
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Arch x86_64
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.5-200.fc44.x86_64, Ncurses 6.6.20251230, bash 5.3.9

--- scripts/loadavg.sh
+++ scripts/loadavg.sh
@@ -11,8 +11,8 @@
 #     awk '{print $1, $2}' /proc/loadavg
 #   done | ttyplot -t "Load Averages (1m, 5m)" -u "load" -2
 #
-# TODO: `while :; do tput; print; done` mode is too slow, we should use pure C.
-# see also tools/ncurses/loadavg.c.
+# Note: `while :; do tput; print; done` mode is too slow, we should use pure C.
+# see tools/ncurses/tools/plotcake.c.
 set -e
 
 if [[ ${VERBOSE} ]]; then
