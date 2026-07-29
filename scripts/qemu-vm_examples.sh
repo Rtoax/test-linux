8990ba157b5dedb26fac7fad1ea148a220f090a8
Rong Tao <rongtao@cestc.cn>
2026-07-29 12:20:26 +0800

    qemu-vm_examples.sh: test all builtin devices
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Arch x86_64
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.5-200.fc44.x86_64, Expect 5.45.4, NDCtl 85, Qemu 10.2.2, bash 5.3.9

--- scripts/qemu-vm_examples.sh
+++ scripts/qemu-vm_examples.sh
@@ -32,6 +32,19 @@ run --initrd=initramfs.img --rdinit=/bin/bash --rootfs vm.qcow2
 readonly CXL_DEVS=( cxl-vmem cxl-vmem-lsa cxl-vmem-4way cxl-vmem-4way-switch
 			cxl-pmem cxl-pmem-4way cxl-pmem-4way-switch )
 
+# Test CXL builtin devices
+dev_list1=( $(run --cxl device=list) )
+dev_list2=( $(run --cxl device=?) )
+if [[ "${CXL_DEVS[@]}" != "${dev_list1[@]}" ]] ||
+   [[ "${CXL_DEVS[@]}" != "${dev_list2[@]}" ]] ||
+   [[ "${dev_list1[@]}" != "${dev_list2[@]}" ]]; then
+	error "cxl: get device list failed, expect '${CXL_DEVS[@]}' but get '${dev_list1[@]}' and '${dev_list2[@]}'"
+fi
+for dev in ${CXL_DEVS[@]}; do
+	run --memory 5GiB --cxl device=${dev}
+done
+
+# Test CXL custom device
 run --cxl pxb=pxb.1
 run --cxl pxb=pxb.1 --cxl pxb=pxb.2,fmw=1
 run --cxl pxb=pxb.1 --cxl pxb=pxb.2,fmw=1 \
@@ -42,11 +55,3 @@ run --cxl pxb=pxb.1 --cxl pxb=pxb.2,fmw=1 \
 	--cxl pxb=pxb.4,fixed-memory-window=4 \
 	--cxl rp=rp.1,bus=pxb.1,port=1 \
 	--cxl root-port=rp.2,bus=pxb.2,port=1
-
-dev_list1=( $(run --cxl device=list) )
-dev_list2=( $(run --cxl device=?) )
-if [[ "${CXL_DEVS[@]}" != "${dev_list1[@]}" ]] ||
-   [[ "${CXL_DEVS[@]}" != "${dev_list2[@]}" ]] ||
-   [[ "${dev_list1[@]}" != "${dev_list2[@]}" ]]; then
-	error "cxl: get device list failed, expect '${CXL_DEVS[@]}' but get '${dev_list1[@]}' and '${dev_list2[@]}'"
-fi
