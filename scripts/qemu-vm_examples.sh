78eed76594c581d93126540d5ed12fa59b82b2c2
Rong Tao <rongtao@cestc.cn>
2026-07-29 11:28:48 +0800

    qemu-vm_examples.sh: test more cxl
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Arch x86_64
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.5-200.fc44.x86_64, NDCtl 85, Qemu 10.2.2, bash 5.3.9

--- scripts/qemu-vm_examples.sh
+++ scripts/qemu-vm_examples.sh
@@ -17,6 +17,7 @@ qemu --cxl help
 qemu --disk help
 qemu --uefi help
 
+# Test Basic
 run
 run --memory 4GiB
 run --cpu 10
@@ -24,6 +25,14 @@ run --cpu nr=10
 run --cpu model=base
 run --initrd=initramfs.img --rdinit=/bin/bash --rootfs vm.qcow2
 
+# Test CXL
 run --cxl pxb=pxb.1
 run --cxl pxb=pxb.1 --cxl pxb=pxb.2,fmw=1
-run --cxl pxb=pxb.1 --cxl pxb=pxb.2,fmw=1 --cxl pxb=pxb.3,fixed-memory-window=2
+run --cxl pxb=pxb.1 --cxl pxb=pxb.2,fmw=1 \
+	--cxl pxb=pxb.3,fixed-memory-window=2 \
+	--cxl pxb=pxb.4,fixed-memory-window=4
+run --cxl pxb=pxb.1 --cxl pxb=pxb.2,fmw=1 \
+	--cxl pxb=pxb.3,fixed-memory-window=2 \
+	--cxl pxb=pxb.4,fixed-memory-window=4 \
+	--cxl rp=rp.1,bus=pxb.1,port=1 \
+	--cxl root-port=rp.2,bus=pxb.2,port=1
