926a1a3f157d1f594923f2ffce2d4c1c34646676
Rong Tao <rongtao@cestc.cn>
2026-07-30 15:48:40 +0800

    oom.c: update code format
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: Arch x86_64
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.5-200.fc44.x86_64, GCC 16.1.1, GLibc 2.43

--- mm/oom/oom.c
+++ mm/oom/oom.c
@@ -62,15 +62,19 @@ const char argp_prog_doc[] =
 ;
 
 static const struct argp_option opts[] = {
-	{ "operation", 'e', "OPERATION", 0, "specify operation, glibc, mmap-anon, mmap-file" },
-	{ "size", 's', "SIZE", 0, "only allocate size of memory, instead of oom, suffix KB, MB, GB" },
+	{ "operation", 'e', "OPERATION", 0,
+	  "specify operation, glibc, mmap-anon, mmap-file" },
+	{ "size", 's', "SIZE", 0,
+	  "only allocate size of memory, instead of oom, suffix KB, MB, GB" },
 	{ "rate", 'r', "RATE", 0, "limit the alloc rate, suffix KB, MB, GB" },
 	{ "popen", 'p', NULL, 1, "test popen(3) after memory" },
 	{ "verbose", 'v', NULL, 1, "display detail" },
 	{ "oom_adj", 'a', "OOM_ADJ", 0, "set oom_adj (-17 to 15)" },
-	{ "oom_score_adj", 'c', "OOM_SCORE_ADJ", 0, "set oom_score_adj (-1000 to 1000)" },
+	{ "oom_score_adj", 'c', "OOM_SCORE_ADJ", 0,
+	  "set oom_score_adj (-1000 to 1000)" },
 #ifdef HAVE_LIBCGROUP
-	{ "memcg-size", 'M', "MEMCG_SIZE", 0, "create and attach to memory cgroup, suffix KB, MB, GB" },
+	{ "memcg-size", 'M', "MEMCG_SIZE", 0,
+	  "create and attach to memory cgroup, suffix KB, MB, GB" },
 #endif
 	{},
 };
@@ -86,7 +90,8 @@ static error_t parse_arg(int key, char *arg, struct argp_state *state)
 		else if (!strcmp(arg, "mmap-file"))
 			env.ops_type = OP_MMAP_FILE;
 		else {
-			fprintf(stderr, "ERROR: operation only glibc, mmap-anon, mmap-file");
+			fprintf(stderr,
+				"ERROR: operation only glibc, mmap-anon, mmap-file");
 			exit(EXIT_FAILURE);
 		}
 		break;
@@ -273,10 +278,13 @@ void hold_mem(struct oom_operations *ops)
 
 	if (size >= totalram()) {
 		if (size >= totalram() + totalswap()) {
-			fprintf(stderr, "\033[31mERROR: alloc > ram + swap\033[m\n");
+			fprintf(stderr,
+				"\033[31mERROR: alloc > ram + swap\033[m\n");
 			exit(EXIT_FAILURE);
 		}
-		fprintf(stderr, "\033[31mWARNING: Trying to alloc memory size bigger than system RAM size. May use Swap\033[m\n");
+		fprintf(stderr,
+			"\033[31mWARNING: Trying to alloc memory size bigger "
+			"than system RAM size. May use Swap\033[m\n");
 	}
 
 	fprintf(stderr, "Hold %ld B (%ldKiB, %ldMiB) of memory\n", size,
@@ -394,22 +402,23 @@ int memcg_limit(void)
 
 	err = cgroup_set_value_uint64(memcg, MEMORY_LIMIT, env.limit_in_bytes);
 	if (err) {
-		fprintf(stderr, "Error setting memory limit: %s, %s\n", CGRP_NAME,
-			cgroup_strerror(cgroup_get_last_errno()));
+		fprintf(stderr, "Error setting memory limit: %s, %s\n",
+			CGRP_NAME, cgroup_strerror(cgroup_get_last_errno()));
 		goto free;
 	}
 
 	/* Forbidden swap */
 	err = cgroup_set_value_uint64(memcg, MEMORY_SWAP_LIMIT, 0);
 	if (err) {
-		fprintf(stderr, "Error setting memory swap limit: %s, %s\n", CGRP_NAME,
-			cgroup_strerror(cgroup_get_last_errno()));
+		fprintf(stderr, "Error setting memory swap limit: %s, %s\n",
+			CGRP_NAME, cgroup_strerror(cgroup_get_last_errno()));
 		goto free;
 	}
 
 	cgroup_get_value_uint64(memcg, MEMORY_LIMIT, &limit_in_bytes);
 	if (env.verbose) {
-		printf("Set "MEMORY_LIMIT" to %ld MB\n", limit_in_bytes / 1024 / 1024);
+		printf("Set " MEMORY_LIMIT " to %ld MB\n",
+		       limit_in_bytes / 1024 / 1024);
 	}
 
 	err = cgroup_create_cgroup(cgrp, 0);
@@ -424,8 +433,8 @@ int memcg_limit(void)
 	}
 	err = cgroup_attach_task_pid(cgrp, getpid());
 	if (err) {
-		fprintf(stderr, "Error attaching task to cgroup: %s, %s\n", CGRP_NAME,
-			cgroup_strerror(cgroup_get_last_errno()));
+		fprintf(stderr, "Error attaching task to cgroup: %s, %s\n",
+			CGRP_NAME, cgroup_strerror(cgroup_get_last_errno()));
 		goto delete;
 	}
 
@@ -484,7 +493,8 @@ int main(int argc, char *argv[])
 
 	if (env.verbose) {
 		char comm[64];
-		printf("pid %d, comm %s\n", getpid(), proc_comm(comm, sizeof(comm)));
+		printf("pid %d, comm %s\n", getpid(),
+		       proc_comm(comm, sizeof(comm)));
 		printf("oom_adj %d\n", get_oom_adj(getpid()));
 		printf("oom_score_adj %d\n", get_oom_score_adj(getpid()));
 		printf("oom_score %d\n", get_oom_score(getpid()));
@@ -517,6 +527,5 @@ int main(int argc, char *argv[])
 	}
 
 	memcg_release();
-
 	return 0;
 }
