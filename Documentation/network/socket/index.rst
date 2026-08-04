e2852d06097505da9686e44320ae204bfdac0d56
Rong Tao <rongtao@cestc.cn>
2026-08-04 18:50:55 +0800

    mctp: first commit
    Link: https://docs.kernel.org/networking/mctp.html
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.5-201.fc44.x86_64, GLibc 2.43, LateXmk 4.88, Make 4.4.1, NDCtl 85, Python 3.14.6, Sphinx 8.2.3

--- Documentation/network/socket/index.rst
+++ Documentation/network/socket/index.rst
@@ -20,6 +20,8 @@ Syscall socket(2)
      - IPv6网络协议
    * - PF_LOCAL/AF_LOCAL
      - UNIX协议
+   * - PF_MCTP/AF_MCTP
+     - Management Component Transport Protocol
 
 
 .. list-table::
--- ipc/socket/Build.mk
+++ ipc/socket/Build.mk
@@ -1,13 +1,14 @@
-subdir-y := poll
 subdir-y += epoll
 subdir-y += iovec
-subdir-y += proxy
-subdir-y += syscall
-subdir-y += unix
-subdir-y += tcp
-subdir-y += sctp
-subdir-y += udp
-subdir-y += vsock
 subdir-y += ipv6
+subdir-y += mctp
+subdir-y += poll
+subdir-y += proxy
+subdir-y += sctp
+subdir-y += syscall
+subdir-y += tcp
+subdir-y += udp
+subdir-y += unix
+subdir-y += vsock
 
 target-y := sockaddr-size
