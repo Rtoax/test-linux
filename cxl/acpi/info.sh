feaefb76110b4909a1ee9554cd4146ebba072b37
Rong Tao <rongtao@cestc.cn>
2026-08-05 23:25:12 +0000

    cxl: acpi: list /sys/bus/acpi/devices/ACPI0017:*
    $ tree /sys/bus/acpi/devices/ACPI0017:*
    /sys/bus/acpi/devices/ACPI0017:00
    ├── hid
    ├── modalias
    ├── path
    ├── physical_node -> ../../../platform/ACPI0017:00
    ├── power
    │   ├── autosuspend_delay_ms
    │   ├── control
    │   ├── runtime_active_time
    │   ├── runtime_status
    │   └── runtime_suspended_time
    ├── status
    ├── subsystem -> ../../../../bus/acpi
    ├── uevent
    └── wakeup
        └── wakeup17
            ├── active_count
            ├── active_time_ms
            ├── device -> ../../../ACPI0017:00
            ├── event_count
            ├── expire_count
            ├── last_change_ms
            ├── max_time_ms
            ├── name
            ├── prevent_suspend_time_ms
            ├── subsystem -> ../../../../../../class/wakeup
            ├── total_time_ms
            ├── uevent
            └── wakeup_count
    
    8 directories, 21 files
    
    $ realpath /sys/bus/acpi/devices/ACPI0017:*
    /sys/devices/LNXSYSTM:00/LNXSYBUS:00/ACPI0017:00
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Envs: CPU: Hygon C86 7265 24-core Processor, Arch x86_64, kvm
    Vers: Fedora Linux 44 (Forty Four), Linux 6.14.11-300.fc42_cxl.x86_64, acpica-tools 20260408, ACPITool 0.5.1, Findutils 4.10.0, NDCtl 85, bash 5.3.9

--- cxl/acpi/info.sh
+++ cxl/acpi/info.sh
@@ -14,11 +14,16 @@ if [[ -f cedt.dat ]]; then
 	cat cedt.dsl
 fi
 
-sudo ls /sys/kernel/debug/cxl
+LS=ls
+[[ $(which find) ]] && LS=find
+[[ $(which tree) ]] && LS=tree
+
+sudo ${LS} /sys/kernel/debug/cxl
 
 # CXL Root Port or Device on Switch
-sudo ls /sys/bus/acpi/devices/ACPI0017:*
+#
+sudo ${LS} /sys/bus/acpi/devices/ACPI0017:*
 # commit c8687bc871c5 ("cxl: list /sys/devices/platform/ACPI0017:00/")
-sudo ls /sys/devices/platform/ACPI0017*
+sudo ${LS} /sys/devices/platform/ACPI0017*
 # CXL Host Bridge (CHBS: CXL Host Bridge Structure)
-sudo ls /sys/bus/acpi/devices/ACPI0016:*
+sudo ${LS} /sys/bus/acpi/devices/ACPI0016:*
