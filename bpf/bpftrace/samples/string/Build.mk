7f4e078b5ff71eea368f358e23d7af58f016a7bf
Rong Tao <rongtao@cestc.cn>
2026-08-11 17:10:44 +0800

    bpftrace/samples: string: add map.bt test
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 24.04.4 LTS, Linux 6.17.0-1014-nvidia, BPFTrace 0.26.0, LibBpf 1.3.0, Make 4.3
    Envs: CPU: Cortex-X925 Cortex-A725, Arch aarch64

--- bpf/bpftrace/samples/string/Build.mk
+++ bpf/bpftrace/samples/string/Build.mk
@@ -3,6 +3,7 @@ include bpf/bpftrace.mk
 bpftrace-y += assignment.bt
 bpftrace-y += base.bt
 bpftrace-y += let.bt
+bpftrace-y += map.bt
 bpftrace-y += strcap.bt
 bpftrace-y += strlen.bt
 bpftrace-y += str_concat.bt
