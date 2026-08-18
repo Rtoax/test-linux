2c586595e8be576989bda63cf7553f9fe49b9d31
Rong Tao <rongtao@cestc.cn>
2026-08-18 16:02:26 +0800

    qemu-vm_examples.sh: test all --cxl args
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.8-200.fc44.x86_64, Libpmem 2.1.0, NDCtl 85, Qemu 10.2.2, bash 5.3.9
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64

--- scripts/qemu-vm_examples.sh
+++ scripts/qemu-vm_examples.sh
@@ -78,13 +78,84 @@ for dev in ${CXL_DEVS[@]}; do
 done
 
 # Test CXL custom device
-run --cxl pxb=pxb.1
-run --cxl pxb=pxb.1 --cxl pxb=pxb.2,fmw=1
-run --cxl pxb=pxb.1 --cxl pxb=pxb.2,fmw=1 \
-	--cxl pxb=pxb.3,fixed-memory-window=2 \
-	--cxl pxb=pxb.4,fixed-memory-window=4
-run --cxl pxb=pxb.1 --cxl pxb=pxb.2,fmw=1 \
-	--cxl pxb=pxb.3,fixed-memory-window=2 \
-	--cxl pxb=pxb.4,fixed-memory-window=4 \
-	--cxl rp=rp.1,bus=pxb.1,port=1 \
-	--cxl root-port=rp.2,bus=pxb.2,port=1
+cxl_pxb1() {
+	run --cxl pxb=pxb.1 "${@}"
+}
+cxl_pxb2() {
+	cxl_pxb1 --cxl pxb=pxb.2,fmw=1 "${@}"
+}
+cxl_pxb4() {
+	cxl_pxb2 \
+		--cxl pxb=pxb.3,fixed-memory-window=2 \
+		--cxl pxb=pxb.4,fixed-memory-window=4 \
+		"${@}"
+}
+cxl_pxb4_rp4() {
+	cxl_pxb4 \
+		--cxl rp=rp.1,bus=pxb.1,port=1 \
+		--cxl root-port=rp.2,bus=pxb.2,port=1 \
+		--cxl root-port=rp.3,bus=pxb.3,port=1 \
+		--cxl root-port=rp.4,bus=pxb.4,port=1 \
+		"${@}"
+}
+cxl_pxb4_rp4_sw4() {
+	cxl_pxb4_rp4 \
+		--cxl switch,bus=rp.1,nport=4,portprefix=sw1 \
+		--cxl switch,bus=rp.2,nport=4,portprefix=sw2 \
+		--cxl switch,bus=rp.3,nport=4,portprefix=sw3 \
+		--cxl switch,bus=rp.4,nport=4,portprefix=sw4 \
+		"${@}"
+}
+cxl_pxb4_rp4_sw4_pmem4() {
+	cxl_pxb4_rp4_sw4 \
+		--cxl pmem=pmem.1,bus=sw1.1,lsa=pmem.1.lsa,size=2G \
+		--cxl pmem=pmem.2,bus=sw1.2,lsa=pmem.2.lsa,size=2G \
+		--cxl pmem=pmem.3,bus=sw1.3,lsa=pmem.3.lsa,size=2G \
+		--cxl pmem=pmem.4,bus=sw1.4,lsa=pmem.4.lsa,size=2G \
+		"${@}"
+}
+cxl_pxb4_rp4_pmem4() {
+	cxl_pxb4_rp4 \
+		--cxl pmem=pmem.1,bus=rp.1,lsa=pmem.1.lsa,size=2G \
+		--cxl pmem=pmem.2,bus=rp.2,lsa=pmem.2.lsa,size=2G \
+		--cxl pmem=pmem.3,bus=rp.3,lsa=pmem.3.lsa,size=2G \
+		--cxl pmem=pmem.4,bus=rp.4,lsa=pmem.4.lsa,size=2G \
+		"${@}"
+}
+cxl_pxb4_rp4_vmem4() {
+	cxl_pxb4_rp4 \
+		--cxl vmem=vmem.1,bus=rp.1,size=2G \
+		--cxl vmem=vmem.2,bus=rp.2,size=2G \
+		--cxl vmem=vmem.3,bus=rp.3,lsa=vmem.3.lsa,size=2G \
+		--cxl vmem=vmem.4,bus=rp.4,lsa=vmem.4.lsa,size=2G \
+		"${@}"
+}
+cxl_pxb4_rp4_sw4_vmem4() {
+	cxl_pxb4_rp4_sw4 \
+		--cxl vmem=vmem.1,bus=sw1.1,size=2G \
+		--cxl vmem=vmem.2,bus=sw1.2,size=2G \
+		--cxl vmem=vmem.3,bus=sw1.3,lsa=vmem.3.lsa,size=2G \
+		--cxl vmem=vmem.4,bus=sw1.4,lsa=vmem.4.lsa,size=2G \
+		"${@}"
+}
+cxl_pxb4_rp4_sw4_vmem4_dc() {
+	cxl_pxb4_rp4_sw4 \
+		--cxl vmem=vmem.1,bus=sw1.1,size=2G,dc \
+		--cxl vmem=vmem.2,bus=sw1.2,size=2G,dc \
+		--cxl vmem=vmem.3,bus=sw1.3,lsa=vmem.3.lsa,size=2G,dynamic-capacity \
+		--cxl vmem=vmem.4,bus=sw1.4,lsa=vmem.4.lsa,size=2G,dynamic-capacity \
+		"${@}"
+}
+
+cxl_pxb1
+cxl_pxb2
+cxl_pxb4
+cxl_pxb4_rp4
+cxl_pxb4_rp4_sw4
+cxl_pxb4_rp4_pmem4
+cxl_pxb4_rp4_sw4_pmem4
+cxl_pxb4_rp4_vmem4
+cxl_pxb4_rp4_sw4_vmem4
+cxl_pxb4_rp4_sw4_vmem4_dc
+
+echo -e "\nDONE"
