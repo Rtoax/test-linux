54593f9e7c04a1da7c317aace87a7e7b13edd0b9
Rong Tao <rongtao@cestc.cn>
2026-08-18 09:14:57 +0800

    libnbd.sh: move qemu/qemu-nbd.sh content to libnbd.sh
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.8-200.fc44.x86_64, e2fsprogs 1.47.3, KMod 34.2, Qemu 10.2.2, bash 5.3.9
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64

--- scripts/libnbd.sh
+++ scripts/libnbd.sh
@@ -6,20 +6,49 @@ readonly KMOD_NBD_ROOT="/sys/module/nbd"
 
 . ${LIBNBD_ROOT}/liblog.sh
 
+# VALID_NBD_DEV: no one use this nbd device
+# BUSY_NBD_DEV: nbd device already used
+declare -a ALL_NBD_DEV VALID_NBD_DEV BUSY_NBD_DEV
+
 __auto_modprobe_nbd() {
 	if [[ ! -e ${KMOD_NBD_ROOT} ]]; then
 		sudo modprobe nbd max_part=16 || error "modprobe nbd failed"
 	fi
 }
 
-all_dev_ndb() {
+# Auto probe nbd driver and statistic
+__auto_update_nbd_devices() {
+	local nbd
+
 	__auto_modprobe_nbd
 
-	local devices=( $(ls /dev/nbd* | grep -Eo 'nbd[0-9]+') )
-	echo ${devices[@]}
+	# Partitions not appear in /sys/block/
+	ALL_NBD_DEV=( $(ls /dev/nbd* | grep -Eo 'nbd[0-9]+') )
+
+	# Reset valid NBD
+	unset VALID_NBD_DEV
+	unset BUSY_NBD_DEV
+	for nbd in ${ALL_NBD_DEV[@]}
+	do
+		if [[ -e /sys/block/${nbd}/pid ]]; then
+			BUSY_NBD_DEV+=( ${nbd} )
+		else
+			VALID_NBD_DEV+=( ${nbd} )
+		fi
+	done
 }
 
+all_dev_ndb() {
+	__auto_modprobe_nbd
+	__auto_update_nbd_devices
+
+	echo ${ALL_NBD_DEV[@]}
+}
+
+# echo device with '/dev/' prefix
 nbd_find_idle_dev() {
+	__auto_update_nbd_devices
+
 	local dev
 	for dev in $(all_dev_ndb)
 	do
