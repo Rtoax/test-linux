a3abb6c4a44b9e536e2b4659e20aa09848bc8fe3
Rong Tao <rongtao@cestc.cn>
2026-08-19 09:33:43 +0800

    kmsg: write.sh: test log level
    $ journalctl -kf
    Aug 19 09:33:11 NUC10 unknown: test-linux[1506872] Wed Aug 19 09:33:11 AM CST 2026
    Aug 19 09:33:11 NUC10 unknown: test-linux[1506872] Wed Aug 19 09:33:11 AM CST 2026
    Aug 19 09:33:11 NUC10 unknown: test-linux[1506872] Wed Aug 19 09:33:11 AM CST 2026
    Aug 19 09:33:11 NUC10 unknown: test-linux[1506872] Wed Aug 19 09:33:11 AM CST 2026
    Aug 19 09:33:12 NUC10 unknown: test-linux[1506872] Wed Aug 19 09:33:11 AM CST 2026
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.8-200.fc44.x86_64, CoreUtils 9.10, bash 5.3.9
    Envs: Arch x86_64

--- device/kmsg/write.sh
+++ device/kmsg/write.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 set -e
 
-date | sudo tee /dev/kmsg
-cat /etc/os-release | sudo tee /dev/kmsg
+__kmsg_log() {
+	local lvl=$1
+	shift
+	echo "<${lvl}>test-linux[$$] ${@}" | sudo tee /dev/kmsg
+}
+
+kmsg_fatal() {
+	__kmsg_log 2 ${@}
+}
+
+kmsg_error() {
+	__kmsg_log 3 ${@}
+}
+
+kmsg_warn() {
+	__kmsg_log 4 ${@}
+}
+
+kmsg_info() {
+	__kmsg_log 6 ${@}
+}
+
+kmsg_debug() {
+	__kmsg_log 7 ${@}
+}
+
+kmsg_fatal `date`
+kmsg_error `date`
+kmsg_warn `date`
+kmsg_info `date`
+kmsg_debug `date`
