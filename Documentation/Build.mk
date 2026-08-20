2bbd9d8f615a6f0a60510814c38035dee948fb92
Rong Tao <rongtao@cestc.cn>
2026-08-20 17:35:11 +0800

    Doc: test abbrev.sh check and $1 in Build.mk
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.8-200.fc44.x86_64, LateXmk 4.88, Make 4.4.1, bash 5.3.9, Sphinx 8.2.3
    Envs: Arch x86_64

--- Documentation/Build.mk
+++ Documentation/Build.mk
@@ -4,4 +4,9 @@ subdir-y += images
 prep-y += htmldocs
 
 prog-y += abbrev.sh
+prog-y += abbrev.sh.1
+prog-y += abbrev.sh.2
 prog-y += history.sh
+
+PROG_ARGS_abbrev.sh.1 := -C --check
+PROG_ARGS_abbrev.sh.2 := BPF
