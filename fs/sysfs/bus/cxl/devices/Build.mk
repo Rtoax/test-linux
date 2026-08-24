3342e1a5c5f9d8595c1d6cc4d3671984f6fd7074
Rong Tao <rongtao@cestc.cn>
2026-08-24 10:42:17 +0000

    sysfs: cxl: print more /sys/bus/cxl/devices/mem0
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44 (Forty Four), Linux 6.14.11-300.fc42_cxl.x86_64, Libpmem 2.1.0, Make 4.4.1, NDCtl 85, bash 5.3.9
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, kvm

--- fs/sysfs/bus/cxl/devices/Build.mk
+++ fs/sysfs/bus/cxl/devices/Build.mk
@@ -1,1 +1,4 @@
+include file.mk
+
+subdir-$(call fexist,/sys/bus/cxl/devices/mem0) += mem0
 subdir-y += root0
---
+++ fs/sysfs/bus/cxl/devices/mem0/Build.mk
@@ -0,0 +1,6 @@
+subdir-y += pmem
+subdir-y += ram
+
+prog-y += payload_max.sh
+prog-y += serial.sh
+prog-y += numa_node.sh
