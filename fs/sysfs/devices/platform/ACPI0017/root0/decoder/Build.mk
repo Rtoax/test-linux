5550c8753afe4746db29cdeead9fb940b3aa3cc6
Rong Tao <rongtao@cestc.cn>
2026-08-24 08:26:07 +0000

    sysfs/cxl: add more about /sys/devices/platform/ACPI0017:00/root0/decoder0.0/region0
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44 (Forty Four), Linux 6.14.11-300.fc42_cxl.x86_64, Make 4.4.1, NDCtl 85, bash 5.3.9
    Envs: Arch x86_64, kvm

--- fs/sysfs/devices/platform/ACPI0017/root0/decoder/Build.mk
+++ fs/sysfs/devices/platform/ACPI0017/root0/decoder/Build.mk
@@ -4,3 +4,4 @@ subdir-$(call fexist,/sys/bus/cxl/devices/root0/decoder0.0/region0) += region
 
 prog-y += create_ram_region.sh
 prog-y += devtype.sh
+prog-y += target_list.sh
---
+++ fs/sysfs/devices/platform/ACPI0017/root0/decoder/region/Build.mk
@@ -0,0 +1,8 @@
+prog-y += devtype.sh
+prog-y += interleave_granularity.sh
+prog-y += interleave_ways.sh
+prog-y += mode.sh
+prog-y += resource.sh
+prog-y += size.sh
+prog-y += target.sh
+prog-y += uuid.sh
