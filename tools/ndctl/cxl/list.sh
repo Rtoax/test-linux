1f866e735c789820cd369be6a16d15404b329c31
Rong Tao <rongtao@cestc.cn>
2026-08-25 03:17:37 +0000

    cxl: list: 1-way vmem
        $ sudo cxl list --decoders
        [
          {
            "root decoders":[
              {
                "decoder":"decoder0.0",
                "resource":45365592064,
                "size":1073741824,
                "interleave_ways":1,
                "max_available_extent":1073741824,
                "pmem_capable":true,
                "volatile_capable":true,
                "accelmem_capable":true,
                "qos_class":0,
                "nr_targets":1
              }
            ]
          },
          {
            "port decoders":[
              {
                "decoder":"decoder1.0",
                "resource":45634027520,
                "size":536870912,
                "interleave_ways":1,
                "nr_targets":1
              }
            ]
          }
        ]
        $ sudo cxl list --decoders --decoder decoder0.0
        [
          {
            "decoder":"decoder0.0",
            "resource":45365592064,
            "size":1073741824,
            "interleave_ways":1,
            "max_available_extent":1073741824,
            "pmem_capable":true,
            "volatile_capable":true,
            "accelmem_capable":true,
            "qos_class":0,
            "nr_targets":1
          }
        ]
        $ sudo cxl list --memdevs --decoder decoder0.0
        [
          {
            "memdev":"mem0",
            "ram_size":1073741824,
            "serial":9707,
            "host":"0000:0c:00.0",
            "firmware_version":"BWFW VERSION 00",
            "poison_injectable":true
          }
        ]
        $ sudo cxl list --decoders --decoder decoder1.0
        [
          {
            "decoder":"decoder1.0",
            "resource":45634027520,
            "size":536870912,
            "interleave_ways":1,
            "nr_targets":1
          }
        ]
        $ sudo cxl list --memdevs --decoder decoder1.0
        [
          {
            "memdev":"mem0",
            "ram_size":1073741824,
            "serial":9707,
            "host":"0000:0c:00.0",
            "firmware_version":"BWFW VERSION 00",
            "poison_injectable":true
          }
        ]
        $ sudo cxl list --memdev mem0
        [
          {
            "memdev":"mem0",
            "ram_size":1073741824,
            "serial":9707,
            "host":"0000:0c:00.0",
            "firmware_version":"BWFW VERSION 00",
            "poison_injectable":true
          }
        ]
        $ sudo ndctl list --buses
        [
          {
            "provider":"CXL",
            "dev":"ndbus0"
          }
        ]
        $ sudo ndctl list --dimms
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44 (Forty Four), Linux 6.14.11-300.fc42_cxl.x86_64, Jq 1.8, NDCtl 85, bash 5.3.9
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, kvm

--- tools/ndctl/cxl/list.sh
+++ tools/ndctl/cxl/list.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 set -e
 
+readonly ROOTDECODERS=( $(sudo cxl list --decoders | jq -r '.[] | .["root decoders"][]? | .decoder') )
+readonly PORTDECODERS=( $(sudo cxl list --decoders | jq -r '.[] | .["port decoders"][]? | .decoder') )
 readonly MEMDEVS=( $(sudo cxl list --memdevs | jq -r '.[].memdev') )
 
+run() {
+	echo -e "\033[1;32m$ ${@}\033[m"
+	eval "${@}"
+}
+
 # Check info, see commit 5a32ccae21a6 ("cxl: cxl.sh: check decoder informations")
-sudo cxl list -D
-sudo cxl list -D -d decoder0.0
-sudo cxl list -M -d decoder0.0
+run sudo cxl list --decoders
+
+for decoder in ${ROOTDECODERS[@]} ${PORTDECODERS[@]}
+do
+	run sudo cxl list --decoders --decoder ${decoder}
+	run sudo cxl list --memdevs --decoder ${decoder}
+done
+
+for memdev in ${MEMDEVS[@]}
+do
+	run sudo cxl list --memdev ${memdev}
+done
+
 # Bus and dimm
 # commit a8b5d8d7094b ("cxl: ndctl list: shows bus and dimm info")
-sudo ndctl list -BD
-
+run sudo ndctl list --buses
+run sudo ndctl list --dimms
