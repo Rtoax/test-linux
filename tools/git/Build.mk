5bf5bac6584a1d9cc0ac56bd8ca57d9c4014b8bb
Rong Tao <rongtao@cestc.cn>
2026-08-26 14:37:02 +0800

    git/hooks/pre-commit: skip broken-symlinks that no-belongs to git-repo and ignored
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.10-200.fc44.x86_64, Findutils 4.10.0, Git 2.55.0, Make 4.4.1, bash 5.3.9
    Envs: Arch x86_64

--- tools/git/Build.mk
+++ tools/git/Build.mk
@@ -1,3 +1,5 @@
+prog-y += check-ignore.sh
 prog-y += diff.sh
 prog-y += log.sh
+prog-y += ls-files.sh
 prog-y += rev-list.sh
---
+++ tools/git/check-ignore.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+touch a.out
+git check-ignore a.out
