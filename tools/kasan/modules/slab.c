fa000abac4bd53db140058b7439ec511ce0bd5fe
Rong Tao <rongtao@cestc.cn>
2026-08-27 10:06:55 +0800

    slab: linux v6.7-rc1 remove slab_def.h and header api not contains kmem_cache
    Linux: commit 16a1d968358a ("mm/slab: remove mm/slab.c and slab_def.h")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44 (Workstation Edition), Linux 7.1.10-200.fc44.x86_64, GCC 16.2.1, GLibc 2.43
    Envs: Arch x86_64

--- tools/kasan/modules/slab.c
+++ tools/kasan/modules/slab.c
@@ -1,20 +1,17 @@
-/**
- *	File	slab.c
- *	Time	2021.11.05
- *	Author	Rong Tao <rtoax@foxmail.com>
- *
- *	Modify:
- *	2022-04-29	KASAN Testing <rtoax@foxmail.com>
- */
-
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/version.h>
 #include <linux/printk.h>
 #include <linux/slab.h>
+/**
+ * linux v6.7-rc1-9-g16a1d968358a
+ * commit 16a1d968358a ("mm/slab: remove mm/slab.c and slab_def.h")
+ */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 7, 0)
 #include <linux/slab_def.h>
+#endif
 #include <linux/gfp.h>
-
 #include "kasan.h"
 
 struct test_struct {
@@ -36,23 +33,16 @@ void test_constructor(void *data)
 }
 
 void print_kmem_cache(struct kmem_cache *cache)
-{	
+{
 	printk("%-5s %-5s %-8s %-4s\n", "NUM", "SIZE", "OBJSIZE", "REF");
-	printk("%-5d %-5d %-8d %-4d\n", 
-			test_slab->num, 
-			test_slab->size,
-			test_slab->object_size,
-			test_slab->refcount);
-
+	printk("%-5d %-5d %-8d %-4d\n", test_slab->num, test_slab->size,
+	       test_slab->object_size, test_slab->refcount);
 }
 
 static int create_slab(void)
 {
-	test_slab = kmem_cache_create(SLAB_NAME, 
-				sizeof(struct test_struct),
-				0, 
-				SLAB_PANIC, 
-				test_constructor);
+	test_slab = kmem_cache_create(SLAB_NAME, sizeof(struct test_struct), 0,
+				      SLAB_PANIC, test_constructor);
 	if (!test_slab) {
 		pr_err("Create slab fail.\n");
 		return -1;
@@ -72,7 +62,7 @@ static void test__slab(void)
 	}
 
 	for (i = 0; i < SLAB_ELEM; i++) {
-		datas[i] = kmem_cache_alloc(test_slab, GFP_KERNEL);	
+		datas[i] = kmem_cache_alloc(test_slab, GFP_KERNEL);
 	}
 
 	print_kmem_cache(test_slab);
@@ -102,7 +92,7 @@ static int kernel_init(void)
 	printk(KERN_INFO "my init.\n");
 	create_slab();
 	test__slab();
-	
+
 	printk("count_ctor = %d\n", count_ctor);
 	return 0;
 }
