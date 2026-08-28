157695e768f3056b6cb0ac64f3f9268f23e7a6c8
Rong Tao <rongtao@cestc.cn>
2026-08-28 06:00:29 +0000

    cxl: list.sh: move 'sudo' into run()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44 (Forty Four), Linux 6.14.11-300.fc42_cxl.x86_64, NDCtl 85, bash 5.3.9
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, kvm

--- tools/ndctl/cxl/list.sh
+++ tools/ndctl/cxl/list.sh
@@ -9,32 +9,32 @@ readonly MY_ROOT=$(dirname $(realpath $0))
 . ${MY_ROOT}/../../../scripts/libcxl.sh
 
 run() {
-	echo -e "\033[1;32m$ ${@}\033[m"
-	eval "${@}"
+	echo -e "\033[1;32m$ sudo ${@}\033[m"
+	eval sudo "${@}"
 }
 
 # commit cf0a146594a4 ("cxl: list verbose of: 4-ways pmem + 4-ways vmem + 4-ways vmem with dynamic capacity")
-run sudo cxl list --verbose
+run cxl list --verbose
 
 # Check info, see commit 5a32ccae21a6 ("cxl: cxl.sh: check decoder informations")
-run sudo cxl list --decoders
+run cxl list --decoders
 
 for decoder in ${ROOTDECODERS[@]} ${PORTDECODERS[@]} ${DECODERS[@]}
 do
-	run sudo cxl list --decoders --decoder ${decoder}
-	run sudo cxl list --memdevs --decoder ${decoder}
+	run cxl list --decoders --decoder ${decoder}
+	run cxl list --memdevs --decoder ${decoder}
 done
 
 for memdev in ${ALL_MEMDEVS[@]}
 do
-	run sudo cxl list --memdev ${memdev} --health
+	run cxl list --memdev ${memdev} --health
 done
 
 # Bus and dimm
 # commit a8b5d8d7094b ("cxl: ndctl list: shows bus and dimm info")
 # commit 79942b178114 ("cxl: list: cxl list --buses .vs. ndctl list --buses")
-run sudo cxl list --buses
-run sudo ndctl list --buses
-run sudo ndctl list --dimms
+run cxl list --buses
+run ndctl list --buses
+run ndctl list --dimms
 
 cxl_info_all
