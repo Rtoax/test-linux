97277109504a3579a8e8e474b0a225e19e880827
Rong Tao <rongtao@cestc.cn>
2026-09-02 19:36:01 +0800

    bpftrace/samples: config: test more config:: fields
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 24.04.4 LTS, Linux 6.17.0-1014-nvidia, BPFTrace 0.27.0, LibBpf 1.3.0, Make 4.3
    Envs: CPU: Cortex-X925 Cortex-A725, Arch aarch64

--- bpf/bpftrace/samples/config/Build.mk
+++ bpf/bpftrace/samples/config/Build.mk
@@ -1,4 +1,22 @@
 bpftrace-y += base.bt
 bpftrace-y += __builtin_config.bt
 bpftrace-y += config.bt
+
+# Fields
+bpftrace-y += cpp_demangle.bt
+bpftrace-y += lazy_symbolication.bt
+bpftrace-y += license.bt
+bpftrace-y += log_size.bt
+bpftrace-y += max_bpf_progs.bt
+bpftrace-y += max_cat_bytes.bt
+bpftrace-y += max_map_keys.bt
+bpftrace-y += max_probes.bt
 bpftrace-y += max_strlen.bt
+bpftrace-y += missing_probes.bt
+bpftrace-y += on_stack_limit.bt
+bpftrace-y += perf_rb_pages.bt
+bpftrace-y += print_maps_on_exit.bt
+bpftrace-y += show_debug_info.bt
+bpftrace-y += stack_mode.bt
+bpftrace-y += str_trunc_trailer.bt
+bpftrace-y += use_blazesym.bt
