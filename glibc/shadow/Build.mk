773b8537ba03e8706e55d561961cdd9b9349ac5a
Rong Tao <rongtao@cestc.cn>
2026-09-02 11:09:03 +0800

    glibc/shadow: try run with admin
    [rongtao@thinkForce shadow]$ make
     PROG             .output/getspnam.prog.log
    getspnam: Permission denied
    ERROR: getspnam: run failed, exit with 1 (expect 0)
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: openEuler 24.03 (LTS), Linux 6.6.0-28.0.0.34.oe2403.aarch64, GLibc 2.38, Make 4.4.1
    Envs: Arch aarch64

--- glibc/shadow/Build.mk
+++ glibc/shadow/Build.mk
@@ -1,7 +1,9 @@
-target-y := getspent
+target-y += getspent
 target-y += getspnam
 
 prog-y += $(filter-out getspent, ${target-y})
+
+PROG_ENVS_getspnam := --maybe-sudo
 
 getspent-objs := shadow_helpers.o
 getspnam-objs := shadow_helpers.o
