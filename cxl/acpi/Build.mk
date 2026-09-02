67c677e43049cfd1f2af8bd15ec1bbb8db33faff
Rong Tao <rongtao@cestc.cn>
2026-09-02 07:51:50 +0000

    cxl/acpi: Build.mk: auto test cedt and acpi0016
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Fedora Linux 44 (Forty Four), Linux 6.14.11-300.fc42_cxl.x86_64, acpica-tools 20260408, ACPITool 0.5.1, EDK-II 2, Linux-FirmWare 20260622-1, Make 4.4.1, NDCtl 85
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64, kvm

--- cxl/acpi/Build.mk
+++ cxl/acpi/Build.mk
@@ -6,5 +6,9 @@ target-y += cedt
 cedt-objs := chbs.o cfmws.o cxims.o rdpas.o
 
 prog-$(call fexist,/sys/bus/acpi/devices/ACPI0017:00) += info.sh
+prog-$(call fexist,/sys/firmware/acpi/tables/DSDT) += acpi0016
+prog-$(call fexist,/sys/firmware/acpi/tables/CEDT) += cedt
+
+RUNPROG_ARGS := --maybe-sudo
 
 CFLAGS := -I ../../libs/
