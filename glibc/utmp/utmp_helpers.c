c02331a3c7d4a7eeba9a0c44037a4a5564e16f78
Rong Tao <rongtao@cestc.cn>
2026-09-07 21:51:53 +0800

    glibc/utmp: utmp_helpers.c: return -EINVAL if pass NUL
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Ubuntu 26.04.1 LTS, Linux 7.0.0-31-generic, GCC 15.2.0, GLibc 2.43
    Envs: Arch x86_64

--- glibc/utmp/utmp_helpers.c
+++ glibc/utmp/utmp_helpers.c
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+#include <errno.h>
 #include <stdio.h>
 #include <utmp.h>
 #include "utmp_helpers.h"
 
-void print_utmp(struct utmp *ut)
+int print_utmp(struct utmp *ut)
 {
 	if (!ut) {
 		fprintf(stderr, "try print NULL utmp.\n");
-		return;
+		return -EINVAL;
 	}
 
 	printf("ut_type: %d\n", ut->ut_type);
@@ -15,5 +16,6 @@ void print_utmp(struct utmp *ut)
 	printf("ut_line: %s\n", ut->ut_line);
 	printf("ut_id: %s\n", ut->ut_id);
 	printf("ut_host: %s\n", ut->ut_host);
+	return 0;
 }
 
