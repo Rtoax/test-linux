5e5fa50df55e24e97465ec3af5abd068e5461a8f
Rong Tao <rongtao@cestc.cn>
2026-09-07 16:47:42 +0800

    git: refs.sh: git 2.27.0 not support 'refs'
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Vers: Kylin Linux Advanced Server V10 (Lance), Linux 4.19.90-52.66.v2207.ky10.x86_64, Git 2.27.0, Make 4.3, bash 5.0.18
    Envs: Arch x86_64, kvm

--- tools/git/Build.mk
+++ tools/git/Build.mk
@@ -1,6 +1,8 @@
+include git.mk
+
 prog-y += check-ignore.sh
 prog-y += diff.sh
 prog-y += log.sh
 prog-y += ls-files.sh
-prog-y += refs.sh
+prog-$(call git_gt,2,27,0) += refs.sh
 prog-y += rev-list.sh
