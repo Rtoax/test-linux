fb5a7fd149a86bc5327dc8f9dcf78939cde438b7
Rong Tao <rtoax@foxmail.com>
2026-09-11 02:55:32 -0400

    gdb/samples: Build.mk: skip mmap-file.sh test if gdb <= 8.2
    $ ./mmap-file.sh
    ....
    Breakpoint 1, main (argc=1, argv=0x7fffffffe198) at hello.c:43
    43		if (argc > 1) {
    mmap-file.gdb:8: Error in sourced command file:
    No symbol "fstat" in current context.
    Missing separate debuginfos, use: yum debuginfo-install glibc-2.28-164.el8.x86_64
    (gdb)
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    OS: CentOS Linux 8
    Linux: 4.18.0-348.el8.x86_64
    Vers: GDB 8.2, GLibc 2.28, Make 4.2.1, bash 4.4.20
    Envs: Arch x86_64, kvm

--- gnu/gdb/samples/Build.mk
+++ gnu/gdb/samples/Build.mk
@@ -1,3 +1,5 @@
+include gdb.mk
+
 target-y += set-value
 target-y += set-value-func
 target-y += set-value-volatile
@@ -11,7 +13,35 @@ target-cpp-y := global-var
 
 prep-y := hello.strip
 
-prog-y := $(shell ls *.sh)
+prog-y += args.sh
+prog-y += break.sh
+prog-y += define.sh
+prog-y += directory.sh
+prog-y += disassemble.sh
+prog-y += dump-memory.sh
+prog-y += finish.sh
+prog-y += fork.sh
+prog-y += frame.sh
+prog-y += global-var.sh
+prog-y += info-address.sh
+prog-y += info.sh
+prog-y += info-symbol.sh
+prog-y += layout.sh
+prog-y += list.sh
+prog-$(call gdb_gt,8,2) += mmap-file.sh
+prog-y += print.sh
+prog-y += pthread-gdb.sh
+prog-y += ptype.sh
+prog-y += set-value.sh
+prog-y += show.sh
+prog-y += signal.sh
+prog-y += symbol-file.sh
+prog-y += symbol-value.sh
+prog-y += until.sh
+prog-y += watch.sh
+prog-y += whatis.sh
+prog-y += while.sh
+prog-y += x.sh
 
 PROG_ENVS_layout.sh := --tty
 
