1edd7cb0ef77ebe506c59b6373389dd3c6f2ea66
Rong Tao <rongtao@cestc.cn>
2026-09-15 09:37:24 +0800

    git: add log subdir
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    OS: Fedora Linux 44 (Workstation Edition)
    Linux: 7.2.5-200.fc44.x86_64
    Vers: Git 2.55.0, Make 4.4.1, bash 5.3.9
    Envs: Arch x86_64

--- tools/git/Build.mk
+++ tools/git/Build.mk
@@ -1,8 +1,9 @@
 include git.mk
 
+subdir-y += log
+
 prog-y += check-ignore.sh
 prog-y += diff.sh
-prog-y += log.sh
 prog-y += ls-files.sh
 prog-$(call git_gt,2,27,0) += refs.sh
 prog-y += rev-list.sh
--- tools/git/log/log.sh
+++ tools/git/log/log.sh
@@ -6,7 +6,8 @@ git log v2.3.16^..v2.3.17 -p ../../Makefile | cat
 git log v2.4.6-Kappa..v2.4.7-Lamada --oneline | tee v2.4.6-Kappa..v2.4.7-Lamada.log
 git log 6d1db619e0f2..v2.4.7-Lamada --oneline | tee 6d1db619e0f2..v2.4.7-Lamada.log
 git log ${LAST_TAG}..HEAD --oneline | tee ${LAST_TAG}..HEAD.log
-
+git log -1 --oneline
+git log -2 --oneline
 
 # Do some checks
 if [[ "$(diff -up v2.4.6-Kappa..v2.4.7-Lamada.log 6d1db619e0f2..v2.4.7-Lamada.log)" ]]; then
