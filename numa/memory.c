1f88119a660e51b66914e9bd4e48e86760e8e6b9
Rong Tao <rongtao@cestc.cn>
2026-09-16 09:52:54 +0800

    numa: memory.c: add commit comment
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    OS: openEuler 24.03 (LTS)
    Linux: 6.6.0-28.0.0.34.oe2403.aarch64
    Vers: GCC 12.3.1, GLibc 2.38, NumaCTL 2.0.16
    Envs: CPU: Cortex-A77, Arch aarch64

--- numa/memory.c
+++ numa/memory.c
@@ -6,7 +6,9 @@
  * Examples:
  * - commit a49afb708c0c ("numa: memory.c: Kunpeng 920")
  * - commit dd97054cbcf6 ("numa: memory.c: Phytium 5000C")
- * - ThinkForce (DSU 2.5 CMN 1.8) Cortex-A77, TF7180(with NPU) 14 CPU @ 2.5GHz
+ * - commit cbddd8ff6b6d ("numa: memory.c: ThinkForce (DSU 2.5 CMN 1.8)
+ *                                         Cortex-A77
+ *                                         TF7180(with NPU) 14 CPU @ 2.5GHz")
  */
 #include <stdio.h>
 #include <malloc.h>
