ead9e2e0c6435836ac6cbca3749467c30f6478b7
Rong Tao <rongtao@cestc.cn>
2026-09-16 09:22:13 +0800

    openssl.mk: use make cache to speed up
    Including ./openssl.mk
    Include ./openssl.mk took 2208 ms
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    OS: openEuler 24.03 (LTS)
    Linux: 6.6.0-28.0.0.34.oe2403.aarch64
    Vers: Make 4.4.1, OpenSSL 3.0.12, bash 5.2.15
    Envs: Arch aarch64

--- template/openssl.mk
+++ template/openssl.mk
@@ -10,24 +10,49 @@ ifndef _OPENSSL_MK
 _OPENSSL_MK = 1
 
 include define.mk
+include dir.mk
+include file.mk
 
 $(call find_cmd_and_def,openssl)
 
+openssl_mk_cachefile := ${TOPDIR}/template/.openssl.mk.cache
+openssl_mk_origfile := ${TOPDIR}/template/openssl.mk
+
+ifeq ($(call is_newer,${openssl_mk_cachefile},${openssl_mk_origfile}),y)
+  include ${openssl_mk_cachefile}
+else
+
+include bits/mk-cache.mk
+
+$(call make_gen_cachefile,${openssl_mk_cachefile})
+
 ifneq (${OPENSSL},)
+  sslversh = ${TOPDIR}/scripts/version/openssl.sh
 
-include dir.mk
+  OPENSSL_VERSION := $(shell ${sslversh})
+  OPENSSL_MAJOR := $(shell ${sslversh} --major)
+  OPENSSL_MINOR := $(shell ${sslversh} --minor)
+  OPENSSL_PATCH := $(shell ${sslversh} --patchlevel)
 
-sslversh = ${TOPDIR}/scripts/version/openssl.sh
+  # $1: output pem file name
+  define openssl_genrsa
+  ${Q}${OPENSSL} genrsa -out $(1) -3 3072
+  endef
 
-OPENSSL_VERSION := $(shell ${sslversh})
-OPENSSL_MAJOR := $(shell ${sslversh} --major)
-OPENSSL_MINOR := $(shell ${sslversh} --minor)
-OPENSSL_PATCH := $(shell ${sslversh} --patchlevel)
+  ifneq (${OPENSSL_VERSION},${OPENSSL_MAJOR}.${OPENSSL_MINOR}.${OPENSSL_PATCH})
+    $(error ${OPENSSL_VERSION} != ${OPENSSL_MAJOR}.${OPENSSL_MINOR}.${OPENSSL_PATCH})
+  endif
 
-# $1: output pem file name
-define openssl_genrsa
-${Q}${OPENSSL} genrsa -out $(1) -3 3072
-endef
+endif # end of HAVE_OPENSSL
+
+$(call make_append_var_to_file,OPENSSL_VERSION,${openssl_mk_cachefile})
+$(call make_append_var_to_file,OPENSSL_MAJOR,${openssl_mk_cachefile})
+$(call make_append_var_to_file,OPENSSL_MINOR,${openssl_mk_cachefile})
+$(call make_append_var_to_file,OPENSSL_PATCH,${openssl_mk_cachefile})
+
+endif # end of cache
+
+export OPENSSL_VERSION OPENSSL_MAJOR OPENSSL_MINOR OPENSSL_PATCH
 
 ifdef DEBUG
   $(info OPENSSL_VERSION ${OPENSSL_VERSION})
@@ -35,13 +60,5 @@ ifdef DEBUG
   $(info OPENSSL_MINOR ${OPENSSL_MINOR})
   $(info OPENSSL_PATCH ${OPENSSL_PATCH})
 endif
-
-ifneq (${OPENSSL_VERSION},${OPENSSL_MAJOR}.${OPENSSL_MINOR}.${OPENSSL_PATCH})
-  $(error ${OPENSSL_VERSION} != ${OPENSSL_MAJOR}.${OPENSSL_MINOR}.${OPENSSL_PATCH})
-endif
-
-export OPENSSL_VERSION OPENSSL_MAJOR OPENSSL_MINOR OPENSSL_PATCH
-
-endif # end of HAVE_OPENSSL
 
 endif
