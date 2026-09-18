ffaa760a4a9a99c10d484ce83941d1bcb0368beb
Rong Tao <rongtao@cestc.cn>
2026-09-18 09:51:13 +0800

    cxl: vm.sh: add CUSTOM=<4|big-vmem>
    FIXME: in guest os:
    
    $ sudo cxl create-region --decoder decoder0.0 --size 4096M --type ram --memdevs mem0 mem1 mem2 mem3
    cxl region: create_region: region0: failed to set target2 to mem2
    cxl region: cmd_create_region: created 0 regions
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    OS: Fedora Linux 44 (Workstation Edition)
    Linux: 7.2.5-200.fc44.x86_64
    Vers: NDCtl 85, bash 5.3.9
    Envs: CPU: Intel(R) Core(TM) i7-10710U CPU @ 1.10GHz, Arch x86_64

--- cxl/vm.sh
+++ cxl/vm.sh
@@ -199,6 +199,24 @@ custom_cxl_3() {
 	cxlargs+=( --cxl pmem=pmem.1,bus=sw${lv}.2,lsa=pmem.1.lsa )
 }
 
+# FIXME: Could not create region in guest os.
+custom_cxl_4_big_vmem() {
+	cxlargs+=( --cxl pxb=pxb.1 )
+	cxlargs+=( --cxl pxb=pxb.2 )
+	cxlargs+=( --cxl pxb=pxb.3 )
+	cxlargs+=( --cxl pxb=pxb.4 )
+
+	cxlargs+=( --cxl rp=rp.1,bus=pxb.1,port=1 )
+	cxlargs+=( --cxl rp=rp.2,bus=pxb.2,port=1 )
+	cxlargs+=( --cxl rp=rp.3,bus=pxb.3,port=1 )
+	cxlargs+=( --cxl rp=rp.4,bus=pxb.4,port=1 )
+
+	cxlargs+=( --cxl vmem=vmem.1,bus=rp.1,lsa=vmem.1.lsa,size=32GB )
+	cxlargs+=( --cxl vmem=vmem.2,bus=rp.2,lsa=vmem.2.lsa,size=32GB )
+	cxlargs+=( --cxl vmem=vmem.3,bus=rp.3,lsa=vmem.3.lsa,size=32GB )
+	cxlargs+=( --cxl vmem=vmem.4,bus=rp.4,lsa=vmem.4.lsa,size=32GB )
+}
+
 case ${CUSTOM} in
 1)
 	custom_cxl_1
@@ -209,9 +227,16 @@ case ${CUSTOM} in
 3 | multi-level-switch)
 	custom_cxl_3
 	;;
+4 | big-vmem)
+	custom_cxl_4_big_vmem
+	;;
 "")
 	CUSTOM=cxl-vmem-4way
 	cxlargs+=( --cxl device=${CUSTOM} )
+	;;
+help | "?")
+	echo "CUSTOM=[1|2|<3|multi-level-switch>|<4|big-vmem>]"
+	exit 1
 	;;
 *)
 	cxlargs+=( --cxl device=${CUSTOM} )
