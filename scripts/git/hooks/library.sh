788482966512b82e857e5800d35f17c561ab927e
Rong Tao <rtoax@foxmail.com>
2026-09-23 09:58:43 +0800

    git/hooks: library.sh: add try_xxx
    Signed-off-by: Rong Tao <rtoax@foxmail.com>
    OS: Kylin Linux Advanced Server V10 (GFB)
    Linux: 4.19.90-52.23.v2207.gfb01.ky10.x86_64
    Vers: Git 2.27.0, bash 5.0.18
    Envs: Arch x86_64

--- scripts/git/hooks/library.sh
+++ scripts/git/hooks/library.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Copyright (C) 2026 Rong Tao. All rights reserved.
 
+readonly GIT_TOPDIR=$(git rev-parse --show-toplevel 2>/dev/null || :)
+
+declare -a try_add try_del try_modify try_rename try_all
+
 # $1: is amend commit
 git_file_status() {
 	if [[ -n ${1} ]]; then
@@ -8,4 +12,17 @@ git_file_status() {
 	else
 		git status --short
 	fi
+}
+
+# $1: is amend commit
+statistic_file_changes() {
+	local is_amend=${1}
+
+	pushd ${GIT_TOPDIR} >/dev/null
+	try_add=( $(git_file_status ${is_amend} | grep ^A | awk '{print $2}' || true) )
+	try_del=( $(git_file_status ${is_amend} | grep ^D | awk '{print $2}' || true) )
+	try_modify=( $(git_file_status ${is_amend} | grep ^M | awk '{print $2}' || true) )
+	try_rename=( $(git_file_status ${is_amend} | grep ^R | awk '{print $2}' || true) )
+	try_all=( ${try_add[@]} ${try_del[@]} ${try_modify[@]} ${try_rename} )
+	popd >/dev/null
 }
