36253a3e0ceb8c5a5c6751edf743e2d819000b6a
Rong Tao <rongtao@cestc.cn>
2026-09-23 14:37:23 +0800

    openmp: _OPENMP macro
    Doc: hpc: update openmp.rst
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    OS: Fedora Linux 44 (Workstation Edition)
    Linux: 7.2.5-200.fc44.x86_64
    Vers: GCC 16.2.1, GLibc 2.43, LateXmk 4.88, Python 3.14.7, Sphinx 8.2.3
    Envs: Arch x86_64

--- compiler/openmp/samples/_OPENMP.c
+++ compiler/openmp/samples/_OPENMP.c
@@ -1,19 +1,23 @@
 /**
- * _OPENMP:
- * 200505：OpenMP 2.5
- * 200805：OpenMP 3.0
- * 201107：OpenMP 3.1
- * 201307：OpenMP 4.0
- * 201511：OpenMP 4.5
- * 201811：OpenMP 5.0
- * 202011：OpenMP 5.1
- * 202111：OpenMP 5.2
+ * _OPENMP (sync Doc::openmp.rst)
+ *
+ * - 200505：OpenMP 2.5
+ * - 200805：OpenMP 3.0
+ * - 201107：OpenMP 3.1
+ * - 201307：OpenMP 4.0
+ * - 201511：OpenMP 4.5
+ * - 201811：OpenMP 5.0
+ * - 202011：OpenMP 5.1
+ * - 202111：OpenMP 5.2
  */
 #include <stdio.h>
 #include <omp.h>
 
 int main(int argc, char *argv[])
 {
+#ifdef __STDC_VERSION__
+	printf("__STDC_VERSION__ = %ld, ", __STDC_VERSION__);
+#endif
 	printf("_OPENMP = %d\n", _OPENMP);
 	return 0;
 }
